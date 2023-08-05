fa046b48894407a5755a38cc76ac7ac16bd9be24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package django-qrmixin/1.0.1/django-qrmixin-1.0.1.tar.gz

---
+++ packages/django-qrmixin/django-qrmixin-1.0.1.tar.gz/django-qrmixin-1.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django-qrmixin',
+    version='1.0.1',
+    description="Provides a QR code generating mixin for django models",
+    author="Danemco, LLC",
+    author_email='dev@velocitywebworks.com',
+    url='https://bitbucket.org/freakypie/django-qrmixin',
+    packages=find_packages(),
+    install_requires=['qrcode'],
+)
