73208ea028c48e5009c0768cf8f6bee85beba1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:25 +0000

    Add package pycaption/0.3.6/pycaption-0.3.6.tar.gz

---
+++ packages/pycaption/pycaption-0.3.6.tar.gz/pycaption-0.3.6/pycaption/dfxp.py
@@ -0,0 +1,291 @@
+from bs4 import BeautifulSoup, NavigableString
+from xml.sax.saxutils import escape
+
+from .base import (
+    BaseReader, BaseWriter, CaptionSet, Caption, CaptionNode,
+    force_byte_string, DEFAULT_LANGUAGE_CODE)
+from .exceptions import CaptionReadNoCaptions
+
+
+DFXP_BASE_MARKUP = '''
+<tt xmlns="http://www.w3.org/ns/ttml"
+    xmlns:tts="http://www.w3.org/ns/ttml#styling">
+    <head>
+        <styling/>
+    </head>
+    <body/>
+</tt>
+'''
+
+
+class DFXPReader(BaseReader):
+    def __init__(self, *args, **kw):
+        self.nodes = []
+
+    def detect(self, content):
+        if '</tt>' in content.lower():
+            return True
+        else:
+            return False
+
+    def read(self, content):
+        content = force_byte_string(content)
+        dfxp_soup = BeautifulSoup(content)
+        captions = CaptionSet()
+
+        # Each div represents all the captions for a single language.
+        for div in dfxp_soup.find_all('div'):
+            lang = div.attrs.get('xml:lang', DEFAULT_LANGUAGE_CODE)
+            captions.set_captions(lang, self._translate_div(div))
+
+        for style in dfxp_soup.find_all('style'):
+            id = style.attrs.get('id')
+            if not id:
+                id = style.attrs.get('xml:id')
+            captions.add_style(id, self._translate_style(style))
+
+        captions = self._combine_matching_captions(captions)
+
+        if captions.is_empty():
+            raise CaptionReadNoCaptions("empty caption file")
+
+        return captions
+
+    def _translate_div(self, div):
+        captions = []
+        for p_tag in div.find_all('p'):
+            captions.append(self._translate_p_tag(p_tag))
+        return captions
+
+    def _translate_p_tag(self, p_tag):
+        start, end = self._find_times(p_tag)
+        self.nodes = []
+        self._translate_tag(p_tag)
+        styles = self._translate_style(p_tag)
+
+        caption = Caption()
+        caption.start = start
+        caption.end = end
+        caption.nodes = self.nodes
+        caption.style = styles
+        return caption
+
+    def _find_times(self, p_tag):
+        start = self._translate_time(p_tag['begin'])
+
+        try:
+            end = self._translate_time(p_tag['end'])
+        except KeyError:
+            dur = self._translate_time(p_tag['dur'])
+            end = start + dur
+
+        return start, end
+
+    def _translate_time(self, stamp):
+        timesplit = stamp.split(':')
+        if not '.' in timesplit[2]:
+            timesplit[2] = timesplit[2] + '.000'
+        secsplit = timesplit[2].split('.')
+        if len(timesplit) > 3:
+            secsplit.append((int(timesplit[3]) / 30) * 100)
+        microseconds = (int(timesplit[0]) * 3600000000 +
+                        int(timesplit[1]) * 60000000 +
+                        int(secsplit[0]) * 1000000 +
+                        int(secsplit[1]) * 1000)
+        return microseconds
+
+    def _translate_tag(self, tag):
+        # convert text
+        if isinstance(tag, NavigableString):
+            if tag.strip() != '':
+                node = CaptionNode.create_text(tag.strip())
+                self.nodes.append(node)
+        # convert line breaks
+        elif tag.name == 'br':
+            self.nodes.append(CaptionNode.create_break())
+        # convert italics
+        elif tag.name == 'span':
+            # convert span
+            self._translate_span(tag)
+        else:
+            # recursively call function for any children elements
+            for a in tag.contents:
+                self._translate_tag(a)
+
+    def _translate_span(self, tag):
+        # convert tag attributes
+        args = self._translate_style(tag)
+        # only include span tag if attributes returned
+        if args != '':
+            node = CaptionNode.create_style(True, args)
+            node.start = True
+            node.content = args
+            self.nodes.append(node)
+
+            # recursively call function for any children elements
+            for a in tag.contents:
+                self._translate_tag(a)
+            node = CaptionNode.create_style(False, args)
+            node.start = False
+            node.content = args
+            self.nodes.append(node)
+        else:
+            for a in tag.contents:
+                self._translate_tag(a)
+
+    # convert style from DFXP
+    def _translate_style(self, tag):
+        attrs = {}
+        dfxp_attrs = tag.attrs
+        for arg in dfxp_attrs:
+            if arg == "style":
+                attrs['class'] = dfxp_attrs[arg]
+            elif arg == "tts:fontstyle" and dfxp_attrs[arg] == "italic":
+                attrs['italics'] = True
+            elif arg == "tts:textalign":
+                attrs['text-align'] = dfxp_attrs[arg]
+            elif arg == "tts:fontfamily":
+                attrs['font-family'] = dfxp_attrs[arg]
+            elif arg == "tts:fontsize":
+                attrs['font-size'] = dfxp_attrs[arg]
+            elif arg == "tts:color":
+                attrs['color'] = dfxp_attrs[arg]
+        return attrs
+
+    # Merge together captions that have the same start/end times.
+    def _combine_matching_captions(self, caption_set):
+        for lang in caption_set.get_languages():
+            captions = caption_set.get_captions(lang)
+            new_caps = captions[:1]
+
+            for caption in captions[1:]:
+                if (caption.start == new_caps[-1].start
+                        and caption.end == new_caps[-1].end):
+                    new_caps[-1].nodes.append(CaptionNode.create_break())
+                    new_caps[-1].nodes.extend(caption.nodes)
+                else:
+                    new_caps.append(caption)
+
+            caption_set.set_captions(lang, new_caps)
+
+        return caption_set
+
+
+class DFXPWriter(BaseWriter):
+    def __init__(self, *args, **kw):
+        self.p_style = False
+        self.open_span = False
+
+    def write(self, captions, force=''):
+        dfxp = BeautifulSoup(DFXP_BASE_MARKUP, 'xml')
+        dfxp.find('tt')['xml:lang'] = "en"
+
+        for style_id, style in captions.get_styles():
+            if style != {}:
+                dfxp = self._recreate_styling_tag(style_id, style, dfxp)
+
+        body = dfxp.find('body')
+
+        if force:
+            langs = [self._force_language(force, captions.get_languages())]
+        else:
+            langs = captions.get_languages()
+
+        for lang in langs:
+            div = dfxp.new_tag('div')
+            div['xml:lang'] = '%s' % lang
+
+            for caption in captions.get_captions(lang):
+                p = self._recreate_p_tag(caption, dfxp)
+                div.append(p)
+
+            body.append(div)
+
+        caption_content = dfxp.prettify(formatter=None)
+        return force_byte_string(caption_content)
+
+    # force the DFXP to only have one language, trying to match on "force"
+    def _force_language(self, force, langs):
+        for lang in langs:
+            if force == lang:
+                return lang
+
+        return langs[-1]
+
+    def _recreate_styling_tag(self, style, content, dfxp):
+        dfxp_style = dfxp.new_tag('style', id="%s" % style)
+
+        attributes = self._recreate_style(content, dfxp)
+        dfxp_style.attrs.update(attributes)
+
+        if dfxp_style != dfxp.new_tag('style', id="%s" % style):
+            dfxp.find('styling').append(dfxp_style)
+
+        return dfxp
+
+    def _recreate_p_tag(self, caption, dfxp):
+        start = caption.format_start()
+        end = caption.format_end()
+        p = dfxp.new_tag("p", begin=start, end=end)
+        p.string = self._recreate_text(caption, dfxp)
+
+        if dfxp.find("style", {"id": "p"}):
+            p['style'] = 'p'
+
+        p.attrs.update(self._recreate_style(caption.style, dfxp))
+
+        return p
+
+    def _recreate_text(self, caption, dfxp):
+        line = ''
+
+        for node in caption.nodes:
+            if node.type == CaptionNode.TEXT:
+                line += escape(node.content) + ' '
+
+            elif node.type == CaptionNode.BREAK:
+                line = line.rstrip() + '<br/>\n    '
+
+            elif node.type == CaptionNode.STYLE:
+                line = self._recreate_span(line, node, dfxp)
+
+        return line.rstrip()
+
+    def _recreate_span(self, line, node, dfxp):
+        if node.start:
+            styles = ''
+
+            content_with_style = self._recreate_style(node.content, dfxp)
+            for style, value in content_with_style.items():
+                styles += ' %s="%s"' % (style, value)
+
+            if styles:
+                if self.open_span:
+                    line = line.rstrip() + '</span> '
+                line += '<span%s>' % styles
+                self.open_span = True
+
+        elif self.open_span:
+            line = line.rstrip() + '</span> '
+            self.open_span = False
+
+        return line
+
+    def _recreate_style(self, content, dfxp):
+        dfxp_style = {}
+
+        if 'class' in content:
+            if dfxp.find("style", {"id": content['class']}):
+                dfxp_style['style'] = content['class']
+        if 'text-align' in content:
+            dfxp_style['tts:textAlign'] = content['text-align']
+        if 'italics' in content:
+            dfxp_style['tts:fontStyle'] = 'italic'
+        if 'font-family' in content:
+            dfxp_style['tts:fontfamily'] = content['font-family']
+        if 'font-size' in content:
+            dfxp_style['tts:fontsize'] = content['font-size']
+        if 'color' in content:
+            dfxp_style['tts:color'] = content['color']
+
+        return dfxp_style
---
+++ packages/pycaption/pycaption-0.3.6.tar.gz/pycaption-0.3.6/pycaption/sami.py
@@ -0,0 +1,512 @@
+from collections import deque
+from htmlentitydefs import name2codepoint
+from HTMLParser import HTMLParser, HTMLParseError
+from logging import FATAL
+from xml.sax.saxutils import escape
+
+from cssutils import parseString, log, css as cssutils_css
+from bs4 import BeautifulSoup, NavigableString
+
+from .base import (
+    BaseReader, BaseWriter, CaptionSet, Caption, CaptionNode,
+    force_byte_string, DEFAULT_LANGUAGE_CODE)
+from .exceptions import CaptionReadNoCaptions, CaptionReadSyntaxError
+
+
+# change cssutils default logging
+log.setLevel(FATAL)
+
+
+SAMI_BASE_MARKUP = '''
+<sami>
+    <head>
+        <style type="text/css"/>
+    </head>
+    <body/>
+</sami>'''
+
+
+class SAMIReader(BaseReader):
+    def __init__(self, *args, **kw):
+        self.line = []
+
+    def detect(self, content):
+        if '<sami' in content.lower():
+            return True
+        else:
+            return False
+
+    def read(self, content):
+        content = force_byte_string(content)
+        content, doc_styles, doc_langs = SAMIParser().feed(content)
+        sami_soup = BeautifulSoup(content)
+        captions = CaptionSet()
+        captions.set_styles(doc_styles)
+
+        for language in doc_langs:
+            lang_captions = self._translate_lang(language, sami_soup)
+            captions.set_captions(language, lang_captions)
+
+        if captions.is_empty():
+            raise CaptionReadNoCaptions("empty caption file")
+
+        return captions
+
+    def _translate_lang(self, language, sami_soup):
+        captions = []
+        milliseconds = 0
+
+        for p in sami_soup.select('p[lang|=%s]' % language):
+            milliseconds = int(float(p.parent['start']))
+            start = milliseconds * 1000
+            end = 0
+
+            if captions != [] and captions[-1].end == 0:
+                captions[-1].end = milliseconds * 1000
+
+            if p.get_text().strip():
+                self.line = []
+                self._translate_tag(p)
+                text = self.line
+                styles = self._translate_attrs(p)
+                caption = Caption()
+                caption.start = start
+                caption.end = end
+                caption.nodes = text
+                caption.style = styles
+                captions.append(caption)
+
+        if captions and captions[-1].end == 0:
+            # Arbitrarily make this last 4 seconds. Not ideal...
+            captions[-1].end = (milliseconds + 4000) * 1000
+
+        return captions
+
+    def _translate_tag(self, tag):
+        # convert text
+        if isinstance(tag, NavigableString):
+            self.line.append(CaptionNode.create_text(tag.strip()))
+        # convert line breaks
+        elif tag.name == 'br':
+            self.line.append(CaptionNode.create_break())
+        # convert italics
+        elif tag.name == 'i':
+            self.line.append(CaptionNode.create_style(True, {'italics': True}))
+            # recursively call function for any children elements
+            for a in tag.contents:
+                self._translate_tag(a)
+            self.line.append(
+                CaptionNode.create_style(False, {'italics': True}))
+        elif tag.name == 'span':
+            self._translate_span(tag)
+        else:
+            # recursively call function for any children elements
+            for a in tag.contents:
+                self._translate_tag(a)
+
+    def _translate_span(self, tag):
+        # convert tag attributes
+        args = self._translate_attrs(tag)
+        # only include span tag if attributes returned
+        if args != '':
+            node = CaptionNode.create_style(True, args)
+            self.line.append(node)
+            # recursively call function for any children elements
+            for a in tag.contents:
+                self._translate_tag(a)
+            node = CaptionNode.create_style(False, args)
+            self.line.append(node)
+        else:
+            for a in tag.contents:
+                self._translate_tag(a)
+
+    def _translate_attrs(self, tag):
+        attrs = {}
+        css_attrs = tag.attrs
+
+        if 'class' in css_attrs:
+            attrs['class'] = css_attrs['class'][0].lower()
+        if 'id' in css_attrs:
+            attrs['class'] = css_attrs['id'].lower()
+        if 'style' in css_attrs:
+            styles = css_attrs['style'].split(';')
+            attrs.update(self._translate_style(attrs, styles))
+
+        return attrs
+
+    # convert attributes from CSS
+    def _translate_style(self, attrs, styles):
+        for style in styles:
+            style = style.split(':')
+            if style[0] == 'text-align':
+                attrs['text-align'] = style[1].strip()
+            elif style[0] == 'font-family':
+                attrs['font-family'] = style[1].strip()
+            elif style[0] == 'font-size':
+                attrs['font-size'] = style[1].strip()
+            elif style[0] == 'font-style' and style[1].strip() == 'italic':
+                attrs['italics'] = True
+            elif style[0] == 'lang':
+                attrs['lang'] = style[1].strip()
+            elif style[0] == 'color':
+                attrs['color'] = style[1].strip()
+
+        return attrs
+
+
+class SAMIWriter(BaseWriter):
+    def __init__(self, *args, **kw):
+        self.open_span = False
+        self.last_time = None
+
+    def write(self, captions):
+        sami = BeautifulSoup(SAMI_BASE_MARKUP, "xml")
+        stylesheet = self._recreate_stylesheet(captions)
+        sami.find('style').append(stylesheet)
+        primary = None
+
+        for lang in captions.get_languages():
+            self.last_time = None
+            if primary is None:
+                primary = lang
+            for caption in captions.get_captions(lang):
+                sami = self._recreate_p_tag(
+                    caption, sami, lang, primary, captions)
+
+        a = sami.prettify(formatter=None).split('\n')
+        caption_content = '\n'.join(a[1:])
+        return force_byte_string(caption_content)
+
+    def _recreate_p_tag(self, caption, sami, lang, primary, captions):
+        time = caption.start / 1000
+
+        if self.last_time and time != self.last_time:
+            sami = self._recreate_blank_tag(
+                sami, caption, lang, primary, captions)
+
+        self.last_time = caption.end / 1000
+
+        sami, sync = self._recreate_sync(sami, lang, primary, time)
+
+        p = sami.new_tag("p")
+
+        p_style = ''
+        for attr, value in self._recreate_style(caption.style).items():
+            p_style += '%s:%s;' % (attr, value)
+        if p_style:
+            p['p_style'] = p_style
+
+        p['class'] = self._recreate_p_lang(caption, lang, captions)
+        p.string = self._recreate_text(caption.nodes)
+
+        sync.append(p)
+
+        return sami
+
+    def _recreate_sync(self, sami, lang, primary, time):
+        if lang == primary:
+            sync = sami.new_tag("sync", start="%s" % time)
+            sami.body.append(sync)
+        else:
+            sync = sami.find("sync", start="%s" % time)
+            if sync is None:
+                sami, sync = self._find_closest_sync(sami, time)
+
+        return sami, sync
+
+    def _find_closest_sync(self, sami, time):
+        sync = sami.new_tag("sync", start="%s" % time)
+
+        earlier = sami.find_all("sync", start=lambda x: int(x) < time)
+        if earlier:
+            last_sync = earlier[-1]
+            last_sync.insert_after(sync)
+        else:
+            def later_syncs(start):
+                return int(start) > time
+            later = sami.find_all("sync", start=later_syncs)
+            if later:
+                last_sync = later[0]
+                last_sync.insert_before(sync)
+        return sami, sync
+
+    def _recreate_blank_tag(self, sami, caption, lang, primary, captions):
+        sami, sync = self._recreate_sync(sami, lang, primary, self.last_time)
+
+        p = sami.new_tag("p")
+        p['class'] = self._recreate_p_lang(caption, lang, captions)
+        p.string = '&nbsp;'
+
+        sync.append(p)
+
+        return sami
+
+    def _recreate_p_lang(self, caption, lang, captions):
+        try:
+            if 'lang' in captions.get_style(caption.style['class']):
+                return caption.style['class']
+        except KeyError:
+            pass
+        return lang
+
+    def _recreate_stylesheet(self, captions):
+        stylesheet = '<!--'
+
+        for attr, value in captions.get_styles():
+            if value != {}:
+                stylesheet += self._recreate_style_tag(attr, value)
+
+        for lang in captions.get_languages():
+            if 'lang: %s' % lang not in stylesheet:
+                stylesheet += '\n    .%s {\n     lang: %s;\n    }\n' % (lang,
+                                                                        lang)
+
+        return stylesheet + '   -->'
+
+    def _recreate_style_tag(self, style, content):
+        if style not in ['p', 'sync', 'span']:
+            element = '.'
+        else:
+            element = ''
+
+        sami_style = '\n    %s%s {\n    ' % (element, style)
+
+        for attr, value in self._recreate_style(content).items():
+            sami_style += ' %s: %s;\n    ' % (attr, value)
+
+        return sami_style + '}\n'
+
+    def _recreate_text(self, caption):
+        line = ''
+
+        for node in caption:
+            if node.type == CaptionNode.TEXT:
+                line += escape(node.content) + ' '
+            elif node.type == CaptionNode.BREAK:
+                line = line.rstrip() + '<br/>\n    '
+            elif node.type == CaptionNode.STYLE:
+                line = self._recreate_line_style(line, node)
+
+        return line.rstrip()
+
+    def _recreate_line_style(self, line, node):
+        if node.start:
+            if self.open_span:
+                line = line.rstrip() + '</span> '
+            line = self._recreate_span(line, node.content)
+        else:
+            if self.open_span:
+                line = line.rstrip() + '</span> '
+                self.open_span = False
+
+        return line
+
+    def _recreate_span(self, line, content):
+        style = ''
+        klass = ''
+        if 'class' in content:
+            klass += ' class="%s"' % content['class']
+
+        for attr, value in self._recreate_style(content).items():
+            style += '%s:%s;' % (attr, value)
+
+        if style or klass:
+            if style:
+                style = ' style="%s"' % style
+            line += '<span%s%s>' % (klass, style)
+            self.open_span = True
+
+        return line
+
+    def _recreate_style(self, content):
+        sami_style = {}
+
+        if 'text-align' in content:
+            sami_style['text-align'] = content['text-align']
+        if 'italics' in content:
+            sami_style['font-style'] = 'italic'
+        if 'font-family' in content:
+            sami_style['font-family'] = content['font-family']
+        if 'font-size' in content:
+            sami_style['font-size'] = content['font-size']
+        if 'color' in content:
+            sami_style['color'] = content['color']
+        if 'lang' in content:
+            sami_style['lang'] = content['lang']
+
+        return sami_style
+
+
+class SAMIParser(HTMLParser):
+    def __init__(self, *args, **kw):
+        HTMLParser.__init__(self, *args, **kw)
+        self.sami = u''
+        self.line = ''
+        self.styles = {}
+        self.queue = deque()
+        self.langs = {}
+        self.last_element = ''
+        self.name2codepoint = name2codepoint.copy()
+        self.name2codepoint['apos'] = 0x0027
+
+    # override the parser's handling of starttags
+    def handle_starttag(self, tag, attrs):
+        self.last_element = tag
+
+        # treat divs as spans
+        if tag == 'div':
+            tag = 'span'
+
+        if tag == 'i':
+            tag = 'span'
+            attrs = [('style', 'font-style:italic;')]
+
+        # figure out the caption language of P tags
+        if tag == 'p':
+            lang = self._find_lang(attrs)
+
+            # if no language detected, set it as the default
+            lang = lang or DEFAULT_LANGUAGE_CODE
+            attrs.append(('lang', lang))
+            self.langs[lang] = 1
+
+        # clean-up line breaks
+        if tag == 'br':
+            self.sami += "<br/>"
+        # add tag to queue
+        else:
+            # if already in queue, first close tags off in LIFO order
+            while tag in self.queue:
+                closer = self.queue.pop()
+                self.sami = self.sami + "</%s>" % closer
+            # open new tag in queue
+            self.queue.append(tag)
+            # add tag with attributes
+            for attr, value in attrs:
+                tag += ' %s="%s"' % (attr.lower(), value)
+            self.sami += "<%s>" % tag
+
+    # override the parser's handling of endtags
+    def handle_endtag(self, tag):
+        # treat divs as spans
+        if tag == 'div' or tag == 'i':
+            tag = 'span'
+
+        # handle incorrectly formatted sync/p tags
+        if tag in ['p', 'sync'] and tag == self.last_element:
+            return
+
+        # close off tags in LIFO order, if matching starting tag in queue
+        while tag in self.queue:
+            closing_tag = self.queue.pop()
+            self.sami += "</%s>" % closing_tag
+
+    def handle_entityref(self, name):
+        if name in ['gt', 'lt']:
+            self.sami += '&%s;' % name
+        else:
+            try:
+                self.sami += unichr(self.name2codepoint[name])
+            except (KeyError, ValueError):
+                self.sami += '&%s' % name
+
+        self.last_element = ''
+
+    def handle_charref(self, name):
+        if name[0] == 'x':
+            self.sami += unichr(int(name[1:], 16))
+        else:
+            self.sami += unichr(int(name))
+
+    # override the parser's handling of data
+    def handle_data(self, data):
+        self.sami += data.decode('utf-8')
+        self.last_element = ''
+
+    # override the parser's feed function
+    def feed(self, data):
+        no_cc = 'no closed captioning available'
+
+        if '<html' in data.lower():
+            raise CaptionReadSyntaxError('SAMI File seems to be an HTML file.')
+        elif no_cc in data.lower():
+            raise CaptionReadSyntaxError('SAMI File contains "%s"' % no_cc)
+
+        # try to find style tag in SAMI
+        try:
+            # prevent BS4 error with huge SAMI files with unclosed tags
+            index = data.lower().find("</head>")
+
+            self.styles = self._css_parse(
+                BeautifulSoup(data[:index]).find('style').get_text())
+        except AttributeError:
+            self.styles = {}
+
+        # fix erroneous italics tags
+        data = data.replace('<i/>', '<i>')
+
+        # fix awkward tags found in some SAMIs
+        data = data.replace(';>', '>')
+        try:
+            HTMLParser.feed(self, data)
+        except HTMLParseError as e:
+            raise CaptionReadSyntaxError(e)
+
+        # close any tags that remain in the queue
+        while self.queue != deque([]):
+            closing_tag = self.queue.pop()
+            self.sami += "</%s>" % closing_tag
+
+        return self.sami, self.styles, self.langs
+
+    # parse the SAMI's stylesheet
+    def _css_parse(self, css):
+        # parse via cssutils modules
+        sheet = parseString(css)
+        style_sheet = {}
+
+        for rule in sheet:
+            not_empty = False
+            new_style = {}
+            selector = rule.selectorText.lower()
+            if selector[0] in ['#', '.']:
+                selector = selector[1:]
+            # keep any style attributes that are needed
+            for prop in rule.style:
+                if prop.name == 'text-align':
+                    new_style['text-align'] = prop.value
+                    not_empty = True
+                if prop.name == 'font-family':
+                    new_style['font-family'] = prop.value
+                    not_empty = True
+                if prop.name == 'font-size':
+                    new_style['font-size'] = prop.value
+                    not_empty = True
+                if prop.name == 'color':
+                    cv = cssutils_css.ColorValue(prop.value)
+                    # Code for RGB to hex conversion comes from
+                    # http://bit.ly/1kwfBnQ
+                    new_style['color'] = "#%02x%02x%02x" % (
+                        cv.red, cv.green, cv.blue)
+                    not_empty = True
+                if prop.name == 'lang':
+                    new_style['lang'] = prop.value
+                    not_empty = True
+            if not_empty:
+                style_sheet[selector] = new_style
+
+        return style_sheet
+
+    def _find_lang(self, attrs):
+        for attr, value in attrs:
+            # if lang is an attribute of the tag
+            if attr.lower() == 'lang':
+                return value[:2]
+            # if the P tag has a class, try and find the language
+            if attr.lower() == 'class':
+                try:
+                    return self.styles[value.lower()]['lang']
+                except KeyError:
+                    pass
+
+        return None
