09df2c23c1ff83dcff49060c3df7783624659bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:15 +0000

    Add package isbntools/3.2.2a/isbntools-3.2.2a.tar.gz

---
+++ packages/isbntools/isbntools-3.2.2a.tar.gz/isbntools-3.2.2a/isbntools/dev/lab.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# flake8:noqa
+# pylint: skip-file
+from .fmt import fmtbib, fmts
+from .helpers import unicode_to_utf8tex as to_utf8tex
+from .helpers import normalize_space, last_first, in_virtual
+from .. import bouth23
