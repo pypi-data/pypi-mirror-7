163ea5c4f996e83de762f1d6f9f318210b59617d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package linkGrabber/0.1.6/linkGrabber-0.1.6.tar.gz

---
+++ packages/linkGrabber/linkGrabber-0.1.6.tar.gz/linkGrabber-0.1.6/linkGrabber/scrape.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import re
+import urllib2
+from BeautifulSoup import BeautifulSoup
+from .links import Links
+
+class ScrapeLinks:
+    """Grabs links from a web page
+    based upon a URL, filters, and limits"""
+    def __init__(self, href):
+        if 'http://' not in href and 'https://' not in href:
+            raise Exception("URL must contain http:// or https://")
+        self._href = href
+        self._soup = None
+        self._get_page()
+
+    def __repr__(self):
+        return "<ScrapeLinks %r>" % self._href
+
+    def _get_page(self):
+        page = urllib2.urlopen(self._href)
+        self._soup = BeautifulSoup(page)
+        return self._soup
+
+    def find_links(self, filters=None, limit=None, limit_reverse=False, limit_sort=None):
+        if filters is not None and not isinstance(filters, dict):
+            raise Exception("filters parameter must be a dictionary")
+        if limit is not None and not isinstance(limit, int):
+            raise Exception("limit parameter must be an integer")
+        if limit_sort is not None and not hasattr(limit_sort, "__call__"):
+            raise Exception("sort parameter must be a function")
+
+        if filters is not None:
+            search = self._soup.findAll('a', **filters)
+        else:
+            search = self._soup.findAll('a')
+
+        #print(search)
+
+        if limit_sort is not None:
+            search = sorted(search, key=limit_sort, reverse=limit_reverse)
+
+        if limit_reverse is not None and limit_sort is None:
+            search = sorted(search, reverse=limit_reverse)
+
+        links = []
+        for anchor in search:
+            link_href = anchor['href']
+            last_slash = anchor['href'].rfind('/')
+            link_seo = anchor['href'][last_slash+1:] \
+                        .replace('-', ' ') \
+                        .replace('  ', ' ')
+            if anchor.string is None:
+                link_text = link_seo
+            else:
+                link_text = anchor.string
+            links.append(Links(link_text, link_href, link_seo))
+            if limit is not None and len(links) >= limit:
+                break
+        return links
---
+++ packages/linkGrabber/linkGrabber-0.1.6.tar.gz/linkGrabber-0.1.6/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='linkGrabber',
+    version='0.1.6',
+    author='Eric Bower',
+    author_email='neurosnap@gmail.com',
+    packages=['linkGrabber', 'linkGrabber.tests'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/linkGrabber',
+    license='LICENSE.rst',
+    description='Scrape links from a single web site',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "BeautifulSoup"
+    ],
+)
