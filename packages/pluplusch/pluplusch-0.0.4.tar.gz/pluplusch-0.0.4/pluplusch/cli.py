b44466e83e46db7db932243865a4d01791e0baf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package pluplusch/0.0.4/pluplusch-0.0.4.tar.gz

---
+++ packages/pluplusch/pluplusch-0.0.4.tar.gz/pluplusch-0.0.4/pluplusch/cli.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+import argparse
+import json
+import sys
+
+from pluplusch.main import pluplusch
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+
+    cache_dir = os.path.join(os.path.expanduser('~'), '.pluplusch')
+    parser.add_argument('--cache-dir', '-d', metavar = 'DIR', default = cache_dir)
+    parser.add_argument('--download-data', '-D', action = 'store_true', default = False)
+    parser.add_argument('--urls', '-u', action = 'store_true', default = False)
+    parser.add_argument('catalog', metavar = 'CATALOG', nargs = '*')
+
+    return parser
+
+def main(stdout = sys.stdout):
+    p = arg_parser().parse_args()
+
+    generator = pluplusch(catalogs = p.catalog, cache_dir = p.cache_dir, standardize = True, download_data = p.download_data)
+    for metadata in generator:
+        if p.urls:
+            url = metadata['download_url']
+            if url != None:
+                stdout.write(url + '\n')
+        else:
+            for k,v in metadata.items():
+                if isinstance(v, datetime.datetime):
+                    metadata[k] = v.isoformat()
+                elif isinstance(v, set):
+                    metadata[k] = list(v)
+            stdout.write(json.dumps(metadata) + '\n')
---
+++ packages/pluplusch/pluplusch-0.0.4.tar.gz/pluplusch-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+from pluplusch import __version__
+
+setup(name='pluplusch',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Big dada: Download data from lots of open data platforms.',
+      url='https://github.com/tlevine/pluplusch',
+      packages=['pluplusch'],
+      install_requires = [
+          'thready>=0.1.4',
+          'picklecache>=0.0.3',
+          'requests>=2.2.1',
+          'pickle_warehouse>=0.0.17'
+      ],
+      scripts = ['bin/pluplusch'],
+      tests_require = ['nose'],
+      version=__version__,
+      license='AGPL',
+)
