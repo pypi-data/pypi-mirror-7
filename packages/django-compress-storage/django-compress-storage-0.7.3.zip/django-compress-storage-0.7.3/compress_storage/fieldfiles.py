9969a0c4d34e2af190dde49dcab1ec6be7817298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:58 +0000

    Add package django-compress-storage/0.7.3/django-compress-storage-0.7.3.zip

---
+++ packages/django-compress-storage/django-compress-storage-0.7.3.zip/django-compress-storage-0.7.3/compress_storage/fieldfiles.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from zipfile import ZipFile, ZIP_DEFLATED
+import sys
+from .base import CompressFieldFile
+
+
+class ZipCompressFieldFile(CompressFieldFile):
+    compress_ext = 'zip'
+
+    def compress_content(self):
+        compress_file_fullname = self.compress_name
+
+        # to work with py2.6 or lower
+        if sys.version_info < (2, 7):
+            compress_file_fullname = file(compress_file_fullname, 'w')
+
+        if self.is_compressed:
+            return 'File alredy compress'
+
+        ziped = ZipFile(compress_file_fullname, 'w', ZIP_DEFLATED)
+        ziped.write(str(self.file.name))
+        ziped.close()
+
+        return ziped
