1ddcd47a3730b7c5807320d8a0f4c3f5dabcb44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:12 +0000

    Add package pySecurityCenter/2.0a1/pySecurityCenter-2.0a1.tar.gz

---
+++ packages/pySecurityCenter/pySecurityCenter-2.0a1.tar.gz/pySecurityCenter-2.0a1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import securitycenter
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="pySecurityCenter",
+    version=securitycenter.__version__,
+    description="Security Center API Library",
+    long_description=long_description,
+    author=", ".join(securitycenter.__authors__),
+    #author_email="steve@chigeek.com",
+    url="https://github.com/SteveMcGrath/pySecurityCenter",
+    packages=[
+        "securitycenter",
+        "securitycenter.orm",
+        "securitycenter.orm.modules",
+    ],
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ]
+)
