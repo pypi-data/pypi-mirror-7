df357fd813b239c874a20bc9c39b59b1a69711f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:58 +0000

    Add package confluent_common/0.1.1/confluent_common-0.1.1.tar.gz

---
+++ packages/confluent_common/confluent_common-0.1.1.tar.gz/confluent_common-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='confluent_common',
+    version='0.1.1',
+    author='Jarrod Johnson',
+    author_email='jbjohnso@us.ibm.com',
+    description='common content for confluent client and server',
+    url='http://xcat.sf.net/',
+    packages=['confluent'],
+    )
