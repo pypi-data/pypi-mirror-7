f1d6bb10a4b0d29b2bacfaf684730064def35b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package django-fias/0.4.0/django-fias-0.4.0.tar.gz

---
+++ packages/django-fias/django-fias-0.4.0.tar.gz/django-fias-0.4.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+sys.path.insert(0, '..')
+
+from fias.version import __version__
+
+setup(
+    name='django-fias',
+    version=__version__,
+    author='Artem Vlasov',
+    author_email='root@proscript.ru',
+    url='https://github.com/Yuego/django-fias',
+    download_url='https://github.com/Yuego/django-fias/archive/{0}.tar.gz'.format(__version__),
+
+    description='FIAS Django integration',
+    long_description=open('README.rst').read(),
+
+    license='MIT license',
+    install_requires=[
+        'Django>=1.4',
+        'django_select2>=4.2.2',
+        'django-extensions>=1.0.0',
+        'suds>=0.4',
+        'rarfile',
+        'six',
+        'lxml',
+        'south>=0.8.4',
+        'unrar',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Russian',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
