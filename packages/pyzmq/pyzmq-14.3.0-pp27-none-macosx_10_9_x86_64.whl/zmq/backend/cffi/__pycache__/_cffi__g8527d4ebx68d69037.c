e22bbc29d0861b35f0eec0e00a689a46b2c8d171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package pyzmq/14.3.0/pyzmq-14.3.0-pp27-none-macosx_10_9_x86_64.whl

---
+++ packages/pyzmq/pyzmq-14.3.0-pp27-none-macosx_10_9_x86_64.whl/zmq/backend/cffi/__pycache__/_cffi__g8527d4ebx68d69037.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/types.h>   /* XXX for ssize_t on some platforms */
+
+#ifdef _WIN32
+#  include <Windows.h>
+#  define snprintf _snprintf
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+typedef SSIZE_T ssize_t;
+typedef unsigned char _Bool;
+#else
+#  include <stdint.h>
+#endif
+
+#include <zmq.h>
+void _cffi_f_zmq_version(int * x0, int * x1, int * x2)
+{
+  zmq_version(x0, x1, x2);
+}
+
