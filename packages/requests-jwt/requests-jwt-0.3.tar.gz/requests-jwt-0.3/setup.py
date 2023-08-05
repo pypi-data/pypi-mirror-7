ec1034f11c9012d48da7d25feffa4bfd065df74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package requests-jwt/0.3/requests-jwt-0.3.tar.gz

---
+++ packages/requests-jwt/requests-jwt-0.3.tar.gz/requests-jwt-0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='requests-jwt',
+    version='0.3',
+    url='https://github.com/tgs/requests-jwt',
+    packages=['requests_jwt'],
+    install_requires=[ 'requests', 'PyJWT' ],
+    tests_require=['httpretty'],
+    test_suite='tests.suite',
+    provides=[ 'requests_jwt' ],
+    author='Thomas Grenfell Smith',
+    author_email='thomathom@gmail.com',
+    description='This package allows for HTTP JSON Web Token (JWT) authentication using the requests library.',
+    license='ISC',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        'License :: OSI Approved :: ISC License (ISCL)',
+    ],
+)
