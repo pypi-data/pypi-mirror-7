e7243e76f336aa67fa1809f3f9c2fd55f2ee4db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:22 +0000

    Add package django-image-tools/0.4.b/django-image-tools-0.4.b.tar.gz

---
+++ packages/django-image-tools/django-image-tools-0.4.b.tar.gz/django-image-tools-0.4.b/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='django-image-tools',
+      version='0.4.b',
+      description='Image tools for Django',
+      author='Bonsai Studio',
+      author_email='info@bonsai-studio.net',
+      url='http://github.com/bonsaistudio/django-image-tools',
+      packages=['django_image_tools', ],
+     )
