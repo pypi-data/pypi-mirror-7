091047a009c0c25df82ff883cec8e91acf56f1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:08 +0000

    Add package print_tree/1.0.0/print_tree-1.0.0.zip

---
+++ packages/print_tree/print_tree-1.0.0.zip/print_tree-1.0.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='print_tree',
+      version = '1.0.0',
+      description = "print a tree",
+      author = "zszer",
+      author_email = "zsz150@qq.com",
+      url = "http://hi.baidu.com/zszer/",
+      #packages=['print_tree'],
+      #scripts=['scripts/print_tree'],
+)
