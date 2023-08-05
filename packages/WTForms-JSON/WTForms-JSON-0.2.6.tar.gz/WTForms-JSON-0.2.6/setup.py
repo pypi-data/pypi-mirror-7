7548167ee5dd13c90cd9f4c312d8c5607f85e06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:11 +0000

    Add package WTForms-JSON/0.2.6/WTForms-JSON-0.2.6.tar.gz

---
+++ packages/WTForms-JSON/WTForms-JSON-0.2.6.tar.gz/WTForms-JSON-0.2.6/setup.py
@@ -0,0 +1,66 @@
+"""
+WTForms-JSON
+------------
+
+Adds smart json support for WTForms. Useful for when using WTForms with RESTful
+APIs.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+
+extras_require = {
+    'test': [
+        'pytest>=2.2.3',
+        'WTForms-Alchemy>=0.8.6'
+    ],
+}
+
+
+setup(
+    name='WTForms-JSON',
+    version='0.2.6',
+    url='https://github.com/kvesteri/wtforms-json',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description=(
+        'Adds smart json support for WTForms. Useful for when using'
+        ' WTForms with RESTful APIs.'
+    ),
+    long_description=__doc__,
+    packages=['wtforms_json'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'WTForms>=1.0.2',
+        'six>=1.3.0'
+    ],
+    extras_require=extras_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
