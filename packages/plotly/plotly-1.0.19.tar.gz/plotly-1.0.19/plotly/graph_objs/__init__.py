2fb30e875d7909227b5ea4a0fbc66f3f6422261c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:35 +0000

    Add package plotly/1.0.19/plotly-1.0.19.tar.gz

---
+++ packages/plotly/plotly-1.0.19.tar.gz/plotly-1.0.19/plotly/graph_objs/__init__.py
@@ -0,0 +1,77 @@
+"""
+graph_objs
+==========
+
+This package imports definitions for all of Plotly's graph objects. For more
+information, run help(Obj) on any of the following objects defined here...
+
+Defined Objects:
+
+    Data Objects (things that make up your 'Data' list:
+        Bar
+        Box
+        Contour
+        Heatmap
+        Histogram
+        Histogram2d
+        Histogram2dContour
+        Scatter
+
+    Other Objects (everything else):
+
+        Data
+        Annotations
+        Annotation
+        AngularAxis
+        ColorBar
+        Contours
+        ErrorX
+        ErrorY
+        Figure
+        Font
+        Layout
+        Legend
+        Line
+        Margin
+        Marker
+        RadialAxis
+        Stream
+        Trace
+        XAxis
+        XBins
+        YAxis
+        YBins
+
+"""
+from graph_objs import *
+
+__all__ = ["Data",
+           "Annotations",
+           "Bar",
+           "Box",
+           "Contour",
+           "Heatmap",
+           "Histogram",
+           "Histogram2d",
+           "Histogram2dContour",
+           "Scatter",
+           "Annotation",
+           "AngularAxis",
+           "ColorBar",
+           "Contours",
+           "ErrorX",
+           "ErrorY",
+           "Figure",
+           "Font",
+           "Layout",
+           "Legend",
+           "Line",
+           "Margin",
+           "Marker",
+           "RadialAxis",
+           "Stream",
+           "Trace",
+           "XAxis",
+           "XBins",
+           "YAxis",
+           "YBins"]
