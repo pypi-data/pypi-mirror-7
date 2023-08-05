eea75944dd863807c12c338b85ff6363053c9707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:50 +0000

    Add package pylibscrypt/1.1.2/pylibscrypt-1.1.2.tar.gz

---
+++ packages/pylibscrypt/pylibscrypt-1.1.2.tar.gz/pylibscrypt-1.1.2/pylibscrypt/__init__.py
@@ -0,0 +1,52 @@
+'Scrypt for Python'
+
+__version__ = '1.1.2'
+
+# First, try loading libscrypt
+_done = False
+try:
+    from pylibscrypt import *
+except ImportError:
+    pass
+else:
+    _done = True
+
+# If that didn't work, try the scrypt module
+if not _done:
+    try:
+        from pyscrypt import *
+    except ImportError:
+        pass
+    else:
+        _done = True
+
+# Unless we are on pypy, we want to try libsodium as well
+if not _done:
+    import platform
+    if platform.python_implementation() != 'PyPy':
+        try:
+            from pylibsodium import *
+        except ImportError:
+            pass
+        else:
+            _done = True
+
+# If that didn't work either, the inlined Python version
+if not _done:
+    try:
+        from pypyscrypt_inline import *
+    except ImportError:
+        pass
+    else:
+        _done = True
+
+# Finally the non-inlined
+if not _done:
+    from pypyscrypt import *
+
+__all__ = ['scrypt', 'scrypt_mcf', 'scrypt_mcf_check']
+
+# Clean up pydoc output
+del __path__
+del consts
+
