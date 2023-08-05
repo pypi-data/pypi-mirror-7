7184a91bbf2b7c6bf33f73b2f8544cf4c3dcb601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:46 +0000

    Add package infi.hbaapi/0.1.27/infi.hbaapi-0.1.27.tar.gz

---
+++ packages/infi.hbaapi/infi.hbaapi-0.1.27.tar.gz/infi.hbaapi-0.1.27/src/infi/hbaapi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.27"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.27)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '290d6baa09a40bfb6432e6c18fc015405176c568'
+__git_head_subject__ = 'Finished release v0.1.27.'
+__git_head_message__ = '* release/v0.1.27:\n  fixing previous commit\n  disbling port statistics on linux'
+__git_dirty_diff__ = ''
