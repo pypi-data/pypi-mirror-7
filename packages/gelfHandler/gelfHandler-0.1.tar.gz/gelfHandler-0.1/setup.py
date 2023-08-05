8a9e0795879a63f76820f9adf84a40b9d39ebce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:14 +0000

    Add package gelfHandler/0.1/gelfHandler-0.1.tar.gz

---
+++ packages/gelfHandler/gelfHandler-0.1.tar.gz/gelfHandler-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gelfHandler',
+    version='0.1',
+    packages=['gelfHandler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    author='Stewart Rutledge',
+    author_email='stew.rutledge@gmail.com',
+    url='http://pypi.python.org/pypi/gelfHandler/'
+)
