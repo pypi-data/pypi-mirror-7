fa0e24ab1eab3437ba3321adde700612d88559bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package nesterql/1.0/nesterql-1.0.tar.gz

---
+++ packages/nesterql/nesterql-1.0.tar.gz/nesterql-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "nesterql",
+    version = '1.0',
+    py_modules = ['nesterql'],
+    author = 'aaa',
+    author_email = 'aaa',
+    description = 'aaa',
+    )
