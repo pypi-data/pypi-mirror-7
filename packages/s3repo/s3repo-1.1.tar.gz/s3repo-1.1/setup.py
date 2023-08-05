5ed326f05f0fb54be6b326d704255c008c973f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:50 +0000

    Add package s3repo/1.1/s3repo-1.1.tar.gz

---
+++ packages/s3repo/s3repo-1.1.tar.gz/s3repo-1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+long_description = file("README.md", "r").read()
+long_description = long_description.replace(":\n", "::\n")  # lame md -> rst
+
+setup(
+  name="s3repo",
+  description="A tool to manage simple Debian repositories in S3",
+  long_description=long_description,
+  license="BSD",
+  author="Ross Kinder",
+  author_email="ross+czNyZXBv@kndr.org",
+  version="1.1",
+  url="http://github.com/crewjam/s3repo",
+  package_dir={'': 'src'},
+  packages=['s3repo'],
+  entry_points={
+    "console_scripts": [
+      "s3repo = s3repo.command:Main",
+    ],
+  },
+  requires=["boto"],
+)
