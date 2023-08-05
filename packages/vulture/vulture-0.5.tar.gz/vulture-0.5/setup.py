8fe28979f16eb9e402fbc4cbf7419b2ec1939745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package vulture/0.5/vulture-0.5.tar.gz

---
+++ packages/vulture/vulture-0.5.tar.gz/vulture-0.5/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+from wake import __version__
+
+
+setup(name='vulture',
+      version=__version__,
+      description="Find dead code",
+      long_description='\n\n'.join([open('README.txt').read(),
+                                    open('NEWS.txt').read()]),
+      keywords='vulture',
+      author='Jendrik Seipp',
+      author_email='jendrikseipp@web.de',
+      url='https://bitbucket.org/jendrikseipp/vulture',
+      license='GPL3+',
+      py_modules=['wake'],
+      scripts=['vulture'],
+      classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        ],
+      )
