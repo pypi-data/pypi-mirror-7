f2dd7695568052a96bd24cdf79c2db70c2d637a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package watermarks/0.1.1/watermarks-0.1.1.tar.gz

---
+++ packages/watermarks/watermarks-0.1.1.tar.gz/watermarks-0.1.1/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+import os
+import sys
+src_dir = os.path.join(os.path.dirname(__file__), 'src')
+sys.path.insert(0, src_dir)
+import watermarks
+
+
+setup(
+    name='watermarks',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    version=watermarks.__version__,
+    description='Library for adding/reading watermarks from images. Currently visible and LSB watermark methods are supported.',
+    author='Vladimir Chovanec',
+    author_email='vladimir.chovanec.zc@gmail.com',
+    maintainer_email='watermarks.py@gmail.com',
+    keywords=['watermark', 'watermarks', 'watermarker', 'watermarking', 'lsb'],
+    url='https://github.com/vladozc/watermarks',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Graphics",
+        ],
+    install_requires=[
+        'Pillow==2.3.0',
+        'six==1.6.1',
+    ],
+)
---
+++ packages/watermarks/watermarks-0.1.1.tar.gz/watermarks-0.1.1/src/watermarks/core/watermark.py
@@ -0,0 +1,62 @@
+import logging
+
+import six
+from PIL import Image
+
+
+ALLOWED_FORMATS = ('BMP', 'JPEG', 'PNG')
+# mode color depth, http://effbot.org/imagingbook/concepts.htm
+MODE_DEPTHS = {
+    '1': 1,
+    'L': 8,
+    'RGB': 8,
+    'RGBA': 8,
+    'CMYK': 8,
+    #'I': 32,
+    #'F': 32,
+}
+logger = logging.getLogger()
+
+
+class Watermark(object):
+    '''Basic class for image that will be put to other image as watermark.
+    As input, it works with image object.
+    '''
+    def __init__(self, img):
+        self.img = img
+
+    def init(self):
+        '''Initializes watermark - load, set width/height, ...'''
+        if self.img.format not in ALLOWED_FORMATS:
+            logger.warning('Watermark format "%s" is not allowed. (skip)', self.img.format)
+            raise ValueError()
+        self.img.load()
+        self.width, self.height = self.img.size
+        self.band = self.img.split()[0]
+        self.threshold = self.get_px_max_value() / 2
+
+    def get_px_max_value(self):
+        '''Determines maximum pixel value according to used mode.'''
+        try:
+            return 2 ** MODE_DEPTHS[self.img.mode] - 1
+        except KeyError:
+            logger.critical('Watermark mode "%s" is not supported!', 
+                            self.img.mode)
+            raise
+
+
+def create_watermark(wm, width=None, height=None, *args, **kwargs):
+    if isinstance(wm, six.string_types):
+        wm = Image.open(wm)
+
+    if width and height:
+        img_w, img_h = wm.size
+        if img_w != width or img_h != height:
+            sized_img = Image.new(wm.mode, (width, height), 'white')
+            sized_img.paste(wm, (0, 0))
+            sized_img.format = wm.format
+            wm = sized_img
+
+    wm_instance = Watermark(wm, *args, **kwargs)
+    wm_instance.init()
+    return wm_instance
