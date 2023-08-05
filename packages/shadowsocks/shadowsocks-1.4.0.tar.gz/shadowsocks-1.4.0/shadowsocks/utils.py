a5549d04edebdee2f0571c37c25b37d6e34d8535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package shadowsocks/1.4.0/shadowsocks-1.4.0.tar.gz

---
+++ packages/shadowsocks/shadowsocks-1.4.0.tar.gz/shadowsocks-1.4.0/shadowsocks/utils.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 clowwindy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import logging
+
+
+def find_config():
+    config_path = 'config.json'
+    if os.path.exists(config_path):
+        return config_path
+    config_path = os.path.join(os.path.dirname(__file__), '../', 'config.json')
+    if os.path.exists(config_path):
+        return config_path
+    return None
+
+
+def check_config(config):
+    if config.get('local_address', '') in ['0.0.0.0']:
+        logging.warn('warning: local set to listen 0.0.0.0, which is not safe')
+    if config.get('server', '') in ['127.0.0.1', 'localhost']:
+        logging.warn('warning: server set to listen %s:%s, are you sure?' %
+                     (config['server'], config['server_port']))
+    if (config.get('method', '') or '').lower() == 'rc4':
+        logging.warn('warning: RC4 is not safe; please use a safer cipher, '
+                     'like AES-256-CFB')
+    if (config.get('timeout', 600) or 600) < 100:
+        logging.warn('warning: your timeout %d seems too short' %
+                     config.get('timeout'))
+    if (config.get('timeout', 600) or 600) > 600:
+        logging.warn('warning: your timeout %d seems too long' %
+                     config.get('timeout'))
+
+
+def print_local_help():
+    print '''usage: sslocal [-h] -s SERVER_ADDR -p SERVER_PORT [-b LOCAL_ADDR]
+                -l LOCAL_PORT -k PASSWORD -m METHOD [-c config] [--fast-open]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SERVER_ADDR        server address
+  -p SERVER_PORT        server port
+  -b LOCAL_ADDR         local binding address, default is 127.0.0.1
+  -l LOCAL_PORT         local port
+  -k PASSWORD           password
+  -m METHOD             encryption method, for example, aes-256-cfb
+  -c CONFIG             path to config file
+  --fast-open           use TCP_FASTOPEN, requires Linux 3.7+
+'''
+
+
+def print_server_help():
+    print '''usage: ssserver [-h] -s SERVER_ADDR -p SERVER_PORT -k PASSWORD
+                -m METHOD [-c config] [--fast-open]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SERVER_ADDR        server address
+  -p SERVER_PORT        server port
+  -k PASSWORD           password
+  -m METHOD             encryption method, for example, aes-256-cfb
+  -c CONFIG             path to config file
+  --fast-open           use TCP_FASTOPEN, requires Linux 3.7+
+'''
