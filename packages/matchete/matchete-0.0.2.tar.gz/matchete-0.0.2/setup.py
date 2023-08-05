0d43a4b24ba07b00fc4799f399a9f99f31a8cd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:53 +0000

    Add package matchete/0.0.2/matchete-0.0.2.tar.gz

---
+++ packages/matchete/matchete-0.0.2.tar.gz/matchete-0.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='matchete',
+      version='0.0.2',
+      description='Python simple pattern-matching based method overloading',
+      author='Alexander Ivanov',
+      author_email='alehander42@gmail.com',
+      url='http://github.com/alehander42/py-matchete',
+      license='LICENSE',
+      packages=['matchete'],
+     )
