8b1a6c4812be667233f72eff06158030dc7dd638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:37 +0000

    Add package incuna-test-utils/2.0.1/incuna-test-utils-2.0.1.tar.gz

---
+++ packages/incuna-test-utils/incuna-test-utils-2.0.1.tar.gz/incuna-test-utils-2.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='incuna-test-utils',
+    packages=find_packages(),
+    include_package_data=True,
+    version='2.0.1',
+    description='',
+    long_description=open('README.rst').read(),
+    author='Incuna',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/incuna-test-utils/',
+    install_requires=[],
+    zip_safe=False,
+)
