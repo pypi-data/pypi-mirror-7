151b8744dbf6634f311810b087373c1e72fda36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package hercules/0.0/hercules-0.0.tar.gz

---
+++ packages/hercules/hercules-0.0.tar.gz/hercules-0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+long_description = ''
+
+setup(name='hercules',
+      version='0.0',
+      packages=find_packages(),
+      author='Thom Neale',
+      author_email='twneale@gmail.com',
+      url='http://github.com/twneale/hercules',
+      description='handy tools for strengthy domination',
+      long_description=long_description,
+      platforms=['any'],
+      install_requires=[
+      ]
+)
