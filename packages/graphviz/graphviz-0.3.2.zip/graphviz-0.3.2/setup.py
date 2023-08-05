eecfa4fc5eb053237e1165d169d603720dbb0197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:47 +0000

    Add package graphviz/0.3.2/graphviz-0.3.2.zip

---
+++ packages/graphviz/graphviz-0.3.2.zip/graphviz-0.3.2/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='graphviz',
+    version='0.3.2',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Simple Python interface for Graphviz',
+    keywords='graph visualization dot render',
+    license='MIT',
+    url='http://github.com/xflr6/graphviz',
+    packages=['graphviz'],
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
