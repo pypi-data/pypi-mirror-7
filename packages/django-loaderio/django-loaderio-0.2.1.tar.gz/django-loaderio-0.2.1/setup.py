db4a4fe2357e850e439b8509319d1e7f069c3adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package django-loaderio/0.2.1/django-loaderio-0.2.1.tar.gz

---
+++ packages/django-loaderio/django-loaderio-0.2.1.tar.gz/django-loaderio-0.2.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-loaderio',
+    version='0.2.1',
+    packages=['loaderio', 'loaderio.migrations'],
+    include_package_data=True,
+    license='MIT License',
+    description='An app for adding loader.io validation tokens',
+    long_description=README,
+    author='Lorin Hochstein',
+    author_email='lorin.hochstein@sendgrid.com',
+    url='https://github.com/sendgridlabs/loaderio-django',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
