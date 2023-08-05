aa4ec1ac82f803d7d15b05dee7c3949ffea7d71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:05 +0000

    Add package arvados-python-client/0.1.1401203545.037ec42/arvados-python-client-0.1.1401203545.037ec42.tar.gz

---
+++ packages/arvados-python-client/arvados-python-client-0.1.1401203545.037ec42.tar.gz/arvados-python-client-0.1.1401203545.037ec42/arvados/errors.py
@@ -0,0 +1,16 @@
+# errors.py - Arvados-specific exceptions.
+
+class ArgumentError(Exception):
+    pass
+class SyntaxError(Exception):
+    pass
+class AssertionError(Exception):
+    pass
+class NotFoundError(Exception):
+    pass
+class CommandFailedError(Exception):
+    pass
+class KeepWriteError(Exception):
+    pass
+class NotImplementedError(Exception):
+    pass
