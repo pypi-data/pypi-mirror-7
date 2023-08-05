585517146ce0e4eb651c31a96ea5bf9a5da8d06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:57 +0000

    Add package lisa-server/0.1.1.39/lisa-server-0.1.1.39.tar.gz

---
+++ packages/lisa-server/lisa-server-0.1.1.39.tar.gz/lisa-server-0.1.1.39/lisa/plugins/TestPlugin/README.rst
@@ -0,0 +1,3 @@
+LISA TestPlugin plugin
+======
+This module is automatically generated. Please fill this file with all the docs you can.
---
+++ packages/lisa-server/lisa-server-0.1.1.39.tar.gz/lisa-server-0.1.1.39/lisa/server/libs/rulesengine.py
@@ -0,0 +1,52 @@
+# -*- coding: UTF-8 -*-
+import json, re, sys, os, inspect, gettext
+from pymongo import MongoClient
+from twisted.python.reflect import namedAny
+from twisted.python import log
+from wit import Wit
+
+class RulesEngine():
+    def __init__(self, configuration):
+        self.configuration = configuration
+        client = MongoClient(self.configuration['database']['server'], self.configuration['database']['port'])
+        self.database = client.lisa
+        path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
+        inspect.getfile(inspect.currentframe()))[0],os.path.normpath("lang/"))))
+        self._ = translation = gettext.translation(domain='lisa', localedir=path, languages=[self.configuration['lang']]).ugettext
+        self.wit = Wit(self.configuration['wit_token'])
+
+    def Rules(self, jsonData, lisaprotocol):
+        rulescollection = self.database.rules
+        intentscollection = self.database.intents
+        jsonInput = self.wit.get_message(unicode(jsonData['body']))
+        jsonInput['from'], jsonInput['type'], jsonInput['zone'] = jsonData['from'], jsonData['type'], jsonData['zone']
+
+        if self.configuration['debug']['debug_before_before_rule']:
+            log.msg("Before 'before' rule: " + str(jsonInput))
+        for rule in rulescollection.find({"enabled": True, "before": {"$ne":None}}).sort([("order", 1)]):
+            exec(rule['before'])
+        if self.configuration['debug']['debug_after_before_rule']:
+            log.msg("After 'before' rule: " + str(jsonInput))
+        if self.configuration['debug']['debug_wit']:
+            log.msg("WIT: " + str(jsonInput['outcome']))
+
+        oIntent = intentscollection.find_one({"name": jsonInput['outcome']['intent']})
+        if oIntent and jsonInput['outcome']['confidence'] >= self.configuration['wit_confidence']:
+            instance = namedAny(str(oIntent["module"]))()
+            methodToCall = getattr(instance, oIntent['function'])
+            jsonOutput = methodToCall(jsonInput)
+        else:
+            jsonOutput = {}
+            jsonOutput['plugin'] = "None"
+            jsonOutput['method'] = "None"
+            jsonOutput['body'] = self._("I have not the right plugin installed to answer you correctly")
+        jsonOutput['from'] = jsonData['from']
+        if self.configuration['debug']['debug_before_after_rule']:
+            log.msg("Before 'after' rule: " + str(jsonOutput))
+        for rule in rulescollection.find({"enabled": True, "after": {"$ne":None}}).sort([("order", 1)]):
+            exec(rule['after'])
+            #todo it doesn't check if the condition of the rule after has matched to end the rules
+            if rule['end']:
+                break
+        if self.configuration['debug']['debug_after_after_rule']:
+            log.msg("After 'after' rule: " + str(jsonOutput))
