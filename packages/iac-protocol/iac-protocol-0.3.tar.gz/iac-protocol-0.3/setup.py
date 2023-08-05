2c4c56c25ac1d8036a3f3ea09145a7f02d027b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:27 +0000

    Add package iac-protocol/0.3/iac-protocol-0.3.tar.gz

---
+++ packages/iac-protocol/iac-protocol-0.3.tar.gz/iac-protocol-0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iac-protocol',
+    version='0.3',
+    license='BSD',
+    author='Risto Stevcev',
+    author_email='risto1@gmail.com',
+    url='https://github.com/Risto-Stevcev/iac-protocol',
+    long_description=open("README.rst","r").read(),
+    packages=find_packages(),
+    description="An interface/protocol that provides inter-application communication and scripting.",
+    entry_points = {
+        'console_scripts': ['iacs=iac.server:main',
+                            'iaci=iac.interpreter:main',
+                            'iacmodify=iac.modify_interfaces:main'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta', 
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+        'Topic :: Software Development :: Interpreters',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Utilities',
+        ], 
+)
