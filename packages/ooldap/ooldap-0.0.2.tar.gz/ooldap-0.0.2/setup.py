20c4c5a48daa8d0bbb286d34957c5ea807fb58ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:22 +0000

    Add package ooldap/0.0.2/ooldap-0.0.2.tar.gz

---
+++ packages/ooldap/ooldap-0.0.2.tar.gz/ooldap-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='ooldap',
+    version='0.0.2',
+    packages=['ooldap', 'ooldap.objects'],
+    url='http://github.com/TAMUArch/ooldap',
+    author='John',
+    author_email='st_jphillips@tamu.edu',
+    description='Object Oriented LDAP',
+)
