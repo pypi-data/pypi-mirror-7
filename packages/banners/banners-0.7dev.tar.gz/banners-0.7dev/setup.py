0d790d0917006a460cbc2addd10d1c03414fdecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package banners/0.7dev/banners-0.7dev.tar.gz

---
+++ packages/banners/banners-0.7dev.tar.gz/banners-0.7dev/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = '0.7'
+
+setup(name='banners',
+      version=version,
+      description="A simple banner manager app",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Banners, Django',
+      author='Revoluciones Informáticas',
+      author_email='info@revolucionesweb.com.ar',
+      url='http://revolucionesweb.com.ar',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      package_data={
+        'banners' : ['templates/*.html'],
+      },
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'django'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
