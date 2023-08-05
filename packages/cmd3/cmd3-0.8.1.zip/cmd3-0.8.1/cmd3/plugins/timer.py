93524b5d57603cc6825fc3938c05821088cb7236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package cmd3/0.8.1/cmd3-0.8.1.zip

---
+++ packages/cmd3/cmd3-0.8.1.zip/cmd3-0.8.1/cmd3/plugins/timer.py
@@ -0,0 +1,34 @@
+from cmd3.shell import command
+
+
+class timer:
+
+    """
+    needs to be integrated in pre post command
+    """
+    #
+    # TIMER
+    #
+
+    @command
+    def do_timer(self, args, arguments):
+        """
+        Usage:
+            timer (on|off)
+
+        switches timers on and off not yet implemented
+        """
+        args = args.lower()
+
+        if args in ("on", "off"):
+            self.with_timers = (line == "on")
+            print "Timers are now:", self.with_timers
+        if args == 'list':
+            self.list_timers()
+        else:
+            self.help_timer()
+
+    def list_tmers(self):
+        """list of all timers"""
+        print "TODO"
+        pass
