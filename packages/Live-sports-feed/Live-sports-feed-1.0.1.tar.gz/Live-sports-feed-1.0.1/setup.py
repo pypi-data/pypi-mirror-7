a997bf51f46df5e2c3628f92aecca35f6fbd9dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:59 +0000

    Add package Live-sports-feed/1.0.1/Live-sports-feed-1.0.1.tar.gz

---
+++ packages/Live-sports-feed/Live-sports-feed-1.0.1.tar.gz/Live-sports-feed-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+   name = "Live-sports-feed",
+   version = "1.0.1",
+   description = " Gives live feeds of cricket and football matches in a python file and text file , can be easily integrated inside an app",
+   author = "Ayush Tiwari",
+   author_email = "tiwari.ayush2412@gmail.com",
+   url = "https://github.com/tiwariayush/Live-sports-feed",
+   keywords = ["scraping" , "sports" , "cricket" ,"football", "beautiful soup"],
+   classifiers = [
+       "Programming Language :: Python",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+       ],
+  long_description = """\
+      
+     Sports feeds for football(soccer) and cricket .
+     Just run the python script feeds1.py and you will get the required data in 2 texts files and 1 python file
+
+     This version requires Python and BeautifulSoup(http://www.crummy.com/software/BeautifulSoup/) installed in your system
+   """
+   ) 
