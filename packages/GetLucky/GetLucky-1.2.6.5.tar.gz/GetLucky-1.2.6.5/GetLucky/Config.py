b78b9dd916b21cfb7351e0302b80fb43cd9fc28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:25 +0000

    Add package GetLucky/1.2.6.5/GetLucky-1.2.6.5.tar.gz

---
+++ packages/GetLucky/GetLucky-1.2.6.5.tar.gz/GetLucky-1.2.6.5/GetLucky/Config.py
@@ -0,0 +1,9 @@
+UserID = ''
+Songza_Shortcuts = {
+    'makeout':['activity','making_out'],
+    'sexy':['mood', 'sexual'],
+    'seductive':['mood','seductive'],
+    'aggressive':['mood', 'aggressive'],
+    'extreme':['genre', 'metal']
+    }
+
