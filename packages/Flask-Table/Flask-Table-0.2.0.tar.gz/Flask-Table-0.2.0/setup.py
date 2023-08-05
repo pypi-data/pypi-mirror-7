e20b8c7635e5290aaff9db8c3ee80137c5f38ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package Flask-Table/0.2.0/Flask-Table-0.2.0.tar.gz

---
+++ packages/Flask-Table/Flask-Table-0.2.0.tar.gz/Flask-Table-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-Table',
+    packages=['flask_table'],
+    version='0.2.0',
+    author='Andrew Plummer',
+    author_email='plummer574@gmail.com',
+    url='https://github.com/plumdog/flask_table',
+    description='HTML tables for use with the Flask micro-framework',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+        'Framework :: Flask',
+    ])
