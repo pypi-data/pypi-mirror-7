4257e3180560626eafc3bf37c330d7983d2c94fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package DreamScreenWeatherApp/0.3/DreamScreenWeatherApp-0.3.tar.gz

---
+++ packages/DreamScreenWeatherApp/DreamScreenWeatherApp-0.3.tar.gz/DreamScreenWeatherApp-0.3/DreamScreenWeatherApp/instance/weatherservice-config.py
@@ -0,0 +1,13 @@
+DEBUG=True
+
+# MANDATORY DATA URL
+# weather info - keys must be in upper case!!!!
+QUERY_URL='http://www.wunderground.com/weather-forecast/zmw:10308.1.99999'
+
+# these are optional simply to adjust the display of city, state and zip
+ZIPCODE=10308
+CITY='Staten Island'
+STATE='NY'
+COUNTRY='USA'
+
+
