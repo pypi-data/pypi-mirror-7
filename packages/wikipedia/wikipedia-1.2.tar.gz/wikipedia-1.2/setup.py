a3a74707f454ccf7761ed57459b687d6e2b1a49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:40 +0000

    Add package wikipedia/1.2/wikipedia-1.2.tar.gz

---
+++ packages/wikipedia/wikipedia-1.2.tar.gz/wikipedia-1.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+import codecs
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+long_description = codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r', 'utf-8').read()
+
+install_reqs = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
+dependencies = [str(ir.req) for ir in install_reqs]
+
+setup(
+  name = "wikipedia",
+  version = "1.2",
+  author = "Jonathan Goldsmith",
+  author_email = "jhghank@gmail.com",
+  description = "Wikipedia API for Python",
+  license = "MIT",
+  keywords = "python wikipedia API",
+  url = "https://github.com/goldsmith/Wikipedia",
+  install_requires = dependencies,
+  packages = ['wikipedia'],
+  long_description = long_description,
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Topic :: Software Development :: Libraries',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3'
+  ],
+)
