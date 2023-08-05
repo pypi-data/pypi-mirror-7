196cae5ec3911e8553488a422d5665e5e5b79c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:13 +0000

    Add package catapult/0.2.4/catapult-0.2.4.tar.gz

---
+++ packages/catapult/catapult-0.2.4.tar.gz/catapult-0.2.4/catapult/__init__.py
@@ -0,0 +1,6 @@
+""" Catapult """
+from catapult.plugin import CatapultPlugin
+from catapult.runner import CatapultTestRunner
+from catapult.result import CatapultTestResult
+
+from ._version import __version__, __version_info__
