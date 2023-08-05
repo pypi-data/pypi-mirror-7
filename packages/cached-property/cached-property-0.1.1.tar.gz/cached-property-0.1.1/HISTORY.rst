4ba2b78770ab72c6730f5204ebe91e94dd884f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package cached-property/0.1.1/cached-property-0.1.1.tar.gz

---
+++ packages/cached-property/cached-property-0.1.1.tar.gz/cached-property-0.1.1/HISTORY.rst
@@ -0,0 +1,14 @@
+.. :changelog:
+
+History
+-------
+
+0.1.1 (2014-05-17)
+++++++++++++++++++
+
+* setup.py fix. Whoops!
+
+0.1.0 (2014-05-17)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/cached-property/cached-property-0.1.1.tar.gz/cached-property-0.1.1/tests/test_cached_property.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_cached-property
+----------------------------------
+
+Tests for `cached-property` module.
+"""
+
+import unittest
+
+from cached_property import cached_property
+
+
+class TestCachedProperty(unittest.TestCase):
+
+    def test_cached_property(self):
+
+        class Check(object):
+
+            def __init__(self):
+                self.total1 = 0
+                self.total2 = 0
+
+            @property
+            def add_control(self):
+                self.total1 += 1
+                return self.total1
+
+            @cached_property
+            def add_cached(self):
+                self.total2 += 1
+                return self.total2
+
+        c = Check()
+
+        # The control shows that we can continue to add 1.
+        self.assertEqual(c.add_control, 1)
+        self.assertEqual(c.add_control, 2)
+
+        # The cached version demonstrates how nothing new is added
+        self.assertEqual(c.add_cached, 1)
+        self.assertEqual(c.add_cached, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
