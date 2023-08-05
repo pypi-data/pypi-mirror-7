6295a6bd031e34eb14156ed0c092ec6800b15e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package Mopidy-SoundCloud/1.2.3/Mopidy_SoundCloud-1.2.3-py2.py3-none-any.whl

---
+++ packages/Mopidy-SoundCloud/Mopidy_SoundCloud-1.2.3-py2.py3-none-any.whl/mopidy_soundcloud/actor.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+import logging
+import pykka
+
+from mopidy import backend
+
+from .library import SoundCloudLibraryProvider
+from .soundcloud import SoundCloudClient
+
+logger = logging.getLogger(__name__)
+
+
+class SoundCloudBackend(pykka.ThreadingActor, backend.Backend):
+
+    def __init__(self, config, audio):
+        super(SoundCloudBackend, self).__init__()
+        self.config = config
+        self.remote = SoundCloudClient(config['soundcloud'])
+        self.library = SoundCloudLibraryProvider(backend=self)
+        self.playback = SoundCloudPlaybackProvider(audio=audio, backend=self)
+
+        self.uri_schemes = ['soundcloud']
+
+
+class SoundCloudPlaybackProvider(backend.PlaybackProvider):
+
+    def play(self, track):
+        track_id = self.backend.remote.parse_track_uri(track)
+        track = self.backend.remote.get_track(track_id, True)
+        return super(SoundCloudPlaybackProvider, self).play(track)
