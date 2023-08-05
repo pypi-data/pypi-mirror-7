8491c5e2bdc9053954c75ab807c3ececba9ab368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:23 +0000

    Add package django-modelclone/0.3.0/django-modelclone-0.3.0.tar.gz

---
+++ packages/django-modelclone/django-modelclone-0.3.0.tar.gz/django-modelclone-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from os.path import dirname, join
+
+setup(
+    name = "django-modelclone",
+    version = "0.3.0",
+    description = u"Django application that allows users to clone a model in Admin",
+    packages = [
+        'modelclone',
+    ],
+    package_data = {
+        'modelclone': ['templates/modelclone/*'],
+    },
+    author = "Igor Sobreira",
+    author_email = "igor@realgeeks.com",
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='MIT',
+)
