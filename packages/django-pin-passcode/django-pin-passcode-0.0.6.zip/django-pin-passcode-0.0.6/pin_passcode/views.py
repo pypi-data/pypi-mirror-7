3ab40b9d98424d9b6a7fe2c9201e8464a3a0b343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package django-pin-passcode/0.0.6/django-pin-passcode-0.0.6.zip

---
+++ packages/django-pin-passcode/django-pin-passcode-0.0.6.zip/django-pin-passcode-0.0.6/pin_passcode/views.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib.auth import login, get_user_model
+from django.shortcuts import render, HttpResponse
+
+
+def form(request):
+    return render(request, 'pin_passcode/form.html')
+
+
+def auth(request):
+    if request.method == 'POST':
+        pin = request.POST.get('pin', None)
+        if pin == settings.PIN_PASSCODE_PIN:
+            username = settings.PIN_PASSCODE_USERNAME
+
+            if not username:
+                username = 'admin'
+
+            user = get_user_model().objects.get(username=username)
+            user.backend = 'django.contrib.auth.backends.ModelBackend'
+
+            login(request, user)
+            return HttpResponse(status=200)
+
+    return HttpResponse(status=401)
