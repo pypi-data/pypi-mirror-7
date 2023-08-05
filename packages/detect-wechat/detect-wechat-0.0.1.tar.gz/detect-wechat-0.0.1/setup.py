c72e086f2c279981d7d3e979f1745fd8ca815a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:42 +0000

    Add package detect-wechat/0.0.1/detect-wechat-0.0.1.tar.gz

---
+++ packages/detect-wechat/detect-wechat-0.0.1.tar.gz/detect-wechat-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup	
+
+setup(
+	name='detect-wechat',
+	version='0.0.1',
+	description='detect whether the client is a wechat browser',
+	author='yangchen',
+	author_email='yuhan534@126.com',
+	url='https://github.com/jarvys/wechat-agent',
+	install_requires=[],
+	py_modules=['detect_wechat'],
+	scripts=[],
+	keywords=['wechat', 'utilities', 'user-agent', 'detection']
+)
+
