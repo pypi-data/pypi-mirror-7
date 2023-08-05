c4be1edfe99dcdd854a92d8db48f0b5ea4af4c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:34 +0000

    Add package ssl_api/0.2.9/ssl_api-0.2.9.tar.gz

---
+++ packages/ssl_api/ssl_api-0.2.9.tar.gz/ssl_api-0.2.9/sapi/config/api/server.py
@@ -0,0 +1,27 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+# At the top of the request, perhaps to verify the caller by the client-hash or 
+# the information in the CSR.
+API_CSR_AUTHORIZE_HOOK = lambda \
+                            subject_alt_name_exts, \
+                            csr_tuple, \
+                            public_key_hash, \
+                            client_hash: None
+
+# Called with the certificate before it's signed, to add extensions or such.
+API_CSR_PRESIGN_HOOK = lambda \
+                            certificate, \
+                            public_key_hash, \
+                            client_hash: None
+
+# Called with the certificate after it's signed, perhaps to store the 
+# certificate's fingerprint.
+API_CSR_POSTSIGN_HOOK = lambda \
+                            certificate, \
+                            public_key_hash, \
+                            client_hash: None
+
+_logger.debug("Importing custom API hooks (required).")
+from sapi_custom_ca.api import *
