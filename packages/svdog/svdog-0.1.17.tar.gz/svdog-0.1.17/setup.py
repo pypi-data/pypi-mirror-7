c9468ce4f003ca42c1181404fa1d1078d4dd4e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package svdog/0.1.17/svdog-0.1.17.tar.gz

---
+++ packages/svdog/svdog-0.1.17.tar.gz/svdog-0.1.17/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="svdog",
+    version='0.1.17',
+    zip_safe=False,
+    platforms='any',
+    packages=['svdog'],
+    install_requires=['supervisor', 'flylog'],
+    scripts=['svdog/bin/run_svdog.py'],
+    url="https://github.com/dantezhu/svdog",
+    license="BSD",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="supervisor's dog, should deploy with flylog",
+)
