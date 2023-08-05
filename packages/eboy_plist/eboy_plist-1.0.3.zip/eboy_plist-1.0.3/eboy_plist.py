994620177289412e8d602b3f81186c7e5aa648fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package eboy_plist/1.0.3/eboy_plist-1.0.3.zip

---
+++ packages/eboy_plist/eboy_plist-1.0.3.zip/eboy_plist-1.0.3/eboy_plist.py
@@ -0,0 +1,12 @@
+""" 只是个循环列表的接口 """
+def print_list(this_List,indest=False,tabNum=0):
+    """ 只是个循环列表的接口 """
+    for movie in this_List:
+        if isinstance(movie,list) :
+           print_list(movie,tabNum+1)
+        else:
+            if indest:
+                for num in range(tabNum):
+                    print('\t',end='')
+            print(movie)
+print('_________________________')
---
+++ packages/eboy_plist/eboy_plist-1.0.3.zip/eboy_plist-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name ='eboy_plist',
+    version = '1.0.3',
+    py_modules =['eboy_plist'],
+    author ='eboy',
+    author_email = 'eboyasaboy@gmail.com',
+    url = 'http://www.baidu.com',
+    description = 'a example',
+    )
