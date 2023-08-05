c1a6f93e9c72b3ffd99107fb8b7b74ccc49eb1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package format-sql/0.0.2/format_sql-0.0.2-py2.py3-none-any.whl

---
+++ packages/format-sql/format_sql-0.0.2-py2.py3-none-any.whl/format_sql/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+format-sql
+
+Copyright (c) 2014, Friedrich Paetzke (paetzke@fastmail.fm)
+All rights reserved.
+
+"""
+from .file_handling import format_file
+from .formatter import format_sql
+
+__version__ = '0.0.2'
+__author__ = 'Friedrich Paetzke'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2014 Friedrich Paetzke'
