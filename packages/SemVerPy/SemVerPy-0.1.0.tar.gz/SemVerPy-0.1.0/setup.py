f0f8107e99f30103f9ec4893c1614f81c6b33eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package SemVerPy/0.1.0/SemVerPy-0.1.0.tar.gz

---
+++ packages/SemVerPy/SemVerPy-0.1.0.tar.gz/SemVerPy-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from semverpy import __version__, __author__, __license__
+
+setup(
+    name='SemVerPy',
+    description='Bump versions. Semantically.',
+    version=__version__,
+    author=__author__,
+    author_email='k@mackwerk.dk',
+    license=__license__,
+    url='https://github.com/Dinoshauer/SemVerPy',
+    py_modules=['semverpy']
+)
