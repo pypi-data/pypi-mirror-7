0835a904d6f97961e56002039a85ded816f6cd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package cases/0.1/cases-0.1.zip

---
+++ packages/cases/cases-0.1.zip/cases-0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+import cases
+
+setup(
+    name='cases',
+    version=cases.__version__,
+    description='Test cases generation tool',
+    author='Pavel Vetokhin',
+    url='https://github.com/pavetok/cases',
+    packages=find_packages(),
+    license='MIT',
+    long_description=open('README.md').read(),
+    install_requires = [
+        'AllPairs==2.0.1'
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities'
+    ]
+)
