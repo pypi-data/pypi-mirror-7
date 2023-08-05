f8052d7133aadbc85f0b70bf94913d92e65d48ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:31 +0000

    Add package vtapi/1.0/vtapi-1.0.tar.gz

---
+++ packages/vtapi/vtapi-1.0.tar.gz/vtapi-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="vtapi",
+      version="1.0",
+      py_modules=["vtapi"],
+      author="z.sean.huang",
+      author_email="z.sean.huang",
+     )
+
