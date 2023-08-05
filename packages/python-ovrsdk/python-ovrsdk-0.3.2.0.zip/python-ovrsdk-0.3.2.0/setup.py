ccbd87a091e50cdbc743073fb898c9e140c21001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:41 +0000

    Add package python-ovrsdk/0.3.2.0/python-ovrsdk-0.3.2.0.zip

---
+++ packages/python-ovrsdk/python-ovrsdk-0.3.2.0.zip/python-ovrsdk-0.3.2.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup ( \
+    name = 'python-ovrsdk',
+    version = '0.3.2.0',
+    description = 'Python wrapper for Oculus VR SDK.',
+    long_description = open('README.rst', 'r').read(),
+    author = 'Rye Terrell',
+    author_email = 'wwwtyro@gmail.com',
+    url = 'https://github.com/wwwtyro/python-ovrsdk',
+    packages = ['ovrsdk', 'ovrsdk.windows'],
+    package_data = {'': ['libovr.dll']}
+)
