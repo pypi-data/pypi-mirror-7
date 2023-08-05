fbb6c83db4bd10790aef5854ed12b2710a749170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:38 +0000

    Add package pyjks/0.2/pyjks-0.2.zip

---
+++ packages/pyjks/pyjks-0.2.zip/pyjks-0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='pyjks',
+    version='0.2',
+    author="Kurt Rose",
+    author_email="kurt@kurtrose.com",
+    description='pure python jks file parser',
+    license="MIT",
+    url="http://github.com/doublereedkurt/pyjks",
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=['jks'],
+    requires=['pyasn1'],)
