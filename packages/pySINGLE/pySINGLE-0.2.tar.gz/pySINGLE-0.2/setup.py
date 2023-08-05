d256db2aff75c55a7a51fd5b4114fd61957aaf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:28 +0000

    Add package pySINGLE/0.2/pySINGLE-0.2.tar.gz

---
+++ packages/pySINGLE/pySINGLE-0.2.tar.gz/pySINGLE-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='pySINGLE', 
+    packages = ['pySINGLE'],
+    version='0.2', 
+    description = 'Python implementation of SINGLE algorithm',
+    author = 'Ricardo Pio Monti',
+    author_email = 'ricardo.monti08@gmail.com',
+    url = 'https://github.com/piomonti/pySINGLE',
+    download_url = 'https://github.com/piomonti/pySINGLE/tarball/0.1',
+    #py_modules=['pySINGLE']
+    )
