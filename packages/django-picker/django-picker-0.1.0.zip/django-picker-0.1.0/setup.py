6df4a283ba0e1919aa97f86390a35fe58303785d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package django-picker/0.1.0/django-picker-0.1.0.zip

---
+++ packages/django-picker/django-picker-0.1.0.zip/django-picker-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name='django-picker',
+    version=version,
+    author=u'ShinZ Natkid',
+    author_email='shinznatkid@gmail.com',
+    packages=find_packages(), #['picker'],
+    url='http://github.com/shinznatkid/picker',
+    license='BSD',
+    include_package_data=True,
+    description='Put javascript, css framework into your project.',
+    long_description=open('README.md').read(),
+    zip_safe=True,
+    # package_data={
+    #     'picker': ['static/*'],
+    # },
+    install_requires=[
+        "django",
+    ],
+)
