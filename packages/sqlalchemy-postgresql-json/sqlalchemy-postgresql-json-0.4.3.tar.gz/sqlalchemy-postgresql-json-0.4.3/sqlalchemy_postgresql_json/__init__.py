cf64aa01ed1325c6968f8042a29fca17e19f9ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:49 +0000

    Add package sqlalchemy-postgresql-json/0.4.3/sqlalchemy-postgresql-json-0.4.3.tar.gz

---
+++ packages/sqlalchemy-postgresql-json/sqlalchemy-postgresql-json-0.4.3.tar.gz/sqlalchemy-postgresql-json-0.4.3/sqlalchemy_postgresql_json/__init__.py
@@ -0,0 +1,15 @@
+from pgjson import *
+from ltree import *
+from mutable import *
+
+__all__ = ['monkey_patch_array',
+           'monkey_patch_json',
+           'monkey_patch_all',
+           'JSONMutableDict',
+           'JSONMutableList',
+           'JSON',
+           'LTXTQUERY',
+           'LTREE',
+           'LQUERY',
+           'MutableList',
+           'MutableDict']
