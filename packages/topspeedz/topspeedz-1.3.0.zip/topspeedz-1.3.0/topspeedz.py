082cb7dbc0b4e78ef687c0ea301ae76ce49824ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package topspeedz/1.3.0/topspeedz-1.3.0.zip

---
+++ packages/topspeedz/topspeedz-1.3.0.zip/topspeedz-1.3.0/topspeedz.py
@@ -0,0 +1,13 @@
+
+"""printing element of list"""
+def print_lol(aList,indent=false,num=0):
+	"""function explanation"""
+
+	for each_item in aList:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,num+1)
+		else:
+			for i in range(num):
+				if indent:
+					print("\t",end="")
+			print(each_item)
