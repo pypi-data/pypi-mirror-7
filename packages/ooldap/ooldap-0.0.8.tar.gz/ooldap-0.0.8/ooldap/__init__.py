424c257fd1f2b93b5ad231868beed3d4e17d300f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:30 +0000

    Add package ooldap/0.0.8/ooldap-0.0.8.tar.gz

---
+++ packages/ooldap/ooldap-0.0.8.tar.gz/ooldap-0.0.8/ooldap/__init__.py
@@ -0,0 +1,20 @@
+import ldap
+
+
+GLOBAL_GROUP = '-2147483646'
+UNIVERSAL_GROUP = '-2147483640'
+
+
+class Connection(object):
+    def __init__(self, uri, dn, password):
+        self.uri = uri
+        self.dn = dn
+        self.password = password
+        assert uri and dn and password
+
+    def bind(self):
+        self.stream = ldap.open(self.uri)
+        self.stream.simple_bind_s(self.dn, self.password)
+
+    def unbind(self):
+        self.stream.unbind_s()
