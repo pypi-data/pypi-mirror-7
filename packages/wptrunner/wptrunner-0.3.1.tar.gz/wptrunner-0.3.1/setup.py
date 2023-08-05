1a006b4d1516d8291222b6bbbbf9cb08f22e59b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:55 +0000

    Add package wptrunner/0.3.1/wptrunner-0.3.1.tar.gz

---
+++ packages/wptrunner/wptrunner-0.3.1.tar.gz/wptrunner-0.3.1/setup.py
@@ -0,0 +1,48 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = 'wptrunner'
+PACKAGE_VERSION = '0.3.1'
+
+# dependencies
+with open('requirements.txt') as f:
+    deps = f.read().splitlines()
+
+profile_dest = None
+dest_exists = False
+
+setup(name=PACKAGE_NAME,
+      version=PACKAGE_VERSION,
+      description="Harness for running the W3C web-platform-tests against various products",
+      author='Mozilla Automation and Testing Team',
+      author_email='tools@lists.mozilla.org',
+      license='MPL 1.1/GPL 2.0/LGPL 2.1',
+      packages=find_packages(exclude=["tests", "metadata", "prefs"]),
+      entry_points={
+          'console_scripts': [
+              'wptrunner = wptrunner.wptrunner:main',
+              'wptupdate = wptrunner.update:main',
+          ]
+      },
+      zip_safe=False,
+      platforms=['Any'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+                   'Operating System :: OS Independent'],
+      package_data={"wptrunner": ["testharness.js",
+                                  "reftest.js",
+                                  "testharnessreport.js",
+                                  "gecko_runner.html",
+                                  "config.json",
+                                  "server-locations.txt",
+                                  "device_setup/*",
+                                  "prefs/*"]},
+      include_package_data=True,
+      data_files=[("config", ["config.ini"])],
+      install_requires=deps
+     )
