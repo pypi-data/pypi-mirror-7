883b4ee5af937c210d73b1a09f525e4359da02ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package olegdb-python/0.1.3/olegdb-python-0.1.3.tar.gz

---
+++ packages/olegdb-python/olegdb-python-0.1.3.tar.gz/olegdb-python-0.1.3/olegdb/oleg.py
@@ -0,0 +1,97 @@
+import requests, msgpack, pickle, time, calendar
+
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = 8080
+
+class OlegDB(object):
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        self.host = host
+        self.port = port
+
+    def _build_host_str(self, key):
+        connect_str = "http://{host}:{port}/{key}".format(
+                host=self.host, port=self.port, key=key)
+        return connect_str
+
+    def _pack_item(self, value):
+        new_value = None
+        try:
+            new_value = msgpack.packb(value, use_bin_type=True)
+        except TypeError as e:
+            print(e)
+            new_value = pickle.dumps(value)
+        return new_value
+
+    def add(self, key, value, timeout=None):
+        resp = requests.get(self._build_host_str(key))
+        if resp.status_code == 404:
+            self.set(key, value, timeout)
+            return True
+        return False
+
+    def get(self, key, default=None):
+        resp = requests.get(self._build_host_str(key), stream=True)
+
+        if resp.status_code == 404:
+            return default
+
+        raw_response = resp.raw.read()
+        try:
+            return msgpack.unpackb(raw_response, encoding='utf-8')
+        except msgpack.ExtraData:
+            # Fall back to pickle
+            return pickle.loads(raw_response)
+
+    def set(self, key, value, timeout=None):
+        new_value = self._pack_item(value)
+        connect_str = self._build_host_str(key)
+        headers = {}
+
+        if timeout is not None:
+            gmtime = time.gmtime()
+            seconds = int(calendar.timegm(gmtime))
+            expiration =  seconds + timeout
+            headers["X-OlegDB-use-by"] = expiration
+
+        requests.post(connect_str, data=new_value, headers=headers)
+        return True
+
+    def delete(self, key):
+        connect_str = self._build_host_str(key)
+        resp = requests.delete(connect_str)
+
+    def get_expiration(self, key):
+        connect_str = self._build_host_str(key)
+        resp = requests.head(connect_str)
+
+        if resp.status_code == 404:
+            return -2
+
+        if not resp.headers.get('expires', None):
+            return -1
+
+        return int(resp.headers['expires'])
+
+    def get_many(self, keys):
+        many = {}
+        for key in keys:
+            returned = self.get(key, version)
+            if returned:
+                many[key] = returned
+        return many
+
+    def has_key(self, key):
+        connect_str = self._build_host_str(key)
+        resp = requests.get(connect_str, stream=True)
+        if resp.status_code == 404:
+            return False
+        return True
+
+    def set_many(self, data, timeout=None):
+        for key, value in data.iteritems():
+            self.set(key, value, timeout)
+
+    def delete_many(self, keys):
+        for key in keys:
+            self.delete(key, value, timeout)
+
---
+++ packages/olegdb-python/olegdb-python-0.1.3.tar.gz/olegdb-python-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    author='Quinlan Pfiffer',
+    author_email='qpfiffer@gmail.com',
+    url='https://code.kyoto.io/qpfiffer/olegdb-python',
+    name='olegdb-python',
+    description='Python wrappers for generic OlegDB methods.',
+    version='0.1.3',
+    license='BSD',
+    keywords='OlegDB',
+    packages=['olegdb'],
+    zip_safe=False,
+    install_requires = [
+        "requests",
+        "msgpack-python>=0.4.1"
+    ]
+)
