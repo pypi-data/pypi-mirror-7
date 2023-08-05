83413c6bf401fa14a4106dd687f23b7dc7110e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:07 +0000

    Add package exoline/0.7.10/exoline-0.7.10.tar.gz

---
+++ packages/exoline/exoline-0.7.10.tar.gz/exoline-0.7.10/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+from exoline import __version__ as version
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    required.append('ordereddict>=1.1')
+
+try:
+    import importlib
+except ImportError:
+    required.append('importlib>=1.0.2')
+
+if sys.version_info < (3, 0):
+    required.append('unicodecsv==0.9.4')
+
+
+setup(
+    name='exoline',
+    version=version,
+    url = 'http://github.com/exosite/exoline',
+    author = 'Dan Weaver',
+    author_email = 'danweaver@exosite.com',
+    description = 'Command line interface for Exosite platform.',
+    long_description = open('README.md').read() + '\n\n' +
+                      open('HISTORY.md').read(),
+    packages=['exoline', 'exoline.plugins'],
+    package_dir={'exoline': 'exoline', 'plugins': 'exoline/plugins'},
+    scripts=['bin/exo', 'bin/exoline'],
+    keywords=['exosite', 'onep', 'one platform', 'm2m'],
+    install_requires=required,
+    zip_safe=False,
+    )
