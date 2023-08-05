81f90d99ce268ec05dbbd465ab665b6f487b459a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package svdog/0.1.18/svdog-0.1.18.tar.gz

---
+++ packages/svdog/svdog-0.1.18.tar.gz/svdog-0.1.18/svdog/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.18'
+
+import re
+import sys
+import logging
+
+from supervisor import childutils
+
+
+class SVDog(object):
+    def __init__(self, logger_name=None, processes=None, excludes=None):
+        self.processes = processes
+        self.excludes = excludes
+        self.stdin = sys.stdin
+        self.stdout = sys.stdout
+        self.logger = logging.getLogger(logger_name or __name__)
+
+    def run(self):
+        while True:
+            headers, payload = childutils.listener.wait(self.stdin, self.stdout)
+
+            if not payload.endswith('\n'):
+                payload = payload + '\n'
+
+            pheaders, pdata = childutils.eventdata(payload)
+
+            process_name = pheaders.get('processname')
+
+            if process_name and self.excludes and filter(lambda x: re.match(x, process_name), self.excludes):
+                childutils.listener.ok(self.stdout)
+                continue
+
+            if process_name and self.processes and not filter(lambda x: re.match(x, process_name), self.processes):
+                childutils.listener.ok(self.stdout)
+                continue
+
+            self.logger.fatal('%s\n%s', headers, payload)
+            childutils.listener.ok(self.stdout)
