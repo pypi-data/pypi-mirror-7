9cfc001b6b78c1a55122399e147c68ed48f28348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:39 +0000

    Add package Geoza/0.0.1/Geoza-0.0.1.tar.gz

---
+++ packages/Geoza/Geoza-0.0.1.tar.gz/Geoza-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='Geoza',
+    version='0.0.1',
+    author='Tanner Baldus',
+    author_email='tbaldus285@gmail.com',
+    packages=['geoza'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/geoza/',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='A prototype for playing songza playlists based on geoloction.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 2.2.1",
+        "docopt >= 0.6.1",
+    ],
+    entry_points={
+        'console_scripts': [
+         'geoza = geoza.geoza:main',
+        ],
+        }
+)
