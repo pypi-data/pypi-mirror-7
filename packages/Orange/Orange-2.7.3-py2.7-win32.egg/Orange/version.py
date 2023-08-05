e2c8d5cf2eeefc0e0bdcf5bb2ba44848a7679728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package Orange/2.7.3/Orange-2.7.3-py2.7-win32.egg

---
+++ packages/Orange/Orange-2.7.3-py2.7-win32.egg/Orange/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '2.7.3'
+version = '2.7.3'
+full_version = '2.7.3'
+hg_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
