db95b9c74491b487ab141cad0760f8628481b9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package rjson/0.72/rjson-0.72.zip

---
+++ packages/rjson/rjson-0.72.zip/rjson-0.72/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name="rjson", 
+	version="0.72", 
+	author="Mike Roylance",
+	py_modules=['rjson'],
+	description="easy way to get n number of json objects from a string automatically",
+	author_email="roylance.michael@gmail.com",
+	url="https://github.com/roylanceMichael/RandomLengthJsonLoader")
