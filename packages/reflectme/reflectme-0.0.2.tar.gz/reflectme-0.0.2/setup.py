5f615fdf6884b97bf382020cfc9c76eb05f866ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package reflectme/0.0.2/reflectme-0.0.2.tar.gz

---
+++ packages/reflectme/reflectme-0.0.2.tar.gz/reflectme-0.0.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+with open(os.path.join(package_dir, 'requirements.txt')) as f:
+    requires = [p.rstrip() for p in f.readlines()]
+
+
+setup(
+    name='reflectme',
+    version='0.0.2',
+    description='Simple HTTP server for recording requests and returning arbitrary responses.',
+    author='Charles Lavery',
+    author_email='charles.lavery@gmail.com',
+    url='https://github.com/clavery/reflectme',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT License',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ),
+    entry_points = {
+        'console_scripts': ['reflectme = reflectme.cli:main'],
+    },
+)
