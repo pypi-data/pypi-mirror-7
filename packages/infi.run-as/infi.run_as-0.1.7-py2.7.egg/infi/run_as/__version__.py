fbb89df91328f0aba90d208f94bfb907d04503dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package infi.run-as/0.1.7/infi.run_as-0.1.7-py2.7.egg

---
+++ packages/infi.run-as/infi.run_as-0.1.7-py2.7.egg/infi/run_as/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '67a76323314b688d3d9025a79829ef94ecedbfa0'
+__git_head_subject__ = 'Finished release v0.1.7.'
+__git_head_message__ = '* release/v0.1.7:\n  fixing the recursion in a different way\n  Revert "patching subprocess can yield to maximum recursion depth"'
+__git_dirty_diff__ = ''
