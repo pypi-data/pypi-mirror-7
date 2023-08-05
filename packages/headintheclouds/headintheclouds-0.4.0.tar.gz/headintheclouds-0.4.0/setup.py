85146016903f5e3b18dba7f100e5a88e0bfcb4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package headintheclouds/0.4.0/headintheclouds-0.4.0.tar.gz

---
+++ packages/headintheclouds/headintheclouds-0.4.0.tar.gz/headintheclouds-0.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='headintheclouds',
+      version='0.4.0',
+      description='Provider-agnostic cloud provisioning and Docker orchestration',
+      author='Andreas Jansson',
+      author_email='andreas@jansson.me.uk',
+      license='GNU GPLv3',
+      packages=['headintheclouds', 'headintheclouds.ensemble'],
+      url='https://github.com/andreasjansson/head-in-the-clouds',
+      install_requires=[
+          'Fabric>=1.6.1',
+          'PyYAML==3.10',
+          'boto==2.9.8',
+          'dop==0.1.6',
+          'python-dateutil==2.1',
+          'simplejson',
+          'PyDbLite>=2.5.0',
+      ],
+      dependency_links = [
+          'http://downloads.sourceforge.net/project/pydblite/pydblite/PyDbLite-2.5/PyDbLite-2.5.zip#egg=PyDbLite-2.5.0'
+      ],
+  )
