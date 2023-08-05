a970dd07bd2d2d04875719508abba5d468f5db45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package tacl/1.0/tacl-1.0.tar.gz

---
+++ packages/tacl/tacl-1.0.tar.gz/tacl-1.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+with open('README.rst') as fh:
+    long_description = fh.read()
+
+setup(name='tacl',
+      version='1.0',
+      description='Text analyser for corpus linguistics',
+      long_description=long_description,
+      author='Jamie Norrish',
+      author_email='jamie@artefact.org.nz',
+      url='https://github.com/ajenhl/tacl',
+      packages=['tacl'],
+      scripts=['bin/tacl', 'bin/tacl-helper'],
+      requires=['lxml', 'pandas'],
+      classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Linguistic',
+        ]
+      )
