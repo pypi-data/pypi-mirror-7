7cbeb2efc17ac6840c62291866e79d7acdad5bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package reFILE/0.2/reFILE-0.2-py2.py3-none-any.whl

---
+++ packages/reFILE/reFILE-0.2-py2.py3-none-any.whl/refile/printing.py
@@ -0,0 +1,26 @@
+# the future statement in __init__.py isn't picked up for some reason
+from __future__ import print_function
+
+
+def print_err(msg='unknown error', **kwargs):
+    ERROR = '\033[31mError:\033[0m'
+    print(ERROR, msg, **kwargs)
+
+
+def print_warn(msg='unknown warning', **kwargs):
+    WARNING = '\033[33mWarning:\033[0m'
+    print(WARNING, msg, **kwargs)
+
+
+def print_info(msg='well this isn\'t particularly helpful', **kwargs):
+    INFO = '\033[34mInfo:\033[0m'
+    print(INFO, msg, **kwargs)
+
+
+def print_files(d, f_list):
+    # if not current directory ('.')
+    if d.name != '' and f_list:
+        print(d, end='\n  ')
+        print('\n  '.join(f.name for f in f_list))
+    elif f_list:
+        print('\n'.join(f.name for f in f_list))
