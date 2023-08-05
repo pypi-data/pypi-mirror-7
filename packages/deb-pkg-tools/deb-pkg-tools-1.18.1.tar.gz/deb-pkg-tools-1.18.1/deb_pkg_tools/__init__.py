025aa91c0c252689d3e5c27376826c9bfffbe21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:51 +0000

    Add package deb-pkg-tools/1.18.1/deb-pkg-tools-1.18.1.tar.gz

---
+++ packages/deb-pkg-tools/deb-pkg-tools-1.18.1.tar.gz/deb-pkg-tools-1.18.1/deb_pkg_tools/__init__.py
@@ -0,0 +1,33 @@
+# Debian packaging tools.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: May 25, 2014
+# URL: https://github.com/xolox/python-deb-pkg-tools
+
+# Standard library modules.
+import logging
+
+# Initialize a logger.
+logger = logging.getLogger(__name__)
+
+# Semi-standard module versioning.
+__version__ = '1.18.1'
+
+# The following non-essential Debian packages need to be
+# installed in order for deb-pkg-tools to work properly.
+debian_package_dependencies = (
+    'apt',        # apt-get
+    'apt-utils',  # apt-ftparchive
+    'dpkg-dev',   # dpkg-scanpackages
+    'fakeroot',   # fakeroot
+    'gnupg',      # gpg
+    'lintian',    # lintian
+    'python-apt',
+)
+
+def generate_stdeb_cfg():
+    print('[deb-pkg-tools]')
+    print('Depends: %s' % ', '.join(debian_package_dependencies))
+    print('XS-Python-Version: >= 2.6')
+
+# vim: ts=4 sw=4 et
