9563f4db4c3124cec6123f10a9a9614f1682cd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package astack/0.0.4/astack-0.0.4.tar.gz

---
+++ packages/astack/astack-0.0.4.tar.gz/astack-0.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -
+from setuptools import setup
+
+setup(
+    name='astack',
+    version='0.0.4',
+    description='Simple stacktrace analysis tool for the JVM',
+    long_description=open('README.markdown').read(),
+    author='Michael Axiak',
+    author_email='mike@axiak.net',
+    license=open('LICENSE').read(),
+    url='https://github.com/HubSpot/astack/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    py_modules=['astack'],
+    entry_points={
+        'console_scripts':
+        ['astack=astack:main'],
+    })
