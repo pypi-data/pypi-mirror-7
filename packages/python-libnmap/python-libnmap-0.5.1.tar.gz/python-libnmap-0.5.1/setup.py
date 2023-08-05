38a36307987d9c4760a0962038241094a2f4dee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:32 +0000

    Add package python-libnmap/0.5.1/python-libnmap-0.5.1.tar.gz

---
+++ packages/python-libnmap/python-libnmap-0.5.1.tar.gz/python-libnmap-0.5.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+with open("README.rst") as rfile:
+    long_description = rfile.read()
+
+setup(
+    name='python-libnmap',
+    version='0.5.1',
+    author='Ronald Bister',
+    author_email='mini.pelle@gmail.com',
+    packages=['libnmap', 'libnmap.plugins', 'libnmap.objects'],
+    url='http://pypi.python.org/pypi/python-libnmap/',
+    license='Creative Common "Attribution" license (CC-BY) v3',
+    description=('Python NMAP library enabling you to start async nmap tasks, '
+                 'parse and compare/diff scan results'),
+    long_description=long_description,
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Environment :: Console",
+                 "Programming Language :: Python :: 2",
+                 "Programming Language :: Python :: 2.6",
+                 "Programming Language :: Python :: 2.7",
+                 "Topic :: System :: Networking"]
+)
