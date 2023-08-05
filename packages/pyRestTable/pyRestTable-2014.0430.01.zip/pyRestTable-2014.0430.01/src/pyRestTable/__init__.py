b963e2b3a253dcc66d4254de1614e4aabaf7bf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package pyRestTable/2014.0430.01/pyRestTable-2014.0430.01.zip

---
+++ packages/pyRestTable/pyRestTable-2014.0430.01.zip/pyRestTable-2014.0430.01/src/pyRestTable/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: iso-8859-1 -*-
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+import rest_table
+Table = rest_table.Table    # shorten the import trail
+
+
+__version__ = '2014.0430.01'
+__release__ = __version__
+__author__ = 'Pete R. Jemian'
+__email__ = 'prjemian@gmail.com'
+__copyright__ = '2014, Pete R. Jemian'
+
+__package_name__ = 'pyRestTable'
+__license_url__ = 'http://creativecommons.org/licenses/by/4.0/deed.en_US'
+__license__ = 'Creative Commons Attribution 4.0 International Public License (see LICENSE file)'
+__description__ = 'Format a nice table in reST (reStructuredText ) from Python'
+__author_name__ = __author__
+__author_email__ = __email__
+__url__ = 'https://github.com/prjemian/pyRestTable'
+__download_url__ = 'https://github.com/prjemian/pyRestTable/tarball/' + __version__
+__keywords__ = ['reST', 'table', 'documentation']
