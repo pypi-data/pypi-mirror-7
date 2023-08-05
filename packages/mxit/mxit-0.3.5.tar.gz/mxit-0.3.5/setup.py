ed248a2c19dcccd8c80aaa4864c0e64226968ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package mxit/0.3.5/mxit-0.3.5.tar.gz

---
+++ packages/mxit/mxit-0.3.5.tar.gz/mxit-0.3.5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
+
+
+setup(
+    name='mxit',
+    version='0.3.5',
+    author='Philip Perold',
+    author_email='prperold@gmail.com',
+    packages=['mxit'],
+    url='https://github.com/Mxit/python-mxit',
+    license='LICENSE',
+    description="Python utility library for accessing Mxit's public APIs.",
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+        "requests == 2.0.1",
+    ],
+)
+
