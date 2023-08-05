9da24fc9816508be8ff9825658103be0c841babc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package pyqode.core/1.3.2/pyqode.core-1.3.2.tar.gz

---
+++ packages/pyqode.core/pyqode.core-1.3.2.tar.gz/pyqode.core-1.3.2/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#The MIT License (MIT)
+#
+#Copyright (c) <2013-2014> <Colin Duquesnoy and others, see AUTHORS.txt>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+#
+"""
+Setup script for pyqode.core
+"""
+from setuptools import setup, find_packages
+
+
+def read_version():
+    with open("pyqode/core/__init__.py") as f:
+        lines = f.read().splitlines()
+        for l in lines:
+            if "__version__" in l:
+                return l.split("=")[1].strip().replace('"', '')
+
+
+def readme():
+    return str(open('README.rst').read())
+
+
+setup(
+    name='pyqode.core',
+    namespace_packages=['pyqode'],
+    version=read_version(),
+    packages=find_packages(),
+    keywords=["QCodeEdit PySide PyQt code editor widget"],
+    url='https://github.com/pyQode/pyqode.core',
+    license='MIT',
+    author='Colin Duquesnoy',
+    author_email='colin.duquesnoy@gmail.com',
+    description='Python/Qt Code Editor widget',
+    long_description=readme(),
+    install_requires=['pygments'],
+    entry_points={'pyqode_plugins':
+                  ['pyqode_core = pyqode.core.plugins.pyqode_core_plugin']},
+    zip_safe=False,
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: X11 Applications :: Qt',
+          'Environment :: Win32 (MS Windows)',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+          'Topic :: Software Development :: Widget Sets',
+          'Topic :: Text Editors :: Integrated Development Environments (IDE)'])
