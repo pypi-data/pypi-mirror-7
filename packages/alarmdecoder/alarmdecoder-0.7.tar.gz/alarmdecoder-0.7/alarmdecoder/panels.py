aec7c65ae895b15dbd3c60d9f88655b6ec69146a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package alarmdecoder/0.7/alarmdecoder-0.7.tar.gz

---
+++ packages/alarmdecoder/alarmdecoder-0.7.tar.gz/alarmdecoder-0.7/alarmdecoder/panels.py
@@ -0,0 +1,15 @@
+"""
+Representations of Panels and their templates.
+
+.. moduleauthor:: Scott Petersen <scott@nutech.com>
+"""
+
+VISTA20 = 0
+
+TEMPLATES = {
+    VISTA20: {
+        'name': 'Vista 20',
+        # number of expanders, starting_address, number of channels
+        'expanders': (5, 7, 7)
+    }
+}
