78fbe94fc93bf64b1664c2454aa57ba83f5d887e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:23 +0000

    Add package thealot-compendium/0.1/thealot-compendium-0.1.tar.gz

---
+++ packages/thealot-compendium/thealot-compendium-0.1.tar.gz/thealot-compendium-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(name='thealot-compendium',
+      version='0.1',
+      author='Edvin "nCrazed" Malinovskis',
+      author_email='edvin.malinovskis@gmail.com',
+      url='https://github.com/nCrazed/CompendiumPlugin',
+      packages=['thealot.plugins'],
+      namespace_packages=['thealot'],
+      install_requires=[
+          'requests',
+          'thealot',
+          ]
+      )
