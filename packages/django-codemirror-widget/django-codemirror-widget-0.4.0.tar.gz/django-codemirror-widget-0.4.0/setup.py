d9aa1a97dc36574c7c6975f06af0e36a05c81346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package django-codemirror-widget/0.4.0/django-codemirror-widget-0.4.0.tar.gz

---
+++ packages/django-codemirror-widget/django-codemirror-widget-0.4.0.tar.gz/django-codemirror-widget-0.4.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author:       Alisue
+# Last Change:  18-Mar-2011.
+#
+from setuptools import setup, find_packages
+
+version = "0.4.0"
+
+def read(filename):
+    import os.path
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+setup(
+    name="django-codemirror-widget",
+    version=version,
+    description = "django-codemirror-widget is Django form widget library for using CodeMirror on textarea",
+    long_description=read('README.rst'),
+    classifiers = [
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords = "django widget textarea codemirror",
+    author = "Alisue",
+    author_email = "lambdalisue@hashnote.net",
+    url=r"https://github.com/lambdalisue/django-codemirror-widget",
+    download_url = r"https://github.com/lambdalisue/django-codemirror-widget/tarball/master",
+    license = 'BSD',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires=['setuptools'],
+)
