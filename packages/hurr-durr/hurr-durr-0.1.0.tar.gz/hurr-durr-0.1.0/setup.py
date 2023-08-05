0caa54a238ded4ba12427c544408833e51500fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package hurr-durr/0.1.0/hurr-durr-0.1.0.tar.gz

---
+++ packages/hurr-durr/hurr-durr-0.1.0.tar.gz/hurr-durr-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import hurr_durr
+import os
+
+setup_pth = os.path.dirname(__file__)
+readme_pth = os.path.join(setup_pth, 'README.md')
+
+setup(
+    name='hurr-durr',
+    version=hurr_durr.__version__,
+    description='Streaming async interface to 4chan boards',
+    author='Emils Solmanis',
+    author_email='emils.solmanis@gmail.com',
+    license='Apache License (2.0)',
+    keywords='4chan stream async',
+    url='https://github.com/emilssolmanis/hurr-durr',
+    download_url='https://github.com/emilssolmanis/hurr-durr/archive/0.0.5.tar.gz',
+    long_description=open(readme_pth).read(),
+
+    packages=find_packages(),
+    scripts=['hurr-durr'],
+    zip_safe=True,
+
+    install_requires=['tornado==3.2']
+)
