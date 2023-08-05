8628b5142a31975fb9b2c7df593bbb942d59fde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:43 +0000

    Add package scrapy_model/0.1.5/scrapy_model-0.1.5.tar.gz

---
+++ packages/scrapy_model/scrapy_model-0.1.5.tar.gz/scrapy_model-0.1.5/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.md').read()
+
+requirements = [
+    'wheel==0.23.0',
+    'cffi==0.8.2',
+    'Scrapy==0.22.2',
+    'redis==2.8.0',
+    'requests==1.2.3',
+]
+
+test_requirements = [
+]
+
+setup(
+    name='scrapy_model',
+    version='0.1.5',
+    description='Scrapy helper to create scrapers from models',
+    long_description=readme,
+    author='Bruno Rocha',
+    author_email='rochacbruno@gmail.com',
+    url='https://github.com/rochacbruno/scrapy_model',
+    py_modules=['scrapy_model'],
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='scrapy_model',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
