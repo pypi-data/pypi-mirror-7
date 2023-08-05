09193c760ea0fb8103f5c42a3496210d4fb7da69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:49 +0000

    Add package solrcloudpy/1.3.460/solrcloudpy-1.3.460.tar.gz

---
+++ packages/solrcloudpy/solrcloudpy-1.3.460.tar.gz/solrcloudpy-1.3.460/solrcloudpy/__init__.py
@@ -0,0 +1,6 @@
+from solrcloudpy.connection import SolrConnection
+from solrcloudpy.collection import SolrCollection
+from solrcloudpy.parameters import SearchOptions
+
+__version__ = "1.3.460"
+__all__ = ['SolrCollection','SolrConnection','SearchOptions']
