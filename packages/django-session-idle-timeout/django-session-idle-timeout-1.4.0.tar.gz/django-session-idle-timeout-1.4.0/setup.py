a5eb36d3df088b5e4397b328f7f7e3cf4ddf1238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:20 +0000

    Add package django-session-idle-timeout/1.4.0/django-session-idle-timeout-1.4.0.tar.gz

---
+++ packages/django-session-idle-timeout/django-session-idle-timeout-1.4.0.tar.gz/django-session-idle-timeout-1.4.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-session-idle-timeout",
+    install_requires = [
+        'Django',
+    ],
+    packages = find_packages(),
+    include_package_data=True,
+    version = "1.4.0",
+    description = "Timeout a logged user after a period of time",
+    long_description=open('README.md').read(-1),
+    author = "Tomas Zulberti",
+    zip_safe = False,
+    author_email = "tzulberti@gmail.com",
+    url = "http://github.com/tzulberti/django-session-idle-timeout",
+    keywords = [
+        "django contrib",
+        "session login expiration"
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent'
+        ],
+    license = 'License :: OSI Approved :: BSD License',
+)
