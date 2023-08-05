60cf03e99836bca1079ee94601efe2bb802fd6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:57 +0000

    Add package Flask-Widgets/0.3/Flask-Widgets-0.3.tar.gz

---
+++ packages/Flask-Widgets/Flask-Widgets-0.3.tar.gz/Flask-Widgets-0.3/setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-Widgets
+-------------
+
+Basic widgets support for Flask
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-Widgets',
+    version='0.3',
+    url='https://github.com/rgv151/flask-widgets',
+    license='MIT',
+    author='Bruce Doan',
+    author_email='me@huy.im',
+    description='This extension provides basic template widget feature for Flask',
+    long_description=__doc__,
+    py_modules=['flask_widgets'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
