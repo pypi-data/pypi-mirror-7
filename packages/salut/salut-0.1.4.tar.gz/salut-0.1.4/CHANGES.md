f3846a286c1d655583fc234dbe6c3019831b8f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package salut/0.1.4/salut-0.1.4.tar.gz

---
+++ packages/salut/salut-0.1.4.tar.gz/salut-0.1.4/CHANGES.md
@@ -0,0 +1,45 @@
+Salut Changelog
+==============
+
+Salut follows the Semantic Versioning standard, although not for development
+before version 1. Read more at http://semvar.org
+
+
+Version 0.1.4
+-------------
+
+Released on May 30nd 2014
+
+- Fix a bug when the FQDN is returned from browsing services
+
+
+Version 0.1.3
+-------------
+
+Released on May 22nd 2013
+
+- Fix a bug that happens when browsing before network is up
+
+
+Version 0.1.2
+-------------
+
+Released on January 22nd 2013
+
+- Include this changes file with setup
+
+
+Version 0.1.1
+-------------
+
+Released on January 22nd 2013
+
+- Add readme and license files with setup
+
+
+Version 0.1.0
+-------------
+
+Released on January 22nd 2013
+
+- First release
---
+++ packages/salut/salut-0.1.4.tar.gz/salut-0.1.4/setup.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2012-2013 - Max Persson <max@looplab.se>
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+execfile('salut/version.py')
+
+
+from setuptools import setup
+
+
+setup(
+    name=__title__,
+    version=__version__,
+    packages=['salut'],
+
+    install_requires=['gevent', 'pybonjour'],
+
+    tests_require=['nose'],
+    test_suite='nose.collector',
+
+    author=__author__,
+    author_email='max@looplab.se',
+    description='Gevent based wrapper for pybonjour',
+    license=__license__,
+    url=__project_url__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking'
+    ],
+)
