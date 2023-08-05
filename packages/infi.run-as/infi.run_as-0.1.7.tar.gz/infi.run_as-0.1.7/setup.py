092514e836aa55249ae763bdad64c7fcfcaa418e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:39 +0000

    Add package infi.run-as/0.1.7/infi.run_as-0.1.7.tar.gz

---
+++ packages/infi.run-as/infi.run_as-0.1.7.tar.gz/infi.run_as-0.1.7/setup.py
@@ -0,0 +1,54 @@
+
+SETUP_INFO = dict(
+    name = 'infi.run_as',
+    version = '0.1.7',
+    author = 'Guy Rozendorn',
+    author_email = 'guy@rzn.co.il',
+
+    url = 'https://github.com/Infinidat/infi.run_as',
+    license = 'PSF',
+    description = """run_as utility""",
+    long_description = """run_as utility""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.cwrap',
+'infi.execute',
+'infi.instruct',
+'infi.pyutils',
+'infi.winver',
+'mock',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = ['run_as = infi.run_as:run_as'],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
