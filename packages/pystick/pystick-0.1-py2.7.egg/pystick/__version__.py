8ab4a8ee82b1ceb3088beb541cfb424f3f698411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:47 +0000

    Add package pystick/0.1/pystick-0.1-py2.7.egg

---
+++ packages/pystick/pystick-0.1-py2.7.egg/pystick/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '213c1f69e0cdae4243bf0c4d3d9a89a6284f8b07'
+__git_head_subject__ = 'Finished release v0.1.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
