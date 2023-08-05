69f41bbda55e770dbab543ab462be9292af8ca7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package shanbay/0.1.1.dev/shanbay-0.1.1.dev.zip

---
+++ packages/shanbay/shanbay-0.1.1.dev.zip/shanbay-0.1.1.dev/CHANGELOG.rst
@@ -0,0 +1,16 @@
+Changelog
+=========
+
+
+0.1.1 (2014-xx-yy)
+------------------
+
+- [add] 站内消息 api
+- [add] 小组管理 api
+- [change] api 接口
+
+
+0.1.0 (2014-03-31)
+------------------
+
+- 封装 `扇贝网 API v0.8 <http://www.shanbay.com/help/developer/api>`__.
---
+++ packages/shanbay/shanbay-0.1.1.dev.zip/shanbay-0.1.1.dev/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from codecs import open
+import sys
+import os
+
+__version__ = '0.1.1.dev'
+__author__ = 'mozillazg'
+__license__ = 'MIT'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+requirements = [
+    'requests>=2.0.1',
+    'beautifulsoup4',
+    'html5lib',
+]
+packages = [
+    'shanbay',
+]
+
+
+def long_description():
+    readme = open('README.rst', encoding='utf8').read()
+    text = readme + '\n\n' + open('CHANGELOG.rst', encoding='utf8').read()
+    return text
+
+setup(
+    name='shanbay',
+    version=__version__,
+    description='Python wrapper for shanbay.com',
+    long_description=long_description(),
+    url='https://github.com/mozillazg/python-shanbay',
+    download_url='https://github.com/mozillazg/python-shanbay/archive/master.zip',
+    author=__author__,
+    author_email='mozillazg101@gmail.com',
+    license=__license__,
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'shanbay': 'shanbay'},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+    ],
+    keywords='shanbay, 扇贝网',
+)
