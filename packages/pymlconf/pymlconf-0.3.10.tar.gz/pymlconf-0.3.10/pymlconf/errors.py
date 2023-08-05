00d9555b8b54ae1acafe39f4cbe45cf07d3295fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package pymlconf/0.3.10/pymlconf-0.3.10.tar.gz

---
+++ packages/pymlconf/pymlconf-0.3.10.tar.gz/pymlconf-0.3.10/pymlconf/errors.py
@@ -0,0 +1,15 @@
+
+
+
+class ConfigurationError(Exception):
+    def __init__(self, message):
+        super(ConfigurationError,self).__init__(message)
+
+class ConfigKeyError(ConfigurationError, AttributeError):
+    def __init__(self, key):
+        AttributeError.__init__(self)
+        ConfigurationError.__init__(self,'Config key was not found: "%s"' % key)
+
+class ConfigurationMergeError(ConfigurationError, ValueError):
+    def __init__(self, message):
+        super(ConfigurationError,self).__init__(message)
