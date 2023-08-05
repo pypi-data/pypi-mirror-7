0a467d4d5f8ab5ed8544b6d4810430b9bb2688e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:17 +0000

    Add package areyoustillthere/0.1.1/areyoustillthere-0.1.1.tar.gz

---
+++ packages/areyoustillthere/areyoustillthere-0.1.1.tar.gz/areyoustillthere-0.1.1/setup.py
@@ -0,0 +1,19 @@
+"""Are You Still There is a simple Python library testing if
+email addresses actually exist.
+
+"""
+from setuptools import setup
+
+setup(
+    name='areyoustillthere',
+    version='0.1.1',
+    long_description=__doc__,
+    packages=['ayst'],
+    include_package_data=True,
+    author='Anthony Grimes',
+    author_email='i@raynes.me',
+    url='https://github.com/Raynes/areyoustillthere',
+    description='Find dem emails yo.',
+    license='MIT',
+    install_requires=['pydns>=2.3.6']
+)
