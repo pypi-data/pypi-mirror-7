d8851a567c40da03af9909c404a2d45b7358a367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package sftdyn/0.1/sftdyn-0.1.tar.gz

---
+++ packages/sftdyn/sftdyn-0.1.tar.gz/sftdyn-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+from sftdyn import VERSION
+
+setup(
+	name="sftdyn",
+	version=VERSION,
+	description="HTTPs-based DDNS updater",
+	long_description="dyndns-like service that accepts update requests " +
+					 "via HTTPs and forwards them to a locally running DNS " +
+					 "server via nsupdate -l",
+	author="Michael Enßlin",
+	author_email="michael@ensslin.cc",
+	url="https://github.com/SFTtech/sftdyn",
+	license="GPLv3 or higher",
+	packages=["sftdyn"],
+	scripts=["bin/sftdyn"],
+	data_files=[("/etc/sftdyn/", ["sample.conf"])]
+)
