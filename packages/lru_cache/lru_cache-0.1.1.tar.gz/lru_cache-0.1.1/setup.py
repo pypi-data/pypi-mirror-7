b17119bdaa108a3e3e0b6db3fa97d45070a97bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package lru_cache/0.1.1/lru_cache-0.1.1.tar.gz

---
+++ packages/lru_cache/lru_cache-0.1.1.tar.gz/lru_cache-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='lru_cache',
+    version='0.1.1',
+    author='acmerfight',
+    author_email='acmerfight@gmail.com',
+    packages=['lru_cache'],
+    url='https://github.com/acmerfight/lru_cache',
+    license='GPL',
+    description='thread safe lru cache',
+    long_description=open('README.md').read(),
+)
