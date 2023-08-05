6592d0dec831b18beeaff84f595fc92bc06bdf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:53 +0000

    Add package streamcorpus_pipeline/0.5.24/streamcorpus_pipeline-0.5.24-py2.7.egg

---
+++ packages/streamcorpus_pipeline/streamcorpus_pipeline-0.5.24-py2.7.egg/streamcorpus_pipeline/util.py
@@ -0,0 +1,7 @@
+'''
+This software is released under an MIT/X11 open source license.
+
+Copyright 2012-2013 Diffeo, Inc.
+'''
+from __future__ import absolute_import
+from streamcorpus_pipeline._clean_visible import cleanse
