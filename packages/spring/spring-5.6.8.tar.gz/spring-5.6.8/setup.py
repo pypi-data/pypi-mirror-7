63afa4ad3ecfd4e18925f7678dd71ec87a0ff1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package spring/5.6.8/spring-5.6.8.tar.gz

---
+++ packages/spring/spring-5.6.8.tar.gz/spring-5.6.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, Extension
+
+from spring.version import VERSION
+
+fastdocgen = Extension('fastdocgen', sources=['src/fastdocgen.c'])
+
+
+setup(
+    name='spring',
+    version=VERSION,
+    description='Simple Couchbase workload generator based on pylibcouchbase',
+    author='Pavel Paulau',
+    author_email='pavel.paulau@gmail.com',
+    packages=['spring'],
+    entry_points={
+        'console_scripts': ['spring = spring.__main__:main']
+    },
+    install_requires=[
+        'couchbase==1.2.0',
+        'decorator',
+        'logger',
+        'numpy',
+        'requests==2.1.0',
+        'twisted==12.1.0',
+    ],
+    ext_modules=[
+        fastdocgen
+    ],
+)
