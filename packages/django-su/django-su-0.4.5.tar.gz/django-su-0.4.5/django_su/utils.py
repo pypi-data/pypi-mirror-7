2d213d8f24ebeecf4402a42bb3b705a079134404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:09 +0000

    Add package django-su/0.4.5/django-su-0.4.5.tar.gz

---
+++ packages/django-su/django-su-0.4.5.tar.gz/django-su-0.4.5/django_su/utils.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+
+
+def import_function(name, package=None):
+    path = name.split('.')
+    module_path = '.'.join(path[:-1])
+    try:
+        from django.utils.importlib import import_module
+        module = import_module(module_path, package)
+    except ImportError:  # compatible with old version of Django
+        module = __import__(module_path, {}, {}, path[-1])
+    return getattr(module, path[-1])
+
+
+def can_su_login(user):
+    su_login = getattr(settings, 'SU_LOGIN', None)
+    if su_login:
+        if not callable(su_login):
+            su_login = import_function(su_login)
+        return su_login(user)
+    return user.has_perm('auth.change_user')
+
+
+def custom_login_action(request, su_user):
+    custom_login_action = getattr(settings, 'SU_CUSTOM_LOGIN_ACTION', None)
+    if custom_login_action:
+        if not callable(custom_login_action):
+            custom_login_action = import_function(custom_login_action)
+        custom_login_action(request, su_user)
+        return True
+    return False
+
+
+def get_static_url():
+    static_url = getattr(settings, 'STATIC_URL', None)
+    if static_url:
+        return static_url
+    else:  # To old django versions
+        return '%sajax_select/' % getattr(settings, 'MEDIA_URL', None)
