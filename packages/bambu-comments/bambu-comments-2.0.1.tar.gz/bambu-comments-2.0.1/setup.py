40af280db907d0f97ee743c0bbfdee4e8a16e685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:17 +0000

    Add package bambu-comments/2.0.1/bambu-comments-2.0.1.tar.gz

---
+++ packages/bambu-comments/bambu-comments-2.0.1.tar.gz/bambu-comments-2.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+from os import path
+
+setup(
+    name = 'bambu-comments',
+    version = '2.0.1',
+    description = 'Generic model commenting',
+    author = 'Steadman',
+    author_email = 'mark@steadman.io',
+    url = 'https://github.com/iamsteadman/bambu-comments',
+    long_description = open(path.join(path.dirname(__file__), 'README')).read(),
+    install_requires = [
+        'Django>=1.4',
+        'pyquery',
+        'html2text',
+        'markdown',
+        'bambu-mail>=2.0',
+        'requests>=2.2.1'
+    ],
+    packages = [
+        'bambu_comments',
+        'bambu_comments.templatetags',
+        'bambu_comments.migrations'
+    ],
+    package_data = {
+        'bambu_comments': [
+            'templates/comments/*.html',
+            'templates/comments/*.txt',
+            'templates/search/indexes/bambu_comments/*.txt'
+        ]
+    },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django'
+    ]
+)
