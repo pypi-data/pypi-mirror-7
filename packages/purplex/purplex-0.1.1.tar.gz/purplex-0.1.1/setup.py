9b4e001a87da243a6c1c0c0278332643e96f2506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package purplex/0.1.1/purplex-0.1.1.tar.gz

---
+++ packages/purplex/purplex-0.1.1.tar.gz/purplex-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+# from http://pytest.org/latest/goodpractises.html
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(name='purplex',
+      version='0.1.1',
+      description='Pure python lexer implementation.',
+      author='Michael Tom-Wing',
+      author_email='mtomwing@gmail.com',
+      url='https://github.com/mtomwing/purplex',
+      packages=['purplex'],
+      license='MIT',
+      install_requires=['ply==3.4'],
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
+      zip_safe=False)
