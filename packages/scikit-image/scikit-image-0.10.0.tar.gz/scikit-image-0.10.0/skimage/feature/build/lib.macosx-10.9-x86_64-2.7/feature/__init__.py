f9395f1aac2728bdf4151e20bc72331b5a7b1bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:21 +0000

    Add package scikit-image/0.10.0/scikit-image-0.10.0.tar.gz

---
+++ packages/scikit-image/scikit-image-0.10.0.tar.gz/scikit-image-0.10.0/skimage/feature/build/lib.macosx-10.9-x86_64-2.7/feature/__init__.py
@@ -0,0 +1,45 @@
+from ._daisy import daisy
+from ._hog import hog
+from .texture import greycomatrix, greycoprops, local_binary_pattern
+from .peak import peak_local_max
+from .corner import (corner_kitchen_rosenfeld, corner_harris,
+                     corner_shi_tomasi, corner_foerstner, corner_subpix,
+                     corner_peaks, corner_fast, structure_tensor,
+                     structure_tensor_eigvals, hessian_matrix,
+                     hessian_matrix_eigvals)
+from .corner_cy import corner_moravec, corner_orientations
+from .template import match_template
+from .brief import BRIEF
+from .censure import CENSURE
+from .orb import ORB
+from .match import match_descriptors
+from .util import plot_matches
+from .blob import blob_dog
+
+
+__all__ = ['daisy',
+           'hog',
+           'greycomatrix',
+           'greycoprops',
+           'local_binary_pattern',
+           'peak_local_max',
+           'structure_tensor',
+           'structure_tensor_eigvals',
+           'hessian_matrix',
+           'hessian_matrix_eigvals',
+           'corner_kitchen_rosenfeld',
+           'corner_harris',
+           'corner_shi_tomasi',
+           'corner_foerstner',
+           'corner_subpix',
+           'corner_peaks',
+           'corner_moravec',
+           'corner_fast',
+           'corner_orientations',
+           'match_template',
+           'BRIEF',
+           'CENSURE',
+           'ORB',
+           'match_descriptors',
+           'plot_matches',
+           'blob_dog']
