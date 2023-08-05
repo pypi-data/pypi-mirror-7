d33ba04f012b05c02eddb0e7f119a73f9b9658ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package munerator/0.4.4/munerator-0.4.4.tar.gz

---
+++ packages/munerator/munerator-0.4.4.tar.gz/munerator-0.4.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+setup(
+    name='munerator',
+    version="0.4.4",
+    description='Manager of OpenArena battles',
+    long_description=read("README.rst"),
+    author='Johan Bloemberg',
+    author_email='mail@ijohan.nl',
+    url='https://github.com/aequitas/munerator',
+    install_requires=[
+        'docopt',
+        'pyzmq',
+        'webcolors',
+        'python-Levenshtein'
+    ],
+    packages=find_packages(),
+    license=read("LICENSE"),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points={
+        'console_scripts': [
+            "munerator = munerator:main",
+        ]
+    },
+)
