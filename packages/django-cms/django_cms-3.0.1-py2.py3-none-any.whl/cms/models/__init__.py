7991d727be691ce5bd2291dbe556d9bf391ed84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:18 +0000

    Add package django-cms/3.0.1/django_cms-3.0.1-py2.py3-none-any.whl

---
+++ packages/django-cms/django_cms-3.0.1-py2.py3-none-any.whl/cms/models/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings as d_settings
+from .settingmodels import *  # nopyflakes
+from .pagemodel import *  # nopyflakes
+from .permissionmodels import *  # nopyflakes
+from .placeholdermodel import *  # nopyflakes
+from .pluginmodel import *  # nopyflakes
+from .titlemodels import *  # nopyflakes
+from .placeholderpluginmodel import *  # nopyflakes
+from .static_placeholder import *  # nopyflakes
+
+# must be last
+from cms import signals as s_import  # nopyflakes
+
+
+def validate_settings():
+    if "django.core.context_processors.request" not in d_settings.TEMPLATE_CONTEXT_PROCESSORS:
+        raise ImproperlyConfigured('django-cms needs django.core.context_processors.request in settings.TEMPLATE_CONTEXT_PROCESSORS to work correctly.')
+    if 'mptt' not in d_settings.INSTALLED_APPS:
+        raise ImproperlyConfigured('django-cms needs django-mptt installed.')
+    if 'cms.middleware.multilingual.MultilingualURLMiddleware' in d_settings.MIDDLEWARE_CLASSES and 'django.middleware.locale.LocaleMiddleware' in d_settings.MIDDLEWARE_CLASSES:
+        raise ImproperlyConfigured('django-cms MultilingualURLMiddleware replaces django.middleware.locale.LocaleMiddleware! Please remove django.middleware.locale.LocaleMiddleware from your MIDDLEWARE_CLASSES settings.')
+
+
+def validate_dependencies():
+    # check for right version of reversions
+    if 'reversion' in d_settings.INSTALLED_APPS:
+        from reversion.admin import VersionAdmin
+        if not hasattr(VersionAdmin, 'get_urls'):
+            raise ImproperlyConfigured('django-cms requires never version of reversion (VersionAdmin must contain get_urls method)')
+
+validate_dependencies()
+validate_settings()
