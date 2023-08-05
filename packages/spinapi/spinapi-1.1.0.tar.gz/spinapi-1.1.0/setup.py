1f53f2bbd9ce5b1bf975219e26ecc23522cc54fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package spinapi/1.1.0/spinapi-1.1.0.tar.gz

---
+++ packages/spinapi/spinapi-1.1.0.tar.gz/spinapi-1.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# To upload a version to PyPI, run:
+#
+#    python setup.py sdist upload
+#
+# If the package is not registered with PyPI yet, do so with:
+#
+# python setup.py register
+
+from distutils.core import setup
+import os
+
+VERSION = '1.1.0'
+
+# Auto generate a __version__ package for the package to import
+with open(os.path.join('spinapi', '__version__.py'), 'w') as f:
+    f.write("__version__ = '%s'\n"%VERSION)
+    
+setup(name='spinapi',
+      version=VERSION,
+      description='Python wrapper around the Spincore PulseBlaster API using ctypes.',
+      author='Chris Billington',
+      author_email='chrisjbillington@gmail.com',
+      url='https://bitbucket.org/cbillington/spinapi/',
+      license="BSD",
+      packages=['spinapi']
+     )
