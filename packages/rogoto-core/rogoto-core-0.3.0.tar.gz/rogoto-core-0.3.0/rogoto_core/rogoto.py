dc8a9b7ea2d2bac96635a1d635a980b1cef6cae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package rogoto-core/0.3.0/rogoto-core-0.3.0.tar.gz

---
+++ packages/rogoto-core/rogoto-core-0.3.0.tar.gz/rogoto-core-0.3.0/rogoto_core/rogoto.py
@@ -0,0 +1,51 @@
+from BrickPi import *
+
+
+speed = 150
+
+BrickPiSetup()
+BrickPi.MotorEnable[PORT_A] = 1
+BrickPi.MotorEnable[PORT_B] = 1
+BrickPi.MotorEnable[PORT_C] = 1
+
+
+def pen_down():
+    BrickPi.MotorSpeed[PORT_A] = speed
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_A] = 0
+
+
+def pen_up():
+    BrickPi.MotorSpeed[PORT_A] = -speed
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_A] = 0
+
+
+def forward(steps):
+    BrickPi.MotorSpeed[PORT_B] = steps * speed
+    BrickPi.MotorSpeed[PORT_C] = steps * speed
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_B] = 0
+    BrickPi.MotorSpeed[PORT_C] = 0
+
+
+def backward(steps):
+    BrickPi.MotorSpeed[PORT_B] = steps * -speed
+    BrickPi.MotorSpeed[PORT_C] = steps * -speed
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_B] = 0
+    BrickPi.MotorSpeed[PORT_C] = 0
+
+
+def left(deg):
+    BrickPi.MotorSpeed[PORT_B] = deg * speed
+    BrickPi.MotorSpeed[PORT_C] = deg * -speed
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_B] = 0
+
+
+def right(deg):
+    BrickPi.MotorSpeed[PORT_C] = deg * speed
+    BrickPi.MotorSpeed[PORT_B] = deg * -speed
+    BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_C] = 0
