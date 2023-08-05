00d624c0943257c7293d1daa419ea47db7ffc8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:55 +0000

    Add package pretenders/1.1.1/pretenders-1.1.1.tar.gz

---
+++ packages/pretenders/pretenders-1.1.1.tar.gz/pretenders-1.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+from pretenders import __version__
+
+
+setup(
+    name='pretenders',
+    version=__version__,
+    description='Fake servers for testing',
+    long_description=open('README.rst').read(),
+    author='Carles Barrobés, Alex Couper',
+    author_email='carles@barrobes.com, amcouper@gmail.com',
+    url='https://github.com/pretenders/pretenders',
+    packages=find_packages(),
+    install_requires=['bottle', 'argparse'],
+    #include_package_data=True,
+    #package_data={
+    #    '': ['*.txt', '*.rst'],
+    #},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+    ],
+)
