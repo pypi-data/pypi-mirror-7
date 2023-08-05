f4fad2fc155c389193cfb4f394bd4788d1283d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:51 +0000

    Add package pgextrascli/0.1.0/pgextrascli-0.1.0.tar.gz

---
+++ packages/pgextrascli/pgextrascli-0.1.0.tar.gz/pgextrascli-0.1.0/tests/test_pgextrascli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pgextrascli
+----------------------------------
+
+Tests for `pgextrascli` module.
+"""
+
+import unittest
+
+
+class Testpgextrascli(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_some(self):
+        self.assertEqual(True, True)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
