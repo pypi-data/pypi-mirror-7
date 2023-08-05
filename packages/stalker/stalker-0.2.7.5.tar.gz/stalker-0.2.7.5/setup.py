a2f2dfa82e706b6a2178e75a61efcbe06c1a2736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:00 +0000

    Add package stalker/0.2.7.5/stalker-0.2.7.5.tar.gz

---
+++ packages/stalker/stalker-0.2.7.5.tar.gz/stalker-0.2.7.5/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README')).read()
+CHANGES = open(os.path.join(here, 'CHANGELOG')).read()
+
+requires = [
+    'sqlalchemy>=0.8',
+    'alembic',
+    'jinja2',
+    'unittest2',
+    'mocker',
+]
+
+setup(
+    name='stalker',
+    version='0.2.7.5',
+    description='A Production Asset Management (ProdAM) System',
+    long_description=README,
+    classifiers=[
+      "Programming Language :: Python",
+      "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+      "Operating System :: OS Independent",
+      "Development Status :: 5 - Production/Stable",
+      "Intended Audience :: Developers",
+      "Intended Audience :: End Users/Desktop",
+      "Topic :: Database",
+      "Topic :: Software Development",
+      "Topic :: Utilities",
+      "Topic :: Office/Business :: Scheduling",
+    ],
+    author='Erkan Ozgur Yilmaz',
+    author_email='eoyilmaz@gmail.com',
+    url='http://github.com/eoyilmaz/stalker',
+    keywords=['production', 'asset', 'management', 'vfx', 'animation',
+              'maya', 'houdini', 'nuke', 'fusion', 'softimage', 'blender',
+              'vue'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    test_suite='stalker',
+    install_requires=requires,
+)
+
