8db285d4a223ecdc2664d936090d04789701ec62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package asyncinflux/0.0.2/asyncinflux-0.0.2.tar.gz

---
+++ packages/asyncinflux/asyncinflux-0.0.2.tar.gz/asyncinflux-0.0.2/asyncinflux/__init__.py
@@ -0,0 +1,29 @@
+from codap import spawn, Queue
+
+class AsyncWriter:
+
+    def __init__(self, client):
+        """Wraps the client connection with an async call"""
+        self.q = Queue()
+        self.client = client
+
+    def _run(self):
+        """The actual code to write the points"""
+        try:
+            for data in self.q:
+                self.client.write_points(data)
+        except KeyboardInterrupt:
+            raise
+
+    def start(self):
+        """Backgrounds the writing of data points"""
+        self.background = spawn(self._run)
+
+    def stop(self):
+        """Stop the writting of data points"""
+        self.q.put_nowait(StopIteration)
+        self.background.kill()
+
+    def write_points(self, body):
+        """ Basically wraps the influxdb call """
+        self.q.put_nowait(body)
