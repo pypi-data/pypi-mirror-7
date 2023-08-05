047bb1fabdb60fd0d13395fb91bc07f3956c59ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:32 +0000

    Add package requests-toolbelt/0.3.0/requests-toolbelt-0.3.0.tar.gz

---
+++ packages/requests-toolbelt/requests-toolbelt-0.3.0.tar.gz/requests-toolbelt-0.3.0/AUTHORS.rst
@@ -0,0 +1,27 @@
+Requests-toolbelt is written and maintained by Ian Cordasco, Cory Benfield and
+various contributors:
+
+Development Lead
+````````````````
+
+- Ian Cordasco
+
+- Cory Benfield
+
+
+Requests
+````````
+
+- Kenneth Reitz <me@kennethreitz.com> and various contributors
+
+
+Urllib3
+```````
+
+- Andrey Petrov <andrey.petrov@shazow.net>
+
+
+Patches and Suggestions
+```````````````````````
+
+- Jay De Lanoy <jay@delanoy.co>
---
+++ packages/requests-toolbelt/requests-toolbelt-0.3.0.tar.gz/requests-toolbelt-0.3.0/requests_toolbelt/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+requests-toolbelt
+=================
+
+See http://toolbelt.rtfd.org/ for documentation
+
+:copyright: (c) 2014 by Ian Cordasco and Cory Benfield
+:license: Apache v2.0, see LICENSE for more details
+"""
+
+__title__ = 'requests-toolbelt'
+__authors__ = 'Ian Cordasco, Cory Benfield'
+__license__ = 'Apache v2.0'
+__copyright__ = 'Copyright 2014 Ian Cordasco, Cory Benfield'
+__version__ = '0.3.0'
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
+
+from .adapters import SSLAdapter, SourceAddressAdapter
+from .auth import GuessAuth
+from .multipart import (
+    MultipartEncoder, MultipartEncoderMonitor, MultipartDecoder,
+    ImproperBodyPartContentException, NonMultipartContentTypeException
+    )
+from .user_agent import user_agent
+
+__all__ = [
+    'GuessAuth', 'MultipartEncoder', 'MultipartEncoderMonitor',
+    'MultipartDecoder', 'SSLAdapter', 'SourceAddressAdapter', 'user_agent',
+    'ImproperBodyPartContentException', 'NonMultipartContentTypeException'
+]
