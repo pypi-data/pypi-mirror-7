c8a5a84f178d95e5a24a3814221a5784de162d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package infi.pkgmgr/0.1.7/infi.pkgmgr-0.1.7.tar.gz

---
+++ packages/infi.pkgmgr/infi.pkgmgr-0.1.7.tar.gz/infi.pkgmgr-0.1.7/src/infi/pkgmgr/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd9f50ea31343504a1087c37e48dcb60e4eae9caa'
+__git_head_subject__ = 'Finished release v0.1.7.'
+__git_head_message__ = '* release/v0.1.7:\n  checking for installed packages on redhat with rpm, not yum\n  buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_dirty_diff__ = ''
