f5c743ccbbd7eba6f3eb68b0f83a4f8ed1944b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:17 +0000

    Add package collective.recaptcha/1.1.5/collective.recaptcha-1.1.5.zip

---
+++ packages/collective.recaptcha/collective.recaptcha-1.1.5.zip/collective.recaptcha-1.1.5/collective/recaptcha/settings.py
@@ -0,0 +1,78 @@
+from persistent import Persistent
+from plone.registry.interfaces import IRegistry
+from zope.interface import Interface, implements
+from zope.component import adapts, getUtility
+from zope import schema
+from zope.annotation import factory, IAttributeAnnotatable
+from bbb import getSite
+
+try:
+    # Zope 2.12+
+    from five.formlib.formbase import EditForm
+except ImportError:
+    try:
+        # older Zope 2s
+        from Products.Five.formlib.formbase import EditForm
+    except ImportError:
+        # Zope 3
+        from zope.formlib.form import EditForm
+
+from zope.formlib.form import FormFields
+
+try:
+    from plone.formwidget.recaptcha.interfaces import IReCaptchaSettings
+    TRY_REGISTRY = True
+except ImportError:
+    TRY_REGISTRY = False
+
+from collective.recaptcha import RecaptchaMessageFactory as _
+
+
+class IRecaptchaSettings(Interface):
+
+    public_key = schema.TextLine(
+        title=_(u'Public Key')
+    )
+
+    private_key = schema.TextLine(
+        title=_(u'Private Key')
+    )
+
+
+class RecaptchaSettingsAnnotations(Persistent):
+    implements(IRecaptchaSettings)
+    adapts(IAttributeAnnotatable)
+
+    def __init__(self):
+        self.public_key = None
+        self.private_key = None
+
+RecaptchaSettings = factory(RecaptchaSettingsAnnotations)
+
+
+def getRecaptchaSettings():
+    registry = getUtility(IRegistry)
+    if TRY_REGISTRY:
+        # if plone.formwidget.recaptcha is installed, try getting
+        # its settings from the registry
+        try:
+            settings = registry.forInterface(IReCaptchaSettings)
+            if settings.public_key and settings.private_key:
+                return settings
+        except:
+            pass
+    # try getting settings from the registry first
+    try:
+        settings = registry.forInterface(IRecaptchaSettings)
+        if settings.public_key and settings.private_key:
+            return settings
+    except KeyError:
+        # fall back to our storage of an annotation on the site if the settings
+        # haven't been configured
+        site = getSite()
+        return IRecaptchaSettings(site)
+
+
+class RecaptchaSettingsForm(EditForm):
+    form_fields = FormFields(IRecaptchaSettings)
+    label = _(u"Recaptcha settings")
---
+++ packages/collective.recaptcha/collective.recaptcha-1.1.5.zip/collective.recaptcha-1.1.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.5'
+
+setup(name='collective.recaptcha',
+      version=version,
+      description="Wraps the recaptcha-client library to provide a drop-in replacement for collective.captcha.",
+      long_description=(open("README.rst").read() + "\n" +
+                        open(os.path.join("docs", "HISTORY.txt")).read()),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords='captcha recaptcha zope plone',
+      author='David Glick, Groundwire',
+      author_email='davidglick@groundwire.org',
+      url='http://plone.org/products/collective.recaptcha',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'recaptcha-client >= 1.0.6',
+          'Plone',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ]},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
