36af481d9730e89dbab0ba50922a40c49ab8e675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package seriesly/0.6.5/seriesly-0.6.5.tar.gz

---
+++ packages/seriesly/seriesly-0.6.5.tar.gz/seriesly-0.6.5/seriesly/decorators.py
@@ -0,0 +1,31 @@
+import time
+
+from decorator import decorator
+from requests.exceptions import ConnectionError
+
+from seriesly.exceptions import ConnectionError as _ConnectionError, BadRequest
+
+
+MAX_RETRY = 5
+RETRY_DELAY = 5
+
+
+@decorator
+def handle_error(method, self, *args, **kargs):
+    """Gracefully handle request errors"""
+    error = ''
+    for _ in range(MAX_RETRY):
+        try:
+            response = method(self, *args, **kargs)
+        except ConnectionError as e:
+            print e
+            print e.message
+            error = e.message
+            time.sleep(RETRY_DELAY)
+            continue
+        else:
+            if response.status_code >= 400:
+                raise BadRequest(response.text)
+            else:
+                return response
+    raise _ConnectionError(error)
---
+++ packages/seriesly/seriesly-0.6.5.tar.gz/seriesly-0.6.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="seriesly",
+    version="0.6.5",
+    description="Python client for seriesly database",
+    author="Pavel Paulau",
+    author_email="pavel.paulau@gmail.com",
+    install_requires=[
+        "requests==2.1.0",
+        "decorator",
+    ],
+    packages=[
+        "seriesly",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
