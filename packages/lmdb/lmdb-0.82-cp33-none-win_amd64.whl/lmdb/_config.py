7d883d163e82a0cbbda1fca7b92217bcd7dfeb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:19 +0000

    Add package lmdb/0.82/lmdb-0.82-cp33-none-win_amd64.whl

---
+++ packages/lmdb/lmdb-0.82-cp33-none-win_amd64.whl/lmdb/_config.py
@@ -0,0 +1,2 @@
+CONFIG = {'extra_compile_args': ['-UNDEBUG', '-w', '/FIPython.h'], 'libraries': ['Advapi32'], 'extra_include_dirs': ['lib', 'lib\\win32'], 'extra_library_dirs': [], 'extra_sources': ['lib/mdb.c', 'lib/midl.c']}
+
