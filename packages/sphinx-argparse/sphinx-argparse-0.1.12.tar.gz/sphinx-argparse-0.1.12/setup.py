9e0d361a1c5c8b23ed4be451ed9fd8dfc192adf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:11 +0000

    Add package sphinx-argparse/0.1.12/sphinx-argparse-0.1.12.tar.gz

---
+++ packages/sphinx-argparse/sphinx-argparse-0.1.12.tar.gz/sphinx-argparse-0.1.12/setup.py
@@ -0,0 +1,30 @@
+from distutils.version import StrictVersion
+import os
+from setuptools import setup
+# from tests import PyTest
+import sys
+
+deps = ["sphinx"]
+
+if sys.version_info[:2] == (2, 6):
+    deps.append('argparse')
+
+setup(
+    name='sphinx-argparse',
+    version='0.1.12',
+    packages=[
+        'sphinxarg',
+    ],
+
+    url='',
+    license='MIT',
+    author='Aleksandr Rudakov',
+    author_email='ribozz@gmail.com',
+    description='Sphinx extension that automatically document argparse commands and options',
+    long_description='',
+    install_requires=deps,
+
+    extras_require={
+        'dev': ['pytest', 'sphinx_rtd_theme', 'sphinx'],
+    }
+)
