5f10c5e851ac98a459c017fa3876fed97f430ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:19 +0000

    Add package cadquery/0.1.5/cadquery-0.1.5.zip

---
+++ packages/cadquery/cadquery-0.1.5.zip/cadquery-0.1.5/cadquery/__init__.py
@@ -0,0 +1,20 @@
+
+
+#these items point to the freecad implementation
+from .freecad_impl.geom import Plane,BoundBox,Vector,Matrix,sortWiresByBuildOrder
+from .freecad_impl.shapes import Shape,Vertex,Edge,Face,Wire,Solid,Shell,Compound
+from .freecad_impl import exporters
+
+#these items are the common implementation
+
+#the order of these matter
+from .selectors import NearestToPointSelector,ParallelDirSelector,DirectionSelector,PerpendicularDirSelector,TypeSelector,DirectionMinMaxSelector,StringSyntaxSelector,Selector
+from .CQ import CQ,CQContext,Workplane
+
+
+__all__ = [
+	'CQ','Workplane','plugins','selectors','Plane','BoundBox','Matrix','Vector','sortWiresByBuildOrder',
+	'Shape','Vertex','Edge','Wire','Solid','Shell','Compound','exporters',	'NearestToPointSelector','ParallelDirSelector','DirectionSelector','PerpendicularDirSelector','TypeSelector','DirectionMinMaxSelector','StringSyntaxSelector','Selector','plugins'
+]
+
+__version__ = 0.9
