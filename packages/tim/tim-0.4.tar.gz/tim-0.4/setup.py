041e69324249555852600ffa03d210d8dc4720f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:39 +0000

    Add package tim/0.4/tim-0.4.tar.gz

---
+++ packages/tim/tim-0.4.tar.gz/tim-0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='tim',
+      version='0.4',
+      description='TIM: Timed Iteration Monitor',
+      author='Olivier Le Thanh Duong',
+      long_description=open("README.md", "r").read(),
+      author_email='olivier@lethanh.be',
+      url='https://github.com/olethanh/tim',
+      install_requires=[],
+      license= 'WTFPL',
+      py_modules=["tim"],
+      keywords='',
+     )
