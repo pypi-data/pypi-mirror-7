ad7026fff8abcc3a8644a49e6754ddaf9c4d6893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package pyteomics/2.4.1/pyteomics-2.4.1.tar.gz

---
+++ packages/pyteomics/pyteomics-2.4.1.tar.gz/pyteomics-2.4.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+setup.py file for pyteomics
+'''
+
+from distutils.core import setup
+
+version = open('VERSION').readline().strip()
+
+setup(
+    name = 'pyteomics',
+    version = version,
+    description      = '''A framework for proteomics data analysis.''',
+    long_description = (''.join(open('README').readlines()) + '\n'
+                        + ''.join(open('INSTALL').readlines())),
+    author           = 'Anton Goloborodko & Lev Levitsky',
+    author_email     = 'pyteomics@googlegroups.com',
+    url              = 'http://hg.theorchromo.ru/pyteomics',
+    packages         = ['pyteomics', ],
+    requires         = [line.strip() for line in open('requirements.txt')],
+    classifiers      = ['Intended Audience :: Science/Research',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3',
+                        'Topic :: Education',
+                        'Topic :: Scientific/Engineering :: Bio-Informatics',
+                        'Topic :: Scientific/Engineering :: Chemistry',
+                        'Topic :: Scientific/Engineering :: Physics',
+                        'Topic :: Software Development :: Libraries'],
+    license          = 'License :: OSI Approved :: Apache Software License',
+    )
