503689f40bcfbd900b1d79ef42dc1d27dbac9f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package pyconsul/0.0.2/pyconsul-0.0.2.tar.gz

---
+++ packages/pyconsul/pyconsul-0.0.2.tar.gz/pyconsul-0.0.2/pyconsul/utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+'''
+  :copyright (c) 2014 Xavier Bruhiere.
+  :license: MIT, see LICENSE for more details.
+'''
+
+import base64
+import requests
+
+
+def decode_values(fct):
+    def inner(*args, **kwargs):
+        data = fct(*args, **kwargs)
+        if 'error' not in data:
+            for result in data:
+                result['Value'] = base64.b64decode(result['Value'])
+        return data
+    return inner
+
+
+def safe_request(fct):
+    def inner(*args, **kwargs):
+        try:
+            _data = fct(*args, **kwargs)
+        except requests.exceptions.ConnectionError as error:
+            return {'error': str(error), 'status': 404}
+
+        if _data.ok:
+            if _data.content:
+                safe_data = _data.json()
+            else:
+                safe_data = {'success': True}
+        else:
+            safe_data = {'error': _data.reason, 'status': _data.status_code}
+
+        return safe_data
+    return inner
