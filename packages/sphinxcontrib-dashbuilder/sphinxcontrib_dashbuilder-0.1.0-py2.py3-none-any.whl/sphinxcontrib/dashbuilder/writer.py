cf552b1b928416e8c9674ca6cb3253225d69e8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:08 +0000

    Add package sphinxcontrib-dashbuilder/0.1.0/sphinxcontrib_dashbuilder-0.1.0-py2.py3-none-any.whl

---
+++ packages/sphinxcontrib-dashbuilder/sphinxcontrib_dashbuilder-0.1.0-py2.py3-none-any.whl/sphinxcontrib/dashbuilder/writer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    sphinxcontrib-dashbuilder
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Sphinx Dash builder.
+
+    :copyright: Copyright 2014 by shimizukawa@gmail.com.
+    :license: BSD, see LICENSE for details.
+"""
+from sphinx.writers.html import HTMLTranslator
+
+
+class DashTranslator(HTMLTranslator):
+
+    pass
