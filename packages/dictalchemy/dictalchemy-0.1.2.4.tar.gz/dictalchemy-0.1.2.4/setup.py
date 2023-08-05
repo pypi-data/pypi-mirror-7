38c5ea5ee6984777fc90cdd2829b3713c1f82a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:14 +0000

    Add package dictalchemy/0.1.2.4/dictalchemy-0.1.2.4.tar.gz

---
+++ packages/dictalchemy/dictalchemy-0.1.2.4.tar.gz/dictalchemy-0.1.2.4/setup.py
@@ -0,0 +1,58 @@
+"""
+~~~~~~~~~~~
+Dictalchemy
+~~~~~~~~~~~
+
+Contains asdict() and fromdict() methods that will work on SQLAlchemy
+declarative models.
+
+Read more in the source or on github
+<https://github.com/danielholmstrom/dictalchemy>.
+"""
+
+import os
+import sys
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+# Requirements for the package
+install_requires = [
+    'SQLAlchemy>=0.9.4',
+]
+
+# Requirement for running tests
+test_requires = install_requires
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name='dictalchemy',
+      version='0.1.2.4',
+      description="Contains asdict and fromdict methods for SQL-Alchemy "
+      "declarative models",
+      long_description=README,
+      url='http://github.com/danielholmstrom/dictalchemy/',
+      license='MIT',
+      author='Daniel Holmstrom',
+      author_email='holmstrom.daniel@gmail.com',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: MIT License',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Software Development :: '
+                   'Libraries :: Python Modules'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=test_requires,
+      test_suite='dictalchemy',
+      **extra
+      )
