d6a9012212a19da072f38f2adb80e1126f251acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package PyLocache/0.0.3/PyLocache-0.0.3.tar.gz

---
+++ packages/PyLocache/PyLocache-0.0.3.tar.gz/PyLocache-0.0.3/CHANGES.rst
@@ -0,0 +1,9 @@
+0.0.3
+----------------
+
+* Fixed installation bug.
+
+0.0.2
+----------------
+
+* Added delete method.
---
+++ packages/PyLocache/PyLocache-0.0.3.tar.gz/PyLocache-0.0.3/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(*path):
+    base_dir = os.path.dirname(__file__)
+    file_path = (base_dir, ) + tuple(path)
+    return open(os.path.join(*file_path)).read()
+
+
+setup(
+    name="PyLocache",
+    url="https://github.com/psjay/PyLocache",
+    version="0.0.3",
+    license='WTFPL',
+    description="PyLocache is a Python implementation of LRU local cache.",
+    long_description=(
+        read_file("README.rst") + "\n\n" +
+        "Change History\n" +
+        "==============\n\n" +
+        read_file("CHANGES.rst")),
+    author='psjay',
+    author_email="psjay.peng@gmail.com",
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=['Nose'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
