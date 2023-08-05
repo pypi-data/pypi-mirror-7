81ae580e4487be019c37aac2dbf8b18e63ab777d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package sftdyn/0.0/sftdyn-0.0.tar.gz

---
+++ packages/sftdyn/sftdyn-0.0.tar.gz/sftdyn-0.0/sftdyn/args.py
@@ -0,0 +1,34 @@
+import argparse
+
+p = argparse.ArgumentParser(description="SFT dynamic DNS updater HTTPS server")
+p.add_argument("-v", "--verbose", action='count', default=0)
+p.add_argument("-c", "--cert", type=str,
+			   help="X.509 cert file")
+p.add_argument("-k", "--key", type=str,
+			   help="X.509 key file")
+p.add_argument("client", type=str, nargs='*',
+			   help="clientkey:hostname tuples")
+p.add_argument("-p", "--port", type=int, default=4443)
+p.add_argument("-l", "--listen", type=str, default="0.0.0.0")
+p.add_argument("--conf", type=str,
+			   help="conf file, will be exec'd as python3")
+p.add_argument("--nsupdatecommand", type=str,
+			   default="update delete $HOST A\n" +
+					   "update add $HOST 30 A $IP\n" +
+					   "send\n",
+			   help="template command list for nsupdate")
+
+
+def parse():
+	args = p.parse_args()
+
+	args.clients = [c.split(':', maxsplit=1) for c in args.client]
+	args.clients = {ckey: chost for ckey, chost in args.clients}
+	del args.client
+
+	if args.conf:
+		exec(open(args.conf).read(), args.__dict__)
+	if not args.cert or not args.key:
+		p.error("You must provide key and cert files")
+
+	return args
---
+++ packages/sftdyn/sftdyn-0.0.tar.gz/sftdyn-0.0/sftdyn/server.py
@@ -0,0 +1,55 @@
+import sftdyn.args as args
+import http
+import ssl
+from http.server import BaseHTTPRequestHandler
+from subprocess import Popen, PIPE
+
+currentips = {}
+
+
+def handle_request(key, ip):
+	if not key:
+		return ip, 200
+
+	try:
+		host = args.clients[key]
+	except:
+		return "BADKEY", 403
+
+	if currentips.get(host, None) == ip:
+		return "UPTODATE", 200
+
+	print("updating " + host + " to " + ip)
+
+	p = Popen(['nsupdate', '-l'], stdin=PIPE)
+	cmd = args.nsupdatecommand.replace('$HOST', host).replace('$IP', ip)
+	p.communicate(input=cmd.encode('utf-8'))
+
+	if p.returncode is 0:
+		currentips[host] = ip
+		return "OK", 200
+	else:
+		return "FAIL", 500
+
+
+class GetHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		path = self.path.lstrip('/')
+		text, code = handle_request(path, self.client_address[0])
+		self.send_response(code)
+		self.end_headers()
+		self.wfile.write(text.encode('utf-8'))
+
+
+def serve():
+	addr = (args.listen, args.port)
+	httpd = http.server.HTTPServer(addr, GetHandler)
+	httpd.socket = ssl.wrap_socket(
+		httpd.socket,
+		server_side=True,
+		keyfile=args.key,
+		certfile=args.cert,
+		ssl_version=ssl.PROTOCOL_TLSv1)
+
+	print("listening on " + args.listen + ":" + str(args.port))
+	httpd.serve_forever()
