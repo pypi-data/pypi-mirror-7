1c5436d622370b3a266419d9c0673e225da445fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package molp/0.5/molp-0.5.tar.gz

---
+++ packages/molp/molp-0.5.tar.gz/molp-0.5/molp/admin.py
@@ -0,0 +1,14 @@
+#encoding=utf-8
+
+from django.contrib import admin
+from molp.models import Parameter
+
+
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'version', 'channel', 'effect_time')
+    list_filter = ('app', 'channel', 'version')
+
+    search_fields = ('name', 'value')
+
+
+admin.site.register(Parameter, ParameterAdmin)
