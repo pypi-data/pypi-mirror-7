9dafbaed8d2bd3c040673b6d31b41a38456efce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:11 +0000

    Add package cache_flusher/1.0.2/cache_flusher-1.0.2.tar.gz

---
+++ packages/cache_flusher/cache_flusher-1.0.2.tar.gz/cache_flusher-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='cache_flusher',
+      version='1.0.2',
+      author='Roberto Agostino Vitillo',
+      author_email='ra.vitillo@gmail.com',
+      description='Disk cache flusher',
+      url='https://github.com/vitillo/python_cache_flusher',
+      packages=['cache_flusher'],
+      package_dir={'cache_flusher': 'cache_flusher'},
+      package_data={'cache_flusher': ['data/Flush.exe']})
