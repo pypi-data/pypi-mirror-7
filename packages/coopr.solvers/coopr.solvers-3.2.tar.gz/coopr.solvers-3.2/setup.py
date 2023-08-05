b758f2053038ebe6361635fb00902a6bfe5911dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:09 +0000

    Add package coopr.solvers/3.2/coopr.solvers-3.2.tar.gz

---
+++ packages/coopr.solvers/coopr.solvers-3.2.tar.gz/coopr.solvers-3.2/setup.py
@@ -0,0 +1,70 @@
+#  _________________________________________________________________________
+#
+#  Coopr: A COmmon Optimization Python Repository
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  _________________________________________________________________________
+
+"""
+Script to generate the installer for coopr.solvers.
+"""
+
+import glob
+import os
+
+def _find_packages(path):
+    """
+    Generate a list of nested packages
+    """
+    pkg_list=[]
+    if not os.path.exists(path):
+        return []
+    if not os.path.exists(path+os.sep+"__init__.py"):
+        return []
+    else:
+        pkg_list.append(path)
+    for root, dirs, files in os.walk(path, topdown=True):
+        if root in pkg_list and "__init__.py" in files:
+            for name in dirs:
+                if os.path.exists(root+os.sep+name+os.sep+"__init__.py"):
+                    pkg_list.append(root+os.sep+name)
+    return [pkg for pkg in map(lambda x:x.replace(os.sep,"."), pkg_list)]
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+from setuptools import setup
+packages = _find_packages('coopr')
+
+scripts = glob.glob("scripts/*")
+
+setup(name='coopr.solvers',
+      version='3.2',
+      maintainer='William E. Hart',
+      maintainer_email='wehart@sandia.gov',
+      url = 'https://software.sandia.gov/svn/public/coopr/coopr.solvers',
+      license = 'BSD',
+      platforms = ["any"],
+      description = 'Plugins that are typically bundled with Coopr',
+      long_description = read('README.txt'),
+      classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Programming Language :: Unix Shell',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Scientific/Engineering'
+        ],
+      packages=packages,
+      keywords=['optimization'],
+      scripts=scripts,
+      namespace_packages=['coopr']
+      )
