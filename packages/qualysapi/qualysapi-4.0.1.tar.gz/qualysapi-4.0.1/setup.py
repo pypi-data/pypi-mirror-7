fecfffb6be08f160729e107fa85da0b1c0a32c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:33 +0000

    Add package qualysapi/4.0.1/qualysapi-4.0.1.tar.gz

---
+++ packages/qualysapi/qualysapi-4.0.1.tar.gz/qualysapi-4.0.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__author__ = 'Parag Baxi <parag.baxi@gmail.com>'
+__copyright__ = 'Copyright 2011-2013, Parag Baxi'
+__license__ = 'BSD-new'
+# Make pyflakes happy.
+__pkgname__ = None
+__version__ = None
+execfile('qualysapi/version.py')
+
+# A utility function to read the README file into the long_description field.
+def read(fname):
+    """ Takes a filename and returns the contents of said file relative to
+    the current directory.
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name=__pkgname__,
+      version=__version__,
+      author='Parag Baxi',
+      author_email='parag.baxi@gmail.com',
+      description='QualysGuard(R) Qualys API Package',
+      license ='BSD-new',
+      keywords ='Qualys QualysGuard API helper network security',
+      url='https://github.com/paragbaxi/qualysapi',
+      package_dir={'': '.'},
+      packages=['qualysapi',],
+      # package_data={'qualysapi':['LICENSE']},
+      # scripts=['src/scripts/qhostinfo.py', 'src/scripts/qscanhist.py', 'src/scripts/qreports.py'],
+      long_description=read('README.md'),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Topic :: Utilities',
+          'License :: OSI Approved :: Apache Software License',
+          'Intended Audience :: Developers',
+      ],
+      install_requires=[
+          'lxml',
+          'requests',
+      ],
+     )
