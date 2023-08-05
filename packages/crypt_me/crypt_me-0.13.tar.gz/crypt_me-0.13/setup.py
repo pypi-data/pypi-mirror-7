e0226c973567439009fff0825ed2ea728e40b5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:21 +0000

    Add package crypt_me/0.13/crypt_me-0.13.tar.gz

---
+++ packages/crypt_me/crypt_me-0.13.tar.gz/crypt_me-0.13/setup.py
@@ -0,0 +1,18 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(name="crypt_me",
+      version="0.13",
+      description="Caesar cipher with qt GUI",
+      py_modules=['crypt_me','ez_setup'],
+      long_description="""\
+      Caesar cipher with qt GUI
+""",
+      author="Martin Bednar",
+      author_email="martin.bednar@hotmail.sk",
+      license="GNU GPL v3 license",
+      keywords="Caesar",
+      #packages=find_packages(exclude='tests'),
+      #package_data={'mypackage': ['data/*.xml']},
+      )
