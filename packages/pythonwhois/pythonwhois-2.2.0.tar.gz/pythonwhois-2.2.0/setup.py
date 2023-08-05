4a208f5794067a0dcff0f0b2fa794895a6ef35d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:16 +0000

    Add package pythonwhois/2.2.0/pythonwhois-2.2.0.tar.gz

---
+++ packages/pythonwhois/pythonwhois-2.2.0.tar.gz/pythonwhois-2.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pythonwhois',
+      version='2.2.0',
+      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
+      author='Sven Slootweg',
+      author_email='pythonwhois@cryto.net',
+      url='http://cryto.net/pythonwhois',
+      packages=['pythonwhois'],
+      install_requires=['argparse'],
+      provides=['pythonwhois'],
+      scripts=["pwhois"],
+      license="WTFPL"
+     )
