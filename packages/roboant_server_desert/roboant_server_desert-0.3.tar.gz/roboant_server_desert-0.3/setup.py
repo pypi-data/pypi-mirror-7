435774b335e71948369275d5f5398f15e6206202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package roboant_server_desert/0.3/roboant_server_desert-0.3.tar.gz

---
+++ packages/roboant_server_desert/roboant_server_desert-0.3.tar.gz/roboant_server_desert-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='roboant_server_desert',
+      version='0.3',
+      description='Server for the RoboAnt project',
+      url='http://github.com/d3kod/roboant',
+      author='Aleksandar Kodzhabashev',
+      author_email='akodzhabashev@gmail.com',
+      license='MIT',
+      packages=['roboant_server_desert'],
+      scripts=['bin/roboant_server_desert'],
+      install_requires=[
+          'pygame',
+          'twisted',
+          'pygame'],
+      zip_safe=False)
