6aa78ea19cc611b2544f1a36ebdee3ba34ce02ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package ice_pick/0.2/ice_pick-0.2.tar.gz

---
+++ packages/ice_pick/ice_pick-0.2.tar.gz/ice_pick-0.2/ice_pick/__init__.py
@@ -0,0 +1,83 @@
+"""
+Ice Pick
+~~~~~~~~~~~~~~~~~~~~
+
+Ice Pick is a library that allows easy access to AWS billing data collected by
+the Netflix OSS Ice tool.
+
+Basic usage:
+
+>>> from ice_pick.api import APIRequest
+
+>>> api_request = APIRequest('http://example.com/ice/')
+>>> api_request.get_data()
+{
+    'data': {
+        'AWS Data Pipeline': [0.25148882999999966],
+        'Alexa Web Information Service': [0.11130000000000001],
+        'aggregated': [48884.90918908445],
+        'cloudfront': [3241.575401989994],
+        .
+        .
+        .
+        'vpc': [296.7824844800026]
+    },
+    'groupBy': 'Product',
+    'hours': [588],
+    'start': 1391212800000,
+    'stats': {
+        'AWS Data Pipeline': {
+            'average': 0.25148882999999966,
+            'max': 0.25148882999999966,
+            'total': 0.25148882999999966
+        },
+        'Alexa Web Information Service': {
+            'average': 0.11130000000000001,
+            'max': 0.11130000000000001,
+            'total': 0.11130000000000001
+        },
+        'aggregated': {
+            'average': 48884.90918908445,
+            'max': 48884.90918908445,
+            'total': 48884.90918908445
+        },
+        'cloudfront': {
+            'average': 3241.575401989994,
+            'max': 3241.575401989994,
+            'total': 3241.575401989994
+        },
+        .
+        .
+        .
+        'vpc': {
+            'average': 296.7824844800026,
+            'max': 296.7824844800026,
+            'total': 296.7824844800026
+            }
+        },
+    'status': 200,
+    'time': [1391212800000]
+}
+
+Copyright 2014 Demand Media.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+
+
+__title__ = 'ice_pick'
+__version__ = '0.2'
+__author__ = 'Demand Media'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2014 Demand Media'
---
+++ packages/ice_pick/ice_pick-0.2.tar.gz/ice_pick-0.2/ice_pick/filters/regions.py
@@ -0,0 +1,31 @@
+"""
+ice_pick.filters.regions
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains a set of constants that can be used as region
+filters. All the constants are based on AWS definitions.
+
+Copyright 2014 Demand Media.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+
+AP_NORTHEAST_1 = 'ap-northeast-1'  # Asia Pacific (Tokyo) Region
+AP_SOUTHEAST_1 = 'ap-southeast-1'  # Asia Pacific (Singapore) Region
+AP_SOUTHEAST_2 = 'ap-southeast-2'  # Asia Pacific (Sydney) Region
+EU_WEST_1 = 'eu-west-1'  # EU (Ireland) Region
+SA_EAST_1 = 'sa-east-1'  # South America (Sao Paulo) Region
+US_EAST_1 = 'us-east-1'  # US East (Northern Virginia) Region
+US_WEST_1 = 'us-west-1'  # US West (Northern California) Region
+US_WEST_2 = 'us-west-2'  # US West (Oregon) Region
