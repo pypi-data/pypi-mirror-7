01b65fb41637348604c47277f6eeb4e35e70e6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:19 +0000

    Add package django-nonrel-enuff/0.4/django-nonrel-enuff-0.4.tar.gz

---
+++ packages/django-nonrel-enuff/django-nonrel-enuff-0.4.tar.gz/django-nonrel-enuff-0.4/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-nonrel-enuff',
+    version="0.4",
+    author='Steve Yeago',
+    author_email='yeago999@gmail.com',
+    description='Crutch for SQL users',
+    url='http://github.com/yeago/django-nonrel-enuff',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
