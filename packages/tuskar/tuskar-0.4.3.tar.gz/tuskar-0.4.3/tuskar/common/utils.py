01343aa52cc80f758f84600be70f3e3c8d783308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:13 +0000

    Add package tuskar/0.4.3/tuskar-0.4.3.tar.gz

---
+++ packages/tuskar/tuskar-0.4.3.tar.gz/tuskar-0.4.3/tuskar/common/utils.py
@@ -0,0 +1,114 @@
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2011 Justin Santa Barbara
+# Copyright (c) 2012 NTT DOCOMO, INC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Utilities and helper functions."""
+
+import os
+
+from oslo.config import cfg
+
+from tuskar.common import exception
+from tuskar.openstack.common import log as logging
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+class LazyPluggable(object):
+    """A pluggable backend loaded lazily based on some value."""
+
+    def __init__(self, pivot, config_group=None, **backends):
+        self.__backends = backends
+        self.__pivot = pivot
+        self.__backend = None
+        self.__config_group = config_group
+
+    def __get_backend(self):
+        if not self.__backend:
+            if self.__config_group is None:
+                backend_name = CONF[self.__pivot]
+            else:
+                backend_name = CONF[self.__config_group][self.__pivot]
+            if backend_name not in self.__backends:
+                msg = _('Invalid backend: %s') % backend_name
+                raise exception.TuskarException(msg)
+
+            backend = self.__backends[backend_name]
+            if isinstance(backend, tuple):
+                name = backend[0]
+                fromlist = backend[1]
+            else:
+                name = backend
+                fromlist = backend
+
+            self.__backend = __import__(name, None, None, fromlist)
+        return self.__backend
+
+    def __getattr__(self, key):
+        backend = self.__get_backend()
+        return getattr(backend, key)
+
+
+def is_int_like(val):
+    """Check if a value looks like an int."""
+    try:
+        return str(int(val)) == str(val)
+    except Exception:
+        return False
+
+
+def read_cached_file(filename, cache_info, reload_func=None):
+    """Read from a file if it has been modified.
+
+    :param cache_info: dictionary to hold opaque cache.
+    :param reload_func: optional function to be called with data when
+                        file is reloaded due to a modification.
+
+    :returns: data from file
+
+    """
+    mtime = os.path.getmtime(filename)
+    if not cache_info or mtime != cache_info.get('mtime'):
+        LOG.debug("Reloading cached file %s" % filename)
+        with open(filename) as fap:
+            cache_info['data'] = fap.read()
+        cache_info['mtime'] = mtime
+        if reload_func:
+            reload_func(cache_info['data'])
+    return cache_info['data']
+
+
+def diff_items(old, new, key=None):
+    """Calculates a difference between two collections of items.
+
+    Returns a tuple of two lists, the first contains all the items from
+    the ``new`` collection that are not in the ``old`` collection, the
+    second all the items from the ``old`` collection that are not in the
+    ``new`` collection. The order of items is preserved.
+
+    If an optional ``key`` function is specified, it is used to compare
+    the items, instead of comparing them directly. That function may be
+    called multiple times, so it should have no side effects.
+    """
+    if key is None:
+        key = lambda item: item
+    old_keys = set(key(item) for item in old)
+    new_keys = set(key(item) for item in new)
+    added = [item for item in new if key(item) not in old_keys]
+    removed = [item for item in old if key(item) not in new_keys]
+    return added, removed
