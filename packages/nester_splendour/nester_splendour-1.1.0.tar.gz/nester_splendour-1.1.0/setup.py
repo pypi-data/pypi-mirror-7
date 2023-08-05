72a549122e5b03927094fc0cf91b84d432f04a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package nester_splendour/1.1.0/nester_splendour-1.1.0.tar.gz

---
+++ packages/nester_splendour/nester_splendour-1.1.0.tar.gz/nester_splendour-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name            = 'nester_splendour',
+    version         = '1.1.0',
+    py_modules      = ['nester_splendour'],
+    author          = 'Splendour',
+    author_email    = 'splendourhui@foxmail.com',
+    url             = 'http://splendourhui.com',
+    description     = 'A simple printer of a list',
+       )
