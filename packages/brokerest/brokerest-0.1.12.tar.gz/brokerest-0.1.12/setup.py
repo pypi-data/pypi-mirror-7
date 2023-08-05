58d082142be45bb1f96e7adcb30f5ab9412be18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package brokerest/0.1.12/brokerest-0.1.12.tar.gz

---
+++ packages/brokerest/brokerest-0.1.12.tar.gz/brokerest-0.1.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='brokerest',
+      version='0.1.12',
+      description='Python REST API client',
+      url='https://github.com/satrails/brokerest',
+      author='Jonathon Morgan, Wiktor Wojcikowski',
+      author_email='jonathon@satrails.com',
+      license='MIT',
+      packages=['brokerest', 'brokerest/resources', ],
+      install_requires=[
+        "requests >= 1.2.3",
+      ],
+      zip_safe=False)
