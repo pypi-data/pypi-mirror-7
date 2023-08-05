4452fcb6ac988469c7edade255aca13899ae4619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:01 +0000

    Add package simprint/1.0.0/simprint-1.0.0.tar.gz

---
+++ packages/simprint/simprint-1.0.0.tar.gz/simprint-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name	='simprint',
+		version	='1.0.0',
+		py_modules	=['simprint'],
+		author	='ligeek49',
+		author_email	='ligeek49@gmail.com',
+		url	='http://google.kr',
+		description	='A simple printer'
+	)
---
+++ packages/simprint/simprint-1.0.0.tar.gz/simprint-1.0.0/simprint.py
@@ -0,0 +1,3 @@
+def print_sim(bar):
+	print(bar)
+
