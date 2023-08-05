b2754a3aae835a484527420d019c6538c1459369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package pymarc/3.0.0/pymarc-3.0.0.tar.gz

---
+++ packages/pymarc/pymarc-3.0.0.tar.gz/pymarc-3.0.0/setup.py
@@ -0,0 +1,40 @@
+import pymarc
+version = '3.0.0'
+
+from setuptools import setup
+
+install_requires = ['six']
+try:
+    import xml.etree
+except ImportError:
+    install_requires.append('elementtree>=1.2.6')
+
+import sys
+if sys.version_info < (2 , 6):
+    install_requires.append('simplejson>=1.7.3')
+del sys
+
+classifiers = """
+Intended Audience :: Education
+Intended Audience :: Developers
+Intended Audience :: Information Technology
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Topic :: Text Processing :: General
+"""
+
+import pymarc
+
+setup( 
+    name = 'pymarc',
+    version = version,
+    url = 'http://github.com/edsu/pymarc',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    license = 'http://www.opensource.org/licenses/bsd-license.php',
+    py_modules = ['pymarc'],
+    install_requires = install_requires,
+    description = 'read, write and modify MARC bibliographic data',
+    classifiers = filter(None, classifiers.split('\n')),
+    test_suite = 'test',
+)
