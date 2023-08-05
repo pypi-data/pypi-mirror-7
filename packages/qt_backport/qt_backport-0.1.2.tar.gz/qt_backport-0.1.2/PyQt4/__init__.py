70b90192596271cf9672a8333c0dbdf2d161d4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package qt_backport/0.1.2/qt_backport-0.1.2.tar.gz

---
+++ packages/qt_backport/qt_backport-0.1.2.tar.gz/qt_backport-0.1.2/PyQt4/__init__.py
@@ -0,0 +1,10 @@
+import qt_backport as _qt_backport
+if _qt_backport.CURRENT_EMULATOR is not None:
+    raise ImportError("qt_backport emulator already running: '%s'" % \
+                      _qt_backport.CURRENT_EMULATOR)
+
+emulator = __name__
+wrapper = _qt_backport.PYQT5
+_qt_backport.emulate(emulator, wrapper)
+
+del emulator, wrapper
