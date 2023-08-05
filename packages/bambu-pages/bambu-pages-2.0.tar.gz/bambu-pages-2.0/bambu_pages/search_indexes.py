eda967c36ba142e9e19dfd870feea0e8fb7342d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:32 +0000

    Add package bambu-pages/2.0/bambu-pages-2.0.tar.gz

---
+++ packages/bambu-pages/bambu-pages-2.0.tar.gz/bambu-pages-2.0/bambu_pages/search_indexes.py
@@ -0,0 +1,9 @@
+from haystack import indexes
+from bambu_pages.models import Page
+from datetime import datetime
+
+class PageIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document = True, use_template = True)
+    
+    def get_model(self):
+        return Page
