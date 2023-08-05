5b5d3db8a987ff435f199e28b2ec89f0fe8c516a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:02 +0000

    Add package pyolite/1.0.3/pyolite-1.0.3.tar.gz

---
+++ packages/pyolite/pyolite-1.0.3.tar.gz/pyolite-1.0.3/examples/users.py
@@ -0,0 +1,25 @@
+from pyolite import Pyolite
+
+# initial olite object
+admin_repository = '/home/absolute/path/to/gitolite/repo/'
+olite = Pyolite(admin_repository=admin_repository)
+
+# create user object
+vlad = olite.users.create(name='bob',
+                          key_path='~/.ssh/second_rsa.pub')
+
+# get user by name
+vlad = olite.users.get(name='admin')
+
+# get_or_create django style
+vlad = olite.users.get_or_create('alice')
+
+# add new key to user
+vlad.keys.append('/path/to/key')
+vlad.keys.append('just put the key here')
+
+# check if user is admin or not
+print vlad.is_admin
+
+# TODO:
+# vlad.repos['oxygen'].permissions = 'RW+'
