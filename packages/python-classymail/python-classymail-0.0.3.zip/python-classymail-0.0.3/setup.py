f73a4e52a9fceb59dce9c56e8541d0eac85be1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package python-classymail/0.0.3/python-classymail-0.0.3.zip

---
+++ packages/python-classymail/python-classymail-0.0.3.zip/python-classymail-0.0.3/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+__author__ = 'Dan Ostrowski <dan.ostrowski@gmail.com>'
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='python-classymail',
+    version='0.0.3',
+    description='Quickly create email messages using an inheritance API.',
+    long_description=(read('README.rst') + '\n\n' +
+                      read('HISTORY.rst') + '\n\n' +
+                      read('AUTHORS.rst')),
+    url='https://bitbucket.org/danostrowski/python-classymail',
+    license='MIT',
+    author='Dan Ostrowski',
+    author_email='dan.ostrowski@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
