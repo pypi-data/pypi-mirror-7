b4a92a01f7e6808b6b3b8d810466a46603789d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:58 +0000

    Add package django-render-csv/0.1.1/django-render-csv-0.1.1.tar.gz

---
+++ packages/django-render-csv/django-render-csv-0.1.1.tar.gz/django-render-csv-0.1.1/django_render_csv.py
@@ -0,0 +1,23 @@
+import csv
+from django.http import HttpResponse
+
+def render_csv(data, filename="data.csv"):
+	res = HttpResponse(content_type='text/csv')
+	res['Content-Disposition'] = u'attachment; filename="%s"' % filename
+
+	writer = csv.writer(res)
+	for item in data:
+		writer.writerow(item)
+
+	return res
+
+def as_csv(fn=None, filename=None):
+	def decorator(fn):
+		def _fn(request, *args, **kwargs):
+			data = fn(request, *args, **kwargs)
+			return render_csv(data, filename=filename)
+
+		return _fn
+
+	return decorator(fn) if fn else decorator
+
