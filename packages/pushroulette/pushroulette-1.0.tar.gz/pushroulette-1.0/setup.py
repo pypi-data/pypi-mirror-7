9ee6efdb1c901c239b63e508bc2d07b42db6b5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package pushroulette/1.0/pushroulette-1.0.tar.gz

---
+++ packages/pushroulette/pushroulette-1.0.tar.gz/pushroulette-1.0/setup.py
@@ -0,0 +1,12 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup
+setup(name='pushroulette',
+      version='1.0',
+      description='Plays a random 5 second clip from soundcloud for every git push',
+      author='Matt Carrier',
+      author_email='mcarrieruri@gmail.com',
+      url='https://github.com/Astonish-Results/push-roulette',
+      py_modules=['pushroulette'],
+      install_requires=['soundcloud==0.4.1', 'pydub==0.9.2']
+      )
