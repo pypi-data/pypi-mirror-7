919f61987297fd15ae79aaff15607a44281b0d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:12 +0000

    Add package dict2xml/1.2/dict2xml-1.2.tar.gz

---
+++ packages/dict2xml/dict2xml-1.2.tar.gz/dict2xml-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# Setup the project
+setup(
+      name = "dict2xml"
+    , version = '1.2'
+    , packages = ['dict2xml']
+
+    , extras_require =
+      { 'tests' :
+        [ 'fudge'
+        , 'noseOfYeti>=1.5.0'
+        , 'nose'
+        ]
+      }
+
+    , install_requires =
+      [ "six"
+      ]
+
+    # metadata
+    , author = "Stephen Moore"
+    , author_email = "stephen@delfick.com"
+    , description = "small script to output xml as a string from a python dictionary"
+    , license = "WTFPL"
+    )
+
