77afe95d90ae6e2837c4b31fa56ee36cfcf282c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package django-projectname/1.0.0/django-projectname-1.0.0.tar.gz

---
+++ packages/django-projectname/django-projectname-1.0.0.tar.gz/django-projectname-1.0.0/django_projectname/context_processors.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+"""
+project name valiable to settings.py
+"""
+from django.conf import settings
+
+DEFAULT_PROJECT_NAME = "Django Project(Please set settings.PROJECT_NAME)"
+
+
+def project_name(request, *args, **kwargs):
+    """
+    Set project_name to context
+    """
+    project_name = DEFAULT_PROJECT_NAME
+
+
+    return {
+        'project_name': getattr(settings, "PROJECT_NAME", DEFAULT_PROJECT_NAME)
+    }
