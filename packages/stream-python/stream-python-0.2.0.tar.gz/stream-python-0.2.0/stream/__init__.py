52f553cec7fb7eabe051b281ad08722bc9231047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:31 +0000

    Add package stream-python/0.2.0/stream-python-0.2.0.tar.gz

---
+++ packages/stream-python/stream-python-0.2.0.tar.gz/stream-python-0.2.0/stream/__init__.py
@@ -0,0 +1,28 @@
+
+__author__ = 'Thierry Schellenbach'
+__copyright__ = 'Copyright 2012, Thierry Schellenbach'
+__credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
+__license__ = 'BSD'
+__version__ = '0.2.0'
+__maintainer__ = 'Thierry Schellenbach'
+__email__ = 'thierryschellenbach@gmail.com'
+__status__ = 'Production'
+
+
+def connect(api_key, api_secret):
+    '''
+    Returns a Client object
+
+    :param api_key: your api key
+    :param api_secret: the api secret
+    '''
+    from stream.client import StreamClient
+    return StreamClient(api_key, api_secret)
+
+
+'''
+TODO:
+- test coverage
+- read the docs
+- Update getstream.io/docs page
+'''
