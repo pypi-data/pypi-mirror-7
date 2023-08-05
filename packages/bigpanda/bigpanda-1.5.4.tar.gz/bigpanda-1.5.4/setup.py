a5136fe7a30d2c75b50d13470f590cf1d6dbdfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:56 +0000

    Add package bigpanda/1.5.4/bigpanda-1.5.4.tar.gz

---
+++ packages/bigpanda/bigpanda-1.5.4.tar.gz/bigpanda-1.5.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+requires = open('requirements.txt').readlines()
+setup(name='bigpanda',
+      version='1.5.4',
+      description='Python module for integration with BigPanda',
+      author='BigPanda',
+      author_email='support@bigpanda.io',
+      url='bigpanda.io',
+      packages=['bigpanda'],
+      install_requires=requires
+)
