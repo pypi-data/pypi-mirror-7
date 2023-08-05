0855bd8b6dd05d35d91bd81a33bcbe9506fff406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:08 +0000

    Add package django-su/0.4.4/django-su-0.4.4.tar.gz

---
+++ packages/django-su/django-su-0.4.4.tar.gz/django-su-0.4.4/setup.py
@@ -0,0 +1,32 @@
+import os.path
+import sys
+from setuptools import setup, find_packages
+
+from django_su import __version__
+
+setup(
+    name='django-su',
+    version=__version__,
+    description="Login as any user from the Django admin interface, then switch back when done",
+    long_description=open('README.rst').read() if os.path.exists("README.rst") else "",
+    author="Adam Charnock",
+    author_email="adam@adamcharnock.com",
+    url="https://github.com/adamcharnock/django-su",
+    license="MIT",
+
+    install_requires=["django"],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Security",
+        "Framework :: Django",
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
