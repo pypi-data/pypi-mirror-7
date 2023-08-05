b05cb5c48bca69ced08d4d92a004cf6cc7ae88b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package datasift-beta/2.0.6/datasift-beta-2.0.6.tar.gz

---
+++ packages/datasift-beta/datasift-beta-2.0.6.tar.gz/datasift-beta-2.0.6/datasift/live_stream.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
+from twisted.internet.protocol import ReconnectingClientFactory
+
+
+class LiveStream(WebSocketClientProtocol):  # pragma: no cover
+    """ Internal class used to call the websocket callbacks. """
+
+    def onOpen(self):
+        self.factory.datasift['send_message'] = self.sendMessage
+        self.factory.datasift['on_open']()
+
+    def onClose(self, wasClean, code, reason):
+        self.factory.datasift['on_close'](wasClean, code, reason)
+
+    def onMessage(self, msg, binary):
+        self.factory.datasift['on_message'](msg, binary)
+        self.factory.resetDelay()
+
+    def onPing(self, payload):
+        self.factory.resetDelay()
+        # Intentionally commented out for the beta, revert when suitable
+        #self.sendPong(payload=payload)
+
+
+class LiveStreamFactory(ReconnectingClientFactory, WebSocketClientFactory):  # pragma: no cover
+    """ Internal class used to implement the WebSocketClientFactory used in :class:`~datasift.client.Client` with reconnection"""
+
+    protocol = LiveStream
+
+    maxDelay = 320
+    delay = 1
+
+    def startedConnecting(self, connector):
+        pass
+
+    def clientConnectionLost(self, connector, reason):
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
