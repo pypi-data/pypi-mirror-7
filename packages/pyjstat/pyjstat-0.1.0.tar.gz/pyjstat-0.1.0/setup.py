a8ae9c538b798d65a0145c729886c724ca16e474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package pyjstat/0.1.0/pyjstat-0.1.0.tar.gz

---
+++ packages/pyjstat/pyjstat-0.1.0.tar.gz/pyjstat-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pyjstat',
+    version='0.1.0',
+    author='Miguel Expósito Martín',
+    author_email='miguel.exposito@gmail.com',
+    packages=['pyjstat', 'pyjstat.test'],
+    scripts=['bin/example.py'],
+    url='http://pypi.python.org/pypi/pyjstat/',
+    license='Apache License 2.0',
+    description='Library to handle JSON-stat data in python using pandas '
+                'DataFrames.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+       'pandas'
+    ],
+    test_suite='pyjstat.test',
+    keywords=['json-stat', 'json', 'statistics', 'dataframe', 'converter'],
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Software Development :: Libraries'
+          ],
+)
