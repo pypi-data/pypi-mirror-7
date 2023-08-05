9a19d3ecc9c98d80946a41bce88db04a8bcb039f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:15 +0000

    Add package rtmidi2/0.5.3/rtmidi2-0.5.3.tar.gz

---
+++ packages/rtmidi2/rtmidi2-0.5.3.tar.gz/rtmidi2-0.5.3/setup.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import distutils
+import sys
+from Cython.Distutils import build_ext
+
+module_source = 'rtmidi2.pyx'
+
+extension_args = {}
+version = open("version.cfg").read().strip()
+print("version: {version}".format(version=version))
+
+if sys.platform.startswith('linux'):
+    extension_args = dict(
+        define_macros=[('__LINUX_ALSASEQ__', None)],
+        libraries=['asound', 'pthread']
+    )
+
+if sys.platform == 'darwin':
+    extension_args = dict(
+        define_macros=[('__MACOSX_CORE__', None)],
+        extra_compile_args=['-frtti'],
+        extra_link_args=[
+            '-framework', 'CoreMidi',
+            '-framework', 'CoreAudio',
+            '-framework', 'CoreFoundation'
+        ]
+    )
+
+if sys.platform == 'win32':
+    extension_args = dict(
+        define_macros=[('__WINDOWS_MM__', None)],
+        libraries=['winmm']
+    )
+
+rtmidi_module = distutils.extension.Extension(
+    'rtmidi2',
+    [module_source, 'RtMidi/RtMidi.cpp'],
+    language='c++',
+    **extension_args
+)
+
+distutils.core.setup(
+    name='rtmidi2',
+    version=version,
+    description='Python wrapper for RtMidi written in Cython. Allows sending raw messages, multi-port input and sending multiple messages in one call.',
+    author='originally by Guido Lorenz, modified by Eduardo Moguillansky',
+    author_email='eduardo.moguillansky@gmail.com',
+    url="https://github.com/gesellkammer/rtmidi2",
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[rtmidi_module],
+    license='MIT',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Cython',
+        'Topic :: Multimedia :: Sound/Audio :: MIDI',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
