cf37986fa5cbcb4170003715241ca28ad93b57aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package cached-property/0.1.4/cached-property-0.1.4.tar.gz

---
+++ packages/cached-property/cached-property-0.1.4.tar.gz/cached-property-0.1.4/HISTORY.rst
@@ -0,0 +1,29 @@
+.. :changelog:
+
+History
+-------
+
+0.1.4 (2014-05-18)
+++++++++++++++++++
+
+* Fix the dang-blarged py_modules argument.
+
+0.1.3 (2014-05-17)
+++++++++++++++++++
+
+* Removed import of package into setup.py
+
+0.1.2 (2014-05-17)
+++++++++++++++++++
+
+* Documentation fixes. Not opening up a RTFD instance for this because it's so simple to use.
+
+0.1.1 (2014-05-17)
+++++++++++++++++++
+
+* setup.py fix. Whoops!
+
+0.1.0 (2014-05-17)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/cached-property/cached-property-0.1.4.tar.gz/cached-property-0.1.4/cached_property.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Daniel Greenfeld'
+__email__ = 'pydanny@gmail.com'
+__version__ = '0.1.4'
+__license__ = 'BSD'
+
+
+class cached_property(object):
+    """ A property that is only computed once per instance and then replaces
+        itself with an ordinary attribute. Deleting the attribute resets the
+        property. """
+
+    def __init__(self, func):
+        self.__doc__ = getattr(func, '__doc__')
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
