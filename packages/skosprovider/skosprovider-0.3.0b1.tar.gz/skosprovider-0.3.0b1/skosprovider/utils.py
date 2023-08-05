ffecadba779e315522c9715324cf667d3b1e9b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package skosprovider/0.3.0b1/skosprovider-0.3.0b1.tar.gz

---
+++ packages/skosprovider/skosprovider-0.3.0b1.tar.gz/skosprovider-0.3.0b1/skosprovider/utils.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''
+This module contains utility functions for dealing with skos providers.
+'''
+
+from __future__ import unicode_literals
+
+from skosprovider.skos import (
+    Concept,
+    Collection
+)
+
+
+def dict_dumper(provider):
+    '''
+    Dump a provider to a format that can be passed to a
+    :class:`skosprovider.providers.DictionaryProvider`.
+
+    :param skosprovider.providers.VocabularyProvider provider: The provider
+        that wil be turned into a `dict`.
+    :rtype: A list of dicts.
+
+    .. versionadded:: 0.2.0
+    '''
+    ret = []
+    for stuff in provider.get_all():
+        c = provider.get_by_id(stuff['id'])
+        labels = [l.__dict__ for l in c.labels]
+        if isinstance(c, Concept):
+            notes = [n.__dict__ for n in c.notes]
+            ret.append({
+                'id': c.id,
+                'uri': c.uri,
+                'type': c.type,
+                'labels': labels,
+                'notes': notes,
+                'narrower': c.narrower,
+                'broader': c.broader,
+                'related': c.related,
+                'member_of': c.member_of
+            })
+        elif isinstance(c, Collection):
+            ret.append({
+                'id': c.id,
+                'uri': c.uri,
+                'type': c.type,
+                'labels': labels,
+                'members': c.members,
+                'member_of': c.member_of
+            })
+    return ret
