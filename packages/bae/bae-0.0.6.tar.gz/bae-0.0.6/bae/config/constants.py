f0807ebc95056d7f775cd5e5c54f113edb9bde61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package bae/0.0.6/bae-0.0.6.tar.gz

---
+++ packages/bae/bae-0.0.6.tar.gz/bae-0.0.6/bae/config/constants.py
@@ -0,0 +1,16 @@
+API_ENTRY = "https://openapi.baidu.com/rest/2.0"
+ONEKEY_ENTRY = "http://csdk.baidu.com/get_token.php"
+VERSION = "0.0.6"
+PROG    = "Bae Client"
+PROG_NAME = "bae"
+LICENSE = '''Bae Client Tools
+@Copyright 2013 Baidu Inc.'''
+AUTHOR  = "zhangguanxing01"
+CONTACT = "zhangguanxing01@baidu.com"
+WIKI    = "http://developer.baidu.com/wiki"
+DEVELOPER = "http://developer.baidu.com/"
+EPILOG  = "If you have any question about Bae Client, please Conatct " + CONTACT + " or visit " + WIKI + " to get more info"
+BAE_SUPPORT = "zhangguanxing01@baidu.com"
+BAE_APP_CONFIG = ".baeapp"
+DEV_APP_CONFIG = ".devapp"
+BAE_GLOBAL_CONFIG = ".bae"
