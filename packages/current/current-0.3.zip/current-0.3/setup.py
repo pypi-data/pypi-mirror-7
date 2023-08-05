0a4789c70878276196a007d1c156bdd768715344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package current/0.3/current-0.3.zip

---
+++ packages/current/current-0.3.zip/current-0.3/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='current',
+    version='0.3',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Current module relative paths and imports',
+    keywords='sys.path import temporary inspect caller',
+    license='CC0',
+    url='http://github.com/xflr6/current',
+    py_modules=['current'],
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
