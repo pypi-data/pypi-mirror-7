30813e9ebe873849473e96b58fc5b82110af8d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package alerta/3.1.0/alerta-3.1.0.tar.gz

---
+++ packages/alerta/alerta-3.1.0.tar.gz/alerta-3.1.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="alerta",
+    version='3.1.0',
+    description="Alerta unified command-line tool",
+    long_description=long_description,
+    license="MIT",
+    author="Nick Satterly",
+    author_email="nick.satterly@theguardian.com",
+    url="http://github.com/alerta/python-alerta-client",
+    packages=setuptools.find_packages(exclude=['tests']),
+    install_requires=[
+        'argparse',
+        'requests',
+        'pytz'
+    ],
+    keywords="alerta client unified command line tool",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Monitoring',
+    ],
+    entry_points={
+        'console_scripts': [ 'alerta = alerta.shell:main']
+    }
+)
