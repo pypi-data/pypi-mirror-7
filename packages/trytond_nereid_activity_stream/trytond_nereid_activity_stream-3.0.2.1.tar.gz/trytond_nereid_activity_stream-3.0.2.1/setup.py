4103fd90b45d4c8a6ce88e474eeaba74292f1f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package trytond_nereid_activity_stream/3.0.2.1/trytond_nereid_activity_stream-3.0.2.1.tar.gz

---
+++ packages/trytond_nereid_activity_stream/trytond_nereid_activity_stream-3.0.2.1.tar.gz/trytond_nereid_activity_stream-3.0.2.1/setup.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from setuptools import setup, Command
+import re
+import os
+import ConfigParser
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class RunAudit(Command):
+    """Audits source code using PyFlakes for following issues:
+        - Names which are used but not defined or used before they are defined.
+        - Names which are redefined without having been used.
+    """
+    description = "Audit source code with PyFlakes"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import os
+        import sys
+        try:
+            import pyflakes.scripts.pyflakes as flakes
+        except ImportError:
+            print "Audit requires PyFlakes installed in your system."
+            sys.exit(-1)
+
+        warns = 0
+        # Define top-level directories
+        dirs = ('.')
+        for dir in dirs:
+            for root, _, files in os.walk(dir):
+                if root.startswith(('./build')):
+                    continue
+                for file in files:
+                    if file != '__init__.py' and file.endswith('.py'):
+                        warns += flakes.checkPath(os.path.join(root, file))
+        if warns > 0:
+            print "Audit finished with total %d warnings." % warns
+        else:
+            print "No problems found in sourcecode."
+
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
+major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
+major_version = int(major_version)
+minor_version = int(minor_version)
+
+requires = [
+    'trytond_nereid>=3.0.7.0, <3.1',
+]
+for dep in info.get('depends', []):
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
+        requires.append(
+            'trytond_%s >= %s.%s, < %s.%s' %
+            (dep, major_version, minor_version, major_version,
+                minor_version + 1)
+        )
+requires.append(
+    'trytond >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)
+)
+
+setup(
+    name='trytond_nereid_activity_stream',
+    version=info.get('version', '0.0.1'),
+    description='Activity Stream (a.k.a news feed) for Tryton nereid',
+    long_description=open('README.rst').read(),
+    author='Openlabs Technologies & consulting (P) Limited',
+    author_email='info@openlabs.co.in',
+    url='https://github.com/openlabs/nereid-activity-stream',
+    package_dir={'trytond.modules.nereid_activity_stream': '.'},
+    packages=[
+        'trytond.modules.nereid_activity_stream',
+        'trytond.modules.nereid_activity_stream.tests',
+    ],
+    package_data={
+        'trytond.modules.nereid_activity_stream': info.get('xml', [])
+        + info.get('translation', []) + ['tryton.cfg'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Tryton',
+        'Topic :: Office/Business',
+    ],
+    license='GPLv3',
+    install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    nereid_blog = trytond.modules.nereid_activity_stream
+    """,
+    test_suite='tests.suite',
+    cmdclass={
+        'audit': RunAudit,
+    },
+    test_loader='trytond.test_loader:Loader',
+)
