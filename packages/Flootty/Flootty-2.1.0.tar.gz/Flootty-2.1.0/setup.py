5c8d4d0699546bf11a5648fda84980862accc8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package Flootty/2.1.0/Flootty-2.1.0.tar.gz

---
+++ packages/Flootty/Flootty-2.1.0.tar.gz/Flootty-2.1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Flootty',
+      version='2.1.0',
+      description='Floobits collaborative terminal',
+      author='Floobits',
+      author_email='info@floobits.com',
+      url='https://floobits.com/',
+      license="Apache2",
+      packages=['flootty'],
+      package_data={
+          '': ['README.md']
+      },
+      entry_points={
+          'console_scripts': [
+              'flootty = flootty.flootty:main',
+          ]},
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Topic :: Terminals',
+          'Topic :: Utilities',
+      ])
