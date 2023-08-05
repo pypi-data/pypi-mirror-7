98ccc2edaf97937de0bbb8b18897898117a18a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package CrowdProcess/0.1/CrowdProcess-0.1.tar.gz

---
+++ packages/CrowdProcess/CrowdProcess-0.1.tar.gz/CrowdProcess-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='CrowdProcess',
+    version='0.1',
+    packages=['crowdprocess'],
+    license='MIT',
+    maintainer='João Jerónimo',
+    maintainer_email='jj@crowdprocess.com',
+    url='https://github.com/CrowdProcess/crpy',
+    long_description=open('README.rst').read(),
+)
