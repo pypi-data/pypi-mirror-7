809145b444813e7350d123d7caa06f3111bcf847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:20 +0000

    Add package elastic/4.0.3/elastic-4.0.3.tar.gz

---
+++ packages/elastic/elastic-4.0.3.tar.gz/elastic-4.0.3/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='elastic',
+    version='4.0.3',
+    packages=['elastic'],
+    license='GPLv3',
+    description = 'Extension for ASE to calculate elastic constants',
+    author = 'Paweł T. Jochym',
+    author_email = 'Pawel.Jochym@ifj.edu.pl',
+    url = 'https://github.com/jochym/Elastic',
+    download_url = 'https://github.com/jochym/Elastic/tarball/4.0.2', 
+    keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'],
+    requires = ['ase','pyspglib'],
+    provides = ['elastic'],
+    platforms = ['all'],
+    classifiers = [],
+)
