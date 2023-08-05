2e18f0107330bbf2d161b9c7c8537459d838c3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package best-before/0.0.1/best-before-0.0.1.tar.gz

---
+++ packages/best-before/best-before-0.0.1.tar.gz/best-before-0.0.1/best_before/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+best-before
+
+Copyright (c) 2014, Friedrich Paetzke (paetzke@fastmail.fm)
+All rights reserved.
+
+"""
+from datetime import datetime
+from functools import wraps
+
+
+class InedibleError(Exception):
+    pass
+
+
+class best_before:
+
+    def __init__(self, date):
+        if isinstance(date, str):
+            self._date = datetime.strptime(date, '%Y-%m-%d')
+        else:
+            self._date = date
+
+    def __call__(self, func):
+        if self._date < datetime.now():
+            raise InedibleError("Time's up.")
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
