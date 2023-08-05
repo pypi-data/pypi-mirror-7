2d242bdf5e8ec6280890682860dcf87307c1ea22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:43 +0000

    Add package pyhammer/0.3.39/pyhammer-0.3.39.zip

---
+++ packages/pyhammer/pyhammer-0.3.39.zip/pyhammer-0.3.39/pyhammer/tasks/helpers/mstesttask.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+from pyhammer.tasks.taskbase import TaskBase
+from pyhammer.utils import execProg
+
+class MsTestTask(TaskBase):
+    """Cs UnitTest Step"""
+
+    def __init__( self, csTestDllPath ):
+        super(MsTestTask, self).__init__()
+
+        self.command = """MSTest.exe /testcontainer:\"%s\"""" % ( csTestDllPath )
+        self.csTestDllPath = csTestDllPath
+
+    def do( self ):
+        self.reporter.message( "RUN CS UNITTEST: %s" % self.csTestDllPath )
+
+        print(self.command)
+
+        result = execProg( self.command, self.reporter, os.path.dirname(self.csTestDllPath) ) == 0
+        return result
+
+
