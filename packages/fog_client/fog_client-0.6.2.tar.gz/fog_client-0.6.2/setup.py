02a601a1c7f629e72db86044d76184333f8285fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package fog_client/0.6.2/fog_client-0.6.2.tar.gz

---
+++ packages/fog_client/fog_client-0.6.2.tar.gz/fog_client-0.6.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='fog_client',
+    version='0.6.2',
+    author='Carles Gonzalez',
+    packages=['components', 'cliapp', ],
+    py_modules=['fog_lib', 'fog_client'],
+    requires=['cuisine',
+              'requests (>=0.13)'],
+    scripts=['fog_client.py'],
+    data_files=[('/etc/init', ['aux/fog_client.conf']),
+                ('/etc', ['aux/fog_client.ini'])],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration'])
