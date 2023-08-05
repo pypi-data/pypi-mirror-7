66bca05e61c4fe1cd9d1eb7f72569d3bb86ef009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:22 +0000

    Add package django-bootstrap-typeahead/1.1.4/django-bootstrap-typeahead-1.1.4.tar.gz

---
+++ packages/django-bootstrap-typeahead/django-bootstrap-typeahead-1.1.4.tar.gz/django-bootstrap-typeahead-1.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-bootstrap-typeahead",
+    version="1.1.4",
+    packages=find_packages(),
+    author="A.J. May",
+    author_email="aj7may@gmail.com",
+    description="Convert django model choice fields into typeahead fields.",
+    license="MIT License",
+    keywords="django bootstrap typeahead form widget input",
+    url="http://thegoods.aj7may.com/django-bootstrap-typeahead",
+    zip_safe=False,
+)
