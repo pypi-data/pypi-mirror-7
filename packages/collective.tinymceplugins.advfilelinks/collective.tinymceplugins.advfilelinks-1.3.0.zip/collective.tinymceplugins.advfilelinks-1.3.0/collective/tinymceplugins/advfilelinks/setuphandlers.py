e59a16f7c7016472307812e1e570eeb73cb20cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:48 +0000

    Add package collective.tinymceplugins.advfilelinks/1.3.0/collective.tinymceplugins.advfilelinks-1.3.0.zip

---
+++ packages/collective.tinymceplugins.advfilelinks/collective.tinymceplugins.advfilelinks-1.3.0.zip/collective.tinymceplugins.advfilelinks-1.3.0/collective/tinymceplugins/advfilelinks/setuphandlers.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from collective.tinymceplugins.advfilelinks import logger
+from Products.CMFCore.utils import getToolByName
+
+def migrateTo_0_2_0(context):
+    setup_tool = getToolByName(context, 'portal_setup')
+#    setup_tool.setBaselineContext('profile-collective.tinymceplugins.advfilelinks:to_0_2_0')
+    setup_tool.runAllImportStepsFromProfile('profile-collective.tinymceplugins.advfilelinks:to_0_2_0')
+    setup_tool.runImportStepFromProfile('profile-collective.tinymceplugins.advfilelinks:default', 'browserlayer')
+    logger.info("Migrated to version 0.2")
+
+def migrateTo_1_3_0(context):
+    setup_tool = getToolByName(context, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-collective.mtrsetup:default')
+    setup_tool.runAllImportStepsFromProfile('profile-collective.mtrsetup:example')
+    setup_tool.runImportStepFromProfile('profile-collective.tinymceplugins.advfilelinks:default', 'mimetypes')
+    logger.info("Migrated to version 1.3.0")
