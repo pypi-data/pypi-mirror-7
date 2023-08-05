1fb3f6d8552f8779c3b34129517e4a81b7aabb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package django_simple_bugs/0.1.3/django_simple_bugs-0.1.3.zip

---
+++ packages/django_simple_bugs/django_simple_bugs-0.1.3.zip/django_simple_bugs-0.1.3/simple_bugs/admin.py
@@ -0,0 +1,15 @@
+__author__ = 'Thomas'
+from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+
+from .models import Requirement, Case
+
+
+class RequirementAdmin(SimpleHistoryAdmin):
+    pass
+admin.site.register(Requirement, RequirementAdmin)
+
+
+class CaseAdmin(SimpleHistoryAdmin):
+    pass
+admin.site.register(Case, CaseAdmin)
---
+++ packages/django_simple_bugs/django_simple_bugs-0.1.3.zip/django_simple_bugs-0.1.3/simple_bugs/serializers.py
@@ -0,0 +1,60 @@
+__author__ = 'Thomas'
+from rest_framework import serializers
+from django.forms import widgets
+
+from .models import Case, Requirement
+
+
+class CaseSerializer(serializers.Serializer):
+    class Meta:
+        model = Case
+        fields = ('pk', 'type', 'title', 'detail', 'closed', 'slug', 'created_on', 'user')
+
+    pk = serializers.Field()
+    type = serializers.ChoiceField(choices=Case.type_choice)
+    title = serializers.CharField(max_length=55)
+    detail = serializers.CharField(widget=widgets.Textarea, max_length=100000)
+    closed = serializers.BooleanField(required=False)
+    slug = serializers.SlugField(required=False)
+    created_on = serializers.DateTimeField()
+    user = serializers.Field(source='user.username')
+
+    def restore_object(self, attrs, instance=None):
+        if instance:
+            instance.type = attrs.get('type', instance.type)
+            instance.title = attrs.get('title', instance.title)
+            instance.detail = attrs.get('detail', instance.detail)
+            instance.closed = attrs.get('closed', instance.closed)
+            return instance
+        return Case(**attrs)
+
+
+class RequirementSerializer(serializers.Serializer):
+    class Meta:
+        model = Requirement
+        fields = ('pk', 'title', 'detail', 'slug', 'user', 'working_on')
+
+    pk = serializers.Field()
+    title = serializers.CharField(max_length=55)
+    detail = serializers.CharField(widget=widgets.Textarea, max_length=100000)
+    slug = serializers.SlugField(required=False)
+    user = serializers.RelatedField()
+    working_on = serializers.RelatedField(many=True)
+
+    def restore_object(self, attrs, instance=None):
+        if instance:
+            instance.title = attrs.get('title', instance.title)
+            instance.detail = attrs.get('detail', instance.detail)
+            return instance
+        return Case(**attrs)
+
+
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    cases = serializers.PrimaryKeyRelatedField(many=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'cases')
