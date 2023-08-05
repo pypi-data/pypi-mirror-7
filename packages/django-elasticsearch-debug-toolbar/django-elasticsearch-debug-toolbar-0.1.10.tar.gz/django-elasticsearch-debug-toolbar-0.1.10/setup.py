013fb3c677185b504dc1f280b961eb1b4f789ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package django-elasticsearch-debug-toolbar/0.1.10/django-elasticsearch-debug-toolbar-0.1.10.tar.gz

---
+++ packages/django-elasticsearch-debug-toolbar/django-elasticsearch-debug-toolbar-0.1.10.tar.gz/django-elasticsearch-debug-toolbar-0.1.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-elasticsearch-debug-toolbar',
+    version='0.1.10',
+    description='A Django Debug Toolbar panel for Elasticsearch',
+    author='Benoit Chabord',
+    author_email='benauf@gmail.com',
+    url='http://github.com/Benoss/elasticsearch-django-debug-toolbar',
+    license='MIT',
+    packages=find_packages(),
+    provides=['elastic_toolbar'],
+    install_requires=[
+        'elasticsearch>=1.0',
+        'django-debug-toolbar>=1.2',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
