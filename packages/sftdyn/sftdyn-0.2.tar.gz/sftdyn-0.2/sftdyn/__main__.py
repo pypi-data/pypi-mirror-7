09be279d1a683220bb986edc721c1dccb0d41365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package sftdyn/0.2/sftdyn-0.2.tar.gz

---
+++ packages/sftdyn/sftdyn-0.2.tar.gz/sftdyn-0.2/sftdyn/__main__.py
@@ -0,0 +1,13 @@
+import sftdyn.args
+import sftdyn.server
+
+
+def main():
+	sftdyn.args.__dict__.update(sftdyn.args.parse().__dict__)
+	try:
+		sftdyn.server.serve()
+	except KeyboardInterrupt:
+		pass
+
+if __name__ == "__main__":
+	main()
