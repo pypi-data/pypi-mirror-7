878576691f0198b6cab0ce855a53374a512bc007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:07 +0000

    Add package ripple-python/0.2.5/ripple-python-0.2.5.tar.gz

---
+++ packages/ripple-python/ripple-python-0.2.5.tar.gz/ripple-python-0.2.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'ecdsa>=0.10',
+    'six>=1.5.2',
+    'websocket-client==dev'
+]
+dependency_links = [
+    # dependency_links will be gone in pip 1.6, but by that time
+    # websocket-client on PyPI should have Python3 support.
+    'https://github.com/ralphbean/websocket-client/archive/master.zip#egg=websocket-client-dev'
+]
+
+
+setup(
+    name="ripple-python",
+    description="Python routines for the Ripple payment network",
+    author='Michael Elsdörfer',
+    author_email='michael@elsdoerfer.com',
+    version="0.2.5",
+    url="https://github.com/miracle2k/ripple-python",
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
