3fe344e0c055ff374491341cffe344e3066b312e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:08 +0000

    Add package django-addendum/0.2.1/django-addendum-0.2.1.tar.gz

---
+++ packages/django-addendum/django-addendum-0.2.1.tar.gz/django-addendum-0.2.1/addendum/models.py
@@ -0,0 +1,63 @@
+from django.core.cache import cache
+from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
+class CachedManager(models.Manager):
+
+    def get_from_cache(self, key):
+        """
+        Fetches the snippet from cache.
+
+        Returns a `Snippet` object or `None`.
+
+        This method addes every queried key to the cache to ensure that misses
+        doesn't continue to generate database lookups. Since `None` is the
+        default return value for a cache miss, the method uses -1 as the miss
+        value. If this is returned we know that the value should not be present
+        in the database, either.
+        """
+        snippet = cache.get('snippet:{0}'.format(key))
+
+        if snippet == -1:
+            return None
+
+        if snippet is None:
+            try:
+                snippet = Snippet.objects.get(key=key)
+            except Snippet.DoesNotExist:
+                cache.set('snippet:{0}'.format(key), -1)
+            else:
+                cache.set('snippet:{0}'.format(key), snippet)
+
+        return snippet
+
+
+class Snippet(models.Model):
+    """
+    Model for storing snippets of text for replacement in templates.
+    """
+    key = models.CharField(max_length=250, primary_key=True)
+    text = models.TextField()
+    objects = CachedManager()
+
+    class Meta:
+        ordering = ('key',)
+
+    def __unicode__(self):
+        return self.key
+
+
+@receiver(post_save, sender=Snippet)
+def set_cached_business(sender, **kwargs):
+    """Update the cached copy of the snippet on creation or change"""
+    instance = kwargs.pop('instance')
+    cache.set('snippet:{0}'.format(instance.key), instance)
+
+
+@receiver(post_delete, sender=Snippet)
+def clear_cached_business(sender, **kwargs):
+    """Remove the cached copy of the snippet after deletion"""
+    instance = kwargs.pop('instance')
+    cache.delete('snippet:{0}'.format(instance.key))
---
+++ packages/django-addendum/django-addendum-0.2.1.tar.gz/django-addendum-0.2.1/tests/__init__.py
@@ -0,0 +1,78 @@
+from django.test import TestCase
+from django.template import Context, Template
+
+from addendum.models import Snippet
+
+
+class TagTests(TestCase):
+    """
+    Tests that the template tag renders the correct text
+    """
+
+    def setUp(self):
+        self.plain_snippet = Snippet.objects.create(key="plain",
+                text="Hello, humans")
+        self.rich_snippet = Snippet.objects.create(key="rich",
+                text="<h1>Hello, humans</h1>")
+        self.template_snippet = Snippet.objects.create(key="django",
+                text="{{ dog|upper }}")
+
+    def test_has_snippet(self):
+        """Ensure that the saved snippet text is displayed"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'plain' %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({})
+        result = t.render(c)
+        self.assertEqual(result, "Hello, humans")
+
+    def test_no_snippet(self):
+        """Ensure that the default text is displayed"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'missing' %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({})
+        result = t.render(c)
+        self.assertEqual(result, "Hello world")
+
+    def test_plain_text(self):
+        """Ensure that content is not escaped"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'rich' %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({})
+        result = t.render(c)
+        self.assertEqual(result, "&lt;h1&gt;Hello, humans&lt;/h1&gt;")
+
+    def test_richtext(self):
+        """Ensure that with richtext argument content is escaped"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'rich' richtext=True %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({})
+        result = t.render(c)
+        self.assertEqual(result, "<h1>Hello, humans</h1>")
+
+    def test_raw_template_text(self):
+        """Ensure template code is not compiled by default"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'django' %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({'dog': 'woof'})
+        result = t.render(c)
+        self.assertEqual(result, "{{ dog|upper }}")
+
+    def test_template_text(self):
+        """Ensure template code is rendered with the template option"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'django' template=True %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({'dog': 'woof'})
+        result = t.render(c)
+        self.assertEqual(result, "WOOF")
+
+    def test_safe_template_text(self):
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'django' template=True %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({'dog': '<h1>woof</h1>'})
+        result = t.render(c)
+        self.assertEqual(result, "&lt;H1&gt;WOOF&lt;/H1&gt;")
+
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet 'django' template=True safe=True %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({'dog': '<h1>woof</h1>'})
+        result = t.render(c)
+        self.assertEqual(result, "<H1>WOOF</H1>")
+
+    def test_variable_key_name(self):
+        """Ensure a variable can be passed for the snippet key"""
+        t = Template("""{% spaceless %}{% load addendum_tags %}{% snippet snippetname %}Hello world{% endsnippet %}{% endspaceless %}""")
+        c = Context({'snippetname': 'plain'})
+        result = t.render(c)
+        self.assertEqual(result, "Hello, humans")
