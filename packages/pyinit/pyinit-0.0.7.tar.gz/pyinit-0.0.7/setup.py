04a8f5666b3cc6f8340f899f5ffde3981ccbebb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package pyinit/0.0.7/pyinit-0.0.7.tar.gz

---
+++ packages/pyinit/pyinit-0.0.7.tar.gz/pyinit-0.0.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup	
+
+setup(
+	name='pyinit',
+	version='0.0.7',
+	description='A cli tool for initializing a python module',
+	author= 'Jarvys',
+	author_email='yuhant534@126.com',
+	url="https://github.com/jarvys/pyinit",
+	py_modules=[],
+	scripts=['pyinit'],
+	keywords=['module', 'cli']
+)
+
