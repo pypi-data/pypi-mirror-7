133f498a2af949744bd09f3af5f7fde6a0208857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package pypsh/0.7.2/pypsh-0.7.2-py3-none-any.whl

---
+++ packages/pypsh/pypsh-0.7.2-py3-none-any.whl/pypsh/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+pypsh - cli for parallel ssh command execution on all known hosts that match a
+given regex.
+
+"""
+
+__author__ = 'Mathias Fußenegger'
+__version__ = '0.7.2'
+__license__ = 'MIT'
