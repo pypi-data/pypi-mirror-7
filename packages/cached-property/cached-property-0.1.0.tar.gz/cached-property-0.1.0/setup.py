1624fcf98f347275e0c25e419c6012b38cd2cda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package cached-property/0.1.0/cached-property-0.1.0.tar.gz

---
+++ packages/cached-property/cached-property-0.1.0.tar.gz/cached-property-0.1.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+import cached_property
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel upload')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (cached_property.__version__,
+        cached_property.version))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='cached-property',
+    version='0.1.0',
+    description='A cached-property for decorating methods in classes.',
+    long_description=readme + '\n\n' + history,
+    author='Daniel Greenfeld',
+    author_email='pydanny@gmail.com',
+    url='https://github.com/pydanny/cached-property',
+    py_modules=['dj_database_url'],
+    include_package_data=True,
+    license="BSD",
+    zip_safe=False,
+    keywords='cached-property',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
