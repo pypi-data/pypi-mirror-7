b95366ad029868abe988653d8369e17953cc816f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:09 +0000

    Add package python-libnmap/0.5.0/python-libnmap-0.5.0.tar.gz

---
+++ packages/python-libnmap/python-libnmap-0.5.0.tar.gz/python-libnmap-0.5.0/libnmap/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'Ronald Bister, Mike Boutillier'
+__credits__ = ['Ronald Bister', 'Mike Boutillier']
+__maintainer__ = 'Ronald Bister'
+__email__ = 'mini.pelle@gmail.com'
+__license__ = 'CC-BY'
+__version__ = '0.2'
