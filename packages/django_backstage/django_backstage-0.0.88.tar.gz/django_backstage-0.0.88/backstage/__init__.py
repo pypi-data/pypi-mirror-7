77ed043c5bb235b79f4bcf603a2fa89ebd44856d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:14 +0000

    Add package django_backstage/0.0.88/django_backstage-0.0.88.tar.gz

---
+++ packages/django_backstage/django_backstage-0.0.88.tar.gz/django_backstage-0.0.88/backstage/__init__.py
@@ -0,0 +1,2 @@
+from backstage.venue import Venue, new_venue
+from backstage.site import Site
---
+++ packages/django_backstage/django_backstage-0.0.88.tar.gz/django_backstage-0.0.88/backstage/act/act.py
@@ -0,0 +1,21 @@
+import os
+from backstage.venue import use_venue
+from backstage.venue import Venue
+
+def new_act(venue, actname):
+    """create a new Act within a backstage Venue"""
+    if isinstance(venue, str):
+        try:
+            venue = use_venue(venue)
+        except:
+            venue = None
+    if not isinstance(venue, Venue):
+        print '%s is not a valid backstage venue' % (venue)
+        return None
+    acts_dir = os.path.join(venue.VENUE_ROOT, 'acts')
+    act_home = os.path.join(acts_dir, actname)
+    if os.path.exists(act_home):
+        print 'A folder named %s already exists under %s' % (actname, acts_dir)
+        return None
+    os.mkdir(act_home)
+
