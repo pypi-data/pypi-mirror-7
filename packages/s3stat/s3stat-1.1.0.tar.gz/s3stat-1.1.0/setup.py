08bd588e6775e0082726c9f56210b7460dd125c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package s3stat/1.1.0/s3stat-1.1.0.tar.gz

---
+++ packages/s3stat/s3stat-1.1.0.tar.gz/s3stat-1.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import s3stat
+
+setup(
+    name="s3stat",
+    version="1.1.0",
+    description='An extensible Amazon S3 and Cloudfront log parser.',
+    long_description=s3stat.__doc__,
+    author="Viktor Nagy",
+    author_email='v@pulilab.com',
+    url='https://github.com/nagyv/s3stat',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['boto'],
+    py_modules=['s3stat'],
+    scripts=['s3stat.py'],
+    keywords="s3stat amazon statistics goaccess"
+    # tests_require=['pytest'],
+    # cmdclass = {
+    #     'test': PyTest,
+    # }
+)
