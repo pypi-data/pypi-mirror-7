2a863a4bfbe18806d53a65c09d4a854f53c485ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:29 +0000

    Add package simpleeval/0.8.1/simpleeval-0.8.1.tar.gz

---
+++ packages/simpleeval/simpleeval-0.8.1.tar.gz/simpleeval-0.8.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+__version__ = '0.8.1'
+
+setup(
+    name = 'simpleeval',
+    py_modules = ['simpleeval'],
+    version = __version__,
+    description = 'A simple, safe single expression evaluator library.',
+    long_description=open('README.rst','r').read(),
+    author = 'Daniel Fairhead',
+    author_email = 'danthedeckie@gmail.com',
+    url = 'https://github.com/danthedeckie/simpleeval',
+    download_url = 'https://github.com/danthedeckie/simpleeval/tarball/' + __version__,
+    keywords = ['eval', 'simple', 'expression', 'parse', 'ast'],
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Programming Language :: Python',
+                  ],
+    )
