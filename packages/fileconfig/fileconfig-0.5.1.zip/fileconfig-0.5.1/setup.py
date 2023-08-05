7b66e9189bb8bde707b1c63fc33d0bb9dfca5791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package fileconfig/0.5.1/fileconfig-0.5.1.zip

---
+++ packages/fileconfig/fileconfig-0.5.1.zip/fileconfig-0.5.1/setup.py
@@ -0,0 +1,34 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fileconfig',
+    version='0.5.1',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Config file sections as objects',
+    keywords='configuration ini file inheritance aliasing',
+    license='MIT',
+    url='http://github.com/xflr6/fileconfig',
+    packages=find_packages(),
+    extras_require={
+        'dev': ['wheel'],
+        'test': ['nose', 'coverage', 'flake8', 'pep8-naming'],
+    },
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
