436e622ec059bbe259ab86e6e1c872a0c196cc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package hfpython_chapter1_nester/1.1.0/hfpython_chapter1_nester-1.1.0.tar.gz

---
+++ packages/hfpython_chapter1_nester/hfpython_chapter1_nester-1.1.0.tar.gz/hfpython_chapter1_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+__author__ = 'jonasezitis'
+from distutils.core import setup
+setup(
+name = 'hfpython_chapter1_nester',
+version = '1.1.0',
+py_modules = ['hfpython_chapter1_nester'],
+author = 'jonasezitis',
+author_email = 'jonasezitis@gmail.com',
+url = 'http://www.ezitis.se',
+description = 'Tutorial for creating a python distribution'
+)
