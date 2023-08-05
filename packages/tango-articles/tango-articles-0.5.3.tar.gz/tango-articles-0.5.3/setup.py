c927be5cc6390b37c87f1c26d320cf30e2889ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:22 +0000

    Add package tango-articles/0.5.3/tango-articles-0.5.3.tar.gz

---
+++ packages/tango-articles/tango-articles-0.5.3.tar.gz/tango-articles-0.5.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('docs/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tango-articles',
+    version='0.5.3',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    url='http://github.com/tBaxter/tango-articles',
+    license='LICENSE.txt',
+    description='Reusable Django articles/blog content app. Can be used with or without Tango.',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=required,
+    zip_safe=False,
+    include_package_data=True,
+    dependency_links = ['http://github.com/tBaxter/tango-shared-core/tarball/master#egg=tango-shared-0.6']
+)
