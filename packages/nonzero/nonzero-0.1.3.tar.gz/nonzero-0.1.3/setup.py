aecab713506dd1c4cd03115415dad58648f049d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package nonzero/0.1.3/nonzero-0.1.3.tar.gz

---
+++ packages/nonzero/nonzero-0.1.3.tar.gz/nonzero-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'nonzero',
+  py_modules = ['nonzero'],
+  version = '0.1.3',
+  description = 'A class to assist with doing non-zero math',
+  author = 'Z. D. Smith',
+  author_email = 'zd@zdsmith.com',
+  url = 'https://github.com/subsetpark/nonzero',
+  download_url = 'https://github.com/subsetpark/nonzero/releases/tag/0.1.3',
+  keywords = ['zero', 'year', 'date'],
+  classifiers = [],
+)
