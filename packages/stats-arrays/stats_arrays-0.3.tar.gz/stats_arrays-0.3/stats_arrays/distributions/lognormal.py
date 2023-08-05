298b6c402c9eb9d00a002c88e4ed9ec5134b1820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:42 +0000

    Add package stats-arrays/0.3/stats_arrays-0.3.tar.gz

---
+++ packages/stats-arrays/stats_arrays-0.3.tar.gz/stats_arrays-0.3/stats_arrays/distributions/lognormal.py
@@ -0,0 +1,118 @@
+from __future__ import division
+from ..errors import InvalidParamsError
+from ..utils import one_row_params_array
+from .base import UncertaintyBase
+from scipy import stats
+import numpy as np
+
+
+class LognormalUncertainty(UncertaintyBase):
+    id = 2
+    description = "Lognormal uncertainty"
+
+    @classmethod
+    def validate(cls, params):
+        """Custom validation because mean gets log-transformed"""
+        if np.isnan(params['loc']).sum():
+            raise InvalidParamsError(
+                "Real location (mu) values are required for"
+                " lognormal uncertainties."
+            )
+        if np.isnan(params['scale']).sum() or (params['scale'] <= 0).sum():
+            raise InvalidParamsError(
+                "Real, positive scale (sigma) values are required for"
+                " lognormal uncertainties."
+            )
+
+    @classmethod
+    def random_variables(cls, params, size, seeded_random=None):
+        if not seeded_random:
+            seeded_random = np.random
+        data = seeded_random.lognormal(
+            params['loc'],  # Mu
+            params['scale'],  # Sigma
+            size=(size, params.shape[0])
+        ).T
+        # Negative is needed because log loses sign information.
+        # Error handling not included, as this loop is called many times
+        data[params['negative'], :] = -1 * data[params['negative'], :]
+        return data
+
+    @classmethod
+    def cdf(cls, params, vector):
+        vector = cls.check_2d_inputs(params, vector)
+        vector[params['negative']] = -1 * vector[params['negative']]
+        results = np.zeros(vector.shape)
+        for row in range(params.shape[0]):
+            results[row, :] = stats.lognorm.cdf(
+                vector[row, :],
+                params['scale'][row],
+                scale=np.exp(params['loc'][row])
+            )
+        return results
+
+    @classmethod
+    def ppf(cls, params, percentages):
+        percentages = cls.check_2d_inputs(params, percentages)
+        results = np.zeros(percentages.shape)
+        for row in range(percentages.shape[0]):
+            results[row, :] = stats.lognorm.ppf(
+                percentages[row, :],
+                params['scale'][row],
+                scale=np.exp(params['loc'][row])
+            )
+        results[params['negative']] = -1 * results[params['negative']]
+        return results
+
+    @classmethod
+    @one_row_params_array
+    def statistics(cls, params):
+        negative = -1 if bool(params['negative']) else 1
+        geometric_mu = float(np.exp(params['loc']))
+        sigma = float(params['scale'])
+        mu = float(params['loc'])
+        geometric_sigma = float(np.exp(sigma))
+        mean = np.exp(mu + (sigma ** 2) / 2)
+        mode = np.exp(mu - sigma ** 2)
+        ci_95_lower = geometric_mu / (geometric_sigma ** 2)
+        ci_95_upper = geometric_mu * (geometric_sigma ** 2)
+        if negative == -1:
+            ci_95_lower, ci_95_upper = ci_95_upper, ci_95_lower
+        return {
+            'median': negative * geometric_mu,
+            'mode': negative * mode,
+            'mean': negative * mean,
+            'lower': negative * ci_95_lower,
+            'upper': negative * ci_95_upper
+        }
+
+    @classmethod
+    @one_row_params_array
+    def pdf(cls, params, xs=None):
+        """Generate probability distribution function for lognormal distribution."""
+        if xs is None:
+            # Find nice range to graph
+            if np.isnan(params['minimum']):
+                minimum = params['scale'] / (np.exp(params['shape']) **
+                                             cls.standard_deviations_in_default_range)
+            else:
+                minimum = np.abs(params['minimum'])
+            if np.isnan(params['maximum']):
+                maximum = params['scale'] * (np.exp(params['shape']) **
+                                             cls.standard_deviations_in_default_range)
+            else:
+                maximum = np.abs(params['minimum'])
+
+            xs = np.linspace(
+                minimum,
+                maximum,
+                cls.default_number_points_in_pdf
+            ).ravel()
+            print xs.shape
+
+        if params['negative']:
+            xs = -1 * xs
+        ys = stats.lognorm.pdf(xs, params['scale'], scale=np.exp(params['loc']))
+        if params['negative']:
+            xs = -1 * xs
+        return xs, ys.ravel()
---
+++ packages/stats-arrays/stats_arrays-0.3.tar.gz/stats_arrays-0.3/stats_arrays/tests/distributions/lognormal.py
@@ -0,0 +1,149 @@
+from ...errors import InvalidParamsError
+from ...distributions import LognormalUncertainty as LU
+from ..base import UncertaintyTestCase
+from scipy.special import erf
+import numpy as np
+
+
+class LognormalTestCase(UncertaintyTestCase):
+
+    def pdf(self, x, mu, sigma):
+        return 1 / (x * np.sqrt(2 * np.pi * sigma ** 2)
+                    ) * np.e ** (-((np.log(x) - mu) ** 2) / (
+            2 * sigma ** 2))
+
+    def cdf(self, x, mu, sigma):
+        return 0.5 * (1 + erf((np.log(x) - mu) / np.sqrt(
+            2 * sigma ** 2)))
+
+    def test_pdf_positive(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma})
+        self.assertTrue(np.allclose(
+            self.pdf(1.2, mu, sigma),
+            # [0] are X values, [1] are Y values
+            LU.pdf(pa, np.array((1.2,)))[1]
+        ))
+
+    def test_pdf_negative(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma, 'negative': True})
+        self.assertTrue(np.allclose(
+            self.pdf(1.2, mu, sigma),
+            # [0] are X values, [1] are Y values
+            LU.pdf(pa, np.array((-1.2,)))[1]
+        ))
+
+    def test_cdf_positive(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma})
+        self.assertTrue(np.allclose(
+            self.cdf(1.2, mu, sigma),
+            LU.cdf(pa, np.array((1.2,)))[0]
+        ))
+
+    def test_cdf_negative(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma, 'negative': True})
+        self.assertTrue(np.allclose(
+            self.cdf(1.2, mu, sigma),
+            LU.cdf(pa, np.array((-1.2,)))[0]
+        ))
+
+    def test_cdf_multirow(self):
+        pa = LU.from_dicts(
+            {'loc': 0.4, 'scale': 0.1, 'negative': True},
+            {'loc': 0.6, 'scale': 0.2, 'negative': False}
+        )
+        self.assertTrue(np.allclose(
+            self.cdf(
+                np.array((1.2, 1.5)),
+                np.array((0.4, 0.6)),
+                np.array((0.1, 0.2))
+            ),
+            LU.cdf(pa, np.array((-1.2, 1.5))).ravel()
+        ))
+
+    def test_ppf_positive(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma})
+        cdf = self.cdf(1.3, mu, sigma)
+        self.assertTrue(np.allclose(
+            1.3,
+            LU.ppf(pa, np.array((cdf,)))[0]
+        ))
+
+    def test_ppf_negative(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma, 'negative': True})
+        cdf = self.cdf(1.3, mu, sigma)
+        self.assertTrue(np.allclose(
+            -1.3,
+            LU.ppf(pa, np.array((cdf,)))[0]
+        ))
+
+    def test_validation(self):
+        dicts = [
+            {'loc': np.NaN, 'scale': 0.1},
+            {'loc': 0.1, 'scale': np.NaN},
+        ]
+        for d in dicts:
+            with self.assertRaises(InvalidParamsError):
+                LU.validate(LU.from_dicts(d))
+
+    def test_seeded_random(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma})
+        self.assertTrue(np.allclose(
+            LU.random_variables(pa, 100, self.seeded_random()),
+            LU.random_variables(pa, 100, self.seeded_random())
+        ))
+
+    def test_rng(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        median = np.exp(mu)
+        ci_upper_975 = np.exp(mu) * (np.exp(sigma) ** 1.96)
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma})
+        sample = LU.random_variables(pa, size=1e5).ravel()
+        sample.sort()
+        self.assertTrue(np.allclose(np.median(sample), median, 0.01))
+        self.assertTrue(np.allclose(sample[97500], ci_upper_975, 0.01))
+
+    def test_rng_negative(self):
+        mu, sigma = np.random.random() / 5 + 0.2, np.random.random() / 10 + 0.1
+        pa = LU.from_dicts({'loc': mu, 'scale': sigma, 'negative': True})
+        sample = LU.random_variables(pa, size=100).ravel()
+        self.assertEqual((sample < 0).sum(), 100)
+
+#     def test_lognormal_statistics(self):
+#         oneDparams = self.biased_params_1d()
+#         oneDparams['scale'] = 0.8
+#         to_array = lambda x: np.array([x[key] for key in sorted(x.keys())])
+#         self.assertTrue(np.allclose(
+#             to_array({
+#                 'upper': 14.859097922170479,
+#                 'lower': 0.60568955155650162,
+#                 'median': 3.0000000595022631,
+#                 'mode': 1.5818772733323232,
+#                 'mean': 4.131383414350033
+#             }),
+#             to_array(LognormalUncertainty.statistics(oneDparams))
+#         ))
+
+#     def test_lognormal_negative_statistics(self):
+#         oneDparams = self.biased_params_1d()
+#         oneDparams['scale'] = 0.8
+#         oneDparams['minimum'] = -4
+#         oneDparams['loc'] = -3
+#         oneDparams['maximum'] = -1
+#         to_array = lambda x: np.array([x[key] for key in sorted(x.keys())])
+#         self.assertTrue(np.allclose(
+#             to_array({
+#                 'lower': -14.859097922170479,
+#                 'upper': -0.60568955155650162,
+#                 'median': -3.0000000595022631,
+#                 'mode': -1.5818772733323232,
+#                 'mean': -4.131383414350033
+#             }),
+#             to_array(LognormalUncertainty.statistics(oneDparams, transform=True))
+#         ))
