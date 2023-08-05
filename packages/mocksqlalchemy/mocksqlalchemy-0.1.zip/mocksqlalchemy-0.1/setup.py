794ceda33d47313759314002b5f08d89874a8d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package mocksqlalchemy/0.1/mocksqlalchemy-0.1.zip

---
+++ packages/mocksqlalchemy/mocksqlalchemy-0.1.zip/mocksqlalchemy-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='mocksqlalchemy',
+      version='0.1',
+      description='',
+      author='Andy Levisay',
+      author_email='levisaya@gmail.com',
+      url='https://github.com/levisaya/mocksqlalchemy',
+      download='https://github.com/levisaya/mocksqlalchemy/tarball/0.1',
+      zip_safe=False,
+      packages=find_packages(),
+     )
