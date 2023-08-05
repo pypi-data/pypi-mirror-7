1bd250b6954771d741f1c8622f4b9b69ba465bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:03 +0000

    Add package boatd_client/0.1/boatd_client-0.1.tar.gz

---
+++ packages/boatd_client/boatd_client-0.1.tar.gz/boatd_client-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+with open('README.md') as file:
+        long_description = file.read()
+
+setup(
+    name='boatd_client',
+    version='0.1',
+    author='Louis Taylor',
+    author_email='kragniz@gmail.com',
+    description=('Python wrapper for the boatd API, used to write behavior scripts.'),
+    long_description=long_description,
+    license='GPL',
+    keywords='boat sailing wrapper rest',
+    url='https://github.com/boatd/python-boatd',
+    packages=['boatd_client'],
+    requires=['docopt'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
