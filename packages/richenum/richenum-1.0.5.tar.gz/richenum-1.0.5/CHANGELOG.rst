fd39a96e93d8899198b1029a324c205a2eb5e774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:43 +0000

    Add package richenum/1.0.5/richenum-1.0.5.tar.gz

---
+++ packages/richenum/richenum-1.0.5.tar.gz/richenum-1.0.5/CHANGELOG.rst
@@ -0,0 +1,25 @@
+Changelog
+=========
+
+1.0.4 (2013-12-03)
+------------------
+    - Better unicode handling in ``__str__``, ``__unicode__``, and
+      ``__repr__`` magic methods.
+
+1.0.3 (2013-12-03)
+------------------
+    - Stop throwing warnings.
+
+1.0.2 (2013-11-05)
+------------------
+    - Suppress warnings from mismatched type comparisons when generated
+      in RichEnum.lookup.
+
+1.0.1 (2013-09-20)
+------------------
+    - Raise warnings when comparing enum values to other types, but not
+      when checking membership or comparing to None.
+
+1.0.0 (2013-08-16)
+------------------
+    - Initial public release.
---
+++ packages/richenum/richenum-1.0.5.tar.gz/richenum-1.0.5/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+tests_require = [
+    'unittest2',
+]
+
+
+setup(
+    name='richenum',
+    version='1.0.5',
+    description='Enum library for python.',
+    long_description=(
+        open('README.rst').read() + '\n\n' +
+        open('CHANGELOG.rst').read() + '\n\n' +
+        open('AUTHORS.rst').read()),
+    classifiers=[],
+    keywords='python enum richenum',
+    url='https://github.com/hearsaycorp/richenum',
+    author='Hearsay Social',
+    author_email='opensource@hearsaysocial.com',
+    license='MIT',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    tests_require=tests_require,
+    test_suite='tests'
+)
