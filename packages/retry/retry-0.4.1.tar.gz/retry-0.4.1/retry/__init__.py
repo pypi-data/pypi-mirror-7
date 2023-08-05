accb30dcf2392142ccc73b67471b6b8f8504a3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package retry/0.4.1/retry-0.4.1.tar.gz

---
+++ packages/retry/retry-0.4.1.tar.gz/retry-0.4.1/retry/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import time
+from itertools import count
+
+from decorator import decorator
+
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+def retry(exceptions=Exception, tries=float('inf'), delay=0, backoff=1):
+    """Return a decorator for retrying.
+
+    :param exceptions: an exception or a tuple of exceptions to catch
+    :param tries: the maximum number of attempts
+    :param delay: how many seconds to wait between attmpts
+    :param backoff: delay growth factor
+    """
+
+    @decorator
+    def retry_decorator(f, *args, **kwargs):
+        for i in count():
+            try:
+                return f(*args, **kwargs)
+            except exceptions, e:
+                if i >= tries:
+                    raise
+                round_delay = delay * backoff ** i
+                log.warning('{}, retrying in {} seconds...'.format(e, round_delay))
+                time.sleep(round_delay)
+
+    return retry_decorator
+
+if __name__ == '__main__':
+    logging.basicConfig()
+
+    @retry(tries=3, delay=1)
+    def f():
+        1 / 0
+    f()
