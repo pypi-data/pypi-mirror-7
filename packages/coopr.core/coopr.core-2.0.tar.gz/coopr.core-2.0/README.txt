b319037ac2474bc6babf6ee0068b2680a935d22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:09 +0000

    Add package coopr.core/2.0/coopr.core-2.0.tar.gz

---
+++ packages/coopr.core/coopr.core-2.0.tar.gz/coopr.core-2.0/README.txt
@@ -0,0 +1,47 @@
+=================
+coopr.core README
+=================
+
+A Coopr package that provides cross-cutting utilities used by other Coopr packages.
+
+
+-------
+License
+-------
+
+BSD.  See the LICENSE.txt file.
+
+
+------------
+Organization
+------------
+
++ Directories
+
+  * coopr - The root directory for Coopr source code
+
++ Documentation and Bug Tracking
+
+  * Trac wiki: https://software.sandia.gov/trac/coopr
+
++ Authors
+
+  * See the AUTHORS.txt file.
+
++ Project Managers
+
+  * William Hart, wehart@sandia.gov
+
++ Mailing List
+
+  * coopr-forum@googlegroups.com
+    - The main list for help and announcements
+  * coopr-developers@googlegroups.com
+    - Where developers of Coopr discuss new features
+
+--------------------
+Third Party Software
+--------------------
+
+This package relies on pyutilib.component.core and pyutilib.workflow.
+
---
+++ packages/coopr.core/coopr.core-2.0.tar.gz/coopr.core-2.0/setup.py
@@ -0,0 +1,70 @@
+#  _________________________________________________________________________
+#
+#  Coopr: A COmmon Optimization Python Repository
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  _________________________________________________________________________
+
+"""
+Script to generate the installer for coopr.core.
+"""
+
+import glob
+import os
+
+def _find_packages(path):
+    """
+    Generate a list of nested packages
+    """
+    pkg_list=[]
+    if not os.path.exists(path):
+        return []
+    if not os.path.exists(path+os.sep+"__init__.py"):
+        return []
+    else:
+        pkg_list.append(path)
+    for root, dirs, files in os.walk(path, topdown=True):
+      if root in pkg_list and "__init__.py" in files:
+         for name in dirs:
+           if os.path.exists(root+os.sep+name+os.sep+"__init__.py"):
+              pkg_list.append(root+os.sep+name)
+    return [pkg for pkg in map(lambda x:x.replace(os.sep,"."), pkg_list)]
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+from setuptools import setup
+packages = _find_packages('coopr')
+
+setup(name='coopr.core',
+      version='2.0',
+      maintainer='William Hart',
+      maintainer_email='wehart@sandia.gov',
+      url = 'https://software.sandia.gov/svn/public/coopr/coopr.core',
+      license = 'BSD',
+      platforms = ["any"],
+      description = 'A Coopr package that provides cross-cutting utilities used by other Coopr packages.',
+      long_description = read('README.txt'),
+      classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Programming Language :: Unix Shell',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Scientific/Engineering'
+        ],
+      packages=packages,
+      keywords=['optimization'],
+      namespace_packages=['coopr'],
+      install_requires=['pyutilib.workflow']
+
+      )
+
