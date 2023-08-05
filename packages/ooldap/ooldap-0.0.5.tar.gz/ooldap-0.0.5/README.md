15d04600c5707b0f6bb16f223cf0aefff6a13e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package ooldap/0.0.5/ooldap-0.0.5.tar.gz

---
+++ packages/ooldap/ooldap-0.0.5.tar.gz/ooldap-0.0.5/README.md
@@ -0,0 +1,15 @@
+Using the python-ldap library: python-ldap.com
+
+
+Provides the foundation for manipulating ldap
+taking a highly object oriented approach.
+
+
+Use the provided object types, override them, or create your own.
+
+
+Add the three following environmental variables:
+
+- LDAP_CONNECTION_URI
+- LDAP_CONNECTION_DN
+- LDAP_CONNECTION_PASSWORD
---
+++ packages/ooldap/ooldap-0.0.5.tar.gz/ooldap-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+
+version = '0.0.5'
+
+setup(
+    name='ooldap',
+    version=version,
+    packages=find_packages(),
+    url='http://github.com/TAMUArch/ooldap',
+    author='John',
+    author_email='st_jphillips@tamu.edu',
+    description='Object Oriented LDAP',
+    license='Apache License 2.0',
+)
