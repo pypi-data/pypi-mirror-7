9212dd415c18cea9c401027358829f81bade070e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package Flask-Authorization-Panda/0.3/Flask-Authorization-Panda-0.3.tar.gz

---
+++ packages/Flask-Authorization-Panda/Flask-Authorization-Panda-0.3.tar.gz/Flask-Authorization-Panda-0.3/flask_authorization_panda/__init__.py
@@ -0,0 +1,36 @@
+"""
+**Flask-Authorization-Panda is a Flask extension that provides decorators
+for various authentication methods for RESTful web services.
+
+Currently, only HTTP Basic Authentication is supported. **
+
+Usage
+-----
+
+    >>> from flask.ext.flask_authorization_panda import basic_auth
+
+During app initialization, store your required username/password in
+the config attribute::
+
+    app = Flask(__name__)
+    app.config['basic_auth_credentials'] = dict(username='admin',
+                                                password='secret')
+
+
+Finally, simple apply the @basic_auth decorator to methods which you
+want to require HTTP Basic Auth::
+
+    >>> @app.route('/')
+    >>> @basic_auth
+    >>> def hello_world():
+    >>>    return jsonify({"statusCode": 200, "message": "Ok"})
+
+This will result in all calls against the decorated method to (1) check for
+for credentials on the request.authorization object and (2) verify that
+they match the contents of app.config['basic_auth_credentials]'
+
+"""
+
+__version__ = '0.3'
+
+from basic_auth import basic_auth
