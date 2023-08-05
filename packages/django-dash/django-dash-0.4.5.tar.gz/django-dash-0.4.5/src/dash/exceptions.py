bc3b71d935682b07506f7b65b0300ff72e688940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:34 +0000

    Add package django-dash/0.4.5/django-dash-0.4.5.tar.gz

---
+++ packages/django-dash/django-dash-0.4.5.tar.gz/django-dash-0.4.5/src/dash/exceptions.py
@@ -0,0 +1,31 @@
+__title__ = 'dash.exceptions'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = 'Copyright (c) 2013 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = (
+    'InvalidRegistryItemType', 'LayoutDoesNotExist', 'NoActiveLayout',
+    'PluginWidgetOutOfPlaceholderBoundaries'
+)
+
+class InvalidRegistryItemType(ValueError):
+    """
+    Raised when an attempt is made to register an item in the registry which does not have a proper type.
+    """
+
+
+class LayoutDoesNotExist(Exception):
+    """
+    Raised when layout does not exist.
+    """
+
+
+class NoActiveLayoutChosen(Exception):
+    """
+    Raised when no active layout is chosen.
+    """
+
+
+class PluginWidgetOutOfPlaceholderBoundaries(Exception):
+    """
+    Raised when plugin widget is out of placeholder boundaries.
+    """
