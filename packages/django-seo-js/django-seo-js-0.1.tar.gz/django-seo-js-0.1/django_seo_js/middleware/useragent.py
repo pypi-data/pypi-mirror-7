6b4f5e72189649312f32e18afaa3f0a88a335b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package django-seo-js/0.1/django-seo-js-0.1.tar.gz

---
+++ packages/django-seo-js/django-seo-js-0.1.tar.gz/django-seo-js-0.1/django_seo_js/middleware/useragent.py
@@ -0,0 +1,28 @@
+import re
+from django.conf import settings
+from django_seo_js.backends import SelectedBackend
+from django.http import HttpResponse
+
+DEFAULT_SEO_JS_USER_AGENTS = [
+    "Googlebot",
+    "Yahoo",
+    "bingbot",
+    "Badiu",
+    "Ask Jeeves",
+]
+
+class UserAgentMiddleware(SelectedBackend):
+    def __init__(self, *args, **kwargs):
+        super(UserAgentMiddleware, self).__init__(*args, **kwargs)
+        if getattr(settings, "SEO_JS_USER_AGENTS", None):
+            agents = getattr(settings, "SEO_JS_USER_AGENTS")
+        else:
+            agents = DEFAULT_SEO_JS_USER_AGENTS
+        regex_str = "|".join(agents)
+        regex_str = ".*?(%s)" % regex_str
+        self.USER_AGENT_REGEX = re.compile(regex_str, re.IGNORECASE)
+
+    def process_request(self, request):
+        if self.USER_AGENT_REGEX.match(request.META["HTTP_USER_AGENT"]):
+            url = request.build_absolute_uri()
+            return HttpResponse(self.backend.get_rendered_page(url))
