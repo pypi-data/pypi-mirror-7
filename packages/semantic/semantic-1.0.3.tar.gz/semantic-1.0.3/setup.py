ca67ccfb92b6c041a778d66e5280eb25d9189a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package semantic/1.0.3/semantic-1.0.3.tar.gz

---
+++ packages/semantic/semantic-1.0.3.tar.gz/semantic-1.0.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+requires = ['quantities', 'numpy']
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        import test
+        errno = test.run()
+        sys.exit(errno)
+
+
+setup(name='semantic',
+      version='1.0.3',
+      description='Common Natural Language Processing Tasks for Python',
+      author='Charles Marsh',
+      author_email='crmarsh@princeton.edu',
+      url='https://github.com/crm416/semantic',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+          'Topic :: Text Processing :: Linguistic'
+      ],
+      install_requires=requires,
+      tests_require=requires,
+      cmdclass={'test': PyTest},
+      long_description=open('README.txt').read(),
+      packages=['semantic', 'semantic.test']
+      )
