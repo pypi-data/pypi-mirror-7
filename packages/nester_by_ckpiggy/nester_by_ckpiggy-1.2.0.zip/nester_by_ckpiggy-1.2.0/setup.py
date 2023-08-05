539459443ec5aca03e3d42579cf3d2f6b9d9e259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package nester_by_ckpiggy/1.2.0/nester_by_ckpiggy-1.2.0.zip

---
+++ packages/nester_by_ckpiggy/nester_by_ckpiggy-1.2.0.zip/nester_by_ckpiggy-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_by_ckpiggy',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'ckpiggy',
+    author_email = 'mrgmp2004@gmail.com',
+    url='http:\\www.headfirstlabs.com',
+    description = 'A simple nest printer',
+    )
