f30a0badf657724489a60c7c5453f7990d89d227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package totango/0.3.0/totango-0.3.0.tar.gz

---
+++ packages/totango/totango-0.3.0.tar.gz/totango-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+requires = ['requests']
+
+from totango import __version__
+
+setup(name='totango',
+      version=__version__,
+      description='Totango Python Library',
+      author='Dominik Gehl',
+      author_email='dominik.gehl@returnpath.com',
+      url='http://github.com/returnpath/totango-python',
+      license='Apache 2.0',
+      packages=find_packages(),
+      install_requires = requires,
+      keywords="totango",
+      zip_safe=False)
