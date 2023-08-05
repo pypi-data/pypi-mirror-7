2b8b427730c7503ea63deb5e83d17f6c1f947878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:29 +0000

    Add package micropython-os.path/0.0.2/micropython-os.path-0.0.2.tar.gz

---
+++ packages/micropython-os.path/micropython-os.path-0.0.2.tar.gz/micropython-os.path-0.0.2/os/path.py
@@ -0,0 +1,40 @@
+import os
+
+
+def normcase(s):
+    return s
+
+def normpath(s):
+    return s
+
+def abspath(s):
+    return os.getcwd() + "/" + s
+
+def join(*args):
+    # TODO: this is non-compliant
+    return "/".join(args)
+
+def split(path):
+    if path == "":
+        return ("", "")
+    r = path.rsplit("/", 1)
+    if len(r) == 1:
+        return ("", path)
+    head = r[0] #.rstrip("/")
+    if not head:
+        head = "/"
+    return (head, r[1])
+
+def exists(path):
+    return os.access(path, os.F_OK)
+
+# TODO
+lexists = exists
+
+def isdir(path):
+    import stat
+    try:
+        mode = os.stat(path)[0]
+        return stat.S_ISDIR(mode)
+    except OSError:
+        return False
