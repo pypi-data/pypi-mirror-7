9a5df7b5f4d4cb98a659a67f539c3b9f2d3e5470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package tiddlywebplugins.csrf/0.5.0/tiddlywebplugins.csrf-0.5.0.tar.gz

---
+++ packages/tiddlywebplugins.csrf/tiddlywebplugins.csrf-0.5.0.tar.gz/tiddlywebplugins.csrf-0.5.0/setup.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.csrf'
+DESCRIPTION = 'CSRF Middleware Protector for TiddlyWeb'
+VERSION = '0.5.0'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    platforms = 'Posix; MacOS X; Windows',
+    packages = find_packages(exclude=['test']),
+    install_requires = ['setuptools',
+        'httpexceptor',
+        'tiddlyweb>=2.2.0'],
+    zip_safe = False
+    )
