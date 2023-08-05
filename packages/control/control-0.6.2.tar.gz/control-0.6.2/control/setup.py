329b6e035b6f6cff58ccff2921256bceb6c14c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:10 +0000

    Add package control/0.6.2/control-0.6.2.tar.gz

---
+++ packages/control/control-0.6.2.tar.gz/control-0.6.2/control/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('control', parent_package, top_path)
+    config.add_data_dir('tests')
+    return config
+
+if __name__ == '__main__':
+    print('This is the wrong setup.py file to run')
---
+++ packages/control/control-0.6.2.tar.gz/control-0.6.2/control/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CONTROL SETUP.PY
+short_version = '0.6.2'
+version = '0.6.2'
+full_version = '0.6.2'
+git_revision = '560cf7b9a84a2cd01c162842a99d21be916d6edf'
+release = True
+
+if not release:
+    version = full_version
