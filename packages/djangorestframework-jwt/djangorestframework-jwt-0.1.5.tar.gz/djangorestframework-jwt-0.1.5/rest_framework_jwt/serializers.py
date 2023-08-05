c584ba49a349c5fa764b90251bc16d5c453fb051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:11 +0000

    Add package djangorestframework-jwt/0.1.5/djangorestframework-jwt-0.1.5.tar.gz

---
+++ packages/djangorestframework-jwt/djangorestframework-jwt-0.1.5.tar.gz/djangorestframework-jwt-0.1.5/rest_framework_jwt/serializers.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+from rest_framework_jwt.settings import api_settings
+
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+
+class JSONWebTokenSerializer(serializers.Serializer):
+    """
+    Serializer class used to validate a username and password.
+
+    Returns a JSON Web Token that can be used to authenticate later calls.
+    """
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if user:
+                if not user.is_active:
+                    msg = 'User account is disabled.'
+                    raise serializers.ValidationError(msg)
+
+                payload = jwt_payload_handler(user)
+
+                return {
+                    'token': jwt_encode_handler(payload)
+                }
+            else:
+                msg = 'Unable to login with provided credentials.'
+                raise serializers.ValidationError(msg)
+        else:
+            msg = 'Must include "username" and "password"'
+            raise serializers.ValidationError(msg)
