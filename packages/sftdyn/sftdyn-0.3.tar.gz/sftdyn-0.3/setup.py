5d3aa8309be192bfe58492e158504c0bcec28888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package sftdyn/0.3/sftdyn-0.3.tar.gz

---
+++ packages/sftdyn/sftdyn-0.3.tar.gz/sftdyn-0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+from sftdyn import VERSION
+from sys import version_info
+import sys
+
+if version_info[0] < 3:
+	print("use python3 to install sftdyn (e.g. pip-3.2)")
+	exit(1)
+
+setup(
+	name="sftdyn",
+	version=VERSION,
+	description="HTTPs-based DDNS updater",
+	long_description="dyndns-like service that accepts update requests " +
+					 "via HTTPs and forwards them to a locally running DNS " +
+					 "server via nsupdate -l. Readme: " +
+					 "https://github.com/SFTtech/sftdyn/blob/master/README.md",
+	author="Michael Ensslin",
+	author_email="michael@ensslin.cc",
+	url="https://github.com/SFTtech/sftdyn",
+	license="GPLv3 or higher",
+	packages=["sftdyn"],
+	scripts=["bin/sftdyn"],
+	data_files=[("/etc/sftdyn/", ["sample.conf"])]
+)
---
+++ packages/sftdyn/sftdyn-0.3.tar.gz/sftdyn-0.3/sftdyn/args.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+p = argparse.ArgumentParser(description="SFT dynamic DNS updater HTTPS server")
+p.add_argument("-c", "--cert", type=str,
+			   help="X.509 cert file")
+p.add_argument("-k", "--key", type=str,
+			   help="X.509 key file")
+p.add_argument("conffile", type=str, nargs="?", default="/etc/sftdyn/conf",
+			   help="conf file, will be exec'd as python3")
+p.add_argument("-p", "--port", type=int, default=4443)
+p.add_argument("-l", "--listen", type=str, default="0.0.0.0")
+p.add_argument("--nsupdatecommand", type=str,
+			   default="update delete $HOST A\n" +
+					   "update add $HOST 30 A $IP\n" +
+					   "send\n",
+			   help="template command list for nsupdate")
+
+
+def parse():
+	args = p.parse_args()
+	args.clients = {}
+
+	if not os.path.isfile(args.conffile):
+		p.error("Not a valid conf file: " + args.conffile)
+
+	exec(open(args.conffile).read(), args.__dict__)
+
+	if not args.cert or not args.key:
+		p.error("You must provide key and cert files")
+
+	return args
