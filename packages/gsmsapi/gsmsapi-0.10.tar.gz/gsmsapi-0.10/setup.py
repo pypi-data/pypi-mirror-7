1a483f76ccca41d9930731ac132150384d1adf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:30 +0000

    Add package gsmsapi/0.10/gsmsapi-0.10.tar.gz

---
+++ packages/gsmsapi/gsmsapi-0.10.tar.gz/gsmsapi-0.10/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='gsmsapi',
+	version='0.10',
+	description='SMS API for (german) SMS providers',
+	author='Torge Szczepanek',
+	author_email='debian@cygnusnetworks.de',
+	maintainer='Torge Szczepanek',
+	maintainer_email='debian@cygnusnetworks.de',
+	license='MIT',
+	packages=['gsmsapi'],
+	url = 'https://github.com/CygnusNetworks/python-gsmsapi',
+	download_url = 'https://github.com/CygnusNetworks/python-gsmsapi/tarball/v0.10',
+	keywords = ["sms", "german", "sipgate", "smstrade", "api"],
+)
