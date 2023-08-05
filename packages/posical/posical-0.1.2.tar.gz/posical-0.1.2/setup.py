120b0739b2c71fda73de8ec1415d40198a03e261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:33 +0000

    Add package posical/0.1.2/posical-0.1.2.tar.gz

---
+++ packages/posical/posical-0.1.2.tar.gz/posical-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'posical',
+  py_modules = ['posical'],
+  version = '0.1.2',
+  description = 'Calendar reform for Python',
+  author = 'Z. D. Smith',
+  author_email = 'zd@zdsmith.com',
+  url = 'https://github.com/subsetpark/posical',
+  download_url = 'https://github.com/subsetpark/posical/releases/tag/0.1',
+  install_requires = 'nonzero',
+  keywords = ['calendar', 'comte', 'date', 'humanism'],
+  classifiers = [],
+)
