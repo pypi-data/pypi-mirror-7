ab71c2d6204ece51ce73ce668688fd3b33e955e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:02 +0000

    Add package webhooks/0.4.0/webhooks-0.4.0.tar.gz

---
+++ packages/webhooks/webhooks-0.4.0.tar.gz/webhooks-0.4.0/HISTORY.rst
@@ -0,0 +1,44 @@
+.. :changelog:
+
+History
+-------
+
+0.4.0 (2014-05-20)
+++++++++++++++++++
+
+* Replaced `utils.encoders` with `json262` package.
+* utf-8 encoding everywhere
+* Add `from __future__ import absolute_import` everywhere.
+
+0.3.2 (2014-05-17)
+++++++++++++++++++
+
+* Brought in simplified cached_property decorator
+
+
+0.3.1 (2014-05-15)
+++++++++++++++++++
+
+* Added more Senderable attributes to make it easier to track what's going on.
+* Added the missing webhooks.sender package to setup.py.
+
+
+0.3.0 (2014-05-14)
+++++++++++++++++++
+
+* Added extensible Senderable class to expedite creating new senders.
+* Added async_redis sender.
+* Added travis-ci.
+
+0.2.0 (2014-05-13)
+++++++++++++++++++
+
+* Added functioning hook decorator.
+* Ramped up test coverage.
+* Hash functions placed in their own module.
+* Cleaned up JSON encoder thanks to Audrey Roy Greenfeld!
+
+0.1.0 (2014-05-07)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/webhooks/webhooks-0.4.0.tar.gz/webhooks-0.4.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "0.4.0"
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel upload')
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='webhooks',
+    version=version,
+    description='Python + Webhooks mMade Easy',
+    long_description=readme + '\n\n' + history,
+    author='Daniel Greenfeld',
+    author_email='pydanny@gmail.com',
+    url='https://github.com/pydanny/webhooks',
+    packages=[
+        'webhooks',
+        'webhooks.senders'
+    ],
+    package_dir={'webhooks':
+                 'webhooks'},
+    include_package_data=True,
+    install_requires=[
+        'cached-property>=0.1.2',
+        'json262==0.2.0',
+        'requests>=2.2.1',
+        'wrapt'
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='webhooks',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
