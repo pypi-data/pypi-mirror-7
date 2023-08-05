3fdfd3918741d60d1d08aa7d54438e2b3b7a66e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package ablog/0.1/ablog-0.1.tar.gz

---
+++ packages/ablog/ablog-0.1.tar.gz/ablog-0.1/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+
+__version__ = ''
+with open('ablog/__init__.py') as inp:
+  for line in inp:
+      if (line.startswith('__version__')):
+          exec(line.strip())
+          break
+with open('README.rst') as inp:
+    long_description = inp.read()
+
+
+setup(
+    name='ablog',
+    version=__version__,
+    author='Ahmet Bakan',
+    author_email='lordnapi@gmail.com',
+    description='ABlog for blogging with Sphinx',
+    long_description=long_description,
+    url='http://github.com/abakan/ablog',
+    packages=['ablog'],
+    package_dir={'ablog': 'ablog'},
+    package_data={'ablog': ['templates/*.html']},
+    license='MIT License',
+    keywords=('Sphinx, extension, blogging, atom feeds'),
+    classifiers=[
+                 'Development Status :: 3 - Alpha',
+                 'Topic :: Software Development :: Documentation',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 3',
+                ],
+    provides=['ablog ({0:s})'.format(__version__)],
+    requires=['Werkzeug', 'Sphinx'],
+)
