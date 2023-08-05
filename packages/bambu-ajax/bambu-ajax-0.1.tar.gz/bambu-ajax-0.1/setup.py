b3232bb7b371275dac353cae22e801fc7c40ed61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package bambu-ajax/0.1/bambu-ajax-0.1.tar.gz

---
+++ packages/bambu-ajax/bambu-ajax-0.1.tar.gz/bambu-ajax-0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name = 'bambu-ajax',
+	version = '0.1',
+	description = 'AJAX utility functions for Django',
+	author = 'Steadman',
+	author_email = 'mark@steadman.io',
+	url = 'http://pypi.python.org/pypi/bambu-ajax',
+	namespace_packages = ['bambu'],
+	packages = [
+		'bambu.ajax',
+		'bambu.ajax.templatetags'
+	],
+	package_data = {
+		'bambu.ajax': [
+			'templates/ajax/utils.js'
+		]
+	},
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Web Environment',
+		'Framework :: Django'
+	]
+)
