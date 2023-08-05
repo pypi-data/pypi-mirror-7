56df2d9a48d110fd98a49d6a1b6dc2aeea221e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:38 +0000

    Add package django_backstage/0.0.74/django_backstage-0.0.74.tar.gz

---
+++ packages/django_backstage/django_backstage-0.0.74.tar.gz/django_backstage-0.0.74/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_backstage',
+    version='0.0.74',
+    description='Django project and site deployment using uWSGI, nginx, etc.',
+    author='MiddleFork',
+    author_email='walker@mfgis.com',
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires=[
+        "setuptools_git >= 0.3",
+    ],
+    entry_points = {
+      'console_scripts':
+            'backstage_project = backstage.project:new_project'
+    }
+)
