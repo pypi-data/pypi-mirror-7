ba8d08984b329a03ab254b3d581666c945f2742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package ansible-vagrant/1.1.1/ansible-vagrant-1.1.1.tar.gz

---
+++ packages/ansible-vagrant/ansible-vagrant-1.1.1.tar.gz/ansible-vagrant-1.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='ansible-vagrant',
+      version='1.1.1',
+      description='Simple helper to use ansible with vagrant',
+      long_description=open('README.rst').read(),
+      url='https://github.com/michaelcontento/ansible-vagrant',
+      author='Michael Contento',
+      author_email='michaelcontento@gmail.com',
+      license='GPLv3',
+      install_requires=['ansible'],
+      keywords=['vagrant', 'ansible'],
+      scripts=[
+          'bin/ansible-vagrant-update-hosts',
+          'bin/ansible-playbook-vagrant',
+          'bin/ansible-playbookv',
+          'bin/ansible-vagrant',
+          'bin/ansiblev',
+      ],
+      classifiers=[
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: System :: Clustering',
+          'Topic :: System :: Software Distribution',
+          'Topic :: System :: Systems Administration',
+          'Topic :: Utilities'
+      ],
+)
