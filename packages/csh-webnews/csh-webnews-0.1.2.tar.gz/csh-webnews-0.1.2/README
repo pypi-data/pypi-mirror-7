66f6e1cf1e9e28c6a7c6e283f07125f00e528fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package csh-webnews/0.1.2/csh-webnews-0.1.2.tar.gz

---
+++ packages/csh-webnews/csh-webnews-0.1.2.tar.gz/csh-webnews-0.1.2/README
@@ -0,0 +1,4 @@
+pywebnews
+=========
+
+A python wrapper for CSH webnews. Examples and documentation to come.
---
+++ packages/csh-webnews/csh-webnews-0.1.2.tar.gz/csh-webnews-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup( name             = 'csh-webnews'
+	 , version          = '0.1.2'
+	 , description      = 'A CSH webnews API wrapper'
+	 , author           = 'Matt Gambogi'
+	 , author_email     = 'matt.gambogi@gmail.com'
+	 , url              = 'http://www.github.com/gambogi/pywebnews'
+	 , py_modules       = ['csh_webnews']
+	 , license          = 'MIT'
+	 , install_requires = ['requests']
+	 )
