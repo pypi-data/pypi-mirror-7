a5708fb8b025e196ca06b58d791b54d7209c867e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:09 +0000

    Add package tgext.admin/0.6.4/tgext.admin-0.6.4.tar.gz

---
+++ packages/tgext.admin/tgext.admin-0.6.4.tar.gz/tgext.admin-0.6.4/README.txt
@@ -0,0 +1,25 @@
+Introduction
+============
+
+Changelog
+===========
+
+0.6.0
+------
+Support for Layouts in TGAdminConfig with Bootstrap3 Layout
+
+0.3
+----
+Lookup now uses ProviderSelector, for better mongo support
+
+0.2.6
+-----
+Bugfix release.  Support for _method fields in TG2.1
+
+0.2.5
+------
+* Addition of defaultCrudRestController to
+CRCConfig allows one to customize the controller
+that the admin uses.
+
+* Fixes tgadminconfig to override the Password properly for Users
---
+++ packages/tgext.admin/tgext.admin-0.6.4.tar.gz/tgext.admin-0.6.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6.4'
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'docs/HISTORY.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+setup(name='tgext.admin',
+      version=version,
+      description="Admin Controller add-on for basic TG identity model.",
+      long_description=README + "\n" +
+                       CHANGES,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='turbogears2.extension, TG2, TG, sprox, Rest, internet, admin',
+      author='Christopher Perkins',
+      author_email='chris@percious.com',
+      url='https://github.com/TurboGears/tgext.admin',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['tgext'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'tgext.crud>=0.7.1',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
