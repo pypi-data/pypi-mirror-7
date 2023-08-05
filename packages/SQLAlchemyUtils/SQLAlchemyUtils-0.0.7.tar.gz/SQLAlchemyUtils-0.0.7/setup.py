aa25740edc2fb94dcfb1bbe38f4c07d7bf88a242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:13 +0000

    Add package SQLAlchemyUtils/0.0.7/SQLAlchemyUtils-0.0.7.tar.gz

---
+++ packages/SQLAlchemyUtils/SQLAlchemyUtils-0.0.7.tar.gz/SQLAlchemyUtils-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '0.0.7'
+
+setup_args = dict(
+    name='SQLAlchemyUtils',
+    description='Useful utilities for working with SQLAlchemy.',
+    url='https://github.com/tylertreat/SQLAlchemyUtils',
+    version=VERSION,
+    license='Apache',
+    packages=find_packages(),
+    include_package_data=True,
+    author='Tyler Treat',
+    author_email='ttreat31@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
+
