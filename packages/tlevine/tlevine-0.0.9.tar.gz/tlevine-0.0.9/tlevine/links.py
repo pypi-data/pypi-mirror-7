857457f385ef5d756d71b80cec90727b7ac1a069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:21 +0000

    Add package tlevine/0.0.9/tlevine-0.0.9.tar.gz

---
+++ packages/tlevine/tlevine-0.0.9.tar.gz/tlevine-0.0.9/tlevine/links.py
@@ -0,0 +1,77 @@
+# -*- encoding: utf-8 -*-
+import os
+import re
+import sys
+import itertools
+import json
+try:
+    import xmlrpclib as xmlrpc_client
+except ImportError:
+    import xmlrpc.client as xmlrpc_client
+
+import lxml.html
+from picklecache import cache
+import requests
+
+if sys.version > '3':
+    # Some lock object appeared, so caching is only supported in Python3
+    # Maybe caching should be disabled altogether anyway...
+    get = cache(os.path.expanduser('~/.tlevine'))(requests.get)
+else:
+    get = requests.get
+
+def github(username):
+    def pages():
+        for page_number in itertools.count(1):
+            response = get('https://api.github.com/users/%s/repos?page=%d' % (username, page_number))
+            yield json.loads(response.text)
+
+    for page in itertools.takewhile(lambda r: r != [], pages()):
+        for repository in page:
+            yield repository['html_url']
+
+def thomaslevine():
+    url = 'http://thomaslevine.com/!/'
+    response = get(url)
+    html = lxml.html.fromstring(response.text)
+    html.make_links_absolute(url)
+    return (str(link) for link in html.xpath('//a/@href') if link.startswith(url))
+
+def scraperwiki(url = 'https://classic.scraperwiki.com/profiles/tlevine/index.html'):
+    response = get(url)
+    html = lxml.html.fromstring(response.text)
+    html.make_links_absolute(url)
+    for href in html.xpath('//li[@class="code_object_line"]/descendant::h3/a[position()=2]/@href'):
+        yield re.sub(r'index.html$', '', str(href))
+    nexts = html.xpath(u'//a[text()="Next »"]/@href')
+    if nexts != []:
+        for scraper in scraperwiki(nexts[0]):
+            yield scraper
+
+def gitorious():
+    url = 'https://gitorious.org/tlevine.xml'
+    response = get(url)
+    project = lxml.html.fromstring(response.text.encode('utf-8'))
+    project.make_links_absolute(url)
+    for repository in project.xpath('//repository[owner[text()="tlevine"]]'):
+        yield 'https' + repository.xpath('clone_url/text()')[0][3:-4]
+
+def pypi():
+    client = xmlrpc_client.ServerProxy('http://pypi.python.org/pypi')
+    for role, package in client.user_packages('tlevine'):
+        yield 'https://pypi.python.org/pypi/%s' % package
+
+def manual():
+    return [
+        'https://chrome.google.com/webstore/detail/simple-webcam/cejgmnpegppdhkmmgmdobfelcdgfhkmo?hl=en',
+    ]
+
+def main(fp = sys.stdout):
+    iter_github = list(map(github, ['tlevine', 'csv', 'csvsoundsystem', 'appgen', 'risley', 'mapshit']))
+    iter_other  = [gitorious(), scraperwiki(), thomaslevine(), manual()]
+    args = iter_other + iter_github
+    for link in itertools.chain(*args):
+        try:
+            fp.write('%s\n' % link)
+        except BrokenPipeError:
+            break
