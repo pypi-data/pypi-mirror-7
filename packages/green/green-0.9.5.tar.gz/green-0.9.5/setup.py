46c01f115433a5db7079d966605ebd0853946a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package green/0.9.5/green-0.9.5.tar.gz

---
+++ packages/green/green-0.9.5.tar.gz/green-0.9.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+import sys
+
+version = open(os.path.join(os.path.dirname(__file__), 'green', 'VERSION')).read().strip()
+
+setup(
+    name = 'green',
+    packages = ['green'],
+    data_files = [('green', ['green/VERSION'])],
+    version = version,
+    install_requires = [
+        'python-termstyle'
+        ],
+    entry_points = {
+        'console_scripts' : [
+            'green = green:main',
+            'green%d = green:main' % sys.version_info[:1],     # green2 or green3
+            'green-%d.%d = green:main' % sys.version_info[:2], # green-3.4 etc.
+            ],
+    },
+    description = 'Green provides clean output for Python unit tests.  Compare it to nose, nose2, or trial.',
+    long_description = open('README.md').read(),
+    author = 'Nathan Stocks',
+    author_email = 'nathan.stocks@gmail.com',
+    license = 'MIT',
+    url = 'https://github.com/CleanCut/green',
+    download_url = 'https://github.com/CleanCut/green/tarball/' + version,
+    keywords = ['nose', 'nose2', 'trial', 'green', 'test', 'tests', 'functional test', 'system test', 'unit test', 'unittest', 'color', 'tabular', 'clean', 'red', 'rednose'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities'],
+)
