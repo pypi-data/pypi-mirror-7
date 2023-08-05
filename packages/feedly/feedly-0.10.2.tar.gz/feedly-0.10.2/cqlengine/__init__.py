246506e01330ba810ddb2b06b820e6216522f524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:39 +0000

    Add package feedly/0.10.2/feedly-0.10.2.tar.gz

---
+++ packages/feedly/feedly-0.10.2.tar.gz/feedly-0.10.2/cqlengine/__init__.py
@@ -0,0 +1,8 @@
+from cqlengine.columns import *
+from cqlengine.functions import *
+from cqlengine.models import Model
+from cqlengine.query import BatchQuery
+
+# compaction
+SizeTieredCompactionStrategy = "SizeTieredCompactionStrategy"
+LeveledCompactionStrategy = "LeveledCompactionStrategy"
