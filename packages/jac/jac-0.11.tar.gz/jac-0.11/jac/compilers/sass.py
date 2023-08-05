ec538cf649edac89d8cecabefea5089df4a7917c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:46 +0000

    Add package jac/0.11/jac-0.11.tar.gz

---
+++ packages/jac/jac-0.11.tar.gz/jac-0.11/jac/compilers/sass.py
@@ -0,0 +1,27 @@
+import subprocess
+
+from . import CompilerMeta
+
+
+class SassCompiler(object):
+    __metaclass__ = CompilerMeta
+    supported_mimetypes = ['text/sass', 'text/scss']
+
+    @classmethod
+    def compile(cls, what, mimetype='text/sass', cwd=None,
+                uri_cwd=None, debug=None):
+        args = ['sass', '-s']
+        if mimetype == 'text/scss':
+            args.append('--scss')
+
+        if cwd:
+            args += ['-I', cwd]
+
+        handler = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, cwd=None)
+
+        (stdout, stderr) = handler.communicate(input=what)
+        if handler.returncode == 0:
+            return stdout
+        else:
+            raise RuntimeError('Test this :S %s' % stderr)
