e19b1e8df4d60c3042551dd0a0a365bdaf3c3a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:16 +0000

    Add package kegbot/0.9.26/kegbot-0.9.26.tar.gz

---
+++ packages/kegbot/kegbot-0.9.26.tar.gz/kegbot-0.9.26/pykeg/web/api/api_test.py
@@ -0,0 +1,340 @@
+# Copyright 2014 Bevbot LLC, All Rights Reserved
+#
+# This file is part of the Pykeg package of the Kegbot project.
+# For more information on Pykeg or Kegbot, see http://kegbot.org/
+#
+# Pykeg is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pykeg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unittests for pykeg.web.api"""
+
+from django.core import mail
+from django.core.urlresolvers import reverse
+from django.test import TransactionTestCase
+from django.test.utils import override_settings
+from pykeg.core import models
+from pykeg.core import defaults
+from kegbot.util import kbjson
+
+import os
+TESTDATA_DIR = os.path.join(os.path.dirname(__file__), 'testdata/')
+
+### Helper methods
+
+
+def create_site():
+    return defaults.set_defaults(set_is_setup=True, create_controller=True)
+
+
+def get_filename(f):
+    return os.path.join(TESTDATA_DIR, f)
+
+
+class BaseApiTestCase(TransactionTestCase):
+    def get(self, subpath, data={}, follow=False, **extra):
+        response = self.client.get('/api/%s' % subpath, data=data, follow=follow,
+            **extra)
+        return response, kbjson.loads(response.content)
+
+    def post(self, subpath, data={}, follow=False, **extra):
+        response = self.client.post('/api/%s' % subpath, data=data, follow=follow,
+            **extra)
+        return response, kbjson.loads(response.content)
+
+
+class ApiClientNoSiteTestCase(BaseApiTestCase):
+    def testNotSetUp(self):
+        '''Api endpoints should all error out prior to site setup.'''
+
+        endpoints = ('events/', 'taps/', 'kegs/', 'drinks/')
+        for endpoint in endpoints:
+            response, data = self.get(endpoint)
+            self.assertEquals(data.meta.result, 'error')
+            self.assertEquals(data.error.code, 'BadRequestError')
+
+        create_site()
+
+        # Ordinary results expected after site installed.
+        for endpoint in endpoints:
+            response, data = self.get(endpoint)
+            self.assertEquals(data.meta.result, 'ok')
+
+
+class ApiClientTestCase(BaseApiTestCase):
+    def setUp(self):
+        self.site = create_site()
+        self.admin = models.User.objects.create(username='admin', is_staff=True)
+        self.admin.set_password('testpass')
+        self.admin.save()
+
+        self.normal_user = models.User.objects.create(username='normal_user', is_staff=True)
+        self.normal_user.set_password('testpass')
+        self.normal_user.save()
+
+        self.apikey = models.ApiKey.objects.create(user=self.admin, key='123')
+        self.bad_apikey = models.ApiKey.objects.create(user=self.normal_user, key='456')
+
+    def testSiteDefaults(self):
+        empty_endpoints = ('events/', 'kegs/')
+        for endpoint in empty_endpoints:
+            response, data = self.get(endpoint)
+            self.assertEquals(data.objects, [])
+
+        response, data = self.get('taps/')
+        taps = data.objects
+        self.assertEquals(2, len(taps))
+        self.assertEquals('Main Tap', taps[0].name)
+        self.assertEquals('kegboard.flow0', taps[0].meter_name)
+        self.assertEquals('Second Tap', taps[1].name)
+        self.assertEquals('kegboard.flow1', taps[1].meter_name)
+
+        for tap in taps:
+            response1, data1 = self.get('taps/%s' % tap.meter_name)
+            self.assertEquals(data1.meta.result, 'ok')
+
+            response2, data2 = self.get('taps/%s' % tap.id)
+            self.assertEquals(data2.meta.result, 'ok')
+
+            self.assertEquals(data1, data2)
+
+    def testApiAccess(self):
+        endpoint = 'users/'
+
+        # No API key.
+        response, data = self.get(endpoint)
+        self.assertEquals(data.meta.result, 'error')
+        self.assertEquals(data.error.code, 'NoAuthTokenError')
+
+        # Non-existent key.
+        response, data = self.get(endpoint, HTTP_X_KEGBOT_API_KEY='foobar')
+        self.assertEquals(data.meta.result, 'error')
+        self.assertEquals(data.error.code, 'BadApiKeyError')
+
+        # Key exists, non-superuser.
+        response, data = self.get(endpoint, HTTP_X_KEGBOT_API_KEY=self.bad_apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+
+        # Finally ok.
+        response, data = self.get(endpoint, HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+
+        endpoint = 'events/'
+
+        # Alter privacy and compare.
+        response, data = self.get(endpoint)
+        self.assertEquals(data.meta.result, 'ok')
+        self.site.privacy = 'members'
+        self.site.save()
+
+        response, data = self.get(endpoint)
+        self.assertEquals(data.meta.result, 'error')
+
+        self.client.login(username='admin', password='testpass')
+        response, data = self.get(endpoint)
+        self.assertEquals(data.meta.result, 'ok')
+
+        # Alert to staff-only.
+        self.site.privacy = 'staff'
+        self.site.save()
+        response, data = self.get(endpoint)
+        self.assertEquals(data.meta.result, 'ok')
+
+        self.client.logout()
+        response, data = self.get(endpoint)
+        self.assertEquals(data.meta.result, 'error')
+        self.assertEquals(data.error.code, 'NoAuthTokenError')
+
+        # Login endpoint works despite privacy settings
+        for ep in ('login', 'login/', 'v1/login', 'v1/login/'):
+            response, data = self.post(ep, data={'username': 'admin', 'password': 'testpass'})
+            self.assertEquals(data.meta.result, 'ok')
+
+    def test_record_drink(self):
+        response, data = self.get('taps/1')
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(data.object.get('current_keg'), None)
+
+        new_keg_data = {
+            'keg_size': 'half-barrel',
+            'beverage_name': 'Test Brew',
+            'producer_name': 'Test Producer',
+            'style_name': 'Test Style,'
+        }
+        response, data = self.post('taps/1/activate', data=new_keg_data)
+        self.assertEquals(data.meta.result, 'error')
+        self.assertEquals(data.error.code, 'NoAuthTokenError')
+
+        response, data = self.post('taps/1/activate', data=new_keg_data,
+            HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertIsNotNone(data.object.get('current_keg'))
+
+        response, data = self.post('taps/1', data={'ticks': 1000})
+        self.assertEquals(data.meta.result, 'error')
+        self.assertEquals(data.error.code, 'NoAuthTokenError')
+
+        response, data = self.post('taps/1', HTTP_X_KEGBOT_API_KEY=self.apikey.key,
+            data={'ticks': 1000})
+        self.assertEquals(data.meta.result, 'ok')
+
+    @override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+    @override_settings(EMAIL_FROM_ADDRESS='test-from@example')
+    def test_registration(self):
+        response, data = self.post('new-user/', data={'username': 'newuser', 'email': 'foo@example.com'})
+        self.assertEquals(data.meta.result, 'error')
+        self.assertEquals(data.error.code, 'NoAuthTokenError')
+
+        self.assertEquals(0, len(mail.outbox))
+
+        response, data = self.post('new-user/', data={'username': 'newuser', 'email': 'foo@example.com'},
+            HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(1, len(mail.outbox))
+
+        msg = mail.outbox[0]
+        self.assertEquals('[My Kegbot] Complete your registration', msg.subject)
+        self.assertEquals(['foo@example.com'], msg.to)
+        self.assertEquals('test-from@example', msg.from_email)
+
+        # Simulate clicking on the activation link.
+        user = models.User.objects.get(username='newuser')
+        self.assertIsNotNone(user.activation_key)
+        activation_url = reverse('activate-account', args=(),
+            kwargs={'activation_key': user.activation_key})
+        self.client.logout()
+        response = self.client.get(activation_url)
+        self.assertContains(response, 'Choose a Password', status_code=200)
+
+    def test_pictures(self):
+        kbsite = models.KegbotSite.get()
+        kbsite.hostname = 'localhost:123'
+        kbsite.use_ssl = True
+        kbsite.save()
+
+        image_data = open(get_filename('test_image_800x600.png'))
+        response, data = self.post('pictures/', data={'photo': image_data}, HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        print data
+        self.assertEquals(data.meta.result, 'ok')
+
+        picture = data['object']
+        picture_url = picture['url']
+        self.assertTrue(picture_url.startswith('https://localhost:123/media/'))
+
+    def test_controller_data(self):
+        for endpoint in ('controllers', 'flow-meters'):
+            response, data = self.get(endpoint)
+            self.assertEquals(data.meta.result, 'error')
+            self.assertEquals(data.error.code, 'NoAuthTokenError')
+
+        response, data = self.get('controllers', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        expected = {
+            'objects': [
+                {
+                    'id': 1,
+                    'name': 'kegboard',
+                }
+            ],
+            'meta': {
+                'result': 'ok',
+            }
+        }
+        self.assertEquals(expected, data)
+
+        response, data = self.get('flow-meters', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        expected = {
+            'objects': [
+                {
+                    'id': 1,
+                    'ticks_per_ml': 5.4,
+                    'port_name': 'flow0',
+                    'controller': {
+                        'id': 1,
+                        'name': 'kegboard',
+                    },
+                    'name': 'kegboard.flow0'
+                },
+                {
+                    'id': 2,
+                    'ticks_per_ml': 5.4,
+                    'port_name': 'flow1',
+                    'controller': {
+                        'id': 1,
+                        'name': 'kegboard'
+                    },
+                    'name': 'kegboard.flow1'
+                },
+            ],
+            'meta': {'result': 'ok'}
+        }
+        self.assertEquals(expected, data)
+
+        response, data = self.get('flow-toggles', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        expected = {
+            'objects': [
+                {
+                    'id': 1,
+                    'port_name': 'relay0',
+                    'controller': {
+                        'id': 1,
+                        'name': 'kegboard',
+                    },
+                    'name': 'kegboard.relay0'
+                },
+                {
+                    'id': 2,
+                    'port_name': 'relay1',
+                    'controller': {
+                        'id': 1,
+                        'name': 'kegboard'
+                    },
+                    'name': 'kegboard.relay1'
+                },
+            ],
+            'meta': {'result': 'ok'}
+        }
+        self.assertEquals(expected, data)
+
+    def test_add_remove_meters(self):
+        response, data = self.get('taps/1', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(data.object.meter.id, 1)
+        original_data = data
+
+        response, data = self.post('taps/1/disconnect-meter', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(data.object.get('meter'), None)
+
+        response, data = self.post('taps/1/connect-meter', HTTP_X_KEGBOT_API_KEY=self.apikey.key,
+            data={'meter': 1})
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(data.object.meter.id, 1)
+        self.assertEquals(original_data, data)
+
+    def test_add_remove_toggles(self):
+        response, data = self.get('taps/1', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(data.object.toggle.id, 1)
+
+        response, data = self.post('taps/1/disconnect-toggle', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertEquals(data.object.get('toggle'), None)
+
+        response, data = self.post('taps/1/connect-toggle', HTTP_X_KEGBOT_API_KEY=self.apikey.key,
+            data={'toggle': 1})
+        response, data = self.get('taps/1', HTTP_X_KEGBOT_API_KEY=self.apikey.key)
+        self.assertEquals(data.meta.result, 'ok')
+        self.assertIsNotNone(data.object.get('toggle'))
+        self.assertEquals(data.object.toggle.id, 1)
---
+++ packages/kegbot/kegbot-0.9.26.tar.gz/kegbot-0.9.26/pykeg/web/middleware.py
@@ -0,0 +1,226 @@
+# Copyright 2014 Bevbot LLC, All Rights Reserved
+#
+# This file is part of the Pykeg package of the Kegbot project.
+# For more information on Pykeg or Kegbot, see http://kegbot.org/
+#
+# Pykeg is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pykeg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pykeg.  If not, see <http://www.gnu.org/licenses/>.
+
+from pykeg import EPOCH
+
+from pykeg.backend import get_kegbot_backend
+from pykeg.core import models
+from pykeg.web.api.util import is_api_request
+
+from pykeg.plugin import util as plugin_util
+
+from django.conf import settings
+from django.contrib import messages
+from django.core.exceptions import SuspiciousOperation
+from django.http import HttpResponse
+from django.http import HttpResponseServerError
+from django.template.response import SimpleTemplateResponse
+from django.template import RequestContext
+from django.utils import timezone
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+# TODO(mikey): rename me
+ALLOWED_PATHS = (
+    '/api/login/',
+    '/api/get-api-key/',
+    '/account/activate/',
+    '/accounts/login/',
+    '/admin/',
+    '/media/',
+    '/setup/',
+)
+
+
+def _path_allowed(path, kbsite):
+    for p in ALLOWED_PATHS:
+        if path.startswith(p):
+            return True
+    return False
+
+
+class KegbotSiteMiddleware:
+    ALLOWED_VIEW_MODULE_PREFIXES = (
+        'pykeg.web.setup_wizard.',
+    )
+
+    def process_request(self, request):
+        request.need_setup = False
+        request.need_upgrade = False
+        request.kbsite = None
+
+        # Select only the `epoch` column, as pending database migrations could
+        # make a full select crash.
+        rows = models.KegbotSite.objects.filter(name='default').values('epoch')
+        if not rows:
+            request.need_setup = True
+        elif rows[0].get('epoch', 0) < EPOCH:
+            request.need_upgrade = True
+        else:
+            request.kbsite = models.KegbotSite.objects.get(name='default')
+            if request.kbsite.is_setup:
+                timezone.activate(request.kbsite.timezone)
+                request.plugins = dict((p.get_short_name(), p) for p in plugin_util.get_plugins().values())
+            else:
+                request.need_setup = True
+
+        request.backend = get_kegbot_backend()
+
+        return None
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        for prefix in self.ALLOWED_VIEW_MODULE_PREFIXES:
+            if view_func.__module__.startswith(prefix):
+                return None
+
+        if is_api_request(request):
+            # API endpoints handle "setup required" differently.
+            return None
+
+        if request.need_setup:
+            return self._setup_required(request)
+        elif request.need_upgrade:
+            return self._upgrade_required(request)
+
+        return None
+
+    def _setup_required(self, request):
+        if settings.EMBEDDED:
+            return HttpResponseServerError('Site is not set up.', content_type='text/plain')
+        return SimpleTemplateResponse('setup_wizard/setup_required.html',
+            context=RequestContext(request), status=403)
+
+    def _upgrade_required(self, request, current_epoch=None):
+        if settings.EMBEDDED:
+            return HttpResponseServerError('Site needs upgrade.', content_type='text/plain')
+        context = RequestContext(request)
+        context['current_epoch'] = current_epoch
+        return SimpleTemplateResponse('setup_wizard/upgrade_required.html',
+            context=context, status=403)
+
+
+class SiteActiveMiddleware:
+    """Middleware which throws 503s when KegbotSite.is_active is false."""
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if not hasattr(request, 'kbsite') or not request.kbsite:
+            return None
+        kbsite = request.kbsite
+
+        # We have a KegbotSite, and that site is active: nothing to do.
+        if kbsite.is_active:
+            return None
+
+        # If the request is for a whitelisted path, allow it.
+        if _path_allowed(request.path, kbsite):
+            return None
+
+        # Allow staff/superusers access if inactive.
+        if request.user.is_staff or request.user.is_superuser:
+            return None
+
+        return HttpResponse('Site temporarily unavailable', status=503)
+
+
+class HttpHostMiddleware:
+    """Middleware which checks a dynamic version of settings.ALLOWED_HOSTS."""
+
+    def process_request(self, request):
+        if not getattr(request, 'kbsite', None):
+            return None
+
+        host = request.get_host()
+        allowed_hosts_str = request.kbsite.allowed_hosts
+        if allowed_hosts_str:
+            host_patterns = allowed_hosts_str.strip().split()
+        else:
+            host_patterns = []
+        valid = HttpHostMiddleware.validate_host(host, host_patterns)
+
+        if not valid:
+            message = "Invalid HTTP_HOST header (you may need to change Kegbot's ALLOWED_HOSTS setting): %s" % host
+            if request.user.is_superuser or request.user.is_staff:
+                messages.warning(request, message)
+            else:
+                raise SuspiciousOperation(message)
+
+    @classmethod
+    def validate_host(cls, host, allowed_hosts):
+        """Clone of django.http.request.validate_host.
+
+        Local differences: treats an empty `allowed_hosts` list as a pass.
+        """
+        # Validate only the domain part.
+        if not allowed_hosts:
+            return True
+
+        if host[-1] == ']':
+                # It's an IPv6 address without a port.
+            domain = host
+        else:
+            domain = host.rsplit(':', 1)[0]
+
+        for pattern in allowed_hosts:
+            pattern = pattern.lower()
+            match = (
+                pattern == '*' or
+                pattern.startswith('.') and (
+                    domain.endswith(pattern) or domain == pattern[1:]
+                ) or
+                pattern == domain
+            )
+            if match:
+                return True
+
+        return False
+
+
+class PrivacyMiddleware:
+    """Enforces site privacy settings.
+
+    Must be installed after ApiRequestMiddleware (in request order) to
+    access is_kb_api_request attribute.
+    """
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if not hasattr(request, 'kbsite'):
+            return None
+        elif _path_allowed(request.path, request.kbsite):
+            return None
+        elif request.is_kb_api_request:
+            # api.middleware will enforce access requirements.
+            return None
+
+        privacy = request.kbsite.privacy
+
+        if privacy == 'public':
+            return None
+        elif privacy == 'staff':
+            if not request.user.is_staff:
+                return SimpleTemplateResponse('kegweb/staff_only.html',
+                    context=RequestContext(request), status=401)
+            return None
+        elif privacy == 'members':
+            if not request.user.is_authenticated or not request.user.is_active:
+                return SimpleTemplateResponse('kegweb/members_only.html',
+                    context=RequestContext(request), status=401)
+            return None
+
+        return HttpResponse('Server misconfigured, unknown privacy setting:%s' % privacy, status=500)
