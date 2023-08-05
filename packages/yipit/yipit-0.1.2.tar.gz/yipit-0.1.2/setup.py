3a0fe4753d8db035e7f4c92436df07ee8a36a290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package yipit/0.1.2/yipit-0.1.2.tar.gz

---
+++ packages/yipit/yipit-0.1.2.tar.gz/yipit-0.1.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import re
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='yipit',
+        version="0.1.2",
+        description="Yipit",
+        author=u'Andrew Gross',
+        author_email=u'andrew@yipit.com',
+        url='http://example.com',
+        install_requires=[],
+        dependency_links=[],
+        packages=[],
+        include_package_data=True,
+    )
