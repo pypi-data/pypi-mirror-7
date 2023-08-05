34190a969e9d70c181172ddfe5be23868e9e2ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:26 +0000

    Add package invocations/0.6.0/invocations-0.6.0.tar.gz

---
+++ packages/invocations/invocations-0.6.0.tar.gz/invocations-0.6.0/invocations/testing.py
@@ -0,0 +1,20 @@
+from invoke import ctask as task
+
+
+@task(help={
+    'module': "Just runs tests/STRING.py.",
+    'runner': "Use STRING to run tests instead of 'spec'.",
+    'opts': "Extra flags for the test runner",
+})
+def test(ctx, module=None, runner=None, opts=None):
+    """
+    Run a Spec or Nose-powered internal test suite.
+    """
+    runner = runner or 'spec'
+    # Allow selecting specific submodule
+    specific_module = " --tests=tests/%s.py" % module
+    args = (specific_module if module else "")
+    if opts:
+        args += " " + opts
+    # Use pty so the spec/nose/Python process buffers "correctly"
+    ctx.run(runner + args, pty=True)
