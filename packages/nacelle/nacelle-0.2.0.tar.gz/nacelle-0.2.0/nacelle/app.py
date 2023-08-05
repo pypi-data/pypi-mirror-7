08b686a4a51ff2846ba3b71633de70eadba6d1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:56 +0000

    Add package nacelle/0.2.0/nacelle-0.2.0.tar.gz

---
+++ packages/nacelle/nacelle-0.2.0.tar.gz/nacelle-0.2.0/nacelle/app.py
@@ -0,0 +1,44 @@
+"""
+Main WSGI entry point to Nacelle
+"""
+# marty mc fly imports
+from __future__ import absolute_import
+
+# stdlib imports
+import itertools
+
+# third-party imports
+import webapp2
+
+# local imports
+from nacelle.conf import settings
+
+# use webapp2's import_string function to lazily import required modules for
+# WSGI config
+routes = webapp2.import_string(settings.ROUTES_MODULE)
+dispatcher = webapp2.import_string(settings.DISPATCHER_MODULE)
+error_handler = webapp2.import_string(settings.ERROR_HANDLER_MODULE)
+secret_key_store = webapp2.import_string('nacelle.core.sessions.models.SecretKey')
+
+# some of nacelle's contrib apps have routes, so we need to make sure and
+# append those to our routes list before passing it into the WSGI app
+mail_routes = webapp2.import_string('nacelle.contrib.mail.routes.ROUTES')
+routes = list(itertools.chain(routes, mail_routes))
+
+# Define our WSGI app so GAE can run it
+wsgi = webapp2.WSGIApplication(routes, debug=settings.DEBUG, config={
+    'webapp2_extras.sessions': {
+        'secret_key': secret_key_store.get_key(),
+    },
+    'webapp2_extras.jinja2': {
+        'globals': settings.JINJA_GLOBALS,
+    },
+    'webapp2_extras.i18n': {
+        'translations_path': settings.TRANSLATIONS_PATH,
+        'locale_selector': settings.TRANSLATIONS_SELECTOR,
+    },
+})
+
+# attach dispatcher and error_handler to the WSGI app
+wsgi.router.set_dispatcher(dispatcher)
+wsgi.error_handlers[500] = error_handler
---
+++ packages/nacelle/nacelle-0.2.0.tar.gz/nacelle-0.2.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='nacelle',
+    version='0.2.0',
+    description='A lightweight Python framework (built on top of webapp2) for use on Google Appengine',
+    long_description=open('README.rst').read(),
+    author='Patrick Carey',
+    author_email='patrick@rehabstudio.com',
+    url='https://github.com/nacelle/nacelle',
+    packages=[
+        'nacelle',
+    ],
+    package_dir={'nacelle':
+                 'nacelle'},
+    include_package_data=True,
+    license="MIT",
+    zip_safe=False,
+    keywords='nacelle',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+)
