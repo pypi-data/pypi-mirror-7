daab3bf6c7b22aad08d8d90cf4619a9fe9357cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package radical.pilot/0.10/radical.pilot-0.10.tar.gz

---
+++ packages/radical.pilot/radical.pilot-0.10.tar.gz/radical.pilot-0.10/src/radical/pilot/states.py
@@ -0,0 +1,85 @@
+#pylint: disable=C0301, C0103, W0212
+
+"""
+.. module:: radical.pilot.states
+   :platform: Unix
+   :synopsis: State constants.
+
+.. moduleauthor:: Ole Weidner <ole.weidner@rutgers.edu>
+"""
+
+__copyright__ = "Copyright 2013-2014, http://radical.rutgers.edu"
+__license__ = "MIT"
+
+import datetime
+
+# -----------------------------------------------------------------------------
+# The state "struct" encapsulates a state and its timestamp.
+class State(object):
+    __slots__ = ('state', 'timestamp')
+
+    # ----------------------------------------
+    #
+    def __init__(self, state, timestamp):
+        """Le constructeur.
+        """
+        self.state = state
+        self.timestamp = timestamp
+
+    # ----------------------------------------
+    #
+    def __eq__(self, other):
+        """Comparison operator for 'equal'. Compares 'State' as well 
+           as 'str' types.
+        """
+        if type(other) == str:
+            return self.state == other
+        else:
+            return self.state == other.state
+
+    # ----------------------------------------
+    #
+    def __ne__(self, other):
+        """Comparison operator for 'not equal'. Compares 'State' as well 
+           as 'str' types.
+        """
+        if type(other) == str:
+            return not self.__eq__(other)
+        else:
+            return not self.__eq(other.state)
+
+    # ----------------------------------------
+    #
+    def as_dict(self):
+        """Returns the state and its timestamp as a Python dictionary.
+        """
+        return {"state": self.state, "timestamp": self.timestamp}
+
+    # ----------------------------------------
+    #
+    def __str__(self):
+        """Returns the string representation of the state. The string 
+           representation is just the state as string without its timestamp.
+        """
+        return self.state
+
+
+# -----------------------------------------------------------------------------
+# States
+UNKNOWN                 = 'Unknown'
+NEW                     = 'New'
+
+PENDING                 = 'Pending'
+
+PENDING_EXECUTION       = 'PendingExecution'
+RUNNING                 = 'Running'
+
+PENDING_INPUT_TRANSFER  = 'PendingInputTransfer'
+TRANSFERRING_INPUT      = 'TransferringInput'
+
+PENDING_OUTPUT_TRANSFER = 'PendingOutputTransfer'
+TRANSFERRING_OUTPUT     = 'TransferringOutput'
+
+DONE                    = 'Done'
+CANCELED                = 'Canceled'
+FAILED                  = 'Failed'
