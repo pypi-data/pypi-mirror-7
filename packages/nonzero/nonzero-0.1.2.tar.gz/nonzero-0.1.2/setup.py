81df2c7693f94d82209e57a4f57fb8924ccaf21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:21 +0000

    Add package nonzero/0.1.2/nonzero-0.1.2.tar.gz

---
+++ packages/nonzero/nonzero-0.1.2.tar.gz/nonzero-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'nonzero',
+  packages = ['nonzero'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'A class to assist with doing non-zero math',
+  author = 'Z. D. Smith',
+  author_email = 'zd@zdsmith.com',
+  url = 'https://github.com/subsetpark/nonzero', # use the URL to the github repo
+  download_url = 'https://github.com/subsetpark/nonzero/releases/tag/0.1.2', # I'll explain this in a second
+  keywords = ['zero', 'year', 'date'], # arbitrary keywords
+  classifiers = [],
+)
