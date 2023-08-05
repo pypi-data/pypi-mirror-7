3c8fe5a8bd5216fd76b42781fece1129b83d6890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package rss2producer/0.1.1/rss2producer-0.1.1.tar.gz

---
+++ packages/rss2producer/rss2producer-0.1.1.tar.gz/rss2producer-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='rss2producer',
+    version='0.1.1',
+    description="Simplifies the process of creating an RSS 2.0 feed.",
+    author='Nathan Osman',
+    author_email='admin@quickmediasolutions.com',
+    url='https://github.com/nathan-osman/rss2producer',
+    license='MIT',
+    packages=['rss2producer'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
