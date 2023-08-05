059c3e824561bbf7a4969a94cffd8695466ca496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:37 +0000

    Add package hisparc-sapphire/0.9.12/hisparc-sapphire-0.9.12.tar.gz

---
+++ packages/hisparc-sapphire/hisparc-sapphire-0.9.12.tar.gz/hisparc-sapphire-0.9.12/sapphire/analysis/__init__.py
@@ -0,0 +1,26 @@
+"""Data analysis
+
+This package contains modules for performing various analysis tasks:
+
+:mod:`~sapphire.analysis.coincidence_queries`
+    Filter coincidences analysed with
+    :class:`~sapphire.analysis.coincidences.CoincidenceESD`
+
+:mod:`~sapphire.analysis.coincidences`
+    Search for coincidences between HiSPARC stations
+
+:mod:`~sapphire.analysis.core_reconstruction`
+    Shower core and shower size reconstruction
+
+:mod:`~sapphire.analysis.direction_reconstruction`
+    EAS direction reconstruction
+
+:mod:`~sapphire.analysis.landau`
+    Landau distribution
+
+:mod:`~sapphire.analysis.process_events`
+    Process HiSPARC events
+
+"""
+from direction_reconstruction import (DirectionReconstruction,
+                                      BinnedDirectionReconstruction)
