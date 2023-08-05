646fd905d381c2e509152675fb112b4da2b61b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:27 +0000

    Add package ciscoconfparse/1.1.0/ciscoconfparse-1.1.0.tar.gz

---
+++ packages/ciscoconfparse/ciscoconfparse-1.1.0.tar.gz/ciscoconfparse-1.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+import sys
+sys.path.insert(0,os.getcwd()+'/ciscoconfparse')
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='ciscoconfparse',
+      version=__import__("ciscoconfparse").__version__,
+      description='Parse, Audit, Query, Build, and Modify Cisco IOS-style configurations',
+      url='http://www.pennington.net/py/ciscoconfparse/',
+      author='David Michael Pennington',
+      author_email='mike /|at|\ pennington.net',
+      license='GPL',
+      platforms='any',
+      keywords='Parse audit query modify Cisco IOS configuration',
+      entry_points = "",
+      long_description=read('README.rst'),
+      include_package_data=True,
+      packages = find_packages(),
+      setup_requires=["setuptools_hg"],  # setuptools_hg must be installed as a python module
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Plugins',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Telecommunications Industry',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Communications',
+          'Topic :: Internet',
+          'Topic :: System :: Networking',
+          'Topic :: System :: Networking :: Monitoring',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+     )
