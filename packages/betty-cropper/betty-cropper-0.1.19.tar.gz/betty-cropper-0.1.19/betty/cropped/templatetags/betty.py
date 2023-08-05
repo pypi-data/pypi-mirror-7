b3e0e53c96a88ddf9482a4cce818d7523cc7fbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package betty-cropper/0.1.19/betty-cropper-0.1.19.tar.gz

---
+++ packages/betty-cropper/betty-cropper-0.1.19.tar.gz/betty-cropper-0.1.19/betty/cropped/templatetags/betty.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+
+from django import template
+from django.template.loader import select_template
+
+from betty.cropped.fields import ImageFieldFile, default_storage
+
+register = template.Library()
+
+
+class AnonymousImageField(object):
+
+    def __init__(self, id):
+        self.id = id
+        self.storage = default_storage
+
+    def get_crop_url(self, ratio="original", width=600, format="jpg"):
+        return self.storage.url(self.id, ratio=ratio, width=width, format=format)
+
+
+@register.simple_tag
+def cropped_url(image, ratio="original", width=600, format="jpg"):
+    if not isinstance(image, ImageFieldFile):
+        try:
+            image_id = int(image)
+        except:
+            raise
+            raise template.TemplateSyntaxError("\"{}\" is not a valid ImageField or image id".format(image))
+        image = AnonymousImageField(image_id)
+
+    return image.get_crop_url(ratio=ratio, width=width, format=format)
+
+
+@register.simple_tag(takes_context=True)
+def cropped(context, image, ratio="original", width=600, format="jpg"):
+    if not isinstance(image, ImageFieldFile):
+        try:
+            image_id = int(image)
+        except:
+            raise
+            raise template.TemplateSyntaxError("\"{}\" is not a valid ImageField or image id".format(image))
+        image = AnonymousImageField(image_id)
+
+    context["image"] = image
+    context["image_url"] = image.get_crop_url(ratio=ratio, width=width, format=format)
+    context["ratio"] = ratio
+    context["width"] = width
+    context["format"] = format
+
+    t = select_template(["betty/cropped.html", "betty/cropped_default.html"])
+    return t.render(context)
