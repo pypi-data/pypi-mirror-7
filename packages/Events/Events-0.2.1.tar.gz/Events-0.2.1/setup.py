edbdc4032cf90c99c3d3db7735eaa4a32da8ed77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:38 +0000

    Add package Events/0.2.1/Events-0.2.1.tar.gz

---
+++ packages/Events/Events-0.2.1.tar.gz/Events-0.2.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+DESCRIPTION = ("Bringing the elegance of C# EventHanlder to Python")
+LONG_DESCRIPTION = open('README.rst').read()
+VERSION = __import__('events').__version__
+
+setup(
+    name='Events',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author='Nicola Iarocci',
+    author_email='nicola@nicolaiarocci.com',
+    url='http://github.com/nicolaiarocci/events',
+    license=open('LICENSE').read(),
+    platforms=["any"],
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite="events.tests",
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        #'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+    ],
+)
