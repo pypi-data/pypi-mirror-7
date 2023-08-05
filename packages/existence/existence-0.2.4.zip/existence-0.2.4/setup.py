f5d1ad1fb60bb3e71d40be4f4b24a49868c9411e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:42 +0000

    Add package existence/0.2.4/existence-0.2.4.zip

---
+++ packages/existence/existence-0.2.4.zip/existence-0.2.4/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+
+try:
+    with open('README.md') as readme:
+        long_description = readme.read()
+except (IOError, ImportError):
+    long_description = ''
+
+entry_points = {
+    'console_scripts': [
+        'existence = existence:main',
+    ]
+}
+
+setup(
+    install_requires = [
+        "lxml>=3.3.4",
+        "cssselect>=0.9.1"
+    ],
+    name="existence",
+    py_modules=["existence"],
+    entry_points=entry_points,
+    version="0.2.4",
+    author="Eric Carmichael",
+    author_email="eric@ckcollab.com",
+    description="Checks static .html files for bad links",
+    long_description=long_description,
+    license="MIT",
+    keywords="link checker",
+    url="https://github.com/ckcollab/existence",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
