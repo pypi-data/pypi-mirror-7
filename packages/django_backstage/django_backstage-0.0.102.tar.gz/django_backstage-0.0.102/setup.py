96eb9818024b3a2d9e5f657486be45f0353c5a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:33 +0000

    Add package django_backstage/0.0.102/django_backstage-0.0.102.tar.gz

---
+++ packages/django_backstage/django_backstage-0.0.102.tar.gz/django_backstage-0.0.102/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_backstage',
+    version='0.0.102',
+    description='Django project and site deployment using virtualenv, uWSGI, nginx, etc.',
+    author='MiddleFork',
+    author_email='walker@mfgis.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "setuptools_git >= 0.3",
+    ],
+    entry_points = {
+      'console_scripts':
+            'backstage_venue = backstage.venue:new_venue'
+    }
+)
