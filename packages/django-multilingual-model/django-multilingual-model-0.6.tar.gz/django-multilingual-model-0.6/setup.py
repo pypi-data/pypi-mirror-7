3c2515a00fcf7a6b33dfdc2f6115d3632bc98983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package django-multilingual-model/0.6/django-multilingual-model-0.6.tar.gz

---
+++ packages/django-multilingual-model/django-multilingual-model-0.6.tar.gz/django-multilingual-model-0.6/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#from distribute_setup import use_setuptools
+#use_setuptools()
+
+from setuptools import setup, find_packages
+
+try:
+    README = open('README.rst').read()
+except:
+    README = None
+
+#try:
+#    REQUIREMENTS = open('requirements.txt').lines()
+#except:
+#    REQUIREMENTS = None
+
+setup(
+    name='django-multilingual-model',
+    version='0.6',
+    description='Django Simple Multilingual Support for Models.',
+    long_description=README,
+    #install_requires=REQUIREMENTS,
+    author='Mathijs de Bruin',
+    author_email='mathijs@mathijsfietst.nl',
+    url='http://github.com/dokterbob/django-multilingual-model',
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+        'argparse',  # apparently needed by django-setuptest on python 2.6
+    ),
+)
