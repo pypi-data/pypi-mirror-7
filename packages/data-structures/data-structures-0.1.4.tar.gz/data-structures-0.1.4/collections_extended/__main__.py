0cd15e6cde86a14afec0ab5aeb088e7b03947927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:22 +0000

    Add package data-structures/0.1.4/data-structures-0.1.4.tar.gz

---
+++ packages/data-structures/data-structures-0.1.4.tar.gz/data-structures-0.1.4/collections_extended/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import doctest
+
+import collections_extended
+
+doctest.testmod(collections_extended)
+doctest.testmod(collections_extended.bags)
+doctest.testmod(collections_extended.setlists)
+
