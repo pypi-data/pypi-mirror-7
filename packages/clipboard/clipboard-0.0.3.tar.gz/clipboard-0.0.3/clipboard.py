dc5362df09c7461f8331dd23464c175f2ecd8b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:35 +0000

    Add package clipboard/0.0.3/clipboard-0.0.3.tar.gz

---
+++ packages/clipboard/clipboard-0.0.3.tar.gz/clipboard-0.0.3/clipboard.py
@@ -0,0 +1,11 @@
+'''
+A croos platform clipboard read/write library.
+
+...well, as I were trying to implement this. I realize that everything is already in the pyperclip module.
+
+'''
+from pyperclip import copy, paste
+
+VERSION = "0.0.3"
+
+
