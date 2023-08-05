f48a4ce618aa7641a77cc6a6beb98595e6535cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package grid/0.7.1/grid-0.7.1.tar.gz

---
+++ packages/grid/grid-0.7.1.tar.gz/grid-0.7.1/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Volumetric data module',
+    'long_description': open('README.txt').read(),
+    'author': 'Daniel J. Sindhikara',
+    #'url':'https://github.com/dansind/grid',
+    #'download_url': 'https://github.com/dansind/grid/tarball/master',
+    'author_email': 'sindhikara@gmail.com',
+    'version': '0.7.1',
+    'install_requires': ['numpy', 'argparse', ], #
+    #'py_modules': ['grid'],
+    'packages': ["grid","grid.tests"],
+    'package_data': {'grid': ['data/shells.json', 'data/points/*', "tests/data/dxfiles/*"],
+                     "grid.tests": ["data/dxfiles/*", "data/UxDATAfiles/*", "data/TKRguv/*"]},
+    'scripts': ["grid/tests/grid_tests.py", "bin/RDFfromDX.py", "bin/gridconvert.py"],
+    'license': 'LGPL',
+    'name': 'grid'
+}
+
+setup(**config)
