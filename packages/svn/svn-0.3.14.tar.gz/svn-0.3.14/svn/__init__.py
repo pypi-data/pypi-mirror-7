029dd50e7ebd4c79ebb52ce9bd59460a3068538f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package svn/0.3.14/svn-0.3.14.tar.gz

---
+++ packages/svn/svn-0.3.14.tar.gz/svn-0.3.14/svn/__init__.py
@@ -0,0 +1,8 @@
+from svn.local import LocalClient
+from svn.remote import RemoteClient
+
+(T_URL, T_PATH) = ('url', 'path')
+
+#from ._version import get_versions
+#__version__ = get_versions()['default']
+#del get_versions
