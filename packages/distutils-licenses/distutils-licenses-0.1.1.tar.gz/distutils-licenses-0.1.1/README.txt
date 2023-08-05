e1615d18a906eb4f30cf1d802629a01c4f858c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package distutils-licenses/0.1.1/distutils-licenses-0.1.1.tar.gz

---
+++ packages/distutils-licenses/distutils-licenses-0.1.1.tar.gz/distutils-licenses-0.1.1/README.txt
@@ -0,0 +1,14 @@
+==================
+distutils-licenses
+==================
+
+A command for setup.py to print the licenses of installed packages.
+
+To use, place in your setup.py:
+
+    setup_requires=['distutils-licenses']
+
+From then on, you can do:
+
+    python setup.py licenses
+
---
+++ packages/distutils-licenses/distutils-licenses-0.1.1.tar.gz/distutils-licenses-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='distutils-licenses',
+    version='0.1.1',
+
+    author='Daniel A. Koepke',
+    author_email='dkoepke@gmail.com',
+    license='MIT',
+
+    description='adds a command to setup.py to list licenses of packages',
+    long_description=open('README.txt').read(),
+
+    url='http://github.com/dkoepke/distutils-licenses',
+    packages=['distutils_licenses'],
+
+    entry_points={
+        'distutils.commands': [
+            'licenses = distutils_licenses.command:LicenseCommand',
+        ],
+    }
+)
