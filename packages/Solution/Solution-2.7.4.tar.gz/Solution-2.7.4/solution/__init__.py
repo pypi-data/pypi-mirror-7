5b82b67476e19ede210cdaad4a1c4e87ac3f4a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:50 +0000

    Add package Solution/2.7.4/Solution-2.7.4.tar.gz

---
+++ packages/Solution/Solution-2.7.4.tar.gz/Solution-2.7.4/solution/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+=============================
+Solution
+=============================
+
+An amazing form solution
+
+
+BSD License. See ``LICENSE`` for more details.
+Copyright © 2011 - 2014, Lúcuma labs (http://lucumalabs.com)
+
+"""
+from .form import Form
+from .formset import FormSet
+from .fields import *
+from .validators import *
+from .utils import Markup, get_html_attrs, to_unicode
+
+
+__version__ = '2.7.4'
