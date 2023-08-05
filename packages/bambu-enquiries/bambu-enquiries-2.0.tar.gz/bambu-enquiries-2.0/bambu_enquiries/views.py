c6555200399d3aa507ba7ab2ccbfc371471a9f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package bambu-enquiries/2.0/bambu-enquiries-2.0.tar.gz

---
+++ packages/bambu-enquiries/bambu-enquiries-2.0.tar.gz/bambu-enquiries-2.0/bambu_enquiries/views.py
@@ -0,0 +1,50 @@
+from django.template.response import TemplateResponse
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.utils.http import urlencode
+from bambu_enquiries.models import Enquiry
+from bambu_enquiries.forms import EnquiryForm
+
+def enquiry(request):
+    form = EnquiryForm(request.POST or None)
+    
+    if request.method == 'POST' and form.is_valid():
+        enquiry = form.save(commit = False)
+        spam = enquiry.check_for_spam(request)
+        enquiry.save(notify = not spam)
+        
+        if 'bambu_analytics' in settings.INSTALLED_APPS:
+            from bambu_analytics import track_event, events
+            track_event(request, events.EVENT,
+                category = u'Enquiry',
+                action = u'Submit',
+                option_label = enquiry.subject,
+                option_value = enquiry.pk
+            )
+        
+        return HttpResponseRedirect(
+            'thanks/?%s' % urlencode(
+                {
+                    'next': request.GET.get('next', '/')
+                }
+            )
+        )
+    
+    return TemplateResponse(
+        request,
+        'enquiries/enquiry.html',
+        {
+            'form': form,
+            'menu_selection': 'enquiry',
+            'next': request.GET.get('next', '/')
+        }
+    )
+    
+def enquiry_thanks(request):
+    return TemplateResponse(
+        request,
+        'enquiries/thanks.html',
+        {
+            'next': request.GET.get('next', '/')
+        }
+    )
