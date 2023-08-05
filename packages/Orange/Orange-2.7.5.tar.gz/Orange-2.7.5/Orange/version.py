5d44d9970301e97f048d34f15c48e68ff84ececc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:38 +0000

    Add package Orange/2.7.5/Orange-2.7.5.tar.gz

---
+++ packages/Orange/Orange-2.7.5.tar.gz/Orange-2.7.5/Orange/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '2.7.5'
+version = '2.7.5'
+full_version = '2.7.5'
+hg_revision = '601e48a52066'
+release = True
+
+if not release:
+    version = full_version
