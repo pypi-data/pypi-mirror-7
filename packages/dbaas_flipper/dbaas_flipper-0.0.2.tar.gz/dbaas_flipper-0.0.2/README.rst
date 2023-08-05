f134fa01e9b5243e4dd3a08759ab71a1a8fc943a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:34 +0000

    Add package dbaas_flipper/0.0.2/dbaas_flipper-0.0.2.tar.gz

---
+++ packages/dbaas_flipper/dbaas_flipper-0.0.2.tar.gz/dbaas_flipper-0.0.2/README.rst
@@ -0,0 +1,23 @@
+===============================
+DBaaS Flipper
+===============================
+
+.. image:: https://badge.fury.io/py/dbaas-flipper.png
+    :target: http://badge.fury.io/py/dbaas-flipper
+    
+.. image:: https://travis-ci.org/felippemr/dbaas-flipper.png?branch=master
+        :target: https://travis-ci.org/felippemr/dbaas-flipper
+
+.. image:: https://pypip.in/d/dbaas-flipper/badge.png
+        :target: https://pypi.python.org/pypi/dbaas-flipper
+
+
+Flipper integration 4 DBaaS
+
+* Free software: BSD license
+* Documentation: http://dbaas-flipper.rtfd.org.
+
+Features
+--------
+
+* TODO
---
+++ packages/dbaas_flipper/dbaas_flipper-0.0.2.tar.gz/dbaas_flipper-0.0.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='dbaas_flipper',
+    version='0.0.2',
+    description='Flipper integration 4 DBaaS',
+    long_description=readme + '\n\n' + history,
+    author='Felippe da Motta Raposo',
+    author_email='raposo.felippe@gmail.com',
+    url='https://github.com/felippemr'
+        '/dbaas-flipper',
+    packages=[
+        'dbaas_flipper',
+    ],
+    package_dir={'dbaas_flipper':
+                 'dbaas_flipper'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='dbaas_flipper',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
+)
