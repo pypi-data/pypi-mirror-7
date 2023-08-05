51a4c71c1e33fa9a74c98ae63c8ef2bd990b6c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package clay/2.9.3/Clay-2.9.3.tar.gz

---
+++ packages/clay/Clay-2.9.3.tar.gz/Clay-2.9.3/clay/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+# Clay
+
+An amazing prototyping tool
+
+------------
+© 2011 by [Lúcuma](http://lucumalabs.com).
+
+See `AUTHORS.md` for more details.
+
+[MIT License](http://www.opensource.org/licenses/mit-license.php)
+
+"""
+from .main import Clay
+
+
+__version__ = '2.9.3'
