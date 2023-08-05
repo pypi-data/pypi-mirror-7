9c6d248bec204010327c70ae788c42ec9cb82f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:35 +0000

    Add package ryuno_showlist/1.3.0/ryuno_showlist-1.3.0.tar.gz

---
+++ packages/ryuno_showlist/ryuno_showlist-1.3.0.tar.gz/ryuno_showlist-1.3.0/ryuno_showlist.py
@@ -0,0 +1,10 @@
+added_list=[1,2,3,4,5,[61,62,63,64,65],7,[81,82,83,[841,842,843,844],85]]
+
+def showlist(l,tap_num=0):
+	for i in l:
+		if isinstance(i,list):
+			showlist(i,tap_num+1)
+		else:
+			for num in range(tap_num):
+				print("\t",end='')
+			print(i)
