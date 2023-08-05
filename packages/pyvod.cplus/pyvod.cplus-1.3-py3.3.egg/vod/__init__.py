f347e093bb524c279d7041de1a4c6fe3699fb576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package pyvod.cplus/1.3/pyvod.cplus-1.3-py3.3.egg

---
+++ packages/pyvod.cplus/pyvod.cplus-1.3-py3.3.egg/vod/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__import__('pkg_resources').declare_namespace(__name__)
+
+import sys
+
+if sys.version_info.major != 3:
+    raise Exception("Sorry this software only works with python 3.")
+
