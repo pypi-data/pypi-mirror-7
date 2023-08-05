5eba3768d9bde0dac735edc2b8b2a2af2ee1de29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package postdoc/0.1.3/postdoc-0.1.3.tar.gz

---
+++ packages/postdoc/postdoc-0.1.3.tar.gz/postdoc-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+setup(
+    name='postdoc',
+    version='0.1.3',
+    description='A helper for Postgres + Docker that works for free',
+    long_description=open('README.rst').read(),
+    author='Chris Chang',
+    author_email='c@crccheck.com',
+    url='https://github.com/crccheck/postdoc',
+    py_modules=['postdoc'],
+    entry_points={
+        'console_scripts': [
+            'phd = postdoc:main',
+        ],
+    },
+    license='Apache',
+    tests_require=[
+        'mock==1.0.1',
+    ],
+    test_suite='test_postdoc',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+        'Topic :: Utilities',
+    ],
+)
