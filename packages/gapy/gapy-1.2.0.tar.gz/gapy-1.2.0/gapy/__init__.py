113b132ae016c918acd9dc44aa1e66c4a8957d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:05 +0000

    Add package gapy/1.2.0/gapy-1.2.0.tar.gz

---
+++ packages/gapy/gapy-1.2.0.tar.gz/gapy-1.2.0/gapy/__init__.py
@@ -0,0 +1,3 @@
+__title__ = "gapy"
+__version__ = "1.2.0"
+__author__ = "Rob Young"
---
+++ packages/gapy/gapy-1.2.0.tar.gz/gapy-1.2.0/setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import gapy
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+packages = [
+    "gapy"
+]
+
+requires = [
+    "google-api-python-client==1.0",
+    "pyOpenSSL==0.13"
+]
+
+try:
+    long_description = open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+except:
+    long_description = None
+
+setup(
+    name=gapy.__title__,
+    version=gapy.__version__,
+    description="Painless Google Analytics",
+    long_description=long_description,
+    author=gapy.__author__,
+    author_email="rob@roryoung.co.uk",
+    url="https://github.com/alphagov/gapy",
+    packages=packages,
+    package_dir={"gapy": "gapy"},
+    include_package_data=True,
+    install_requires=requires,
+    license="https://raw.github.com/alphagov/gapy/master/LICENSE",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
