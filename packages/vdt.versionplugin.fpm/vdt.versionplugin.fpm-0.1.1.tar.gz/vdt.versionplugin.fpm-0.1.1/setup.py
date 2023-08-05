d13a8535d5353341cb92364b912c132965db8c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:01 +0000

    Add package vdt.versionplugin.fpm/0.1.1/vdt.versionplugin.fpm-0.1.1.tar.gz

---
+++ packages/vdt.versionplugin.fpm/vdt.versionplugin.fpm-0.1.1.tar.gz/vdt.versionplugin.fpm-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+pkgname = "vdt.versionplugin.fpm"
+
+setup(name=pkgname,
+      version="0.1.1",
+      description="Version Increment Plugin that builds with fpm",
+      author="Martijn Jacobs",
+      author_email="martijn@kamaramusic.nl",
+      maintainer="Martijn Jacobs",
+      maintainer_email="martijn@kamaramusic.nl",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['vdt', 'vdt.versionplugin'],
+      zip_safe=True,
+      install_requires=[
+          "setuptools",
+          "vdt.version",
+          "vdt.versionplugin.default",
+      ],
+      entry_points={},
+)
+
+
+
