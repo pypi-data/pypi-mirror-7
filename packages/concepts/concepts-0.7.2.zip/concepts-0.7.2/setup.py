e65a399c4b3eb562df539f96a1ea00047e59548b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package concepts/0.7.2/concepts-0.7.2.zip

---
+++ packages/concepts/concepts-0.7.2.zip/concepts-0.7.2/setup.py
@@ -0,0 +1,38 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='concepts',
+    version='0.7.2',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Formal Concept Analysis with Python',
+    keywords='fca complete lattice graph join meet galois',
+    license='MIT',
+    url='http://github.com/xflr6/concepts',
+    packages=['concepts'],
+    install_requires=[
+        'bitsets>=0.7, <0.8',
+        'graphviz>=0.3, <0.4',
+    ],
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
