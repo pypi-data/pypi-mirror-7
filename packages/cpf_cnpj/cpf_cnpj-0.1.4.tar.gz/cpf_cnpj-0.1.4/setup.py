56fa5616737711632f9c62414cedbf8c340a30be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:50 +0000

    Add package cpf_cnpj/0.1.4/cpf_cnpj-0.1.4.tar.gz

---
+++ packages/cpf_cnpj/cpf_cnpj-0.1.4.tar.gz/cpf_cnpj-0.1.4/setup.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    # install_requires=REQUIREMENTS,
+    name = "cpf_cnpj",
+    version = '0.1.4',
+    description = "Validador de CPF e CNPJ para Python",
+    license = "MIT",
+    author = "Adriano Margarin",
+    author_email = "adriano.margarin@gmail.com",
+    url = "https://github.com/amargarin/cpf_cnpj",
+    packages = find_packages(exclude = ['tests']),
+    keywords = "python cpf cnpj validador",
+    zip_safe = True
+)
