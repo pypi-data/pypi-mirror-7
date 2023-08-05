aeec4f07d6004d56aa5a33ebe6edb2a4e6ad7c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:50 +0000

    Add package python-weed/0.1.7/python-weed-0.1.7.tar.gz

---
+++ packages/python-weed/python-weed-0.1.7.tar.gz/python-weed-0.1.7/test/test_weed.py
@@ -0,0 +1,365 @@
+# ** -- coding: utf-8 -- **
+#!/usr/bin/env python
+#
+#Copyright (c) 2011 darkdarkfruit <darkdarkfruit@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+#
+
+'''
+jsend test
+==========
+You should have py.test installed first.
+pip install -U pytest
+
+Then, run
+>> py.test  test_jsend.py
+
+
+note:
+    ensure weed master-server and at least one volume-server are up
+    default:
+        master-server: 127.0.0.1:9333
+        volume-server: 127.0.0.1:27000
+        filer-server : 127.0.0.1:27100
+
+'''
+
+from env_test_env import *
+
+
+import weed
+print(weed)
+from weed import master
+from weed.conf import *
+from weed.util import *
+from weed.master import *
+from weed.volume import *
+from weed.operation import *
+from weed.filer import WeedFiler
+
+set_global_logger_level(logging.DEBUG)
+
+WEED_MASTER_IP = '127.0.0.1'
+WEED_MASTER_PORT = 9333
+
+
+def test_WeedMaster():
+    master = WeedMaster()
+    assert master.__repr__()
+
+    # assign
+    assign_key_dict = master.acquire_assign_info()
+    assert isinstance(assign_key_dict, dict)
+    assert assign_key_dict.has_key('fid')
+    assert assign_key_dict['fid'].replace(',', '') > 0x001
+    fid = assign_key_dict['fid']
+    volume_id = fid.split(',')[0]
+
+    # lookup
+    lookup_dict = master.lookup(fid)
+    assert isinstance(lookup_dict, dict)
+    assert lookup_dict.has_key('locations')
+    locations_list = lookup_dict['locations']
+    assert locations_list[0].has_key('url')
+    assert locations_list[0].has_key('publicUrl')
+
+    # vacuum
+    vacuum_dict = master.vacuum()
+    assert isinstance(vacuum_dict, dict)
+    assert vacuum_dict.has_key('Topology')
+
+
+    # status
+    status_dict = master.get_status()
+    assert isinstance(status_dict, dict)
+    assert status_dict.has_key('Topology')
+
+
+def test_WeedMaster2():
+    master = WeedMaster()
+    assert master.__repr__()
+
+    # assign
+    wak = master.acquire_new_assign_key(10)
+    assert isinstance(wak, dict)
+    assert wak.has_key('fid')
+
+    fid = wak.fid
+
+    fids = [fid] + [fid + '_' + str(i + 1) for i in range(10)]
+    locations = []
+    for i in fids:
+        l = master.lookup(i)
+        assert l.has_key('locations')
+        locations.append(l)
+
+    for i, l in enumerate(locations):
+        if i < (len(locations) - 1):
+            assert locations[i] == locations[i + 1]
+
+
+
+def test_WeedVolume():
+    volume = WeedVolume()
+    assert volume.__repr__()
+
+    # status
+    status_dict = volume.get_status()
+    assert isinstance(status_dict, dict)
+    assert status_dict.has_key('Version')
+    assert status_dict.has_key('Volumes')
+
+
+def test_WeedAssignKey():
+    wak = WeedAssignKey()
+    assert wak
+    assert wak.__repr__()
+
+
+def test_WeedAssignKeyExtended():
+    wak = WeedAssignKeyExtended()
+    assert wak
+    assert wak.__repr__()
+
+
+def test_WeedOperationResponse():
+    wor = WeedOperationResponse()
+    assert wor
+    assert wor.__repr__()
+
+
+def test_volume_put_get_delete_file():
+    master = WeedMaster()
+    assert master.__repr__()
+
+    # assign
+    assign_key_dict = master.get_assign_key()
+    assert isinstance(assign_key_dict, dict)
+    assert assign_key_dict.has_key('fid')
+    assert assign_key_dict['fid'].replace(',', '') > 0x001
+    fid = assign_key_dict['fid']
+    volume_id = fid.split(',')[0]
+
+    # lookup
+    lookup_dict = master.lookup(fid)
+    assert isinstance(lookup_dict, dict)
+    assert lookup_dict.has_key('locations')
+    locations_list = lookup_dict['locations']
+    assert locations_list[0].has_key('url')
+    assert locations_list[0].has_key('publicUrl')
+
+    volume_url = 'http://' + locations_list[0]['publicUrl']
+    url = urlparse.urlparse(volume_url)
+
+    #volume = WeedVolume(host=url.hostname, port=url.port)
+    volume = master.get_volume(fid)
+    status_dict = volume.get_status()
+    assert isinstance(status_dict, dict)
+    assert status_dict.has_key('Version')
+    assert status_dict.has_key('Volumes')
+    assert isinstance(status_dict['Volumes'], list)
+
+    # file_to_post = './test_file_to_post_to_weed.xml'
+    file_to_update = 'test_file_to_post_to_weed.xml'
+    file_to_post = os.path.join(TEST_PATH, file_to_update)
+
+    with open(file_to_post, 'wb') as tmp_file:
+        tmp_file.write("nonsense " * 1024 * 256)
+    put_result = volume.put_file(os.path.abspath(file_to_post),fid)
+    assert not 'error' in put_result
+    assert 'size' in put_result
+
+    data = volume.get_file(fid)
+    assert data
+    # print data
+    with open(file_to_post, 'r') as fd:
+        file_data = fd.read()
+    assert data == file_data
+
+    delete_result = volume.delete_file(fid)
+    assert delete_result
+    assert not 'error' in delete_result
+    assert 'size' in delete_result
+
+
+def test_accquire_new_fid():
+    op = WeedOperation()
+    fids = op.acquire_new_fids()
+    assert isinstance(fids, (tuple, list))
+    assert ',' in fids[0]
+    count = 5
+    fids = op.acquire_new_fids(count)
+    for i in range(count):
+        assert ',' in fids[i]
+
+
+def test_put_a_file_with_fid():
+    op = WeedOperation()
+    fid = op.acquire_new_fids()[0]
+    fname = 'test_opensource_logo.jpg'
+    fpath = os.path.join(TEST_PATH, fname)
+    assert op.put(open(fpath, 'r'), fid, fname)
+
+
+
+def test_put_file():
+    # test put_file in weed.util
+    op = WeedOperation()
+    fid = ''
+    fname = 'test_opensource_logo.jpg'
+    fpath = os.path.join(TEST_PATH, fname)
+    master = WeedMaster()
+    fid_full_url = master.acquire_new_assign_key()['fid_full_url']
+    print('test_weed.py, fid_full_url: %s, fpath: %s' % (fid_full_url, fpath))
+    with open(fpath, 'rb') as f:
+        LOGGER.info('fp position: %d' % f.tell())
+        LOGGER.info('fp info: length: %d' % len(f.read()))
+        f.seek(0)
+
+        rsp = put_file(f, fid_full_url, fname)
+        assert rsp
+        assert rsp.status == 'success'
+        assert rsp.has_key('url') and rsp.url
+        assert rsp.storage_size > 0
+        fid = os.path.split(fid_full_url)[1]
+
+    # read
+    content = op.crud_read(fid, fname).content
+    with open(fpath) as _:
+        original_content = _.read()
+    assert content == original_content
+
+
+
+def test_file_operations():
+    op = WeedOperation()
+    assert op.__repr__()
+
+    # create
+    fid = ''
+    fname = 'test_opensource_logo.jpg'
+    fpath = os.path.join(TEST_PATH, fname)
+    with open(fpath, 'r') as f:
+        rsp = op.crud_create(f, fname)
+        assert rsp
+        assert rsp.has_key('fid')
+        fid = rsp['fid']
+        assert os.path.split(op.get_fid_full_url(fid))[1] == fid
+        assert rsp['fid'].replace(',', '') > '01'
+        assert rsp['url']
+        assert rsp['storage_size'] > 0
+
+    # test exists
+    assert op.exists(fid)
+    assert op.exists(fid + 'wrong_fid') == False
+    assert op.exists('wrong_fid') == False
+    assert op.exists(fid.replace(',', '/')) == False
+
+
+    # read
+    content = op.get(fid, fname).content
+    content2 = op.get_content(fid, fname)
+    assert content == content2
+    with open(fpath) as _:
+        original_content = _.read()
+    assert content == original_content
+
+    # update
+    file_to_update = 'test_file_to_post_to_weed.xml'
+    fpath = os.path.join(TEST_PATH, file_to_update)
+    with open(fpath, 'rwb+') as tmp_file:
+        tmp_file.write("testdata " * 1024 * 256)
+        tmp_file.seek(0)
+        rsp = op.crud_update(tmp_file, fid)
+        assert rsp
+        assert rsp.has_key('fid')
+        fid_2 = rsp['fid']
+        assert fid_2 == fid
+        assert rsp['fid'].replace(',', '') > '01'
+        assert rsp['url']
+        assert rsp['storage_size'] > 0
+
+    # delete
+    rsp = op.crud_delete(fid)
+    assert rsp.storage_size > 0
+    rsp = op.crud_delete('3,20323023') # delete an unexisted file, should return False
+    assert rsp.storage_size == 0
+
+
+def test_WeedFiler():
+    wf = WeedFiler()
+    assert wf.host == '127.0.0.1'
+    assert wf.port == 27100
+    assert wf.uri == '127.0.0.1:27100'
+    assert wf.url == 'http://127.0.0.1:27100'
+
+    d = '/test/'
+    new_d = '/test/new_dir/'
+
+    # put f1
+    f1_content = 'hello, how are you'
+    f1 = StringIO.StringIO(f1_content)
+    f1_path = d + 'test.txt'
+    assert wf.put(f1, f1_path) == f1_path
+
+    # mkdir
+    assert wf.mkdir(new_d)
+
+    # put f2
+    f2_content = 'hello, how are you?'
+    f2 = StringIO.StringIO(f2_content)
+    f2_path = new_d + 'hello.txt'
+    assert wf.put(f2, f2_path) == f2_path
+
+    # list
+    j = wf.list(new_d)
+    assert isinstance(j, dict)
+    assert j.has_key('Directory')
+    assert j['Directory'] == '/test/new_dir/'
+    assert j['Files']
+    files = j['Files']
+    fnames = []
+    for f in files:
+        fnames.append(f['Name'])
+    assert 'hello.txt' in fnames
+
+    # get f1
+    wf_get = wf.get(f1_path)
+    assert wf_get['content_length'] > 0
+    if 'gzip' in wf_get['content_type']:
+        content_gz = wf['content']
+        content = gzip.open(StringIO.StringIO(content_gz)).read()
+        assert content == f1_content
+    elif 'text' in wf_get['content_type']:
+        assert wf_get['content'] == f1_content
+    else:
+        pass
+
+    # delete f1, f2
+    assert wf.delete(f1_path)
+    assert wf.delete(f2_path)
+
+
+
+if __name__ == '__main__':
+    test_WeedMaster()
+    test_WeedVolume()
+    test_file_operations()
+    test_volume_put_get_delete_file()
---
+++ packages/python-weed/python-weed-0.1.7.tar.gz/python-weed-0.1.7/weed/operation.py
@@ -0,0 +1,361 @@
+# ** -- coding: utf-8 -- **
+#!/usr/bin/env python
+#
+#Copyright (c) 2011 darkdarkfruit <darkdarkfruit@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+#
+
+'''
+operations to weedfs.
+'''
+
+
+__all__ = ['WeedOperation']
+
+
+import json
+import random
+import StringIO
+import urlparse
+import requests
+from conf import *
+from util import *
+
+from master import *
+from volume import *
+
+
+
+class WeedOperation(object):
+    """ DO CRUD operations to weed-fs.
+
+    Arguments you need to supply is just weed master.
+    The master will find a volume automaticlly.
+    Currently, implement it with requests. Maybe *tornado or  *mongrel2 + brubeck* is better?
+
+    """
+
+    def __init__(self, master_host='127.0.0.1', master_port=9333, prefetch_volumeIds=False):
+        self.master = WeedMaster(master_host, master_port, prefetch_volumeIds=prefetch_volumeIds)
+
+
+    def get_volume_fid_full_url(self, fid):
+        ''' return a random fid_full_url of volume by @fid
+
+        eg: (randomly choosed from locations)
+          return:  'http://127.0.0.1:27000/3,1234101234'  or
+          return:  'http://127.0.0.1:27001/3,1234101234'  or
+          return:  'http://127.0.0.1:27002/3,1234101234'
+        '''
+
+
+    def acquire_new_fids(self, count=1):
+        """
+        get a new avalable fid or a fid list
+        return:
+          ['3,12301234'] or
+          ['3,12301234', '3,12301234_1', '3,12301234_2',  '3,12301234_3', etc..]
+
+        ----------
+
+        """
+        wak = self.master.acquire_new_assign_key(count=count)
+        # print(wak)
+        fid = wak.fid
+
+        if count == 1:
+            return [fid]
+        else:
+            fids = [fid] + [fid + ('_%d' % (i + 1)) for i in range(count)]
+            return fids
+
+
+    def get_fid_full_url(self, fid):
+        ''' return a random fid_full_url of volume by @fid
+
+        eg: (randomly choosed from locations)
+          return:  'http://127.0.0.1:27000/3,1234101234'  or
+          return:  'http://127.0.0.1:27001/3,1234101234'  or
+          return:  'http://127.0.0.1:27002/3,1234101234'
+
+        tip: WeedMaster.looup returns a dict like below:
+        -----------
+        {
+          "locations": [
+            {
+              "publicUrl": "localhost:8080",
+              "url": "localhost:8080"
+            }
+          ]
+        }
+        -----------
+
+        return something like: 'http://127.0.0.1:8080/3,0230203913'
+        '''
+        volume_id = fid.split(',')[0]
+        full_url = None
+        try:
+            r = self.master.lookup(volume_id)
+            locations = r['locations']
+
+            # choose a random location
+            location = locations[random.randint(0, len(locations) - 1)]
+            full_url = 'http://%s/%s' % (location['url'], fid)
+        except Exception as e:
+            LOGGER.error('Could not get volume location of this fid: %s. Exception is: %s' % (fid, e))
+        return full_url
+
+
+    ## -----------------------------------------------------------
+    ##    weedfs operation: get/put/delete, and CRUD-aliases starts
+    ## -----------------------------------------------------------
+    def get(self, fid, fname=''):
+        """
+        read/get a file from weed-fs with @fid.
+
+        @just_url(default is False):
+          if True -> just return an object of WeedOperationResponse(web-servers/browsers like nginx, chrome can get resource by this url( WeedOperationResponse.fid_full_url ))
+          if False -> return a http response of requests(package requests) if @just_content is False else return file_content
+
+        @just_content(default is True):
+          if True -> just return the file's content
+          if False -> return a response of requests.Respond object. (eg: You can get content_type of the file being get)
+
+        return a WeedOperationResponse instance
+        """
+        LOGGER.debug('|--> Getting file. fid: %s, fname:%s' % (fid, fname))
+
+        fid_full_url = 'wrong_url'
+        wor = WeedOperationResponse()
+        try:
+            fid_full_url = self.get_fid_full_url(fid)
+            LOGGER.debug('Reading file fid: %s, fname: %s, fid_full_url: %s' % (fid, fname, fid_full_url))
+            rsp = self.get_http_response(fid_full_url)
+            wor.status = 'success'
+            wor.fid = fid
+            wor.url = fid_full_url
+            wor.name = fname
+            wor.content = rsp.content
+            wor.content_type = rsp.headers.get('content-type')
+        except Exception as e:
+            err_msg = 'Could not read file fid: %s, fname: %s, fid_full_url: %s, e: %s' % (fid, fname, fid_full_url, e)
+            LOGGER.error(err_msg)
+            wor.status = 'fail'
+            wor.message = err_msg
+
+        return wor
+
+
+    def get_url(self, fid):
+        ''' return a random fid_full_url of volume by @fid, alias to get_fid_full_url(fid)
+
+        eg: (randomly choosed from locations)
+          return:  'http://127.0.0.1:27000/3,1234101234'  or
+          return:  'http://127.0.0.1:27001/3,1234101234'  or
+          return:  'http://127.0.0.1:27002/3,1234101234'
+
+        return something like: 'http://127.0.0.1:8080/3,0230203913'
+        '''
+        return self.get_fid_full_url(fid)
+
+
+    def get_http_response(self, fid_full_url):
+        ''' return a "requests.Response" if we want whole info of the http request '''
+        return requests.get(fid_full_url)
+
+
+    def get_content(self, fid, fname=''):
+        ''' return just file's content. use method "get" to get more file's info '''
+        return self.get(fid, fname).content
+
+
+    def put(self, fp, fid=None, fname=''):
+        """  put a file to weed-fs.
+
+        if @fid provided, put @fp with @fid;
+        if @fid not provided, generate a new fid for it.
+
+        return a WeedOperationResponse instance
+        """
+        LOGGER.info('|--> Putting file@fid:%s, fname:%s' % (fid, fname))
+        fid_full_url = 'wrong_url'
+        _fid = fid
+        try:
+            if not fid:
+                wak = self.master.acquire_new_assign_key()
+                # print(wak)
+                _fid = wak.fid
+                LOGGER.debug('no fid. accquired new one: "%s"' % _fid)
+                fid_full_url = wak.fid_full_url
+            else:
+                fid_full_url = self.get_fid_full_url(fid)
+        except Exception as e:
+            err_msg = 'Could not put file. fp: "%s", fname: "%s", fid_full_url: "%s", e: %s' % (fp, fname, fid_full_url, e)
+            LOGGER.error(err_msg)
+            return None
+
+        wor = WeedOperationResponse()
+        try:
+            _fp = open(fp, 'r') if isinstance(fp, str) else fp
+
+            LOGGER.info('Putting file with fid: %s, fid_full_url:%s for file: fp: %s, fname: %s'
+                         % (_fid, fid_full_url, fp, fname))
+            wor = put_file(_fp, fid_full_url, fname)
+            LOGGER.info('%s' % wor)
+            wor.fid = _fid
+        except Exception as e:
+            err_msg = 'Could not put file. fp: "%s", fname: "%s", fid_full_url: "%s", e: %s' % (fp, fname, fid_full_url, e)
+            LOGGER.error(err_msg)
+            wor.status = 'fail'
+            wor.message = err_msg
+
+        # close fp if parameter fp is a str
+        if isinstance(fp, str):
+            try:
+                _fp.close()
+            except Exception as e:
+                LOGGER.warning('Could not close fp: %s. e: %s' % (_fp, e))
+
+        return wor
+
+
+    def delete(self, fid, fname=''):
+        """ remove a file in weed-fs with @fid.
+
+        if storage == 0, then @fid in weedfs is not exist.
+
+        return a WeedOperationResponse instance
+        """
+        LOGGER.debug('|--> Deleting file@%s, fname: %s' % (fid, fname))
+        wor = WeedOperationResponse()
+        fid_full_url = 'wrong_url'
+        try:
+            fid_full_url = self.get_fid_full_url(fid)
+            LOGGER.debug('Deleting file: fid: %s, fname: %s, fid_full_url: %s' % (fid, fname, fid_full_url))
+
+            r = requests.delete(fid_full_url)
+            rsp_json = r.json()
+
+            wor.status = 'success'
+            wor.fid = fid
+            wor.url = fid_full_url
+            wor.name = fname
+
+            if rsp_json.has_key('size'):
+                wor.storage_size = rsp_json['size']
+                if wor.storage_size == 0:
+                    err_msg = ('Error: fid@%s is not exist.' % fid)
+                    wor.status = 'fail'
+                    wor.message = err_msg
+                    LOGGER.error(err_msg)
+        except Exception as e:
+            err_msg = 'Deleting file: fid: %s, fname: %s, fid_full_url: %s, e: %s' % (fid, fname, fid_full_url, e)
+            LOGGER.error(err_msg)
+            wor.status = 'fail'
+            wor.message = err_msg
+            LOGGER.error(err_msg)
+
+        return wor
+
+
+    def exists(self, fid):
+        ''' detects @fid's existence '''
+        if ',' not in fid:      # fid should have a volume_id
+            return False
+        try:
+            volume_id = fid.split(',')[0]
+        except Exception as e:
+            LOGGER.error('Invalid fid:"%s". e: %s' % (fid, e))
+            return False
+
+        if not self.master.lookup(volume_id):
+            return False
+        fid_full_url = self.get_fid_full_url(fid)
+        try:
+            rsp = requests.head(fid_full_url, allow_redirects=True)
+            if not rsp.ok:
+                return False
+            else:
+                return True
+        except Exception as e:
+            LOGGER.error('Error occurs while requests.head. e: %s' % e)
+            return False
+
+
+
+    def crud_create(self, fp, fname=''):
+        """  CREATE of CRUD(C). Alias to method: "put"
+        """
+        LOGGER.debug('--> Trying to create a file. fp:%s, fname:%s' % (fp, fname))
+        return self.put(fp, fname=fname)
+
+
+    def crud_read(self, fid, fname=''):
+        """  READ of CRUD(R). Alias to method: "get"
+        """
+        LOGGER.debug('--> Trying to read a file. fid: %s, fname:%s' % (fid, fname))
+        return self.get(fid, fname=fname)
+
+
+    def crud_update(self, fp, fid, fname=''):
+        """ UPDATE of CRUD(U). Alias to method: "put" but giving parameter: @fid
+        """
+        LOGGER.info('--> Trying to update a file@fid:%s, fname: %s' % (fid, fname))
+        return self.put(fp, fid=fid, fname=fname)
+
+    def crud_delete(self, fid, fname=''):
+        """ DELETE of CRUD(U). Alias to method: "put" but giving parameter: @fid
+        """
+        LOGGER.info('--> Trying to delete a file@fid:%s, fname: %s' % (fid, fname))
+        return self.delete(fid=fid, fname=fname)
+
+
+
+    ## -----------------------------------------------------------
+    ##    weedfs operation: CRUD ends
+    ## -----------------------------------------------------------
+
+
+    def create_multiple(self, fp_array):
+        ''' create/save multiple files '''
+        pass
+
+
+    def cp(self, src_fid, dst_fid, src_fname=''):
+        """ cp src_fid dst_fid
+
+        replace file@dst_fid with file@src_fid
+        """
+        try:
+            src_file_rsp = self.crud_read(src_fid, fname=src_fname, just_url=False)
+            fp = StringIO.StringIO(src_file_rsp.content)
+            LOGGER.debug('Updating file: dst_fid: %s, src_fid: %s, src_fname: %s,  fp: %s' % (dst_fid, src_fid, src_fname, fp))
+            return self.crud_update(fp, dst_fid, src_fname)
+        except Exception as e:
+            err_msg = 'Could not Updating file: dst_fid: %s, src_fid: %s, src_fname: %s. e: %s' % (dst_fid, src_fid, src_fname, e)
+            LOGGER.error(err_msg)
+            return None
+
+
+    def __repr__(self):
+        return '<WeedOperation: @master(%s:%d)>' % (self.master.host, self.master.port)
+
+
+
