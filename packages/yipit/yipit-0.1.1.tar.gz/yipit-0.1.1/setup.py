ffb3dc19d1d152c4ad539bd6322d1145ef6f3979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package yipit/0.1.1/yipit-0.1.1.tar.gz

---
+++ packages/yipit/yipit-0.1.1.tar.gz/yipit-0.1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import re
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='yipit',
+        version="0.1.1",
+        description="Yipit",
+        author=u'Andrew Gross',
+        author_email=u'andrew@yipit.com',
+        url='https://github.com/Yipit/yipit',
+        install_requires=[],
+        dependency_links=[],
+        packages=[],
+        include_package_data=True,
+    )
