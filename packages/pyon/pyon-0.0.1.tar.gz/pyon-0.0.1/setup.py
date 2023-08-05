3927952734593448dbfe3688f717990ca6a9abf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package pyon/0.0.1/pyon-0.0.1.tar.gz

---
+++ packages/pyon/pyon-0.0.1.tar.gz/pyon-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pyon',
+    version='0.0.1',
+    packages=['pyon', 'pyon.lib', 'pyon.test',
+              ],
+    url='',
+    license='',
+    author='Shane Drury',
+    author_email='shane.r.drury@gmail.com',
+    description='',
+    install_requires=['numpy', 'scipy', 'simplejson', 'Jinja2'],
+)
