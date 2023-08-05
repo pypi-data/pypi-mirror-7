7b01ede4f5821bcf7d876cef20243d35115e1d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:54 +0000

    Add package cmsplugin_satchmo/0.1.6/cmsplugin_satchmo-0.1.6.tar.gz

---
+++ packages/cmsplugin_satchmo/cmsplugin_satchmo-0.1.6.tar.gz/cmsplugin_satchmo-0.1.6/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+	name='cmsplugin_satchmo',
+	version='0.1.6',
+	description='The Satchmo plugin for Django CMS',
+	url='https://bitbucket.org/tomaasch/cmsplugin_satchmo.git',
+	author=u'Tomáš Benda',
+	author_email='tomas@labs.cz',
+	license='MIT',
+	packages=['cmsplugin_satchmo'],
+	zip_safe=False,
+	include_package_data=True,
+)
+
