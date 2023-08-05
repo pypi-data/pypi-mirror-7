73c318d47b4e8339e0374d17078c0900e0974157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:03 +0000

    Add package PyMonad/1.2/PyMonad-1.2.tar.gz

---
+++ packages/PyMonad/PyMonad-1.2.tar.gz/PyMonad-1.2/pymonad/test/runTests.py
@@ -0,0 +1,15 @@
+# --------------------------------------------------------
+# (c) Copyright 2014 by Jason DeLaat. 
+# Licensed under BSD 3-clause licence.
+# --------------------------------------------------------
+
+import unittest
+
+from test_Reader import *
+from test_Maybe import *
+from test_Either import *
+from test_List import *
+from test_Monoid import *
+from test_Writer import *
+
+unittest.main()
---
+++ packages/PyMonad/PyMonad-1.2.tar.gz/PyMonad-1.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='PyMonad',
+    version='1.2',
+    author='Jason DeLaat',
+    author_email='jason.develops@gmail.com',
+    packages=['pymonad', 'pymonad.test'],
+    url='http://pypi.python.org/pypi/PyMonad',
+    license=open('LICENSE.txt').read(),
+    description='Collection of classes for programming with functors, applicative functors and monads.',
+    long_description=open('README.txt').read() + open("CHANGES.txt").read(),
+	classifiers=[ "Intended Audience :: Developers"
+				, "License :: OSI Approved :: BSD License"
+				, "Operating System :: OS Independent"
+				, "Programming Language :: Python :: 2.7"
+				, "Programming Language :: Python :: 3"
+				, "Topic :: Software Development"
+				, "Topic :: Software Development :: Libraries"
+				, "Topic :: Utilities"
+				],
+)
