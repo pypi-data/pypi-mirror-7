7092ffd6112948fd9005d555db44284329e939d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package rfc3161/1.0.2/rfc3161-1.0.2.tar.gz

---
+++ packages/rfc3161/rfc3161-1.0.2.tar.gz/rfc3161-1.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from distutils.core import setup, Command
+from unittest import TextTestRunner, TestLoader
+import os
+
+class TestCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+        tests = TestLoader().loadTestsFromName('tests.api')
+        t = TextTestRunner(verbosity = 1)
+        t.run(tests)
+
+setup(name='rfc3161',
+        version='1.0.2',
+        license='MIT',
+        url='https://dev.entrouvert.org/projects/python-rfc3161',
+        description='Python implementation of the RFC3161 specification, using pyasn1',
+        long_description=file('README').read(),
+        author='Benjamin Dauvergne',
+        author_email='bdauvergne@entrouvert.com',
+        packages=['rfc3161'],
+        requires=['pyasn1', 'pyasn1_modules', 'M2Crypto'],
+        cmdclass={'test': TestCommand})
