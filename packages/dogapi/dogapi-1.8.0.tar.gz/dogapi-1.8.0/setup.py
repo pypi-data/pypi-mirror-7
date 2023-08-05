e784e555b000a0862575a030828808699ac9d0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:24 +0000

    Add package dogapi/1.8.0/dogapi-1.8.0.tar.gz

---
+++ packages/dogapi/dogapi-1.8.0.tar.gz/dogapi-1.8.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys
+
+reqs = []
+if sys.version_info[0] == 2:
+    # simplejson is not python3 compatible
+    reqs.append("simplejson>=2.0.9")
+reqs.append("decorator>=3.3.2")
+if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
+    reqs.append("argparse>=1.2")
+
+setup(
+    name = "dogapi",
+    version = "1.8.0",
+    packages = find_packages("src"),
+    package_dir = {'':'src'},
+    author = "Datadog, Inc.",
+    author_email = "packages@datadoghq.com",
+    description = "Python bindings to Datadog's API and a user-facing command line tool.",
+    license = "BSD",
+    keywords = "datadog data",
+    url = "http://www.datadoghq.com",
+    install_requires = reqs,
+    entry_points={
+        'console_scripts': [
+            'dog = dogshell:main',
+            'dogwrap = dogshell.wrap:main',
+        ],
+    },
+)
