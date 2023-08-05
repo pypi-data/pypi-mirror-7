ccc47e26a7fffd02f84582c8fd90b5676ac1dbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:21 +0000

    Add package tg2ext.express/0.2.15/tg2ext.express-0.2.15.tar.gz

---
+++ packages/tg2ext.express/tg2ext.express-0.2.15.tar.gz/tg2ext.express-0.2.15/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+__version__ = '0.2.15'
+
+setup(name='tg2ext.express',
+      version=__version__,
+      description='tg2ext.express, a small extension for TurboGears2',
+      long_description=open("README.md").read(),
+      author='Mingcai SHEN',
+      author_email='archsh@gmail.com',
+      packages=['tg2ext', 'tg2ext.express'],
+      #package_dir={'tg2ext': 'tg2ext'},
+      #package_data={'tg2ext': ['controller', 'exceptions']},
+      license="The MIT License (MIT)",
+      platforms=["any"],
+      install_requires=[
+          'TurboGears2>=2.3.1',
+          'SQLAlchemy>=0.8.2',
+      ],
+      url='https://github.com/archsh/tg2ext.express')
