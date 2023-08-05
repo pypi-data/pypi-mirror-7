b31bc1f98bc849b7915fa96be5163a95bd62f5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package django-bootstrap-markdown/1.5.2/django-bootstrap-markdown-1.5.2.tar.gz

---
+++ packages/django-bootstrap-markdown/django-bootstrap-markdown-1.5.2.tar.gz/django-bootstrap-markdown-1.5.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-bootstrap-markdown",
+    version="1.5.2",
+    packages=find_packages(),
+    author="A.J. May",
+    author_email="aj7may@gmail.com",
+    description="An extension of the Django Textarea widget made for editing Markdown with a live preview.",
+    license="Creative Commons Attribution-ShareAlike 4.0 International License",
+    keywords="django bootstrap markdown live preview auto scroll",
+    url="http://thegoods.aj7may.com/django-bootstrap-markdown",
+    zip_safe=False,
+    package_data={
+        'django_bootstrap_markdown': [
+            'static/js/*',
+            'static/css/*',
+            'templates/django_bootstrap_markdown/*'
+        ],
+    },
+    install_requires=[
+        'Django>=1.6.1',
+        'Pillow>=2.2.2',
+        'South>=0.8.4',
+        'django-imagekit>=3.1'
+    ],
+)
