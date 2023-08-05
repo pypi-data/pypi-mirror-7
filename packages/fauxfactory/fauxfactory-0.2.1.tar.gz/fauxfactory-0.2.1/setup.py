d3bb1801f6ed84a4aef8c3a154d188369970c16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package fauxfactory/0.2.1/fauxfactory-0.2.1.tar.gz

---
+++ packages/fauxfactory/fauxfactory-0.2.1.tar.gz/fauxfactory-0.2.1/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+with open('HISTORY.rst') as f:
+    history = f.read()
+with open('AUTHORS.rst') as f:
+    authors = f.read()
+
+long_description = readme + '\n\n' + authors + '\n\n' + history
+
+setup(
+    name='fauxfactory',
+    description='Generates random data for your tests.',
+    long_description=long_description,
+    version='0.2.1',
+    author='Og Maciel',
+    author_email='omaciel@redhat.com',
+    url='https://github.com/omaciel/fauxfactory',
+    packages=['fauxfactory'],
+    keywords='python automation data',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing',
+    ]
+)
