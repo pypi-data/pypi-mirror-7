3101187d83a4ded07b134d363af90a06000ae6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:13 +0000

    Add package django-email-obfuscator/0.1.3/django-email-obfuscator-0.1.3.tar.gz

---
+++ packages/django-email-obfuscator/django-email-obfuscator-0.1.3.tar.gz/django-email-obfuscator-0.1.3/email_obfuscator/templatetags/email_obfuscator.py
@@ -0,0 +1,29 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+def obfuscate_string(value):
+    return ''.join(['&#%s;'.format(str(ord(char))) for char in value])
+
+
+@register.filter
+@stringfilter
+def obfuscate(value):
+    return mark_safe(obfuscate_string(value))
+
+
+@register.filter
+@stringfilter
+def obfuscate_mailto(value, text=False):
+    mail = obfuscate_string(value)
+
+    if text:
+        link_text = text
+    else:
+        link_text = mail
+
+    return mark_safe('<a href="%s%s">%s</a>'.format(
+        obfuscate_string('mailto:'), mail, link_text))
