9a839a00bc5e2157e7c5c19165b1de92569d1556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package smite/0.1a2/smite-0.1a2.tar.gz

---
+++ packages/smite/smite-0.1a2.tar.gz/smite-0.1a2/smite/__init__.py
@@ -0,0 +1,8 @@
+from smite.message import Message
+from smite.servant import (
+    Servant,
+    SecureServant,
+    SecureServantIdent,
+)
+from smite.client import Client
+from smite.proxy import Proxy
---
+++ packages/smite/smite-0.1a2.tar.gz/smite-0.1a2/smite/client.py
@@ -0,0 +1,103 @@
+import uuid
+import logging
+
+import zmq
+import msgpack
+
+from smite.message import Message
+from smite.aes_cipher import AESCipher
+from smite.exceptions import (
+    ConnectionError,
+    ClientTimeout,
+    MessageException,
+)
+
+log = logging.getLogger('smite.client')
+
+
+class Client(object):
+
+    def __init__(self, host, port, secret_key=None, ident=None,
+                 default_timeout=5):
+        self.host = host
+        self.port = port
+        self.ident = ident
+        self._default_timeout = default_timeout
+        self.cipher = AESCipher(secret_key) if secret_key is not None else None
+
+        self.ctx = zmq.Context()
+        self._create_socket()
+        self._uuid_node = uuid.getnode()
+
+    def send(self, msg, timeout=None, noreply=False):
+        if not isinstance(msg, Message):
+            raise TypeError('\'msg\' argument should be type of \'Message\'')
+        if timeout is None:
+            timeout = self._default_timeout
+
+        msg_d = {
+            '_method': msg.method,
+            '_uid': uuid.uuid1(self._uuid_node).hex,
+        }
+        if noreply:
+            msg_d['_noreply'] = 1
+        if msg.args:
+            msg_d['args'] = msg.args
+        if msg.kwargs:
+            msg_d['kwargs'] = msg.kwargs
+
+        msg = msg_d
+        del msg_d
+
+        log.debug('Sending message: {}'.format(msg))
+
+        msg = msgpack.packb(msg)
+
+        if self.cipher is not None:
+            msg = self.cipher.encrypt(msg)
+
+        if self.ident is not None:
+            msg = '{}{}'.format(self.ident, msg)
+        self._socket.send(msg)
+
+        if noreply:
+            return True
+        else:
+            return self._wait_for_reply(timeout)
+
+    def _wait_for_reply(self, timeout):
+        sockets = dict(self._poll.poll(timeout * 1000))
+
+        if sockets.get(self._socket) == zmq.POLLIN:
+            rep = self._socket.recv()
+            if self.cipher is not None:
+                rep = self.cipher.decrypt(rep)
+            rep = msgpack.unpackb(rep)
+            # TODO: check reply uid and raise exc eventually
+            if '_error' in rep:
+                if rep['_error'] == 50:
+                    log.error(rep['_traceback'])
+                    raise MessageException(rep['_exc_msg'], rep['_traceback'])
+        else:
+            log.error('Message timeout ({} sec) reached -> recreating socket'
+                      .format(timeout))
+            self._socket.setsockopt(zmq.LINGER, 0)
+            self._socket.close()
+            self._poll.unregister(self._socket)
+            self._create_socket()
+            raise ClientTimeout
+
+        return rep['_result']
+
+    def _create_socket(self):
+        self._socket = self.ctx.socket(zmq.DEALER)
+        self._poll = zmq.Poller()
+        self._poll.register(self._socket, zmq.POLLIN)
+        try:
+            connection_uri = 'tcp://{}:{}'.format(self.host, self.port)
+            self._socket.connect(connection_uri)
+        except Exception as e:
+            raise ConnectionError(
+                'Could not connect to: {} ({})'
+                .format(connection_uri, e.message)
+            )
