6cfc04ef556688c24f85e8e04b8d93d8b13b9e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package ishell/0.1.2/ishell-0.1.2.tar.gz

---
+++ packages/ishell/ishell-0.1.2.tar.gz/ishell-0.1.2/ishell/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .log import logger
+
+__version__ = "0.1.0"
+_current_prompt = None
---
+++ packages/ishell/ishell-0.1.2.tar.gz/ishell-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='ishell',
+    version='0.1.2',
+    author=u'Ítalo Rossi',
+    author_email='italorossib@gmail.com',
+    description='Build Interactive Shells with Python',
+    license='MIT',
+    keywords='cli terminal console shell interactive',
+    url='http://github.com/italorossi/ishell',
+    packages=['ishell'],
+    long_description='Build Interactive Shells with Python',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        "readline",
+    ]
+)
