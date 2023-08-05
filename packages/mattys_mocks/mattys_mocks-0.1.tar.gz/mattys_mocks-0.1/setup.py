a173d2a647e59e1e1e950dcdb77813293b1d77af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package mattys_mocks/0.1/mattys_mocks-0.1.tar.gz

---
+++ packages/mattys_mocks/mattys_mocks-0.1.tar.gz/mattys_mocks-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mattys_mocks',
+      version='0.1',
+      description='Yet another mocking library',
+      author='Matt ODonnell',
+      author_email='matty@mavenrd.com',
+      url='https://github.com/mattyo/mattysmocks',
+      packages=['mattys_mocks', 'mattys_mocks.tests', 'mattys_mocks.tests.mock_these'],
+     )
