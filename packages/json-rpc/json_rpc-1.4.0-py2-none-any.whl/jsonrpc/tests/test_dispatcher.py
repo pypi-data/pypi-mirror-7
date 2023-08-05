0fd7433b8a90dc3ef00463948eb241a54db7dc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package json-rpc/1.4.0/json_rpc-1.4.0-py2-none-any.whl

---
+++ packages/json-rpc/json_rpc-1.4.0-py2-none-any.whl/jsonrpc/tests/test_dispatcher.py
@@ -0,0 +1,75 @@
+from ..dispatcher import Dispatcher
+import unittest
+
+
+class TestDispatcher(unittest.TestCase):
+
+    """ Test Dispatcher functionality."""
+
+    def test_getter(self):
+        d = Dispatcher()
+
+        with self.assertRaises(KeyError):
+            d["method"]
+
+        d["add"] = lambda *args: sum(args)
+        self.assertEqual(d["add"](1, 1), 2)
+
+    def test_in(self):
+        d = Dispatcher()
+        d["method"] = lambda: ""
+        self.assertIn("method", d)
+
+    def test_add_method(self):
+        d = Dispatcher()
+
+        @d.add_method
+        def add(x, y):
+            return x + y
+
+        self.assertIn("add", d)
+        self.assertEqual(d["add"](1, 1), 2)
+
+    def test_del_method(self):
+        d = Dispatcher()
+        d["method"] = lambda: ""
+        self.assertIn("method", d)
+
+        del d["method"]
+        self.assertNotIn("method", d)
+
+    def test_to_dict(self):
+        d = Dispatcher()
+        func = lambda: ""
+        d["method"] = func
+        self.assertEqual(dict(d), {"method": func})
+
+    def test_init_from_object_instance(self):
+
+        class Dummy():
+
+            def one(self):
+                pass
+
+            def two(self):
+                pass
+
+        dummy = Dummy()
+
+        d = Dispatcher(dummy)
+
+        self.assertIn("one", d)
+        self.assertIn("two", d)
+        self.assertNotIn("__class__", d)
+
+    def test_init_from_dictionary(self):
+
+        dummy = {
+            'one': lambda x: x,
+            'two': lambda x: x,
+        }
+
+        d = Dispatcher(dummy)
+
+        self.assertIn("one", d)
+        self.assertIn("two", d)
