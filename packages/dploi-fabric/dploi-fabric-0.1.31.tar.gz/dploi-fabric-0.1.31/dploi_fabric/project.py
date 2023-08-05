266accad7ef3e7617e22f72ae2a929bf3ce89e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package dploi-fabric/0.1.31/dploi-fabric-0.1.31.tar.gz

---
+++ packages/dploi-fabric/dploi-fabric-0.1.31.tar.gz/dploi-fabric-0.1.31/dploi_fabric/project.py
@@ -0,0 +1,53 @@
+import os
+
+from fabric.api import env, task, prompt, run, put
+from fabric.contrib import files
+from dploi_fabric import git
+from github import upload_ssh_deploy_key
+from supervisor import update_config_file as supervisor_update_config_file
+from nginx import update_config_file as nginx_update_config_file
+import django_utils
+from .utils import config
+
+
+@task
+def init():
+    if files.exists(os.path.join(env.path, 'bin')):
+        print "buildout environment exists already"
+        return
+    upload_ssh_deploy_key()
+    run('mkdir -p %(path)s' % env)
+    if env.repo.startswith('git'):
+        run('cd %(path)s; git clone -b %(branch)s %(repo)s .' % env)
+        git.update()
+    elif env.repo.startswith('ssh+svn'):
+        run('cd %(path)s; svn co %(repo)s' % env)
+    tool = config.sites['main'].get('checkout',{}).get('tool')
+    if tool == "buildout":
+        run('cd %(path)s; sh init.sh -c %(buildout_cfg)s' % env)
+        django_utils.append_settings()
+    elif tool == "virtualenv":
+        import virtualenv
+        virtualenv.create()
+        django_utils.append_settings()
+        django_utils.manage("syncdb --all --noinput")
+        django_utils.manage("migrate --fake")
+    else:
+        print "WARNING: Couldnt find [checkout] tool - please set it to either virtualenv or buildout in your config.ini"
+        print "Got tool: %s" % tool
+        django_utils.append_settings()
+    supervisor_update_config_file()
+    nginx_update_config_file()
+
+
+@task
+def upload_ssl():
+    """
+    Upload the SSL key and certificate to the directories and with the filenames
+    specified in your settings.
+    """
+    for site, site_dict in config.sites.items():
+        ssl_key_path = prompt("SSL Key path (%s):" % site)
+        ssl_cert_path = prompt("SSL Certificate path (%s):" % site)
+        put(ssl_key_path, site_dict.get("deployment").get("ssl_key_path"))
+        put(ssl_cert_path, site_dict.get("deployment").get("ssl_cert_path"))
---
+++ packages/dploi-fabric/dploi-fabric-0.1.31.tar.gz/dploi-fabric-0.1.31/dploi_fabric/supervisor.py
@@ -0,0 +1,101 @@
+import StringIO
+from copy import copy
+from fabric.decorators import task
+from fabric.api import run, put, env
+from dploi_fabric.toolbox.template import render_template
+from dploi_fabric.utils import config
+import posixpath
+
+
+@task
+def stop():
+    for site, site_config in config.sites.items():
+        run('sudo supervisorctl stop %s:*' %  get_group_name(site, site_config))
+
+
+@task
+def start():
+    for site, site_config in config.sites.items():
+        run('sudo supervisorctl start %s:*' % get_group_name(site, site_config))
+
+
+@task
+def restart():
+    for site, site_config in config.sites.items():
+        run('sudo supervisorctl restart %s:*' % get_group_name(site, site_config))
+
+
+@task
+def status():
+    """
+    print status of the supervisor process
+
+    Note: "status" does not yet support the group syntax
+    """
+    for site, site_config in config.sites.items():
+        group_name = get_group_name(site, site_config)
+        for process_name, process_cmd in site_config.processes.items():
+            run('sudo supervisorctl status %s:%s' % (group_name, process_name))
+
+
+@task
+def add():
+    for site, site_config in config.sites.items():
+        group_name = get_group_name(site, site_config)
+        for process_name, process_cmd in site_config.processes.items():
+            run('sudo supervisorctl add %s:%s' % (group_name, process_name))
+
+
+@task
+def update():
+    for site, site_config in config.sites.items():
+        group_name = get_group_name(site, site_config)
+        for process_name, process_cmd in site_config.processes.items():
+            run('sudo supervisorctl update %s:%s' % (group_name, process_name))
+
+
+def get_group_name(site, site_config):
+    return '%s-%s' % (site_config['deployment']['user'], site)
+
+
+@task
+def update_config_file(dryrun=False, update_command=update):
+    output = ''
+    groups = {}
+    for site, site_config in config.sites.items():
+        template_path = site_config['supervisor']['template']
+        group_template_path = site_config['supervisor']['group_template']
+        group_name = get_group_name(site, site_config)
+        groups[group_name] = []
+        for process_name, process_dict in site_config.processes.items():
+            context_dict = copy(site_config)
+            env_dict = {
+                'HOME': site_config.deployment['home'],
+                'USER': site_config.deployment['user'],
+                'PYTHONPATH': ":".join([
+                    site_config.deployment['path'],
+                    posixpath.join(site_config.deployment['path'], site_config.get("django").get("base")+'/'),
+                ]),
+            }
+            env_dict.update(site_config.environment)
+            context_dict.update({
+                'process_name': process_name,
+                'process_cmd': process_dict["command"],
+                'socket': process_dict["socket"],
+                'env': env_dict,
+                'priority': process_dict.get('priority', 200),
+                'autostart': 'True' if getattr(env, 'autostart', True) else 'False',
+                'killasgroup': process_dict.get('killasgroup', None),
+                'stopasgroup': process_dict.get('killasgroup', None),
+                'stopwaitsecs': process_dict.get('stopwaitsecs', None),
+            })
+            output += render_template(template_path, context_dict)
+            groups[group_name].append(process_name)
+    output += render_template(group_template_path, {'groups': groups})
+    path = posixpath.abspath(posixpath.join(config.sites["main"].deployment['path'], '..', 'config', 'supervisor.conf'))
+    if dryrun:
+        print path + ':'
+        print output
+    else:
+        put(StringIO.StringIO(output), path)
+        update_command()
