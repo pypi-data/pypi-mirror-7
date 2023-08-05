929edeaa84e1207e5b870df3b5713fcb911ec104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package cell_acceptance/0.2.5/cell_acceptance-0.2.5.tar.gz

---
+++ packages/cell_acceptance/cell_acceptance-0.2.5.tar.gz/cell_acceptance-0.2.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='cell_acceptance',
+    version="0.2.5",
+    description='LibreOffice Calc Calculation Engine',
+    long_description='LibreOffice Calc Calculation Engine',
+    author='James Rakich',
+    author_email='james@fullandbydesign.com.au',
+    url='https://bitbucket.org/MalucoMarinero/cellacceptance',
+    packages=['cell_acceptance'],
+    install_requires=['flask', 'requests'],
+    scripts=['bin/cell-acceptance-load-calc'],
+    entry_points = {
+        'console_scripts': ['cell-acceptance=cell_acceptance.server:run_server']
+    },
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    license='MIT License',
+    zip_safe=False,
+    keywords='testing'
+)
