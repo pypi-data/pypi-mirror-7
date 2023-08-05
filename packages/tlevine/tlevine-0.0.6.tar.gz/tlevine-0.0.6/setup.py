c2ef980d7349f259807b832fc83abc6419ec9e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package tlevine/0.0.6/tlevine-0.0.6.tar.gz

---
+++ packages/tlevine/tlevine-0.0.6.tar.gz/tlevine-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+from tlevine import __version__
+
+setup(name='tlevine',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Get links to Tom things.',
+      url='https://github.com/tlevine/tlevine',
+      packages=['tlevine'],
+      install_requires = ['picklecache==0.0.3'],
+      tests_require = ['nose'],
+      version=__version__,
+      license='AGPL',
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+      ],
+)
