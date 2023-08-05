0b26a00da389f3907a7bd5cd33cec611e42a9de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package wdb.server/2.0.1/wdb.server-2.0.1.tar.gz

---
+++ packages/wdb.server/wdb.server-2.0.1.tar.gz/wdb.server-2.0.1/wdb.server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from wdb_server import server
+from tornado.ioloop import IOLoop
+from tornado.options import options
+from wdb_server.streams import handle_connection
+from tornado.netutil import bind_sockets, add_accept_handler
+from logging import getLogger
+
+
+log = getLogger('wdb_server')
+log.setLevel(10 if options.debug else 30)
+
+
+ioloop = IOLoop.instance()
+
+log.debug('Binding sockets')
+sockets = bind_sockets(options.socket_port)
+
+log.debug('Accepting')
+for socket in sockets:
+    add_accept_handler(socket, handle_connection, ioloop)
+
+
+log.debug('Listening')
+server.listen(options.server_port)
+
+log.debug('Starting loop')
+ioloop.start()
