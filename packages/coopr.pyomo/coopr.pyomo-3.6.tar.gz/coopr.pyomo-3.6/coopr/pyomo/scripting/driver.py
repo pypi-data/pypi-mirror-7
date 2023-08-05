91e795c8e9a4bfad86bd4ea10f50ad0f99a286f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:33 +0000

    Add package coopr.pyomo/3.6/coopr.pyomo-3.6.tar.gz

---
+++ packages/coopr.pyomo/coopr.pyomo-3.6.tar.gz/coopr.pyomo-3.6/coopr/pyomo/scripting/driver.py
@@ -0,0 +1,24 @@
+import argparse
+import coopr.misc.coopr_parser
+from coopr.pyomo import TransformationFactory
+import textwrap
+
+
+def main_exec(options):
+    wrapper = textwrap.TextWrapper()
+    wrapper.initial_indent = '      '
+    wrapper.subsequent_indent = '      '
+    print("")
+    for xform in sorted(TransformationFactory.services()):
+        print("  "+xform)
+        print(wrapper.fill(TransformationFactory.doc(xform)))
+
+
+#
+# Add a subparser for the check command
+#
+coopr.misc.coopr_parser.add_subparser('transformations',
+        func=main_exec, 
+        help='List the available model transformations.',
+        description='This coopr subcommand is used to list the available model transformations.'
+        )
---
+++ packages/coopr.pyomo/coopr.pyomo-3.6.tar.gz/coopr.pyomo-3.6/setup.py
@@ -0,0 +1,79 @@
+#  _________________________________________________________________________
+#
+#  Coopr: A COmmon Optimization Python Repository
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  _________________________________________________________________________
+
+"""
+Script to generate the installer for coopr.pyomo.
+"""
+
+import glob
+import os
+
+def _find_packages(path):
+    """
+    Generate a list of nested packages
+    """
+    pkg_list=[]
+    if not os.path.exists(path):
+        return []
+    if not os.path.exists(path+os.sep+"__init__.py"):
+        return []
+    else:
+        pkg_list.append(path)
+    for root, dirs, files in os.walk(path, topdown=True):
+        if root in pkg_list and "__init__.py" in files:
+            for name in dirs:
+                if os.path.exists(root+os.sep+name+os.sep+"__init__.py"):
+                    pkg_list.append(root+os.sep+name)
+    return [pkg for pkg in map(lambda x:x.replace(os.sep,"."), pkg_list)]
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+from setuptools import setup
+packages = _find_packages('coopr')
+
+setup(name='coopr.pyomo',
+      version='3.6',
+      maintainer='William E. Hart',
+      maintainer_email='wehart@sandia.gov',
+      url = 'https://software.sandia.gov/svn/public/coopr/coopr.pyomo',
+      license = 'BSD',
+      platforms = ["any"],
+      description = "Coopr's Pyomo math programming language",
+      long_description = read('README.txt'),
+      classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: Unix',
+            'Programming Language :: Python',
+            'Programming Language :: Unix Shell',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Scientific/Engineering'
+        ],
+      packages=packages,
+      keywords=['optimization'],
+      namespace_packages=['coopr'],
+      entry_points = """
+        [coopr.command]
+        coopr.check=coopr.pyomo.check.driver
+        coopr.pyomo=coopr.pyomo.scripting.pyomo
+        coopr.pyomo2nl=coopr.pyomo.scripting.convert
+        [console_scripts]
+        pyomo=coopr.pyomo.scripting.pyomo:main
+        pyomo2nl=coopr.pyomo.scripting.convert:pyomo2nl_main
+        pyomo2lp=coopr.pyomo.scripting.convert:pyomo2lp_main
+        pyomo2osil=coopr.pyomo.scripting.convert:pyomo2osil_main
+        pyomo2dakota=coopr.pyomo.scripting.convert:pyomo2dakota_main
+      """
+      )
