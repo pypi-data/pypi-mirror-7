7c92ff3a5a54ea0820d3de3ae3aa62d3638a4cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package inquirer/0.5/inquirer-0.5.tar.gz

---
+++ packages/inquirer/inquirer-0.5.tar.gz/inquirer-0.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from inquirer import __version__
+
+
+def read_description():
+    with open('README.rst') as fd:
+        return fd.read()
+
+
+setup(name='inquirer',
+      version=__version__,
+      description="Collection of common interactive command line user interfaces, based on Inquirer.js",
+      long_description=read_description(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: User Interfaces',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      keywords='interface,command line',
+      author='Miguel Ángel García',
+      author_email='miguelangel.garcia@gmail.com',
+      url='https://github.com/magmax/python-inquirer',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'blessings==1.5.1',
+        'readchar==0.4',
+        ],
+      )
