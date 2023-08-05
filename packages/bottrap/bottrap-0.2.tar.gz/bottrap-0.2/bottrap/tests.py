1e0896d9d9ee29529fc1794572de0061d5849d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package bottrap/0.2/bottrap-0.2.tar.gz

---
+++ packages/bottrap/bottrap-0.2.tar.gz/bottrap-0.2/bottrap/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from bottrap.middleware import BotTrapMiddleware
+from bottrap.views import honeypot
+
+
+class BotTrapTestCase(TestCase):
+            
+    def test_middleware(self):
+        
+        class FakeRequest(object):
+            ip = "foo.foo.foo.foo"
+            
+            META = {
+                "REMOTE_ADDR": ip
+            }
+        
+        request = FakeRequest()        
+        response = BotTrapMiddleware().process_request(request)
+        self.assertEqual(response, None)
+        
+        response = honeypot(request)
+        self.assertEqual(response.status_code, 200)        
+        
+        response = BotTrapMiddleware().process_request(request)
+        self.assertEqual(response.status_code, 403)
+        
---
+++ packages/bottrap/bottrap-0.2.tar.gz/bottrap-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='bottrap',
+    version='0.2',
+    description="Provides a honeypot for bots to digest",
+    author="Danemco, LLC",
+    author_email='john@velocitywebworks.com',
+    url='https://bitbucket.org/freakypie/bottrap',
+    packages=find_packages()
+)
