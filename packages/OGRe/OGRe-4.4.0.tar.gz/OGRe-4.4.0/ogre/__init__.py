dbdc5a639cf607d7db47ed2145fba7469e6d7e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package OGRe/4.4.0/OGRe-4.4.0.tar.gz

---
+++ packages/OGRe/OGRe-4.4.0.tar.gz/OGRe-4.4.0/ogre/__init__.py
@@ -0,0 +1,15 @@
+"""OpenFusion GIS Retriever
+
+`ogre.test` -- subpackage for testing OGRe
+
+:mod:`ogre.api` -- module for getting data from public APIs
+
+:mod:`ogre.Twitter` -- module for getting data from Twitter
+
+:mod:`ogre.validation` -- module for parameter validation and sanitation
+
+"""
+
+from ogre.api import OGRe
+
+__version__ = "4.4.0"
---
+++ packages/OGRe/OGRe-4.4.0.tar.gz/OGRe-4.4.0/ogre/test/__init__.py
@@ -0,0 +1,9 @@
+"""OpenFusion GIS Retriever Tests
+
+:mod:`test_api` -- query handling tests
+
+:mod:`test_Twitter` -- Twitter interface tests
+
+:mod:`test_validation` -- parameter validation and sanitation tests
+
+"""
