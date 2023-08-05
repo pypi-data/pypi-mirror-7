57d60a8ceadec4112ef950d534603e53f2fcedb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package git-pre-commit-hook/0.0.8/git-pre-commit-hook-0.0.8.tar.gz

---
+++ packages/git-pre-commit-hook/git-pre-commit-hook-0.0.8.tar.gz/git-pre-commit-hook-0.0.8/git_pre_commit_hook/builtin_plugins/flake8_check.py
@@ -0,0 +1,27 @@
+"""Check files with flake8."""
+import flake8.main
+import re
+
+
+DEFAULTS = {
+    'ignore': 'E226',
+    'complexity': '10',
+}
+
+
+PYTHON_SHEBANG_REGEX = re.compile(r'''^#!.*python''')
+
+
+def check(file_staged_for_commit, options):
+    if file_staged_for_commit.path.endswith('.py') or \
+            PYTHON_SHEBANG_REGEX.search(file_staged_for_commit.contents):
+        status = flake8.main.check_code(
+            file_staged_for_commit.contents,
+            ignore=(
+                c for c in options.flake8_ignore.split(',') if c
+            ),
+            complexity=int(options.flake8_complexity),
+        )
+        return status == 0
+    else:
+        return True
