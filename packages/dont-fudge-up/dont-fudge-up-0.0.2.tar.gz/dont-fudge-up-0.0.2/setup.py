1015e25671d7f2b7b6a6e0c453a16487a68b8b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package dont-fudge-up/0.0.2/dont-fudge-up-0.0.2.tar.gz

---
+++ packages/dont-fudge-up/dont-fudge-up-0.0.2.tar.gz/dont-fudge-up-0.0.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = []
+
+tests_require = [
+    'flake8',
+]
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='dont-fudge-up',
+    version='0.0.2',
+    author='Adam Hitchcock',
+    author_email='adam@disqus.com',
+    url='https://github.com/NorthIsUp/dont-fudge-up',
+    description='Find operations that might fudge up production, like a pdb.set_trace',
+    license='BSD',
+    entry_points={
+        'flake8.extension': [
+            'D4 = dfu.flake8.check:DFUChecker',
+        ],
+    },
+    long_description=long_description,
+    packages=find_packages(exclude=['tests']),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'tests': tests_require,
+    },
+    zip_safe=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
