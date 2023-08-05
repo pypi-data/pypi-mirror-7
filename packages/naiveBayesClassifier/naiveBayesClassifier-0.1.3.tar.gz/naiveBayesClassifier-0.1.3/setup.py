33141aabc9f8cec208e14ccce0e4ae210b3fe897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package naiveBayesClassifier/0.1.3/naiveBayesClassifier-0.1.3.tar.gz

---
+++ packages/naiveBayesClassifier/naiveBayesClassifier-0.1.3.tar.gz/naiveBayesClassifier-0.1.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='naiveBayesClassifier',
+      version='0.1.3',
+      license='MIT',
+      description='yet another general purpose naive bayesian classifier',
+      long_description=open('README.md').read(),
+      url='https://github.com/muatik/naive-bayes-classifier',
+      author='Mustafa Atik',
+      author_email='muatik@gmail.com',
+      maintainer='Nejdet Yucesoy',
+      maintainer_email='nejdetyucesoy@gmail.com',
+      packages=['naiveBayesClassifier'],
+      platforms='any')
