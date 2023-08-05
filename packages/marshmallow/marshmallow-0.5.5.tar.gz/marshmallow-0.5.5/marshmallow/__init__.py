1bd7a19d5d53bd7988ce2690f1695733551223c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package marshmallow/0.5.5/marshmallow-0.5.5.tar.gz

---
+++ packages/marshmallow/marshmallow-0.5.5.tar.gz/marshmallow-0.5.5/marshmallow/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+__version__ = '0.5.5'
+__author__ = 'Steven Loria'
+__license__ = "MIT"
+
+from marshmallow.serializer import Serializer
+
+from marshmallow.utils import pprint
+
+__all__ = [
+    'Serializer',
+    'pprint'
+]
