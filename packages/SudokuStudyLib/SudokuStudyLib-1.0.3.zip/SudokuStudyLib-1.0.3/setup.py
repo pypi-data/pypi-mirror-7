e26bb28e6cfa7181e62a37b28e9f83081eeac1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package SudokuStudyLib/1.0.3/SudokuStudyLib-1.0.3.zip

---
+++ packages/SudokuStudyLib/SudokuStudyLib-1.0.3.zip/SudokuStudyLib-1.0.3/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = []
+
+setup(name='SudokuStudyLib',
+      version='1.0.3',
+      description='Sudoku Solving Library',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Environment :: Console",
+          "Programming Language :: Python",
+          "Topic :: Games/Entertainment :: Puzzle Games",
+          "Topic :: Education",
+      ],
+      author='Robert J. Hwang',
+      author_email='RobertOfTaiwan@gmail.com',
+      keywords='sudoku, python, studying',
+      packages=['sudoku', 'matrix'],
+      url = "https://github.com/RobertOfTaiwan/SudokuStudyLib",
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+)
+
