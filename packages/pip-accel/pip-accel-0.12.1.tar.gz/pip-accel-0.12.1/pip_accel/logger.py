4492de595a5f2515162ef852c54e16e5a5d0a72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:01 +0000

    Add package pip-accel/0.12.1/pip-accel-0.12.1.tar.gz

---
+++ packages/pip-accel/pip-accel-0.12.1.tar.gz/pip-accel-0.12.1/pip_accel/logger.py
@@ -0,0 +1,15 @@
+# Logging for the pip accelerator.
+#
+# Author: Peter Odding <peter.odding@paylogic.eu>
+# Last Change: August 7, 2013
+# URL: https://github.com/paylogic/pip-accel
+
+"""
+Logging for the pip accelerator.
+"""
+
+# Standard library modules.
+import logging
+
+# Initialize the logging subsystem.
+logger = logging.getLogger('pip-accel')
