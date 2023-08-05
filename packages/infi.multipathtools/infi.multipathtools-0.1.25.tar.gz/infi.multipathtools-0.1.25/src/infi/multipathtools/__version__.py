1bfe77ae0abc6f67332e61e077ac69e4387a2aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:08 +0000

    Add package infi.multipathtools/0.1.25/infi.multipathtools-0.1.25.tar.gz

---
+++ packages/infi.multipathtools/infi.multipathtools-0.1.25.tar.gz/infi.multipathtools-0.1.25/src/infi/multipathtools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.25"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.25)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2d2902edea20582af51941bfc284d8058a0d9f77'
+__git_head_subject__ = 'Finished release v0.1.25.'
+__git_head_message__ = '* release/v0.1.25:\n  parse_multipaths_topology: adding _ to the alias regex\n  buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_dirty_diff__ = ''
