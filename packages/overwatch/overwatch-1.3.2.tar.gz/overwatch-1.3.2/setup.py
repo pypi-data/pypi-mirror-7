2c96c8df99c26ceaa85cb8310c11af1c1a9afc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:29 +0000

    Add package overwatch/1.3.2/overwatch-1.3.2.tar.gz

---
+++ packages/overwatch/overwatch-1.3.2.tar.gz/overwatch-1.3.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+'''Overwatch: Python Logging Browser.'''
+
+from distutils.core import setup
+from setuptools import find_packages
+
+classifiers = '''
+Development Status :: 4 - Beta
+Environment :: Console
+Environment :: X11 Applications :: GTK
+Intended Audience :: Developers
+Intended Audience :: End Users/Desktop
+Intended Audience :: System Administrators
+License :: OSI Approved :: GNU General Public License v2 (GPLv2)
+Operating System :: POSIX :: Linux
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Debuggers
+Topic :: System :: Monitoring
+'''
+
+doclines = __doc__.split('\n')
+
+long_desc = open('README.rst', 'r').read()
+
+setup(name='overwatch',
+        version='1.3.2',
+        author='Andrew Dunai',
+        author_email='andunai@gmail.com',
+        url='http://andrewdunai.com/projects/overwatch/',
+        packages=find_packages(),
+        package_data={'overwatch': ['data/*']},
+        platforms=['unix'],
+        classifiers=filter(None, classifiers.split("\n")),
+        description=doclines[0],
+        long_description=long_desc,
+     )
