2f251be9e917aebc2fe16b1fcaedad2b25b1e26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:37 +0000

    Add package openweather/0.10/openweather-0.10.tar.gz

---
+++ packages/openweather/openweather-0.10.tar.gz/openweather-0.10/setup.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+from setuptools import setup
+
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = ''
+
+setup(name='openweather',
+      version='0.10',
+      description='OpenWeatherMap.org API wrapper',
+      long_description=description,
+      author='Marian Steinbach',
+      author_email='marian@sendung.de',
+      url='http://github.com/marians/openweather',
+      py_modules=['openweather'],
+      install_requires=['daterangestr'],
+      entry_points={
+        'console_scripts': [
+            'openweather = openweather:main'
+        ]
+      })
