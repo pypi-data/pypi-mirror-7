07b4ec5a5f0fda3a6ec2a235686b3500fc4f9369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:57 +0000

    Add package mak/1.1.0/mak-1.1.0.zip

---
+++ packages/mak/mak-1.1.0.zip/mak-1.1.0/mak.py
@@ -0,0 +1,11 @@
+def maksimum(a,b,c):
+    if a>b:
+        if a>c:
+            print("maks = ",a)
+        else:
+            print("maks = ",c)
+    else:
+        if b>c:
+            print("maks = ",b)
+        else:
+            print("maks = ",c)
---
+++ packages/mak/mak-1.1.0.zip/mak-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'mak',
+    version = '1.1.0',
+    py_modules = ['mak'],
+    author = 'yusif',
+    author_email = 'yusifedu@gmail.com',
+    url = 'http://www.alizadey.com',
+    description = 'A simple maximum finder',
+    )
