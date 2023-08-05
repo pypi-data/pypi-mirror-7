b2e54a4919fa0e87ad2bc51c886735b49d58d1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:47 +0000

    Add package PySolarized/1.4/PySolarized-1.4.tar.gz

---
+++ packages/PySolarized/PySolarized-1.4.tar.gz/PySolarized-1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name="PySolarized",
+      version="1.4",
+      description="A simple library for accessing Apache Solr full-text search engine. Allows updating and queries over"
+                  "multiple cores.",
+      author="Jernej Virag",
+      author_email="jernej@virag.si",
+      packages=["pysolarized"],
+      license="MIT",
+      install_requires=["requests", "httpcache"],
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Topic :: Software Development :: Libraries",
+          "Topic :: Internet :: WWW/HTTP :: Indexing/Search"
+])
