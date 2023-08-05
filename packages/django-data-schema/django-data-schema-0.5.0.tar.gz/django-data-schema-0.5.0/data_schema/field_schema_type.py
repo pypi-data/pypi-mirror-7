b630658d3ab6ea5de8c5042cf48f94dbe97274a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:11 +0000

    Add package django-data-schema/0.5.0/django-data-schema-0.5.0.tar.gz

---
+++ packages/django-data-schema/django-data-schema-0.5.0.tar.gz/django-data-schema-0.5.0/data_schema/field_schema_type.py
@@ -0,0 +1,10 @@
+class FieldSchemaType(object):
+    """
+    Specifies all of the field schema types supported.
+    """
+    DATE = 'DATE'
+    DATETIME = 'DATETIME'
+    INT = 'INT'
+    FLOAT = 'FLOAT'
+    STRING = 'STRING'
+    BOOLEAN = 'BOOLEAN'
