3a5a0db0c8edaf2c0433350257a9dab59e65fc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:15 +0000

    Add package ftp2http/0.3.2/ftp2http-0.3.2.tar.gz

---
+++ packages/ftp2http/ftp2http-0.3.2.tar.gz/ftp2http-0.3.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='ftp2http',
+    version='0.3.2',
+    description='FTP to HTTP server',
+    author='Raymond Butcher',
+    author_email='randomy@gmail.com',
+    url='https://github.com/apn-online/ftp2http',
+    license='MIT',
+    packages=(
+        'ftp2http',
+    ),
+    scripts=(
+        'bin/ftp2http',
+    ),
+    install_requires=(
+        'argparse',
+        'pyftpdlib',
+        'pyOpenSSL',
+        'py-bcrypt',
+        'python-swiftclient==1.9.0',
+    ),
+)
