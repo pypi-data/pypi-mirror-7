59198eb89843452c500af0b5d5cf29271da5e410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package PyPDFLite/0.1.32/PyPDFLite-0.1.32.tar.gz

---
+++ packages/PyPDFLite/PyPDFLite-0.1.32.tar.gz/PyPDFLite-0.1.32/pypdflite/pdfobjects/pdfline.py
@@ -0,0 +1,18 @@
+from pdfdraw import PDFDraw
+
+
+class PDFLine(PDFDraw):
+
+    def __init__(self, session, page, cursor_start, cursor_end, color=None, style=None, size=1):
+        super(PDFLine, self).__init__(session, page, color, style, size=size)
+        self.start = cursor_start
+        self.end = cursor_end
+
+    def _draw(self):
+        self._draw_color()
+        if self.style is not None:
+            self._draw_style()
+        self._draw_line_size()
+        s = '%.2f %.2f m %.2f %.2f l %s' % (
+            self.start.x, self.start.y_prime, self.end.x, self.end.y_prime, self.stroke)
+        self.session._out(s, self.page)
