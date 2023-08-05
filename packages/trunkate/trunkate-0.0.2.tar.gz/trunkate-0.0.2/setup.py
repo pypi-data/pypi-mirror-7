29fcae0ba5b95f47320b4f81ce5b30ff059dc381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:07 +0000

    Add package trunkate/0.0.2/trunkate-0.0.2.tar.gz

---
+++ packages/trunkate/trunkate-0.0.2.tar.gz/trunkate-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup	
+
+setup(
+	name='trunkate',
+	version='0.0.2',
+	description='truncate string',
+	author='yangchen',
+	author_email='yuhan534@126.com',
+	url='https://github.com/limijiaoyin/truncate',
+	install_requires=[],
+	py_modules=['truncate'],
+	packages=[],
+	scripts=[],
+	keywords=['string', 'truncate', 'utilities']
+)
+
