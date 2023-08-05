584c536920a42ba2b1f22bdc05a6025cf3612575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package hubspot/0.1.5dev/hubspot-0.1.5dev.tar.gz

---
+++ packages/hubspot/hubspot-0.1.5dev.tar.gz/hubspot-0.1.5dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='hubspot',
+    version='0.1.5dev',
+    packages=['hubspot',],
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+)
