3b79fe0c03a4de16eb60a073e5186b162e0f4e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package onion-proctor/0.1.1/onion-proctor-0.1.1.tar.gz

---
+++ packages/onion-proctor/onion-proctor-0.1.1.tar.gz/onion-proctor-0.1.1/TODO.md
@@ -0,0 +1,5 @@
+TODO
+====
+
+Use tor control port when possible instead of brutally killing the process.
+Or use stem (https://stem.torproject.org/).
---
+++ packages/onion-proctor/onion-proctor-0.1.1.tar.gz/onion-proctor-0.1.1/setup.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import proctor
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='onion-proctor',
+      version=proctor.__version__,
+      author=proctor.__author__,
+      author_email=proctor.__email__,
+      maintainer=proctor.__maintainer__,
+      maintainer_email=proctor.__email__,
+      url=proctor.__url__,
+      description='Proctor is an HTTP proxy that will distribute requests '
+                  'across a number of managed Tor circuits',
+      long_description=read('README.md'),
+      license='LICENSE.txt',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['desub', 'pymiproxy', 'SocksiPy-branch'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Plugins',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Documentation',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Internet :: WWW/HTTP'],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'proctor = proctor.scripts:main']})
