32fd49fe736061848130b0ada4afc634b75aecaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:26 +0000

    Add package mkwheelhouse/0.1.1/mkwheelhouse-0.1.1.tar.gz

---
+++ packages/mkwheelhouse/mkwheelhouse-0.1.1.tar.gz/mkwheelhouse-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='mkwheelhouse',
+    version='0.1.1',
+    author='Nikhil Benesch',
+    author_email='benesch@whoop.com',
+    py_modules=['mkwheelhouse'],
+    url='https://github.com/WhoopInc/mkwheelhouse',
+    description='Amazon S3 wheelhouse generator',
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools'
+    ],
+    install_requires=[
+        "awscli >= 1.3.6",
+        "yattag >= 0.9.2",
+        "wheel >= 0.23.0",
+        "pip >= 1.5.4",
+    ],
+    entry_points={
+        'console_scripts': [
+            'mkwheelhouse=mkwheelhouse:main'
+        ],
+    }
+)
