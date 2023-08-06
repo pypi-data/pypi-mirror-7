8d666a7350b3aa3972703555491f471ad4c3b0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:27 +0000

    Add package socket.io-emitter/0.1.2/socket.io-emitter-0.1.2.tar.gz

---
+++ packages/socket.io-emitter/socket.io-emitter-0.1.2.tar.gz/socket.io-emitter-0.1.2/setup.py
@@ -0,0 +1,16 @@
+__author__ = 'ziyasal'
+
+from setuptools import setup
+
+setup(
+    name='socket.io-emitter',
+    version='0.1.2',
+    author='Ziya SARIKAYA',
+    author_email='sarikayaziya@gmail.com',
+    packages=['emitter'],
+    url='https://github.com/ziyasal/socket.io-python-emitter',
+    license='LICENSE',
+    description='A Python implementation of socket.io-emitter',
+    install_requires=['msgpack-python', 'redis'],
+    include_package_data=True
+)
