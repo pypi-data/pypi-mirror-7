da23a57e56f5274185ced8ca3bdf3bfc6953d22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package athletelistyy/1.5.0/athletelistyy-1.5.0.zip

---
+++ packages/athletelistyy/athletelistyy-1.5.0.zip/athletelistyy-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+        name = 'athletelistyy',
+        version = '1.5.0',
+        py_modules = ['athletelist'],
+        author = 'sunyingying',
+        author_email = 'sunyingying@sun.com',
+        url = 'http://www.sunyingying.com',
+        description = 'AthleteList',
+
+    )
