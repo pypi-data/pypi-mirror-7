37a4500a736c52dc4f2e88f7db43274f37522ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:28 +0000

    Add package LightWriter/1.0.1/LightWriter-1.0.1.tar.gz

---
+++ packages/LightWriter/LightWriter-1.0.1.tar.gz/LightWriter-1.0.1/bin/example.py
@@ -0,0 +1,49 @@
+import time
+from lightwriter import LightWriter, Frame
+
+
+def main():
+    lw = LightWriter()
+
+    while True:
+        # Set all lights to Blue for 1 second.
+        frame = Frame()
+        frame.set_color_by_name('blue')
+        lw.write_frame(frame)
+        time.sleep(1)
+
+        # Make a gradient of Blue across the strip and rotate it through.
+        frame.gradiant()
+        lw.write_frame(frame)
+        lw.rotate()
+
+        # Turn off all lights fr 1 second.
+        lw.clear()
+        time.sleep(1)
+
+        # Set an exact RGB value for 3 seconds.
+        frame = Frame()
+        frame.set_color_by_rgb(50, 100, 150)
+        lw.write_frame(frame)
+        time.sleep(3)
+
+        # Set the color of a specific node on the strip (there are 10, 0 indexed).
+        frame.set_color_by_name('purple', node=9)
+        lw.write_frame()
+        time.sleep(3)
+
+        # Shift the purple node back and forth
+        lw.back_and_forth()
+
+        # Set a few color groups
+        frame.set_node_colors_by_name(
+            ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+        )
+        lw.write_frame(frame)
+
+        # Go Crazy
+        lw.random()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/LightWriter/LightWriter-1.0.1.tar.gz/LightWriter-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LightWriter',
+    version='1.0.1',
+    author='Mike Kadin',
+    author_email='michaelkadin@gmail.com',
+    packages=find_packages(),
+    scripts=['bin/example.py'],
+    url='http://pypi.python.org/pypi/LightWriter/',
+    license='LICENSE.txt',
+    description='Control a RadioShack Light Strip with an Arduino and Python',
+    long_description=open('README.md').read(),
+    install_requires=[req.strip() for req in open('requirements.txt').readlines()],
+)
