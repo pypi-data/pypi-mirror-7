391dc224643503147eca31220e310ee5c64ace68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:06 +0000

    Add package whelk/2.3/whelk-2.3.tar.gz

---
+++ packages/whelk/whelk-2.3.tar.gz/whelk-2.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = "whelk",
+      version = "2.3",
+      author = "Dennis Kaarsemaker",
+      author_email = "dennis@kaarsemaker.net",
+      url = "http://github.com/seveas/whelk",
+      description = "Easy access to shell commands from python",
+      packages = ["whelk"],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: zlib/libpng License',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ]
+)
