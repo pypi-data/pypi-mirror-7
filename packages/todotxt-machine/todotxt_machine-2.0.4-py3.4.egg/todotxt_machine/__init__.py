7e39155734bfbf6eb798e3c1e4a68a884cd3a887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:05 +0000

    Add package todotxt-machine/2.0.4/todotxt_machine-2.0.4-py3.4.egg

---
+++ packages/todotxt-machine/todotxt_machine-2.0.4-py3.4.egg/todotxt_machine/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+todotxt_machine
+~~~~~~~~~~~~~~~
+
+An interactive terminal based todo.txt file editor with an interface similar to mutt
+
+"""
+
+__name__ = "todotxt_machine"
+__title__ = "todotxt_machine"
+__author__ = "Anthony DiGirolamo"
+__license__ = "GPL3"
+__version__ = (2, 0, 4)
+
+version = "%s.%s.%s" % __version__
+
+__all__ = ["cli", "todo", "colorscheme", "urwid_ui", "terminal_operations"]
