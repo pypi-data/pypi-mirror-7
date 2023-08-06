f2c4bc316b171e2f1d75efc407056bb27bbcd86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:25 +0000

    Add package codplayer/0.9.1/codplayer-0.9.1-py2.7-linux-x86_64.egg

---
+++ packages/codplayer/codplayer-0.9.1-py2.7-linux-x86_64.egg/codplayer/__init__.py
@@ -0,0 +1,19 @@
+# codplayer supporting package
+#
+# Copyright 2013-2014 Peter Liljenberg <peter.liljenberg@gmail.com>
+#
+# Distributed under an MIT license, please see LICENSE in the top dir.
+
+# Don't include the audio device modules in the list of modules,
+# as they may not be available on all systems
+
+__all__ = [
+    'audio',
+    'config',
+    'db',
+    'model',
+    'player',
+    'serialize',
+    ]
+
+version = '0.1'
