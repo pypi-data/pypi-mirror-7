7cb90e721a2000a7409f14717d433cfc3145d2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:05 +0000

    Add package Showtime-Python/1.0.5.1/Showtime-Python-1.0.5.1.tar.gz

---
+++ packages/Showtime-Python/Showtime-Python-1.0.5.1.tar.gz/Showtime-Python-1.0.5.1/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+readme = open('README.rst', 'r')
+README_TXT = readme.read()
+
+setup(name='Showtime-Python',
+      version='1.0.5.1',
+      description='Showtime-Python allows you to connect multiple programs for live performances using nodes.',
+      long_description=README_TXT,
+      author='Byron Mallett',
+      author_email='byronated@gmail.com',
+      url='http://github.com/Mystfit/Showtime',
+      license='MIT',
+      install_requires=["pyzmq==14.3.1"],
+      packages=find_packages(),
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: Microsoft :: Windows :: Windows 7',
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: OS Independent',
+      'Environment :: Console',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: C#',
+      'Programming Language :: Java',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Software Development :: Object Brokering',
+      'Intended Audience :: Developers',
+        'Natural Language :: English'
+      ],
+      scripts=['scripts/st-stage.py', 
+      			'scripts/st-methodeditor.py',
+      			'scripts/st-sinewriter.py',
+      			'scripts/st-methodsubscriber.py']
+      )
