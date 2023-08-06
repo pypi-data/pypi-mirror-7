20613767dd8d02ce460e4fc42c03f63a8e5ddcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:37 +0000

    Add package haven/1.0.19/haven-1.0.19.tar.gz

---
+++ packages/haven/haven-1.0.19.tar.gz/haven-1.0.19/haven/request.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from .log import logger
+
+
+class Request(object):
+    """
+    请求
+    """
+
+    conn = None
+    box_class = None
+    raw_data = None
+    box = None
+    is_valid = False
+
+    def __init__(self, conn, box_class, raw_data):
+        # 为了书写方便
+        self.close = conn.close
+        self.write = conn.write
+
+        self.conn = conn
+        self.box_class = box_class
+        self.raw_data = raw_data
+        self.is_valid = self._parse_raw_data()
+
+    def _parse_raw_data(self):
+        try:
+            self.box = self.box_class()
+        except Exception, e:
+            logger.error('parse raw_data fail. e: %s, request: %s', e, self)
+            return False
+
+        if self.box.unpack(self.raw_data) > 0:
+            return True
+        else:
+            logger.error('unpack fail. request: %s', self)
+            return False
+
+    @property
+    def app(self):
+        return self.conn.app
+
+    @property
+    def address(self):
+        return self.conn.address
+
+    @property
+    def cmd(self):
+        try:
+            return self.box.cmd
+        except:
+            return None
+
+    def make_rsp(self, **kwargs):
+        """
+        生成响应
+        """
+        assert self.box is not None
+
+        return self.box.map(**kwargs)
+
+    def echo(self, **kwargs):
+        """
+        快速响应
+        """
+        self.write(self.make_rsp(**kwargs))
+
+    def __repr__(self):
+        return repr(self.raw_data)
