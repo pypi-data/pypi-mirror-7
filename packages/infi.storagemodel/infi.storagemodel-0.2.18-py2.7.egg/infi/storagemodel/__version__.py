fc16595018a4a4894d68a2f52cc083c2407ff4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:31 +0000

    Add package infi.storagemodel/0.2.18/infi.storagemodel-0.2.18-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.18-py2.7.egg/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.18"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.18)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'e643695b568788ee4804682a250441cb733ddacd'
+__git_head_subject__ = 'fix indentation'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
