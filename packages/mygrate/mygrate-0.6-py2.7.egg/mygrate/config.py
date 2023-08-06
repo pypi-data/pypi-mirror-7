0027fc7773cb62834d269df26037d047452e9e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package mygrate/0.6/mygrate-0.6-py2.7.egg

---
+++ packages/mygrate/mygrate-0.6-py2.7.egg/mygrate/config.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2013 Ian C. Good
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import os
+import os.path
+from ConfigParser import SafeConfigParser, NoSectionError, NoOptionError
+
+from .exceptions import MygrateError
+
+
+class MygrateConfigError(MygrateError):
+    """Informational exception for bad MyGrate configuration."""
+    pass
+
+
+class MygrateConfig(object):
+
+    def __init__(self, section='mygrate'):
+        self.section = section
+        self.parser = SafeConfigParser()
+        self.parser.optionxform = str
+
+        fnames = self._get_paths()
+        if not self.parser.read(fnames):
+            raise MygrateConfigError('No valid config files in '+repr(fnames))
+
+    def _get_paths(self):
+        env_config = os.getenv('MYGRATE_CONFIG', None)
+        if env_config:
+            return [os.path.expanduser(env_config)]
+        else:
+            home_config = os.path.expanduser('~/.mygrate.conf')
+            global_config = '/etc/mygrate.conf'
+            return [home_config, global_config]
+
+    def call_entry_point(self, callbacks):
+        try:
+            entry_point = self.parser.get(self.section, 'entry_point')
+        except (NoSectionError, NoOptionError):
+            msg = 'Please specify entry_point in configuration'
+            raise MygrateConfigError(msg)
+        mod_name, attr_name = entry_point.rsplit(':', 1)
+        mod = __import__(mod_name, fromlist=[attr_name])
+        func = getattr(mod, attr_name)
+        func(callbacks, self)
+
+    def get_mysql_connection_info(self, section=None):
+        section = section or self.section
+        ret = {}
+        mapping = {'host': 'host',
+                   'port': 'port',
+                   'user': 'user',
+                   'passwd': 'password',
+                   'db': 'database',
+                   'unix_socket': 'unix_socket'}
+        for key, value in mapping.items():
+            try:
+                ret[key] = self.parser.get(section, value)
+            except (NoSectionError, NoOptionError):
+                pass
+        return ret
+
+    def get_mysql_binlog_info(self):
+        try:
+            index_file = self.parser.get(self.section, 'index_file')
+        except (NoSectionError, NoOptionError):
+            index_file = '/var/lib/mysql/mysql-bin.index'
+        if not os.path.exists(index_file):
+            raise MygrateConfigError('Invalid binlog index file: '+index_file)
+        try:
+            delay = self.parser.getfloat(self.section, 'tracking_delay')
+        except (NoSectionError, NoOptionError):
+            delay = 1.0
+        return index_file, float(delay)
+
+    def get_tracking_dir(self):
+        try:
+            tracking_dir = self.parser.get(self.section, 'tracking_dir')
+        except (NoSectionError, NoOptionError):
+            tracking_dir = os.path.expanduser('~/.binlog-tracking')
+            try:
+                os.makedirs(tracking_dir)
+            except OSError:
+                pass
+        tracking_dir = os.path.expanduser(tracking_dir)
+        if not os.path.isdir(tracking_dir):
+            msg = 'Tracking directory does not exist: '+tracking_dir
+            raise MygrateConfigError(msg)
+        return tracking_dir
+
+
+cfg = MygrateConfig()
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
---
+++ packages/mygrate/mygrate-0.6-py2.7.egg/mygrate/query.py
@@ -0,0 +1,136 @@
+# Copyright (c) 2013 Ian C. Good
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+
+import os
+import os.path
+import sys
+import logging
+import optparse
+import cPickle
+import MySQLdb
+import MySQLdb.cursors
+
+
+class InitialQuery(object):
+    """Manages direct queries to MySQL for importing, validation, and
+    re-validation.
+
+    """
+
+    def __init__(self, mysql_info, callbacks, action='INSERT'):
+        self.mysql_info = mysql_info
+        self.callbacks = callbacks
+        self.action = action
+        self.log = logging.getLogger('mygrate.query')
+
+    def run_callback(self, table, cols):
+        """Executes the INSERT callback for the given table.
+
+        :param table: The table being imported from.
+        :param cols: The column dict containing the row data.
+
+        """
+        self.callbacks.execute(table, self.action, cols)
+
+    def get_connection(self, db, streaming=True):
+        """Creates and returns a connection to the MySQL server and selects the
+        given database.
+
+        :param db: The database name to select after connection.
+        :param streaming: Whether or not a streaming MySQL cursor should be
+                          used, for large data sets.
+        :returns: A MySQL connection object.
+
+        """
+        kwargs = self.mysql_info.copy()
+        kwargs['db'] = db
+        kwargs['charset'] = 'utf8'
+        if streaming:
+            kwargs['cursorclass'] = MySQLdb.cursors.SSDictCursor
+        else:
+            kwargs['cursorclass'] = MySQLdb.cursors.DictCursor
+        conn = MySQLdb.connect(**kwargs)
+        return conn
+
+    def process_table(self, full_table):
+        """Does a streaming, full-table SELECT on the requested table to
+        retrieve every row. Rows are then passed to `run_callback()`.
+
+        :param full_table: The database and table names, separated by a period,
+                           as given on the command line.
+
+        """
+        db, table = full_table.split('.')
+        conn = self.get_connection(db)
+        cur = conn.cursor()
+        try:
+            sql = 'SELECT * FROM `{0}`'.format(table)
+            cur.execute(sql)
+            while True:
+                rows = cur.fetchmany(1000)
+                if not rows:
+                    break
+                for row in rows:
+                    self.run_callback(full_table, row)
+        except Exception:
+            self.log.exception('Unhandled exception')
+        finally:
+            cur.close()
+            conn.close()
+
+
+def main():
+    description = """\
+This program attempts to import an entire table by creating job tasks for each
+row.
+
+Configuration for %prog is done with configuration files. This is either
+/etc/mygrate.conf, ~/.mygrate.conf, or an alternative specified by the
+MYGRATE_CONFIG environment variable.
+
+If no tables are given in the command-line arguments, all tables that have
+registered callbacks are queried.
+"""
+    usage = 'usage: %prog [options] [<database>.<table> ...]'
+    op = optparse.OptionParser(usage=usage, description=description)
+    options, requested_tables = op.parse_args()
+
+    from .config import cfg
+    from .callbacks import MygrateCallbacks
+
+    callbacks = MygrateCallbacks()
+    mysql_info = cfg.get_mysql_connection_info()
+    cfg.call_entry_point(callbacks)
+
+    if not requested_tables:
+        requested_tables = callbacks.get_registered_tables()
+
+    query = InitialQuery(mysql_info, callbacks)
+    for table in requested_tables:
+        query.process_table(table)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
