43814e3ccac99cce7a2bc2c9313ec490b24d8cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:00 +0000

    Add package py_debug/0.0.0/py_debug-0.0.0.tar.gz

---
+++ packages/py_debug/py_debug-0.0.0.tar.gz/py_debug-0.0.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py_debug',
+    version='0.0.0',
+    description='A debug tool for noder',
+    url='http://github.com/poying/py_debug',
+    license='MIT',
+    author='Po-Ying Chen',
+    author_email='poying.me@gmail.com',
+    maintainer='Po-Ying Chen',
+    maintainer_email='poying.me@gmail.com',
+    packages=[
+        'py_debug'
+    ]
+)
