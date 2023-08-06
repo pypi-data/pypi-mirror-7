656af514013ccd6bb4919aa048a54b9de01b8165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:06 +0000

    Add package plushcap/0.2.2/plushcap-0.2.2.tar.gz

---
+++ packages/plushcap/plushcap-0.2.2.tar.gz/plushcap-0.2.2/README.rst
@@ -0,0 +1,23 @@
+===============================
+Plushcap
+===============================
+
+.. image:: https://travis-ci.org/makaimc/plushcap.png?branch=master
+        :target: https://travis-ci.org/makaimc/plushcap
+
+.. image:: https://pypip.in/d/plushcap/badge.png
+        :target: https://pypi.python.org/pypi/plushcap
+
+
+Plushcap monitors websites and alerts people via text or phone call if there 
+is a problem.
+
+* Free software: BSD license
+* Documentation: http://plushcap.readthedocs.org.
+
+Features
+--------
+
+* Continuously monitors a URL to determine if it is responding appropriately.
+* Sends an SMS alert to a number if the URL HTTP response status code is
+  anything other than 200 OK.
---
+++ packages/plushcap/plushcap-0.2.2.tar.gz/plushcap-0.2.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'plushcap'))
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'requests',
+]
+
+test_requirements = [
+    'requests',
+]
+
+setup(
+    name='plushcap',
+    version='0.2.2',
+    description='Plushcap monitors websites and alerts people via text or phone call if there is a problem.',
+    long_description=readme + '\n\n' + history,
+    author='Matt Makai',
+    author_email='mmakai@twilio.com',
+    url='https://github.com/makaimc/plushcap',
+    packages=[
+        'plushcap',
+    ],
+    package_dir={'plushcap':
+                 'plushcap'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='plushcap',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
