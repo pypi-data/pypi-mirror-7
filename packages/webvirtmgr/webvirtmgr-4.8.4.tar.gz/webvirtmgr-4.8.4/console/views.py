3fd8916a51dc1a23f4e9a9b75b0805c8162934da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:45 +0000

    Add package webvirtmgr/4.8.4/webvirtmgr-4.8.4.tar.gz

---
+++ packages/webvirtmgr/webvirtmgr-4.8.4.tar.gz/webvirtmgr-4.8.4/console/views.py
@@ -0,0 +1,45 @@
+import re
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+
+from instance.models import Instance
+from vrtManager.instance import wvmInstance
+
+
+WS_PORT = 6080
+
+
+def console(request):
+    """
+    VNC instance block
+    """
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/login')
+
+    if request.method == 'GET':
+        token = request.GET.get('token', '')
+
+    try:
+        temptoken = token.split('-', 1)
+        host = int(temptoken[0])
+        uuid = temptoken[1]
+        instance = Instance.objects.get(compute_id=host, uuid=uuid)
+        conn = wvmInstance(instance.compute.hostname,
+                           instance.compute.login,
+                           instance.compute.password,
+                           instance.compute.type,
+                           instance.name)
+        vnc_passwd = conn.get_vnc_passwd()
+    except:
+        vnc_passwd = None
+
+    wsproxy_port = WS_PORT
+    wsproxy_host = request.get_host()
+    if ':' in wsproxy_host:
+        wsproxy_host = re.sub(':[0-9]+', '', wsproxy_host)
+
+    response = render_to_response('console.html', locals(), context_instance=RequestContext(request))
+    response.set_cookie('token', token)
+    return response
