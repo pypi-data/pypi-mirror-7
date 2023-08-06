35708039d85d61abd168a53e9f761490df5bd78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:37 +0000

    Add package Django-Pizza/14.08.1/Django-Pizza-14.08.1.tar.gz

---
+++ packages/Django-Pizza/Django-Pizza-14.08.1.tar.gz/Django-Pizza-14.08.1/setup.py
@@ -0,0 +1,40 @@
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django-Pizza",
+    version = '14.08.1',
+    description = "Yet another Django CMS.",
+    url = "https://github.com/pizzapanther/Django-Pizza",
+    author = "Paul Bailey",
+    author_email = "paul.m.bailey@gmail.com",
+    license = "BSD",
+    packages = [
+      'pizza',
+      'pizza.kitchen_sink',
+      'pizza.kitchen_sink.migrations',
+      'pizza.kitchen_sink.templatetags',
+      'pizza.kitchen_sink.management',
+      'pizza.kitchen_sink.management.commands',
+      'pizza.blog',
+      'pizza.blog.migrations',
+      'pizza.blog.templatetags',
+      'pizza.calendar',
+      'pizza.calendar.migrations',
+    ],
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [
+      "sorl-thumbnail>=11.01",
+      "Django-Next-Please>=13.12",
+    ]
+)
