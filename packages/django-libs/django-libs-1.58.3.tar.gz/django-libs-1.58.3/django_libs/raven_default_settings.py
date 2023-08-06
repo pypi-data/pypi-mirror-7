33a413a431bda9976126a434f7953e989388d358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:49 +0000

    Add package django-libs/1.58.3/django-libs-1.58.3.tar.gz

---
+++ packages/django-libs/django-libs-1.58.3.tar.gz/django-libs-1.58.3/django_libs/raven_default_settings.py
@@ -0,0 +1,9 @@
+"""Default settings for the CustomRaven404CatchMiddleware"""
+RAVEN_IGNORABLE_USER_AGENTS = [
+    r'^.*EasouSpider.*$',
+    r'^.*Feedfetcher-Google.*$',
+    r'^.*Googlebot.*$',
+    r'^.*Twitterbot.*$',
+    r'^.*Yahoo! Slurp.*$',
+    r'^.*bingbot.*$',
+]
