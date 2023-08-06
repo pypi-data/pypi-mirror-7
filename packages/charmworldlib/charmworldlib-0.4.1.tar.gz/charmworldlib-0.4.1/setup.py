2974be0ff1ae9472011b88cc610d81444b473671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:27 +0000

    Add package charmworldlib/0.4.1/charmworldlib-0.4.1.tar.gz

---
+++ packages/charmworldlib/charmworldlib-0.4.1.tar.gz/charmworldlib-0.4.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Marco Ceppi.  This software is licensed under the
+# GNU General Public License version 3 (see the file LICENSE).
+
+from setuptools import setup
+
+
+setup(
+    name='charmworldlib',
+    version="0.4.1",
+    packages=['charmworldlib'],
+    maintainer='Marco Ceppi',
+    maintainer_email='marco@ceppi.net',
+    description=('Library to access charmworld data'),
+    license='GPL v3',
+    url='https://launchpad.net/charmworldlib',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
