945f1b5f2fde290618cfb64a2808de715719ba9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package infi.exceptools/0.2.7/infi.exceptools-0.2.7-py2.7.egg

---
+++ packages/infi.exceptools/infi.exceptools-0.2.7-py2.7.egg/infi/exceptools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2d81b0b2e7240cd0ce39bc4b07dc3ee91b7327a9'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
