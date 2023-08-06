93c9417167799d4833eb077256fb05e0b3e9cd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:43 +0000

    Add package boardgamegeek/0.0.7/boardgamegeek-0.0.7.tar.gz

---
+++ packages/boardgamegeek/boardgamegeek-0.0.7.tar.gz/boardgamegeek-0.0.7/boardgamegeek/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+"""
+.. module:: boardgamegeek
+   :platform: Unix, Windows
+   :synopsis: interface to boardgamegeek.com
+
+.. moduleauthor:: Cosmin Luță <q4break@gmail.com>
+"""
+from .api import BoardGameGeek
+from .version import __version__
+
+__all__ = [BoardGameGeek]
