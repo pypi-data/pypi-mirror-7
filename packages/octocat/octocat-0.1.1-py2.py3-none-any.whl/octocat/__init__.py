42ef616885cb3ca9767158c49b64d3b8b31e90d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:45 +0000

    Add package octocat/0.1.1/octocat-0.1.1-py2.py3-none-any.whl

---
+++ packages/octocat/octocat-0.1.1-py2.py3-none-any.whl/octocat/__init__.py
@@ -0,0 +1,9 @@
+""" octocat description. """
+
+# Package information
+# ===================
+
+__version__ = "0.1.1"
+__project__ = "octocat"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
