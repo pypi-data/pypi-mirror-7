85adbc0b2c184b0e3b7addf43d48f42787ce62f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package Toster/0.1.1/Toster-0.1.1-py3.4.egg

---
+++ packages/Toster/Toster-0.1.1-py3.4.egg/toster/manager.py
@@ -0,0 +1,53 @@
+from importlib import import_module
+
+
+class TestManager(object):
+
+    def __init__(self, runner):
+        self.tests = {}
+        self.groups = {}
+        self.runner = runner
+
+    def add_testcases(self, testcases):
+        for testcase in testcases:
+            self.add_testcase(testcase)
+
+    def add_testcase(self, testcase):
+        testcase = self.maybe_dotted(testcase)
+        key = testcase.__module__ + ':' + testcase.__name__
+        self.tests[key] = testcase
+
+        testcase.runner = self.runner
+
+        for name in testcase.groups:
+            group = self.groups.get(name, [])
+            group.append(testcase)
+            self.groups[name] = group
+
+    def maybe_dotted(self, testcase):
+        if type(testcase) is not str:
+            return testcase
+
+        package_url, class_name = testcase.split(':')
+        package = import_module(package_url)
+        return getattr(package, class_name)
+
+    def get_by_name(self, name):
+        return [
+            testcase
+            for fullname, testcase in self.tests.items()
+            if fullname.split(':')[1] == name
+        ]
+
+    def get_by_group(self, name):
+        return self.groups[name]
+
+    def get_by_module(self, module):
+        return [
+            testcase
+            for fullname, testcase in self.tests.items()
+            if fullname.startswith(module)
+        ]
+
+    def get_all(self):
+        return self.tests.values()
