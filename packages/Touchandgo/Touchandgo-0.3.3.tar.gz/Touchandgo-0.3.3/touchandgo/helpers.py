b4781f0f9973fcae83ef55ce50d677d27fd554e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:17 +0000

    Add package Touchandgo/0.3.3/Touchandgo-0.3.3.tar.gz

---
+++ packages/Touchandgo/Touchandgo-0.3.3.tar.gz/Touchandgo-0.3.3/touchandgo/helpers.py
@@ -0,0 +1,99 @@
+import os
+import re
+import signal
+import socket
+
+from daemon import DaemonContext
+from datetime import datetime
+from lock import Lock
+from os.path import getmtime
+from subprocess import PIPE, STDOUT, Popen
+
+from netifaces import interfaces, ifaddresses
+
+
+LOCKFILE = "/tmp/touchandgo"
+
+
+def get_free_port():
+    socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_.bind(('localhost', 0))
+    addr, port = socket_.getsockname()
+    socket_.close()
+    return port
+
+
+def get_interface():
+    for ifaceName in interfaces():
+        addresses = ifaddresses(ifaceName)
+        for address in addresses.values():
+            for item in address:
+                if item.get('netmask') is not None and \
+                        not item['addr'].startswith("127") and \
+                        not item['addr'].startswith(":"):
+                    return item['addr']
+
+
+def is_process_running(process_id):
+    try:
+        os.kill(process_id, 0)
+        return True
+    except OSError:
+        return False
+
+
+def execute(command):
+    process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
+    print("Running peerflix")
+    while True:
+        nextline = process.stdout.readline()
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                          nextline)
+        if len(urls):
+            print("streaming url: %s" % urls[0])
+            break
+        if nextline == '' and process.poll() is not None:
+            break
+
+    output = process.communicate()[0]
+    exitCode = process.returncode
+    #print output, exitCode
+
+
+def daemonize(args, callback):
+    with DaemonContext():
+        create_process = False
+        lock = Lock(LOCKFILE, os.getpid(), args.name, args.sea_ep[0],
+                    args.sea_ep[1], args.port)
+        if lock.is_locked():
+            lock_pid = lock.get_pid()
+            if not lock.is_same_file(args.name, args.sea_ep[0],
+                                     args.sea_ep[1]) \
+                    or not is_process_running(lock_pid):
+                try:
+                    os.kill(lock_pid, signal.SIGQUIT)
+                except OSError:
+                    pass
+                except TypeError:
+                    pass
+                lock.break_lock()
+                create_process = True
+        else:
+            create_process = True
+
+        if create_process:
+            lock.acquire()
+            callback(args.name, season=args.sea_ep[0], episode=args.sea_ep[1],
+                     serve=True, port=args.port)
+            lock.release()
+
+
+def get_lock_diff():
+    timediff = 0
+    try:
+        now = datetime.now()
+        timediff = now - datetime.fromtimestamp(getmtime(LOCKFILE + ".lock"))
+        timediff = timediff.total_seconds()
+    except OSError:
+        pass
+    return timediff
---
+++ packages/Touchandgo/Touchandgo-0.3.3.tar.gz/Touchandgo-0.3.3/touchandgo/server.py
@@ -0,0 +1,44 @@
+import argparse
+
+from os.path import abspath, dirname, join
+
+from subprocess import PIPE, STDOUT, Popen
+from time import sleep
+from flask import Flask, redirect
+
+from helpers import get_interface, get_lock_diff
+
+
+def serve(py_exec=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--python", default="python")
+    args = parser.parse_args()
+
+    py_exec = args.python
+
+    app = Flask("touchandgo")
+
+    @app.route("/<name>")
+    @app.route("/<name>/<season>/<episode>")
+    def redirect_to(name, season=None, episode=None):
+        interface = get_interface()
+        path = abspath(__file__)
+        dir_path = dirname(path)
+        exec_ = join(dir_path, "touchandgo.py")
+        command = '%s %s \"%s\" ' % (py_exec, exec_, name)
+        if season is not None:
+            command += "%s %s " % (season, episode)
+        command += "--daemon"
+        print "running", command
+        process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
+        sleep(1)
+        while get_lock_diff() < 10:
+            sleep(1)
+        port = 8888
+        return redirect("http://%s:%s" % (interface, port))
+
+    app.debug = True
+    app.run(host="0.0.0.0")
+
+if __name__ == '__main__':
+    serve()
