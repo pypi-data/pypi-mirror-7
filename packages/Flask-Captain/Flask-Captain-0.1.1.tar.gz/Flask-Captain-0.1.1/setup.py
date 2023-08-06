12c72f8988005e0a949fd8184edf91ca58dcda91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:14 +0000

    Add package Flask-Captain/0.1.1/Flask-Captain-0.1.1.tar.gz

---
+++ packages/Flask-Captain/Flask-Captain-0.1.1.tar.gz/Flask-Captain-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+  name='Flask-Captain',
+  description="Handle webhooks with Flask",
+  version='0.1.1',
+  author='Mark Steve Samson',
+  author_email='hello@marksteve.com',
+  url='https://github.com/marksteve/flask-captain',
+  license='MIT',
+  py_modules=['flask_captain'],
+  install_requires=[
+    'Flask',
+  ],
+)
