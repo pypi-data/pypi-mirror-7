9ddbc813c0b04bea6968c803dfe1490783ade811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package pydle/0.8.0/pydle-0.8.0.tar.gz

---
+++ packages/pydle/pydle-0.8.0.tar.gz/pydle-0.8.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import pydle
+
+setup(
+    name=pydle.__name__,
+    version=pydle.__version__,
+    packages=[
+        'pydle',
+        'pydle.features',
+        'pydle.features.rfc1459',
+        'pydle.features.ircv3_1',
+        'pydle.features.ircv3_2',
+        'pydle.utils'
+    ],
+    requires=['tornado'],
+    extras_require={
+        'sasl': 'pure-sasl >=0.1.6',   # for pydle.features.sasl
+        'docs': 'sphinx_rtd_theme',    # the Sphinx theme we use
+        'tests': 'pytest',             # collect and run tests
+        'coverage': 'pytest-cov'       # get test case coverage
+    },
+    entry_points={
+        'console_scripts': [
+            'pydle = pydle.utils.run:main',
+            'ipydle = pydle.utils.console:main',
+            'pydle-irccat = pydle.utils.irccat:main'
+        ]
+    },
+
+    author='Shiz',
+    author_email='hi@shiz.me',
+    url='https://github.com/Shizmob/pydle',
+    keywords='irc library python3 compact flexible',
+    description='A compact, flexible and standards-abiding IRC library for Python 3.',
+    license=pydle.__license__,
+
+    zip_safe=True,
+    test_suite='tests'
+)
