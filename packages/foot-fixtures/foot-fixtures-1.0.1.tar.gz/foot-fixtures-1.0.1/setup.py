b7d77170ee7ba58d3123e3aca73b96d6f5cd8834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:11 +0000

    Add package foot-fixtures/1.0.1/foot-fixtures-1.0.1.tar.gz

---
+++ packages/foot-fixtures/foot-fixtures-1.0.1.tar.gz/foot-fixtures-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='foot-fixtures',
+    version='1.0.1',
+    author_email='hsharma1205@gmail.com',
+	py_modules=['fixtures','teams_prac','codes'],
+    entry_points={
+        'console_scripts': [
+            'foot-fixtures = fixtures:main',
+        ],
+    },
+    url = 'https://github.com/supercr7/foot-fixtures',
+	download_url = 'https://github.com/supercr7/foot-fixtures/tarball/1.5',
+    install_requires=[
+        'colorama==0.3.1'
+    ],
+    classifiers=[
+      'Environment :: Console'
+    ],
+)
+
+
