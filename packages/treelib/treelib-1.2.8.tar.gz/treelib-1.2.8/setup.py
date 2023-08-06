bd34f12d95f64c87ff95eda15ff72939c9c55cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:04 +0000

    Add package treelib/1.2.8/treelib-1.2.8.tar.gz

---
+++ packages/treelib/treelib-1.2.8.tar.gz/treelib-1.2.8/setup.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from setuptools import setup
+from treelib import __version__
+
+setup(
+    name = "treelib",
+    version = __version__,
+    url = 'https://github.com/caesar0301/pyTree',
+    author = 'caesar0301',
+    author_email = 'chenxm35@gmail.com',
+    description = 'A Python 2/3 implementation of tree structure.',
+    long_description='''This is a simple tree data structure implementation in python.''',
+    license = "LICENSE",
+    packages = ['treelib'],
+    keywords = ['data structure', 'tree', 'tools'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: Freely Distributable',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.2',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
