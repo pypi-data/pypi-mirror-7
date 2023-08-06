0254cca994b1841d22170a4890a9dc15f510f6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:18 +0000

    Add package python-tuskarclient/0.1.9/python-tuskarclient-0.1.9.tar.gz

---
+++ packages/python-tuskarclient/python-tuskarclient-0.1.9.tar.gz/python-tuskarclient-0.1.9/tuskarclient/tests/v2/test_plans.py
@@ -0,0 +1,106 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+
+import tuskarclient.tests.utils as tutils
+from tuskarclient.v2 import plans
+
+
+class PlanManagerTest(tutils.TestCase):
+
+    def setUp(self):
+        """Create a mock API object and bind to the PlanManager manager.
+        """
+        super(PlanManagerTest, self).setUp()
+        self.api = mock.Mock()
+        self.pm = plans.PlanManager(self.api)
+
+    def test_get(self):
+        """Test a standard GET operation to read/retrieve the plan."""
+        self.pm._get = mock.Mock(return_value='fake_plan')
+
+        self.assertEqual(self.pm.get('fake_plan'), 'fake_plan')
+        self.pm._get.assert_called_with('/v2/plans/fake_plan')
+
+    def test_get_404(self):
+        """Test a 404 response to a standard GET."""
+        self.pm._get = mock.Mock(return_value=None)
+
+        self.assertEqual(self.pm.get('fake_plan'), None)
+        self.pm._get.assert_called_with('/v2/plans/fake_plan')
+
+    def test_list(self):
+        """Test retrieving a list of plans via GET."""
+        self.pm._list = mock.Mock(return_value=['fake_plan'])
+
+        self.assertEqual(self.pm.list(), ['fake_plan'])
+        self.pm._list.assert_called_with('/v2/plans')
+
+    def test_create(self):
+        """Test creating a new plan via POST."""
+        self.pm._create = mock.Mock(return_value=['fake_plan'])
+
+        self.assertEqual(
+            self.pm.create(dummy='dummy plan data'),
+            ['fake_plan'])
+
+        self.pm._create.assert_called_with(
+            '/v2/plans',
+            {'dummy': 'dummy plan data'})
+
+    def test_patch(self):
+        """Test patching a plan."""
+        self.pm._patch = mock.Mock(return_value=['fake_plan'])
+
+        self.assertEqual(
+            self.pm.patch('42', dummy='dummy plan data'),
+            ['fake_plan'])
+
+        self.pm._patch.assert_called_with(
+            '/v2/plans/42',
+            {'dummy': 'dummy plan data'})
+
+    def test_delete(self):
+        """Test deleting/removing an plan via DELETE."""
+        self.pm._delete = mock.Mock(return_value=None)
+
+        self.assertEqual(self.pm.delete(42), None)
+        self.pm._delete.assert_called_with('/v2/plans/42')
+
+    def test_roles_path_with_role_id(self):
+        """Test for building path for Role using UUID."""
+        self.assertEqual(self.pm._roles_path('plan_42', 'role_abc'),
+                         '/v2/plans/plan_42/roles/role_abc')
+
+    def test_roles_path_without_role_id(self):
+        """Test for building path for Role for POST requests."""
+        self.assertEqual(self.pm._roles_path('plan_42'),
+                         '/v2/plans/plan_42/roles')
+
+    def test_add_role(self):
+        """Test assigning Role to a Plan."""
+        self.pm._create = mock.Mock(return_value='dummy plan')
+
+        self.assertEqual(self.pm.add_role('42', role_uuid='qwert12345'),
+                         'dummy plan')
+        self.pm._create.assert_called_with(
+            '/v2/plans/42/roles',
+            {'uuid': 'qwert12345'})
+
+    def test_remove_role(self):
+        """Test assigning Role to a Plan."""
+        self.pm._delete = mock.Mock(return_value=None)
+
+        self.assertEqual(self.pm.remove_role('42', role_uuid='qwert12345'),
+                         None)
+        self.pm._delete.assert_called_with('/v2/plans/42/roles/qwert12345')
