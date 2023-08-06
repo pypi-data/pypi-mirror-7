b6b5c97b0bafa5307786f1924b1853302b4da048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:49 +0000

    Add package ajenti/1.2.21.14/ajenti-1.2.21.14.tar.gz

---
+++ packages/ajenti/ajenti-1.2.21.14.tar.gz/ajenti-1.2.21.14/ajenti/plugins/services/__init__.py
@@ -0,0 +1,30 @@
+from ajenti.api import *
+from ajenti.plugins import *
+
+
+info = PluginInfo(
+    title='Services',
+    icon='play',
+    dependencies=[
+        PluginDependency('main'),
+        PluginDependency('dashboard'),
+    ],
+)
+
+
+def init():
+    import api
+
+    try:
+        import dbus
+        import sm_systemd
+    except ImportError:
+        pass
+        
+    import sm_sysvinit
+    import sm_centos
+    import sm_freebsd
+    import sm_osx
+    import main
+    import widget
+    import sensor
