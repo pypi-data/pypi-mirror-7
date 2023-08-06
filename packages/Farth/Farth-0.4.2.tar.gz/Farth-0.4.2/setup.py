e35fd28175a2068d062710113d8d7adeb849a3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:55 +0000

    Add package Farth/0.4.2/Farth-0.4.2.tar.gz

---
+++ packages/Farth/Farth-0.4.2.tar.gz/Farth-0.4.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+v = "0.4.2"
+
+setup(name="Farth",
+	version=v,
+	author="Ivan Konovalov",
+	author_email="rvan.mega@gmail.com",
+	description="Farth - is an attempt to implement Forth",
+	packages=["Farth"],
+	url="https://github.com/SPython/farth",
+	download_url="https://github.com/SPython/farth/tarball/"+v,
+	keywords=["forth", "implementation", "language"])
