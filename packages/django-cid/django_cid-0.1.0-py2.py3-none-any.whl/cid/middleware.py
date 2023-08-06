025e6e26a9fabc56180a8589f261458239cb5254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:57 +0000

    Add package django-cid/0.1.0/django_cid-0.1.0-py2.py3-none-any.whl

---
+++ packages/django-cid/django_cid-0.1.0-py2.py3-none-any.whl/cid/middleware.py
@@ -0,0 +1,27 @@
+from uuid import uuid4
+from django.conf import settings
+
+from .locals import set_cid, get_cid
+
+
+class CidMiddleware(object):
+    """
+    Middleware class to extract the correlation id from incoming headers
+    and add them to out going headers
+    """
+
+    def __init__(self):
+        self.cid_header = getattr(settings, 'CID_HEADER', 'X_CORRELATION_ID')
+        self.generate_cid = getattr(settings, 'CID_GENERATE', False)
+
+    def process_request(self, request):
+        cid = request.META.get(self.cid_header, None)
+        if cid is None and self.generate_cid:
+            cid = str(uuid4())
+        request.correlation_id = cid
+        set_cid(request.correlation_id)
+
+    def process_response(self, request, response):
+        cid = get_cid()
+        if cid:
+            response[self.cid_header] = cid
