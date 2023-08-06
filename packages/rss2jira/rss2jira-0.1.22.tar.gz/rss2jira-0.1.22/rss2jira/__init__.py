d1cf036ab3ff80e372643c5d27eea97beabf2b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:48 +0000

    Add package rss2jira/0.1.22/rss2jira-0.1.22.tar.gz

---
+++ packages/rss2jira/rss2jira-0.1.22.tar.gz/rss2jira-0.1.22/rss2jira/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.1.22'
+
+from trackedentries import Sqlite3TrackedEntries
+from binding import BindingFactory
+from rssreader import RssReader
+from app import MainLoop
+from issueFactory import JiraWrapper
+
+import logging
+
+try:
+    logging.getLogger('rss2jira').addHandler(logging.NullHandler())
+except AttributeError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+    logging.getLogger('rss2jira').addHandler(NullHandler())
