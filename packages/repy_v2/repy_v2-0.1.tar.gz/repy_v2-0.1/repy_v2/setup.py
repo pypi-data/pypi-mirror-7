279af14d8179b148673165ecd168202075c0e07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:41 +0000

    Add package repy_v2/0.1/repy_v2-0.1.tar.gz

---
+++ packages/repy_v2/repy_v2-0.1.tar.gz/repy_v2-0.1/repy_v2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'repy_v2',
+  packages = ['repy_v2'], 
+  version = '0.1',
+  description = "Seattle's repy_v2 package",
+  author = 'Seattle',
+  url = 'https://github.com/choksi81/repy_v2', # use the URL to the github repo
+  classifiers = [],
+)
---
+++ packages/repy_v2/repy_v2-0.1.tar.gz/repy_v2-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'repy_v2',
+  packages = ['repy_v2'], # this must be the same as the name above
+  version = '0.1',
+  description = "Seattle's seash package",
+  author = 'Seattle',
+  author_email = 'cc4256@nyu.edu',
+  url = 'https://github.com/choksi81/repy_v2', # use the URL to the github repo
+  download_url = 'https://github.com/choksi81/repy_v2/tarball/0.1', # I'll explain this in a second
+  classifiers = [],
+)
