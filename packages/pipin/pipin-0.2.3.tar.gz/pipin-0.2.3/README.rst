15e721293d7f7b103972718ce44f75bacb6ac4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:17 +0000

    Add package pipin/0.2.3/pipin-0.2.3.tar.gz

---
+++ packages/pipin/pipin-0.2.3.tar.gz/pipin-0.2.3/README.rst
@@ -0,0 +1,64 @@
+pipin
+=====
+
+Let's *pipin*!
+
+pipin is a little Python script to search for a given app(s) within your project's dependencies.
+
+You could probably handle same needs with `ack` or `grep`, but `pipin` is less characters to write, colorizes the output nicely etc.
+
+.. image:: https://travis-ci.org/mattack108/pipin.png?branch=master
+   :target: https://travis-ci.org/mattack108/pipin
+
+.. image:: https://pypip.in/v/pipin/badge.png
+   :target: https://pypi.python.org/pypi/pipin
+
+.. image:: https://pypip.in/d/pipin/badge.png
+   :target: https://pypi.python.org/pypi/pipin
+
+Requirements
+------------
+
+- Python 2.6+ or 3.3+
+- `argparse <https://pypi.python.org/pypi/argparse>`_ if py2.6
+
+Installation
+------------
+
+To install ``pipin``, simply run: ::
+
+    sudo pip install pipin
+
+Note: you probably want to install it as sudo in order to *pipin everywhere*.
+
+Usage
+-----
+
+Go to folder where all your projects are stored: ::
+
+    cd /home/projects/
+
+Just tell *what* and *where* you want to pipin: ::
+
+    pipin Django==1.4.2 .
+
+This will search for "Django==1.4.2" in requirements.txt file in current directory (because ".").
+
+Search within specific project only? Not a problem! ::
+
+    pipin Django==1.4.2 /path/to/project/
+
+Different requirements file? Not a problem too! ::
+
+    pipin Django==1.4.2 . -f dev_requirements.txt
+
+You might also need to search for multiple apps at once. Then do: ::
+
+    pipin Django==1.4.2 South .
+
+Also, literal asterisk (*) is supported: ::
+
+    pipin Django*1.4 .
+
+This will find occurences of ``Django==1.4`` and ``Django>=1.4`` as they are
+both valid.
---
+++ packages/pipin/pipin-0.2.3.tar.gz/pipin-0.2.3/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+install_requires = []
+if sys.version_info[:2] < (2, 7):
+    install_requires.append('argparse')
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='pipin',
+    version='0.2.3',
+    author='Maciek Lenc',
+    author_email='matt.lenc@gmail.com',
+    url='http://github.com/mattack108/pipin',
+    license='MIT',
+    description='pipin is a little script to search for dependencies within'
+                'your project(s).',
+    install_requires=install_requires,
+    cmdclass={'test': PyTest},
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pipin = pipin:run',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='pipin pip requirements dependencies',
+)
