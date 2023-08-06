6ca1a1f0be641942f4d38ae1515b08a6a589cb9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:35 +0000

    Add package calc/1.0/calc-1.0.zip

---
+++ packages/calc/calc-1.0.zip/calc-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+         name = 'calc',
+         version = '1.0',
+         py_modules = ['calc'],
+         author = 'g_k_ragav',
+         author_email = 'kishoreragav@outlook.com ',
+         url = 'NONE',
+         description = 'A simple module with the basic functions of a calculator and the calculator itself.'
+         
+         )
