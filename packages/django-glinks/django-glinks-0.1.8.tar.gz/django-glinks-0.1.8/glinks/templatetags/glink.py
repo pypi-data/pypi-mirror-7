b571496d4526c89c611f569b1214bb039835f091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package django-glinks/0.1.8/django-glinks-0.1.8.tar.gz

---
+++ packages/django-glinks/django-glinks-0.1.8.tar.gz/django-glinks-0.1.8/glinks/templatetags/glink.py
@@ -0,0 +1,153 @@
+from django import template
+from django.conf import settings
+from django.utils.encoding import smart_str
+from django.template import TextNode, Node
+
+from utilities import *
+
+
+register = template.Library()
+
+from random import randint
+
+def parseGlinkNode(command):
+	options = {}
+
+	for op in command:
+		if "=" in op:
+			tmp_list = op.split("=")
+			if tmp_list[0] in possibleGlinkOptions():
+				options[tmp_list[0]] = tmp_list[1].replace('"', '').replace("'", '').strip()
+			else:
+				print glinkErrorLead() + "Incorrect command given in template.\n"
+		if "as" in op:
+			options["as"] = command[command.index("as")+1]
+	return options
+
+class GlinkNodeCustom(Node):
+	child_nodelists = ('nodelist_file', 'nodelist_empty')
+
+	def __init__(self, parser, token):
+		command = token.split_contents()
+		self.must_be_first = None
+		self.as_var = None
+		self.blueprint = None
+		self.glink = None
+		self.options = {}
+		self.nodelist_file = None
+
+		self.options = parseGlinkNode(command)
+
+		for key, value in self.options.items():
+			if key == "blueprint":
+				self.blueprint = value
+
+		# Close block.
+		if command[-2] == 'as':
+			self.as_var = command[-1]
+			self.nodelist_file = parser.parse(('empty', 'endcustom_glink',))
+			if parser.next_token().contents == 'empty':
+				self.nodelist_empty = parser.parse(('endcustom_glink',))
+				parser.delete_first_token()
+		
+		# Nifty little switcharoo from string to object.
+		self.blueprint = Blueprint.objects.get(name=self.blueprint)
+
+		try:
+			glinks = Glink.objects.filter(blueprint=self.blueprint)
+			self.glink = glinks.order_by('?')[0]
+
+			# Increment impression count.
+			self.glink.impressions = self.glink.impressions + 1
+			self.glink.save()
+		except Exception as e:
+			print str(e)
+
+	def render(self, context):
+		output = None
+
+		if self.as_var:
+			context.push()
+			context[self.as_var] = self.glink
+			output = self.nodelist_file.render(context)
+			context.pop()
+		else:
+			output = self.glink.image.url
+
+		return output
+
+class GlinkNode(Node):
+	child_nodelists = ('nodelist_file', 'nodelist_empty')
+
+	def __init__(self, parser, token):
+		command = token.split_contents()
+		self.must_be_first = None
+		self.as_var = None
+		self.blueprint = None
+		self.glink = None
+		self.options = {}
+		self.nodelist_file = None
+
+		self.options = parseGlinkNode(command)
+
+		for key, value in self.options.items():
+			if key == "blueprint":
+				self.blueprint = value
+		# Nifty little switcharoo from string to object.
+		self.blueprint = Blueprint.objects.get(name=self.blueprint)
+
+		try:
+			glinks = Glink.objects.filter(blueprint=self.blueprint)
+			weight_list = generateWeightedListFromGlinks(glinks)
+
+			weight = weight_list[randint(0, len(weight_list)-1)]
+			glinks = glinks.filter(weight=weight)
+			self.glink = glinks.order_by('?')[0]
+
+			# Increment impression count.
+			self.glink.impressions = self.glink.impressions + 1
+			self.glink.save()
+		except Exception as e:
+			print str(e)
+
+	def render(self, context):
+		#height="42" width="42"
+		'''
+		glink_page = "'glink/" + str(self.glink.id) + "'"
+		img_lead = "<img src="
+		img_url = "'" + str(self.glink.image.url) + "'"
+		on_click = " onclick=location.href="
+		glink_page = "'/glink/" + str(self.glink.id) + "'"
+		height = " height='" + str(self.blueprint.height) + "'"
+		width = " width='" + str(self.blueprint.width) + "'"
+		img_close = ">"
+		return img_lead + img_url + on_click + glink_page + height + width + img_close
+		'''
+		a_lead = "<a href="
+		glink_page = "'/glink/" + str(self.glink.id) + "'>"
+		img_lead = "<img src="
+		img_url = "'" + str(self.glink.image.url) + "'"
+		height = " height='" + str(self.blueprint.height) + "'"
+		width = " width='" + str(self.blueprint.width) + "'"
+		img_close = "> "
+		a_close = "</a>"
+		print a_lead + glink_page + img_lead + img_url + height + width + img_close + a_close
+		return a_lead + glink_page + img_lead + img_url + height + width + img_close + a_close
+
+
+
+@register.assignment_tag
+def getRandomGlink():
+	return Glink.objects.all().order_by('?')[0]
+		
+@register.tag
+def custom_glink(parser, token):
+	node = GlinkNodeCustom(parser, token)
+	return node
+
+@register.tag
+def glink(parser, token):
+	node = GlinkNode(parser, token)
+	return node
+
+
---
+++ packages/django-glinks/django-glinks-0.1.8.tar.gz/django-glinks-0.1.8/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+import os
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-glinks',
+    version='0.1.8',
+    author='Shawn Simon',
+    author_email='shawn.simon.developer@gmail.com',
+    packages=['glinks', "glinks.templatetags"],
+    url='http://pypi.python.org/pypi/django-glinks/',
+    license='LICENSE.txt (CC-BY-NC)',
+    description='Interal add manager for django sites with ads not blocked by ad blocker.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.6.0",
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
