c8cebdee1326d87b542e7af76dce635e8515195f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:51 +0000

    Add package pyrax/1.9.1/pyrax-1.9.1.tar.gz

---
+++ packages/pyrax/pyrax-1.9.1.tar.gz/pyrax-1.9.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import re
+import sys
+if sys.version < "2.2.3":
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+# Workaround for problems caused by this import
+# It's either this or hardcoding the version.
+#from pyrax.version import version
+with open("pyrax/version.py", "rt") as vfile:
+    version_text = vfile.read()
+vmatch = re.search(r'version ?= ?"(.+)"$', version_text)
+version = vmatch.groups()[0]
+
+testing_requires = ["mock"]
+
+setup(
+    name="pyrax",
+    version=version,
+    description="Python language bindings for OpenStack Clouds.",
+    author="Rackspace",
+    author_email="ed.leafe@rackspace.com",
+    url="https://github.com/rackspace/pyrax",
+    keywords="pyrax rackspace cloud openstack",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+    ],
+    install_requires=[
+        "python-novaclient>=2.13.0",
+        "rackspace-novaclient",
+        "keyring",
+        "requests>=2.2.1",
+        "six>=1.5.2",
+    ] + testing_requires,
+    packages=[
+        "pyrax",
+        "pyrax/identity",
+    ],
+)
