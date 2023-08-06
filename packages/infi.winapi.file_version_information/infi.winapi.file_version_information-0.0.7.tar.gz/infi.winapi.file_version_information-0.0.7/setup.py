db092cd953e0ec84e1fa040d029eacc24885dc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package infi.winapi.file_version_information/0.0.7/infi.winapi.file_version_information-0.0.7.tar.gz

---
+++ packages/infi.winapi.file_version_information/infi.winapi.file_version_information-0.0.7.tar.gz/infi.winapi.file_version_information-0.0.7/setup.py
@@ -0,0 +1,46 @@
+
+SETUP_INFO = dict(
+    name = 'infi.winapi.file_version_information',
+    version = '0.0.7',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'http://www.infinidat.com',
+    license = 'PSF',
+    description = """Simple API to retreive a Windows file version""",
+    long_description = """Simple API to retreive a Windows file version""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.cwrap',
+'infi.instruct',
+'setuptools'],
+    namespace_packages = ['infi', 'infi.winapi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = []),
+    )
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
