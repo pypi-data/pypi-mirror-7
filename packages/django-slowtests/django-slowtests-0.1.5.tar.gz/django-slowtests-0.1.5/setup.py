a20d47ff67d0549a194e50e853d4e477c25d4281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:54 +0000

    Add package django-slowtests/0.1.5/django-slowtests-0.1.5.tar.gz

---
+++ packages/django-slowtests/django-slowtests-0.1.5.tar.gz/django-slowtests-0.1.5/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+from django_slowtests import __version__
+
+README = open(os.path.join(os.path.dirname(__file__), 'readme.rst')).read()
+
+setup(
+    name='django-slowtests',
+    version=__version__,
+    description="Locate your slowest tests.",
+    long_description=README,
+    author='Michael Herman',
+    author_email='michael@realpython.com',
+    url='https://github.com/realpython/django-discover-slowest-tests-runner',
+    license='MIT',
+    packages=['django_slowtests'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
