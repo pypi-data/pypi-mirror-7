8bb374a218ac84da85fc5fb9f78ba3e533477722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package tomdb/0.4.2/tomdb-0.4.2.tar.gz

---
+++ packages/tomdb/tomdb-0.4.2.tar.gz/tomdb-0.4.2/setup.py
@@ -0,0 +1,14 @@
+import distutils.core
+
+version = "0.4.2"
+
+distutils.core.setup(
+    name="tomdb",
+    version=version,
+    py_modules=["tomdb", "atomdb"],
+    author="Tom",
+    author_email="wmttom@gmail.com",
+    url="https://github.com/wmttom/tomdb",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    description="A simple wrapper around MySQLdb and pymysql.",
+    )
