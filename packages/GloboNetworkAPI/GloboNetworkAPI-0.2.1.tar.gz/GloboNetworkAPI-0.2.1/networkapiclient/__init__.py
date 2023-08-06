780000d4f231eadb5f1eb365d2b6398a82a8109c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:36 +0000

    Add package GloboNetworkAPI/0.2.1/GloboNetworkAPI-0.2.1.tar.gz

---
+++ packages/GloboNetworkAPI/GloboNetworkAPI-0.2.1.tar.gz/GloboNetworkAPI-0.2.1/networkapiclient/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+'''
+Title: Infrastructure NetworkAPI
+Author: globo.com / TQI
+Copyright: ( c )  2009 globo.com todos os direitos reservados.
+'''
+
+MAJOR_VERSION = '0'
+MINOR_VERSION = '2'
+PATCH_VERSION = '1'
+VERSION = '.'.join((MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION,))
+
