a87bcff5f093383b1d40d9d0391e35db8b476b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:36 +0000

    Add package uwsgiFouine/1.03/uwsgiFouine-1.03.tar.gz

---
+++ packages/uwsgiFouine/uwsgiFouine-1.03.tar.gz/uwsgiFouine-1.03/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import os
+readme = open('README.rst').read()
+
+setup(
+    name='uwsgiFouine',
+    version='1.03',
+    author_email='gattster@gmail.com',
+    author='Philip Gatt',
+    py_modules=['uwsgifouinelib'],
+    scripts=['uwsgiFouine'],
+    description="A uwsgi log parser.",
+    long_description=readme,
+    url='http://github.com/defcube/uwsgiFouine',
+    )
