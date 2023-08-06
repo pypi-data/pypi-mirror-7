0994e734a587c51b0a928b19fad1e1df6fba5a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:22 +0000

    Add package pomade/0.0.3/pomade-0.0.3.tar.gz

---
+++ packages/pomade/pomade-0.0.3.tar.gz/pomade-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name = "pomade",
+    packages = ["pomade"],
+    version = "0.0.3",
+    description = "Selenium PageObjects implementation with easy Sauce Labs integration",
+    author = "Sauce Labs",
+    author_email = "help@saucelabs.com",
+    url = "https://github.com/saucelabs/pomade",
+    install_requires=['decorator>=3.4.0', 'selenium>=2.38.3', 'nose>=1.3.0'],
+)
