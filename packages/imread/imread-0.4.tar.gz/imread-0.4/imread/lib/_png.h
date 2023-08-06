a6508cde78fa92fb25f168db0fbcbf896bf01c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:01 +0000

    Add package imread/0.4/imread-0.4.tar.gz

---
+++ packages/imread/imread-0.4.tar.gz/imread-0.4/imread/lib/_png.h
@@ -0,0 +1,16 @@
+// Copyright 2012 Luis Pedro Coelho <luis@luispedro.org>
+// License: MIT (see COPYING.MIT file)
+
+#ifndef LPC_PNG_H_INCLUDE_GUARD_WED_FEB__1_16_34_50_WET_2012
+#define LPC_PNG_H_INCLUDE_GUARD_WED_FEB__1_16_34_50_WET_2012
+class PNGFormat : public ImageFormat {
+    public:
+        bool can_read() const { return true; }
+        bool can_write() const { return true; }
+
+        std::auto_ptr<Image> read(byte_source* src, ImageFactory* factory, const options_map& opts);
+        void write(Image* input, byte_sink* output, const options_map& opts);
+};
+
+
+#endif // LPC_PNG_H_INCLUDE_GUARD_WED_FEB__1_16_34_50_WET_2012
