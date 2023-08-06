2ce8fae9e95bf2c5e64c812f8c6e28907beed66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:12 +0000

    Add package pycollada/0.4.1/pycollada-0.4.1.tar.gz

---
+++ packages/pycollada/pycollada-0.4.1.tar.gz/pycollada-0.4.1/setup.py
@@ -0,0 +1,33 @@
+import sys
+from setuptools import find_packages, setup
+
+install_requires = []
+
+try: import numpy
+except ImportError: install_requires.append('numpy')
+
+if sys.version_info[0] > 2:
+    install_requires.append('python-dateutil>=2.0')
+else:
+    import unittest
+    if not hasattr(unittest.TestCase, "assertIsNone"):
+        install_requires.append('unittest2')
+    install_requires.append('python-dateutil==1.5')
+
+setup(
+    name = "pycollada",
+    version = "0.4.1",
+    description = "python library for reading and writing collada documents",
+    author = "Jeff Terrace and contributors",
+    author_email = 'jterrace@gmail.com',
+    platforms=["any"],
+    license="BSD",
+    install_requires=install_requires,
+    extras_require = {
+        'prettyprint': ["lxml"],
+        'validation': ["lxml"]
+    },
+    url = "http://pycollada.readthedocs.org/",
+    test_suite = "collada.tests",
+    packages = find_packages()
+)
