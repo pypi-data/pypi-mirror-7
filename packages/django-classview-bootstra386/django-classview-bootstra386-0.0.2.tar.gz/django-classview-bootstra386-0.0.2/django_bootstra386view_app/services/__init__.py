5953b85c8a0d41e298e2c3a9b1bb63409dd983d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:35 +0000

    Add package django-classview-bootstra386/0.0.2/django-classview-bootstra386-0.0.2.tar.gz

---
+++ packages/django-classview-bootstra386/django-classview-bootstra386-0.0.2.tar.gz/django-classview-bootstra386-0.0.2/django_bootstra386view_app/services/__init__.py
@@ -0,0 +1,5 @@
+"""Base service class for services for Django Bootstra.386 view project.
+
+.. moduleauthor:: Ernesto Bossi <bossi.ernestog@gmail.com>
+
+"""
---
+++ packages/django-classview-bootstra386/django-classview-bootstra386-0.0.2.tar.gz/django-classview-bootstra386-0.0.2/django_bootstra386view_app/views/__init__.py
@@ -0,0 +1,5 @@
+"""Base class fot view for the Django Bootstra.386 view project.
+
+.. moduleauthor:: Ernesto Bossi <bossi.ernestog@gmail.com>
+
+"""
