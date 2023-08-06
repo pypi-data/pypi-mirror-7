e22872ed932e2b9043cd260c59dc12b747935512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:31 +0000

    Add package EVE-SRP/0.9.7/EVE-SRP-0.9.7.tar.gz

---
+++ packages/EVE-SRP/EVE-SRP-0.9.7.tar.gz/EVE-SRP-0.9.7/evesrp/default_config.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from .killmail import CRESTMail, ZKillmail
+
+CSRF_ENABLE = True
+
+# This really needs to be set to something
+SRP_AUTH_METHODS = []
+
+# Default database is an in-memory SQLite DB.
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+# CREST killmails don't have any external dependencies
+SRP_KILLMAIL_SOURCES = [ZKillmail, CRESTMail]
+
+SRP_SITE_NAME = 'EVE-SRP'
+
+SRP_DETAILS_DESCRIPTION = 'Supporting details about your loss.'
