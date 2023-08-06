c28b0d6a1a4a2ebce89e1625fba6e235e8082f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:56 +0000

    Add package publishing_pip_packages/0.1.0/publishing_pip_packages-0.1.0.tar.gz

---
+++ packages/publishing_pip_packages/publishing_pip_packages-0.1.0.tar.gz/publishing_pip_packages-0.1.0/publishing_pip_packages/cli.py
@@ -0,0 +1,39 @@
+from subprocess import call
+from landslide.generator import Generator
+import os
+import time
+
+description = """
+%prog [slides - print the slides | links - print the links]
+"""
+
+
+def dirname():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def file_in_dir(filename):
+    return os.path.join(dirname(), 'data', filename)
+
+
+def open_slides():
+    slides_path = file_in_dir('slides.md')
+    output_path = 'pipped-slides.html'
+    Generator(slides_path, **{
+        'embed': True,
+        'relative': True,
+        'destination_file': output_path,
+        'theme': file_in_dir('avalanche'),
+    }).execute()
+    call(['open', output_path])
+
+    # sleep to let the browser open
+    time.sleep(1)
+
+    call(['rm', output_path])
+
+
+def run():
+    print("publising pip packages - nyc python talk on 2014-06-12")
+    print("code available at https://github.com/brianc/publishing-pip-packages")
+    open_slides()
