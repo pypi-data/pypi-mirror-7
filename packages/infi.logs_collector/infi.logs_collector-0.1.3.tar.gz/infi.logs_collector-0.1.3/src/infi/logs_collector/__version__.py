9e5e6873525d99a2acca59b2de9a41138d79febb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:14 +0000

    Add package infi.logs_collector/0.1.3/infi.logs_collector-0.1.3.tar.gz

---
+++ packages/infi.logs_collector/infi.logs_collector-0.1.3.tar.gz/infi.logs_collector-0.1.3/src/infi/logs_collector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.3"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a7e970641dd5161d62c8754b68eb4cddf0d4eb87'
+__git_head_subject__ = 'HOSTDEV-1703 inner directory now based on the collected timestamp instead of now()'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
