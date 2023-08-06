0d83ce5bb1f8e344ddee421395d549065937ef5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package cielo24cli/1.0.1/cielo24cli-1.0.1.zip

---
+++ packages/cielo24cli/cielo24cli-1.0.1.zip/cielo24cli-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+# Script for PyPI
+
+setup(name='cielo24cli',
+      version='1.0.1',
+      description='Commmand line for cielo24 package.',
+      author='cielo24',
+      author_email='support@cielo24.com',
+      url='http://www.cielo24.com',
+      install_requires = ['compago','cielo24'],
+      py_modules=['cielo24cli']
+     )
