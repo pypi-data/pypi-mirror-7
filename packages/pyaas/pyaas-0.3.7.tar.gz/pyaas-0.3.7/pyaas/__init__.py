82562d5c0a005d90ada88dd7d8f1ae6c38cecffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:29 +0000

    Add package pyaas/0.3.7/pyaas-0.3.7.tar.gz

---
+++ packages/pyaas/pyaas-0.3.7.tar.gz/pyaas-0.3.7/pyaas/__init__.py
@@ -0,0 +1,18 @@
+
+VERSION = '0.3.7'
+
+# a generic error class for throwing exceptions
+class error(Exception):
+    def __init__(self, fmt, *args):
+        self.str = fmt % args
+
+    def __str__(self):
+        return self.str
+
+
+from . import settings
+from . import util
+from . import server
+
+util.setPrefix()
+util.setNameSpace('')
