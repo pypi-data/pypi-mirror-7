1e7d7c011c42ebb10b6dfa6015f8545d25577032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:29 +0000

    Add package PGeo/0.1.3/PGeo-0.1.3.tar.gz

---
+++ packages/PGeo/PGeo-0.1.3.tar.gz/PGeo-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PGeo',
+    version='0.1.3',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=['pgeo'],
+    url='http://pypi.python.org/pypi/PGeo/',
+    license='LICENSE.txt',
+    description='PGeo module.',
+    long_description=open('README.md').read()
+)
