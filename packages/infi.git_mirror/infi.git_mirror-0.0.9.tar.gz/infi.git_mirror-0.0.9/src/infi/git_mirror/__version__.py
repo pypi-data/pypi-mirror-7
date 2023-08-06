cdc747cc9e6b87b91ce1721d2abe7302deaf9a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.git_mirror/0.0.9/infi.git_mirror-0.0.9.tar.gz

---
+++ packages/infi.git_mirror/infi.git_mirror-0.0.9.tar.gz/infi.git_mirror-0.0.9/src/infi/git_mirror/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '70252e2ad7e98299a47d923a220fe4651c5cba4c'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
