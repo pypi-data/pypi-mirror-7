736aa5a39a74bc1e9b54004dde3986a7f054c89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:34 +0000

    Add package html5validator/0.1.5/html5validator-0.1.5.tar.gz

---
+++ packages/html5validator/html5validator-0.1.5.tar.gz/html5validator-0.1.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+import re
+
+# extract version from __init__.py
+with open('html5validator/__init__.py', 'r') as f:
+    INIT = f.read()
+    VERSION = next(re.finditer('__version__ = \"(.*?)\"', INIT)).group(1)
+
+setup(
+    name='html5validator',
+    version=VERSION,
+    packages=['html5validator'],
+    license='MIT',
+    description='Validate HTML5 files.',
+    long_description=open('README.rst').read(),
+    author='Sven Kreiss',
+    author_email='me@svenkreiss.com',
+    url='https://github.com/svenkreiss/html5validator',
+
+    data_files=[('', ['vnu/vnu.jar'])],
+    include_package_data=True,
+
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'html5validator = html5validator.cli:main',
+        ]
+    },
+
+    tests_require=[
+        'nose',
+    ],
+    test_suite='nose.collector',
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
