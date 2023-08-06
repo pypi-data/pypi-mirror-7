3fcc752aedb06c74f011000c6a04e19ab7f804ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:03 +0000

    Add package Products.AutoRoleFromHostHeader/0.4.1/Products.AutoRoleFromHostHeader-0.4.1.zip

---
+++ packages/Products.AutoRoleFromHostHeader/Products.AutoRoleFromHostHeader-0.4.1.zip/Products.AutoRoleFromHostHeader-0.4.1/README.rst
@@ -0,0 +1,85 @@
+Add **roles** or **groups** to anonymous or logged-in visitors based on **HTTP headers**
+
+.. contents:: **Table of contents**
+
+Introduction
+============
+
+The AutoRoleFromHostHeader PAS plugin allows to assign roles to users looking
+at HTTP headers.
+
+There is an extraction and authentication plugin included, to enable
+additional roles for anonymous users. They are required since PAS does
+not support roles (or properties or groups) for anonymous users.
+You can disable these interfaces if only logged-in users should get
+additional roles.
+
+AutoRoleFromHostHeader furthermore provides a groups plugin interface,
+allowing you to assign groups instead of roles.
+
+Configuration
+=============
+
+The plugin is configured by editing the ``Header name; regexp; roles/groups``
+property on the plugin's properties screen (through ZMI).
+Each line represents a mapping from an header value (using a regexp match) to
+one or more roles. The format is as follows::
+
+    http_header_name; regular expression; role[, role ...]
+
+Assign groups, not roles
+------------------------
+
+This plugin can be used to assign groups instead of roles if used as a
+*group plugin* instead of a role plugin::
+
+    http_header_name; regular expression; group[, group ...]
+
+Groups plugin is not activated by default.
+
+Caveat
+======
+
+If you have AutoRoleFromHostHeader configured for anonymous users and come
+from a network matching one of its rules, you will **not** be able to log in
+with an account from a higher-up user folder. This is because AutoRole
+authenticates the Anonymous User which stops the lookup process.
+
+Dependencies
+============
+
+Tested with all Plone versions from 3.3. to 4.3.
+
+Credits
+=======
+
+Developed with the support of:
+
+* `Azienda USL Ferrara`__
+
+  .. image:: http://www.ausl.fe.it/logo_ausl.gif
+     :alt: Azienda USL's logo
+  
+* `Regione Emilia Romagna`__
+
+All of them supports the `PloneGov initiative`__.
+
+__ http://www.ausl.fe.it/
+__ http://www.regione.emilia-romagna.it/
+__ http://www.plonegov.it/
+
+Authors
+=======
+
+* This product was developed by RedTurtle Technology team.
+  
+  .. image:: http://www.redturtle.it/redturtle_banner.png
+     :alt: RedTurtle Technology Site
+     :target: http://www.redturtle.it/
+  
+* Special thanks to *Mauro Amico* (mamico) for giving us the main direction.
+* AutoRoleFromHostHeader is not an original idea but is taken from the work
+  made by *Jarn company* for the `AutoRole`__ plugin.
+
+__ http://pypi.python.org/pypi/Products.AutoRole
+
---
+++ packages/Products.AutoRoleFromHostHeader/Products.AutoRoleFromHostHeader-0.4.1.zip/Products.AutoRoleFromHostHeader-0.4.1/src/Products/AutoRoleFromHostHeader/plugins/AutoRole.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+from AccessControl.SecurityInfo import ClassSecurityInfo
+from Globals import InitializeClass
+from Products.AutoRoleFromHostHeader.interfaces import ConfigurationChangedEvent
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
+from Products.PluggableAuthService.interfaces.plugins import IExtractionPlugin
+from Products.PluggableAuthService.interfaces.plugins import IGroupsPlugin
+from Products.PluggableAuthService.interfaces.plugins import IRolesPlugin
+from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
+from Products.PluggableAuthService.utils import classImplements
+from zope.event import notify
+import re
+
+manage_addAutoRoleForm = PageTemplateFile(
+    'www/autoRoleAdd', globals(), __name__='manage_addAutoRoleForm')
+
+
+def addAutoRole( dispatcher
+               , id
+               , title=None
+               , match_roles=()
+               , REQUEST=None
+               ):
+    """ Add an AutoRole plugin to a Pluggable Auth Service. """
+    sp = AutoRole(id, title, match_roles)
+    dispatcher._setObject(sp.getId(), sp)
+
+    if REQUEST is not None:
+        REQUEST['RESPONSE'].redirect( '%s/manage_workspace'
+                                      '?manage_tabs_message='
+                                      'AutoRole+added.'
+                                    % dispatcher.absolute_url() )
+
+class AutoRole(BasePlugin):
+    """ Multi-plugin for assigning auto roles from IP. """
+
+    meta_type = 'Auto Role Header Plugin'
+    security = ClassSecurityInfo()
+
+    _properties = (
+        dict(id='title', label='Title', type='string', mode='w'),
+        dict(id='match_roles', label='Header name; regexp; roles/groups', type='lines',
+             mode='w'),
+        dict(id='anon_only', label='Anonymous Only', type='boolean',
+             mode='w'),
+    )
+    
+    anon_only = False
+
+    def __init__(self, id, title=None, match_roles=()):
+        self._setId(id)
+        self.title = title
+        self.match_roles = match_roles
+        self.anon_only = False
+        self._compiled = []
+    
+    def _compile_matchs(self):
+        self._compiled = compiled = []
+        for line in self.match_roles:
+            try:
+                header_name, regexp, roles = line.split(';')
+                roles = [r.strip() for r in roles.split(',')]
+                roles = set(filter(None, roles))
+                if not roles:
+                    continue
+            except (ValueError, AttributeError):
+                continue
+            compiled.append((header_name, regexp, roles))
+            
+    def _setPropValue(self, id, value):
+        BasePlugin._setPropValue(self, id, value)
+        if id == 'match_roles':
+            self._compile_matchs()
+            if value and len(self._compiled) != len(self.match_roles):
+                raise ValueError(
+                    'match_roles contains invalid parameters!')
+            notify(ConfigurationChangedEvent(self))
+
+    #
+    # IRolesPlugin
+    #
+    security.declarePrivate('getRolesForPrincipal')
+    def getRolesForPrincipal(self, principal, request=None):
+        """ Assign roles based on 'request'. """
+        # we need this for uncontexted calls
+        if request == None:
+         return []
+        if (self.anon_only and 
+            principal is not None and 
+            principal.getUserName() != 'Anonymous User'):
+            return []
+        if not self._compiled:
+            return []
+
+        result = set()
+        for header_name, regexp, roles in self._compiled:
+            header = request.get(header_name)
+            if header:
+                check_header = re.compile(regexp)
+                if check_header.match(header):
+                    result.update(roles)
+        return list(result)
+
+    #
+    # IGroupsPlugin
+    #
+    # This method allows the plugin to be used to assign groups instead of
+    # roles if used as a group plugin instead of a role plugin.
+    security.declarePrivate('getGroupsForPrincipal')
+    def getGroupsForPrincipal(self, principal, request=None):
+        """ Assign groups based on 'request'. """
+        return self.getRolesForPrincipal(principal, request)
+
+    #
+    # IExtractionPlugin
+    #
+    security.declarePrivate('extractCredentials')
+    def extractCredentials(self, request):
+        # Avoid creating anon user if this is a regular user
+        # We actually have to poke request ourselves to avoid users from
+        # root becoming anonymous...
+
+        if getattr(request, '_auth', None):
+            return {}
+        
+        if not self._compiled:
+            return {}
+
+        for header_name, regexp, roles in self._compiled:
+            header = request.get(header_name)
+            if header:
+                check_header = re.compile(regexp)
+                if check_header.match(header):
+                    return dict(AutoRole=True)
+
+        return {}
+
+    #
+    # IAuthenticationPlugin
+    #
+    security.declarePrivate('authenticateCredentials')
+    def authenticateCredentials(self, credentials):
+        # Make sure we don't instantiate anonymous if there are other credentials
+        # BBB: this seems only work for basic authentication. Can we do something better?
+        if credentials.get('login'):
+            return None
+        autorole = credentials.get('AutoRole', None)
+        if not autorole:
+            return None
+        return ('Anonymous User', 'Anonymous User')
+
+
+classImplements( AutoRole
+               , IRolesPlugin
+               , IGroupsPlugin
+               , IExtractionPlugin
+               , IAuthenticationPlugin
+               )
+
+InitializeClass(AutoRole)
