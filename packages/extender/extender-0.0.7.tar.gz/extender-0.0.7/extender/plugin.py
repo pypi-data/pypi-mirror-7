d8efcb41e5357a1b71c7c7d2f94d91defee1e372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:47 +0000

    Add package extender/0.0.7/extender-0.0.7.tar.gz

---
+++ packages/extender/extender-0.0.7.tar.gz/extender-0.0.7/extender/plugin.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import six
+from threading import local
+
+
+class IPlugin(local):
+    """
+    Plugin interface. Should not be inherited from directly.
+
+    A plugin should be treated as if it were a singleton. The owner does not
+    control when or how the plugin gets instantiated, nor is it guaranteed that
+    it will happen, or happen more than once.
+
+    All children should allow ``**kwargs`` on all inherited methods.
+    """
+    # Generic plugin information
+    title = None
+    slug = None
+    description = None
+    version = None
+    priority = 0
+    author = None
+    author_url = None
+    resource_links = ()
+
+    # Global enabled state
+    enabled = True
+    can_disable = True
+
+    def is_enabled(self):
+        """
+        Returns a boolean representing if this plugin is enabled.
+
+        If ``project`` is passed, it will limit the scope to that project.
+        """
+        if not self.enabled:
+            return False
+        if not self.can_disable:
+            return True
+        return True
+
+
+class PluginMount(type):
+    def __new__(mcs, name, bases, attrs):
+        new_cls = type.__new__(mcs, name, bases, attrs)
+        if IPlugin in bases:
+            return new_cls
+        if not new_cls.title:
+            new_cls.title = new_cls.__name__
+        if not new_cls.slug:
+            new_cls.slug = new_cls.title.replace(' ', '-').lower()
+        return new_cls
+
+
+class Plugin(six.with_metaclass(PluginMount, IPlugin)):
+    """
+    A plugin should be treated as if it were a singleton. The owner does not
+    control when or how the plugin gets instantiated, nor is it guaranteed that
+    it will happen, or happen more than once.
+    """
+    pass
