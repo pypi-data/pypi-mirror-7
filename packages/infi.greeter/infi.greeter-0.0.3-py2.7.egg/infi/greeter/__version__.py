f900b9527e1f1989f59c1b6b26434a4a87a5004e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:58 +0000

    Add package infi.greeter/0.0.3/infi.greeter-0.0.3-py2.7.egg

---
+++ packages/infi.greeter/infi.greeter-0.0.3-py2.7.egg/infi/greeter/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.3"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "yalon@infinidat.com"
+__git_branch__ = '(detached from v0.0.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '4d85d535a42a8cce8c69060a6df091042e6e9ece'
+__git_head_subject__ = 'added line wrapping for log display'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
