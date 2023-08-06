410f67e6a4266b1c122b2a3cf35a66bcd1e7f3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:25 +0000

    Add package riak/2.1.0/riak-2.1.0.tar.gz

---
+++ packages/riak/riak-2.1.0.tar.gz/riak-2.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import platform
+from setuptools import setup, find_packages
+from version import get_version
+from commands import preconfigure, configure, create_bucket_types, \
+    setup_security, enable_security, disable_security
+
+install_requires = ["riak_pb >=2.0.0", "pyOpenSSL >= 0.14"]
+requires = ["riak_pb(>=2.0.0)", "pyOpenSSL(>=0.14)"]
+tests_require = []
+if platform.python_version() < '2.7':
+    tests_require.append("unittest2")
+
+setup(
+    name='riak',
+    version=get_version(),
+    packages=find_packages(),
+    requires=requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    package_data={'riak': ['erl_src/*']},
+    description='Python client for Riak',
+    zip_safe=True,
+    options={'easy_install': {'allow_hosts': 'pypi.python.org'}},
+    include_package_data=True,
+    license='Apache 2',
+    platforms='Platform Independent',
+    author='Basho Technologies',
+    author_email='clients@basho.com',
+    test_suite='riak.tests.suite',
+    url='https://github.com/basho/riak-python-client',
+    cmdclass={'create_bucket_types': create_bucket_types,
+              'setup_security': setup_security,
+              'preconfigure': preconfigure,
+              'configure': configure,
+              'enable_security': enable_security,
+              'disable_security': disable_security},
+    classifiers=['License :: OSI Approved :: Apache Software License',
+                 'Intended Audience :: Developers',
+                 'Operating System :: OS Independent',
+                 'Topic :: Database']
+    )
