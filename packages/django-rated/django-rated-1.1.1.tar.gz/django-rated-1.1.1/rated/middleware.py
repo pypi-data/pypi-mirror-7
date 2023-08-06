d51b56c570a588215f706ef093e5d8cfc509d325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package django-rated/1.1.1/django-rated-1.1.1.tar.gz

---
+++ packages/django-rated/django-rated-1.1.1.tar.gz/django-rated-1.1.1/rated/middleware.py
@@ -0,0 +1,25 @@
+
+from . import settings
+from .signals import rate_limited
+from .utils import BACKEND
+
+
+class RatedMiddleware(object):
+    '''
+    Middleware to check for annotated views on each request.
+    '''
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Try to determine the realm for this view
+        try:
+            realm = settings.REALM_MAP[request.resolver_match.url_name]
+        except KeyError:
+            return None
+
+        source = BACKEND.source_for_request(request)
+
+        if BACKEND.check_realm(source, realm):
+            rate_limited.send_robust(realm, client=source)
+            return BACKEND.make_limit_response(realm)
+
+        return None
---
+++ packages/django-rated/django-rated-1.1.1.tar.gz/django-rated-1.1.1/rated/settings.py
@@ -0,0 +1,19 @@
+
+from django.conf import settings
+
+DEFAULT_REALM = getattr(settings, 'RATED_DEFAULT_REALM', 'default')
+DEFAULT_LIMIT = getattr(settings, 'RATED_DEFAULT_LIMIT', 100)
+DEFAULT_DURATION = getattr(settings, 'RATED_DEFAULT_DURATION', 60 * 60)
+
+RESPONSE_CODE = getattr(settings, 'RATED_RESPONSE_CODE', 429)
+RESPONSE_MESSAGE = getattr(settings, 'RATED_RESPONSE_MESSAGE', '')
+
+DEFAULT_WHITELIST = getattr(settings, 'RATED_DEFAULT_WHITELIST', [])
+
+REALMS = getattr(settings, 'RATED_REALMS', {})
+
+REALM_MAP = getattr(settings, 'RATED_REALM_MAP', {})
+
+# Redis config parameters
+REDIS = getattr(settings, 'RATED_REDIS', {})
+
