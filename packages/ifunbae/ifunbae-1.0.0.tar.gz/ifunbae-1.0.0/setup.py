2fefa34414564c9ef5b7a9012b0f817eca39e13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:14 +0000

    Add package ifunbae/1.0.0/ifunbae-1.0.0.tar.gz

---
+++ packages/ifunbae/ifunbae-1.0.0.tar.gz/ifunbae-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'ifunbae',
+        version = '1.0.0',
+        py_modules =['ifuns'],
+        author = 'ifunbae',
+        author_email = 'ifunbae@gmail.com',
+        url = '',
+        description = 'i like python',
+        )
