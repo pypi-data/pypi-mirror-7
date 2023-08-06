a73f1a07db99e1df0c684318f7b48a144495333c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:25 +0000

    Add package clustalo/0.1.1/clustalo-0.1.1.tar.gz

---
+++ packages/clustalo/clustalo-0.1.1.tar.gz/clustalo-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup, Extension
+
+import os
+
+OPENMP_DISABLED = os.environ.get('OPENMP_DISABLED', False)
+libraries=['clustalo', 'stdc++']
+extra_compile_args = []
+
+if not OPENMP_DISABLED:
+    libraries.append('gomp')
+    extra_compile_args.append('-fopenmp')
+
+module = Extension('clustalo',
+                   sources = ['clustalo.c'],
+                   include_dirs=['/usr/include/clustalo', '/usr/local/include/clustalo'],
+                   libraries=libraries,
+                   extra_compile_args=extra_compile_args)
+
+setup(name='clustalo',
+      version='0.1.1',
+      description='Python wrapper around libclustalo',
+      author='Joshua Ma',
+      author_email='josh@benchling.com',
+      url='https://github.com/benchling/clustalo-python',
+      ext_modules=[module])
