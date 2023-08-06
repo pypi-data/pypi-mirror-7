e9613e9f0ffef7f00d05e6e9a263126e61cd33ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:29 +0000

    Add package streamcorpus_elasticsearch/0.1.1.dev1/streamcorpus_elasticsearch-0.1.1.dev1-py2.7.egg

---
+++ packages/streamcorpus_elasticsearch/streamcorpus_elasticsearch-0.1.1.dev1-py2.7.egg/streamcorpus_elasticsearch/__init__.py
@@ -0,0 +1,8 @@
+'''streamcorpus-elasticsearch are tools for loading streamcorpus
+StreamItems into ElasticSearch.
+
+.. This software is released under an MIT/X11 open source license.
+   Copyright 2012-2014 Diffeo, Inc.
+'''
+
+from streamcorpus_elasticsearch.writer import writer
