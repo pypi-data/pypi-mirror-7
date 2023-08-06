85b411e014e9c3f5af5832f1623e58f542226730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:46 +0000

    Add package K2fov/1.3.1/K2fov-1.3.1.tar.gz

---
+++ packages/K2fov/K2fov-1.3.1.tar.gz/K2fov-1.3.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+setup(name='K2fov',
+      version='1.3.1',
+      description='Find which targets are in the field of view of K2',
+      author='Tom Barclay',
+      author_email='tom@tombarclay.com',
+      url='https://github.com/mrtommyb/K2fov',
+      packages=['K2fov'],
+      install_requires=["numpy"],)
+
