a57c8b3f27d620cfe40ba7fdcaabc50a3949999f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:38 +0000

    Add package gaedeploy/0.1/gaedeploy-0.1.tar.gz

---
+++ packages/gaedeploy/gaedeploy-0.1.tar.gz/gaedeploy-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'gaedeploy',
+  packages = ['gaedeploy'],
+  version = '0.1',
+  description = 'Google AppEngine multi-environment deployment system',
+  author = 'Nigel Dunn',
+  author_email = 'nigel.dunn@gmail.com',
+  url = 'https://github.com/nigeldunn/gaedeploy',
+  download_url = 'https://github.com/nigeldunn/gaedeploy/tarball/0.1',
+  keywords = ['gae', 'deployment', 'clemenger'],
+  classifiers = [],
+)
