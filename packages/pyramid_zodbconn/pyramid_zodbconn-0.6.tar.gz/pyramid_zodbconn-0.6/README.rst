32e42f8bf86387cbf41b7cb97a0a0e510c6a0858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:22 +0000

    Add package pyramid_zodbconn/0.6/pyramid_zodbconn-0.6.tar.gz

---
+++ packages/pyramid_zodbconn/pyramid_zodbconn-0.6.tar.gz/pyramid_zodbconn-0.6/README.rst
@@ -0,0 +1,9 @@
+``pyramid_zodbconn``
+====================
+
+A package which provides integration between `ZODB <http://zodb.org>`_ and
+a Pyramid application.
+
+See the documentation at
+https://docs.pylonsproject.org/projects/pyramid_zodbconn/dev/ for more
+information.
---
+++ packages/pyramid_zodbconn/pyramid_zodbconn-0.6.tar.gz/pyramid_zodbconn-0.6/setup.py
@@ -0,0 +1,76 @@
+##############################################################################
+#
+# Copyright (c) 2008-2011 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+
+if sys.version_info < (3,):
+    install_requires = [
+        'pyramid',
+        'zodburi',
+        ]
+else:
+    install_requires = [
+        'pyramid',
+        'zodburi',
+        ]
+
+docs_extras = ['Sphinx']
+testing_extras = ['nose', 'coverage']
+
+setup(name='pyramid_zodbconn',
+      version='0.6',
+      description=('Provide integration betwen Pyramid and ZODB'),
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: Repoze Public License",
+        ],
+      keywords='wsgi pylons pyramid zodb zodbconn',
+      author="Chris McDonough",
+      author_email="pylons-devel@googlegroups.com",
+      url="http://docs.pylonsproject.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=install_requires,
+      test_suite="pyramid_zodbconn",
+      entry_points='',
+      extras_require = {
+          'testing':testing_extras,
+          'docs':docs_extras,
+          },
+      )
