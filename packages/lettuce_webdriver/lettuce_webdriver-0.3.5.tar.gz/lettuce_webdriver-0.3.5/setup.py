1c64bc2d502fa7f3b8fc82150c20c1240e19d97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:42 +0000

    Add package lettuce_webdriver/0.3.5/lettuce_webdriver-0.3.5.tar.gz

---
+++ packages/lettuce_webdriver/lettuce_webdriver-0.3.5.tar.gz/lettuce_webdriver-0.3.5/setup.py
@@ -0,0 +1,36 @@
+__version__ = '0.3.5'
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+setup(name='lettuce_webdriver',
+      version=__version__,
+      description='Selenium webdriver extension for lettuce',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        'Topic :: Software Development :: Testing',
+        ],
+      keywords='web lettuce bdd',
+      author="Nick Pilon, Ben Bangert",
+      author_email="npilon@gmail.com, ben@groovie.org",
+      url="https://github.com/bbangert/lettuce_webdriver/",
+      license="MIT",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = ['lettuce', 'selenium', 'nose'],
+      install_requires=['lettuce','selenium>=2.30.0'],
+      test_suite="lettuce_webdriver",
+      entry_points="""
+      [console_scripts]
+      lettuce_webdriver=lettuce_webdriver.parallel_bin:main
+      """
+      )
