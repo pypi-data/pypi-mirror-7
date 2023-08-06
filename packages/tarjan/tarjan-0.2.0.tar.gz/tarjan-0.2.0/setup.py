ec984e319edaad1b492fced2b1d7deac39914174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:12 +0000

    Add package tarjan/0.2.0/tarjan-0.2.0.tar.gz

---
+++ packages/tarjan/tarjan-0.2.0.tar.gz/tarjan-0.2.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from get_git_version import get_git_version
+
+setup(name='tarjan',
+      version=get_git_version(),
+      description='Implementation of Tarjan\'s algorithm: resolve cyclic deps',
+      author='Bas Westerbaan',
+      author_email='bas@westerbaan.name',
+      url='http://github.com/bwesterb/py-tarjan/',
+      packages=['tarjan'],
+      zip_safe=True,
+      package_dir={'tarjan': 'src'},
+      test_suite='tarjan.tests',
+      classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+        ],
+      )
