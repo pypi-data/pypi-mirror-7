9fdabc62422d8c41dc7688cbe821f7ed765a7ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.sysv_service/0.1.9/infi.sysv_service-0.1.9-py2.7.egg

---
+++ packages/infi.sysv_service/infi.sysv_service-0.1.9-py2.7.egg/infi/sysv_service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f68e3fc5b806d79be4a4585f0a7214dfffadf44d'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
