d4893f7a7ab065b1454c4c825b7adfa4732319c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:39 +0000

    Add package rest-toolkit/0.4.1/rest_toolkit-0.4.1.tar.gz

---
+++ packages/rest-toolkit/rest_toolkit-0.4.1.tar.gz/rest_toolkit-0.4.1/tests/resource_error.py
@@ -0,0 +1,21 @@
+from rest_toolkit import resource
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPPaymentRequired
+
+
+@resource('/keyerror')
+class KeyErrorResource(object):
+    def __init__(self, request):
+        raise KeyError('BOOM!')
+
+
+@resource('/http-error')
+class HTTPErrorResource(object):
+    def __init__(self, request):
+        raise HTTPPaymentRequired('BOOM!')
+
+
+@resource('/http-not-found')
+class HTTPNotFoundResource(object):
+    def __init__(self, request):
+        raise HTTPNotFound()
