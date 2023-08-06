e9386da971e6b18e7cbb6970b2139d85c1156cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package BayesFlare/1.0.0/BayesFlare-1.0.0.tar.gz

---
+++ packages/BayesFlare/BayesFlare-1.0.0.tar.gz/BayesFlare-1.0.0/bayesflare/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Analysis models.
+"""
+
+from .flare import Flare, ModelCurve
+from .transit import Transit
+from .impulse import Impulse
+from .expdecay import Expdecay
---
+++ packages/BayesFlare/BayesFlare-1.0.0.tar.gz/BayesFlare-1.0.0/bayesflare/stats/__init__.py
@@ -0,0 +1,13 @@
+"""
+BayesFlare Statistics
+==================
+
+Functions and classes for carrying out Bayesian analysis on the 
+lightcurves.
+
+"""
+
+
+from .thresholding import Thresholder
+from .general import *
+from .bayes import Bayes, ParameterEstimationGrid
