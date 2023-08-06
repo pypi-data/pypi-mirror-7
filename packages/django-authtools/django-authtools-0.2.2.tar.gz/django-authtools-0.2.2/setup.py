a10f31b2179ba6b47ae50225897f0ce4481878dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:06 +0000

    Add package django-authtools/0.2.2/django-authtools-0.2.2.tar.gz

---
+++ packages/django-authtools/django-authtools-0.2.2.tar.gz/django-authtools-0.2.2/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import subprocess
+import os
+
+__doc__ = ("Custom user model app for Django featuring email as username and"
+           " class-based views for authentication.")
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+install_requires = [
+    'Django>=1.5',
+]
+
+version = (0, 2, 2, 'final')
+
+
+def get_version():
+    number = '.'.join(map(str, version[:3]))
+    stage = version[3]
+    if stage == 'final':
+        return number
+    elif stage == 'alpha':
+        process = subprocess.Popen('git rev-parse HEAD'.split(), stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        return number + '-' + stdout.decode('utf-8').strip()[:8]
+
+setup(
+    name='django-authtools',
+    version=get_version(),
+    author='Fusionbox, Inc.',
+    author_email='programmers@fusionbox.com',
+    description=__doc__,
+    long_description=read('README.rst'),
+    url='https://django-authtools.readthedocs.org/',
+    license='BSD',
+    packages=[package for package in find_packages() if package.startswith('authtools')],
+    install_requires=install_requires,
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
