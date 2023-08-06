d1d09aac24c47142f8c334c70c033414fddd245f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:12 +0000

    Add package seaborn/0.4.0/seaborn-0.4.0.tar.gz

---
+++ packages/seaborn/seaborn-0.4.0.tar.gz/seaborn-0.4.0/seaborn/__init__.py
@@ -0,0 +1,12 @@
+from .rcmod import *
+from .utils import *
+from .palettes import *
+from .linearmodels import *
+from .distributions import *
+from .timeseries import *
+from .miscplot import *
+from .axisgrid import *
+from .xkcd_rgb import xkcd_rgb
+set()
+
+__version__ = "0.4.0"
---
+++ packages/seaborn/seaborn-0.4.0.tar.gz/seaborn-0.4.0/setup.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2012-2014 Michael Waskom <mwaskom@stanford.edu>
+
+DESCRIPTION = "Seaborn: statistical data visualization"
+LONG_DESCRIPTION = """\
+Seaborn is a library for making attractive and informative statistical graphics in Python. It is built on top of matplotlib and tightly integrated with the PyData stack, including support for numpy and pandas data structures and statistical routines from scipy and statsmodels.
+
+Some of the features that seaborn offers are
+
+- Several built-in themes that improve on the default matplotlib aesthetics
+- Tools for choosing color palettes to make beautiful plots that reveal patterns in your data
+- Functions for visualizing univariate and bivariate distributions or for comparing them between subsets of data
+- Tools that fit and visualize linear regression models for different kinds of independent and dependent variables
+- A function to plot statistical timeseries data with flexible estimation and representation of uncertainty around the estimate
+- High-level abstractions for structuring grids of plots that let you easily build complex visualizations
+"""
+
+DISTNAME = 'seaborn'
+MAINTAINER = 'Michael Waskom'
+MAINTAINER_EMAIL = 'mwaskom@stanford.edu'
+URL = 'http://stanford.edu/~mwaskom/software/seaborn/'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
+VERSION = '0.4.0'
+
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from disutils import setup
+
+
+def check_dependencies():
+
+    # Just make sure dependencies exist, I haven't rigorously
+    # tested what the minimal versions that will work are
+    # (help on that would be awesome)
+    try:
+        import numpy
+    except ImportError:
+        raise ImportError("seaborn requires numpy")
+    try:
+        import scipy
+    except ImportError:
+        raise ImportError("seaborn requires scipy")
+    try:
+        import matplotlib
+    except ImportError:
+        raise ImportError("seaborn requires matplotlib")
+    try:
+        import pandas
+    except ImportError:
+        raise ImportError("seaborn requires pandas")
+
+if __name__ == "__main__":
+    import os
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    import sys
+    if not (len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
+            sys.argv[1] in ('--help-commands', 'egg_info', '--version',
+                            'clean'))):
+        check_dependencies()
+
+    setup(name=DISTNAME,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        packages=['seaborn', 'seaborn.external', 'seaborn.tests'],
+        classifiers=[
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4',
+                     'License :: OSI Approved :: BSD License',
+                     'Topic :: Scientific/Engineering :: Visualization',
+                     'Topic :: Multimedia :: Graphics',
+                     'Operating System :: POSIX',
+                     'Operating System :: Unix',
+                     'Operating System :: MacOS'],
+          )
