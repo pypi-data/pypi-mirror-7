e8c9d09e32ea1e97322333a93fb314c270fe6fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:41 +0000

    Add package pyitect/0.1.14/pyitect-0.1.14.tar.gz

---
+++ packages/pyitect/pyitect-0.1.14.tar.gz/pyitect-0.1.14/pyitect/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pyitect is a pluginframe work
+"""
+
+__version__ = "0.1.14"
+
+from .pyitect import System
+
---
+++ packages/pyitect/pyitect-0.1.14.tar.gz/pyitect-0.1.14/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+local_file = lambda *f: \
+    open(os.path.join(os.path.dirname(__file__), *f)).read()
+
+setup(
+    name = "pyitect",
+    version = "0.1.14",
+    packages = find_packages(exclude=['*test*']),
+    install_requires=["setuptools >= 1.0"],
+
+    package_data = {
+        '': ['*.txt', '*.rst', '*.md'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Ryexander",
+    author_email = "Ryexander@gmail.com",
+    description = "A package for structuring a modeler project architecture via plugin like modules",
+    long_description = local_file("README.md"),
+    license = "ISC",
+    keywords = "architect project modeler plugin",
+    url = "https://github.com/Ryex/pyitect",
+    tests_require = [
+    ],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ]
+)
