09a36fa15b3f3b1083415d57ac5158ce24a2bf86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package infi.recipe.buildout_logging/0.1.2/infi.recipe.buildout_logging-0.1.2-py2.7.egg

---
+++ packages/infi.recipe.buildout_logging/infi.recipe.buildout_logging-0.1.2-py2.7.egg/infi/recipe/buildout_logging/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'fd7f3773d17854f5cdb7189a5675c1dcc22c026b'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
