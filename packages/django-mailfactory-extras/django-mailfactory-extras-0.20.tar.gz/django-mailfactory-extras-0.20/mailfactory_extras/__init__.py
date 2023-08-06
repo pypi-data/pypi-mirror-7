7c492c27584dd21383ef2bf35c2e58cb25476f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:28 +0000

    Add package django-mailfactory-extras/0.20/django-mailfactory-extras-0.20.tar.gz

---
+++ packages/django-mailfactory-extras/django-mailfactory-extras-0.20.tar.gz/django-mailfactory-extras-0.20/mailfactory_extras/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+__version__ = '0.20'
+__author__ = 'Javier Cordero'
+__email__ = 'jcorderomartinez@gmail.com'
+__license__ = 'Apache 2.0'
+
+from .smsfactory import SMSFactory
+
+
+smsfactory = SMSFactory()
