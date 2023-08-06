523aecf7b9e1e97107c0a4aaaa954c1d446f5db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:08 +0000

    Add package material/0.1/material-0.1.tar.gz

---
+++ packages/material/material-0.1.tar.gz/material-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'material',
+      version = '0.1',
+      keywords = ('python'),
+      url = '',
+      description = '',
+      license = 'public domain',
+      
+      author = 'Zhang Yiwei',
+      author_email = 'Karl1991223@126.com',
+      
+      packages = find_packages(),
+      platforms = 'any',
+      )
