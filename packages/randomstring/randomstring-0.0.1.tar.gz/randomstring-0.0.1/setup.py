d122db31a20386b510e4ab7241c0a8453c1b1128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package randomstring/0.0.1/randomstring-0.0.1.tar.gz

---
+++ packages/randomstring/randomstring-0.0.1.tar.gz/randomstring-0.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'randomstring',
+    version = '0.0.1',
+    keywords = ('randomstring', 'random', 'string'),
+    description = 'generate random string',
+    license = 'MIT License',
+
+    author = 'Bin He',
+    author_email = 'binhe22@gmail.com',
+    
+    packages = find_packages(),
+    platforms = 'any',
+)
