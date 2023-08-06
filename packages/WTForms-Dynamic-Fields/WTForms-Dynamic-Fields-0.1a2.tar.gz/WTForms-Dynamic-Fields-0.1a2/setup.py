0490c763a533fb833b6c2410d8991395dd691f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:51 +0000

    Add package WTForms-Dynamic-Fields/0.1a2/WTForms-Dynamic-Fields-0.1a2.tar.gz

---
+++ packages/WTForms-Dynamic-Fields/WTForms-Dynamic-Fields-0.1a2.tar.gz/WTForms-Dynamic-Fields-0.1a2/setup.py
@@ -0,0 +1,50 @@
+import sys
+import re
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+long_description = open('README.txt').read()
+
+with open('wtforms_dynamic_fields/__init__.py') as f:
+    m = re.findall(r'__version__\s*=\s*\'(.*)\'', f.read())
+    version = m[0]
+
+setup(
+    name='WTForms-Dynamic-Fields',
+    version=version,
+    url='https://github.com/timusan/wtforms-dynamic-fields',
+    license='BSD',
+    author='Tim van der Linden',
+    tests_require=['pytest'],
+    install_requires=['WTForms>=2.0.1'],
+    cmdclass={'test': PyTest},
+    author_email='tim@shisaa.jp',
+    description='Simple wrapper to add "dynamic" (sets of) fields to an already instantiated WTForms form.',
+    long_description=long_description,
+    packages=['wtforms_dynamic_fields', 'tests'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='wtforms_dynamic_fields.tests',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ]
+)
---
+++ packages/WTForms-Dynamic-Fields/WTForms-Dynamic-Fields-0.1a2.tar.gz/WTForms-Dynamic-Fields-0.1a2/wtforms_dynamic_fields/__init__.py
@@ -0,0 +1,3 @@
+from wtforms_dynamic_fields import WTFormsDynamicFields
+
+__version__ = '0.1a2'
