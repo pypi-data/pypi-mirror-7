19e419d9e070936e6cf81d3c81af766c82112aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:49 +0000

    Add package haven/1.0.29/haven-1.0.29.tar.gz

---
+++ packages/haven/haven-1.0.29.tar.gz/haven-1.0.29/haven/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.29'
+
+from .log import logger
+from .blueprint import Blueprint
+
+try:
+    from gevent_impl import GHaven, GLater
+except:
+    GHaven = GLater = None
+
+from thread_impl import THaven, TLater
