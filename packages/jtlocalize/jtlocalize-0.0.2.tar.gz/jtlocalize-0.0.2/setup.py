b94588ba72679144239deb3a936ef3dde2f60d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:14 +0000

    Add package jtlocalize/0.0.2/jtlocalize-0.0.2.tar.gz

---
+++ packages/jtlocalize/jtlocalize-0.0.2.tar.gz/jtlocalize-0.0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jtlocalize',
+      version='0.0.2',
+      description='iOS localization framework',
+      author='JoyTunes',
+      author_email='info@joytunes.com',
+      url='https://github.com/joytunes/JTLocalize/',
+      packages=['jtlocalize','jtlocalize.core', 'jtlocalize.configuration'],
+      license='MIT',
+     )
