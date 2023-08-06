3f3f4dae34e4d95a9731cf19b194ad550189e31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package git-up-all/0.3.0/git-up-all-0.3.0.tar.gz

---
+++ packages/git-up-all/git-up-all-0.3.0.tar.gz/git-up-all-0.3.0/PyGitUpAll/gitupall.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import os
+import sys
+import docopt
+
+from git import Repo, exc
+from git.repo.fun import is_git_dir
+from PyGitUp.git_wrapper import GitError
+from PyGitUp.gitup import GitUp
+from PyGitUpAll.utils import read_projects_from_json, read_projects_from_sourcetree
+from PyGitUpAll.project import Project
+from termcolor import colored
+
+
+PROJECTS_FILE = "projects.json"
+
+
+class GitUpAll(object):
+
+    start_dir = os.getcwd()
+
+    def git_up_all(self, sourcetree=False):
+
+        if sourcetree:
+            print(colored("Loading projects from ", attrs=["bold"]) + colored("SourceTree" + "\n", color="green"))
+            projects = read_projects_from_sourcetree()
+        else:
+            print(colored("Loading projects from ", attrs=["bold"]) + colored(PROJECTS_FILE + "\n", color="green"))
+            projects = read_projects_from_json(PROJECTS_FILE)
+
+        if not projects:
+            print(colored("Could not read projects\n", color="red"))
+            return False
+
+
+        for project_name, project_settings in projects.iteritems():
+
+            """
+            This nees to be rewritten and tested!
+            """
+            # Check if we are able to initialize settings to an object
+            project = Project(project_settings)
+            if not project:
+                print (colored("Could not read project " + project_name, color="red"))
+                continue
+            print(colored("- Working on: " + project.name + " @" + project.absolute_path, attrs=["underline"]))
+
+            # Check if a dir exists or clone
+            if (not os.path.isdir(project.absolute_path) or os.listdir(project.absolute_path) == []) and project.git_url:
+                print ("Project is not directory or not initialized repo. Cloning from url")
+                project.repo = Repo.clone_from(url=project.git_url, to_path=project.absolute_path)
+                print (colored("Repository cloned", color="green"))
+
+            # Check if the is not git and if not skip
+            if not is_git_dir(os.path.join(project.absolute_path, ".git")) and not project.git_url:
+                print (colored("Project path is not a git dir and has no info. Skipping", color="red"))
+                continue
+
+            self.sync_repository(project)
+
+            print(colored("Repository " + project.name + " updated\n", color="green", attrs=["bold"]))
+
+
+    @classmethod
+    def sync_repository(cls, project):
+
+        # ugly chdir. Will try to patch pygitup
+        os.chdir(project.absolute_path)
+        try:
+            # Creates a new object
+            GitUp().run()
+        except (GitError, exc.GitCommandError) as e:
+            print (colored("Could not update repository: " + project.name, color="red"))
+        finally:
+            os.chdir(cls.start_dir)
+
+
+def run():
+    if '--sourcetree' in sys.argv:
+        GitUpAll().git_up_all(sourcetree=True)
+    else:
+        GitUpAll().git_up_all(sourcetree=False)
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/git-up-all/git-up-all-0.3.0.tar.gz/git-up-all-0.3.0/setup.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="git-up-all",
+    version="0.3.0",
+    packages=find_packages(exclude=["tests"]),
+    scripts=['PyGitUpAll/gitupall.py'],
+    install_requires=['GitPython==0.3.2.RC1', 'git-up==1.1.4', 'biplist==0.7',
+                      'termcolor==1.1.0'],
+
+    # Tests
+    test_suite="nose.collector",
+    tests_require='nose',
+
+    # Executable
+    entry_points={
+        'console_scripts': [
+            'git-up-all = gitupall:run'
+        ]
+    },
+
+    zip_safe=False,
+
+    # Metadata
+    author="Dimitrios Kanellopoulos",
+    author_email="jimmykane9@gmail.com",
+    description="Run git-up trough a json list of repos",
+    license="GPLv3",
+    keywords="git git-up git-up-all",
+    url="https://github.com/jimmykane/PyGitUpAll",
+    download_url='https://github.com/jimmykane/PyGitUpAll/tarball/master/0.3.0',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Software Development :: Version Control",
+        "Topic :: Utilities"
+    ],
+
+    long_description=open('README.rst').read()
+)
