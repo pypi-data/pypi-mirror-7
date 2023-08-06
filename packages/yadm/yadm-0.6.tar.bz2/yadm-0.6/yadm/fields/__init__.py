28f213473ae9dfd0159b17d6c7d0f6cc8d149590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package yadm/0.6/yadm-0.6.tar.bz2

---
+++ packages/yadm/yadm-0.6.tar.bz2/yadm-0.6/yadm/fields/__init__.py
@@ -0,0 +1,27 @@
+"""
+This package contain all fields.
+"""
+
+from yadm.fields.base import FieldDescriptor, Field
+
+from yadm.fields.simple import (
+    ObjectIdField,
+    BooleanField,
+    IntegerField,
+    FloatField,
+    StringField,
+)
+
+from yadm.fields.email import EmailField
+
+from yadm.fields.datetime import DatetimeField
+from yadm.fields.decimal import DecimalField
+from yadm.fields.money import Money, MoneyField
+
+from yadm.fields.embedded import EmbeddedDocumentField
+
+from yadm.fields.list import ListField
+from yadm.fields.set import SetField
+from yadm.fields.map import MapField
+
+from yadm.fields.reference import ReferenceField
