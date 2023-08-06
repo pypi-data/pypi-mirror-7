85a1ec03ab1a766d987760c2eff97257a1892159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package cloneall/1.0.5/cloneall-1.0.5.tar.gz

---
+++ packages/cloneall/cloneall-1.0.5.tar.gz/cloneall-1.0.5/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(name='cloneall',
+	  version='1.0.5',
+	  description="Clone GitHub repositories",
+	  author="Bede Kelly",
+	  author_email="bedekelly97@gmail.com",
+	  url="https://github.com/bedekelly/clone-them-all",
+	  scripts=['cloneall'])
