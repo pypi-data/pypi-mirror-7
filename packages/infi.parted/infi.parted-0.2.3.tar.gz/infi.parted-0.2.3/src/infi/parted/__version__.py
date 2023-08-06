e7cdf90676d9672b06028f9ca55e220ad7032064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package infi.parted/0.2.3/infi.parted-0.2.3.tar.gz

---
+++ packages/infi.parted/infi.parted-0.2.3.tar.gz/infi.parted-0.2.3/src/infi/parted/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.3"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8ab56806bd5d700ea36107e72eda2edbed9c2318'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
