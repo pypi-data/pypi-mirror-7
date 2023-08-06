3c8e7a8948817fb8f6e40cf69c4e2bd6a6481905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:56 +0000

    Add package pip-autoremove/0.4.0/pip-autoremove-0.4.0.tar.gz

---
+++ packages/pip-autoremove/pip-autoremove-0.4.0.tar.gz/pip-autoremove-0.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="pip-autoremove",
+    version="0.4.0",
+    description="Remove a package and its unused dependencies",
+    long_description=open('README.rst').read(),
+    py_modules=["pip_autoremove"],
+    license='Apache License 2.0',
+    url='https://github.com/invl/pip-autoremove',
+    entry_points="""
+    [console_scripts]
+    pip-autoremove = pip_autoremove:main
+    """,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
+)
