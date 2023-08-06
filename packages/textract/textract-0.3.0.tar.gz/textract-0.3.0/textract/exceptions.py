749f2c80bfd900090bb06121e08c5e3e64893087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:50 +0000

    Add package textract/0.3.0/textract-0.3.0.tar.gz

---
+++ packages/textract/textract-0.3.0.tar.gz/textract-0.3.0/textract/exceptions.py
@@ -0,0 +1,49 @@
+import os
+
+
+# traceback from exceptions that inherit from this class are suppressed
+class CommandLineError(Exception):
+    def render(self, msg):
+        return msg % vars(self)
+
+
+class ExtensionNotSupported(CommandLineError):
+    def __init__(self, ext):
+        self.ext = ext
+
+    def __str__(self):
+        return self.render((
+            'The filename extension %(ext)s is not yet supported by\n'
+            'textract. Please suggest this filename extension here:\n'
+            '    https://github.com/deanmalmgren/textract/issues'
+        ))
+
+
+class MissingFileError(CommandLineError):
+    def __init__(self, filename):
+        self.filename = filename
+        self.root, self.ext = os.path.splitext(filename)
+
+    def __str__(self):
+        return self.render((
+            'The file "%(filename)s" can not be found.\n'
+            'Is this the right path/to/file/you/want/to/extract%(ext)s?'
+        ))
+
+
+class UnknownMethod(CommandLineError):
+    def __init__(self, method):
+        self.method = method
+
+    def __str__(self):
+        return self.render((
+            'The method "%(method)s" can not be found for this filetype.'
+        ))
+
+
+class ShellError(CommandLineError):
+    def __init__(self, exit_code):
+        self.exit_code = exit_code
+
+    def __str__(self):
+        return "Command failed with exit code %s" % self.exit_code
