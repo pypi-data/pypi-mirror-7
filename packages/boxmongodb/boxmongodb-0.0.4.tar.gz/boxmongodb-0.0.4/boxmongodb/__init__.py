7ee619dd4f602086e571625687bc4834e54dbebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:43 +0000

    Add package boxmongodb/0.0.4/boxmongodb-0.0.4.tar.gz

---
+++ packages/boxmongodb/boxmongodb-0.0.4.tar.gz/boxmongodb-0.0.4/boxmongodb/__init__.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'boxmongodb',
+    version = '0.0.1',
+    keywords = ('mongodb', 'db'),
+    description = 'just a simple mongodb orm',
+    license = 'MIT License',
+    author = 'CcdjhMarx',
+    author_email = 'ccdjh.marx@all.com',
+    url='http://boxmongodb.box-game.com',
+    
+    packages = find_packages(),
+)
