1af6dcc69b9873ab57e4829c8e3b2c382ba41353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:50 +0000

    Add package chumpy/0.51/chumpy-0.51.tar.gz

---
+++ packages/chumpy/chumpy-0.51.tar.gz/chumpy-0.51/setup.py
@@ -0,0 +1,19 @@
+"""
+Author(s): Matthew Loper
+
+See LICENCE.txt for licensing and contact information.
+"""
+
+from distutils.core import setup
+
+setup(name='chumpy',
+      version='0.51',
+      #py_modules=['ch', 'ch_ops', 'linalg', 'utils', 'api_compatibility', 'ch_random', 'test_ch', 'test_inner_composition', 'test_linalg'],
+      packages = ['chumpy', 'chumpy.test_ch'],
+      package_dir = {'chumpy': '.'},
+      author='Matthew Loper',
+      author_email='matt.loper@gmail.com',
+      url='https://github.com/mattloper/chumpy',
+      description='chumpy',
+      )
+
