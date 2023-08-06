e8b054c82d6e3e47559a94b6029a6169049eb8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:02 +0000

    Add package django-jsonview/0.4.3/django-jsonview-0.4.3.tar.gz

---
+++ packages/django-jsonview/django-jsonview-0.4.3.tar.gz/django-jsonview-0.4.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+import jsonview
+
+
+setup(
+    name='django-jsonview',
+    version=jsonview.__version__,
+    description='Always return JSON from your Django view.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='me@jamessocol.com',
+    url='https://github.com/jsocol/django-jsonview',
+    license='Apache v2.0',
+    packages=find_packages(exclude=['test_settings.py']),
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
