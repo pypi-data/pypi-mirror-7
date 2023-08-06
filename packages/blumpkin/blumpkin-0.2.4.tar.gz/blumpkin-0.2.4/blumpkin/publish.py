09885a5653670ef3d2352cc16ccd1956400ab281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:18 +0000

    Add package blumpkin/0.2.4/blumpkin-0.2.4.tar.gz

---
+++ packages/blumpkin/blumpkin-0.2.4.tar.gz/blumpkin-0.2.4/blumpkin/publish.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+import os
+from subprocess import call
+
+import click
+
+from . import config
+
+
+@click.command('publish')
+@click.option('--branch', nargs=1, default=config['PUBLISH_BRANCH'])
+@click.option('--index', nargs=1, default=config['PYPI_INDEX'])
+def publish(branch, index):
+    if branch and branch != os.environ.get('TRAVIS_BRANCH', branch):
+        click.echo('Not on the publishing branch')
+        return
+    args = ['python', 'setup.py', 'sdist', 'upload']
+    if index:
+        args += ['-r', 'alt']
+    print ' '.join(args)
+    return call(args)
