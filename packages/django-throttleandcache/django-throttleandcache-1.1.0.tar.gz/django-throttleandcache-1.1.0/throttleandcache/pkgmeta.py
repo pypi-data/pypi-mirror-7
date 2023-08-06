423a7656f7c20c9055be01a1f090a43624067f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:08 +0000

    Add package django-throttleandcache/1.1.0/django-throttleandcache-1.1.0.tar.gz

---
+++ packages/django-throttleandcache/django-throttleandcache-1.1.0.tar.gz/django-throttleandcache-1.1.0/throttleandcache/pkgmeta.py
@@ -0,0 +1,5 @@
+__title__ = 'django-throttleandcache'
+__author__ = 'Matthew Dapena-Tretter'
+__version__ = '1.1.0'
+__license__ = 'BSD'
+__all__ = ['__title__', '__author__', '__version__', '__license__']
