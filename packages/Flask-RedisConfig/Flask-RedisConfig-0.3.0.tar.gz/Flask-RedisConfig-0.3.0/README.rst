19dd21fa85b88325d09943c2299b07bd6c48af38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:14 +0000

    Add package Flask-RedisConfig/0.3.0/Flask-RedisConfig-0.3.0.tar.gz

---
+++ packages/Flask-RedisConfig/Flask-RedisConfig-0.3.0.tar.gz/Flask-RedisConfig-0.3.0/README.rst
@@ -0,0 +1,62 @@
+=================
+Flask-RedisConfig
+=================
+
+.. image:: http://img.shields.io/pypi/v/Flask-RedisConfig.png
+
+Redis-backed config for Flask applications based on the `disqus/durabledict <https://github.com/disqus/durabledict>`_ library
+
+-----
+Usage
+-----
+::
+
+    from flask import Flask
+    from flask.ext.redisconfig import RedisConfig
+
+    app = Flask(__name__)
+    config = RedisConfig('app:config')
+    config.init_app(app)
+
+----------------------
+Command Line Interface
+----------------------
+A CLI utility is provided for setting or reviewing config values easily.
+
+::
+
+    config.cli()
+
+::
+
+    Flask-RedisConfig
+    > help
+
+    Documented commands (type help <topic>):
+    ========================================
+    exit  get  help  list  set  set_bool  set_int
+
+    > set SECRET_KEY oo5thuj4kaem2Pai0iviefahkaShah5iemae8Aev
+    > get SECRET_KEY
+    SECRET_KEY = 'oo5thuj4kaem2Pai0iviefahkaShah5iemae8Aev'
+    > set_bool PRESERVE_CONTEXT_ON_EXCEPTION False
+    > set_int SQLALCHEMY_POOL_SIZE 100
+    > list
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SECRET_KEY = 'oo5thuj4kaem2Pai0iviefahkaShah5iemae8Aev'
+    SQLALCHEMY_POOL_SIZE = 100
+    >
+
+-------
+Example
+-------
+
+::
+
+    $ python example.py config
+    $ python example.py
+
+-------
+License
+-------
+http://marksteve.mit-license.org
---
+++ packages/Flask-RedisConfig/Flask-RedisConfig-0.3.0.tar.gz/Flask-RedisConfig-0.3.0/flask_redisconfig.py
@@ -0,0 +1,131 @@
+import cmd
+
+from durabledict import RedisDict
+from redis import Redis
+
+
+class RedisConfig(RedisDict):
+    def __init__(self, key_prefix, host='localhost', port=6379, app=None):
+        self.key_prefix = key_prefix
+        self.host = host
+        self.port = port
+        self.redis = Redis(self.host, self.port)
+        super(RedisConfig, self).__init__(self.key_prefix, self.redis,
+                                          autosync=False)
+        if app:
+            self.init_app(app)
+
+    def load(self, app):
+        app.config.update(**self)
+
+    init_app = load
+
+    def cli(self):
+        try:
+            Cli(self).cmdloop()
+        except KeyboardInterrupt:
+            raise SystemExit
+
+
+class Cli(cmd.Cmd, object):
+
+    intro = 'Flask-RedisConfig'
+    prompt = '> '
+
+    def __init__(self, redisconfig):
+        self.redisconfig = redisconfig
+        super(Cli, self).__init__()
+
+    def do_set(self, args, cast=str):
+        """
+        set KEY VALUE
+
+        Sets a config value
+        """
+        try:
+            key, value = args.split(' ')
+            if cast == bool:
+                if value.isdigit():
+                    value = bool(int(value))
+                else:
+                    value = dict(true=True, false=False).get(value.lower())
+                    if value is None:
+                        raise ValueError
+            else:
+                value = cast(value)
+            self.redisconfig[key] = value
+            print '%s = %r' % (key, value)
+        except ValueError:
+            self.do_help('set')
+
+    def do_set_int(self, args):
+        """
+        set_int KEY VALUE
+
+        Sets a config integer value
+        """
+        self.do_set(args, cast=int)
+
+    def do_set_bool(self, args):
+        """
+        set_bool KEY VALUE
+
+        Sets a config boolean value
+        """
+        self.do_set(args, cast=bool)
+
+    def do_get(self, args):
+        """
+        get KEY
+
+        Gets a config value
+        """
+        try:
+            key, = args.split(' ', 1)
+            if not key:
+                raise ValueError
+            self.redisconfig.sync()
+            print '%s = %r' % (key, self.redisconfig.get(key))
+        except ValueError:
+            self.do_help('get')
+
+    def do_del(self, args):
+        """
+        del KEY
+
+        Deletes a config value
+        """
+        try:
+            key, = args.split(' ', 1)
+            if not key:
+                raise ValueError
+            del self.redisconfig[key]
+            print 'Deleted %r' % key
+        except ValueError:
+            self.do_help('del')
+
+    def do_list(self, args):
+        """
+        list
+
+        Lists all config items
+        """
+        self.redisconfig.sync()
+        for key in sorted(self.redisconfig.durables()):
+            print '%s = %r' % (key, self.redisconfig[key])
+
+    def do_help(self, args):
+        """
+        help
+
+        Prints help stuff
+        """
+        super(Cli, self).do_help(args)
+
+    def do_exit(self, args):
+        """
+        exit
+
+        Quits cli
+        """
+        raise SystemExit
