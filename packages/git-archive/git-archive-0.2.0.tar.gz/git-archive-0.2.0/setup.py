4bd24e3e6f6438707ef32417bfdd21e41763e80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:42 +0000

    Add package git-archive/0.2.0/git-archive-0.2.0.tar.gz

---
+++ packages/git-archive/git-archive-0.2.0.tar.gz/git-archive-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="git-archive",
+    version="0.2.0",
+    author="Eric Chiang",
+    author_email="eric@yhathq.com",
+    url="https://github.com/EricChiang/git-archive",
+    packages=find_packages(),
+    description="Go back into the git archive",
+    license="MIT",
+    scripts=['bin/git-archive'],
+)
