a29fea13f1678eed710604b3b451f5938ea2ff8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:03 +0000

    Add package repackage/0.3/repackage-0.3.tar.gz

---
+++ packages/repackage/repackage-0.3.tar.gz/repackage-0.3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+
+# -------------------------
+# Setup
+# -------------------------
+setup(name='repackage',
+      version='0.3',
+      description= ("Repackaging, call a non-registered package in any "
+                    "directory (with relative call). "
+                    "Used either by modules moved into to a subdirectory "
+                    "or to prepare the import of a non-registered package "
+                    "(in any relative path)."),
+      url='www.settlenext.com',
+      author='Laurent Franceschetti',
+      author_email='developer@settlenext.com',
+      keywords ="package relative path module import library",
+      license='MIT',
+      packages=['repackage'],
+      long_description=open('README.txt').read(),
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License"
+      ],
+      zip_safe=False)
+
+
+
