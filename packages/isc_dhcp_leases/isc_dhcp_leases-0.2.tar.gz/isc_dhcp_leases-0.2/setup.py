12019dd917889bbeb387fdd08147e068faa75cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:44 +0000

    Add package isc_dhcp_leases/0.2/isc_dhcp_leases-0.2.tar.gz

---
+++ packages/isc_dhcp_leases/isc_dhcp_leases-0.2.tar.gz/isc_dhcp_leases-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='isc_dhcp_leases',
+    version='0.2',
+    packages=['isc_dhcp_leases'],
+    url='https://github.com/MartijnBraam/python-isc-dhcp-leases',
+    license='MIT',
+    author='Martijn Braam',
+    author_email='martijn@brixit.nl',
+    description='Small python module for reading /var/lib/dhcp/dhcpd.leases from isc-dhcp-server'
+)
