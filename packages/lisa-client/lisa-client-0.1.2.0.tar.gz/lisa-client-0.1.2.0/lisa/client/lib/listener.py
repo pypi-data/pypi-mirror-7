2f4a634c864ffa7eef0627943dc39dab2c977707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:07 +0000

    Add package lisa-client/0.1.2.0/lisa-client-0.1.2.0.tar.gz

---
+++ packages/lisa-client/lisa-client-0.1.2.0.tar.gz/lisa-client-0.1.2.0/lisa/client/lib/listener.py
@@ -0,0 +1,145 @@
+# -*- coding: UTF-8 -*-
+
+# Imports
+import pygst
+pygst.require('0.10')
+import gobject
+gobject.threads_init()
+from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop(set_as_default=True)
+import gst, os
+import threading
+try: # It fixes a bug with the pocketpshinx import. The first time it fails, but the second import is ok.
+    import pocketsphinx
+except:
+    pass
+import pocketsphinx
+from twisted.python import log
+from lisa.client.lib.speaker import Speaker
+from lisa.client.lib.recorder import Recorder
+from lisa.client.ConfigManager import ConfigManagerSingleton
+
+# Current path
+PWD = os.path.dirname(os.path.abspath(__file__))
+
+
+class Listener(threading.Thread):
+    """
+    The goal is to listen for a keyword, then it starts a voice recording
+    """
+
+    def __init__(self, lisa_client, botname):
+        # Init thread class
+        threading.Thread.__init__(self)
+        self._stopevent = threading.Event()
+
+        configuration = ConfigManagerSingleton.get().getConfiguration()
+        self.botname = lisa_client.botname.lower()
+        self.scores = []
+        self.record_time_start = 0
+        self.record_time_end = 0
+        self.wit_thread = None
+        self.loop = None
+        if configuration.has_key("keyword_score"):
+            self.keyword_score = configuration['keyword_score']
+        else:
+            self.keyword_score = -10000
+
+        # Build Gstreamer pipeline : mic->Pulse->tee|->queue->audioConvert->audioResample->vader->pocketsphinx->fakesink
+        #                                           |->queue->audioConvert->audioResample->lamemp3enc->appsink
+        # fakesink : async=false is mandatory for parallelism
+        self.pipeline = gst.parse_launch('pulsesrc '
+                                        #+ '! ladspa-gate Threshold=-30.0 Decay=2.0 Hold=2.0 Attack=0.1 '
+                                        + '! tee name=audio_tee '
+                                        + ' audio_tee. ! queue '
+                                        + '            ! audioconvert ! audioresample '
+                                        + '            ! audio/x-raw-int, format=(string)S16_LE, channels=1, rate=16000 '
+                                        + '            ! lamemp3enc bitrate=64 mono=true '
+                                        + '            ! appsink name=rec_sink emit-signals=true '
+                                        + ' audio_tee. ! queue '
+                                        + '            ! audioconvert ! audioresample '
+                                        + '            ! vader name=vad_asr auto-threshold=true'
+                                        + '            ! tee name=asr_tee '
+                                        + '             asr_tee. ! fakesink async=false name=asr_sink'
+                                        + '             asr_tee. ! pocketsphinx name=asr '
+                                        + '                      ! fakesink async=false'
+                                         )
+
+        # Create recorder
+        self.recorder = Recorder(lisa_client = lisa_client, listener = self)
+
+        # Find client path
+        if os.path.isdir('/var/lib/lisa/client/pocketsphinx'):
+            client_path = '/var/lib/lisa/client/pocketsphinx'
+        else:
+            client_path = "%s/pocketsphinx" % PWD
+
+        # PocketSphinx configuration
+        asr = self.pipeline.get_by_name('asr')
+        asr.set_property("dict", "%s/%s.dic" % (client_path, self.botname))
+        asr.set_property("lm", "%s/%s.lm" % (client_path, self.botname))
+        if configuration.has_key("hmm"):
+            hmm_path = "%s/%s" % (client_path, configuration["hmm"])
+            if os.path.isdir(hmm_path):
+                asr.set_property("hmm", hmm_path)
+        asr.connect('result', self._asr_result)
+        asr.set_property('configured', True)
+        self.ps = pocketsphinx.Decoder(boxed=asr.get_property('decoder'))
+
+        # Start thread
+        self.start()
+
+    def run(self):
+        """
+        Listener main loop
+        """
+        Speaker.speak("ready")
+        self.pipeline.set_state(gst.STATE_PLAYING)
+
+        # Thread loop
+        self.loop = gobject.MainLoop()
+        self.loop.run()
+
+    def stop(self):
+        """
+        Stop listener.
+        """
+        Speaker.speak('lost_server')
+
+        # Stop everything
+        self.pipeline.set_state(gst.STATE_NULL)
+        self.recorder.stop()
+        if self.loop is not None:
+            self.loop.quit()
+
+    def _asr_result(self, asr, text, uttid):
+        """
+        Result from pocketsphinx : checking keyword recognition
+        """
+        # Check keyword detection
+        if text.lower() == self.botname and self.recorder.get_running_state() == False:
+            # Get scrore from decoder
+            dec_text, dec_uttid, dec_score = self.ps.get_hyp()
+
+            # Detection must have a minimal score to be valid
+            if dec_score < self.keyword_score:
+                log.msg("I recognized the %s keyword but I think it's a false positive according the %s score" % (self.botname, dec_score))
+                return
+
+            # Logs
+            self.scores.append(dec_score)
+            log.msg("======================")
+            log.msg("%s keyword detected" % self.botname)
+            log.msg("score: {} (min {}, moy {}, max {})".format(dec_score, min(self.scores), sum(self.scores) / len(self.scores), max(self.scores)))
+
+            # Start voice recording
+            Speaker.speak('yes')
+
+            # Start recorder
+            self.recorder.set_running_state(True)
+
+    def get_pipeline(self):
+        """
+        Return Gstreamer pipeline
+        """
+        return self.pipeline
---
+++ packages/lisa-client/lisa-client-0.1.2.0.tar.gz/lisa-client-0.1.2.0/lisa/client/service.py
@@ -0,0 +1,265 @@
+# -*- coding: UTF-8 -*-
+
+# Imports
+from twisted.python import log
+import signal
+gobjectnotimported = False
+try:
+    from dbus.mainloop.glib import DBusGMainLoop
+    DBusGMainLoop(set_as_default=True)
+    import gobject
+    import pygst
+    pygst.require('0.10')
+    gobject.threads_init()
+    from lisa.client import lib
+    from lib import Listener
+    from lib import Speaker
+except:
+    gobjectnotimported = True
+from twisted.internet import ssl, utils
+from twisted.internet.protocol import ReconnectingClientFactory
+from twisted.internet.defer import inlineCallbacks, DeferredQueue
+from twisted.application.internet import TCPClient
+from twisted.protocols.basic import LineReceiver
+from twisted.application import internet, service
+from twisted.internet import reactor
+from lisa.client.ConfigManager import ConfigManagerSingleton
+import json, os
+from OpenSSL import SSL
+import platform
+
+# Globals
+PWD = os.path.dirname(os.path.abspath(__file__))
+configuration = None
+LisaFactory = None
+
+
+class LisaClient(LineReceiver):
+    """
+    Lisa TCP client
+    """
+    def __init__(self):
+        self.factory = None
+        self.configuration = ConfigManagerSingleton.get().getConfiguration()
+        self.listener = None
+        self.debug_input = False
+        self.debug_output = False
+        if self.configuration.has_key("debug"):
+            if self.configuration["debug"].has_key("debug_input"):
+                self.debug_input = self.configuration["debug"]["debug_input"]
+            if self.configuration["debug"].has_key("debug_output"):
+                self.debug_output = self.configuration["debug"]["debug_output"]
+        self.zone = ""
+        if self.configuration.has_key("zone"):
+            self.zone = self.configuration['zone']
+
+
+    def sendMessage(self, message, type='chat', dict=None):
+        if dict:
+            line = json.dumps(
+                {
+                    "from": unicode(platform.node()),
+                    "type": type,
+                    "body": unicode(message),
+                    "zone": self.zone,
+                    "outcome": dict
+                }
+            )
+        else:
+            line = json.dumps(
+                {
+                    "from": unicode(platform.node()),
+                    "type": type,
+                    "body": unicode(message),
+                    "zone": self.zone
+                }
+            )
+
+        if self.debug_output:
+            log.msg('OUTPUT: %s' % line)
+
+        # send line to the server
+        self.sendLine(line)
+
+    def lineReceived(self, data):
+        """
+        Data received callback
+        The nolistener in configuration is here to let the travis build pass without loading gst
+        """
+
+        datajson = json.loads(data)
+        if self.debug_input == True:
+            log.msg("INPUT: " + unicode(datajson))
+
+        if datajson.has_key("type"):
+            if datajson['type'] == 'chat':
+                if datajson.has_key('nolistener') == False:
+                    Speaker.speak(datajson['body'])
+
+            elif datajson['type'] == 'command':
+                if datajson['command'] == 'LOGIN':
+                    # Get Bot name
+                    botname = unicode(datajson['bot_name'])
+                    log.msg("setting botname to %s" % botname)
+                    self.botname = botname
+
+                    # Send TTS
+                    if datajson.has_key('nolistener') == False:
+                        Speaker.start()
+                        Speaker.speak(datajson['body'])
+
+                    # Create listener
+                    if datajson.has_key('nolistener') == False and not self.listener:
+                        self.listener = Listener(lisa_client = self, botname = botname)
+
+                # TODO seems a bit more complicated than I thought. I think the reply will be another type like "answer"
+                # TODO and will contains a unique ID. On server side, the question will be stored in mongodb so it will
+                # TODO let possible the multi user / multi client. Questions need to implement a lifetime too.
+                # TODO For the soundqueue, I will need a callback system to be sure to play the audio before recording
+                elif datajson['command'] == 'ASK':
+                    if datajson.has_key('nolistener') == False:
+                        Speaker.speak(datajson['body'])
+
+                    # Start record
+                    if datajson.has_key('nolistener') == False and self.listener:
+                        self.listener.record()
+
+        else:
+            # Send to TTS queue
+            if datajson.has_key('nolistener') == False:
+                Speaker.speak(datajson['body'])
+
+    def connectionMade(self):
+        """
+        Callback on established connections
+        """
+        log.msg('Connected to the server.')
+
+        # Set SSL encryption
+        if self.configuration.has_key('enable_secure_mode') and self.configuration['enable_secure_mode'] == True:
+            ctx = ClientTLSContext()
+            self.transport.startTLS(ctx, self.factory)
+
+        # Login to server
+        self.sendMessage(message='LOGIN', type='command')
+
+    def connectionLost(self, reason):
+        """
+        Callback on connection loss
+        """
+        # Stop listener
+        log.msg("Lost connection with server : " + reason.getErrorMessage())
+        if self.listener:
+            self.listener.stop()
+
+
+class LisaClientFactory(ReconnectingClientFactory):
+    # Create protocol
+    active_protocol = None
+
+    # Warn about failure on first connection to the server
+    first_time = True
+
+    def Init(self):
+        self.configuration = ConfigManagerSingleton.get().getConfiguration()
+
+
+    def startedConnecting(self, connector):
+        pass
+
+    def buildProtocol(self, addr):
+        # Reset retry delay
+        self.resetDelay()
+
+        # We don't need a "no connection" warning anymore
+        self.first_time = False
+
+        # Return protocol
+        self.active_protocol = LisaClient()
+        return self.active_protocol
+
+    def clientConnectionLost(self, connector, reason):
+        # Retry connection
+        log.err('Lost connection.  Reason:', reason.getErrorMessage())
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        # Warn on first failure
+        if self.first_time == True:
+            Speaker.speak("no_server")
+            self.first_time = False
+
+        # Retry
+        self.resetDelay()
+        log.err('Connection failed. Reason:', reason.getErrorMessage())
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
+
+
+class ClientTLSContext(ssl.ClientContextFactory):
+    isClient = 1
+    def getContext(self):
+        return SSL.Context(SSL.TLSv1_METHOD)
+
+
+class CtxFactory(ssl.ClientContextFactory):
+    def getContext(self):
+        self.method = SSL.SSLv23_METHOD
+        ctx = ssl.ClientContextFactory.getContext(self)
+        ctx.use_certificate_file(os.path.normpath(PWD + '/configuration/ssl/client.crt'))
+        ctx.use_privatekey_file(os.path.normpath(PWD + '/configuration/ssl/client.key'))
+        return ctx
+
+
+# Creating MultiService
+application = service.Application("LISA-Client")
+
+# Handle Ctrl-C
+def sigint_handler(signum, frame):
+    global LisaFactory
+
+    # Unregister handler, next Ctrl-C will kill app
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    # Stop factory
+    LisaFactory.stopTrying()
+
+    # Stop reactor
+    reactor.stop()
+
+    # Stop speaker
+    Speaker.stop()
+
+# Make twisted service
+def makeService(config):
+    global LisaFactory
+
+    # Get configuration
+    if config['configuration']:
+        ConfigManagerSingleton.get().setConfiguration(config['configuration'])
+    configuration = ConfigManagerSingleton.get().getConfiguration()
+
+    # Check vital configuration
+    if configuration.has_key('lisa_url') == False or configuration.has_key('lisa_engine_port_ssl') == False:
+        Speaker.start()
+        Speaker.speak("error_conf")
+        return
+
+    # Multiservice mode
+    multi = service.MultiService()
+    multi.setServiceParent(application)
+
+    # Ctrl-C handler
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    # Create factory
+    LisaFactory = LisaClientFactory()
+    LisaFactory.Init()
+
+    # Start client
+    if configuration.has_key('enable_secure_mode') and configuration['enable_secure_mode'] == True:
+        lisaclientService = internet.TCPClient(configuration['lisa_url'], configuration['lisa_engine_port_ssl'], LisaFactory, CtxFactory())
+    else:
+        lisaclientService = internet.TCPClient(configuration['lisa_url'], configuration['lisa_engine_port'], LisaFactory)
+    lisaclientService.setServiceParent(multi)
+
+    return multi
