eb8a7cf7dc9e3dd43fe8c06d2e102c2bdc27399d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:58 +0000

    Add package django-ptree/0.2.148/django-ptree-0.2.148.zip

---
+++ packages/django-ptree/django-ptree-0.2.148.zip/django-ptree-0.2.148/ptree/admin.py
@@ -0,0 +1,21 @@
+__doc__ = """See https://docs.djangoproject.com/en/dev/ref/contrib/admin/"""
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+
+import ptree.adminlib
+import ptree.sessionlib.models
+
+
+admin.site.register(ptree.sessionlib.models.GlobalData, ptree.adminlib.GlobalDataAdmin)
+
+admin.site.register(ptree.sessionlib.models.Session,
+                    ptree.adminlib.SessionAdmin
+                    )
+
+admin.site.register(ptree.sessionlib.models.SessionParticipant,
+                    ptree.adminlib.SessionParticipantAdmin)
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
