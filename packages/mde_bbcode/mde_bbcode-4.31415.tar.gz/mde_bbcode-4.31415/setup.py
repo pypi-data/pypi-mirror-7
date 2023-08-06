2dc979b38af7dacfee16ce88e48e4221bb54a7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:19 +0000

    Add package mde_bbcode/4.31415/mde_bbcode-4.31415.tar.gz

---
+++ packages/mde_bbcode/mde_bbcode-4.31415.tar.gz/mde_bbcode-4.31415/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="mde_bbcode",
+      version="4.31415",
+      description="Klappfallscheibes BBCode Parser v4 auf Python 3 portiert",
+      url="http://stuff.oorgle.de/bbcode/v4.html",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+      ],
+      py_modules = ["bbcode", "bbcode_test"],
+      )
