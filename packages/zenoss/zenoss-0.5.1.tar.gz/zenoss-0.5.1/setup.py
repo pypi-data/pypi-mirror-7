811c974b99294ccae9167cdda84da6adf73a49d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:41 +0000

    Add package zenoss/0.5.1/zenoss-0.5.1.tar.gz

---
+++ packages/zenoss/zenoss-0.5.1.tar.gz/zenoss-0.5.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='zenoss',
+    version='0.5.1',
+    description='Module to work with the Zenoss JSON API.',
+    author="Seth Miller",
+    author_email='seth@seth-miller.com',
+    url='https://github.com/iamseth/python-zenoss',
+    py_modules=['zenoss',],
+    keywords = ['zenoss', 'api', 'json', 'rest'],
+    test_suite='tests'
+)
