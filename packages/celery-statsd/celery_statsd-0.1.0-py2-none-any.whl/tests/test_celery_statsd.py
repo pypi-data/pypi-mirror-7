5e19b6dff22771c0d2b210512a10b1fa2afde62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:55 +0000

    Add package celery-statsd/0.1.0/celery_statsd-0.1.0-py2-none-any.whl

---
+++ packages/celery-statsd/celery_statsd-0.1.0-py2-none-any.whl/tests/test_celery_statsd.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+
+import mock
+
+import celery
+
+import pytest
+
+import celery_statsd
+
+
+celery.current_app.conf.CELERY_ALWAYS_EAGER = True
+
+
+@celery.task
+def _stub_task(arg):
+    return arg
+
+
+@pytest.fixture
+def mock_client(monkeypatch):
+    mock_client = mock.Mock()
+
+    def _get_mock_client(app):
+        return mock_client
+
+    monkeypatch.setattr(celery_statsd, "get_client", _get_mock_client)
+
+    return mock_client
+
+
+def test_run(mock_client):
+    assert _stub_task.delay(1)
+
+    mock_client.timing.assert_called_with(
+        "tests.test_celery_statsd._stub_task.run", mock.ANY)
+    mock_client.incr.assert_called_with(
+        "tests.test_celery_statsd._stub_task.success")
