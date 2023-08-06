e6a32f8c94b16a9a1c63260c52d524c8fb2a0cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package storytracker/0.0.7/storytracker-0.0.7.tar.gz

---
+++ packages/storytracker/storytracker-0.0.7.tar.gz/storytracker-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import sys
+from setuptools import setup
+
+install_requires = [
+    'python-dateutil==2.2',
+    'requests==2.3.0',
+    'htmlmin==0.1.5',
+    'six==1.7.2',
+    'pytz>=2014.4',
+    'beautifulsoup4==4.3.2',
+    'storysniffer==0.0.2',
+]
+
+if sys.version_info < (3,):
+    install_requires.append('unicodecsv==0.9.4')
+
+setup(
+    name='storytracker',
+    version='0.0.7',
+    description='Tools for tracking stories for news homepages',
+    author='Ben Welsh',
+    author_email='ben.welsh@gmail.com',
+    url='https://github.com/pastpages/storytracker',
+    license='MIT',
+    packages=('storytracker',),
+    scripts=(
+        'bin/storytracker-archive',
+        'bin/storytracker-get',
+        'bin/storytracker-links2csv',
+    ),
+    install_requires=install_requires,
+)
