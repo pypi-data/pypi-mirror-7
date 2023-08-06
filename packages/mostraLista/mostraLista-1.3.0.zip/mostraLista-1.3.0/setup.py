8338fa5d436d3a534801dea54dca1aae9188d885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package mostraLista/1.3.0/mostraLista-1.3.0.zip

---
+++ packages/mostraLista/mostraLista-1.3.0.zip/mostraLista-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name          =  'mostraLista',
+    version       =  '1.3.0',
+    py_modules    =  ['mostraLista'],
+    author        =  'Denis Monteiro',
+    author_email  =  'denis.monteiro6@gmail.com',
+    description   =  'Modulo para imprimir listas',
+)
