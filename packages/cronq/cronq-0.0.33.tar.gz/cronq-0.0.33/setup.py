12617a51c5aefe17a3c5c72feb6032e2f2ae8e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:39 +0000

    Add package cronq/0.0.33/cronq-0.0.33.tar.gz

---
+++ packages/cronq/cronq-0.0.33.tar.gz/cronq-0.0.33/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def run_setup():
+    setup(
+        name='cronq',
+        version='0.0.33',
+        description='A Cron-like system for running tasks',
+        keywords='cron amqp',
+        url='http://github.com/seatgeek/cronq',
+        author='SeatGeek',
+        author_email='opensource@seatgeek.com',
+        license='BSD',
+        packages=['cronq', 'cronq.backends', 'cronq.models'],
+        install_requires=[
+            'aniso8601==0.82',
+            'flask',
+            'haigha',
+            'sqlalchemy',
+            'mysql-connector-python',
+            'python-dateutil',
+            'haigha==0.7.0',
+        ],
+        test_suite='tests',
+        long_description=read('README.md'),
+        include_package_data=True,
+        zip_safe=True,
+        classifiers=[
+        ],
+        entry_points="""
+        [console_scripts]
+           cronq-runner=cronq.runner:main
+           cronq-injector=cronq.injector:main
+           cronq-results=cronq.result_aggregator:main
+        """,
+    )
+
+if __name__ == '__main__':
+    run_setup()
