ac779aa2733873fabfce830eb17b47514ff5f6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package elastictex/0.1.0/elastictex-0.1.0.tar.gz

---
+++ packages/elastictex/elastictex-0.1.0.tar.gz/elastictex-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(name="elastictex",
+      version="0.1.0",
+      description="Search with Latex",
+      author="Xander Masotto",
+      author_email="xmasotto@gmail.com",
+      url="http://github.com/xmasotto/elastictex",
+      packages=["elastictex",
+                "mongo_connector",
+                "mongo_connector.doc_managers"],
+      package_data={
+          'elastictex': ['templates/index.html']
+      },
+      install_requires=["elasticsearch", "flask", "BeautifulSoup"],
+      entry_points={
+          'console_scripts': [
+              'elastictex = elastictex.server:main'
+          ]
+      })
