59518a476e971f16e2cd8eb15484d5357616bdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:06 +0000

    Add package birdhousebuilder.recipe.pywps/0.1.7/birdhousebuilder.recipe.pywps-0.1.7.tar.gz

---
+++ packages/birdhousebuilder.recipe.pywps/birdhousebuilder.recipe.pywps-0.1.7.tar.gz/birdhousebuilder.recipe.pywps-0.1.7/birdhousebuilder/recipe/pywps/gunicorn.conf.py
@@ -0,0 +1,15 @@
+bind = 'unix://${prefix}/var/run/${sites}.socket'
+workers = 3
+
+# environment
+raw_env = ["HOME=${prefix}/var/lib/pywps", 
+           "PYWPS_CFG=${prefix}/etc/pywps/${sites}.cfg", 
+	   "PATH=${bin_dir}:${prefix}/bin:/usr/bin:/bin:/usr/local/bin", 
+           ]                                                                                                               
+
+# logging
+
+debug = True
+errorlog = '-'
+loglevel = 'debug'
+accesslog = '-'
