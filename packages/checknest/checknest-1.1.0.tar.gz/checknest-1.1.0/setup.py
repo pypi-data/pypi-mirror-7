a218520fa3b494284ab85f0df1c22a53770c5fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:07 +0000

    Add package checknest/1.1.0/checknest-1.1.0.tar.gz

---
+++ packages/checknest/checknest-1.1.0.tar.gz/checknest-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name		=  'checknest',
+	version 	=  '1.1.0',
+	py_modules	=  ['checknest'],
+	author		=  'sree',
+	author_email 	=  'sree@infomix.com',
+	url		=  'http://infomix.info',
+	description 	=  'Prints nested files',
+     )	 	
