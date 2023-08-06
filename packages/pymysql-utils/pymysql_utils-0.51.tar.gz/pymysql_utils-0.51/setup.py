bedf1ab936bf80b5f7711d6070ee8b88d926bc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:33 +0000

    Add package pymysql-utils/0.51/pymysql_utils-0.51.tar.gz

---
+++ packages/pymysql-utils/pymysql_utils-0.51.tar.gz/pymysql_utils-0.51/setup.py
@@ -0,0 +1,33 @@
+import multiprocessing
+from setuptools import setup, find_packages
+import os
+import glob
+
+datafiles = ['pymysql_utils/data/ipToCountrySoftware77DotNet.csv']
+
+setup(
+    name = "pymysql_utils",
+    version = "0.51",
+    packages = find_packages(),
+
+    # Dependencies on other packages:
+    # Couldn't get numpy install to work without
+    # an out-of-band: sudo apt-get install python-dev
+    setup_requires   = [],
+    install_requires = [#'pymysql3>=0.5',
+                        'MySQL-python>=1.2.5',
+			'configparser>=3.3.0'
+			],
+    tests_require    = ['sentinels>=0.0.6', 'nose>=1.0'],
+
+    # Unit tests; they are initiated via 'python setup.py test'
+    test_suite       = 'nose.collector', 
+
+    # metadata for upload to PyPI
+    author = "Andreas Paepcke",
+    author_email = "paepcke@cs.stanford.edu",
+    description = "Thin wrapper around pymysql. Provides Python iterator for queries. Abstracts away cursor.",
+    license = "BSD",
+    keywords = "MySQL",
+    url = "https://github.com/paepcke/pymysql_utils",   # project home page, if any
+)
