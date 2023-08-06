55d05a3c3480cd38f06c9c6cd41b8e9f8ae66459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:39 +0000

    Add package randomize/0.10/randomize-0.10.tar.gz

---
+++ packages/randomize/randomize-0.10.tar.gz/randomize-0.10/randomize/plugin.py
@@ -0,0 +1,119 @@
+"""
+This plugin randomizes the order of tests within a unittest.TestCase class
+
+The original source of the code is:
+
+http://code.google.com/p/python-nose/issues/detail?id=255
+
+and the original author is: Charles McCreary
+
+"""
+__test__ = False
+
+import logging
+from nose.plugins import Plugin
+from nose import loader
+from inspect import isfunction, ismethod
+from nose.case import FunctionTestCase, MethodTestCase
+from nose.failure import Failure
+from nose.util import isclass, isgenerator, transplant_func, transplant_class
+import random
+import unittest
+
+log = logging.getLogger(__name__)
+
+
+class Randomize(Plugin):
+    """
+    Randomize the order of the tests within a unittest.TestCase class
+    """
+    name = 'randomize'
+    # Generate a seed for deterministic behaviour
+    seed = random.getrandbits(32)
+
+    def options(self, parser, env):
+        """Register commandline options.
+        """
+        Plugin.options(self, parser, env)
+        parser.add_option('--randomize', action='store_true', dest='randomize',
+                          help="Randomize the order of the tests within a unittest.TestCase class")
+        parser.add_option('--seed', action='store', dest='seed', default=None, type=int,
+                          help="Initialize the seed for deterministic behavior in reproducing failed tests")
+
+    def configure(self, options, conf):
+        """
+        Configure plugin.
+        """
+        Plugin.configure(self, options, conf)
+        self.classes_to_look_at = []
+
+        if options.randomize:
+            self.enabled = True
+            if options.seed is not None:
+                self.seed = options.seed
+            random.seed(self.seed)
+            print("Using %d as seed" % (self.seed,))
+
+    def loadTestsFromNames(self, names, module=None):
+        pass
+
+    def wantClass(self, cls):
+        self.classes_to_look_at.append(cls)
+        #Change this to populate a list that makeTest can then process?
+
+    def makeTest(self, obj, parent=None):
+        """Given a test object and its parent, return a test case
+        or test suite.
+        """
+        ldr = loader.TestLoader()
+        if isinstance(obj, unittest.TestCase):
+            return obj
+        elif isclass(obj):
+            if parent and obj.__module__ != parent.__name__:
+                obj = transplant_class(obj, parent.__name__)
+            if issubclass(obj, unittest.TestCase):
+                # Randomize the order of the tests in the TestCase
+                return self.randomized_loadTestsFromTestCase(obj)
+            else:
+                return self.randomized_loadTestsFromTestClass(obj)
+        elif ismethod(obj):
+            if parent is None:
+                parent = obj.__class__
+            if issubclass(parent, unittest.TestCase):
+                return parent(obj.__name__)
+            else:
+                if isgenerator(obj):
+                    return ldr.loadTestsFromGeneratorMethod(obj, parent)
+                else:
+                    return MethodTestCase(obj)
+        elif isfunction(obj):
+            if parent and obj.__module__ != parent.__name__:
+                obj = transplant_func(obj, parent.__name__)
+            if isgenerator(obj):
+                return ldr.loadTestsFromGenerator(obj, parent)
+            else:
+                return [FunctionTestCase(obj)]
+        else:
+            return Failure(TypeError,
+                           "Can't make a test from %s" % obj)
+
+    def randomized_loadTestsFromTestClass(self, cls):
+        tests = loader.TestLoader().loadTestsFromTestClass(cls)
+        return self._shuffler(tests)
+
+    def randomized_loadTestsFromContextSuite(self, suite):
+        tests = loader.TestLoader().loadTestsFromTestModule(suite)
+        return self._shuffler(tests)
+
+    def randomized_loadTestsFromTestCase(self, testCaseClass):
+        tests = loader.TestLoader().loadTestsFromTestCase(testCaseClass)
+        return self._shuffler(tests)
+
+    def _shuffler(self, tests):
+        """Shuffles the given tests"""
+        randomized_tests = []
+        for t in tests._tests:
+            randomized_tests.append(t)
+        random.shuffle(randomized_tests)
+        tests._tests = (t for t in randomized_tests)
+        return tests
---
+++ packages/randomize/randomize-0.10.tar.gz/randomize-0.10/setup.py
@@ -0,0 +1,46 @@
+"""
+Randomize test order plugin
+
+The original source of the code is:
+
+http://code.google.com/p/python-nose/issues/detail?id=255
+
+and the original author is: Charles McCreary
+
+"""
+try:
+    import ez_setup
+    ez_setup.use_setuptools()
+except ImportError:
+    pass
+
+from setuptools import setup
+
+setup(
+    name='randomize',
+    version='0.10',
+    maintainer='Nick Loadholtes',
+    maintainer_email='nick@ironboundsoftware.com',
+    url='https://github.com/nloadholtes/nose-randomize',
+    download_url='https://github.com/nloadholtes/nose-randomize/archive/master.zip',
+    author='Charles McCreary',
+    author_email='charles.mccreary@tiawichiresearch.com',
+    description='Randomize the order of the tests within a unittest.TestCase class',
+    license='GNU LGPL',
+    py_modules=['randomize/plugin', 'randomize/__init__'],
+    entry_points={
+        'nose.plugins.0.10': [
+            'randomize = randomize:Randomize'
+            ],
+        },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities']
+
+    )
