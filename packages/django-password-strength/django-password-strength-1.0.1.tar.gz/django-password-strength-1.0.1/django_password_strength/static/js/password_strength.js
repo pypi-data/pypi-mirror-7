e2e99ee334da7cd16fa6265bff8651e8d8f3ae4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:10 +0000

    Add package django-password-strength/1.0.1/django-password-strength-1.0.1.tar.gz

---
+++ packages/django-password-strength/django-password-strength-1.0.1.tar.gz/django-password-strength-1.0.1/django_password_strength/static/js/password_strength.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.password_strength').keyup(function() {
+		if( $(this).val() ) {
+			result = zxcvbn( $(this).val() );
+
+			if( result.score < 3 ) {
+				$('#password_strength_bar').removeClass('progress-bar-success').addClass('progress-bar-warning');
+				$('#password_strength_info').html('<span class="label label-danger">Warning</span> This password would take <em>'+result.crack_time_display+'</em> to crack.');
+			} else {
+				$('#password_strength_bar').removeClass('progress-bar-warning').addClass('progress-bar-success');
+				$('#password_strength_info').html('This password would take <em>'+result.crack_time_display+'</em> to crack.');
+			}
+
+			$('#password_strength_bar').width( ((result.score+1)/5)*100 + '%' );
+			$('#password_strength_bar').attr('aria-valuenow', result.score);
+
+		} else {
+			$('#password_strength_bar').removeClass('progress-bar-success').addClass('progress-bar-warning');
+			$('#password_strength_bar').width( '0%' );
+			$('#password_strength_bar').attr('aria-valuenow', 0);
+			$('#password_strength_info').html('');
+		}
+	});
+});
---
+++ packages/django-password-strength/django-password-strength-1.0.1.tar.gz/django-password-strength-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-password-strength",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=['django>=1.5'],
+    author="A.J. May",
+    author_email="aj7may@gmail.com",
+    description="This package contains an extention of the Django password widget including a password strength meter and crack time powered by zxcvbn.",
+    license="MIT License",
+    keywords="password meter zxcvbn strength security django",
+    url="http://thegoods.aj7may.com/django-password-strength",
+    zip_safe=False,
+    package_data={
+        'django_password_strength': ['static/js/*'],
+    },
+)
