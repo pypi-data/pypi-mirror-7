dd59e95bb3c2907d7c8c5160d15183e8fd8feef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:22 +0000

    Add package infi.run_as_root/0.1.4/infi.run_as_root-0.1.4.tar.gz

---
+++ packages/infi.run_as_root/infi.run_as_root-0.1.4.tar.gz/infi.run_as_root-0.1.4/setup.py
@@ -0,0 +1,49 @@
+
+SETUP_INFO = dict(
+    name = 'infi.run_as_root',
+    version = '0.1.4',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'https://github.com/Infinidat/infi.run_as_root',
+    license = 'PSF',
+    description = """library for ensuring the user has root privileges""",
+    long_description = """library for ensuring the process is running under root (on Unix systems) or Administrator (on Windows)""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.pyutils',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
---
+++ packages/infi.run_as_root/infi.run_as_root-0.1.4.tar.gz/infi.run_as_root-0.1.4/src/infi/run_as_root/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '60684fd5af94d0b903fb4d3c3ef8410fa13ea283'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
