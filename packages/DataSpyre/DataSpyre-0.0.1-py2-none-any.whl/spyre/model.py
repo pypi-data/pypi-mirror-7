c97f7abe4901db769ff7da806364bc0b5b71f1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:55 +0000

    Add package DataSpyre/0.0.1/DataSpyre-0.0.1-py2-none-any.whl

---
+++ packages/DataSpyre/DataSpyre-0.0.1-py2-none-any.whl/spyre/model.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import matplotlib
+# import ggplot
+import StringIO
+
+class Plot:
+	def getPlotPath(self, plt_obj):
+		buffer = StringIO.StringIO()
+		if type(plt_obj).__name__ == 'Figure':
+			plt_obj.savefig(buffer,format='png')
+		# if type(plt_obj).__name__ == 'ggplot':
+		# 	ggplot.ggsave(plt_obj,filename=buffer,format='png')
+		return(buffer)
+
