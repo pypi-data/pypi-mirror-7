728855e78679436dc430103bc1fa2e0d3da88537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:07 +0000

    Add package monarch/0.0.3/monarch-0.0.3.tar.gz

---
+++ packages/monarch/monarch-0.0.3.tar.gz/monarch-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='monarch',
+    version='0.0.3',
+    description='The un-migration migration tool',
+    url='http://github.com/jtushman/monarch',
+    author='Jonathan Tushman',
+    author_email='jonathan@zefr.com',
+    packages=['monarch'],
+    install_requires=[
+        'Click',
+        'jinja2',
+    ],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    entry_points='''
+        [console_scripts]
+        monarch=monarch:cli
+    ''',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
