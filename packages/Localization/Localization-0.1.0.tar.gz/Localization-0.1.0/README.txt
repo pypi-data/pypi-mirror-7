f6199c19ec2ccfbfde2acc8efcadf0cc0f8d889f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:47 +0000

    Add package Localization/0.1.0/Localization-0.1.0.tar.gz

---
+++ packages/Localization/Localization-0.1.0.tar.gz/Localization-0.1.0/README.txt
@@ -0,0 +1,25 @@
+===========
+Localization
+===========
+
+Localization package provides tool for muultilateration and triangulation
+in '2D' and '3D'. Also with this package you will be able to locate targets
+on sphere and Earth using longitude/latitude info. Typical usage of the package is:
+
+    #!/usr/bin/env python
+
+    import localization
+
+(Note the double-colon and 4-space indent formatting above.)
+
+An Example
+=========
+
+Basic
+-------------
+
+To be completed:
+
+1. README
+
+2. DOC
---
+++ packages/Localization/Localization-0.1.0.tar.gz/Localization-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Localization',
+    version='0.1.0',
+    author='Kamal Shadi',
+    author_email='kamal.shadi85@gmail.com',
+    packages=['localization', 'localization.test'],
+    scripts=['bin/sample.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Miltilateration and triangulation.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "SciPy >= 0.14.0",
+    ],
+)
