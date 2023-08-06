c941541fbf34312e57dbcfe7c71d6c3fe74f7e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package django-periodically/0.3.0/django-periodically-0.3.0.tar.gz

---
+++ packages/django-periodically/django-periodically-0.3.0.tar.gz/django-periodically-0.3.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.rst')
+
+setup(
+    name = "django-periodically",
+    version = "0.3.0",
+    description='Periodic task management for your Django projects.',
+    url = 'https://github.com/hzdg/django-periodically',
+    long_description=README,
+
+    author = 'Matthew Tretter',
+    author_email = 'matthew@exanimo.com',
+    packages = find_packages()
+)
