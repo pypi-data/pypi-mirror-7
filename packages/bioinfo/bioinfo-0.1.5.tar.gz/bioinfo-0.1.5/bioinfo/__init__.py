807fa4755b6e3e126cf9b83ee5ac517de5559aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:15 +0000

    Add package bioinfo/0.1.5/bioinfo-0.1.5.tar.gz

---
+++ packages/bioinfo/bioinfo-0.1.5.tar.gz/bioinfo-0.1.5/bioinfo/__init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''bioinfo
+
+Collection of scripts for bioinformatics problems.
+
+Usage:
+  bioinfo bam_coverage <reference> <alignments> <minmatch> <query> [--mapq=<n>] [--minlen=<n>]
+  bioinfo -h | --help
+  bioinfo --version
+
+Options:
+  --mapq=<n>    Minimum MAPQ quality [default: 30].
+  --minlen=<n>  Minimum alignment length, as a percentage of read length. Max 1.0 [default: 0].
+  -h --help     Show this screen.
+  --version     Show version.
+'''
+
+from __future__ import unicode_literals, print_function, division
+from docopt import docopt
+
+from bioinfo import bam_coverage as bam_coverage_mod
+
+from bioinfo.bam_coverage import bam_coverage
+
+
+__version__ = "0.1.5"
+__author__ = "Luiz Irber"
+__license__ = "BSD License"
+
+
+AVAILABLE_COMMANDS = {
+    'bam_coverage': bam_coverage,
+}
+
+
+def main():
+    '''Main entry point for the bioinfo CLI.'''
+    args = docopt(__doc__, version=__version__)
+
+    if 'bam_coverage' in args:
+        bam_coverage(args['<reference>'],
+                     args['<alignments>'],
+                     int(args['<minmatch>']),
+                     args['<query>'],
+                     min_mapq=int(args['--mapq']),
+                     min_len=float(args['--minlen']))
+
+if __name__ == '__main__':
+    main()
---
+++ packages/bioinfo/bioinfo-0.1.5.tar.gz/bioinfo-0.1.5/tests/test_bioinfo.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import shutil
+import unittest
+
+from nose.tools import *  # PEP8 asserts
+from scripttest import TestFileEnvironment
+
+
+class TestBamConverage(unittest.TestCase):
+
+    '''Unit tests for bam_coverage.'''
+
+    def setUp(self):
+        self.env = TestFileEnvironment()
+        shutil.copytree(os.path.join(os.path.dirname(__file__), 'data'),
+                        os.path.join(self.env.base_path, 'data'))
+        self.total = 237
+        self.coved = 57
+        self.fraction = 0.24
+
+    def tearDown(self):
+        self.env.clear()
+
+    def test_arguments(self):
+        ret = self.env.run("bioinfo", "-h")
+        self.assertEquals(ret.returncode, 0)
+        self.assertIn('bam_coverage', ret.stdout)
+
+    def test_complete_run_cli(self):
+        ret = self.env.run("bioinfo", "bam_coverage",
+                           "data/bam_ref.fasta",
+                           "data/bam_align.sorted.bam",
+                           "10",
+                           "data/bam_query.fasta",
+                           "--mapq=30", expect_stderr=True)
+        self.assertEquals(ret.returncode, 0)
+
+        self.assertIn('total bases in reference', ret.stdout)
+        self.assertIn('total ref bases covered', ret.stdout)
+        self.assertIn('fraction', ret.stdout)
+        self.assertNotIn('elapsed', ret.stdout)
+
+        self.assertNotIn('total bases in reference', ret.stderr)
+        self.assertNotIn('total ref bases covered', ret.stderr)
+        self.assertNotIn('fraction', ret.stderr)
+        self.assertIn('elapsed', ret.stderr)
+
+        for line in ret.stdout.split('\n'):
+            if 'total bases in reference' in line:
+                self.assertIn(str(self.total), line)
+            if 'total ref bases covered' in line:
+                self.assertIn(str(self.coved), ret.stdout)
+            if 'fraction' in line:
+                self.assertIn(str(self.fraction), ret.stdout)
+
+    def test_complete_run_module(self):
+        from bioinfo import bam_coverage
+
+        result = bam_coverage(
+            os.path.join('tests', 'data', 'bam_ref.fasta'),
+            os.path.join('tests', 'data', 'bam_align.sorted.bam'),
+            10,
+            os.path.join('tests', 'data', 'bam_query.fasta'),
+            30
+        )
+        self.assertEqual(result['total'], self.total)
+        self.assertEqual(result['covered'], self.coved)
+        self.assertAlmostEqual(result['fraction'], self.fraction, places=2)
+
+    def test_check_dependencies(self):
+        import bioinfo
+        from bioinfo import bam_coverage
+
+        bioinfo.bam_coverage_mod.DEPENDENCIES['pysam'] = False
+
+        with self.assertRaises(SystemExit):
+            bam_coverage(
+                os.path.join('tests', 'data', 'bam_ref.fasta'),
+                os.path.join('tests', 'data', 'bam_align.sorted.bam'),
+                10,
+                os.path.join('tests', 'data', 'bam_query.fasta'),
+                30
+            )
+
+        bioinfo.bam_coverage_mod.DEPENDENCIES['pysam'] = True
+
+
+if __name__ == '__main__':
+    unittest.main()
