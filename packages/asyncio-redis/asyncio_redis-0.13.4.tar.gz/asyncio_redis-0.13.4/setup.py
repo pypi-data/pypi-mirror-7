dc6e9bec02b95b9e49a700c4effbcb31b0aec65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:28 +0000

    Add package asyncio-redis/0.13.4/asyncio_redis-0.13.4.tar.gz

---
+++ packages/asyncio-redis/asyncio_redis-0.13.4.tar.gz/asyncio_redis-0.13.4/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import sys
+
+if sys.version_info >= (3, 4):
+    install_requires = []
+else:
+    install_requires = ['asyncio']
+
+setup(
+        name='asyncio_redis',
+        author='Jonathan Slenders',
+        version='0.13.4',
+        license='LICENSE.txt',
+        url='https://github.com/jonathanslenders/asyncio-redis',
+
+        description='PEP 3156 implementation of the redis protocol.',
+        long_description=open("README.rst").read(),
+        packages=['asyncio_redis'],
+        install_requires=install_requires,
+        extra_require = {
+            'hiredis': ['hiredis'],
+        }
+)
