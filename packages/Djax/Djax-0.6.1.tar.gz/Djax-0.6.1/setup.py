fa9e3cd3eb496bc6ae14bd0dbb0120ce85e89bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:11 +0000

    Add package Djax/0.6.1/Djax-0.6.1.tar.gz

---
+++ packages/Djax/Djax-0.6.1.tar.gz/Djax-0.6.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup 
+except ImportError, err:
+    from distutils.core import setup
+    
+from djax import VERSION
+
+setup(
+    name='Djax',
+    version='.'.join(map(str,VERSION)),
+    description='Integrates Django projects with Axilent.',
+    packages=['djax'],
+    license='BSD',
+    author='Loren Davie',
+    author_email='code@axilent.com',
+    url='http://github.com/Axilent/Djax',
+    install_requires=['sharrock-client','Django','python-dateutil'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
