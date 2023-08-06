42bfc718e1a12fe6f09eb2f3e36fb9ae10cbb2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:36 +0000

    Add package fruitfly/0.1.1/fruitfly-0.1.1.tar.gz

---
+++ packages/fruitfly/fruitfly-0.1.1.tar.gz/fruitfly-0.1.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name = 'fruitfly'
+   ,  version = '0.1.1'
+   ,  description = 'Modular event-driven framework.'
+   ,  long_description = 'Framework for minimalist, modular, event-driven embedded applications. Intended for use on small Linux systems like the Raspberry Pi.'
+   ,  author = 'Derp Ston'
+   ,  author_email = 'derpston+pypi@sleepygeek.org'
+   ,  url = 'https://github.com/derpston/fruitfly'
+   ,  install_requires = ['PyYAML']
+   ,  packages = []
+   ,  package_dir = {'': 'fruitfly'}
+   )
+
