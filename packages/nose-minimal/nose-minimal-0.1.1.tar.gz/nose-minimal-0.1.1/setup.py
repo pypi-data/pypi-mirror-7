3b6ac8779d6ca176d57365504d47663778bf9d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:42 +0000

    Add package nose-minimal/0.1.1/nose-minimal-0.1.1.tar.gz

---
+++ packages/nose-minimal/nose-minimal-0.1.1.tar.gz/nose-minimal-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name = 'nose-minimal',
+
+    version='0.1.1',
+    url='https://github.com/gardaud/nose-minimal',
+    author='Guillaume Ardaud',
+    author_email = 'gardaud@acm.org',
+    description = 'A minimal output plugin for the Nose testing framework.',
+    license = 'MIT',
+
+    entry_points = {
+        'nose.plugins': [
+            'nose-minimal = noseminimal:NoseMinimal',
+            ]
+    },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+    requires=['nose']
+)
