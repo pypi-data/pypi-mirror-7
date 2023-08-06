10807d829745bbc1bd92be2c6996295c40a379fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:28 +0000

    Add package bogglesolver/0.0.1/bogglesolver-0.0.1.zip

---
+++ packages/bogglesolver/bogglesolver-0.0.1.zip/bogglesolver-0.0.1/README.md
@@ -0,0 +1,28 @@
+BoggleSolver
+============
+
+Create a dictionary and then eventually solve for all words on a boggle board
+
+# Dependencies
+You have python installed.
+
+# Current Install Process
+Boggle is now command line functional. To install, run 'python setup.py install'. 
+
+Once you do this, my app at: https://github.com/theovoss/Kivy_boggle will run with `python boggle.py`
+
+# Game Play
+To play a game, run 'bogglesolver -p'. This will auto-generate a board, display it, and then wait for user input. When you press 'enter', it will display all the valid words for that board. 
+
+Currently there is no time limit, though that will be added fairly soon as an optional addition.
+
+The dictionary used is the twl06 dictionary which is the official tournament and club word list. I added one word ("theo") to the dictionary for fun. :)
+
+[![Build Status](https://travis-ci.org/theovoss/BoggleSolver.svg?branch=master)](https://travis-ci.org/theovoss/BoggleSolver)
+[![Coverage Status](https://coveralls.io/repos/theovoss/BoggleSolver/badge.png?branch=master)](https://coveralls.io/r/theovoss/BoggleSolver?branch=master)
+
+
+Feel free to poke around the code, open issues, and submit pull requests.
+
+If you want to talk, you can get in touch with me through my gmail: theo.voss973
+
---
+++ packages/bogglesolver/bogglesolver-0.0.1.zip/bogglesolver-0.0.1/bogglesolver/solve_boggle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+"""Class to solve the boggle boggle_board."""
+
+
+from bogglesolver.boggle_board import Boggle
+from bogglesolver.load_english_dictionary import Edict
+
+
+class SolveBoggle:
+
+    """
+    Class to solve a boggle board.
+
+    This initializes the dictionary and board.
+    Then it searches the board for all valid words.
+    """
+
+    def __init__(self, use_test_words=False):
+        self.use_test_words = use_test_words
+        self.edict = Edict()
+        self.edict.read_dictionary(self.use_test_words)
+        self.boggle = Boggle()
+        self.min_word_len = 3
+
+    def set_board(self, columns, rows, boggle_list=None):
+        """
+        Set the board for the game.
+
+        :param int columns: number of columns for the board.
+        :param int rows: number of rows for the board.
+        :param boggle_list: list of all the letters for the board (optional).
+        :type boggle_list: list or None
+        """
+        self.boggle.num_columns = columns
+        self.boggle.num_rows = rows
+        if boggle_list is not None:
+            self.boggle.set_array(boggle_list)
+        else:
+            self.boggle.generate_boggle_board()
+
+    def solve(self, ignore_indexes=None, normal_adj=True):
+        """
+        Solve the boggle board, or get all words for scrabble.
+
+        :param bool normal_adj: True to solve for boggle.
+                                False to solve for scrabble.
+        :returns: sorted list of all words found.
+        """
+        if ignore_indexes is None:
+            ignore_indexes = []
+        assert self.boggle.is_full(), "Boggle board has not been set."
+        words = set()
+        for i, letter in enumerate(self.boggle.boggle_array):
+            node = self.edict.get_last_node(self.edict.dictionary_root, letter)
+            if i not in ignore_indexes and node is not None:
+                self.recurse_search_for_words(i, node, ignore_indexes + [i], normal_adj, words)
+        return sorted(words)
+
+    def recurse_search_for_words(self, a_index, node,
+                                 indexes_searched, normal_adj, words=set()):
+        """
+        Recursively search boggle board for words.
+
+        :param int a_index: current board index.
+        :param indexes_searched: indexes searched already.
+        :type indexes_searched: None or list.
+        :param bool normal_adj: whether to solve for boggle or scrabble.
+        """
+        if node.word and (len(node.word) >= self.min_word_len):
+            words.add(node.word)
+        if not node.letters.keys():
+            return
+        for index in self.boggle.get_adjacent(a_index, indexes_searched, normal_adj):
+            new_node = self.edict.get_last_node(node, self.boggle.boggle_array[index])
+            if new_node is not None:
+                self.recurse_search_for_words(index, new_node, indexes_searched + [index],
+                                              normal_adj, words)
