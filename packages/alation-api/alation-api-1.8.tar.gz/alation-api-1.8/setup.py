3de97e3162c3f58a1433a42387846dd922855d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package alation-api/1.8/alation-api-1.8.tar.gz

---
+++ packages/alation-api/alation-api-1.8.tar.gz/alation-api-1.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import setup
+
+
+setup(name='alation-api',
+      version='1.8',
+      packages=['alation_api'],
+      entry_points={
+      	'console_scripts': [
+          'alation-setup = alation_api.main:setup',
+          'alation-query = alation_api.main:get_sql',
+          'alation-result = alation_api.main:get_result',
+      	]
+      }
+    )
