84d23b8ec3ff38fb1b1beeb89dc509115275d191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:54 +0000

    Add package nose-customdescription/0.1.1/nose-customdescription-0.1.1.tar.gz

---
+++ packages/nose-customdescription/nose-customdescription-0.1.1.tar.gz/nose-customdescription-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+README = open("README.md").read()
+
+setup(
+    name='nose-customdescription',
+    author='Tamer Sherif',
+    author_email='me@tamer.sh',
+    version='0.1.1',
+    url="https://github.com/tamers/nosetests-description",
+    keywords='nose test custom description python',
+    description=('A Nose plugin to override test description output when using the'
+                 ' verbose flag (-v)'),
+    entry_points={
+        'nose.plugins.0.10': [
+            'customdescription = custom_test_descriptor:CustomDescription',
+        ]
+    },
+    py_modules=['custom_description'],
+)
