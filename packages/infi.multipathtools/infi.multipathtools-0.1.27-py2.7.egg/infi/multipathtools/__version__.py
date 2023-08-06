748ec8b823e784c1eb788ccb1d5f0c1f8708a35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:13 +0000

    Add package infi.multipathtools/0.1.27/infi.multipathtools-0.1.27-py2.7.egg

---
+++ packages/infi.multipathtools/infi.multipathtools-0.1.27-py2.7.egg/infi/multipathtools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.27"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.27)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '678cbf1bcd7f313e8efdc536588981bd7a0fffec'
+__git_head_subject__ = 'HIP-1210'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
