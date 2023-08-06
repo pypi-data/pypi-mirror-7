003be57966804e59642de9455c25be79d12fea84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:55 +0000

    Add package gphoto2/0.5.1/gphoto2-0.5.1.tar.gz

---
+++ packages/gphoto2/gphoto2-0.5.1.tar.gz/gphoto2-0.5.1/examples/list-cameras.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# python-gphoto2 - Python interface to libgphoto2
+# http://github.com/jim-easterbrook/python-gphoto2
+# Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import logging
+import sys
+
+import gphoto2 as gp
+
+def main():
+    logging.basicConfig(
+        format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+    gp.check_result(gp.use_python_logging())
+    cameras = gp.check_result(gp.gp_list_new())
+    context = gp.gp_context_new()
+    if hasattr(gp, 'gp_camera_autodetect'):
+        # gphoto2 version 2.5+
+        cam_count = gp.check_result(gp.gp_camera_autodetect(cameras, context))
+        assert cam_count == gp.gp_list_count(cameras)
+    else:
+        port_info_list = gp.check_result(gp.gp_port_info_list_new())
+        gp.check_result(gp.gp_port_info_list_load(port_info_list))
+        abilities_list = gp.check_result(gp.gp_abilities_list_new())
+        gp.check_result(gp.gp_abilities_list_load(abilities_list, context))
+        gp.check_result(gp.gp_abilities_list_detect(
+            abilities_list, port_info_list, cameras, context))
+    for n in range(gp.gp_list_count(cameras)):
+        print('camera number', n)
+        print('===============')
+        print(gp.check_result(gp.gp_list_get_name(cameras, n)))
+        print(gp.check_result(gp.gp_list_get_value(cameras, n)))
+        print
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/gphoto2/gphoto2-0.5.1.tar.gz/gphoto2-0.5.1/src/gphoto2/lib/gphoto2_list.i
@@ -0,0 +1,47 @@
+// python-gphoto2 - Python interface to libgphoto2
+// http://github.com/jim-easterbrook/python-gphoto2
+// Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%module(package="gphoto2.lib") gphoto2_list
+
+%{
+#include "gphoto2/gphoto2.h"
+%}
+
+%feature("autodoc", "2");
+
+%include "typemaps.i"
+
+%include "macros.i"
+
+// gp_list_new() returns a pointer in an output parameter
+%typemap(in, numinputs=0) CameraList ** (CameraList *temp) {
+  $1 = &temp;
+}
+%typemap(argout) CameraList ** {
+  RESULT_APPEND(
+    SWIG_NewPointerObj(*$1, SWIGTYPE_p__CameraList, SWIG_POINTER_NEW))
+}
+
+// Mark gp_list_unref as destructor and add default destructor
+%delobject gp_list_unref;
+struct _CameraList {};
+%ignore _CameraList;
+
+// gp_list_get_name() & gp_list_get_value() return pointers in output params
+STRING_ARGOUT()
+
+%include "gphoto2/gphoto2-list.h"
