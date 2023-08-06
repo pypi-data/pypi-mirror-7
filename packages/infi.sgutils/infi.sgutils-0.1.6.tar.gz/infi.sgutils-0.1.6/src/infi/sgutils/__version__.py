abf3c0122947b862f227c7dc77a952f810053a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.sgutils/0.1.6/infi.sgutils-0.1.6.tar.gz

---
+++ packages/infi.sgutils/infi.sgutils-0.1.6.tar.gz/infi.sgutils-0.1.6/src/infi/sgutils/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0671d68a312dfd0a61ab9028715efba3045bbd16'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
