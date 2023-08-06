fcd03dd49dad8a81a2f2f3be6025ff812085dbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:09 +0000

    Add package korail2/0.0.6/korail2-0.0.6.tar.gz

---
+++ packages/korail2/korail2-0.0.6.tar.gz/korail2-0.0.6/korail2/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    korail2
+    ~~~~~~~
+
+    Korail (www.letskorail.com) wrapper for Python.
+
+    :copyright: (c) 2014 by Taehoon Kim.
+    :license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import
+from .korail2 import Korail, AdultPassenger, Passenger, ChildPassenger, SeniorPassenger, TrainType, ReserveOption
+from .korail2 import KorailError, NeedToLoginError, SoldOutError, NoResultsError
+
+__version__ = '0.0.6'
+__all__ = ['Korail', 'AdultPassenger', 'Passenger', 'ChildPassenger', 'SeniorPassenger', 'TrainType', 'ReserveOption',
+           'KorailError', 'NeedToLoginError', 'SoldOutError', 'NoResultsError']
