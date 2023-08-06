21d636952fdf7f770d387bc1d8eb80a9d13a9ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package DjangoSCA/1.3b/DjangoSCA-1.3b.tar.gz

---
+++ packages/DjangoSCA/DjangoSCA-1.3b.tar.gz/DjangoSCA-1.3b/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from os.path import join, dirname
+
+setup(name='DjangoSCA',
+   version='1.3b',
+   description='Django Static Source Code Analyzer',
+   author='Joff Thyer',
+   author_email='jsthyer@gmail.com',
+   license='GPLv3',
+   url='https://bitbucket.org/jsthyer/djangosca',
+   scripts = ['djangoSCA.py'],
+   packages = ['djangoSCAclasses'],
+   data_files = [('/usr/local/etc',['djangoSCA.rules'])]
+)
+
