9d1a5e8fd5a4558e0ddc9004e6c6407813c85f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:21 +0000

    Add package elgato-ixmldsig/0.5/elgato-ixmldsig-0.5.zip

---
+++ packages/elgato-ixmldsig/elgato-ixmldsig-0.5.zip/elgato-ixmldsig-0.5/setup.py
@@ -0,0 +1,9 @@
+__author__ = 'dsedad'
+
+# Upload with python setup.py sdist upload
+from distutils.core import setup
+setup(name='elgato-ixmldsig',
+      version='0.5',
+      packages=['ixmldsig'],
+      )
+
