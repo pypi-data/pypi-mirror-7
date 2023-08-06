aea4cb27dc87196bc30f28df5534824bedf66afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:49 +0000

    Add package HfCh7Levi/1.0.1/HfCh7Levi-1.0.1.tar.gz

---
+++ packages/HfCh7Levi/HfCh7Levi-1.0.1.tar.gz/HfCh7Levi-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	
+    	name='HfCh7Levi',
+    	version='1.0.1',
+    	py_modules=['HfCh7Levi'],
+    	author='levibinlee',
+    	author_email='levibinlee@gmail.com',
+    	url='http://www.headfirstlabs',
+	description='the first Python webapp' ,    
+)
+    
