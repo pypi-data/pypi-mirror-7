9e1b0f5ac7e7ea9ab71b8814b9985b9c64394ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:22 +0000

    Add package baelfire/0.2/baelfire-0.2.tar.gz

---
+++ packages/baelfire/baelfire-0.2.tar.gz/baelfire-0.2/baelfire/template.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from jinja2 import Environment, PackageLoader
+
+from baelfire.task import Task
+from baelfire.dependencies import FileChanged
+
+
+class TemplateTask(Task):
+
+    templates_dir = 'templates'
+
+    def __init__(self, *args, **kwargs):
+        self.check_template = kwargs.pop('check_template', True)
+        super().__init__(*args, **kwargs)
+        self._jinja = None
+
+    def module(self):
+        """Module which store templates."""
+        return sys.modules[self.recipe.__module__]
+
+    def template_absolute_path(self):
+        """Absoluth path to a template."""
+        dirname = os.path.dirname(self.module().__file__)
+        return os.path.join(dirname,
+                            self.templates_dir,
+                            self.get_template_path())
+
+    def generate_dependencies(self):
+        """Generates FileChanged dependency for template file and task file.
+        generate_dependencies method is not needed now."""
+        if self.check_template is True:
+            self.add_dependecy(FileChanged(self.template_absolute_path()))
+            task_file = sys.modules[self.__module__].__file__
+            self.add_dependecy(FileChanged(task_file))
+
+    def jinja(self):
+        """Jinja2 environment generator."""
+        if self._jinja is None:
+            loader = PackageLoader(self.module().__name__,
+                                   self.templates_dir)
+            self._jinja = Environment(loader=loader)
+        return self._jinja
+
+    def generate_data(self):
+        """Generates data which will be used in the template."""
+        data = {}
+        data['settings'] = self.settings
+        data['paths'] = self.paths
+        return data
+
+    def make(self):
+        template = self.jinja().get_template(self.get_template_path())
+        stream = template.stream(**self.generate_data())
+        stream.dump(self.get_output_file())
---
+++ packages/baelfire/baelfire-0.2.tar.gz/baelfire-0.2/baelfire/tests/recipe.py
@@ -0,0 +1,151 @@
+from soktest import TestCase
+
+from baelfire.error import TaskNotFoundError
+from baelfire.recipe import Recipe
+from baelfire.task import Task
+from baelfire import VERSION
+
+
+class ExampleTask(Task):
+
+    def generate_dependencies(self):
+        pass
+
+
+class ExampleRecipe(Recipe):
+
+    def create_settings(self):
+        self.chronology = ['create_settings']
+
+    def gather_recipes(self):
+        self.chronology.append('gather_recipes')
+
+    def gather_tasks(self):
+        self.chronology.append('gather_tasks')
+
+
+class RecipeTest(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.recipe = ExampleRecipe()
+
+    def test_init(self):
+        """Should init recipe and run methods in proper order."""
+        self.assertEqual(['create_settings',
+                          'gather_recipes',
+                          'gather_tasks',
+                          ],
+                         self.recipe.chronology)
+        self.assertEqual(VERSION, self.recipe.settings['minimal version'])
+        self.assertEqual([], self.recipe.recipes)
+        self.assertEqual(None, self.recipe.parent)
+
+    def test_validate_dependencies(self):
+        """Should do nothing (test for code coverage)."""
+        self.recipe.add_task(Task)
+        self.assertEqual(None, self.recipe.validate_dependencies())
+
+    def test_add_recipe(self):
+        """Should set recipe it's parent to self, and add this recipe to
+        self.recipes."""
+        recipe = Recipe()
+        self.recipe.add_recipe(recipe)
+
+        self.assertEqual(self.recipe, recipe.parent)
+        self.assertEqual([recipe], self.recipe.recipes)
+
+    def test_assign_parent(self):
+        """Should assign parent to a recipe and update tasks, settings and
+        paths."""
+        recipe = Recipe()
+        recipe.settings['something'] = 'parent settings'
+        recipe.paths['something'] = 'parent paths'
+        recipe._tasks = {'something 1': 1, 'something': 'parent'}
+        self.recipe.settings['something'] = 'child settings'
+        self.recipe.paths['something'] = 'child paths'
+        self.recipe._tasks = {'something 2': 2, 'something': 'child'}
+
+        self.recipe.assign_parent(recipe)
+
+        self.assertEqual(recipe, self.recipe.parent)
+        self.assertEqual('child settings', self.recipe.settings['something'])
+        self.assertEqual('child paths', self.recipe.paths['something'])
+        self.assertEqual('child settings', recipe.settings['something'])
+        self.assertEqual('child paths', recipe.paths['something'])
+        self.assertEqual({
+            'something 1': 1,
+            'something 2': 2,
+            'something': 'child',
+        }, recipe.tasks)
+        self.assertEqual(id(recipe.settings), id(self.recipe.settings))
+        self.assertEqual(id(recipe.paths), id(self.recipe.paths))
+        self.assertEqual(id(recipe.tasks), id(self.recipe.tasks))
+        self.assertEqual(id(recipe.tasks_dotted), id(self.recipe.tasks_dotted))
+        self.assertEqual(id(recipe.data_log), id(self.recipe.data_log))
+
+    def test_add_task(self):
+        """Should instance Task class, set tasks it's parent to self, and add
+        this task to self.tasks."""
+        self.recipe.add_task(ExampleTask)
+        task = self.recipe.task_from_url('/exampletask')
+
+        self.assertEqual(self.recipe, task.recipe)
+        self.assertEqual({'/exampletask': task}, self.recipe.tasks)
+        self.assertEqual(
+            {'baelfire.tests.recipe:ExampleTask': task},
+            self.recipe.tasks_dotted)
+
+    def test_task_from_url(self):
+        """Should return task with assigned kwargs from url."""
+        self.recipe.add_task(ExampleTask)
+
+        task = self.recipe.task_from_url(
+            '/exampletask?arg=something&arg=somethin2&second_arg=metoo')
+
+        self.assertTrue(isinstance(task, ExampleTask))
+        self.assertEqual({
+            'arg': ['something', 'somethin2'],
+            'second_arg': ['metoo'],
+        }, task.kwargs)
+
+    def test_task_bad_name(self):
+        """Should raise TaskNotFoundError when task is not found."""
+        self.assertRaises(
+            TaskNotFoundError,
+            self.recipe.task,
+            '/exampletask?arg=something&arg=somethin2&second_arg=metoo',
+        )
+
+    def test_kwargs(self):
+        """Should transform all kwargs arguments into lists."""
+        self.recipe.add_task(ExampleTask)
+        task = self.recipe.task_from_url('/exampletask')
+
+        task = self.recipe.task(
+            'baelfire.tests.recipe:ExampleTask', one=(1,), two=[2, ], three=3)
+
+        self.assertEqual((1,), task.kwargs['one'])
+        self.assertEqual([2, ], task.kwargs['two'])
+        self.assertEqual([3, ], task.kwargs['three'])
+
+    def test_set_task_options_fail(self):
+        """set_task_options should raise RuntimeError when invalid option is
+        specyfied."""
+        self.recipe.add_task(ExampleTask)
+
+        self.assertRaises(
+            RuntimeError,
+            self.recipe.set_task_options,
+            'baelfire.tests.recipe:ExampleTask',
+            {'elo': 'somethin2'})
+
+    def test_set_task_options(self):
+        """set_task_options should set an option to a task"""
+        self.recipe.add_task(ExampleTask)
+
+        self.recipe.set_task_options(
+            'baelfire.tests.recipe:ExampleTask', {'hide': 'somethin2'})
+        task = self.recipe.task_from_url('/exampletask')
+
+        self.assertEqual('somethin2', task.hide)
