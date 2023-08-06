967b29c12e12e6575ddf37d2add227777e651098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:16 +0000

    Add package sylcount/1.0.1/sylcount-1.0.1.tar.gz

---
+++ packages/sylcount/sylcount-1.0.1.tar.gz/sylcount-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(name='sylcount',
+	  version='1.0.1',
+	  description="Count syllables in a word, real or made-up.",
+	  author="Bede Kelly",
+	  author_email="bedekelly97@gmail.com",
+	  url="https://github.com/bedekelly/sylcount",
+	  provides=['sylcount'],
+	  scripts=['sylcount'],
+	  classifiers=[
+	  	"Natural Language :: English",
+	  	"Operating System :: OS Independent",
+	  	"Programming Language :: Python :: 3",
+	  	"Programming Language :: Python :: Implementation :: CPython",
+	  	"Topic :: Utilities"
+	  	])
