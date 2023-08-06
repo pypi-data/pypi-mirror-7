f313c529ddbd6d648ea994f5db64b70a5fb3fcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:14 +0000

    Add package kf5py/0.1.dev1/kf5py-0.1.dev1.tar.gz

---
+++ packages/kf5py/kf5py-0.1.dev1.tar.gz/kf5py-0.1.dev1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='kf5py',
+    version='0.1.dev1',
+    author='Chris Teplovs',
+    author_email='dr.chris@problemshift.com',
+    url='https://github.com/problemshift/kf5py',
+    license='LICENSE.txt',
+    description='Python-based utilities for KF5.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests >= 2.3.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
