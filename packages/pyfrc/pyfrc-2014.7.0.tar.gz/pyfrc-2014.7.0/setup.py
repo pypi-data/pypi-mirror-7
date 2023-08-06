86cd28eb07767fae640e0a0f734c7c7eb7c67d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:18 +0000

    Add package pyfrc/2014.7.0/pyfrc-2014.7.0.tar.gz

---
+++ packages/pyfrc/pyfrc-2014.7.0.tar.gz/pyfrc-2014.7.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from os.path import join, dirname
+from distutils.core import setup
+
+setup_dir = dirname(__file__)
+
+packages = [
+    'pyfrc',
+    'pyfrc.cli',
+    'pyfrc.robotpy',
+    'pyfrc.sim',
+    'pyfrc.physics',
+    'pyfrc.tests',
+    'pyfrc.tests.iterative',
+    'pyfrc.tests.simple',
+    'pyfrc.wpilib',
+    'pyfrc.wpilib._wpilib',
+]
+
+def get_version():
+    g = {}
+    with open(join(dirname(__file__), 'lib', 'pyfrc', 'version.py'), 'r') as fp:
+        exec(fp.read(), g)
+    return g['__version__']
+
+install_requires=open(join(setup_dir, 'requirements.txt')).readlines()
+
+setup(name='pyfrc',
+      version=get_version(),
+      description='Development tools library for python interpreter used for the FIRST Robotics Competition',
+      long_description=open(join(dirname(__file__), 'README.md'), 'r').read(),
+      author='Dustin Spicuzza',
+      author_email='dustin@virtualroadside.com',
+      url='https://github.com/robotpy/pyfrc',
+      license='Apache 2.0',
+      packages=packages,
+      package_dir={'': 'lib'},
+      install_requires=install_requires,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development'
+      ]
+)
+
