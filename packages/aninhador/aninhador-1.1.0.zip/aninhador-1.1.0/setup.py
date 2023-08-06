afc7dcefa27800e2e8324a543f128b53afeac606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:13 +0000

    Add package aninhador/1.1.0/aninhador-1.1.0.zip

---
+++ packages/aninhador/aninhador-1.1.0.zip/aninhador-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'aninhador',
+        version      = '1.1.0',
+        py_modules   = ['aninhador'],
+        author       = 'leandrohumberto',
+        author_email = 'lehumberto@msn.com',
+        url          = 'fb.com/leandrohumberto',
+        description  = 'Um simples exibidor de listas aninhadas'
+     )
