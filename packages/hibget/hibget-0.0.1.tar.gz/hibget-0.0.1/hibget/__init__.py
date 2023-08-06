8f6d0c2799587e20f231bb1de96ce405627f362e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package hibget/0.0.1/hibget-0.0.1.tar.gz

---
+++ packages/hibget/hibget-0.0.1.tar.gz/hibget-0.0.1/hibget/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Joel Pedraza"
+__copyright__ = "Copyright 2014, Joel Pedraza"
+__license__ = "MIT"
+
+__all__ = ['requests_logging', 'utils', 'widgets']
+
+from hibget import requests_logging, utils, widgets
