0a1aa6d47b11fc8e66629489efb0ec90f55ca334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:54 +0000

    Add package deimos/0.3.2/deimos-0.3.2.tar.gz

---
+++ packages/deimos/deimos-0.3.2.tar.gz/deimos-0.3.2/deimos/__init__.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+import calendar
+import os
+import signal
+import subprocess
+import sys
+import time
+
+import deimos.cleanup
+import deimos.config
+import deimos.containerizer
+import deimos.containerizer.docker
+from deimos.err import Err
+import deimos.flock
+from deimos.logger import log
+import deimos.sig
+import deimos.usage
+
+
+def cli(argv=None):
+    deimos.sig.install(lambda _: None)
+    if argv is None:
+        argv = sys.argv
+    sub = argv[1] if len(argv) > 1 else None
+
+    if sub in ["-h", "--help", "help"]:
+        print format_help()
+        return 0
+
+    conf = deimos.config.load_configuration()
+
+    if sub == "config":
+        log.info("Final configuration:")
+        for _, conf in conf.items():
+            print "%r" % conf
+        return 0
+
+    if sub == "locks":
+        deimos.flock.lock_browser(os.path.join(conf.state.root, "mesos"))
+        return 0
+
+    if sub == "state":
+        cleanup = deimos.cleanup.Cleanup(conf.state.root)
+        t, rm = time.time(), False
+        for arg in argv[2:]:
+            if arg == "--rm":
+                rm = True
+                continue
+            t = calendar.timegm(time.strptime(arg, "%Y-%m-%dT%H:%M:%SZ"))
+        if rm:
+            return cleanup.remove(t)
+        else:
+            for d in cleanup.dirs(t):
+                sys.stdout.write(d + "\n")
+            return 0
+
+    if sub not in deimos.containerizer.methods():
+        print >>sys.stderr, format_help()
+        print >>sys.stderr, "** Please specify a subcommand **".center(79)
+        log.error("Bad ARGV: %r" % argv[1:])
+        return 1
+
+    deimos.docker.options = conf.docker.argv()
+    containerizer = deimos.containerizer.docker.Docker(
+        container_settings=conf.containers,
+        optimistic_unpack=conf.uris.unpack,
+        state_root=conf.state.root
+    )
+
+    deimos.usage.report()
+    try:
+        result = deimos.containerizer.stdio(containerizer, *argv[1:])
+        deimos.usage.report()
+        if result is not None:
+            if isinstance(result, bool):
+                return 0 if result else 1
+            if isinstance(result, int):
+                return result
+            if isinstance(result, str):
+                sys.stdout.write(result)
+            else:
+                for item in result:
+                    sys.stdout.write(str(item) + "\n")
+    except Err as e:
+        log.error("%s.%s: %s", type(e).__module__, type(e).__name__, str(e))
+        return 4
+    except subprocess.CalledProcessError as e:
+        log.error(str(e))
+        return 4
+    except Exception:
+        log.exception("Unhandled failure in %s", sub)
+        return 8
+    return 0
+
+
+def format_help():
+    return """
+ USAGE: deimos launch  (--no-fork)?
+        deimos usage
+        deimos destroy
+        deimos wait    (--docker <docker-id>)?
+        deimos locks
+        deimos state
+
+  Deimos provides Mesos integration for Docker, allowing Docker to be used as
+  an external containerizer.
+
+ deimos launch (--no-fork)?
+
+  Launches a container and runs the executor or command specified in the
+  TaskInfo, passed in on standard in.
+
+  The launch subcommand always watches the launched container and logs changes
+  in its lifecycle. By default, it forks off a child to do the watching, as
+  part of the contract external containerizers have with Mesos. With
+  --no-fork, launch will watch the container and log in the foreground. This
+  can be helpful during debugging.
+
+ deimos usage
+
+  Generates a protobuf description of the resources used by the container.
+
+ deimos destroy
+
+  Shuts down the specified container.
+
+ deimos wait (--docker <docker-id>)?
+
+  Without --docker, reads STDIN to find the container to watch.
+
+  With --docker, waits for a particular Docker CID to exit and records the
+  result in the state directory, returning an appropriate exit code.
+
+ deimos locks
+
+  List file locks taken by Deimos, associating each file with a PID, an inode,
+  and a lock level. The same file may appear multiple times.
+
+ deimos state (--rm)?
+
+  List stale state directories (those with an exit file). With --rm, removes
+  stale states.
+
+ deimos config
+
+  Load and display the configuration.
+
+""".strip("\n")
+
+if __name__ == "__main__":
+    sys.exit(cli(sys.argv))
---
+++ packages/deimos/deimos-0.3.2.tar.gz/deimos-0.3.2/deimos/config.py
@@ -0,0 +1,201 @@
+from ConfigParser import SafeConfigParser, NoSectionError
+import json
+import logging
+import os
+import sys
+
+import deimos.argv
+import deimos.docker
+from deimos.logger import log
+import deimos.logger
+from deimos._struct import _Struct
+
+
+def load_configuration(f=None, interactive=sys.stdout.isatty()):
+    error = None
+    defaults = _Struct(docker=Docker(),
+                       index=DockerIndex(),
+                       containers=Containers(),
+                       uris=URIs(),
+                       state=State(),
+                       log=Log(
+                       console=(logging.DEBUG if interactive else None),
+                       syslog=(logging.INFO if not interactive else None)
+                       ))
+    parsed = None
+    try:
+        f = f if f else path()
+        if f:
+            parsed = parse(f)
+    except Exception as e:
+        error = e
+    finally:
+        confs = defaults.merge(parsed) if parsed else defaults
+        deimos.logger.initialize(**dict(confs.log.items()))
+        if error:
+            pre = ("Error loading %s: " % f) if f else ""
+            log.exception(pre + str(error))
+            sys.exit(16)
+        if parsed:
+            log.info("Loaded configuration from %s" % f)
+            for _, conf in parsed.items():
+                log.debug("Found: %r", conf)
+    return confs
+
+
+def coercearray(array):
+    if type(array) in deimos.argv.strings:
+        if array[0:1] != "[":
+            return [array]
+        try:
+            arr = json.loads(array)
+            if type(arr) is not list:
+                raise ValueError()
+            return arr
+        except:
+            raise ValueError("Not an array: %s" % array)
+    return list(array)
+
+
+def coerceloglevel(level):
+    if not level:
+        return
+    if type(level) is int:
+        return level
+    levels = {"DEBUG": logging.DEBUG,
+              "INFO": logging.INFO,
+              "WARNING": logging.WARNING,
+              "ERROR": logging.ERROR,
+              "CRITICAL": logging.CRITICAL,
+              "NOTSET": logging.NOTSET}
+    try:
+        return levels[level]
+    except:
+        raise ValueError("Not a log level: %s" % level)
+
+
+def coercebool(b):
+    if type(b) is bool:
+        return b
+    try:
+        bl = json.loads(b)
+        if type(bl) is not bool:
+            raise ValueError()
+        return bl
+    except:
+        raise ValueError("Not a bool: %s" % b)
+
+
+def coerceoption(val):
+    try:
+        return coercearray(val)
+    except:
+        return coercebool(val)
+
+
+class Image(_Struct):
+
+    def __init__(self, default=None, ignore=False):
+        _Struct.__init__(self, default=default, ignore=coercebool(ignore))
+
+    def override(self, image=None):
+        return image if (image and not self.ignore) else self.default
+
+
+class Options(_Struct):
+
+    def __init__(self, default=[], append=[], ignore=False):
+        _Struct.__init__(self, default=coercearray(default),
+                               append=coercearray(append),
+                               ignore=coercebool(ignore))
+
+    def override(self, options=[]):
+        a = options if (len(options) > 0 and not self.ignore) else self.default
+        return a + self.append
+
+
+class Containers(_Struct):
+
+    def __init__(self, image=Image(), options=Options()):
+        _Struct.__init__(self, image=image, options=options)
+
+    def override(self, image=None, options=[]):
+        return self.image.override(image), self.options.override(options)
+
+
+class URIs(_Struct):
+
+    def __init__(self, unpack=True):
+        _Struct.__init__(self, unpack=coercebool(unpack))
+
+
+class Log(_Struct):
+
+    def __init__(self, console=None, syslog=None):
+        _Struct.__init__(self, console=coerceloglevel(console),
+                               syslog=coerceloglevel(syslog))
+
+
+class Docker(_Struct):
+
+    def __init__(self, **properties):
+        for k in properties.keys():
+            properties[k] = coerceoption(properties[k])
+        _Struct.__init__(self, **properties)
+
+    def argv(self):
+        return deimos.argv.argv(**dict(self.items()))
+
+
+class DockerIndex(_Struct):
+
+    def __init__(self, index=None, account_libmesos="libmesos", account=None):
+        _Struct.__init__(self, index=index,
+                               account_libmesos=account_libmesos,
+                               account=account)
+
+
+class State(_Struct):
+
+    def __init__(self, root="/tmp/deimos"):
+        if ":" in root:
+            raise ValueError("Deimos root storage path must not contain ':'")
+        _Struct.__init__(self, root=root)
+
+
+def parse(f):
+    config = SafeConfigParser()
+    config.read(f)
+    parsed = {}
+    sections = [("log", Log), ("state", State), ("uris", URIs),
+                ("docker", Docker),
+                ("docker.index", DockerIndex),
+                ("containers.image", Image),
+                ("containers.options", Options)]
+    for key, cls in sections:
+        try:
+            parsed[key] = cls(**dict(config.items(key)))
+        except:
+            continue
+    containers = {}
+    if "containers.image" in parsed:
+        containers["image"] = parsed["containers.image"]
+        del parsed["containers.image"]
+    if "containers.options" in parsed:
+        containers["options"] = parsed["containers.options"]
+        del parsed["containers.options"]
+    if len(containers) > 0:
+        parsed["containers"] = Containers(**containers)
+    return _Struct(**parsed)
+
+
+def path():
+    for p in search_path:
+        if os.path.exists(p):
+            return p
+
+search_path = ["./deimos.cfg",
+               os.path.expanduser("~/.deimos"),
+               "/etc/deimos.cfg",
+               "/usr/etc/deimos.cfg",
+               "/usr/local/etc/deimos.cfg"]
