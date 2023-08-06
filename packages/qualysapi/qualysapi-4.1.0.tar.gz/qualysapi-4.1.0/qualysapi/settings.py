f7738fbe7a309d5d0ff5f17b3734c87b2f35cb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:48 +0000

    Add package qualysapi/4.1.0/qualysapi-4.1.0.tar.gz

---
+++ packages/qualysapi/qualysapi-4.1.0.tar.gz/qualysapi-4.1.0/qualysapi/settings.py
@@ -0,0 +1,18 @@
+''' Module to hold global settings reused throughout qualysapi. '''
+
+__author__ = "Colin Bell <colin.bell@uwaterloo.ca>"
+__copyright__ = "Copyright 2011-2013, University of Waterloo"
+__license__ = "BSD-new"
+
+global defaults
+global default_filename
+
+import os
+
+if os.name == 'nt':
+    default_filename = "config.ini"
+else:
+    default_filename = ".qcrc"
+
+defaults = {'hostname': 'qualysapi.qualys.com',
+            'max_retries': '3'}
