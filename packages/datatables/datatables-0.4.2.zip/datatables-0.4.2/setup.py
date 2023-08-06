a3987e8ae39f47999fbfd758d7d90c54842cf5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:29 +0000

    Add package datatables/0.4.2/datatables-0.4.2.zip

---
+++ packages/datatables/datatables-0.4.2.zip/datatables-0.4.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+setup(
+    name='datatables',
+    version='0.4.2',
+    packages=['datatables'],
+    url='https://github.com/orf/datatables/',
+    license='MIT',
+    long_description=open("readme.rst").read(),
+    keywords='sqlalchemy datatables jquery pyramid flask',
+    author='Tom',
+    author_email='tom@tomforb.es',
+    description='Integrates SQLAlchemy with DataTables (framework agnostic)',
+    requires=['sqlalchemy'],
+    zip_safe=False,
+    include_package_data=True,
+    data_files = [("", ["LICENSE"])],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Framework :: Flask',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
