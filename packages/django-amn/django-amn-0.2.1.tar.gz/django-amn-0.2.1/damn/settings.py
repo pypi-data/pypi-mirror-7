d174b41597198dc15ae7fe6acc7d280bf6db80dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:01 +0000

    Add package django-amn/0.2.1/django-amn-0.2.1.tar.gz

---
+++ packages/django-amn/django-amn-0.2.1.tar.gz/django-amn-0.2.1/damn/settings.py
@@ -0,0 +1,16 @@
+
+from django.conf import settings
+
+# Map of mode -> processor config
+#   {
+#       'js': {
+#           'processor': 'damn.processors.ScriptProcessor',
+#           'aliases': {},
+#       },
+#   }
+PROCESSORS = getattr(settings, 'DAMN_PROCESSORS', {})
+
+# File extension -> mode name
+MODE_MAP = getattr(settings, 'DAMN_MODE_MAP', {})
+
+MODE_ORDER = getattr(settings, 'DAMN_MODE_ORDER', ['css', 'js', ])
