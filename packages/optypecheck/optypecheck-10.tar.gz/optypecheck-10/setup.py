fd424e6fe4bdeca5a2e1387a67b8536c7f9e76fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package optypecheck/10/optypecheck-10.tar.gz

---
+++ packages/optypecheck/optypecheck-10.tar.gz/optypecheck-10/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 27/06/2014
+@author: Carlo Pires <carlopires@gmail.com>
+"""
+from setuptools import setup
+
+def get_version():
+    from typecheck import __version__
+    return __version__
+
+def get_long_description():
+    with open('README.rst') as readme:
+        return readme.read()
+
+setup(
+    name='optypecheck',
+    version=get_version(),
+    url='https://bitbucket.org/carlopires/optypecheck',
+    author='Carlo Pires',
+    author_email='carlopires@gmail.com',
+    description='A non intrusive optional type checking for Python 3 using annotations',
+    long_description=get_long_description(),
+    zip_safe=True,
+    packages=['typecheck'],
+    platforms='any',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Testing',        
+        'Topic :: Software Development :: Libraries',        
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    test_suite='test.suite',
+)
---
+++ packages/optypecheck/optypecheck-10.tar.gz/optypecheck-10/typecheck/types.py
@@ -0,0 +1,51 @@
+"""
+Created on 30/06/2014
+@author: Carlo Pires <carlopires@gmail.com>
+"""
+NotImplementedType = type(NotImplemented)
+NoneCls = type(None)
+BooleanCls = bool
+StrCls = str
+BytesCls = bytes
+IntegralCls = 'numbers.Integral' # int
+RealCls = 'numbers.Real' # float
+DecimalCls = 'decimal.Decimal'
+TupleCls = ().__class__
+ListCls = [].__class__
+
+HashableCls = 'collections.Hashable'
+IterableCls = 'collections.Iterable'
+IteratorCls = 'collections.Iterator'
+SizedCls = 'collections.Sized'
+ContainerCls = 'collections.Container'
+CallableCls = 'collections.Callable'
+SetCls = 'collections.Set'
+MutableSetCls = 'collections.MutableSet'
+MappingCls = 'collections.Mapping'
+MutableMappingCls = 'collections.MutableMapping'
+MappingViewCls = 'collections.MappingView'
+KeysViewCls = 'collections.KeysView'
+ItemsViewCls = 'collections.ItemsView'
+ValuesViewCls = 'collections.ValuesView'
+SequenceCls = 'collections.Sequence'
+MutableSequenceCls = 'collections.MutableSequence'
+ByteStringCls = 'collections.ByteString'
+
+CodeCls = 'types.CodeType'
+MethodCls = 'types.MethodType'
+GeneratorCls = 'types.GeneratorType'
+FunctionCls = 'types.FunctionType'
+BuiltinFunctionCls = 'types.BuiltinFunctionType'
+MappingProxyCls = 'types.MappingProxyType'
+BuiltinMethodCls = 'types.BuiltinMethodType'
+ModuleCls = 'types.ModuleType'
+FrameCls = 'types.FrameType'
+GetSetDescriptorCls = 'types.GetSetDescriptorType'
+LambdaCls = 'types.LambdaType'
+TracebackCls = 'types.TracebackType'
+MemberDescriptorCls = 'types.MemberDescriptorType'
+
+UUIDCls = 'uuid.UUID'
+TimeCls = 'datetime.time'
+DateCls = 'datetime.date'
+DateTimeCls = 'datetime.datetime'
