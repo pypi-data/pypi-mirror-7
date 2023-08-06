afa6ecba2721f9642bc7f5da31f3a2803444986c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:05 +0000

    Add package pyowfs/0.1.8/pyowfs-0.1.8.tar.gz

---
+++ packages/pyowfs/pyowfs-0.1.8.tar.gz/pyowfs-0.1.8/pyowfs/libcapi.py
@@ -0,0 +1,116 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2009-2014 Marcus Priesch, All rights reserved
+# In Prandnern 31, A--2122 Riedenthal, Austria. office@priesch.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    libcapi
+#
+# Purpose
+#    Wrapper for OWFS' libcapi
+#
+# Revision Dates
+#    22-Dec-2009 (MPH) Creation
+#    18-Mar-2010 (MPH) Changed to use ctypes.cdll.LoadLibrary instead of
+#                      ctypes.CDLL to honor LD_LIBRARY_PATH
+#    31-Jul-2013 (MPR) Added caching to 1wire communication, to improve
+#                      searching on large trees
+#    10-Dec-2013 (MPR) Fixed CACHE for multiple instances
+#    ««revision-date»»···
+#--
+import ctypes
+import sys
+import time
+
+CACHE_MAX_AGE = 30 # seconds
+
+class AlreadyInitialisedError (StandardError) :
+    pass
+# end class AlreadyInitialisedError
+
+class CAPI (object) :
+    def __init__ (self) :
+        if sys.platform == "linux2" :
+            self.libcapi = ctypes.cdll.LoadLibrary ("libowcapi.so")
+            self.libc    = ctypes.CDLL ("libc.so.6")
+        else :
+            raise NotImplementedError ("No support for %r" % sys.platform)
+
+        self.ow = None
+    # end def __init__
+
+    def init (self, params) :
+        self.init_params = params
+        if self.ow is not None :
+            raise AlreadyInitialisedError
+
+        self.ow = self.libcapi.OW_init (params)
+        self.CACHE = dict ()
+    # end def init
+
+    def finish (self) :
+        self.libcapi.OW_finish ()
+        self.ow = None
+    # end def finish
+
+    def reinit (self) :
+        if self.ow :
+            self.finish ()
+        self.init (self.init_params)
+    # end def reinit
+
+    def get (self, path, cached = True) :
+        if cached and path in self.CACHE :
+            res, ts = self.CACHE [path]
+            if (time.time () - ts) > CACHE_MAX_AGE :
+                self.CACHE.pop (path)
+            else :
+                return res
+
+        buf_p   = ctypes.POINTER (ctypes.c_char) ()
+        buf_len = ctypes.c_long ()
+        res = self.libcapi.OW_get \
+            (path, ctypes.byref (buf_p), ctypes.byref (buf_len))
+        if res >= 0 :
+            res = "".join ([i for i in buf_p [:buf_len.value]])
+            self.libc.free (buf_p)
+        else :
+            res = None
+
+        if cached :
+            self.CACHE [path] = (res, time.time ())
+
+        return res
+    # end def get
+
+    def put (self, path, what) :
+        if path in self.CACHE :
+            self.CACHE.pop (path)
+
+        res = self.libcapi.OW_put (path, what, len (what))
+        if res >= 0 :
+            return True
+        else :
+            return False
+    # end def put
+# end class CAPI
+
+### __END__ libcapi
+
+
---
+++ packages/pyowfs/pyowfs-0.1.8.tar.gz/pyowfs-0.1.8/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2009-2013 Marcus Priesch, All rights reserved
+# In Prandnern 31, A--2122 Riedenthal, Austria. office@priesch.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    setup
+#
+# Purpose
+#    distutils setup
+#
+# Revision Dates
+#    22-Dec-2009 (MPH) Creation
+#    18-Mar-2010 (MPH) Version bump to 0.1.1
+#    ««revision-date»»···
+#--
+
+from distutils.core import setup
+
+setup \
+    ( name             = 'pyowfs'
+    , version          = '0.1.8'
+    , description      = "OWFS' libowcapi wrapper using ctypes"
+    , long_description = "This package provides one module with a thin "
+      "wrapper around libowcapi and a slightly smarter wrapper around it "
+      "for easy access to the 1wire devices. It is inspired by the "
+      "OWFS provided python bindings but, however is neither api compatible "
+      "nor will it try to be in the future - at least i hope so ;)"
+    , author           = 'Marcus Priesch'
+    , author_email     = 'marcus@priesch.co.at'
+    , url              = 'http://priesch.co.at/pyowfs'
+    , packages         = ['pyowfs']
+    , requires         = ["ctypes(>1.0.2)"]
+    , license          = "LGPL"
+    , platforms        = ["POSIX"]
+    , classifiers      = \
+          [ 'Development Status :: 4 - Beta'
+          , 'Environment :: Console'
+          , 'Intended Audience :: Developers'
+          , 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)'
+          , 'Operating System :: Microsoft :: Windows'
+          , 'Operating System :: POSIX'
+          , 'Programming Language :: Python'
+          , 'Topic :: Software Development'
+          ]
+    )
+
+### __END__ setup
+
+
