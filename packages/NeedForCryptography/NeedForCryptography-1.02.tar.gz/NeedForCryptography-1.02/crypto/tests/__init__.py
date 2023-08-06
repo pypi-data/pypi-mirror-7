64a425163f1fb34d381e5389d6b706cc4a515c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:47 +0000

    Add package NeedForCryptography/1.02/NeedForCryptography-1.02.tar.gz

---
+++ packages/NeedForCryptography/NeedForCryptography-1.02.tar.gz/NeedForCryptography-1.02/crypto/tests/__init__.py
@@ -0,0 +1,8 @@
+'''
+Tests for all  Ciphers and Tools.
+'''
+
+
+__all__ = ['playfair', 'caeser', 'feistel', 'hill', 'rsa', 'des_des3',
+           'shamir_sharing_scheme', 'el_gamal_signature',
+           'diffie_hellman_exchange']
---
+++ packages/NeedForCryptography/NeedForCryptography-1.02.tar.gz/NeedForCryptography-1.02/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup_info = {'name': 'NeedForCryptography',
+              'version': '1.02',
+              'description': 'Python cryptographic tools and ciphers',
+              'author': 'Veselin Genadiev',
+              'author_email': 'genadiev.veselin@gmail.com',
+              'url': 'https://github.com/Veselin-Genadiev/NeedForCryptography',
+              'packages': find_packages(),
+              'classifiers': [
+                  'Development Status :: 4 - Beta',
+                  'Environment :: Console',
+                  'Environment :: Web Environment',
+                  'Intended Audience :: End Users/Desktop',
+                  'Intended Audience :: Developers',
+                  'Intended Audience :: System Administrators',
+                  'License :: OSI Approved ::'
+                  ' Python Software Foundation License',
+                  'Operating System :: MacOS :: MacOS X',
+                  'Operating System :: POSIX',
+                  'Programming Language :: Python',
+                  'Topic :: Communications :: Email',
+                  'Topic :: Office/Business',
+                  'Topic :: Software Development :: Bug Tracking',
+              ],
+              }
+
+setup(**setup_info)
