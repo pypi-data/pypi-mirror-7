ba2c8d62b827a904a6402719498af4b58933bebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:57 +0000

    Add package rosbag_pandas/0.3/rosbag_pandas-0.3.tar.gz

---
+++ packages/rosbag_pandas/rosbag_pandas-0.3.tar.gz/rosbag_pandas-0.3/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='rosbag_pandas',
+        version='0.3',
+        py_modules=['rosbag_pandas'],
+        scripts=['scripts/bag_graph.py', 'scripts/bag2csv.py'],
+     )
