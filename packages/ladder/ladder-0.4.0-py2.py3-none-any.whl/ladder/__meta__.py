783e6c5137972bbe6d390cc2946153faea9f4fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:00 +0000

    Add package ladder/0.4.0/ladder-0.4.0-py2.py3-none-any.whl

---
+++ packages/ladder/ladder-0.4.0-py2.py3-none-any.whl/ladder/__meta__.py
@@ -0,0 +1,25 @@
+"""Define project metadata
+"""
+
+__all__ = [
+    '__title__',
+    '__summary__',
+    '__url__',
+    '__version__',
+    '__author__',
+    '__email__',
+    '__license__']
+
+__title__ = 'ladder'
+__summary__ = ('General purpose path generation library with support for '
+              'URLs, OS paths, delimited paths, and RESTful HTTP client '
+              'integration.')
+
+__url__ = 'https://github.com/dgilland/ladder'
+
+__version__ = '0.4.0'
+
+__author__ = 'Derrick Gilland'
+__email__ = 'dgilland@gmail.com'
+
+__license__ = 'MIT License'
