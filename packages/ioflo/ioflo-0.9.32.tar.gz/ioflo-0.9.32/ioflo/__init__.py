3dfff279f6a151fc433e718e155c95c716b213b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package ioflo/0.9.32/ioflo-0.9.32.tar.gz

---
+++ packages/ioflo/ioflo-0.9.32.tar.gz/ioflo-0.9.32/ioflo/__init__.py
@@ -0,0 +1,23 @@
+""" ioflo package
+
+"""
+#print("\nPackage at {0}".format( __path__[0]))
+
+from __future__ import division
+
+import importlib
+
+__version__ = "0.9.32"
+__author__ = "Samuel M. Smith"
+__license__ =  "MIT"
+
+
+#from .base.consoling import getConsole
+#console = getConsole()
+#console.profuse("{0} version {1}\n".format(__path__[0], __version__))
+
+__all__ = ['base', 'trim']
+
+for m in __all__:
+    importlib.import_module(".{0}".format(m), package='ioflo')
+
