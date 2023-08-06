ff92effb0b41ae23e7ed983df82e6d22398767bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:53 +0000

    Add package django-nyt/0.9.2/django-nyt-0.9.2.tar.gz

---
+++ packages/django-nyt/django-nyt-0.9.2.tar.gz/django-nyt-0.9.2/django_nyt/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+_disable_notifications = False
+
+VERSION = "0.9.2"
+
+
+def notify(*args, **kwargs):
+    """
+    DEPRECATED - please access django_nyt.utils.notify
+    """
+    from django_nyt.utils import notify
+    return notify(*args, **kwargs)
