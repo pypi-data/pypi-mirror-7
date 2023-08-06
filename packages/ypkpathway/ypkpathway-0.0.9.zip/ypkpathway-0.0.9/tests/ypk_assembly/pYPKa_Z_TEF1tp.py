b1262e7c9bf30fe311aad2636c2523f33875fdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:07 +0000

    Add package ypkpathway/0.0.9/ypkpathway-0.0.9.zip

---
+++ packages/ypkpathway/ypkpathway-0.0.9.zip/ypkpathway-0.0.9/tests/ypk_assembly/pYPKa_Z_TEF1tp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+execfile("header.py")
+
+def design():
+    #---------------------------------------------------------------------------
+    # This script should define a Dseqrecord named seq
+    
+    from pydna import read
+    from pydna import Dseqrecord
+    from pydna import pcr
+
+    from pYPKa import pYPKa
+
+    from Bio.Restriction import ZraI
+    
+    enz = ZraI
+    
+    ins = read("TEF1.txt")
+    
+    fp = read('''
+>pfw579
+ttaaatACAATGCATACTTTGTAC
+''', ds=False)
+    
+    rp = read('''
+>prv579
+taattaaTTTGTAATTAAAACTTAGATTA
+''', ds=False)
+    
+    pYPKa_cut = pYPKa.linearize(enz)
+    
+    ins = pcr(fp, rp, ins)
+    
+    seq = (pYPKa_cut + ins).looped().synced("tcgcgcgtttcggtgatgacggtgaaaacctctg")
+
+    # This script should define a Dseqrecord named seq
+    #---------------------------------------------------------------------------
+    assert isinstance(seq, Dseqrecord)
+    return seq
+    
+execfile("footer.py")
