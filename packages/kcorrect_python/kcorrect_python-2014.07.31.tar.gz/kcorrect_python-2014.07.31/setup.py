2cb918fd3bdcdca22e847973de23318cdfc022f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:54 +0000

    Add package kcorrect_python/2014.07.31/kcorrect_python-2014.07.31.tar.gz

---
+++ packages/kcorrect_python/kcorrect_python-2014.07.31.tar.gz/kcorrect_python-2014.07.31/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup, Extension
+try:
+    import numpy
+except ImportError:
+    raise SystemExit('requires NumPy version > 1.7.0') 
+
+import os
+try:
+    kcorrect_dir = os.environ['KCORRECT_DIR']
+except KeyError:
+    raise SystemExit('KCORRECT_DIR must be set') 
+    
+INC_DIR = [os.path.join(kcorrect_dir,'include'),
+           numpy.get_include()]
+LIB_DIR = [os.path.join(kcorrect_dir,'lib')]
+modulekcorrect = Extension('_kcorrect',
+                           include_dirs = INC_DIR,
+                           libraries = ['kcorrect', 'm'],
+                           library_dirs = LIB_DIR,
+                           sources = ['src/_kcorrectmodule.c'])
+
+setup (name = 'kcorrect_python',
+       version = '2014.07.31',
+       description = 'Python wrapper for Kcorrect library',
+       url = 'https://github.com/nirinA/kcorrect_python',
+       author = 'nirina raseliarison',
+       author_email = 'nirina.raseliarison@gmail.com',
+       py_modules=['kcorrect'],
+       ext_modules = [modulekcorrect],
+       license = 'Public Domain')
