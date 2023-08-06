122a238e26773bb3a8159430a856beab61c60ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:51 +0000

    Add package describe_it/0.1.8/describe_it-0.1.8.tar.gz

---
+++ packages/describe_it/describe_it-0.1.8.tar.gz/describe_it-0.1.8/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='describe_it',
+    version='0.1.8',
+    description=('A nose plugin that supports writing describe/it style '
+                 'unit tests with nested contexts'),
+    long_description=open('README.rst').read(),
+    license=open('LICENSE.txt').read(),
+    url='https://github.com/joakimkarlsson/describe_it',
+    author='Joakim Karlsson',
+    author_email='joakim@jkarlsson.com',
+
+    packages=['describe_it'],
+    include_package_data = True,
+    install_requires=['nose>=1.3.3'],
+
+    tests_require=['nose>=1.3.3', 'mock>=1.0.1'],
+    test_suite='nose.collector',
+
+    entry_points={
+        'nose.plugins.0.10': [
+            'describe_it=describe_it.noseplugin:DescribeItPlugin'
+        ]
+    },
+
+    keywords=['unit testing', 'tdd', 'bdd', 'nose']
+)
