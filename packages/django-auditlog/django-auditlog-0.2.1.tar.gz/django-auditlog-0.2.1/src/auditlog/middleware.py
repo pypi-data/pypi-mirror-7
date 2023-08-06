5e93dcb25ada8744c71d79d3c1fef07efcaf4c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:49 +0000

    Add package django-auditlog/0.2.1/django-auditlog-0.2.1.tar.gz

---
+++ packages/django-auditlog/django-auditlog-0.2.1.tar.gz/django-auditlog-0.2.1/src/auditlog/middleware.py
@@ -0,0 +1,58 @@
+import time
+
+from django.conf import settings
+from django.db.models.signals import pre_save
+from django.utils.functional import curry
+from django.db.models.loading import get_model
+from auditlog.models import LogEntry
+
+
+class AuditlogMiddleware(object):
+    """
+    Middleware to couple the request's user to log items. This is accomplished by currying the signal receiver with the
+    user from the request (or None if the user is not authenticated).
+    """
+
+    def process_request(self, request):
+        """
+        Gets the current user from the request and prepares and connects a signal receiver with the user already
+        attached to it.
+        """
+        if hasattr(request, 'user') and hasattr(request.user, 'is_authenticated') and request.user.is_authenticated():
+            user = request.user
+            request.auditlog_ts = time.time()
+            set_actor = curry(self.set_actor, user)
+            pre_save.connect(set_actor, sender=LogEntry, dispatch_uid=(self.__class__, request.auditlog_ts), weak=False)
+
+    def process_response(self, request, response):
+        """
+        Disconnects the signal receiver to prevent it from staying active.
+        """
+        # Disconnecting the signal receiver is required because it will not be garbage collected (non-weak reference)
+        if hasattr(request, 'auditlog_ts'):
+            pre_save.disconnect(sender=LogEntry, dispatch_uid=(self.__class__, request.auditlog_ts))
+
+        return response
+
+    def process_exception(self, request, exception):
+        """
+        Disconnects the signal receiver to prevent it from staying active in case of an exception.
+        """
+        if hasattr(request, 'auditlog_ts'):
+            pre_save.disconnect(sender=LogEntry, dispatch_uid=(self.__class__, request.auditlog_ts))
+
+        return None
+
+    @staticmethod
+    def set_actor(user, sender, instance, **kwargs):
+        """
+        Signal receiver with an extra, required 'user' kwarg. This method becomes a real (valid) signal receiver when
+        it is curried with the actor.
+        """
+        try:
+            app_label, model_name = settings.AUTH_USER_MODEL.split('.')
+            auth_user_model = get_model(app_label, model_name)
+        except ValueError:
+            auth_user_model = get_model('auth', 'user')
+        if sender == LogEntry and isinstance(user, auth_user_model) and instance.actor is None:
+            instance.actor = user
---
+++ packages/django-auditlog/django-auditlog-0.2.1.tar.gz/django-auditlog-0.2.1/src/auditlog/models.py
@@ -0,0 +1,186 @@
+import json
+
+from django.conf import settings
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class LogEntryManager(models.Manager):
+    """
+    Custom manager for the LogEntry model.
+    """
+
+    def log_create(self, instance, **kwargs):
+        """
+        Helper method to create a new log entry. This method automatically fills in some data when it is left out. It
+        was created to keep things DRY.
+        """
+        changes = kwargs.get('changes', None)
+        pk = self._get_pk_value(instance)
+
+        if changes is not None:
+            kwargs.setdefault('content_type', ContentType.objects.get_for_model(instance))
+            kwargs.setdefault('object_pk', pk)
+            kwargs.setdefault('object_repr', str(instance))
+
+            if isinstance(pk, int):
+                kwargs.setdefault('object_id', pk)
+
+            # Delete log entries with the same pk as a newly created model. This should only be necessary when an pk is
+            # used twice.
+            if kwargs.get('action', None) is LogEntry.Action.CREATE:
+                if kwargs.get('object_id', None) is not None and self.filter(content_type=kwargs.get('content_type'), object_id=kwargs.get('object_id')).exists():
+                    self.filter(content_type=kwargs.get('content_type'), object_id=kwargs.get('object_id')).delete()
+                else:
+                    self.filter(content_type=kwargs.get('content_type'), object_pk=kwargs.get('object_pk', '')).delete()
+
+            return self.create(**kwargs)
+        return None
+        
+    def get_for_object(self, instance):
+        """
+        Get log entries for the specified object.
+        """
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        pk = self._get_pk_value(instance)
+
+        if isinstance(pk, int):
+            return self.filter(content_type=content_type, object_id=pk)
+        else:
+            return self.filter(content_type=content_type, object_pk=pk)
+
+    def get_for_model(self, model):
+        """
+        Get log entries for all objects of a specified type.
+        """
+        content_type = ContentType.objects.get_for_model(model)
+        return self.filter(content_type=content_type)
+
+    def _get_pk_value(self, instance):
+        """
+        Get the primary key field value for a model instance.
+        """
+        pk_field = instance._meta.pk.name
+        return getattr(instance, pk_field, None)
+
+
+class LogEntry(models.Model):
+    """
+    Represents an entry in the audit log. The content type is saved along with the textual and numeric (if available)
+    primary key, as well as the textual representation of the object when it was saved. It holds the action performed
+    and the fields that were changed in the transaction.
+
+    If AuditlogMiddleware is used, the actor will be set automatically. Keep in mind that editing / re-saving LogEntry
+    instances may set the actor to a wrong value - editing LogEntry instances is not recommended (and it should not be
+    necessary).
+    """
+
+    class Action:
+        """
+        The actions that Auditlog distinguishes: creating, updating and deleting objects. Viewing objects is not logged.
+        The values of the actions are numeric, a higher integer value means a more intrusive action. This may be useful
+        in some cases when comparing actions because __lt, __lte, __gt, __gte can be used in queries.
+        """
+        CREATE = 0
+        UPDATE = 1
+        DELETE = 2
+
+        choices = (
+            (CREATE, _("create")),
+            (UPDATE, _("update")),
+            (DELETE, _("delete")),
+        )
+
+    content_type = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE, related_name='+', verbose_name=_("content type"))
+    object_pk = models.TextField(verbose_name=_("object pk"))
+    object_id = models.PositiveIntegerField(blank=True, db_index=True, null=True, verbose_name=_("object id"))
+    object_repr = models.TextField(verbose_name=_("object representation"))
+    action = models.PositiveSmallIntegerField(choices=Action.choices, verbose_name=_("action"))
+    changes = models.TextField(blank=True, verbose_name=_("change message"))
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL, related_name='+', verbose_name=_("actor"))
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name=_("timestamp"))
+
+    objects = LogEntryManager()
+
+    class Meta:
+        get_latest_by = 'timestamp'
+        ordering = ['-timestamp']
+        verbose_name = _("log entry")
+        verbose_name_plural = _("log entries")
+
+    def __unicode__(self):
+        if self.action == self.Action.CREATE:
+            fstring = _("Created {repr:s}")
+        elif self.action == self.Action.UPDATE:
+            fstring = _("Updated {repr:s}")
+        elif self.action == self.Action.DELETE:
+            fstring = _("Deleted {repr:s}")
+        else:
+            fstring = _("Logged {repr:s}")
+
+        return fstring.format(repr=self.object_repr)
+
+    @property
+    def changes_dict(self):
+        """
+        Return the changes recorded in this log entry as a dictionary object.
+        """
+        try:
+            return json.loads(self.changes)
+        except ValueError:
+            return {}
+
+    @property
+    def changes_str(self, colon=': ', arrow=u' \u2192 ', separator='; '):
+        """
+        Return the changes recorded in this log entry as a string. The formatting of the string can be customized by
+        setting alternate values for colon, arrow and separator. If the formatting is still not satisfying, please use
+        changes_dict() and format the string yourself.
+        """
+        substrings = []
+
+        for field, values in self.changes_dict.iteritems():
+            substring = u'{field_name:s}{colon:s}{old:s}{arrow:s}{new:s}'.format(
+                field_name=field,
+                colon=colon,
+                old=values[0],
+                arrow=arrow,
+                new=values[1],
+            )
+            substrings.append(substring)
+
+        return separator.join(substrings)
+        
+
+class AuditlogHistoryField(generic.GenericRelation):
+    """
+    A subclass of django.contrib.contenttypes.generic.GenericRelation that sets some default variables. This makes it
+    easier to implement the audit log in models, and makes future changes easier.
+
+    By default this field will assume that your primary keys are numeric, simply because this is the most common case.
+    However, if you have a non-integer primary key, you can simply pass pk_indexable=False to the constructor, and
+    Auditlog will fall back to using a non-indexed text based field for this model.
+
+    Using this field will not automatically register the model for automatic logging. This is done so you can be more
+    flexible with how you use this field.
+    """
+
+    def __init__(self, pk_indexable=True, **kwargs):
+        kwargs['to'] = LogEntry
+
+        if pk_indexable:
+            kwargs['object_id_field'] = 'object_id'
+        else:
+            kwargs['object_id_field'] = 'object_pk'
+
+        kwargs['content_type_field'] = 'content_type'
+        super(AuditlogHistoryField, self).__init__(**kwargs)
+
+# South compatibility for AuditlogHistoryField
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^southtut\.fields\.UpperCaseField"])
+except ImportError:
+    pass
