bc4830ce794d08ad2a9f61b9f5570918f2a325d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:38 +0000

    Add package flashCardStudy/1.0dev/flashCardStudy-1.0dev.tar.gz

---
+++ packages/flashCardStudy/flashCardStudy-1.0dev.tar.gz/flashCardStudy-1.0dev/flashcardstudy/errors.py
@@ -0,0 +1,34 @@
+import sys
+
+def id(num,no_quit=None):
+	
+	print_help = "\nAdd `--help` or '-h' to see more."
+
+	if num == 0:
+		error = """
+No arguments passed."""
+	elif num == 1:
+		error = """
+Invalid arguments (filename or arg)."""
+	elif num == 2:
+		error = """
+You cannot use this argument in this combination."""
+	elif num == 3:
+		error = """
+You must append argument to launch the program."""
+	elif num == 4:
+		error = """
+Don't use this argument with file(s)."""
+	elif num == 5:
+		error = """
+Invalid card ID. Try again."""
+	elif num == 6:
+		error = """
+You need to add `-d`/`--display` arg"""
+
+	print 'Error!', error, print_help
+
+	if no_quit:
+		pass
+	else:
+		exit(0)
