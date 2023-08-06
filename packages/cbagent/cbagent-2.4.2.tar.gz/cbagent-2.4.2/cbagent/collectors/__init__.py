a66842c2253bac76198684061c0faa24e5db0ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:33 +0000

    Add package cbagent/2.4.2/cbagent-2.4.2.tar.gz

---
+++ packages/cbagent/cbagent-2.4.2.tar.gz/cbagent-2.4.2/cbagent/collectors/__init__.py
@@ -0,0 +1,13 @@
+from collector import Collector
+from active_tasks import ActiveTasks
+from atop import Atop
+from iostat import IO
+from latency import Latency
+from observe import ObserveLatency
+from net import Net
+from ns_server import NSServer
+from ps import PS
+from spring_latency import SpringLatency, SpringQueryLatency, \
+    SpringN1QLQueryLatency
+from sync_gateway import SyncGateway
+from xdcr_lag import XdcrLag
---
+++ packages/cbagent/cbagent-2.4.2.tar.gz/cbagent-2.4.2/cbagent/collectors/libstats/iostat.py
@@ -0,0 +1,48 @@
+from fabric.api import run
+
+
+from cbagent.collectors.libstats.remotestats import (
+    RemoteStats, multi_node_task)
+
+
+class IOstat(RemoteStats):
+
+    METRICS = (
+        ("rps", "r/s", 1),
+        ("wps", "w/s", 1),
+        ("rbps", "rkB/s", 1024),  # kB -> B
+        ("wbps", "wkB/s", 1024),  # kB -> B
+        ("avgqusz", "avgqu-sz", 1),
+        ("await", "await", 1),
+        ("util", "%util", 1),
+    )
+
+    def get_device_name(self, partition):
+        for path in (partition, '/'):
+            stdout = run("mount | grep '{} '".format(path),
+                         warn_only=True, quiet=True)
+            if not stdout.return_code:
+                return stdout.split()[0]
+
+    def get_iostat(self, device):
+        stdout = run(
+            "iostat -xk 1 2 {} | grep -v '^$' | tail -n 2".format(device)
+        )
+        stdout = stdout.split()
+        header = stdout[:len(stdout)/2]
+        data = dict()
+        for i, value in enumerate(stdout[len(stdout)/2:]):
+            data[header[i]] = value
+        return data
+
+    @multi_node_task
+    def get_samples(self, partitions):
+        samples = {}
+
+        for purpose, partition in partitions.items():
+            device = self.get_device_name(partition)
+            data = self.get_iostat(device)
+            for shorthand, metric, multiplier in self.METRICS:
+                key = "{}_{}".format(purpose, shorthand)
+                samples[key] = float(data[metric]) * multiplier
+        return samples
