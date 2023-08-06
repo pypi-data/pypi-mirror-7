21e9bcf6f17f7d1ae507fa6951f592a4ff756d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:55 +0000

    Add package PySide/1.2.2/PySide-1.2.2-cp34-cp34m-macosx_10_6_intel.whl

---
+++ packages/PySide/PySide-1.2.2-cp34-cp34m-macosx_10_6_intel.whl/PySide/examples/itemviews/editabletreemodel/editabletreemodel_rc.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Resource object code
+#
+# Created: Mi. Juni 18 20:46:41 2014
+#      by: The Resource Compiler for PySide (Qt v4.8.6)
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore
+
+qt_resource_data = b"\x00\x00\x07\xb9Getting Started\x09\x09\x09\x09How to familiarize yourself with Qt Designer\x0a    Launching Designer\x09\x09\x09Running the Qt Designer application\x0a    The User Interface\x09\x09\x09How to interact with Qt Designer\x0a\x0aDesigning a Component\x09\x09\x09Creating a GUI for your application\x0a    Creating a Dialog\x09\x09\x09How to create a dialog\x0a    Composing the Dialog\x09\x09Putting widgets into the dialog example\x0a    Creating a Layout\x09\x09\x09Arranging widgets on a form\x0a    Signal and Slot Connections\x09\x09Making widget communicate with each other\x0a\x0aUsing a Component in Your Application\x09Generating code from forms\x0a    The Direct Approach\x09\x09\x09Using a form without any adjustments\x0a    The Single Inheritance Approach\x09Subclassing a form's base class\x0a    The Multiple Inheritance Approach\x09Subclassing the form itself\x0a    Automatic Connections\x09\x09Connecting widgets using a naming scheme\x0a        A Dialog Without Auto-Connect\x09How to connect widgets without a naming scheme\x0a        A Dialog With Auto-Connect\x09Using automatic connections\x0a\x0aForm Editing Mode\x09\x09\x09How to edit a form in Qt Designer\x0a    Managing Forms\x09\x09\x09Loading and saving forms\x0a    Editing a Form\x09\x09\x09Basic editing techniques\x0a    The Property Editor\x09\x09\x09Changing widget properties\x0a    The Object Inspector\x09\x09Examining the hierarchy of objects on a form\x0a    Layouts\x09\x09\x09\x09Objects that arrange widgets on a form\x0a        Applying and Breaking Layouts\x09Managing widgets in layouts \x0a        Horizontal and Vertical Layouts\x09Standard row and column layouts\x0a        The Grid Layout\x09\x09\x09Arranging widgets in a matrix\x0a    Previewing Forms\x09\x09\x09Checking that the design works\x0a\x0aUsing Containers\x09\x09\x09How to group widgets together\x0a    General Features\x09\x09\x09Common container features\x0a    Frames\x09\x09\x09\x09QFrame\x0a    Group Boxes\x09\x09\x09\x09QGroupBox\x0a    Stacked Widgets\x09\x09\x09QStackedWidget\x0a    Tab Widgets\x09\x09\x09\x09QTabWidget\x0a    Toolbox Widgets\x09\x09\x09QToolBox\x0a\x0aConnection Editing Mode\x09\x09\x09Connecting widgets together with signals and slots\x0a    Connecting Objects\x09\x09\x09Making connections in Qt Designer\x0a    Editing Connections\x09\x09\x09Changing existing connections\x0a"
+qt_resource_name = b"\x00\x0b\x0c\xe2\x22\xf4\x00d\x00e\x00f\x00a\x00u\x00l\x00t\x00.\x00t\x00x\x00t"
+qt_resource_struct = b"\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00"
+def qInitResources():
+    QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
---
+++ packages/PySide/PySide-1.2.2-cp34-cp34m-macosx_10_6_intel.whl/PySide/examples/richtext/syntaxhighlighter/syntaxhighlighter_rc.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Resource object code
+#
+# Created: Mi. Juni 18 20:46:42 2014
+#      by: The Resource Compiler for PySide (Qt v4.8.6)
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore
+
+qt_resource_data = b"\x00\x00\x06yTEMPLATE = app\x0aLANGUAGE = C++\x0aTARGET         = assistant\x0a\x0aCONFIG        += qt warn_on\x0aQT            += xml network\x0a\x0aPROJECTNAME        = Assistant\x0aDESTDIR            = ../../bin\x0a\x0aFORMS += finddialog.ui \x5c\x0a        helpdialog.ui \x5c\x0a        mainwindow.ui \x5c\x0a        settingsdialog.ui \x5c\x0a        tabbedbrowser.ui \x5c\x0a        topicchooser.ui\x0a\x0aSOURCES += main.cpp \x5c\x0a        helpwindow.cpp \x5c\x0a        topicchooser.cpp \x5c\x0a        docuparser.cpp \x5c\x0a        settingsdialog.cpp \x5c\x0a        index.cpp \x5c\x0a        profile.cpp \x5c\x0a        config.cpp \x5c\x0a        finddialog.cpp \x5c\x0a        helpdialog.cpp \x5c\x0a        mainwindow.cpp \x5c\x0a        tabbedbrowser.cpp\x0a\x0aHEADERS        += helpwindow.h \x5c\x0a        topicchooser.h \x5c\x0a        docuparser.h \x5c\x0a        settingsdialog.h \x5c\x0a        index.h \x5c\x0a        profile.h \x5c\x0a        finddialog.h \x5c\x0a        helpdialog.h \x5c\x0a        mainwindow.h \x5c\x0a        tabbedbrowser.h \x5c\x0a        config.h\x0a\x0aRESOURCES += assistant.qrc\x0a\x0aDEFINES += QT_KEYWORDS\x0a#DEFINES +=  QT_PALMTOPCENTER_DOCS\x0a!network:DEFINES        += QT_INTERNAL_NETWORK\x0aelse:QT += network\x0a!xml: DEFINES                += QT_INTERNAL_XML\x0aelse:QT += xml\x0ainclude( ../../src/qt_professional.pri )\x0a\x0awin32 {\x0a    LIBS += -lshell32\x0a    RC_FILE = assistant.rc\x0a}\x0a\x0amac {\x0a    ICON = assistant.icns\x0a    TARGET = assistant\x0a#    QMAKE_INFO_PLIST = Info_mac.plist\x0a}\x0a\x0a#target.path = $$[QT_INSTALL_BINS]\x0a#INSTALLS += target\x0a\x0a#assistanttranslations.files = *.qm\x0a#assistanttranslations.path = $$[QT_INSTALL_TRANSLATIONS]\x0a#INSTALLS += assistanttranslations\x0a\x0aTRANSLATIONS        = assistant_de.ts \x5c\x0a                  assistant_fr.ts\x0a\x0a\x0aunix:!contains(QT_CONFIG, zlib):LIBS += -lz\x0a\x0a\x0atarget.path=$$[QT_INSTALL_BINS]\x0aINSTALLS += target\x0a"
+qt_resource_name = b"\x00\x08\x0e\x84\x7fC\x00e\x00x\x00a\x00m\x00p\x00l\x00e\x00s\x00\x07\x0c\xe8G\xe5\x00e\x00x\x00a\x00m\x00p\x00l\x00e"
+qt_resource_struct = b"\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x16\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00"
+def qInitResources():
+    QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
