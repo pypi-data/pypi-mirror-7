4fbd126fb6e0b3bbfb064e96c366480ef3f1d138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package metrology/0.10.1/metrology-0.10.1.tar.gz

---
+++ packages/metrology/metrology-0.10.1.tar.gz/metrology-0.10.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='metrology',
+    version='0.10.1',
+    description='A library to easily measure what\'s going on in your python.',
+    long_description=readme,
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/metrology',
+    license=license,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'astrolabe>=0.4.0',
+        'atomic>=0.7.0',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ]
+)
