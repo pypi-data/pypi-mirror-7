b6b8dfe68f0acff104fa56ae834f01596fc371bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package django-csv-tool/0.3.2/django-csv-tool-0.3.2.tar.gz

---
+++ packages/django-csv-tool/django-csv-tool-0.3.2.tar.gz/django-csv-tool-0.3.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-csv-tool',
+    version='0.3.2',
+    author='John Leith',
+    author_email='leith.john@gmail.com',
+    packages=find_packages(),
+    url='https://bitbucket.org/freakypie/django-csv-tool',
+    description='Tool to help create CSV imports faster',
+    install_requires=[
+    ]
+)
