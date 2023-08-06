26c253be9272f3cfd256ca3d78f8f97c3ee3d69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:11 +0000

    Add package python_sound/1.1.0/python_sound-1.1.0.zip

---
+++ packages/python_sound/python_sound-1.1.0.zip/python_sound-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'python_sound',
+    version = '1.1.0',
+    py_modules = ['python_sound'],
+    author = 'soundharya',
+    author_email = 'soundharya@umail.ucsb.edu',
+    url = '',
+    description = 'A sample code for printing nested lists',
+    )
