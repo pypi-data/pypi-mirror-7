e0dca7700d2716ee61515572cbf7dbcba2c3e676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:28 +0000

    Add package djehutyslack/0.0.3/djehutyslack-0.0.3.tar.gz

---
+++ packages/djehutyslack/djehutyslack-0.0.3.tar.gz/djehutyslack-0.0.3/djehutyslack/slack.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import shlex
+
+from pyramid.httpexceptions import HTTPUnauthorized
+
+from djehuty.service import Service
+
+
+class Slack(Service):
+
+    def validate(self, request):
+        token = request.params.get('token')
+        if not token or token != os.environ.get('DJEHUTY_SLACK_OUTGOING_TOKEN'):
+            raise HTTPUnauthorized()
+
+    def get_user(self, request):
+        return request.params.get('user_name')
+
+    def get_room(self, request):
+        return request.params.get('channel_name')
+
+    def make_command_line(self, request):
+        m = re.match(
+            r'^{}\W*(.*)$'.format(request.params.get('trigger_word', '')),
+            request.params.get('text', '')
+        )
+        return shlex.split(m.group(1).encode('utf8')) if m is not None else []
+
+    def make_response(self, result):
+        return {
+            'text': result,
+            'link_names': 1,
+            'parse': 'full',
+        }
+
+slack = Slack()
