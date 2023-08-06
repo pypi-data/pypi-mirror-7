0cf43bc77cf2b9bf1f7dedb551c7ec4a25fe3d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package permalink_adder/0.1.1/permalink_adder-0.1.1.tar.gz

---
+++ packages/permalink_adder/permalink_adder-0.1.1.tar.gz/permalink_adder-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'permalink_adder',
+  packages = ['permalink_adder'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'SEO tool for adding permalinks to text contained in Django apps databases.',
+  author = 'Piotr Lizonczyk',
+  author_email = 'piotr.lizonczyk@gmail.com',
+  url = 'https://github.com/plizonczyk/permalink_adder', # use the URL to the github repo
+  download_url = 'https://github.com/plizonczyk/permalink_adder/tarball/0.1', # I'll explain this in a second
+  keywords = ['SEO', 'permalinks'], # arbitrary keywords
+  classifiers = ['Development Status :: 2 - Pre-Alpha',
+                 'Framework :: Django',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 2.7'],
+  requires = ['django']
+)
