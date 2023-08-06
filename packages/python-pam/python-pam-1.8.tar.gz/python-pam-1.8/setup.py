074332c5111b2cf0bb0f249b7ec77a3b28edf3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package python-pam/1.8/python-pam-1.8.tar.gz

---
+++ packages/python-pam/python-pam-1.8.tar.gz/python-pam-1.8/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+__sdesc = 'Python PAM for py3/py2'
+
+setup(name='python-pam',
+      description= __sdesc,
+      long_description=read('README.md'),
+      py_modules=['pam'],
+      version='1.8',
+      author='David Ford',
+      author_email='david@blue-labs.org',
+      maintainer='David Ford',
+      maintainer_email='david@blue-labs.org',
+      url='https://github.com/FirefighterBlu3/python-pam',
+      license='LICENSE',
+      classifiers=[
+          'Development Status :: 6 - Mature',
+          'Environment :: Console',
+          'Environment :: No Input/Output (Daemon)',
+          'Environment :: OpenStack',
+          'Environment :: Other Environment',
+          'Environment :: Plugins',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Systems Administration :: Authentication/Directory',
+          ],
+      platforms=['Linux','FreeBSD'],
+      )
