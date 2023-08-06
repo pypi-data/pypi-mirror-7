cc1a016a37e1cb94598fb175e58b56481128ae98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package dbwipes/0.0.2/dbwipes-0.0.2.tar.gz

---
+++ packages/dbwipes/dbwipes-0.0.2.tar.gz/dbwipes-0.0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+import dbwipes
+
+setup(name="dbwipes",
+      version=dbwipes.__version__,
+      description="dbwipes a database",
+      license="MIT",
+      author="Eugene Wu",
+      author_email="eugenewu@mit.edu",
+      url="http://github.com/sirrice/summary",
+      packages = find_packages(),
+      include_package_data = True,      
+      package_data = {
+        'static': 'dbwipes/static',
+        'templates': 'dbwipes/templates'
+      },
+      package_dir = {'dbwipes' : 'dbwipes'},
+      scripts = [
+        'bin/dbwipesserver.py',
+        'bin/summarizedb.py'
+      ],
+      #zip_safe = False,
+      install_requires = ['flask', 'psycopg2', 'sqlalchemy', 'flask-compress', 'scorpionsql'],
+      keywords= "")
