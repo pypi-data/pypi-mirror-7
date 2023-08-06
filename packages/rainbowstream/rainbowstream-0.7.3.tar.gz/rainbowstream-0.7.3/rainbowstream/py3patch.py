c8dba2ccf3a66e54e62c43b9f732603067b4fc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package rainbowstream/0.7.3/rainbowstream-0.7.3.tar.gz

---
+++ packages/rainbowstream/rainbowstream-0.7.3.tar.gz/rainbowstream-0.7.3/rainbowstream/py3patch.py
@@ -0,0 +1,31 @@
+"""
+  Python 3 supports
+"""
+import sys
+
+# StringIO module
+try:
+    from StringIO import StringIO, BytesIO
+except:
+    from io import StringIO, BytesIO
+
+# HTMLParser module
+if sys.version[0] == "2":
+    from HTMLParser import HTMLParser
+else:
+    from html.parser import HTMLParser
+unescape = HTMLParser().unescape
+# According to https://github.com/python/cpython/blob/master/Lib/html/parser.py#L547 ,
+# in python 3.5 maybe I should use
+# from html import unescape
+# but it is a far-future story:)
+
+# raw_input and map function behaviour
+if sys.version[0] == "2":
+    lmap = lambda f, a: map(f, a)
+    unc = lambda x: x.decode('utf-8')
+else:
+    xrange = range
+    raw_input = input
+    lmap = lambda f, a: list(map(f, a))
+    unc = lambda x: x
