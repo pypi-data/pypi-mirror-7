7307479a54d5e5162af630a58b71abbc063ece41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:02 +0000

    Add package wsgi-seo-iframe/0.1.1/wsgi-seo-iframe-0.1.1.tar.gz

---
+++ packages/wsgi-seo-iframe/wsgi-seo-iframe-0.1.1.tar.gz/wsgi-seo-iframe-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='wsgi-seo-iframe',
+    version='0.1.1',
+    author='Tiago Queiroz',
+    author_email='contato@tiago.eti.br',
+    packages=['wsgi_seo_iframe'],
+    url='http://github.com/belimawr/wsgi_seo_iframe',
+    license='LICENSE.txt',
+    description='WSGI middleware for replacing iframes by their html content, allowing them to be crowled by search engines',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "wsgi-content-modifier",
+        "werkzeug",
+        "pyquery",
+        "selenium",
+    ],
+)
