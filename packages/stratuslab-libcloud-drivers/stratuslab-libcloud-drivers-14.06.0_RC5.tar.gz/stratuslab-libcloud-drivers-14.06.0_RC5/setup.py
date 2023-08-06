6a7985b3163c27ec688b6a0ac5f49889524b5ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:11 +0000

    Add package stratuslab-libcloud-drivers/14.06.0_RC5/stratuslab-libcloud-drivers-14.06.0_RC5.tar.gz

---
+++ packages/stratuslab-libcloud-drivers/stratuslab-libcloud-drivers-14.06.0_RC5.tar.gz/stratuslab-libcloud-drivers-14.06.0_RC5/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name='stratuslab-libcloud-drivers',
+    version='14.06.0_RC5',
+    author='StratusLab',
+    author_email='contact@stratuslab.eu',
+    url='http://stratuslab.eu/',
+    license='Apache 2.0',
+    description='Libcloud drivers for the StratusLab cloud services.',
+    long_description=open('README.txt').read(),
+
+    packages=['stratuslab', 'stratuslab.libcloud'],
+
+    install_requires=[
+        "apache-libcloud >=0.14.0",
+        "stratuslab-client",
+        ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Distributed Computing',
+        ],
+
+)
