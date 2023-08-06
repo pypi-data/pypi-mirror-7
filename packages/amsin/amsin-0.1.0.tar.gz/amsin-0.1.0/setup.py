5b25c79a336e22185b5b78d0f44f9ef7599df839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:31 +0000

    Add package amsin/0.1.0/amsin-0.1.0.tar.gz

---
+++ packages/amsin/amsin-0.1.0.tar.gz/amsin-0.1.0/setup.py
@@ -0,0 +1,25 @@
+"""amsin is a simple command line tool to shortlink an
+amazon URL.
+
+"""
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='amsin',
+    description="Simple tool for shortlinking amazon pages.",
+    version='0.1.0',
+    long_description=__doc__,
+    packages=['amsin'],
+    author='Anthony Grimes',
+    author_email='i@raynes.me',
+    url='https://github.com/Raynes/amsin',
+    license='MIT',
+    install_requires=requirements,
+    entry_points = """
+    [console_scripts]
+    amsin=amsin.__main__:amsin
+    """
+)
