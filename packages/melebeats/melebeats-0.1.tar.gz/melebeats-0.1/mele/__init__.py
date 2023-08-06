d25af748be947bde512e258e5eeebd7758ddb91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:22 +0000

    Add package melebeats/0.1/melebeats-0.1.tar.gz

---
+++ packages/melebeats/melebeats-0.1.tar.gz/melebeats-0.1/mele/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1'
+
+from time import sleep
+import random
+
+from termcolor import colored
+
+def beat():
+    colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+    highlights = ['on_grey', 'on_red', 'on_green', 'on_yellow', 'on_blue', 'on_magenta', 'on_cyan', 'on_white']
+    attributes = ['bold', 'dark', 'underline', 'blink', 'reverse', 'concealed']
+
+
+    while True:
+        print(colored('beats', random.choice(colors), random.choice(highlights), attrs=[random.choice(attributes)]))
+        sleep(0.25)
