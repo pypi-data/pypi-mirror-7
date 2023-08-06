a01388bb9a875cc3468b2437754fd88a225dcd69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:18 +0000

    Add package craption/1.0.6/craption-1.0.6.tar.gz

---
+++ packages/craption/craption-1.0.6.tar.gz/craption-1.0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    version='1.0.6',
+    name = "craption",
+    packages = ['craption'],
+    description='Simple screenshot uploader',
+    author='Jakob Hedman',
+    author_email='jakob@hedman.email',
+    maintainer='Jakob Hedman',
+    maintainer_email='jakob@hedman.email',
+    license='GNU GPLv3',
+    url='https://github.com/spillevink/CRAPtion',
+    package_dir = {'craption':'craption'},
+    package_data = {
+        'cod': ['noise.wav'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'craption = craption.cli:dispatch',
+        ],
+    },
+    install_requires = [
+        'dropbox',
+        'pyperclip',
+        'requests',
+        'opster',
+        'configobj',
+    ],
+    long_description = open('README.rst').read(),
+)
