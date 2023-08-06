adf81541af60a1f91a19e251282a3c15133238f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:04 +0000

    Add package handroll/1.3/handroll-1.3.tar.gz

---
+++ packages/handroll/handroll-1.3.tar.gz/handroll-1.3/handroll/site.py
@@ -0,0 +1,174 @@
+# Copyright (c) 2014, Matt Layman
+"""The website model"""
+
+import os
+import shutil
+import time
+
+from handroll import logger
+from handroll import template
+from handroll.composers import Composers
+from handroll.exceptions import AbortError
+from handroll.template import catalog
+
+
+class Site(object):
+
+    CONFIG = 'handroll.conf'
+    OUTPUT = 'output'
+    SKIP_EXTENSION = [
+        '.swp',
+    ]
+    SKIP_FILES = [
+        CONFIG,
+    ]
+
+    def __init__(self, path=None):
+        self.path = path
+        if self.path is None:
+            self.path = self._find_site_root_from(os.getcwd())
+
+        self.catalog = catalog.TemplateCatalog(self.path)
+        self.composers = Composers()
+
+    @property
+    def config_file(self):
+        return os.path.join(self.path, self.CONFIG)
+
+    @property
+    def output_root(self):
+        """The default output root directory"""
+        return os.path.join(self.path, self.OUTPUT)
+
+    def generate(self, config):
+        """Walk the site tree and generate the output."""
+        self._clean_output()
+
+        # When no output directory is given, the default will be used.
+        if config.outdir is None:
+            outdir = self.output_root
+        else:
+            outdir = config.outdir
+
+        self._generate_output(outdir, config.timing)
+
+    def is_valid(self):
+        if not os.path.isdir(self.path):
+            return False, '{0} is not a directory.'.format(self.path)
+
+        return True, ''
+
+    def _clean_output(self):
+        if os.path.exists(self.output_root):
+            logger.info('Removing the old {0} ...'.format(self.output_root))
+            shutil.rmtree(self.output_root)
+
+    def _find_site_root_from(self, cwd):
+        """Ascend through the current working directory provided to find
+        something that looks like the root of a handroll site and return that
+        path. Assumes that ``cwd`` is a valid directory path."""
+        candidate = cwd
+        while True:
+            if self._is_site_root(candidate):
+                return candidate
+
+            parent = os.path.realpath(os.path.join(candidate, os.pardir))
+            if candidate == parent:
+                # When the next candidate is equal to the previous one, then
+                # the root of the filesystem has been reached and tested.
+                break
+
+            candidate = parent
+
+        raise AbortError(
+            'A handroll site was not found in {0}'
+            ' or any of its parents.'.format(cwd))
+
+    def _is_site_root(self, path):
+        """Check if the path provided is the handroll site's root."""
+        # It looks like a site root if it has the config file.
+        if os.path.exists(os.path.join(path, self.CONFIG)):
+            return True
+        # It looks like a site root if it has templates.
+        elif template.has_templates(path):
+            return True
+
+        return False
+
+    def _generate_output(self, outdir, timing):
+        if os.path.exists(outdir):
+            logger.info('Updating {0} ...'.format(outdir))
+        else:
+            logger.info('Creating {0} ...'.format(outdir))
+            os.mkdir(outdir)
+
+        for dirpath, dirnames, filenames in os.walk(self.path):
+            # Prevent work on the output or templates directory.
+            # Skip the template.
+            if dirpath == self.path:
+                if self.OUTPUT in dirnames:
+                    dirnames.remove(self.OUTPUT)
+                if template.TEMPLATES_DIR in dirnames:
+                    dirnames.remove(template.TEMPLATES_DIR)
+                if template.DEFAULT_TEMPLATE in filenames:
+                    filenames.remove(template.DEFAULT_TEMPLATE)
+
+            output_dirpath = self._get_output_dirpath(dirpath, outdir)
+            logger.info('Populating {0} ...'.format(output_dirpath))
+
+            # Create new directories in output.
+            for dirname in dirnames:
+                out_dir = os.path.join(output_dirpath, dirname)
+                # The directory may already exist for updates.
+                if not os.path.exists(out_dir):
+                    logger.info('Creating directory {0} ...'.format(out_dir))
+                    os.mkdir(out_dir)
+
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                self._process_file(filepath, output_dirpath, timing)
+
+    def _get_output_dirpath(self, dirpath, outdir):
+        """Convert an input directory path rooted at the site path into the
+        name destined for the output directory."""
+        # Only split once to prevent the list from splitting multiple times on
+        # a common word like 'site'.
+        remainder = dirpath.split(self.path, 1)[1]
+        if remainder:
+            # Make sure the remainder doesn't look like an abs path.
+            return os.path.join(outdir, remainder.lstrip(os.sep))
+        else:
+            return outdir
+
+    def _process_file(self, filepath, output_dirpath, timing):
+        """Process the file according to its type."""
+        filename = os.path.basename(filepath)
+        if self._should_skip(filename):
+            return
+
+        if timing:
+            start = time.time()
+
+        composer = self.composers.select_composer_for(filename)
+        composer.compose(self.catalog, filepath, output_dirpath)
+
+        if timing:
+            end = time.time()
+            # Put at warn level to be independent of the verbose option.
+            logger.warn('[{:.3f}s]'.format(end - start))
+
+    def _should_skip(self, filename):
+        """Determine if the file type should be skipped."""
+        for skip_type in self.SKIP_EXTENSION:
+            if filename.endswith(skip_type):
+                logger.debug(
+                    'Skipping {0} with skipped file type \'{1}\' ...'.format(
+                        filename, skip_type))
+                return True
+
+        for skip_file in self.SKIP_FILES:
+            if filename.endswith(skip_file):
+                logger.debug('Skipping special file {0} ...'.format(filename))
+                return True
+
+        return False
---
+++ packages/handroll/handroll-1.3.tar.gz/handroll-1.3/handroll/tests/test_template.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2014, Matt Layman
+
+import os
+import tempfile
+import unittest
+
+from handroll.exceptions import AbortError
+from handroll.template.catalog import StringTemplate
+from handroll.template.catalog import TemplateCatalog
+
+
+class TestTemplateCatalog(unittest.TestCase):
+
+    def setUp(self):
+        self.site_path = tempfile.mkdtemp()
+
+    def _make_one(self):
+        return TemplateCatalog(self.site_path)
+
+    def _make_one_with_template(self, template_name):
+        catalog = self._make_one()
+        os.mkdir(catalog.templates_path)
+        template = os.path.join(catalog.templates_path, template_name)
+        with open(template, 'w') as f:
+            f.write('Does not matter.')
+        return catalog
+
+    def test_render_not_implemented(self):
+        from handroll.template.catalog import Template
+        template = Template()
+        self.assertRaises(NotImplementedError, template.render, {})
+
+    def test_renders_default(self):
+        """Test rendering a default template."""
+        content = '<html>${title}${content}</html>\n'
+        with open(os.path.join(self.site_path, 'template.html'), 'w') as f:
+            f.write(content)
+
+        catalog = self._make_one()
+        template = catalog.default
+        self.assertEqual(template.render({'title': 'foo', 'content': 'bar'}),
+                         '<html>foobar</html>\n')
+
+    def test_aborts_missing_default(self):
+        catalog = self._make_one()
+
+        def get_default():  # Function hack to test property exception
+            catalog.default
+        self.assertRaises(AbortError, get_default)
+
+    def test_fails_to_get_template(self):
+        catalog = self._make_one()
+        self.assertRaises(AbortError, catalog.get_template, 'sad.html')
+
+    def test_gets_template(self):
+        catalog = self._make_one_with_template('happy.html')
+        template = catalog.get_template('happy.html')
+        self.assertTrue(isinstance(template, StringTemplate))
+
+    def test_caches_template(self):
+        catalog = self._make_one_with_template('happy.html')
+        first = catalog.get_template('happy.html')
+        second = catalog.get_template('happy.html')
+        self.assertEqual(first, second)
+
+    def test_fails_on_bad_template_type(self):
+        catalog = self._make_one_with_template('confused.bogus')
+        self.assertRaises(AbortError, catalog.get_template, 'confused.bogus')
