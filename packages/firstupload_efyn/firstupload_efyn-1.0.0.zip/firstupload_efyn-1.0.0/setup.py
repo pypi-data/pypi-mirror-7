b387fb795b352d67a3f9fe0ca1fade1c96dc482e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package firstupload_efyn/1.0.0/firstupload_efyn-1.0.0.zip

---
+++ packages/firstupload_efyn/firstupload_efyn-1.0.0.zip/firstupload_efyn-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'firstupload_efyn',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author = 'Efyn',
+        author_email = 'efyn.zhang@gmail.com',
+        url = '',
+        description = 'A simple printer of nested listes',
+    )
