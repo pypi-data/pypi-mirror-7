3e9e47a9bd44bd4b30b9301aee6ffa0a733177a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package infi.nose_html_output/0.1.17/infi.nose_html_output-0.1.17-py2.7.egg

---
+++ packages/infi.nose_html_output/infi.nose_html_output-0.1.17-py2.7.egg/infi/nose_html_output/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.17"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.17)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '5d996f59e36f5235d969e3c0b252ced74b0a191e'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
