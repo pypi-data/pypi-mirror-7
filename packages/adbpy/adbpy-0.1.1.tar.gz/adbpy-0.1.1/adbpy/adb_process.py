6b1ecd3357f3858ea3e0865947211fdb8228c48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package adbpy/0.1.1/adbpy-0.1.1.tar.gz

---
+++ packages/adbpy/adbpy-0.1.1.tar.gz/adbpy-0.1.1/adbpy/adb_process.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import os
+import subprocess
+import socket
+
+from adbpy.socket import Socket
+
+class AdbProcess(object):
+
+    def __init__(self, path, address):
+        self.path = path
+        self.address = address
+
+    def start(self):
+        null = open(os.devnull, 'w')
+        cmd = "{0} -P {1} start-server".format(self.path, self.address[1])
+        return subprocess.call(cmd, shell=True, stdout=null) == 0
+
+    def running(self):
+        sock = Socket(self.address)
+        try:
+            sock.connect()
+        except socket.error:
+            return False
+
+        return True
