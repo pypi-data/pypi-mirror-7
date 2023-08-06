06c8d23c766480e113aa48eada7243028c2da61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package BYONDTools/0.1.1b/BYONDTools-0.1.1b.zip

---
+++ packages/BYONDTools/BYONDTools-0.1.1b.zip/BYONDTools-0.1.1b/CHANGELOG.rst
@@ -0,0 +1,13 @@
+=================
+0.1.1b - 6/2/2014
+=================
+
+* Fixed some leftover packaging issues:
+
+ * post-install didn't create self-executables on Linux, so just calling dmi, dmmfix, etc. wouldn't work.
+ * setup.py didn't specify sub-packages for installation, so installations were fairly broken.
+ 
+* Post-install process now uses sys.executable, rather than trying to figure it out via hackier means.
+* README changed to reStructuredText for eventual pip release.
+* Oh my god there's a changelog now
+* Added MANIFEST.in (fixed a problem uploading *.rst files).
---
+++ packages/BYONDTools/BYONDTools-0.1.1b.zip/BYONDTools-0.1.1b/scripts/byondtools-postinstall.py
@@ -0,0 +1,58 @@
+#------------------------------------------------------------------------------
+# Originally cxfreeze-postinstall
+#   Script run after installation on Windows to fix up the Python location in
+# the script as well as create batch files.
+#------------------------------------------------------------------------------
+
+import distutils.sysconfig
+import glob, os, sys
+
+vars = distutils.sysconfig.get_config_vars()
+prefix = vars["prefix"]
+python = sys.executable #os.path.join(prefix, "python.exe")
+scriptDir = os.path.join(prefix, "Scripts")
+
+# Keep in sync with setup.py.
+scripts = [
+    'dmm',
+    'dmi',
+    'dmindent',
+    'dmmrender',
+    'dmmfix',
+    
+    #TODO: Combine into dmi.
+    'ss13_makeinhands',
+    
+    # Our post-install.  Now run on Linux, as well.
+    "byondtools-postinstall"
+]
+
+for fileName in glob.glob(os.path.join(scriptDir, "*.py")):
+    # skip already created batch files if they exist
+    name, ext = os.path.splitext(os.path.basename(fileName))
+    if name not in scripts or name == 'byondtools-postinstall':
+        continue
+
+    print('Running post-install for {}.'.format(name))
+    # copy the file with the first line replaced with the correct python
+    fullName = os.path.join(scriptDir, fileName)
+    strippedName = os.path.join(scriptDir, name)
+    lines = open(fullName).readlines()
+    startidx=1
+    if not lines[0].strip().startswith('#!'):
+        print('WARNING: {} does not have a shebang.'.format(name))
+        startidx=0
+    
+    targetFile = strippedName
+    if sys.platform == 'win32':
+        targetFile = fullName
+    with open(targetFile, "w") as outFile:
+        outFile.write("#!{}\n".format(python)) # Not sure why this is done on Windows...
+        outFile.writelines(lines[startidx:])
+
+    if sys.platform == 'win32':
+        # create the batch file
+        batchFileName = strippedName + ".bat"
+        command = "{} {} %*".format(python, targetFile)
+        open(batchFileName, "w").write("@echo off\n\n{}".format(command))
+
