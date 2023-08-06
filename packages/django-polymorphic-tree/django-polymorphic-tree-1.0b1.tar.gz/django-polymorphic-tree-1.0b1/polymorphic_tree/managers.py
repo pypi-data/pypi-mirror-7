010394deee9ec544f7ae6de57aba80c44aa0651c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:44 +0000

    Add package django-polymorphic-tree/1.0b1/django-polymorphic-tree-1.0b1.tar.gz

---
+++ packages/django-polymorphic-tree/django-polymorphic-tree-1.0b1.tar.gz/django-polymorphic-tree-1.0b1/polymorphic_tree/managers.py
@@ -0,0 +1,32 @@
+"""
+The manager class for the CMS models
+"""
+from mptt.managers import TreeManager
+from polymorphic import PolymorphicManager
+from polymorphic.query import PolymorphicQuerySet
+
+
+class PolymorphicMPTTQuerySet(PolymorphicQuerySet):
+    """
+    Base class for querysets
+    """
+    def toplevel(self):
+        """
+        Return all nodes which have no parent.
+        """
+        return self.filter(parent__isnull=True)
+
+
+class PolymorphicMPTTModelManager(TreeManager, PolymorphicManager):
+    """
+    Base class for a model manager.
+    """
+    #: The queryset class to use.
+    queryset_class = PolymorphicMPTTQuerySet
+
+
+    def toplevel(self):
+        """
+        Return all nodes which have no parent.
+        """
+        return self.get_query_set().toplevel()
