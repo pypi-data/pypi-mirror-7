e0b363c7cf52659c7c23d77e7a83eb3467486dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:44 +0000

    Add package autowrap/0.5.0/autowrap-0.5.0.tar.gz

---
+++ packages/autowrap/autowrap-0.5.0.tar.gz/autowrap-0.5.0/setup.py
@@ -0,0 +1,47 @@
+# the following  import fixes/avoids some nostests issue
+# see
+
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+# the follwing is standard setup.py for a single package 'autowrap'
+
+from setuptools import setup, find_packages
+
+import autowrap
+
+version = '%s.%s.%s' % autowrap.version
+
+setup(name="autowrap",
+      version=version,
+      maintainer="Uwe Schmitt",
+      maintainer_email="uschmitt@mineway.de",
+      license="http://opensource.org/licenses/BSD-3-Clause",
+      platforms=["any"],
+      description='Generates Python Extension modules from commented Cython PXD files',
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Programming Language :: Python :: 2.7",
+                   "Topic :: Software Development :: Code Generators",
+                   ],
+      long_description="""
+autowrap automatically generates python extension modules for wrapping
+C++ libraries based on annotated (commented) cython pxd files. """,
+
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,   # see MANIFEST.in
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=['Cython>=0.18'],
+      entry_points={
+           'console_scripts':
+           [
+               'autowrap = autowrap.Main:main',
+           ]
+      },
+
+      )
