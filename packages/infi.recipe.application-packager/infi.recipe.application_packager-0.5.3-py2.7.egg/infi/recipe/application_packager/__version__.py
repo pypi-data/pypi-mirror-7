b280796e2cf1a324981c0ddfc52ef04ffa21996b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:43 +0000

    Add package infi.recipe.application-packager/0.5.3/infi.recipe.application_packager-0.5.3-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.3-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.3"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '9aa8cf1a97ca1b84dee36c49773406d1ddd13545'
+__git_head_subject__ = 'TRIVIAL more env vars for pystick'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
