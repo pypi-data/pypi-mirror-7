3fb5014f8ca090d6f116cacd1f28cb97b20c385b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package infi.eventlog/0.1.8/infi.eventlog-0.1.8.tar.gz

---
+++ packages/infi.eventlog/infi.eventlog-0.1.8.tar.gz/infi.eventlog-0.1.8/src/infi/eventlog/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.8"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '17e3020c2d762be86c687c0034ae4984820dc8f3'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
