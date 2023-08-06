af1bbaee181ec4b9255c7e9de0cf7ed1fbe587a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:01 +0000

    Add package slpkg/1.6.7/slpkg-1.6.7.tar.gz

---
+++ packages/slpkg/slpkg-1.6.7.tar.gz/slpkg-1.6.7/slpkg/__metadata__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+__all__ = "slpkg"
+__author__ = "dslackw"
+__version_info__ = (1, 6, 7)
+__version__ = "{0}.{1}.{2}".format(*__version_info__)
+__license__ = "GNU General Public License v3 (GPLv3)"
+__email__ = "d.zlatanidis@gmail.com"
+
+''' file spacer '''
+sp = "-"
+
+''' temponary path '''
+tmp = "/tmp/"
+slpkg_path = tmp + "slpkg/"
+
+''' packages log files path '''
+pkg_path = "/var/log/packages/"
+
+''' computer architecture '''
+uname = os.uname()
+arch = (uname[4])
+
+''' slackbuild fietype binary packages '''
+sbo_arch = "*"
+sbo_tag = "?_SBo"
+sbo_filetype = ".tgz"
---
+++ packages/slpkg/slpkg-1.6.7.tar.gz/slpkg-1.6.7/slpkg/colors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class colors:
+    '''
+    Print foreground colors class
+    '''
+    RED = "\x1b[31m"
+    GREEN = "\x1b[32m"
+    YELLOW = "\x1b[33m"
+    CYAN = "\x1b[36m"
+    WHEAT = "\x1b[38;5;101m"
+    ENDC = "\x1b[0m"
