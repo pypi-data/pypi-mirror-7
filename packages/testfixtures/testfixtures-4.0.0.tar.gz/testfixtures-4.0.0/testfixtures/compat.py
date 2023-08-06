c3d4d29938c1d8ccb3792e4f4d1c5a1357ced758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:23 +0000

    Add package testfixtures/4.0.0/testfixtures-4.0.0.tar.gz

---
+++ packages/testfixtures/testfixtures-4.0.0.tar.gz/testfixtures-4.0.0/testfixtures/compat.py
@@ -0,0 +1,48 @@
+# compatibility module for different python versions
+import sys
+
+if sys.version_info[:2] > (3, 0):
+
+    PY2 = False
+    PY3 = True
+
+    Bytes = bytes
+    Unicode = str
+    basestring = str
+
+    class_type_name = 'class'
+    ClassType = type
+    exception_module = 'builtins'
+    new_class = type
+    self_name = '__self__'
+    from io import StringIO
+    xrange = range
+    
+else:
+    
+    PY2 = True
+    PY3 = False
+    
+    Bytes = str
+    Unicode = unicode
+    basestring = basestring
+    
+    class_type_name = 'type'
+    from types import ClassType
+    exception_module = 'exceptions'
+    from new import classobj as new_class
+    self_name = 'im_self'
+    from cStringIO import StringIO
+    xrange = xrange
+
+try:
+    from mock import call as mock_call
+except ImportError: # pragma: no cover
+    class MockCall: pass
+    mock_call = MockCall()
+
+try:
+    from unittest.mock import call as unittest_mock_call
+except ImportError: 
+    class UnittestMockCall: pass
+    unittest_mock_call = UnittestMockCall()
