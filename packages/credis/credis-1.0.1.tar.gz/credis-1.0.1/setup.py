465bbed0e80116cc2681e82b2b0a1dc36fd7117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:00 +0000

    Add package credis/1.0.1/credis-1.0.1.tar.gz

---
+++ packages/credis/credis-1.0.1.tar.gz/credis-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from distutils.extension import Extension
+
+ext_modules = [ Extension("credis.base", ["credis/base.c"])
+              , Extension("credis.geventpool", ["credis/geventpool.c"])
+              ]
+
+setup(
+    name = 'credis',
+    version = '1.0.1',
+    packages=['credis'],
+    ext_modules = ext_modules,
+    author='huangyi',
+    author_email='yi.codeplayer@gmail.com',
+    url = 'https://github.com/yihuang/credis',
+)
