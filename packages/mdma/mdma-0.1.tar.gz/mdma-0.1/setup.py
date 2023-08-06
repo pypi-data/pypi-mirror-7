f7aa5ad74cb4bcc8e1d250e18403f29f44e93d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package mdma/0.1/mdma-0.1.tar.gz

---
+++ packages/mdma/mdma-0.1.tar.gz/mdma-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    # Metadata
+    name="mdma",
+    version="0.1",
+    description="ipython notebook markdown magic",
+    url="",
+    author="Erik Gafni",
+    author_email="egafni@gmail.com",
+    maintainer="Erik Gafni",
+    maintainer_email="egafni@gmail.com",
+    license="GPLv2",
+    install_requires=["markdown", ],
+    # Packaging Instructions
+    packages=find_packages(),
+    include_package_data=True
+)
