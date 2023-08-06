b1272b84646ba50a096455d5100fe32348ce916e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package fakeemail/0.8/fakeemail-0.8.tar.gz

---
+++ packages/fakeemail/fakeemail-0.8.tar.gz/fakeemail-0.8/README.rst
@@ -0,0 +1,76 @@
+=========
+FakeEmail
+=========
+
+FakeEmail is an email server, for use in a development environment where routing email is either undesirable, or impossible to manage.
+
+The aim of the system is to open a port, accept SMTP connections and to display a list of the emails that it has received. 
+At no point will the emails route further on, so there is no requirement for the addresses to be valid, or to wait around waiting for the email to arrive in your inbox.
+
+Installation
+------------
+
+Via pip
+~~~~~~~
+
+FakeEmail is installable via pip::
+
+  pip install fakeemail
+
+
+From Source
+~~~~~~~~~~~
+
+FakeEmail is installed using the buildout system and depends on Twisted for it's server capabilites. Installation is as straight forward as possible::
+
+  git clone git://github.com/tomwardill/FakeEmail.git
+  cd FakeEmail
+  python bootstrap.py
+  bin/buildout
+  
+  
+Usage
+-----
+
+From pip install
+~~~~~~~~~~~~~~~~
+
+If you installed fakeemail via pip, then running is simple::
+
+  fakeemail 2025 8080 0.0.0.0
+
+
+This will run on SMTP port `2025`, the web interface on `8080` and listen on all network interfaces. Running on port 25 is possible, but you will need to start with root privileges.
+
+Installed from source
+~~~~~~~~~~~~~~~~~~~~~
+
+FakeEmail installs itself as a plugin into the local buildout twisted environment. This makes execution reasonably simple::
+
+  bin/twistd -n fakeemail
+  
+There are two options for the system:
+
+ * -s or --smtp_port : Which port to run the SMTP listener on (defaults to 2025)
+ * -w or --web_port : Which port to run the Web Server on (defaults to 8000)
+ 
+Use these options as shown below::
+
+  bin/twistd -n fakeemail -s 1025 -w 8001
+  
+Note that to run the SMTP listener on port 25 (the standard SMTP port), you may need to start the daemon with root privileges.
+
+Once the server is running, direct any email at the port that you specified, and then load up http://<servername>:webport (http://localhost:8000 for example)
+
+Run the server in the background and output to a log file::
+
+  bin/twistd -l foo.log fakeemail
+
+Dependencies
+------------
+
+The dependencies will be installed through the egg/buildout, but if you require them to be system installed, they are listed below:
+
+ * twisted
+ * jinja2
+ * python (2.4+)
---
+++ packages/fakeemail/fakeemail-0.8.tar.gz/fakeemail-0.8/fakeemail/web_server.py
@@ -0,0 +1,145 @@
+import email
+import base64
+
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.application import internet, service
+
+from jinja2 import Environment, PackageLoader
+
+try:
+    import json
+except:
+        import simplejson as json
+
+class WebMessageDisplay(Resource):
+
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.name = name
+        self.storage = storage
+
+    def render_GET(self, request):
+        email_list = self.storage.get_for_name(self.name)
+        decoded_email_list = []
+        if email_list:
+            for mail in email_list:
+                parts = {}
+                msg = email.message_from_string(mail.encode())
+                parts['headers'] = msg.items()
+                parts['content_type'] = msg.get_content_type()
+                for part in msg.walk():
+                    # multipart/* are just containers
+                    if part.get_content_maintype() == 'multipart':
+                        continue
+
+                    payload = part.get_payload(decode=True)
+                    if not isinstance(payload, unicode):
+                        payload = unicode(payload, 'utf-8')
+
+                    parts['payload'] = payload
+                decoded_email_list.append(parts)
+
+        data = {'email_list' : decoded_email_list,
+                'address_name' : self.name,
+                }
+        
+        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        template = env.get_template('message_display.html')
+        
+        return template.render(data).encode('UTF-8')
+        
+class WebMessageRootDisplay(Resource):
+    
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.storage = storage
+        
+    def internal_render(self, user_message = None):
+        address_list = [ [self.storage.get_count(name), name] for name in self.storage.get_all_names()]
+        data = {'address_list': address_list, 'user_message': user_message}
+        
+        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        template = env.get_template('home_page.html')
+        
+        return template.render(data).encode('utf-8')
+        
+    def render_GET(self, request, user_message = None):
+        
+        return self.internal_render()
+        
+    
+    def render_POST(self, request):
+        self.storage.clear_all()
+        return self.internal_render('Data Cleared')
+    
+class WebDataMessageDisplay(Resource):
+
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.name = name
+        self.storage = storage
+
+    def render_GET(self, request):
+        
+        data = {'email_list' : self.storage.get_for_name(self.name),
+                'address_name' : self.name,
+                }
+        
+        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        template = env.get_template('data_message_display.html')
+        
+        data_dump = {'data': json.dumps(data)}
+        
+        return template.render(data_dump).encode('utf-8')
+    
+    
+class WebDataRootDisplay(Resource):
+    
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.storage = storage
+        
+    def internal_render(self, user_message = ''):
+        address_list = [ [self.storage.get_count(name), name] for name in self.storage.get_all_names()]
+        data = {'address_list': address_list, 'user_message': user_message}
+        
+        env = Environment(loader = PackageLoader('fakeemail', 'templates'))
+        template = env.get_template('data_home_page.html')
+        
+        data_dump = {'data':json.dumps(data)}
+        
+        return template.render(data_dump).encode('utf-8')
+        
+    def render_GET(self, request, user_message = None):
+        
+        return self.internal_render()
+    
+    def render_POST(self, request):
+        self.storage.clear_all()
+        return self.internal_render('Data Cleared')
+    
+    def getChild(self, name, request):
+        return WebDataMessageDisplay(name, self.storage)
+    
+class WebMessageRouter(Resource):
+
+    def __init__(self, storage):
+        Resource.__init__(self)
+        self.storage = storage
+
+    def getChild(self, name, request):
+        if name:
+            if name == 'data':
+                return WebDataRootDisplay('', self.storage)
+            
+            return WebMessageDisplay(name, self.storage)
+        else:
+            return WebMessageRootDisplay(name, self.storage)
+        
+def makeWebService(web_interface, web_port, storage):
+    root = WebMessageRouter(storage)
+    siteFactory = Site(root)
+    webService = internet.TCPServer(web_port, siteFactory,
+                                    interface=web_interface)
+    return webService
