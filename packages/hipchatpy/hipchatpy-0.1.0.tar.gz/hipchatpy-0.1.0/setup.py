d2bdcf83b2ad111b360a2637aee337b883f3e8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:43 +0000

    Add package hipchatpy/0.1.0/hipchatpy-0.1.0.tar.gz

---
+++ packages/hipchatpy/hipchatpy-0.1.0.tar.gz/hipchatpy-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+__author__ = 'Takahiro Ikeuchi'
+
+setup(
+    name="hipchatpy",
+    version="0.1.0",
+    packages=['hipchatpy'],
+    install_requires=open('requirements.txt').read().splitlines(),
+    description="Simple HipChat client library",
+    long_description=open('README.txt').read(),
+    author='Takahiro Ikeuchi',
+    author_email='takahiro.ikeuchi@gmail.com',
+    url='https://github.com/iktakahiro/hipchatpy',
+    keywords=["HipChat", "HipChat Client"],
+    license='MIT',
+    classifies = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: System :: Logging",
+        "Topic :: Communications :: Chat"
+    ],
+    entry_points={
+        "console_scripts": [
+            "hipchatpy=hipchatpy:main",
+        ],
+    },
+)
+
