4e7d0eb4f08dae8dc0c2867345a77a7354f40168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:31 +0000

    Add package mygene/2.2.0/mygene-2.2.0.tar.gz

---
+++ packages/mygene/mygene-2.2.0.tar.gz/mygene-2.2.0/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="mygene",
+    version="2.2.0",
+    author="Chunlei Wu",
+    author_email="cwu@scripps.edu",
+    description="Python Client for MyGene.Info services.",
+    license="BSD",
+    keywords="biology gene annotation web service client api",
+    url="https://bitbucket.org/newgene/mygene",
+    packages=['mygene'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'httplib2>=0.6',
+    ],
+)
