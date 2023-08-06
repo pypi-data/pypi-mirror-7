598f560956129eb1c2d2772c133d0b8b346f540d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package savingzelda/0.3/savingzelda-0.3.tar.gz

---
+++ packages/savingzelda/savingzelda-0.3.tar.gz/savingzelda-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='savingzelda',
+    description="Check if all your webpage's links are ok. Because you can't save the day with a dead Link.",
+    long_description="Check if all your webpage's links are ok. Because you can't save the day with a dead Link.",
+    author='Renata Carreira',
+    author_email='contato@renatacarreira.com',
+    url='https://github.com/recarreira/saving-zelda',
+    version='0.3',
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
+        'console_scripts':
+        ['savingzelda = savingzelda.cli:main'],
+        },
+    packages=[
+        'savingzelda',
+        ],
+    install_requires=[
+        'beautifulsoup4==4.3.2',
+		'requests==2.2.1',
+        ],
+)
