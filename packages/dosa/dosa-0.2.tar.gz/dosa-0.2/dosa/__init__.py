9d87433ec72bfa32ebbae00d2874307715875acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:01 +0000

    Add package dosa/0.2/dosa-0.2.tar.gz

---
+++ packages/dosa/dosa-0.2.tar.gz/dosa-0.2/dosa/__init__.py
@@ -0,0 +1,77 @@
+import logging
+import requests
+
+API_VERSION = 'v2'
+__version__ = '0.2'
+DEBUG = False
+
+
+def show_debug_hints(req_type, endpoint, data, headers, resp):
+    logging.debug('http status code: %s' % resp.status_code)
+    logging.debug('response body: %s' % resp.text)
+    headers_s = ''.join(' -H ' + '%s:%s' % (k, v) for (k, v) in headers.items())
+    curl_cmd = 'curl -X %s %s -d %s %s' % (req_type, endpoint, data, headers_s)
+    logging.debug(curl_cmd)
+
+
+class APIObject(object):
+
+    def __init__(self, api_key, path, **kw):
+        self.api_key = api_key
+        self.path = path.format(kw)
+        for (k, v) in kw.items():
+            setattr(self, k, v)
+
+    def send_req(self, req_type, path, data={}):
+        req_calls = {'GET': requests.get, 'POST': requests.post, 'DELETE': requests.delete}
+        headers = {'authorization': 'Bearer %s' % self.api_key, 'content_type': 'application/json'}
+        endpoint = 'https://api.digitalocean.com/%s/%s' % (API_VERSION, path)
+        req_call = req_calls[req_type]
+        resp = req_call(endpoint, params=data, headers=headers)
+        status_code = resp.status_code
+        failed = False
+        if req_type == 'DELETE':
+            ret = None
+            if status_code not in (200, 204):
+                failed = True
+        else:
+            ret = resp.json()
+            if status_code not in (200, 202):
+                failed = True
+        if failed:
+            show_debug_hints(req_type, endpoint, data, headers, resp)
+            raise Exception(resp)
+        return status_code, ret
+
+
+class Resource(APIObject):
+
+    def get(self):
+        return self.send_req('GET', self.id)
+
+
+class Collection(APIObject):
+
+    def list(self):
+        return self.send_req('GET', self.path)
+
+
+class Droplets(Collection):
+
+    def create(self, name, region, size, image, ssh_keys=None, backups=False, ipv6=False, private_networking=False):
+        data = dict(name=name, region=region, size=size, image=image,  ssh_keys=ssh_keys, backups=backups, private_networking=private_networking)
+        return self.send_req('POST', self.path, data)
+
+    def delete(self, id):
+        path = self.path + '/' + str(id)
+        return self.send_req('DELETE', path)
+
+
+class Client(object):
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.droplets = Droplets(self.api_key, 'droplets')
+        self.images = Collection(self.api_key, 'images')
+
+    def Droplet(self, id):
+        return Resource(self.api_key, 'droplets/{id}', id=id)
