e203b99999b056039c7dbaabd592f9f129358ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:46 +0000

    Add package pyregdom/0.2/pyregdom-0.2.tar.gz

---
+++ packages/pyregdom/pyregdom-0.2.tar.gz/pyregdom-0.2/regdom/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'regdom'
+__version__ = '0.2'
+__build__ = 0x03
+__author__ = 'Huy Phan'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2014 Huy Phan'
+
+from .reg_domain import get_registered_domain
