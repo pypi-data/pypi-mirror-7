c2b3e76cc1b6a8ee0588add77c6bc0aafd7aa145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:17 +0000

    Add package encode-dataframe/0.1/encode-dataframe-0.1.tar.gz

---
+++ packages/encode-dataframe/encode-dataframe-0.1.tar.gz/encode-dataframe-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='encode-dataframe',
+    version='0.1',
+    description="Convert UCSC's ENCODE metadata into pandas DataFrames",
+    long_description=long_description,
+    author='Ryan Dale',
+    author_email='dalerr@niddk.nih.gov',
+    license='MIT',
+    install_requires=['pandas'],
+    packages=find_packages(),
+)
