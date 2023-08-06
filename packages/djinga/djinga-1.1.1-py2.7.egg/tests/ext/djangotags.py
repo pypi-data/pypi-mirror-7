0fd5c5d0cb7f96afc9d30981b359c78ca232fff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:38 +0000

    Add package djinga/1.1.1/djinga-1.1.1-py2.7.egg

---
+++ packages/djinga/djinga-1.1.1-py2.7.egg/tests/ext/djangotags.py
@@ -0,0 +1,38 @@
+from django.test import RequestFactory
+from django.template.defaulttags import CsrfTokenNode
+
+from jinja2 import Markup
+
+from tests.base import TestCase
+
+
+request_factory = RequestFactory()
+
+
+class DjangoTagsTests(TestCase):
+
+    extensions = ('djinga.ext.django',
+                  'djinga.ext.csrf_token')
+
+    def test_django_tag(self):
+        # using django specific template tags, without spaces nor linebreaks
+        self.template = \
+            '{% django %}' \
+            '{% load i18n %}' \
+            '{% blocktrans %}To be translated{% endblocktrans %}' \
+            '{% enddjango %}'
+        self.assertRender('To be translated',
+                          {'request': request_factory.get('/')})
+
+    def test_django_tag_file(self):
+        # using django specific template tags, without spaces nor linebreaks
+        self.template_file = 'djangotag/jinja.jjhtml'
+        self.assertRender('\n\nTo be translated\n',
+                          {'request': request_factory.get('/')})
+
+    def test_csrf_token(self):
+        self.template = '{% csrf_token %}'
+        csrf_token = 'CSRFTOKEN'
+        self.assertRender(
+            Markup(CsrfTokenNode().render({'csrf_token': csrf_token})),
+            {'csrf_token': csrf_token})
---
+++ packages/djinga/djinga-1.1.1-py2.7.egg/tests/ext/htmlcompress.py
@@ -0,0 +1,70 @@
+from tests.base import TestCase
+
+
+class HTMLCompressTests(TestCase):
+
+    extensions = ('djinga.ext.htmlcompress.HTMLCompress',)
+
+    def test_always_compress(self):
+        self.template = '''
+            <html>
+              <head>
+                <title>{{ title }}</title>
+              </head>
+              <script type=text/javascript>
+                if (foo < 42) {
+                  document.write('Foo < Bar');
+                }
+              </script>
+              <body>
+                <li><a href="{{ href }}">{{ title }}</a><br>Test  Foo
+                <li><a href="{{ href }}">{{ title }}</a><img src=test.png>
+              </body>
+            </html>
+        '''
+
+        expected = \
+            '''<html><head><title>42</title></head><script type=text/javascript>
+                if (foo < 42) {
+                  document.write('Foo < Bar');
+                }
+              </script><body><li><a href="index.html">42</a><br>Test  Foo<li><a href="index.html">42</a><img src=test.png></body></html>'''
+        self.assertRender(expected, dict(title=42, href='index.html'))
+
+    def test_leave_spaces_between_vars(self):
+        self.template = '''
+        {%if True %}
+            {{ two }} {{ variables }} in the middle
+            of the text
+        {% endif %}
+        '''
+        expected = 'Two variables in the middle of the text'
+        self.assertRender(expected, dict(two='Two', variables='variables'))
+
+
+class SelectiveHTMLCompressTests(TestCase):
+
+    extensions = ('djinga.ext.htmlcompress.SelectiveHTMLCompress',)
+
+    def test_selective_compress(self):
+        self.template = '''
+            Normal   <span>  unchanged </span> stuff
+            {% strip %}Stripped <span class=foo  >   test   </span>
+            <a href="foo">  test </a> {{ foo }}
+            Normal <stuff>   again {{ foo }}  </stuff>
+            <p>
+              Foo<br>Bar
+              Baz
+            <p>
+              Moep    <span>Test</span>    Moep
+            </p>
+            {% endstrip %}
+        '''
+
+        expected = u'''
+            Normal   <span>  unchanged </span> stuff
+            Stripped <span class=foo  >   test   </span><a href="foo">  test </a> 42Normal <stuff>   again 42  </stuff><p>Foo<br>Bar Baz<p>Moep    <span>Test</span>    Moep</p>
+        '''
+
+        self.maxDiff = None
+        self.assertRender(expected, dict(foo=42))
