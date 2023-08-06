eadec7cbb2e4f370d345fb664a82a06022883c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package crawl-me/0.1.9dev-20140619-1/crawl-me-0.1.9dev-20140619-1.tar.gz

---
+++ packages/crawl-me/crawl-me-0.1.9dev-20140619-1.tar.gz/crawl-me-0.1.9dev-20140619-1/README.md
@@ -0,0 +1,152 @@
+crawl-me [![PyPI version](https://badge.fury.io/py/crawl-me.png)](http://badge.fury.io/py/crawl-me)
+========
+
+[中文README](https://github.com/nyankosama/crawl-me/wiki/%E4%B8%AD%E6%96%87README).
+
+Crawl-me is a light-weight fast plugin based web picture crawler. You can download your favorite pictures via the plugin if the website is supported. For now, the plugins include gamersky and pixiv. If you want to contribute, please just feel free to contact with me.
+
+Fork me on Github :) [https://github.com/nyankosama/crawl-me](https://github.com/nyankosama/crawl-me)
+
+Features
+=======
+
+- Crawl-me core supports muti-thread downloading using http range-headers, so it's very fast.
+- It's plugin based, so you can free add any plugin you want. 
+
+
+Available plugins
+============
+
+- pixiv : This plugin allows you to download any author's paintings in [pixiv](http://www.pixiv.net/) site.
+- gamersky : This plugin supports downloading all pictures in special topic from [gamersky](http://www.gamersky.com/) site.
+
+
+Installation
+========
+
+## install via pip
+
+Make sure you have already installed [python2.7](https://www.python.org/downloads/) and [pip](https://pypi.python.org/pypi/pip/1.5.6).
+
+Due to the fact that package relies on lxml, if your platform is linux, please make sure you have installed lib libxslt-devel libxml2-devel. And for windows please select a suitable [lxml installer](https://pypi.python.org/pypi/lxml/3.3.5#downloads) to install.
+
+And then:
+
+    $ pip install crawl-me
+
+For windows, please add {$python-home}/Scripts/ to systempath
+
+## install via git
+
+### 1. Ubuntu
+
+Install the prerequisite library first:
+    
+    sudo apt-get install libxml2-dev
+    sudo apt-get install libxslt1-dev 
+    
+And then you should install [setuptools](https://pypi.python.org/pypi/setuptools#downloads "setuptools") in order to run the setup.py file
+
+    sudo apt-get install python-setuptools
+
+Finally, git clone the source, and install:
+
+    $ git clone https://github.com/nyankosama/crawl-me.git
+    $ cd crawl-me/
+    $ sudo python setup.py install
+
+### 2. Windows
+
+Make sure you have already installed [python2.7](https://www.python.org/downloads/) and [pip](https://pypi.python.org/pypi/pip/1.5.6)
+
+You can install python2.7 via windows installer. You can install pip via downloading the [get-pip.py](https://bootstrap.pypa.io/get-pip.py), and run it via python:
+
+    python get-pip.py
+
+And then install the prerequisite library lxml. please select a suitable [lxml installer](https://pypi.python.org/pypi/lxml/3.3.5#downloads) to install.
+
+Finally git clone the source, and install:
+    
+    $ git clone https://github.com/nyankosama/crawl-me.git
+    $ cd crawl-me/
+    $ sudo python setup.py install
+
+For windows, please add {$python-home}/Scripts/ to systempath
+
+Usage
+========
+
+## Examples
+
+1. Download 10 pages pictures at the url of http://www.gamersky.com/ent/201404/352055.shtml in gamersky site, and store the pictures into local direcotry.
+
+        crawl-me gamersky http://www.gamersky.com/ent/201404/352055.shtml ./gamersky-crawl 1 10
+
+2. Download all the paintings of 藤原(Fujiwara, Pixiv ID=27517), and store them into local directory. 
+        
+        crawl-me pixiv 27517 ./pixiv-crawl <your pixiv loginid> <your password>
+
+## Command line options
+
+1. general help
+
+        $ crawl-me -h    
+    
+        usage: crawl-me [-h] plugin
+
+        positional arguments:
+            plugin      plugin the crawler uses
+        
+        optional arguments:
+            -h, --help  show this help message and exit
+    
+        available plugins:
+        ----gamersky
+        ----pixiv
+
+2. gamersky
+
+        $ crawl-me gamersky -h
+        
+        usage: crawl-me [-h] plugin url savePath beginPage endPage
+
+        positional arguments:
+            plugin      plugin the crawler uses
+            url         your url to crawl
+            savePath    the path where the imgs ars saved
+            beginPage   the page where we start crawling
+            endPage     the page where we end crawling 
+        
+        optional arguments:
+            -h, --help  show this help message and exit
+
+3. pixiv
+        
+        $ crawl-me pixiv -h
+
+        usage: crawl-me [-h] plugin authorId savePath pixivId password
+
+        positional arguments:
+            plugin      plugin the crawler uses
+            authorId    the author id you want to crawl
+            savePath    the path where the imgs ars saved
+            pixivId     your pixiv login id
+            password    your pixiv login password
+        
+        optional arguments:
+            -h, --help  show this help message and exit
+
+TODO
+========
+
+- Functions:
+    - support breakpoint resume
+
+- Plugins:
+    - weibo
+    - qq zone
+
+Licenses
+========
+
+[MIT](http://opensource.org/licenses/MIT "MIT")
---
+++ packages/crawl-me/crawl-me-0.1.9dev-20140619-1.tar.gz/crawl-me-0.1.9dev-20140619-1/crawl_me/common/utils.py
@@ -0,0 +1,169 @@
+import sys
+import socket
+import os
+import threading
+import urllib2
+import cookielib
+import copy
+from ..sysconf import *
+
+
+def urlopenWithRetry(opener, request, timeout=URL_OPEN_TIMEOUT, retryTime=URL_OPEN_RETRY_TIME):
+    if isinstance(request, urllib2.Request):
+        url = request.get_full_url()
+    else:
+        url = request
+
+    while retryTime >= 0:
+        try:
+            return opener.open(request, timeout=timeout)
+        except socket.timeout, e1:
+            retryTime -= 1
+            # syslog("http timeout, retry again.(retry remains %s) url=%s" % (retryTime, url), LOG_ERROR)
+            continue
+        except urllib2.HTTPError, error:
+            if error.code == 404:
+                syslog("http error 404 not found, url=%s" % (url), LOG_ERROR)
+                return None
+        except Exception, e2:
+            syslog(str(e2) + " (retry remains %s)" % (retryTime) + ", at url=" + url, LOG_ERROR)
+            retryTime -= 1
+            continue
+    return None
+
+
+def resReadWithRetry(opener, request, response, timeout=URL_OPEN_TIMEOUT, retryTime=URL_OPEN_RETRY_TIME):
+    if isinstance(request, urllib2.Request):
+        url = request.get_full_url()
+    else:
+        url = request
+    needRetry = False
+    if response == None:
+        return None
+    while retryTime >= 0:
+        try:
+            if needRetry == False:
+                return response.read()
+            else:
+                return opener.open(request, timeout=timeout).read()
+        except socket.timeout, e1:
+            retryTime -= 1
+            # syslog("response read http timeout, retry again.(retry remains %s), url=%s" % (retryTime, url), LOG_ERROR)
+            needRetry = True
+            continue
+        except Exception, e2:
+            syslog(str(e2) + " (retry remains %s)" % (retryTime) + ", at url=" + url, LOG_ERROR)
+            retryTime -= 1
+            needRetry = True
+            continue
+    return None
+
+def urlReadWithRetry(opener, request, timeout=URL_OPEN_TIMEOUT, retryTime=URL_OPEN_RETRY_TIME):
+    return resReadWithRetry(opener, request, urlopenWithRetry(opener, request, timeout, retryTime), timeout, retryTime)
+
+def checkRangeHeaderSupport(opener, url):
+    retryTime = 3
+    while retryTime >= 0:
+        request = urllib2.Request(url)
+        request.headers['Range'] = 'bytes=%s-' % (0)
+        res = urlopenWithRetry(opener, request)
+        if res == None:
+            syslog("check range headers support fail! url=%s" % (url), LOG_ERROR)
+            return None
+        if res.info().get("Content-Range") != None:
+            return True
+        else:
+            return False
+
+def upDiv(a, b):
+    return (a + b - 1) / b
+
+
+def getShardingConf(size, maxThreadCount=MAX_THREAD_COUNT, perferedSplitNum=SPLIT_NUM):
+    tmpCount = upDiv(size, perferedSplitNum)
+    if tmpCount > maxThreadCount:
+        tmpCount = maxThreadCount
+
+    splitNum = upDiv(size, tmpCount)
+    downloadCount = tmpCount
+    return [splitNum, downloadCount]
+
+
+def getPathWithSep(path):
+    if path[len(path) - 1] != os.sep:
+        return path + os.sep
+    else:
+        return path
+
+
+def createDir(savePath):
+    # create dir
+    if os.path.isdir(savePath) is not True:
+        os.makedirs(savePath)
+
+
+# following from Python cookbook, #475186
+def has_colours(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        return False  # auto color only on TTYs
+    try:
+        import curses
+
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # guess false in case of error
+        return False
+
+
+has_colours = has_colours(sys.stdout)
+
+#constant
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR = range(4)
+level_str = ["DEBUG", "INFO", "WARNING", "ERROR"]
+
+
+def getLogColour(level):
+    color_range = [WHITE, GREEN, YELLOW, RED]
+    return color_range[level]
+
+
+syslogLock = threading.Lock()
+
+
+def syslog(text, level=LOG_INFO):
+    syslogLock.acquire()
+    if has_colours:
+        prefix = "\x1b[1;%dm" % (30 + getLogColour(level)) + "[%s]: " % (level_str[level]) + "\x1b[0m"
+        sys.stdout.write(prefix)
+        sys.stdout.write(text + "\n")
+    else:
+        prefix = "[%s]: " % (level_str[level])
+        sys.stdout.write(prefix)
+        sys.stdout.write(text + "\n")
+    syslogLock.release()
+
+
+def dynamicImport(name):
+    mod = __import__(name)
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
+#default only copy opener's headers and cookiejar'
+def openerDefaultClone(oldOpener, oldCookieJar=None):
+    for handler in oldOpener.handlers:
+        if isinstance(handler, urllib2.HTTPCookieProcessor):
+            oldCookieJar = handler.cookiejar
+    newCookieJar = cookielib.CookieJar()
+    if oldCookieJar != None:
+        newCookieJar._cookies = copy.deepcopy(oldCookieJar._cookies)
+        newCookieJar._policy = copy.deepcopy(oldCookieJar._policy)
+    newOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(newCookieJar))
+    newOpener.addheaders = copy.deepcopy(oldOpener.addheaders)
+    return newOpener
