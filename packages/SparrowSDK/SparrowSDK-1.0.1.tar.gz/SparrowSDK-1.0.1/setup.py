b36d007aa29b43a5bf7de272be439940d06184c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:55 +0000

    Add package SparrowSDK/1.0.1/SparrowSDK-1.0.1.tar.gz

---
+++ packages/SparrowSDK/SparrowSDK-1.0.1.tar.gz/SparrowSDK-1.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='SparrowSDK',
+    version='1.0.1',
+    author='Sparrow SMS',
+    author_email='developers@sparrowsms.com',
+    packages=find_packages(),
+    scripts=['bin/sparrowsms',],
+    url='http://api.sparrowsms.com/',
+    license='',
+    description='Python SDK for Sparrow SMS API.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "requests",
+    ],
+)
