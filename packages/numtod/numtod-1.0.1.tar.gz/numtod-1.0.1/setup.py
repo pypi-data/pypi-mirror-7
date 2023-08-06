93ff6de501d61b2201a009d958abf18e82a46769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:54 +0000

    Add package numtod/1.0.1/numtod-1.0.1.tar.gz

---
+++ packages/numtod/numtod-1.0.1.tar.gz/numtod-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='numtod',
+	version='1.0.1',
+	packages=[''],
+	scripts=['convert.py'],
+	license='Creative Commons Attribution-Noncommercial-Share Alike License',
+	description="This converts numbers into strings od digits made from | and / and _"
+)
