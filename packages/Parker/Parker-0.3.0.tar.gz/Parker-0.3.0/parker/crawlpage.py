789dbdd08a9ba480b415cda1afd80507a2ddbbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:00 +0000

    Add package Parker/0.3.0/Parker-0.3.0.tar.gz

---
+++ packages/Parker/Parker-0.3.0.tar.gz/Parker-0.3.0/parker/crawlpage.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""CrawlPage object for Parker."""
+
+import page
+import parser
+
+_instances = dict()
+
+
+def get_instance(page_to_consume):
+    """Return an instance of CrawlPage."""
+    if isinstance(page_to_consume, basestring):
+        uri = page_to_consume
+        page_to_consume = page.get_instance(uri)
+    elif isinstance(page_to_consume, page.Page):
+        uri = page_to_consume.uri
+    else:
+        raise TypeError(
+            "get_instance() expects a parker.Page or basestring derivative."
+        )
+
+    page_to_consume.fetch()
+    parsed_page = parser.parse(page_to_consume)
+
+    try:
+        instance = _instances[uri]
+    except KeyError:
+        instance = CrawlPage(
+            parsed_page
+        )
+        _instances[uri] = instance
+
+    return instance
+
+
+class CrawlPage(object):
+
+    """A CrawlPage."""
+
+    def __init__(self, parsedpage):
+        """Constructor."""
+        self.parsedpage = parsedpage
+        self.uri = parsedpage.page.uri
+        self.hash = parsedpage.page.hash
+
+    def __repr__(self):
+        """Return an unambiguous representation."""
+        return "%s(%s)" % (self.__class__, self.uri)
+
+    def get_uris(self, base_uri):
+        """Return a set of internal URIs."""
+        return {
+            link.attrib['href']
+            for link in self.parsedpage.get_nodes_by_selector('a')
+            if 'href' in link.attrib
+            and base_uri in link.attrib['href']
+        }
+
+    def has_selector(self, consume_selector):
+        """Test if page has selector."""
+        return bool(self.parsedpage.get_nodes_by_selector(consume_selector))
---
+++ packages/Parker/Parker-0.3.0.tar.gz/Parker-0.3.0/setup.py
@@ -0,0 +1,41 @@
+"""
+Parker
+-----
+
+Parker is a Python-based web spider for collecting specific data across a set
+of configured sites.
+
+"""
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+version_file = open('VERSION')
+version = version_file.read().strip()
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='Parker',
+    version=version,
+    url='http://github.com/nefarioustim/parker/',
+    license='GPL v3',
+    author='Tim Huegdon',
+    author_email='tim@nefariousdesigns.co.uk',
+    description='A web spider for collecting specific data across a set of '
+                'configured sites',
+    long_description=__doc__,
+    install_requires=reqs,
+    packages=['parker'],
+    scripts=['bin/parker-clean', 'bin/parker-crawl'],
+    data_files=[
+        ('/etc/parker', [
+            'etc/parker/client.json',
+            'etc/parker/redis.json'
+        ]),
+        ('/etc/parker/sites', [
+            'etc/parker/sites/staples.json'
+        ])
+    ]
+)
