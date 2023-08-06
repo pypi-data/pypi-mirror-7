2d8eff09bf58d8c62491da9fc4a9bde85dc764d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:55 +0000

    Add package id3parse/0.1.1/id3parse-0.1.1.tar.gz

---
+++ packages/id3parse/id3parse-0.1.1.tar.gz/id3parse-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='id3parse',
+	version='0.1.1',
+	description='A parser and serializer for ID3v2 tags',
+	author='Lorenz Gorse',
+	url='https://github.com/frececroka/id3parse.py',
+	py_modules=['id3parse']
+)
