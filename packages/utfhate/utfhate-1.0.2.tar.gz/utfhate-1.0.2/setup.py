6fef5f06e3227ed3c60484c380bba67b8d120d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:47 +0000

    Add package utfhate/1.0.2/utfhate-1.0.2.tar.gz

---
+++ packages/utfhate/utfhate-1.0.2.tar.gz/utfhate-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='utfhate',
+      version='1.0.2',
+      author='Mikael Kohlmyr',
+      author_email='mikael@kohlmyr.com',
+      license='LICENSE',
+      packages=['utfhate'])
