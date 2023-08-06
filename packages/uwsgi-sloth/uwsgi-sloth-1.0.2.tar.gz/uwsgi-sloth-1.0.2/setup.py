38717af0d8416b32dc46173327096df8b275d3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package uwsgi-sloth/1.0.2/uwsgi-sloth-1.0.2.tar.gz

---
+++ packages/uwsgi-sloth/uwsgi-sloth-1.0.2.tar.gz/uwsgi-sloth-1.0.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(name='uwsgi-sloth',
+    version='1.0.2',
+    description='A simple uwsgi access log analyzer',
+    long_description=open('README.rst').read(),
+    author='piglei',
+    author_email='piglei2007@gmail.com',
+    url='https://github.com/piglei/uwsgi-sloth',
+    keywords='uwsgi log analyzer',
+    license='LICENSE',
+    packages=['uwsgi_sloth'],
+    package_data={"": ['*.html']},
+    install_requires=[
+        'jinja2',
+    ],
+    scripts=['uwsgi_sloth/uwsgi-sloth'])
+
+
