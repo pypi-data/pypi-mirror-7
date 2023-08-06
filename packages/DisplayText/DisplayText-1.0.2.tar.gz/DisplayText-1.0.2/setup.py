1b9e550eda6d6288679e35edf964604b330cdc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:09 +0000

    Add package DisplayText/1.0.2/DisplayText-1.0.2.tar.gz

---
+++ packages/DisplayText/DisplayText-1.0.2.tar.gz/DisplayText-1.0.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(name='DisplayText',
+	  version='1.0.2',
+	  description="Make text display easy. Based on 'More' and 'Less'.",
+	  author="Bede Kelly",
+	  author_email="bedekelly97@gmail.com",
+	  url="https://github.com/bedekelly/DisplayText",
+	  provides=['DisplayText'],
+	  classifiers=[
+	  	"Intended Audience :: Developers",
+	  	"Environment :: Console :: Curses",
+	  	"Natural Language :: English",
+	  	"Operating System :: OS Independent",
+	  	"Programming Language :: Python :: 3",
+	  	"Programming Language :: Python :: Implementation :: CPython",
+	  	"Topic :: Software Development :: Version Control",
+	  	"Topic :: Utilities"
+	  	])
