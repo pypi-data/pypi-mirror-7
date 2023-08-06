0b253ca285e36ad83b2e9d765c7f63d108836386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:28 +0000

    Add package game-of-thrones/0.1.7/game-of-thrones-0.1.7.tar.gz

---
+++ packages/game-of-thrones/game-of-thrones-0.1.7.tar.gz/game-of-thrones-0.1.7/game_of_thrones/main.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+
+"""Generate words that sound like characters from Game of Thrones.
+
+Usage:
+  game-of-thrones [--quantity=<number>] [--min=<length>] [--max=<length>] [--json]
+  game-of-thrones (-h | --help | --version)
+
+Options:
+  --version                show program's version number and exit.
+  -h, --help               show this help message and exit.
+  -q, --quantity=<number>  the quantity of words to generate [default: 10].
+  --min=<length>           the minimum length of each word [default: 4].
+  --max=<length>           the maximum length of each word [default: 10].
+  -j, --json               output the names in JSON format.
+"""
+from docopt import docopt
+from game_of_thrones import MarkovChain, __version__
+from colorama import Fore, Style, init
+from itertools import islice
+import json
+
+
+def main():
+    arguments = docopt(__doc__, version=__version__)
+
+    quantity = int(arguments['--quantity'])
+
+    chain = MarkovChain(
+        min_length=int(arguments['--min']),
+        max_length=int(arguments['--max']),
+    )
+
+    if arguments['--json']:
+        output = {'quantity': quantity}
+        output['names'] = list(islice(chain.unique_word(), quantity))
+        print(json.dumps(output))
+    else:
+        init(autoreset=True)
+
+        for i, word in enumerate(chain.unique_word()):
+            if i == quantity:
+                break
+
+            # alternate row colours
+            colour = Fore.BLUE if i % 2 == 0 else Fore.CYAN
+
+            print(Style.BRIGHT + colour + "{:>3}. {:<12}".format(i + 1, word))
+
+if __name__ == '__main__':
+    main()
