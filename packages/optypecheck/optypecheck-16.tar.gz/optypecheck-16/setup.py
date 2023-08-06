7b606185a14cb6ac077761d61a4fe7b05a6be707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:51 +0000

    Add package optypecheck/16/optypecheck-16.tar.gz

---
+++ packages/optypecheck/optypecheck-16.tar.gz/optypecheck-16/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+optypecheck
+===========
+
+This package implements a non intrusive and optional type checking in
+functions and methods by using annotations. Once types are defined in
+annotations, no changes are required to make the verification of types.
+And, because it is completely optional, it can be used only in the
+desired environments, like unit testings. This way, the performance of
+production code is not affected.
+
+Learn more in:
+
+    https://bitbucket.org/carlopires/optypecheck
+
+
+Installation
+------------
+
+.. code:: bash
+
+    pip3 install optypecheck
+
+Example
+-------
+
+.. code:: python
+
+    def gencode(a: bytes, b: str) -> str:
+        return '{}{}'.format(a[0], b)
+
+    def valid_number(n) -> 'decimal.Decimal':
+        return n
+
+    # enable type checking in DEBUG mode
+    assert __import__('typecheck').typecheck(__name__)
+
+"""
+from setuptools import setup
+
+setup(
+    name='optypecheck',
+    version=16,
+    url='https://bitbucket.org/carlopires/optypecheck',
+    author='Carlo Pires',
+    author_email='carlopires@gmail.com',
+    description='A non intrusive and optional *type checking for Python 3* '
+                'using annotations',
+    long_description=__doc__,
+    zip_safe=True,
+    packages=['typecheck'],
+    platforms='any',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    test_suite='test.suite',
+)
---
+++ packages/optypecheck/optypecheck-16.tar.gz/optypecheck-16/typecheck/types.py
@@ -0,0 +1,53 @@
+"""
+Created on 30/06/2014
+@author: Carlo Pires <carlopires@gmail.com>
+"""
+NotImplementedCls = NotImplemented.__class__
+NoneCls = None.__class__
+BooleanCls = bool
+StrCls = str
+BytesCls = bytes
+IntegralCls = 'numbers.Integral'  # int
+RealCls = 'numbers.Real'  # float
+DecimalCls = 'decimal.Decimal'
+TupleCls = ().__class__
+ListCls = [].__class__
+
+HashableCls = 'collections.Hashable'
+IterableCls = 'collections.Iterable'
+IteratorCls = 'collections.Iterator'
+SizedCls = 'collections.Sized'
+ContainerCls = 'collections.Container'
+CallableCls = 'collections.Callable'
+SetCls = 'collections.Set'
+MutableSetCls = 'collections.MutableSet'
+MappingCls = 'collections.Mapping'
+MutableMappingCls = 'collections.MutableMapping'
+MappingViewCls = 'collections.MappingView'
+KeysViewCls = 'collections.KeysView'
+ItemsViewCls = 'collections.ItemsView'
+ValuesViewCls = 'collections.ValuesView'
+SequenceCls = 'collections.Sequence'
+MutableSequenceCls = 'collections.MutableSequence'
+ByteStringCls = 'collections.ByteString'
+
+CodeCls = 'types.CodeType'
+MethodCls = 'types.MethodType'
+GeneratorCls = 'types.GeneratorType'
+FunctionCls = 'types.FunctionType'
+BuiltinFunctionCls = 'types.BuiltinFunctionType'
+MappingProxyCls = 'types.MappingProxyType'
+BuiltinMethodCls = 'types.BuiltinMethodType'
+ModuleCls = 'types.ModuleType'
+FrameCls = 'types.FrameType'
+GetSetDescriptorCls = 'types.GetSetDescriptorType'
+LambdaCls = 'types.LambdaType'
+TracebackCls = 'types.TracebackType'
+MemberDescriptorCls = 'types.MemberDescriptorType'
+
+AioLoopCls = 'asyncio.events.AbstractEventLoop'
+
+UUIDCls = 'uuid.UUID'
+TimeCls = 'datetime.time'
+DateCls = 'datetime.date'
+DateTimeCls = 'datetime.datetime'
