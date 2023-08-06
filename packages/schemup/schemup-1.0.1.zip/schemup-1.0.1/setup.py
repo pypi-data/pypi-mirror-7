0b88a19ee5c233589572f44599d39ef489cf8d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:36 +0000

    Add package schemup/1.0.1/schemup-1.0.1.zip

---
+++ packages/schemup/schemup-1.0.1.zip/schemup-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name="schemup",
+      version="1.0.1",
+
+      description="Database-agnostic schema upgrade tools",
+      author="Brendon Hogger",
+      author_email="brendonh@gmail.com",
+      url="https://github.com/brendonh/schemup",
+      long_description=open('README.md').read(),
+
+      packages = find_packages(),
+
+      install_requires = [
+      ],
+
+      package_data = {
+      },
+
+)
