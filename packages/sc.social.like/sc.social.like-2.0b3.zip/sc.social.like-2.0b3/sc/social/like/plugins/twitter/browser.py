cac840649d6f9e60a9eba8d89a4f7d76bd6a181c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:53 +0000

    Add package sc.social.like/2.0b3/sc.social.like-2.0b3.zip

---
+++ packages/sc.social.like/sc.social.like-2.0b3.zip/sc.social.like-2.0b3/sc/social/like/plugins/twitter/browser.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.PythonScripts.standard import url_quote
+from sc.social.like.utils import get_language
+from zope.component import getMultiAdapter
+
+
+class PluginView(BrowserView):
+
+    enabled_portal_types = []
+    typebutton = ''
+    twitter_enabled = ''
+    twittvia = ''
+    language = 'en'
+
+    metadata = ViewPageTemplateFile("templates/metadata.pt")
+    plugin = ViewPageTemplateFile("templates/plugin.pt")
+
+    def __init__(self, context, request):
+        super(PluginView, self).__init__(context, request)
+        pp = getToolByName(context, 'portal_properties')
+
+        self.context = context
+        self.request = request
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.portal = self.portal_state.portal()
+        self.site_url = self.portal_state.portal_url()
+        self.portal_title = self.portal_state.portal_title()
+        self.url = context.absolute_url()
+        self.language = get_language(context)
+        self.sheet = getattr(pp, 'sc_social_likes_properties', None)
+        if self.sheet:
+            self.typebutton = self.sheet.getProperty("typebutton", "")
+            self.twittvia = self.sheet.getProperty("twittvia", "")
+        self.urlnoscript = ('http://twitter.com/home?status=' +
+                            url_quote('%s - %s via %s' % (self.context.Title(),
+                                                          self.context.absolute_url(),
+                                                          self.twittvia)))
