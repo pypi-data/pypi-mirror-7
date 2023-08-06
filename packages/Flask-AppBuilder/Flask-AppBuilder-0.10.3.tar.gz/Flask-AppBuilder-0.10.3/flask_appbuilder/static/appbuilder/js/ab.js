4ad7e20202b559ed1b5d64e90f3ecfa92e7362ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:12 +0000

    Add package Flask-AppBuilder/0.10.3/Flask-AppBuilder-0.10.3.tar.gz

---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-0.10.3.tar.gz/Flask-AppBuilder-0.10.3/flask_appbuilder/static/appbuilder/js/ab.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $('.appbuilder_datetime').datetimepicker();
+    $('.appbuilder_date').datetimepicker({
+        pickTime: false });
+    $(".my_select2").select2({placeholder: "Select a State", allowClear: true});
+    $("a").tooltip({'selector': '','placement': 'bottom'});
+});
+
+
+$( ".my_change" ).on("change", function(e) {
+ var theForm=document.getElementById("model_form");
+  theForm.action = "";
+  theForm.method = "get";
+  theForm.submit();
+ })
+
+<<<<<<< HEAD
+
+var JsonSelect2 = function(name) {
+
+    var data = [];
+    var json_url = "./jsonselect/" + name;
+
+    refresh();
+    
+    function query() {
+        $.getJSON( json_url, function( data_json ) {
+            data = data_json.results;
+        });
+    }
+
+    function refresh() {
+        query();
+        $('.json-select2').select2({
+            width: "100%",
+            multiple: false,
+            data: data
+        });
+    }
+};
+
+
+$(".json_select2").each(function() {
+    JsonSelect2(this.id);
+});
+
+
+=======
+>>>>>>> c71c9bd668ebb02036e264ab604603d6ba76b67e
