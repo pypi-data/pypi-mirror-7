b2275037913f664411948bd843dcd47fc5ffc7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:55 +0000

    Add package octave-kernel/0.2/octave_kernel-0.2.tar.gz

---
+++ packages/octave-kernel/octave_kernel-0.2.tar.gz/octave_kernel-0.2/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+from distutils.command.install import install
+import sys
+
+
+class install_with_kernelspec(install):
+    def run(self):
+        install.run(self)
+        from IPython.kernel.kernelspec import install_kernel_spec
+        install_kernel_spec('kernelspec', 'octave', replace=True)
+
+with open('README.rst') as f:
+    readme = f.read()
+
+svem_flag = '--single-version-externally-managed'
+if svem_flag in sys.argv:
+    # Die, setuptools, die.
+    sys.argv.remove(svem_flag)
+
+setup(name='octave_kernel',
+      version='0.2',
+      description='An Octave kernel for IPython',
+      long_description=readme,
+      author='Steven Silvester',
+      author_email='steven.silvester@ieee.org',
+      url='https://github.com/blink1073/octave_kernel',
+      py_modules=['octave_kernel'],
+      cmdclass={'install': install_with_kernelspec},
+      install_requires=['oct2py'],
+      classifiers = [
+          'Framework :: IPython',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Shells',
+      ]
+)
