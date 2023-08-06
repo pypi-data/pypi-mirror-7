aa78fdb9fbc0b816b79c331e195db3e678b591f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:15 +0000

    Add package rainbowstream/0.4.4/rainbowstream-0.4.4.tar.gz

---
+++ packages/rainbowstream/rainbowstream-0.4.4.tar.gz/rainbowstream-0.4.4/rainbowstream/py3patch.py
@@ -0,0 +1,24 @@
+"""
+  Python 3 supports
+"""
+import sys
+
+# StringIO module
+try:
+    from StringIO import StringIO, BytesIO
+except:
+    from io import StringIO, BytesIO
+
+# HTMLParser module
+try:
+    from HTMLParser import HTMLParser
+    unescape = HTMLParser().unescape
+except:
+    from html import unescape
+
+# raw_input and map functiion behaviour
+if sys.version[0] == "3":
+    raw_input = input
+    lmap = lambda f, a: list(map(f, a))
+else:
+    lmap = lambda f, a: map(f, a)
