7cf3c903a8615229054674436004b531ec52410a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:17 +0000

    Add package go-contacts/0.1.3/go_contacts-0.1.3-py2-none-any.whl

---
+++ packages/go-contacts/go_contacts-0.1.3-py2-none-any.whl/go_contacts/backends/riak.py
@@ -0,0 +1,3 @@
+from contacts import (RiakContactsBackend, RiakContactsCollection,
+                      contact_to_dict)
+from groups import (RiakGroupsBackend, RiakGroupsCollection, group_to_dict)
