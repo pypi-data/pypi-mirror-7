349d742dcdffeac7951be7e0f1f0fd35e6dd1228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:19 +0000

    Add package django-enumfields/0.2.1/django-enumfields-0.2.1.tar.gz

---
+++ packages/django-enumfields/django-enumfields-0.2.1.tar.gz/django-enumfields-0.2.1/enumfields/enums.py
@@ -0,0 +1,30 @@
+import inspect
+from enum import Enum as BaseEnum, EnumMeta as BaseEnumMeta
+import six
+
+
+class EnumMeta(BaseEnumMeta):
+    def __new__(cls, name, bases, attrs):
+        Labels = attrs.get('Labels')
+
+        if Labels is not None and inspect.isclass(Labels):
+            del attrs['Labels']
+
+        obj = BaseEnumMeta.__new__(cls, name, bases, attrs)
+        for m in obj:
+            try:
+                m.label = getattr(Labels, m.name)
+            except AttributeError:
+                m.label = m.name.replace('_', ' ').title()
+
+        return obj
+
+
+class Enum(six.with_metaclass(EnumMeta, BaseEnum)):
+    @classmethod
+    def choices(cls):
+        """
+        Returns a list formatted for use as field choices.
+        (See https://docs.djangoproject.com/en/dev/ref/models/fields/#choices)
+        """
+        return tuple((m.value, m.label) for m in cls)
