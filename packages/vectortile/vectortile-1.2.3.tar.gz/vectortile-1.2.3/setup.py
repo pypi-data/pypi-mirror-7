1552884dde13d2e6729d8b34c348bf555859335d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:02 +0000

    Add package vectortile/1.2.3/vectortile-1.2.3.tar.gz

---
+++ packages/vectortile/vectortile-1.2.3.tar.gz/vectortile-1.2.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from glob import glob
+from os.path import sep
+from distutils.core import setup
+
+import vectortile
+
+setup(name='vectortile',
+      version=vectortile.__version__,
+      author=vectortile.__author__,
+      author_email='paul@skytruth.org',
+      description=vectortile.__doc__,
+      long_description=vectortile.__doc__,
+      url=vectortile.__source__,
+      license=vectortile.__license__,
+      packages=['vectortile', 'vectortile.tests'])
