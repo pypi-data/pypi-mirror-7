97ef965f7895eb3f0dfae664c42ec33367af20b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:03 +0000

    Add package iamer/0.1.0/iamer-0.1.0.tar.gz

---
+++ packages/iamer/iamer-0.1.0.tar.gz/iamer-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+setup(
+    name='iamer',
+    version='0.1.0',
+    description='AWS IAM dump and load tool',
+    url='https://github.com/percolate/iamer',
+    author='Laurent Raufaste',
+    author_email='analogue@glop.org',
+    license='GPLv3',
+    keywords='aws iam boto',
+    install_requires=[
+        'boto',
+        'docopt'
+    ],
+    entry_points={
+        'console_scripts': [
+            'iamer=iamer.main:main'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: System :: Systems Administration"
+    ]
+)
