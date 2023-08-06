5c8c5f0a473184e3a0aefa00eb3753d351ab3f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:13 +0000

    Add package uniout/0.3.7/uniout-0.3.7.tar.gz

---
+++ packages/uniout/uniout-0.3.7.tar.gz/uniout-0.3.7/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+import uniout
+
+setup(
+
+    name = 'uniout',
+    version = uniout.__version__,
+    description = 'Never see escaped bytes in output.',
+    long_description = open('README.rst').read(),
+
+    author = 'Mosky',
+    url = 'https://github.com/moskytw/uniout',
+    author_email = 'mosky.tw@gmail.com',
+    license = 'MIT',
+    platforms = 'any',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    py_modules = ['_uniout', 'uniout'],
+
+)
+
