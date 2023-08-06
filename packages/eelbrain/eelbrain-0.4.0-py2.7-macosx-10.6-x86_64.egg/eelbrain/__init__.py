6f7750e46406bd083b922143a13c97e81d2b0789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:04 +0000

    Add package eelbrain/0.4.0/eelbrain-0.4.0-py2.7-macosx-10.6-x86_64.egg

---
+++ packages/eelbrain/eelbrain-0.4.0-py2.7-macosx-10.6-x86_64.egg/eelbrain/__init__.py
@@ -0,0 +1,27 @@
+"""
+
+  "The human brain is like an enormous fish. It's flat
+   and slimy, and has gills through which it can see."
+                                               - Dr. Quat
+
+
+Module Hierarchy
+================
+
+'a < b' means a imports from b, so b can not import from a:
+
+wxterm < wxgui < wxutils
+               < plot < analyze < vessels < fmtxt
+
+
+
+
+
+Created by Christian Brodbeck on 2/20/2012.
+Copyright (c) 2012. All rights reserved.
+
+"""
+
+from .utils import logger
+
+__version__ = '0.4.0'
