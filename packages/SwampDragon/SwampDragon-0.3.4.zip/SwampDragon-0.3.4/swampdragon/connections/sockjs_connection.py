85f8e6197d8533b7eb39410ecc26bafe83c083a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:25 +0000

    Add package SwampDragon/0.3.4/SwampDragon-0.3.4.zip

---
+++ packages/SwampDragon/SwampDragon-0.3.4.zip/SwampDragon-0.3.4/swampdragon/connections/sockjs_connection.py
@@ -0,0 +1,47 @@
+from sockjs.tornado import SockJSConnection
+from ..pubsub_providers.pubsub_factory import get_subscription_provider
+from .. import route_handler
+import json
+
+
+class ConnectionMixin(object):
+    def to_json(self, data):
+        """
+        If the message is a dict, return it.
+        If it's a string try to decode it into json,
+        otherwise assume it's not a dictionary but a simple text message
+        """
+        if isinstance(data, dict):
+            return data
+        try:
+            return json.loads(data)
+        except:
+            return {'message': data}
+
+
+class SubscriberConnection(ConnectionMixin, SockJSConnection):
+    channels = []
+    pub_sub = None
+
+    def __init__(self, session):
+        super(SubscriberConnection, self).__init__(session)
+        self.pub_sub = get_subscription_provider()
+
+    def on_close(self):
+        self.pub_sub.close(self)
+
+    def on_message(self, data):
+        try:
+            data = self.to_json(data)
+            handler = route_handler.get_route_handler(data['route'])
+            handler(self).handle(data)
+        except Exception as e:
+            self.abort_connection()
+            raise e
+
+    def abort_connection(self):
+        self.close()
+
+
+class DjangoSubscriberConnection(SubscriberConnection):
+    pass
---
+++ packages/SwampDragon/SwampDragon-0.3.4.zip/SwampDragon-0.3.4/swampdragon/serializers/object_map.py
@@ -0,0 +1,56 @@
+def _construct_graph(parent_type, child_type, via, is_collection, property_name):
+    return {
+        'parent_type': parent_type,
+        'child_type': child_type,
+        'via': via,
+        'prop_name': property_name,
+        'is_collection': is_collection,
+    }
+
+
+def _serializer_is_ignored(serializer, related_serializer, ignore_serializer_pairs):
+    if not ignore_serializer_pairs:
+        return False
+    if (serializer, related_serializer) in ignore_serializer_pairs:
+        return True
+    return False
+
+
+def get_object_map(serializer, ignore_serializer_pairs=None):
+    """
+    Create an object map from the serializer and it's related serializers.
+
+    For each map created, ignore the pair of serializers that are already mapped
+    """
+    graph = []
+    serializer_instance = serializer()
+    if ignore_serializer_pairs is None:
+        ignore_serializer_pairs = []
+
+    serializers = serializer.get_related_serializers()
+
+    for related_serializer, field_name in serializers:
+        if _serializer_is_ignored(serializer, related_serializer, ignore_serializer_pairs):
+            continue
+
+        field = getattr(serializer_instance.opts.model, field_name)
+        if hasattr(field, 'related'):
+            model = field.related.model
+            attname = field.related.field.name
+            is_collection = True
+        else:
+            model = field.field.related.parent_model
+            attname = field.field.related.var_name
+            is_collection = False
+        graph.append(
+            _construct_graph(
+                serializer_instance.opts.model._meta.model_name,
+                model._meta.model_name,
+                attname,
+                is_collection,
+                field_name
+            )
+        )
+        ignore_serializer_pairs.append((serializer, related_serializer))
+        graph += get_object_map(related_serializer, ignore_serializer_pairs)
+    return graph
