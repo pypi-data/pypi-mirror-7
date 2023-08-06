b278ac5b225d487a7cbf3326dc1e221b88dc1cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:50 +0000

    Add package helga-haskell/0.1.0/helga-haskell-0.1.0.tar.gz

---
+++ packages/helga-haskell/helga-haskell-0.1.0.tar.gz/helga-haskell-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helga-haskell',
+    version='0.1.0',
+    author="Cary M. Robbins",
+    description='Evaluate Haskell expressions using helga!',
+    author_email="carymrobbins@gmail.com",
+    url="https://github.com/carymrobbins/helga-haskell",
+    packages=find_packages(),
+    license='BSD3',
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'helga',
+        'tryhaskell',
+    ],
+    entry_points=dict(
+        helga_plugins=[
+            'haskell = helga_haskell:haskell',
+        ],
+    ),
+)
