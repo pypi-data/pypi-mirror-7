f96d7ecb74c26194d8d17e37aa4dbc86c697e538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:54 +0000

    Add package django-sendfile/0.3.6/django-sendfile-0.3.6.tar.gz

---
+++ packages/django-sendfile/django-sendfile-0.3.6.tar.gz/django-sendfile-0.3.6/setup.py
@@ -0,0 +1,47 @@
+from distutils.core import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+
+version = __import__('sendfile').__version__
+
+
+setup(
+    name='django-sendfile',
+    version=version,
+    description='Abstraction to offload file uploads to web-server (e.g. Apache with mod_xsendfile) once Django has checked permissions etc.',
+    long_description=open('README.rst').read(),
+    author='John Montgomery',
+    author_email='john@sensibledevelopment.com',
+    url='https://github.com/johnsensible/django-sendfile',
+    license='BSD',
+
+    requires=['Django (>=1.3)', 'Unidecode'],
+    install_requires=['Django>=1.3', 'Unidecode'],
+
+    packages=['sendfile', 'sendfile.backends'],
+    package_dir={
+        'sendfile': 'sendfile',
+        'sendfile.backends': 'sendfile/backends',
+    },
+    package_data={
+        'sendfile': ['testfile.txt'],
+    },
+
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    cmdclass={'build_py': build_py},
+)
