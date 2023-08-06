5c03af42a0caa8861c690f52069e530f82ee9e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:00 +0000

    Add package cprotobuf/0.1/cprotobuf-0.1.tar.gz

---
+++ packages/cprotobuf/cprotobuf-0.1.tar.gz/cprotobuf-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from distutils.extension import Extension
+
+ext_modules = [ Extension("cprotobuf/internal", ["cprotobuf/internal.c"])
+              ]
+
+setup(
+    version='0.1',
+    name = 'cprotobuf',
+    ext_modules = ext_modules,
+    scripts = [ 'protoc-gen-cprotobuf' ],
+    packages = ['cprotobuf'],
+    url = 'https://github.com/yihuang/cprotobuf',
+)
