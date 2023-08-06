042fe1c126be15b56e4bab963d1e097447339f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:54 +0000

    Add package numg/1.0.1/numg-1.0.1.tar.gz

---
+++ packages/numg/numg-1.0.1.tar.gz/numg-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name='numg',
+	version='1.0.1',
+	packages=[''],
+	scripts=['numag.py'],
+	license='Creative Commons Attribution-Noncommercial-Share Alike License',
+)
