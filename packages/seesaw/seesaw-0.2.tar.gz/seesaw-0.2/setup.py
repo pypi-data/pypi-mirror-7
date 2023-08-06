779de95aadcee60becacdb958a5fb831c5b7cb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package seesaw/0.2/seesaw-0.2.tar.gz

---
+++ packages/seesaw/seesaw-0.2.tar.gz/seesaw-0.2/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script. See inline comments for packaging documentation.
+"""
+
+import os
+import sys
+
+import seesaw
+
+try:
+    from setuptools import setup
+    # hush pyflakes
+    setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+packages = [
+    'seesaw'
+]
+
+package_dir = {
+    'seesaw': 'seesaw'
+}
+
+package_data = {
+    'seesaw': [
+        'public/index.html',
+        'public/*.js',
+        'public/*.css',
+        'templates/*.html'
+    ]
+}
+
+scripts = [
+    'run-pipeline',
+    'run-warrior'
+]
+
+requires = [
+    'argparse',
+    'ordereddict',
+    'Tornado>=2.3',
+    'tornadio2>=0.0.3'
+]
+
+setup(
+    name='seesaw',
+    version=seesaw.__version__,
+    maintainer='ArchiveTeam',
+    maintainer_email='warrior@archiveteam.org',
+    description='ArchiveTeam seesaw kit',
+    long_description=open('README.md', 'rt').read(),
+    url='http://www.archiveteam.org/',
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+    include_package_data=True,
+    scripts=scripts,
+    install_requires=requires,
+    cmdclass={'build_py': build_py}
+)
