9efdad10b3a7aa11d7b02538d9ddd2019a5799ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:06 +0000

    Add package rosa/0.1.2/rosa-0.1.2.tar.gz

---
+++ packages/rosa/rosa-0.1.2.tar.gz/rosa-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+      with open('README.rst') as f:
+            return f.read()
+
+setup(name='rosa',
+      version='0.1.2',
+      description='Helper script for ROS',
+      long_description=readme(),
+      url='http://github.com/abencz/rosa',
+      author='Alex Bencz',
+      license='BSD',
+      packages=['rosa'],
+      scripts=['bin/rosa'],
+      zip_safe=False)
+
