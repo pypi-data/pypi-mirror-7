1b59c33c0b08c218164ff6ff8f786da9b7d93474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:50 +0000

    Add package ciecam02/0.1.4/ciecam02-0.1.4.tar.gz

---
+++ packages/ciecam02/ciecam02-0.1.4.tar.gz/ciecam02-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+ext_modules = [
+    Extension("ciecam02",
+              ["ciecam02/ciecam02x.pyx",],
+              libraries=["m"],
+              include_dirs=[np.get_include()]
+          )
+]
+setup(
+    #ext_modules = cythonize("glcprocedure.pyx")
+    name = "ciecam02",
+    version = '0.1.4',
+    author = 'Dannyv',
+    author_email = 'yu.yuyudan@gmail.com',
+    packages = ["ciecam02",],
+    license = 'BSD licence',
+    description = 'a set of function convert colorspace among rgb, ciexyz, ciecam02 for python2.7',
+    long_description = open('README.txt').read(),
+    cmdclass = {"build_ext": build_ext},
+    ext_modules = ext_modules
+)
