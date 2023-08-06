2513acde9111282762c3297c9c51fb3a9991693b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:59 +0000

    Add package cobra/0.3.0b2/cobra-0.3.0b2.tar.gz

---
+++ packages/cobra/cobra-0.3.0b2.tar.gz/cobra-0.3.0b2/cobra/core/DictList.py
@@ -0,0 +1,269 @@
+from copy import copy, deepcopy
+import re
+from ..external.six import string_types, iteritems
+
+class DictList(list):
+    """A combined dict and list
+    
+    This object behaves like a list, but has the O(1) speed
+    benefits of a dict when looking up elements by their id.
+
+    """
+    _dict = {}
+    def __init__(self, *args, **kwargs):
+        list.__init__(self, *args, **kwargs)
+        self._generate_index()
+
+    def has_id(self, id):
+        return id in self._dict
+    
+    def _check(self, id):
+        """make sure duplicate id's are not added.
+        This function is called before adding in elements.
+
+        """
+        if id in self._dict:
+            raise ValueError("id %s is already present in list" % str(id))
+
+    def _generate_index(self):
+        """rebuild the _dict index"""
+        self._dict = {v.id: k for k, v in enumerate(self)}
+
+
+    def get_by_id(self, id):
+        """return the element with a matching id"""
+        return self[self._dict[id]]
+
+    def list_attr(self, attribute):
+        """return a list of the given attribute for every object
+
+        """
+        return [getattr(i, attribute) for i in self]
+
+    def query(self, search_function, attribute="id"):
+        """query the list
+
+        search_function: used to select which objects to return
+            * a string, in which case any object.attribute containing
+              the string will be returned
+
+            * a compiled regular expression
+
+            * a function which takes one argument and returns True
+              for desired values
+
+        attribute: the attribute to be searched for (default is 'id').
+                   If this is None, the object itself is used.
+
+        returns: a list of objects which match the query
+        """
+        if attribute == None:
+            select_attribute = lambda x : x
+        else:
+            select_attribute = lambda x: getattr(x, attribute)
+
+        # if the search_function is a regular expression
+        if isinstance(search_function, str):
+            search_function = re.compile(search_function)
+        if hasattr(search_function, "findall"):
+            matches = [i for i in self
+                if search_function.findall(select_attribute(i)) != []]
+        else:
+            matches = [i for i in self
+                if search_function(select_attribute(i))]
+        return DictList(matches)
+
+
+    # overriding default list functions with new ones
+    def __setitem__(self, i, y):
+        if isinstance(i, slice):
+            for obj in y:  # need to be setting to a list
+                self._check(obj.id)
+            list.__setitem__(self, i, y)
+            self._generate_index()
+            return
+        self._dict.pop(self[i].id)
+        the_id = y.id
+        self._check(the_id)
+        list.__setitem__(self, i, y)
+        self._dict[the_id] = i
+
+    def _replace_on_id(self, new_object):
+        """Replace an object by another with the same id."""
+        the_id = new_object.id
+        the_index = self._dict[the_id]
+        list.__setitem__(self, the_index, new_object)
+
+    def append(self, object):
+        """append object to end"""
+        the_id = object.id
+        self._check(the_id)
+        self._dict[the_id] = len(self)
+        list.append(self, object)
+
+    def union(self, iterable):
+        """adds elements with id's not already in the model"""
+        _dict = self._dict
+        append = self.append
+        for i in iterable:
+            if i.id not in _dict:
+                append(i)
+
+    def extend(self, iterable):
+        """extend list by appending elements from the iterable"""
+        append = self.append
+        for i in iterable:
+            append(i)
+
+    def __add__(self, other, should_deepcopy=False):
+        """
+        other: iterable
+            other must contain only unique id's which do not intersect
+            with self
+
+        should_deepcopy: Boolean. 
+
+        """
+        if should_deepcopy:
+            from copy import deepcopy
+            total = deepcopy(self)
+        else:
+            total = DictList()
+            total.extend(self)
+        total.extend(other)
+        return total
+
+    def __iadd__(self, other):
+        self.extend(other)
+        return self
+
+    def index(self, id, *args):
+        """Determine the position in the list
+
+        id: A string or a :class:`~cobra.core.Object.Object`
+
+        """
+        # because values are unique, start and stop are not relevant
+        if isinstance(id, string_types):
+            try:
+                return self._dict[id]
+            except KeyError:
+                raise ValueError("%s not found" % id)
+        try:
+            i = self._dict[id.id]
+            if self[i] is not id:
+                raise ValueError(
+                    "Another object with the identical id (%s) found" % id.id)
+            return i
+        except KeyError:
+            raise ValueError("%s not found" % str(i))
+
+    def __contains__(self, object):
+        """DictList.__contains__(object) <==> object in DictList
+
+        object: str or :class:`~cobra.core.Object.Object`
+
+        """
+        if hasattr(object, "id"):
+            the_id = object.id
+        # allow to check with the object itself in addition to the id
+        else:
+            the_id = object
+        return the_id in self._dict
+
+    def __copy__(self):
+        self._dict.clear()
+        the_copy = copy(super(DictList, self))
+        self._generate_index()
+        the_copy._generate_index()
+        return the_copy
+
+    def __deepcopy__(self, *args, **kwargs):
+        _dict = self._dict
+        self._dict = {}
+        new = deepcopy(super(DictList, self), *args, **kwargs)
+        self._dict = _dict
+        return new
+
+    def insert(self, index, object):
+        """insert object before index"""
+        self._check(object.id)
+        list.insert(self, index, object)
+        # all subsequent entries now have been shifted up by 1
+        _dict = self._dict
+        for i in _dict:
+            j = _dict[i]
+            if j >= index:
+                _dict[i] = j + 1
+        _dict[object.id] = index
+
+    def pop(self, *args):
+        """remove and return item at index (default last)."""
+        value = list.pop(self, *args)
+        index = self._dict.pop(value.id)
+        # If the pop occured from a location other than the end of the list,
+        # we will need to subtract 1 from every entry afterwards
+        if len(args) == 0 or args == [-1]: # removing from the end of the list
+            return
+        _dict = self._dict
+        for i, j in iteritems(_dict):
+            if j > index:
+                _dict[i] = j - 1
+        return value
+
+    def remove(self, x):
+        """remove first occurrence of value."""
+        # Each item is unique in the list which allows this
+        # It is much faster to do a dict lookup than n string comparisons
+        self.pop(self.index(x))
+
+    # these functions are slower because they rebuild the _dict every time
+    def reverse(self):
+        """reverse *IN PLACE*"""
+        list.reverse(self)
+        self._generate_index()
+
+    def sort(self, cmp=None, key=None, reverse=False):
+        """stable sort *IN PLACE*
+
+        cmp(x, y) -> -1, 0, 1
+
+        """
+        if key is None:
+            key = lambda i: i.id
+        list.sort(self, cmp=cmp, key=key, reverse=reverse)
+        self._generate_index()
+
+    def __setslice__(self, *args, **kwargs):
+        list.__setslice__(self, *args, **kwargs)
+        self._generate_index()
+
+    def __delslice__(self, *args, **kwargs):
+        list.__delslice__(self, *args, **kwargs)
+        self._generate_index()
+
+    def __delitem__(self, index):
+        removed = self[index]
+        list.__delitem__(self, index)
+        if isinstance(removed, list):
+            self._generate_index()
+            return
+        _dict = self._dict
+        _dict.pop(removed.id)
+        for i, j in iteritems(_dict):
+            if j > index:
+                _dict[i] = j - 1
+
+    def __getattr__(self, attr):
+        try:
+            return self[self.__dict__["_dict"][attr]]
+        except KeyError:
+            raise AttributeError("DictList has no attribute or entry %s" % \
+                (attr))
+
+    def __dir__(self):
+        # override this to allow tab complete of items by their id
+        attributes = dir(self.__class__)
+        attributes.extend(self.__dict__.keys())
+        attributes.extend(self._dict.keys())
+        return attributes
---
+++ packages/cobra/cobra-0.3.0b2.tar.gz/cobra-0.3.0b2/cobra/solvers/gurobi_solver.py
@@ -0,0 +1,236 @@
+##cobra.solvers.gurobi_solver
+#Interface to the gurobi 5.0.1 python solver
+
+from warnings import warn
+from os import name as __name
+from copy import deepcopy
+from itertools import izip
+###solver specific parameters
+from .parameters import status_dict, variable_kind_dict, \
+     sense_dict, parameter_mappings, parameter_defaults, \
+     objective_senses, default_objective_sense
+
+from ..core.Solution import Solution
+from time import time
+solver_name = 'gurobi'
+objective_senses = objective_senses[solver_name]
+parameter_mappings = parameter_mappings[solver_name]
+parameter_defaults = parameter_defaults[solver_name]
+
+## from numpy import array
+from gurobipy import Model, LinExpr, GRB, QuadExpr
+variable_kind_dict = eval(variable_kind_dict[solver_name])
+status_dict = eval(status_dict[solver_name])
+__solver_class = Model
+def get_status(lp):
+    status = lp.status
+    if status in status_dict:
+        status = status_dict[status]
+    else:
+        status = 'failed'
+    return status
+
+def get_objective_value(lp):
+    return lp.ObjVal
+
+def format_solution(lp, cobra_model, **kwargs):
+    status = get_status(lp)
+    if status not in ('optimal', 'time_limit'):
+        the_solution = Solution(None, status=status)
+    else:
+        objective_value = lp.ObjVal
+        x = [v.X for v  in lp.getVars()]      
+        x_dict = {r.id: value for r, value in izip(cobra_model.reactions, x)}
+        if lp.isMIP:
+            y = y_dict = None #MIP's don't have duals
+        else:
+            y = [c.Pi for c in lp.getConstrs()]
+            y_dict = {m.id: value for m, value in izip(cobra_model.metabolites, y)}
+        the_solution = Solution(objective_value, x=x, x_dict=x_dict, y=y,
+                                y_dict=y_dict, status=status)
+    return(the_solution)
+
+def set_parameter(lp, parameter_name, parameter_value):
+    if parameter_name == 'ModelSense':
+        lp.setAttr(parameter_name, objective_senses[parameter_value])
+    else:
+        lp.setParam(parameter_name, parameter_value)
+
+def change_variable_bounds(lp, index, lower_bound, upper_bound):
+    variable = lp.getVarByName(str(index))
+    variable.lb = lower_bound
+    variable.ub = upper_bound
+
+
+def change_variable_objective(lp, index, objective):
+    variable = lp.getVarByName(str(index))
+    variable.obj = objective
+
+
+def change_coefficient(lp, met_index, rxn_index, value):
+    met = lp.getConstrByName(str(met_index))
+    rxn = lp.getVarByName(str(rxn_index))
+    lp.chgCoeff(met, rxn, value)
+
+
+def update_problem(lp, cobra_model, **kwargs):
+    """A performance tunable method for updating a model problem file
+
+    lp: A gurobi problem object
+
+    cobra_model: the cobra.Model corresponding to 'lp'
+
+    """
+    #When reusing the basis only assume that the objective coefficients or bounds can change
+    try:
+        quadratic_component = kwargs['quadratic_component']
+        if quadratic_component is not None:
+            warn("update_problem does not yet take quadratic_component as a parameter")
+    except:
+        quadratic_component = None
+
+    if 'copy_problem' in kwargs and kwargs['copy_problem']:
+        lp = lp.copy()
+    if 'reuse_basis' in kwargs and not kwargs['reuse_basis']:
+        lp.reset()
+    for the_variable, the_reaction in zip(lp.getVars(),
+                                          cobra_model.reactions):
+        the_variable.lb = float(the_reaction.lower_bound)
+        the_variable.ub = float(the_reaction.upper_bound)
+        the_variable.obj = float(the_reaction.objective_coefficient)
+
+
+sense_dict = eval(sense_dict[solver_name])
+def create_problem(cobra_model, quadratic_component=None, **kwargs):
+    """Solver-specific method for constructing a solver problem from
+    a cobra.Model.  This can be tuned for performance using kwargs
+
+
+    """
+    lp = Model("")
+    #Silence the solver
+    set_parameter(lp, 'OutputFlag', 0)
+
+    the_parameters = parameter_defaults
+    if kwargs:
+        the_parameters = deepcopy(parameter_defaults)
+        the_parameters.update(kwargs)
+
+    [set_parameter(lp, parameter_mappings[k], v)
+         for k, v in the_parameters.iteritems() if k in parameter_mappings]
+
+
+    # Create variables
+    #TODO:  Speed this up
+    variable_list = [lp.addVar(float(x.lower_bound),
+                               float(x.upper_bound),
+                               float(x.objective_coefficient),
+                               variable_kind_dict[x.variable_kind],
+                               str(i))
+                     for i, x in enumerate(cobra_model.reactions)]
+    reaction_to_variable = dict(zip(cobra_model.reactions,
+                                    variable_list))
+    # Integrate new variables
+    lp.update()
+
+    #Constraints are based on mass balance
+    #Construct the lin expression lists and then add
+    #TODO: Speed this up as it takes about .18 seconds
+    #HERE
+    for i, the_metabolite in enumerate(cobra_model.metabolites):
+        constraint_coefficients = []
+        constraint_variables = []
+        for the_reaction in the_metabolite._reaction:
+            constraint_coefficients.append(the_reaction._metabolites[the_metabolite])
+            constraint_variables.append(reaction_to_variable[the_reaction])
+        #Add the metabolite to the problem
+        lp.addConstr(LinExpr(constraint_coefficients, constraint_variables),
+                     sense_dict[the_metabolite._constraint_sense.upper()],
+                     the_metabolite._bound,
+                     str(i))
+
+    # Set objective to quadratic program
+    if quadratic_component is not None:
+        set_quadratic_objective(lp, quadratic_component)
+
+    lp.update()
+    return(lp)
+
+
+def set_quadratic_objective(lp, quadratic_objective):
+    if not hasattr(quadratic_objective, 'todok'):
+        raise Exception('quadratic component must have method todok')
+    variable_list = lp.getVars()
+    linear_objective = lp.getObjective()
+    # If there already was a quadratic expression set, this will be quadratic
+    # and we need to extract the linear component
+    if hasattr(linear_objective, "getLinExpr"):  # duck typing
+        linear_objective = linear_objective.getLinExpr()
+    gur_quadratic_objective = QuadExpr()
+    for (index_0, index_1), the_value in quadratic_objective.todok().items():
+        # gurobi does not multiply by 1/2 (only does v^T Q v)
+        gur_quadratic_objective.addTerms(the_value * 0.5,
+                                         variable_list[index_0],
+                                         variable_list[index_1])
+    # this adds to the existing quadratic objectives
+    lp.setObjective(gur_quadratic_objective + linear_objective)
+
+def solve_problem(lp, **kwargs):
+    """A performance tunable method for updating a model problem file
+
+    """
+    #Update parameter settings if provided
+    if kwargs:
+        [set_parameter(lp, parameter_mappings[k], v)
+         for k, v in kwargs.iteritems() if k in parameter_mappings]
+
+    lp.update()
+    lp.optimize()
+    status = get_status(lp)
+    return status
+
+    
+def solve(cobra_model, **kwargs):
+    """
+
+    """
+    #Start out with default parameters and then modify if
+    #new onese are provided
+    the_parameters = deepcopy(parameter_defaults)
+    if kwargs:
+        the_parameters.update(kwargs)
+    for i in ["new_objective", "update_problem", "the_problem"]:
+        if i in the_parameters:
+            raise Exception("Option %s removed" % i)
+    if 'error_reporting' in the_parameters:
+        warn("error_reporting deprecated")
+
+    #Create a new problem
+    lp = create_problem(cobra_model, **the_parameters)
+
+
+    ###Try to solve the problem using other methods if the first method doesn't work
+    try:
+        lp_method = the_parameters['lp_method']
+    except:
+        lp_method = 0
+    the_methods = [0, 2, 1]
+    if lp_method in the_methods:
+        the_methods.remove(lp_method)
+    #Start with the user specified method
+    the_methods.insert(0, lp_method)
+    for the_method in the_methods:
+        the_parameters['lp_method'] = the_method
+        try:
+            status = solve_problem(lp, **the_parameters)
+        except:
+            status = 'failed'
+        if status == 'optimal':
+            break
+
+    the_solution = format_solution(lp, cobra_model)
+    #if status != 'optimal':
+    #    print '%s failed: %s'%(solver_name, status)
+    #cobra_model.solution = the_solution
+    #solution = {'the_problem': lp, 'the_solution': the_solution}
+    return the_solution
