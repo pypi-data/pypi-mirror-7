f3d6594fed4b17bd7766abb8b9f28a765d77c9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:41 +0000

    Add package mamba-runner/0.0.2/mamba-runner-0.0.2.tar.gz

---
+++ packages/mamba-runner/mamba-runner-0.0.2.tar.gz/mamba-runner-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mamba-runner',
+    version='0.0.2',
+    description='Measure time execution for unittest tests',
+    url='https://github.com/mattack108/mamba-runner',
+    author='Maciek Lenc',
+    author_email='matt.lenc@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Testing',
+    ],
+    install_requires=['blessings'],
+    keywords='unittest timing time mamba tests runner',
+    packages=find_packages(),
+)
