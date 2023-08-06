2d2f6070f31b76db269c52da810fc271fd6561ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:36 +0000

    Add package pysed/0.0.4/pysed-0.0.4.tar.gz

---
+++ packages/pysed/pysed-0.0.4.tar.gz/pysed-0.0.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+
+setup(
+    name = 'pysed',
+    version = "0.0.4",
+    description = "Utility that parses and transforms text",
+    keywords = ["python", "sed", "unix", "linux", "text", 
+		"stream", "editor"],
+    author = "dslackw",
+    author_email = "d.zlatanidis@gmail.com",
+    url = "https://github.com/dslackw/pysed",
+    scripts = ['bin/pysed'],
+    package_data = {"": ["LICENSE", "README.rst", "CHANGELOG"]},
+    classifiers = [
+	"Classifier: Development Status :: 3 - Alpha",
+	"Classifier: Environment :: Console",
+	"Classifier: License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+	"Classifier: Operating System :: POSIX",
+	"Classifier: Operating System :: POSIX :: Linux",
+	"Classifier: Operating System :: Unix",
+	"Classifier: Programming Language :: Python",
+	"Classifier: Programming Language :: Python :: 2.7",
+	"Classifier: Programming Language :: Unix Shell",
+	"Classifier: Topic :: Text Editors",
+	"Classifier: Topic :: Text Editors :: Text Processing",
+	"Classifier: Topic :: Text Processing",
+	"Classifier: Topic :: Text Processing :: General",
+	"Classifier: Topic :: Utilities"],
+    long_description=open("README.rst").read()
+)
