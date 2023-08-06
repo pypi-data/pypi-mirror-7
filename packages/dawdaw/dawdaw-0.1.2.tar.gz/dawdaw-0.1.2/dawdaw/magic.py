60a21dfe8ad319efe548ada85165de256cb562a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:27 +0000

    Add package dawdaw/0.1.2/dawdaw-0.1.2.tar.gz

---
+++ packages/dawdaw/dawdaw-0.1.2.tar.gz/dawdaw-0.1.2/dawdaw/magic.py
@@ -0,0 +1,6 @@
+# "THE BELGIAN BEER-WARE LICENSE" (Revision 42):
+# <cortex@worlddomination.be> wrote this file. As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a belgian beer in return -- Laurent Peuch
+
+global_state = {}
