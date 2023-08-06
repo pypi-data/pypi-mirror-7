45b4c4c2a65d16d5c213d573f629295a41589c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:48 +0000

    Add package pybuilder_header_plugin/0.0.1/pybuilder_header_plugin-0.0.1.tar.gz

---
+++ packages/pybuilder_header_plugin/pybuilder_header_plugin-0.0.1.tar.gz/pybuilder_header_plugin-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'pybuilder_header_plugin',
+          version = '0.0.1',
+          description = '''PyBuilder Release PlugIn''',
+          long_description = '''Please visit https://github.com/aelgru/pybuilder_header_plugin for more information!''',
+          author = "Michael Gruber",
+          author_email = "aelgru@gmail.com",
+          license = 'Apache License, Version 2.0',
+          url = 'https://github.com/aelgru/pybuilder_header_plugin',
+          scripts = [],
+          packages = ['pybuilder_header_plugin'],
+          py_modules = [],
+          classifiers = ['Development Status :: 1 - Planning', 'Environment :: Console', 'Intended Audience :: Developers', 'License :: OSI Approved :: Apache Software License', 'Programming Language :: Python', 'Programming Language :: Python :: 2', 'Programming Language :: Python :: 2.7', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.0', 'Programming Language :: Python :: 3.1', 'Programming Language :: Python :: 3.2', 'Programming Language :: Python :: 3.3', 'Topic :: Utilities'],
+             #  data files
+             # package data
+          install_requires = [ "committer", "wheel" ],
+          
+          zip_safe=True
+    )
