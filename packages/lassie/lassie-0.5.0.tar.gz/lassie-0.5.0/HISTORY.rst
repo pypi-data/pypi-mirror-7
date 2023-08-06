678581c8a82a63171fb89baf0910c514580e118d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:41 +0000

    Add package lassie/0.5.0/lassie-0.5.0.tar.gz

---
+++ packages/lassie/lassie-0.5.0.tar.gz/lassie-0.5.0/HISTORY.rst
@@ -0,0 +1,45 @@
+.. :changelog:
+
+History
+-------
+
+0.5.0 (2014-06-23)
+++++++++++++++++++
+- Added ability to `fetch` links that are image files (jpg, gif, png, bmp)
+- Renamed `_retreive_content` to `_retrieve_content` because I evidently don't know how to spell correctly
+
+
+0.4.0 (2013-09-30)
+++++++++++++++++++
+- Updated `requests` and `beautifulsoup4` library versions
+- Added support for manipulating the request, see Advanced Usage docs
+- Fixed issue where `lassie.fetch` would break if the page had no title
+- Lassie is now more lenient when it comes to width and height values of images (now accepts integers (100) or integer with px (100px)
+- Image URLs for all images are now absolute
+
+0.3.0 (2013-08-15)
+++++++++++++++++++
+
+- Added support for `locale` to be returned. If `lang` is specified in the `html` tag and it normalizes to an actual locale, it will be added to the returned data.
+- Fixed bug where height was not being returned for body images
+- Added test coverage, we're 100% covered! :D
+
+
+0.2.1 (2013-08-13)
+++++++++++++++++++
+
+- Remove spaces from the returned keywords list
+- Fixed issue where favicon was not being retrieved
+- Fixed priority for class level vs method level params
+
+
+0.2.0 (2013-08-06)
+++++++++++++++++++
+
+- Fix package error when importing
+
+
+0.1.0 (2013-08-05)
+++++++++++++++++++
+
+- Initial Release
---
+++ packages/lassie/lassie-0.5.0.tar.gz/lassie-0.5.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = '0.5.0'
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'lassie',
+    'lassie.filters'
+]
+
+setup(
+    name='lassie',
+    version=__version__,
+    install_requires=[
+        'requests==2.3.0',
+        'beautifulsoup4==4.3.1',
+        'html5lib==1.0b3'
+    ],
+    author='Mike Helmick',
+    author_email='me@michaelhelmick.com',
+    license=open('LICENSE').read(),
+    url='https://github.com/michaelhelmick/lassie/tree/master',
+    keywords='lassie open graph web content scrape scraper',
+    description='Lassie is a Python library for retrieving basic content from websites',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    include_package_data=True,
+    packages=packages,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ]
+)
