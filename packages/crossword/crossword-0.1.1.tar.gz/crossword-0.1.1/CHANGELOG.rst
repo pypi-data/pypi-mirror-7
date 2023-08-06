1d9f752d06739f1742ccd72305692ba9b957756e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:34 +0000

    Add package crossword/0.1.1/crossword-0.1.1.tar.gz

---
+++ packages/crossword/crossword-0.1.1.tar.gz/crossword-0.1.1/CHANGELOG.rst
@@ -0,0 +1,14 @@
+Changelog
+=========
+
+0.1.1 (August 19, 2014)
+-----------------------
+
+* Fixed reading .ipuz puzzles that do not have a solution element.
+* Fixed reading .ipuz puzzles that do not have Across and/or Down clues.
+* Fixed writing .ipuz puzzles to only include "block" and/or "empty" when specified.
+
+0.1 (August 17, 2014)
+---------------------
+
+* Initial release.
---
+++ packages/crossword/crossword-0.1.1.tar.gz/crossword-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='crossword',
+    version='0.1.1',
+    description='Python library for handling crossword puzzles',
+    url='https://github.com/svisser/crossword',
+    author='Simeon Visser',
+    author_email='simeonvisser@gmail.com',
+    license='MIT',
+    packages=['crossword'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='crossword puzzle ipuz'
+)
