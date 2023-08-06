893eac2d406dd22fd1d5b840a839d97e85a9c72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:22 +0000

    Add package nester_hkm/1.2.0/nester_hkm-1.2.0.tar.gz

---
+++ packages/nester_hkm/nester_hkm-1.2.0.tar.gz/nester_hkm-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_hkm',
+	version		= '1.2.0',
+	py_modules	= ['nester_hkm'],
+	author		= 'hkm',
+	author_email	= 'hkm73560@gmail.com',
+	url		= 'http://blog.naver.com/hkm73560',
+	description	= 'A simple printer of mested utils',
+    )
