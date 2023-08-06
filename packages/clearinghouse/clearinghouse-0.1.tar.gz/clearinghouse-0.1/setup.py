efc2f9243e30fb901a7d7b3eb3ddc594937ce7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:38 +0000

    Add package clearinghouse/0.1/clearinghouse-0.1.tar.gz

---
+++ packages/clearinghouse/clearinghouse-0.1.tar.gz/clearinghouse-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'clearinghouse',
+  packages = ['clearinghouse'], # this must be the same as the name above
+  version = '0.1',
+  description = "Seattle's seash package",
+  author = 'Seattle',
+  author_email = 'cc4256@nyu.edu',
+  url = 'https://github.com/choksi81/clearinghouse', # use the URL to the github repo
+  download_url = 'https://github.com/choksi81/clearinghouse/tarball/0.1', # I'll explain this in a second
+  classifiers = [],
+)
