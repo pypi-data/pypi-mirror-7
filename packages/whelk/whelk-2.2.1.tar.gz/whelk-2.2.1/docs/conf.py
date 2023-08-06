05eefebd6ae45749d99c67188cd6df6f5a630347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:30 +0000

    Add package whelk/2.2.1/whelk-2.2.1.tar.gz

---
+++ packages/whelk/whelk-2.2.1.tar.gz/whelk-2.2.1/docs/conf.py
@@ -0,0 +1,16 @@
+import cloud_sptheme as csp
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'whelk'
+copyright = u'2010-2014, Dennis Kaarsemaker'
+version = '2.2'
+release = '2.2'
+pygments_style = 'sphinx'
+html_theme = 'cloud'
+html_theme_options = {
+    'roottarget': 'index',
+    'stickysidebar': False,
+}
+html_theme_path = [csp.get_theme_dir()]
+html_show_sourcelink = False
+html_show_sphinx = False
