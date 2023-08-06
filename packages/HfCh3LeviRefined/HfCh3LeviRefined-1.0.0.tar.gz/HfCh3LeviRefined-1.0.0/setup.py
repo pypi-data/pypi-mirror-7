22c78c5503fda11c5a1107e68b542d9bcbacd006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:21 +0000

    Add package HfCh3LeviRefined/1.0.0/HfCh3LeviRefined-1.0.0.tar.gz

---
+++ packages/HfCh3LeviRefined/HfCh3LeviRefined-1.0.0.tar.gz/HfCh3LeviRefined-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+	
+    	name='HfCh3LeviRefined',
+    	version='1.0.0',
+    	py_modules=['HfCh3LeviRefined'],
+    	author='levibinlee',
+    	author_email='levibinlee@gmail.com',
+    	url='http://www.headfirstlabs',
+	description='exception handling',
+   
+    #description=‘exception handling’,
+    #description='A simple printer of nested list element’,    
+)
+    
