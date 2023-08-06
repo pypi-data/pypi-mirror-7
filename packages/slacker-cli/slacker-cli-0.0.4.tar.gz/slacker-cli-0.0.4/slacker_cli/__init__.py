a924e40e6d9a0f826b4d1625e54c930366c69690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:32 +0000

    Add package slacker-cli/0.0.4/slacker-cli-0.0.4.tar.gz

---
+++ packages/slacker-cli/slacker-cli-0.0.4.tar.gz/slacker-cli-0.0.4/slacker_cli/__init__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    Send message to Slack from command line
+"""
+
+from slacker import Slacker
+import argparse
+import sys
+import os
+
+
+def post_message(token, channel, message):
+    channel = '#{}'.format(channel)
+
+    slack = Slacker(token)
+    slack.chat.post_message(channel, message)
+
+
+def args_priority(args, environ):
+    '''
+        priority of token
+        1) as argumment: -t
+        2) as environ variable
+    '''
+
+    arg_token = args.token
+
+    slack_token_var_name = 'SLACK_TOKEN'
+    if slack_token_var_name in environ.keys():
+        token = environ[slack_token_var_name]
+    else:
+        token = None
+
+    if arg_token:
+        token = arg_token
+
+    return token, args.channel
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--channel", help="Slack channel")
+    parser.add_argument("-t", "--token", help="Slack token")
+
+    token, channel = args_priority(parser.parse_args(), os.environ)
+    message = sys.stdin.read()
+
+    if not token or not channel or not message:
+        sys.exit(1)
+
+    post_message(token, channel, message)
+
+
+if __name__ == '__main__':
+    main()
