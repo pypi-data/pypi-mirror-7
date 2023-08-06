6f8d95dcf2bfd9c9dde65bd2e03df09821bd9c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:34 +0000

    Add package bar/0.2.1/bar-0.2.1.tar.gz

---
+++ packages/bar/bar-0.2.1.tar.gz/bar-0.2.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bar',
+      version='0.2.1',
+      description='Configurable progress bars/status monitors for Python console applications.',
+      url='https://github.com/utdemir/bar',
+      author='Utku Demir',
+      author_email='utdemir@gmail.com',
+      py_modules=['bar'],
+      classifiers=[
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+      )
