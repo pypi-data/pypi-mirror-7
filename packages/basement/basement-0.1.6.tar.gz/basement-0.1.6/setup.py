826609fe13752d217f4f05d53f68063f79b96e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:03 +0000

    Add package basement/0.1.6/basement-0.1.6.tar.gz

---
+++ packages/basement/basement-0.1.6.tar.gz/basement-0.1.6/setup.py
@@ -0,0 +1,29 @@
+"""A tool for generating Python project scaffolding based on
+user defined templates.
+
+"""
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+with open('README.md') as f:
+    description = f.read()
+
+setup(
+    name='basement',
+    description="A python project scaffolding generator.",
+    version='0.1.6',
+    long_description=description,
+    packages=['basement'],
+    author='Anthony Grimes',
+    author_email='i@raynes.me',
+    url='https://github.com/Raynes/basement',
+    license='MIT',
+    install_requires=requirements,
+    entry_points = """
+    [console_scripts]
+    basement=basement.__main__:ment
+    ment=basement.__main__:ment
+    """
+)
