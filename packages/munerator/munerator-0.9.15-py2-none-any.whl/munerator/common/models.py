46e63eb3def6066545744fedb699a0e302323f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:11 +0000

    Add package munerator/0.9.15/munerator-0.9.15-py2-none-any.whl

---
+++ packages/munerator/munerator-0.9.15-py2-none-any.whl/munerator/common/models.py
@@ -0,0 +1,34 @@
+__all__ = ['Players', 'Games', 'Votes']
+
+from mongoengine import (BooleanField, Document, IntField, DictField,
+                         ListField, ReferenceField, StringField)
+
+
+class Players(Document):
+    guid = StringField()
+    name = StringField()
+    names = ListField(StringField())
+    online = BooleanField()
+    score = IntField()
+    team = StringField()
+    team_id = IntField()
+    skill = IntField()
+
+
+class Games(Document):
+    timestamp = StringField()
+    mapname = StringField()
+    gametype = IntField()
+    players = ListField(ReferenceField(Players))
+    state = StringField()
+    start = StringField()
+    stop = StringField()
+    num_players = IntField()
+    current = BooleanField()
+    options = DictField()
+
+
+class Votes(Document):
+    game = ReferenceField(Games)
+    player = ReferenceField(Players)
+    vote = IntField()
