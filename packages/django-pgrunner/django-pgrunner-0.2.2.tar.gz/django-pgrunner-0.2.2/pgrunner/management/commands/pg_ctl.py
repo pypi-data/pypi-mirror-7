afbbc0b730875400f42b3dfa6b145797fd585cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:37 +0000

    Add package django-pgrunner/0.2.2/django-pgrunner-0.2.2.tar.gz

---
+++ packages/django-pgrunner/django-pgrunner-0.2.2.tar.gz/django-pgrunner-0.2.2/pgrunner/management/commands/pg_ctl.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.core.management.base import BaseCommand
+import subprocess
+from pgrunner import bin_path
+
+from pgrunner.commands import get_port, CURRENT
+
+class Command(BaseCommand):
+    help = 'Run pg_ctl with correct data dir'
+
+    def run_from_argv(self, argv):
+        pg_ctl = bin_path('pg_ctl')
+        if '--help' in argv:
+            cmd = [pg_ctl, '--help']
+        else:
+            cmd = [pg_ctl, '-D', CURRENT]
+            cmd.extend(argv[2:])
+        print(' '.join(cmd))
+        subprocess.call(cmd)
+
---
+++ packages/django-pgrunner/django-pgrunner-0.2.2.tar.gz/django-pgrunner-0.2.2/pgrunner/management/commands/pg_psql.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.core.management.base import BaseCommand, CommandError
+import subprocess
+from pgrunner import bin_path
+
+from pgrunner.commands import get_port
+
+class Command(BaseCommand):
+    help = 'Run psql with correct database'
+
+    def run_from_argv(self, argv):
+        port = get_port()
+        cmd = [bin_path('psql'), '-p', str(port), '-h', '127.0.0.1', 'django']
+        cmd.extend(argv[2:])
+        print(' '.join(cmd))
+        subprocess.call(cmd)
