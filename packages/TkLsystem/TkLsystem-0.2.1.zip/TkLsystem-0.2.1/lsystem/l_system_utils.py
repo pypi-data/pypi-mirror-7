28f101ca80d30e53f66ebb379de39f74c4b6f678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:03 +0000

    Add package TkLsystem/0.2.1/TkLsystem-0.2.1.zip

---
+++ packages/TkLsystem/TkLsystem-0.2.1.zip/TkLsystem-0.2.1/lsystem/l_system_utils.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import hashlib
+import lzma
+from os.path import join, isdir, isfile
+from os import makedirs
+from tempfile import gettempdir
+
+cachedir = join(gettempdir(), "lsd-cache")
+
+
+def serialize_l_system(string, rules):
+    items = [(k, v) for k, v in rules.items()]
+    items.sort(key=lambda k: k[0])
+    return string + ";" + ";".join("{}:{}".format(k, v) for k, v in items)
+
+
+def expand_string(string, rules):
+    parts = []
+    for x in string:
+        try:
+            parts.append(rules[x])
+        except KeyError:
+            parts.append(x)
+    return "".join(parts)
+
+
+def cached_expand_string(string, rules):
+
+    # don't cache minimal strings to decrase I/O time
+    if not len(string) > 5000:
+        return expand_string(string, rules)
+
+    if not isdir(cachedir):
+        makedirs(cachedir)
+
+    m = hashlib.sha256()
+
+    serialized_system = serialize_l_system(string, rules)
+    m.update(serialized_system.encode('utf-8'))
+    cachefile = join(cachedir, '{}.xz'.format(m.hexdigest()))
+
+    if isfile(cachefile):
+        with lzma.open(cachefile) as f:
+            return f.read().decode('utf-8')
+    else:
+        value = expand_string(string, rules)
+        with lzma.open(cachefile, "w") as f:
+            f.write(value.encode('utf-8'))
+        return value
+
+
+class PlaceHolder(object):
+    """So that we can track what is a placeholder"""
+    def __init__(self, value):
+        self.value = value
