00ac4aec3367c8baacf8fe1b17e2d029995ef306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:19 +0000

    Add package holy_crypt/1.0/holy_crypt-1.0.zip

---
+++ packages/holy_crypt/holy_crypt-1.0.zip/holy_crypt-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'holy_crypt',
+  packages = ['holy_crypt'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A holy encryption method to protect your strings and assets from the eyes of the blasphemes government and their demon pawns ',
+  author = 'Andrew Sampson',
+  author_email = 'andrew@codeusa523.org',
+  url = 'https://github.com/Codeusa/Holy-Crypt', # use the URL to the github repo
+  download_url = 'https://github.com/Codeusa/Holy-Crypt/archive/master.zip', # I'll explain this in a second
+  keywords = ['encryption', 'holy', 'secruity'], # arbitrary keywords
+  classifiers = [],
+)
