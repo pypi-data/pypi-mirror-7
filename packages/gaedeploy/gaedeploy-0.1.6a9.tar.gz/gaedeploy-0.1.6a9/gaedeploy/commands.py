26ae95956ccc8bdb0bdd5541556c1be1dd68cf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package gaedeploy/0.1.6a9/gaedeploy-0.1.6a9.tar.gz

---
+++ packages/gaedeploy/gaedeploy-0.1.6a9.tar.gz/gaedeploy-0.1.6a9/gaedeploy/commands.py
@@ -0,0 +1,25 @@
+import os
+import string
+from subprocess import call
+
+
+class BuildCommand(object):
+    def __init__(self, command, path, params=None):
+        self.command = command
+        self.path = path
+        self.params = params
+
+    def __call__(self):
+        command = string.split(self.command, ' ')
+        if self.params:
+            for key, value in self.params.iteritems():
+                # this option template should be in the yaml file so that the build
+                # tools can be swapped in and out properly
+                option = "'--%s=%s'" % (key, value)
+                command.append(option)
+            print command
+        os.chdir(self.path)
+        call(command)
+
+    def execute(self):
+        self()
