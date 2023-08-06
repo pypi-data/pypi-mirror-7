6a05afe2a55affa23bb47f1c07abd30f1a266854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:14 +0000

    Add package nose-watcher/0.1.2/nose-watcher-0.1.2.tar.gz

---
+++ packages/nose-watcher/nose-watcher-0.1.2.tar.gz/nose-watcher-0.1.2/README.rst
@@ -0,0 +1,51 @@
+===============================
+Nose-Watcher
+===============================
+
+.. image:: https://badge.fury.io/py/nose-watcher.png
+    :target: http://badge.fury.io/py/nose-watcher
+
+.. image:: https://travis-ci.org/solarnz/nose-watcher.png?branch=develop
+        :target: https://travis-ci.org/solarnz/nose-watcher
+
+.. image:: https://pypip.in/d/nose-watcher/badge.png
+        :target: https://pypi.python.org/pypi/nose-watcher
+
+.. image:: https://coveralls.io/repos/solarnz/nose-watcher/badge.png?branch=develop
+        :target: https://coveralls.io/r/solarnz/nose-watcher?branch=develop
+
+
+A nose plugin to watch for changes within the local directory.
+
+* Free software: BSD license
+* Documentation: http://nose-watcher.readthedocs.org.
+
+Inspired by the `nose-watch <https://github.com/lukaszb/nose-watch>`_ nose
+plugin.
+
+Note: nose-watcher will only run on linux, due to the depenency on
+`python-inotify` and `inotify`.
+
+Features
+--------
+
+* Watches for changes in the local directory, then runs nosetests with the
+  specified command line options.
+
+* Doesn't run the tests multiple times if you're using vim, Unlike the similar
+  plugin `nose-watch`.
+
+
+Installation
+------------
+
+.. code-block:: bash
+
+    pip install nose-watcher
+
+Usage
+-----
+
+.. code-block:: bash
+
+    nosetests --with-watcher
---
+++ packages/nose-watcher/nose-watcher-0.1.2.tar.gz/nose-watcher-0.1.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'python-inotify==0.6-test'
+]
+
+setup(
+    name='nose-watcher',
+    version='0.1.2',
+    description='A nose plugin to watch for changes within the local directory.',
+    long_description=readme + '\n\n' + history,
+    author='Chris Trotman',
+    author_email='chris@trotman.io',
+    url='https://github.com/solarnz/nose-watcher',
+    packages=[
+        'nose_watcher',
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='nose-watcher',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Testing'
+    ],
+    test_suite='tests',
+    entry_points={
+        'nose.plugins.0.10': [
+            'watcher = nose_watcher.nose_watcher:WatcherPlugin'
+        ]
+    },
+)
