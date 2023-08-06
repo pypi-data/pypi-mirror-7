884d93e5b43d08687ec602bbb28fc90ac042e8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:04 +0000

    Add package GmailCharlie/0.0.5/GmailCharlie-0.0.5.tar.gz

---
+++ packages/GmailCharlie/GmailCharlie-0.0.5.tar.gz/GmailCharlie-0.0.5/gmail/utils.py
@@ -0,0 +1,15 @@
+
+
+from .gmail import Gmail
+
+
+def login(username, password):
+    gmail = Gmail()
+    gmail.login(username, password)
+    return gmail
+
+
+def authenticate(username, access_token):
+    gmail = Gmail()
+    gmail.authenticate(username, access_token)
+    return gmail
