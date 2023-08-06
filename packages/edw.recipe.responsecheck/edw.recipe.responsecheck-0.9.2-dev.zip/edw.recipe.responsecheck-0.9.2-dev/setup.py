bb1e1ff20d616693ca3d49acb58aac7e49865c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:01 +0000

    Add package edw.recipe.responsecheck/0.9.2-dev/edw.recipe.responsecheck-0.9.2-dev.zip

---
+++ packages/edw.recipe.responsecheck/edw.recipe.responsecheck-0.9.2-dev.zip/edw.recipe.responsecheck-0.9.2-dev/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Installer
+"""
+import os
+from setuptools import setup, find_packages
+
+
+NAME = 'edw.recipe.responsecheck'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
+
+entry_point = 'edw.recipe.responsecheck:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+setup(name=NAME,
+      version=VERSION,
+      description="A recipe that generates bash script for testing connectivity to application servers",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Framework :: Zope2',
+        'Framework :: Zope3',
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='edw recipe zope plone connectivity server',
+      author='Eau de Web',
+      author_email='vitalie.maldur@eaudeweb.ro',
+      url='http://github.com/eaudeweb/edw.recipe.responsecheck',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['edw', 'edw.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      entry_points=entry_points,
+      )
