17662e8ab607900ccb189a43e7118675d05ed6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:00 +0000

    Add package python-fields/0.0/python-fields-0.0.tar.gz

---
+++ packages/python-fields/python-fields-0.0.tar.gz/python-fields-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-fields',
+    version="0.0",
+    url='https://pypi.python.org/pypi/fields/',
+    description='An example package. Replace this with a proper project description. Generated with https://github.com/ionelmc/cookiecutter-pylibrary',
+    long_description='''Use `fields <https://pypi.python.org/pypi/fields/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['fields'],
+)
