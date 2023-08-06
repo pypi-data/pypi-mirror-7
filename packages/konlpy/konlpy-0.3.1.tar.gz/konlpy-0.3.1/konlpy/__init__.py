f70728dafc9a404f01cc2271b748e72a164148de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:39 +0000

    Add package konlpy/0.3.1/konlpy-0.3.1.tar.gz

---
+++ packages/konlpy/konlpy-0.3.1.tar.gz/konlpy-0.3.1/konlpy/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'KoNLPy'
+__version__ = '0.3.1'
+__author__ = 'Lucy Park'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2014 Lucy Park'
+
+from .jvm import init_jvm
+from . import corpus
+from . import tag
---
+++ packages/konlpy/konlpy-0.3.1.tar.gz/konlpy-0.3.1/konlpy/tag/__init__.py
@@ -0,0 +1,6 @@
+from ._hannanum import Hannanum
+from ._kkma import Kkma
+try:
+    from ._mecab import Mecab
+except ImportError:
+    pass
