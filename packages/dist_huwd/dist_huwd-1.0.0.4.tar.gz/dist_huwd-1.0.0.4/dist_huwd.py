20d194ffd968144344e11a744f0c298adc63361f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:46 +0000

    Add package dist_huwd/1.0.0.4/dist_huwd-1.0.0.4.tar.gz

---
+++ packages/dist_huwd/dist_huwd-1.0.0.4.tar.gz/dist_huwd-1.0.0.4/dist_huwd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+'''
+  this is my first python package for distribution
+  just for test,thx
+'''
+
+
+def helloworld(name='jaylee'):
+  print 'helloworld %s' %name
---
+++ packages/dist_huwd/dist_huwd-1.0.0.4.tar.gz/dist_huwd-1.0.0.4/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'dist_huwd',
+  version='1.0.0.4',
+  py_modules =['dist_huwd'],
+  author = 'jaylee',
+  author_email = 'jaylee@163.com',
+  url = 'http://www.jaylee.cc',
+  description = 'A simple test'
+  )
