4c033216950dc383d8eec92d87dff30db9209fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package mylogger/1.8/mylogger-1.8.tar.gz

---
+++ packages/mylogger/mylogger-1.8.tar.gz/mylogger-1.8/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='mylogger',
+      version='1.8',
+      description='My Logger',
+      author='Andrey Tykhonov',
+      author_email='atykhonov@gmail.com',
+      packages=['mylogger'],)
