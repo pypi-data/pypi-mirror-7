ac1737ded6233a0b733c8a75337dd8d3f4948d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:31 +0000

    Add package prov/1.1.0/prov-1.1.0.tar.gz

---
+++ packages/prov/prov-1.1.0.tar.gz/prov-1.1.0/prov/serializers/__init__.py
@@ -0,0 +1,45 @@
+__author__ = 'Trung Dong Huynh'
+__email__ = 'trungdong@donggiang.com'
+
+__all__ = [
+    'get'
+]
+
+from prov import Error
+
+
+class DoNotExist(Error):
+    pass
+
+
+
+
+class Registry:
+    serializers = None
+
+    @staticmethod
+    def load_serializers():
+        from prov.serializers.provjson import ProvJSONSerializer
+        from prov.serializers.provxml import ProvXMLSerializer
+
+        Registry.serializers = {
+            'json': ProvJSONSerializer,
+            'xml': ProvXMLSerializer
+        }
+
+
+def get(format_name):
+    """
+    Returns the serializer class for the specified format. Raises a DoNotExist
+    """
+    # Lazily initialize the list of available serializers to avoid cyclic imports
+    if Registry.serializers is None:
+        Registry.load_serializers()
+    try:
+        return Registry.serializers[format_name]
+    except KeyError:
+        raise DoNotExist(
+            'No serializer avaliable for the format "%s"' % format_name
+        )
+
+
