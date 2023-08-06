3d21d0ce5ae2b60dbcf3da2f4f78af76b68824ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package infi.asi/0.3.27/infi.asi-0.3.27.tar.gz

---
+++ packages/infi.asi/infi.asi-0.3.27.tar.gz/infi.asi-0.3.27/src/infi/asi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.27"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.3.27)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f34b7a441dd18ef0660457168c481868c7339dc8'
+__git_head_subject__ = 'Finished release v0.3.27.'
+__git_head_message__ = "* release/v0.3.27:\n  buildout.cfg: adding infi.instruct>=0.6.25 to requirements\n  buildout.cfg: remove infi.instruct>=0.6.18 from requirements\n  STORAGEMODEL-288 fixing LogicalBlockProvisioningPageCommand\n  STORAGEMODEL-288 fixing DeviceIdentificationVPDPageBuffer\n  STORAGEMODEL-288 fixing json pages\n  STORAGEMODEL-288 fixing SupportedVPDPagesBuffer\n  STORAGEMODEL-288 fixing UnknownVPDPageCommand\n  STORAGEMODEL-288 refactoring inquiriy pages on top of instruct's Buffer\n  STORAGEMODEL-286 inquiry page 'b2h\n  STORAGEMODEL-287 refactoring DeviceIdentificationPage on top of instruct's Buffer"
+__git_dirty_diff__ = ''
