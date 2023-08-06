d7de6524249161181565a7de6a8af2559de4030d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:17 +0000

    Add package django-xworkflows/0.9.3/django-xworkflows-0.9.3.tar.gz

---
+++ packages/django-xworkflows/django-xworkflows-0.9.3.tar.gz/django-xworkflows-0.9.3/tests/runner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2013 Raphaël Barrois
+# This code is distributed under the two-clause BSD license.
+
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from django.conf import settings
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'tests.djworkflows',
+            'django_xworkflows',
+            'django_xworkflows.xworkflow_log',
+        ]
+    )
+
+from django.test import simple
+
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ('djworkflows',)
+    runner = simple.DjangoTestSuiteRunner(failfast=False)
+    failures = runner.run_tests(test_args)
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
+
