2face8af27cfe0477e59bac4cf6f1c3600f05260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:41 +0000

    Add package fixedint/0.1.1/fixedint-0.1.1.tar.gz

---
+++ packages/fixedint/fixedint-0.1.1.tar.gz/fixedint-0.1.1/setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name = 'fixedint',
+    version = '0.1.1',
+    author = 'Robert Xiao',
+    author_email = 'robert.bo.xiao@gmail.com',
+    url = 'https://github.com/nneonneo/fixedint',
+    license = 'PSF',
+    classifiers = [
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Utilities",
+    ],
+    description = "simple fixed-width integers",
+    packages = ['fixedint'],
+    long_description=long_description,
+)
