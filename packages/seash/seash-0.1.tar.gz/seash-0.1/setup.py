831d7885054ee7ad0c9a3a58065800404b7e64d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:46 +0000

    Add package seash/0.1/seash-0.1.tar.gz

---
+++ packages/seash/seash-0.1.tar.gz/seash-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'seash',
+  packages = ['seash'], # this must be the same as the name above
+  version = '0.1',
+  description = "Seattle's seash package",
+  author = 'Seattle',
+  author_email = 'aot221@nyu.edu',
+  url = 'https://github.com/SeattleTestbed/seash', # use the URL to the github repo
+  download_url = 'https://github.com/aot221/seash/tarball/0.1', # I'll explain this in a second
+  classifiers = [],
+)
