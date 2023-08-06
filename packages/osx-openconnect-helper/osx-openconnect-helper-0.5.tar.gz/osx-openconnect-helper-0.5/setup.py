ac6e5e8a05c93cfbc9ba872eb963b95375d94a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:35 +0000

    Add package osx-openconnect-helper/0.5/osx-openconnect-helper-0.5.tar.gz

---
+++ packages/osx-openconnect-helper/osx-openconnect-helper-0.5.tar.gz/osx-openconnect-helper-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='osx-openconnect-helper',
+    version='0.5',
+    description='Wrapper around openconnect and the system keychain',
+    license='BSD',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    url='https://github.com/mitsuhiko/osx-openconnect-helper',
+    py_modules=['openconnect_helper'],
+    install_requires=[
+        'Click',
+        'toml',
+        'requests',
+    ],
+    entry_points='''
+    [console_scripts]
+    openconnect-helper=openconnect_helper:cli
+    '''
+)
