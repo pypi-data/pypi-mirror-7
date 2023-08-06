73c42e2ef0ae4533b4d7c355f4d379c160ee16f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:05 +0000

    Add package munerator/0.8.8/munerator-0.8.8.tar.gz

---
+++ packages/munerator/munerator-0.8.8.tar.gz/munerator-0.8.8/munerator/common/models.py
@@ -0,0 +1,28 @@
+from mongoengine import Document, ListField, StringField, IntField, BooleanField, ReferenceField
+
+
+class Players(Document):
+    guid = StringField()
+    name = StringField()
+    names = ListField(StringField())
+    online = BooleanField()
+    score = IntField()
+    team = StringField()
+    team_id = IntField()
+
+
+class Games(Document):
+    game_id = StringField()
+    mapname = StringField()
+    players = ListField(ReferenceField(Players))
+    state = StringField()
+    start = StringField()
+    stop = StringField()
+    num_players = IntField()
+    current = BooleanField()
+
+
+class Votes(Document):
+    game = ReferenceField(Games)
+    player = ReferenceField(Players)
+    vote = IntField()
