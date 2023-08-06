dc29db41287a7276d5b73efed3da06be6236dd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:41 +0000

    Add package sshtunnel/0.0.1/sshtunnel-0.0.1.zip

---
+++ packages/sshtunnel/sshtunnel-0.0.1.zip/sshtunnel-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+__author__ = 'pahaz'
+
+appname = 'sshtunnel'
+app = __import__(appname)
+version = app.__version__
+
+setup(
+    name=appname,
+    version=version,
+    description="Initiate SSH tunnels",
+    long_description=open('README.md').read(),
+    py_modules=[appname],
+    install_requires=['paramiko>=1.12.3'],
+    author='Pahaz Blinov',
+    author_email='pahaz.blinov@gmail.com',
+    url='https://github.com/pahaz/sshtunnel',
+    download_url='https://github.com/pahaz/sshtunnel/tarball/' + version,
+    keywords=['SSH', 'proxy', 'TCP forwarder'],
+    license='MIT',
+    platforms=['unix', 'macos', 'windows'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet :: Proxy Servers',
+        'Topic :: Security :: Cryptography',
+    ],
+)
+
+# TODO: add entry_point
