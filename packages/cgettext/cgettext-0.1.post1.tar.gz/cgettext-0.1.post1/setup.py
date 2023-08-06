aa32f71ed46455987364d8f8d947f2baa5b64f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:07 +0000

    Add package cgettext/0.1.post1/cgettext-0.1.post1.tar.gz

---
+++ packages/cgettext/cgettext-0.1.post1.tar.gz/cgettext-0.1.post1/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+import platform
+
+
+if platform.python_implementation() == 'CPython':
+    from distutils.command.bdist import bdist as _bdist
+    from distutils.command.sdist import sdist as _sdist
+    from distutils.extension import Extension
+
+    class sdist(_sdist):
+        # sdist override that forces a Cython rebuild of _cgettext.c
+        def run(self):
+            from Cython.Build import cythonize
+            cythonize(['_cgettext.pyx'])
+            _sdist.run(self)
+
+    class bdist(_bdist):
+        # same story
+        def run(self):
+            from Cython.Build import cythonize
+            cythonize(['_cgettext.pyx'])
+            _bdist.run(self)
+
+    cmdclass = dict(
+        bdist=bdist,
+        sdist=sdist,
+    )
+    ext_modules = [Extension('_cgettext', ['_cgettext.c'])]
+else:
+    cmdclass = dict()
+    ext_modules = []
+
+
+setup(
+    name='cgettext',
+    version='0.1.post1',
+    description='This is a drop-in C replacement for Python\'s gettext.',
+    author='Eevee (Alex Munroe)',
+    author_email='eevee.pypi@veekun.com',
+    url='https://github.com/eevee/cgettext',
+    license='MIT',
+
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    py_modules=['cgettext'],
+)
