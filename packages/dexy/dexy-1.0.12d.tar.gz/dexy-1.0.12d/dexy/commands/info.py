3d79e1b8fadbe48c20efd875932cca07582c1d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:49 +0000

    Add package dexy/1.0.12d/dexy-1.0.12d.tar.gz

---
+++ packages/dexy/dexy-1.0.12d.tar.gz/dexy-1.0.12d/dexy/commands/info.py
@@ -0,0 +1,159 @@
+from dexy.batch import Batch
+from dexy.commands.utils import init_wrapper
+from dexy.commands.utils import print_indented
+from dexy.commands.utils import print_rewrapped
+from dexy.utils import defaults
+from operator import attrgetter
+import dexy.exceptions
+import sys
+
+### "info-keys"
+info_attrs = [
+        'name',
+        'ext',
+        'key'
+        ]
+
+info_methods = [
+        'title',
+        'basename',
+        'filesize',
+        'baserootname',
+        'parent_dir',
+        'long_name',
+        'web_safe_document_key'
+        ]
+
+storage_methods = []
+### @end
+
+def links_command(
+        **kwargs
+        ):
+    """
+    Print list of links and sections found in dexy documents.
+    """
+    artifactsdir = kwargs.get('artifactsdir', defaults['artifacts_dir'])
+    wrapper = init_wrapper(locals())
+    batch = Batch.load_most_recent(wrapper)
+
+    if not batch:
+        print "you need to run dexy first"
+        sys.exit(1)
+
+    wrapper.setup_log()
+    wrapper.batch = batch
+
+    wrapper.add_lookups()
+
+    if wrapper.lookup_nodes:
+        print_indented("Nodes:")
+    for label in sorted(wrapper.lookup_nodes):
+        nodes = wrapper.lookup_nodes[label]
+        if len(nodes) > 1:
+            print ''
+            print_indented("'%s'" % label, 2)
+            print_indented("Multiple nodes match %s:" % label, 4)
+            for node in nodes:
+                print_indented(">> %r" % node, 6)
+        elif len(nodes) == 0:
+            print_indented("'%s'" % label, 2)
+            print_indented("NO nodes match %s" % label, 4)
+        else:
+            node = nodes[0]
+            print_indented("'%s'" % label, 2)
+            print_indented("%r" % node, 4)
+        print ''
+
+    print ''
+
+    if wrapper.lookup_sections:
+        print_indented("Sections:")
+    for label in sorted(wrapper.lookup_sections):
+        node = wrapper.lookup_sections[label][0]
+        print_indented("'%s'" % label, 2)
+        print_indented("%r" % node, 4)
+        print ''
+
+### "info-com"
+def info_command(
+        __cli_options=False,
+        expr="", # An expression partially matching document name.
+        key="", # The exact document key.
+        ws=False, # Whether to print website reporter keys and values.
+        **kwargs
+        ):
+    """
+    Prints metadata about a dexy document.
+
+    Dexy must have already run successfully.
+
+    You can specify an exact document key or an expression which matches part
+    of a document name/key. The `dexy grep` command is available to help you
+    search for documents and print document contents.
+    """
+    artifactsdir = kwargs.get('artifactsdir', defaults['artifacts_dir'])
+    wrapper = init_wrapper(locals())
+    wrapper.setup_log()
+    batch = Batch.load_most_recent(wrapper)
+    wrapper.batch = batch
+
+    if expr:
+        print "search expr:", expr
+        matches = sorted([data for data in batch if expr in data.key],
+                key=attrgetter('key'))
+    elif key:
+        matches = sorted([data for data in batch if key == data.key],
+                key=attrgetter('key'))
+    else:
+        raise dexy.exceptions.UserFeedback("Must specify either expr or key")
+
+    for match in matches:
+        print ""
+        print "  Info for Document '%s'" % match.key
+        print ""
+        print "  document output data type:", match.alias
+        print ""
+
+        print_indented("settings:", 2)
+        for k in sorted(match._instance_settings):
+            if not k in ('aliases', 'help'):
+                print_indented("%s: %s" % (k, match.setting(k)), 4)
+
+        print ""
+        print_indented("attributes:", 2)
+        for fname in sorted(info_attrs):
+            print_indented("%s: %s" % (fname, getattr(match, fname)), 4)
+        print ""
+    
+        print_indented("methods:", 2)
+        for fname in sorted(info_methods):
+            print_indented("%s(): %s" % (fname, getattr(match, fname)()), 4)
+        print ""
+
+        if storage_methods:
+            print_indented("storage methods:", 2)
+            for fname in sorted(storage_methods):
+                print_indented("%s(): %s" % (fname, getattr(match.storage, fname)), 4)
+            print ''
+
+        if ws:
+            print_indented("website reporter methods:", 2)
+            print ''
+            reporter = dexy.reporter.Reporter.create_instance('ws')
+            reporter.wrapper = wrapper
+            reporter.setup_navobj()
+            reporter.help(match)
+            print ''
+            print_indented("active template plugins are:", 2)
+            print_indented(", ".join(reporter.setting('plugins')), 4)
+            print ''
+
+
+        else:
+            print_indented("For website reporter tags, run this command with -ws option", 4)
+            print ''
+
+
+        print_rewrapped("""For more information about methods available on this
+        data type run `dexy datas -alias %s`""" % match.alias)
---
+++ packages/dexy/dexy-1.0.12d.tar.gz/dexy-1.0.12d/dexy/filters/wordpress.py
@@ -0,0 +1,184 @@
+from dexy.filters.api import ApiFilter
+import dexy.exceptions
+import json
+import xmlrpclib
+
+try:
+    import mimetypes
+    wp_aliases = ['wp', 'wordpress']
+except UnicodeDecodeError:
+    print "Unable to load mimetypes library. WordPressFilter will not work. See http://bugs.python.org/issue9291"
+    mimetypes = None
+    wp_aliases = []
+
+class WordPressFilter(ApiFilter):
+    """
+    Posts to a WordPress blog.
+
+    WordPress has a very confusing API setup since it implements its own API
+    methods (under the wp namespace) and also supports the Blogger, metaWeblog
+    and MovableType APIs. Unfortunately the wp namespace methods are
+    incomplete, so you have to mix and match.
+
+    Uses the WP XMLRPC API where possible:
+    http://codex.wordpress.org/XML-RPC_wp
+
+    Creating and editing blog posts uses the metaWeblog API:
+    http://xmlrpc.scripting.com/metaWeblogApi.html
+
+    If this filter is applied to a document with file extension in
+    PAGE_CONTENT_EXTENSIONS (defined in ApiFilter class and inherited here)
+    then the document will be uploaded to WordPress as a blog post.
+
+    If not, then the document is assumed to be an image or other binary asset,
+    and file upload will be used instead, so a new element will be added to the
+    Media Library. If this is the case, then the URL is the resulting image is
+    returned, so you can use that URL directly in your blog posts or other
+    documents that need to link to the asset.
+
+    IMPORTANT There is currently a frustrating bug in WP:
+    http://core.trac.wordpress.org/ticket/17604
+    which means that every time you run this filter, a *new* image asset will
+    be created, even though we tell WordPress to overwrite the existing image
+    of the same name. You will end up with dozens of copies of this image
+    cluttering up your media library.
+
+    For now, we recommend using an external site to host your images and
+    assets, such as Amazon S3.
+    """
+    aliases = wp_aliases
+
+    _settings = {
+            'blog-id' : ("The wordpress blog id.", 0),
+            'page-content-extensions' : ('', ['.md', '.txt', '.html']),
+            'document-api-config-file' : 'wordpress.json',
+            'api-key-name' : 'wordpress',
+            'output-extensions' : ['.txt']
+            }
+
+    def docmd_create_keyfile(self):
+        """
+        Creates a key file for WordPress in the local directory.
+        """
+        self.create_keyfile("project-api-key-file")
+
+    def api_url(self):
+        base_url = self.read_param('url')
+        if base_url.endswith("xmlrpc.php"):
+            return base_url
+        else:
+            if not base_url.endswith("/"):
+                base_url = "%s/" % base_url
+            return "%sxmlrpc.php" % base_url
+
+    def api(klass):
+        if not hasattr(klass, "_api"):
+            klass._api = xmlrpclib.ServerProxy(klass.api_url())
+        return klass._api
+
+    def docmd_list_methods(klass):
+        """
+        List API methods exposed by WordPress API.
+        """
+        for method in sorted(klass.api().system.listMethods()):
+            print method
+
+    def docmd_list_categories(self):
+        """
+        List available blog post categories.
+        """
+        username = self.read_param('username')
+        password = self.read_param('password')
+        headers = ['categoryName']
+        print "\t".join(headers)
+        for category_info in self.api().wp.getCategories(self.setting('blog-id'), username, password):
+            print "\t".join(category_info[h] for h in headers)
+
+    def upload_page_content(self):
+        input_text = unicode(self.input_data)
+        document_config = self.read_document_config()
+
+        document_config['description'] = input_text
+        post_id = document_config.get('postid')
+        publish = document_config.get('publish', False)
+
+        for key, value in document_config.iteritems():
+            if not key == "description":
+                self.log_debug("%s: %s" % (key, value))
+
+        if post_id:
+            self.log_debug("Making editPost API call.")
+            self.api().metaWeblog.editPost(
+                    post_id,
+                    self.read_param('username'),
+                    self.read_param('password'),
+                    document_config,
+                    publish
+                    )
+        else:
+            self.log_debug("Making newPost API call.")
+            post_id = self.api().metaWeblog.newPost(
+                    self.setting('blog-id'),
+                    self.read_param('username'),
+                    self.read_param('password'),
+                    document_config,
+                    publish
+                    )
+            document_config['postid'] = post_id
+
+        self.log_debug("Making getPost API call.")
+        post_info = self.api().metaWeblog.getPost(
+                post_id,
+                self.read_param('username'),
+                self.read_param('password')
+                )
+
+        for key, value in post_info.iteritems():
+            if key in ('date_modified_gmt', 'dateCreated', 'date_modified', 'date_created_gmt',):
+                post_info[key] = value.value
+
+            if not key == "description":
+                self.log_debug("%s: %s" % (key, value))
+
+        del document_config['description']
+        document_config['publish'] = publish
+        self.save_document_config(document_config)
+
+        if publish:
+            self.output_data.set_data(post_info['permaLink'])
+        else:
+            self.output_data.set_data(json.dumps(post_info))
+
+    def upload_image_content(self):
+        with open(self.input_data.storage.data_file(), 'rb') as f:
+            image_base_64 = xmlrpclib.Binary(f.read())
+
+            upload_file = {
+                     'name' : self.work_input_filename(),
+                     'type' : mimetypes.types_map[self.prev_ext],
+                     'bits' : image_base_64,
+                     'overwrite' : 'true'
+                     }
+
+            upload_result = self.api().wp.uploadFile(
+                     self.setting('blog-id'),
+                     self.read_param('username'),
+                     self.read_param('password'),
+                     upload_file
+                     )
+
+            self.log_debug("wordpress upload results: %s" % upload_result)
+            url = upload_result['url']
+            self.log_debug("uploaded %s to %s" % (self.key, url))
+
+        self.output_data.set_data(url)
+
+    def process(self):
+        try:
+            if self.prev_ext in self.setting('page-content-extensions'):
+                self.upload_page_content()
+            else:
+                self.upload_image_content()
+
+        except xmlrpclib.Fault as e:
+            raise dexy.exceptions.UserFeedback(unicode(e))
