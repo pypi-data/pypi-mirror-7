af12abf883c0b329c5ea9b2bbe5e04058bdc2c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:30 +0000

    Add package dist_wangkun/1.0.0/dist_wangkun-1.0.0.tar.gz

---
+++ packages/dist_wangkun/dist_wangkun-1.0.0.tar.gz/dist_wangkun-1.0.0/dist_wangkun.py
@@ -0,0 +1,8 @@
+#/usr/bin/env python
+'''
+   this is my first python package for distribution
+   just for test, thx
+'''
+def helloworld(name='wangkun'):
+    print 'hello world,%s' % name
+    
---
+++ packages/dist_wangkun/dist_wangkun-1.0.0.tar.gz/dist_wangkun-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'dist_wangkun',
+    version='1.0.0',
+    py_modules=['dist_wangkun'],
+    author = 'wangkun',
+    author_email = 'wangkun@6rooms.com',
+    description = 'A simple test'
+)
