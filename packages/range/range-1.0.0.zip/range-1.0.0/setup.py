1cddbc476428cbd28581de712ec3edeae1271685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:05 +0000

    Add package range/1.0.0/range-1.0.0.zip

---
+++ packages/range/range-1.0.0.zip/range-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name           = 'range',
+    version        = '1.0.0',
+    py_modules     =['range'],
+    author         = 'Rithish',
+    author_email   = 'rithishvisakoti@gmail.com',
+    url            = 'http://www.starthabit.com',
+    description    = 'Printer of nested lists with Range'
+    )
