fd488c95d310f57bce330be60e758164e2f8cf9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package django-silk/0.1.1/django-silk-0.1.1.tar.gz

---
+++ packages/django-silk/django-silk-0.1.1.tar.gz/django-silk-0.1.1/silk/config.py
@@ -0,0 +1,27 @@
+from copy import copy
+from silk.singleton import Singleton
+
+import six
+
+
+class SilkyConfig(six.with_metaclass(Singleton, object)):
+
+    defaults = {
+        'SILKY_DYNAMIC_PROFILING': [],
+        'SILKY_IGNORE_PATHS': [],
+        'SILKY_HIDE_COOKIES': True,
+        'SILKY_IGNORE_QUERIES': []
+    }
+
+    def _setup(self):
+        from django.conf import settings
+        options = {option: getattr(settings, option) for option in dir(settings) if option.startswith('SILKY')}
+        self.attrs = copy(self.defaults)
+        self.attrs.update(options)
+
+    def __init__(self):
+        super(SilkyConfig, self).__init__()
+        self._setup()
+
+    def __getattr__(self, item):
+        return self.attrs[item]
