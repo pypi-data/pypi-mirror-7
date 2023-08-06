ec1545857d5f7ca82a35dd2992a935f380686a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:33 +0000

    Add package pymntos/0.1/pymntos-0.1.tar.gz

---
+++ packages/pymntos/pymntos-0.1.tar.gz/pymntos-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymntos',
+      version='0.1',
+      description='PyMNtos library',
+      url='http://bitbucket.org/wyattwalter/pymntos-lib',
+      author='Wyatt Walter',
+      author_email='wyattwalter@gmail.com',
+      license='MIT',
+      packages=['pymntos'],
+      scripts=['bin/pymntos'])
