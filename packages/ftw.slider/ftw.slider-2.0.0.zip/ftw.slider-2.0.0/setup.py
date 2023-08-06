1ccaaebae6c64e3255c915888a45807d4284cecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:43 +0000

    Add package ftw.slider/2.0.0/ftw.slider-2.0.0.zip

---
+++ packages/ftw.slider/ftw.slider-2.0.0.zip/ftw.slider-2.0.0/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0.0'
+
+tests_require = [
+    'ftw.testing [splinter]',
+    'ftw.builder',
+    'ftw.testbrowser',
+    'plone.app.testing',
+    'plone.resource',
+    'unittest2',
+    ]
+
+setup(name='ftw.slider',
+      version=version,
+      description='Slider content type for Plone.',
+      long_description=open('README.rst').read() + '\n' +
+      open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone',
+        'Programming Language :: Python',
+        ],
+      keywords='',
+
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.slider',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'ftw.upgrade',
+        'setuptools',
+        'Products.GenericSetup',
+        'Products.CMFCore',
+        'Products.CMFPlone',
+        'plone.behavior',
+        'plone.formwidget.contenttree',
+        'plone.namedfile',
+        'plone.dexterity',
+        'plone.app.dexterity',
+        'plone.directives.form',
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points='''
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
