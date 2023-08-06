f9351b8d07798ce8a066a6309f6b44026e7f6ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:01 +0000

    Add package django-master/0.1.4/django-master-0.1.4.tar.gz

---
+++ packages/django-master/django-master-0.1.4.tar.gz/django-master-0.1.4/django_master/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls import patterns, url
+from django.conf.urls.static import static
+
+from django_master.views import HomeView
+from django_master.views.routes import RoutesListView
+from django_master.views.settings import SettingsListView
+
+
+STATIC_ACTIVATED = getattr(settings, 'DJANGO_MASTER_STATIC_ACTIVATED', False)
+
+
+urlpatterns = patterns(r'',
+
+    url(r'^$', HomeView.as_view(), name='djangomaster_home'),
+    url(r'^routes/$', RoutesListView.as_view(), name='djangomaster_routes'),
+    url(r'^settings/$', SettingsListView.as_view(),
+        name='djangomaster_settings'),
+
+)
+
+if STATIC_ACTIVATED:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
