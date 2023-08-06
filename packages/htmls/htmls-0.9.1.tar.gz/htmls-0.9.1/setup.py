cf631683032d17dcc135a615651f1202207ace5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:01 +0000

    Add package htmls/0.9.1/htmls-0.9.1.tar.gz

---
+++ packages/htmls/htmls-0.9.1.tar.gz/htmls-0.9.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='htmls',
+    description='Makes it easy to write use CSS selectors with HTML in your unit tests.',
+    version='0.9.1',
+    license='BSD',
+    url='https://github.com/espenak/htmls',
+    author='Espen Angell Kristiansen',
+    author_email='post@espenak.net',
+    packages=find_packages(),
+    install_requires=[
+        'lxml',
+        'html5lib',
+    ],
+    include_package_data=True,
+    long_description='See https://github.com/espenak/htmls',
+    zip_safe=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python'
+    ],
+    test_suite='tests'
+)
