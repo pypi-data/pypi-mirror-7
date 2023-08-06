1ac23506b4af13996bc6a3568a2645b84a11bf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:13 +0000

    Add package haven/1.0.65/haven-1.0.65.tar.gz

---
+++ packages/haven/haven-1.0.65.tar.gz/haven-1.0.65/haven/haven.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from .callbacks_mixin import AppCallBacksMixin
+from . import autoreload
+
+
+class Haven(AppCallBacksMixin):
+    debug = False
+    got_first_request = False
+    blueprints = None
+
+    def __init__(self):
+        super(Haven, self).__init__()
+        self.blueprints = list()
+
+    def register_blueprint(self, blueprint):
+        blueprint.register2app(self)
+
+    def run(self, host, port, debug=None, use_reloader=None):
+        if debug is not None:
+            self.debug = debug
+
+        if use_reloader is None:
+            use_reloader = self.debug
+
+        if use_reloader:
+            autoreload.main(self._run, (host, port))
+        else:
+            self._run(host, port)
+
+    def _run(self, host, port):
+        raise NotImplementedError
