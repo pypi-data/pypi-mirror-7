c4ee195502d6cdbe708a4f681fca4a06ec677bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:23 +0000

    Add package auth-userpass/0.3.0/auth-userpass-0.3.0.tar.gz

---
+++ packages/auth-userpass/auth-userpass-0.3.0.tar.gz/auth-userpass-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+#parse requirements
+req_lines = [line.strip() for line in open("requirements.txt").readlines()]
+install_reqs = list(filter(None, req_lines))
+
+setup(
+    name = "auth-userpass",
+    version = "0.3.0",
+    author = "Francis Luong (Franco)",
+    author_email = "networkascode@definefunk.com",
+    description = ("Simple but not so secure username and password storage and retrieval"),
+    license = "LICENSE.txt",
+    url = "https://github.com/francisluong/py-auth-userpass",
+    install_requires=install_reqs,
+    packages=['auth']
+)
