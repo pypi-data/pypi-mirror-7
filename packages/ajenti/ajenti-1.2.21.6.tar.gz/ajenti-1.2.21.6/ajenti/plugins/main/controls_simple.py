f57a9a1740d4bfca0e11ed62c3221509806a1e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package ajenti/1.2.21.6/ajenti-1.2.21.6.tar.gz

---
+++ packages/ajenti/ajenti-1.2.21.6.tar.gz/ajenti-1.2.21.6/ajenti/plugins/main/controls_simple.py
@@ -0,0 +1,43 @@
+from ajenti.api import *
+from ajenti.ui import p, UIElement
+
+
+@p('text', default='', bindtypes=[str, unicode, int, float])
+@plugin
+class Label (UIElement):
+    typeid = 'label'
+
+
+@p('text', default='', bindtypes=[str, unicode, int])
+@plugin
+class Tooltip (UIElement):
+    typeid = 'tooltip'
+
+
+@p('icon', default=None, bindtypes=[str, unicode])
+@plugin
+class Icon (UIElement):
+    typeid = 'icon'
+
+
+@p('text', default='', bindtypes=[str, unicode])
+@p('icon', default=None)
+@p('warning', default=None)
+@plugin
+class Button (UIElement):
+    typeid = 'button'
+
+
+@p('text', default='', bindtypes=[str, unicode])
+@p('icon', default=None)
+@p('pressed', default=False, type=bool)
+@plugin
+class ToggleButton (UIElement):
+    typeid = 'togglebutton'
+
+
+@p('width', default=None)
+@p('value', default=0, type=float, bindtypes=[int, float])
+@plugin
+class ProgressBar (UIElement):
+    typeid = 'progressbar'
