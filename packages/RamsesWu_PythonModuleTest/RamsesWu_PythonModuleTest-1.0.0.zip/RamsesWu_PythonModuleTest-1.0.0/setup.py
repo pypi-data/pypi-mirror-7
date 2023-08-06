0aa8630696ee83f499582e9c4b4f1e3c40479dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:47 +0000

    Add package RamsesWu_PythonModuleTest/1.0.0/RamsesWu_PythonModuleTest-1.0.0.zip

---
+++ packages/RamsesWu_PythonModuleTest/RamsesWu_PythonModuleTest-1.0.0.zip/RamsesWu_PythonModuleTest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		=	'RamsesWu_PythonModuleTest',
+	version		=	'1.0.0',
+	py_modules	=	['RamsesWu_PythonModuleTest'],
+	author		=	'RamsesWu',
+	author_email=	'no1qyyy@gmail.com',
+	description	=	'The first distribution from RamsesWu',
+	)
