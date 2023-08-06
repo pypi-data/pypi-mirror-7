a31ba8d81351e4331f2090e09566e05e74097aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package tokitou/0.1/tokitou-0.1.tar.gz

---
+++ packages/tokitou/tokitou-0.1.tar.gz/tokitou-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'tokitou',
+      version = '0.1',
+      keywords = ('css', 'preprocess', 'preprocessing', 'tokitou', 'python'),
+      url = 'https://github.com/zhangxiansheng/tokitou',
+      description = 'Simple css preprocessing via python',
+      license = 'public domain',
+      
+      author = 'Zhang Yiwei',
+      author_email = 'Karl1991223@126.com',
+      
+      packages = find_packages(),
+      platforms = 'any',
+      )
---
+++ packages/tokitou/tokitou-0.1.tar.gz/tokitou-0.1/tokitou/__init__.py
@@ -0,0 +1,10 @@
+#Author:Zhang Yiwei
+#Email:Karl1991223@126.com
+#!/usr/bin/env python
+"""Simple css preprocessing via python"""
+
+__version__ = "0.1"
+__author__ = "Zhang Yiwei <Karl1991223@126.com>"
+__license__ = "public domain"
+
+from tokitou import *
