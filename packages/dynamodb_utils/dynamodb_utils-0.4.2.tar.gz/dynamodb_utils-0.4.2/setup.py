f2b8f449c1536d3f1202571612eba62a9f681534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:24 +0000

    Add package dynamodb_utils/0.4.2/dynamodb_utils-0.4.2.tar.gz

---
+++ packages/dynamodb_utils/dynamodb_utils-0.4.2.tar.gz/dynamodb_utils-0.4.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='dynamodb_utils',
+    version='0.4.2',
+    author='Adam Johnson',
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/dynamodb_utils',
+    description='A toolchain for AWS DynamoDB to make common operations easier.',
+    long_description=open('README.rst').read(),
+    license='GPLv3',
+    install_requires=[
+        'pynamodb'
+    ],
+    keywords='python dynamodb amazon aws',
+    scripts=[
+        'bin/dynamodb-dumper',
+        'bin/dynamodb-loader',
+    ]
+)
