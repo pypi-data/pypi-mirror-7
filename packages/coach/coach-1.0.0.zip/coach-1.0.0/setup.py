f0b5e365237ca4e65ddcb94e68d43e27b2536afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:27 +0000

    Add package coach/1.0.0/coach-1.0.0.zip

---
+++ packages/coach/coach-1.0.0.zip/coach-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name           = 'coach',
+    version        = '1.0.0',
+    py_modules     =['coach'],
+    author         = 'Rithish',
+    author_email   = 'rithishvisakoti@gmail.com',
+    url            = 'http://www.starthabit.com',
+    description    = 'File name handler'
+    )
