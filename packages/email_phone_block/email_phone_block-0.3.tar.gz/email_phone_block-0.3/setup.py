535f00eec4fe54436974cfe8444e04eeca1e2176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:48 +0000

    Add package email_phone_block/0.3/email_phone_block-0.3.tar.gz

---
+++ packages/email_phone_block/email_phone_block-0.3.tar.gz/email_phone_block-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'email_phone_block',
+  packages = ['email_phone_block'],
+  version = '0.3',
+  description = 'Substitute emails and phone numbers with dummy in your text.',
+  long_description=open('README.md').read(),
+  author = 'Mauro De Giorgi',
+  author_email = 'mauro.degiorgi@gmail.com',
+  url = 'https://github.com/mdgart/email_phone_block',
+  download_url = 'https://github.com/mdgart/email_phone_block/tarball/0.1',
+  keywords = ['email', 'phone number', 'filter', 'blocking', 'text'],
+  classifiers = [],
+)
