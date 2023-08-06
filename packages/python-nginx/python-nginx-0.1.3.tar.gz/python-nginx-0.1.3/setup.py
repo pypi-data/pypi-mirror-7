e6d1c2bda9350f2df0ba557c638ba616bbb6920c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:31 +0000

    Add package python-nginx/0.1.3/python-nginx-0.1.3.tar.gz

---
+++ packages/python-nginx/python-nginx-0.1.3.tar.gz/python-nginx-0.1.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='python-nginx',
+    version='0.1.3',
+    description='Create and modify nginx serverblock configs in Python',
+    author='Jacob Cook',
+    author_email='jacob@peakwinter.net',
+    url='https://github.com/peakwinter/python-nginx',
+    packages=['nginx'],
+    keywords = ['nginx', 'web servers', 'serverblock', 'server block'],
+    download_url = 'https://github.com/peakwinter/python-nginx/archive/0.1.2.tar.gz',
+    license = 'GPLv3',
+    classifiers = [
+    	"Development Status :: 3 - Alpha",
+    	"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    	"Operating System :: Unix",
+    	"Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+    ]
+)
