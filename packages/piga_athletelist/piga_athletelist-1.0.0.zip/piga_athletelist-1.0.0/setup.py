f24a1509e9a04dfba6fa2147ed7e952b3c90f1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:16 +0000

    Add package piga_athletelist/1.0.0/piga_athletelist-1.0.0.zip

---
+++ packages/piga_athletelist/piga_athletelist-1.0.0.zip/piga_athletelist-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'piga_athletelist',
+    version = '1.0.0',
+    py_modules = ['piga_athletelist'],
+    author = 'piga',
+    author_email = 'filipe.rav.piga@gmail.com',
+    url = 'http://localhost/',
+    description = "An object to represent athletes timings based on python type list"
+)
