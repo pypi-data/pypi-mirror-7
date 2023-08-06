5d9a350eb2809d10ef36ca746edb5a753c4826fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package sphinx-vlaamsecodex/0.1.1/sphinx-vlaamsecodex-0.1.1.tar.gz

---
+++ packages/sphinx-vlaamsecodex/sphinx-vlaamsecodex-0.1.1.tar.gz/sphinx-vlaamsecodex-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+requires = ['requests']
+
+setup(
+    name='sphinx-vlaamsecodex',
+    version='0.1.1',
+    url='https://github.com/OnroerendErfgoed/sphinx-vlaamsecodex',
+    license='MIT',
+    author='Flanders Heritage Agency',
+    author_email='ict@onroerenderfgoed.be',
+    description='Sphinx extension to integrate http://codex.vlaanderen.be in a document.',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+      ],
+    platforms='any',
+    packages=find_packages(),
+    package_data={'': ['README.md']},
+    include_package_data=True,
+    install_requires=requires,
+)
