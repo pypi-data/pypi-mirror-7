e3cfcdc189f1cf28524d0358086783c60930d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:58 +0000

    Add package carinata/0.12.1/carinata-0.12.1.tar.gz

---
+++ packages/carinata/carinata-0.12.1.tar.gz/carinata-0.12.1/carinata/__main__.py
@@ -0,0 +1,3 @@
+from . import main_cmdline
+
+main_cmdline()
---
+++ packages/carinata/carinata-0.12.1.tar.gz/carinata-0.12.1/carinata/block.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+"""A simple block class to wrap spec blocks"""
+from . import utils
+
+
+class Block(object):
+    """Represent a block structure by indent, name, description and code"""
+    test = 'test'
+    describe = 'describe'
+    context = 'context'
+    before = 'before'
+    after = 'after'
+    let = 'let'
+    it = 'it'
+
+    valid_children = {
+        test: [describe],
+        describe: [describe, context, before, after, let, it],
+        context: [context, before, after, let, it],
+        before: [],
+        after: [],
+        let: [],
+        it: [],
+    }
+
+    def __init__(self, indent, name, words, lineno, rest=None):
+        self.indent = len(indent)
+        self.name = name
+        self.words = words
+        self.lineno = lineno
+        self.code = []
+        self.decorators = None
+        self.args = "(self)"
+        if rest:
+            if self.name == self.let and not rest.startswith('return'):
+                rest = "return (%s)" % rest
+            self.code.append((lineno, rest))
+        if self.name in [self.before, self.after]:
+            self.words += utils.uuid_hex()
+
+    def __eq__(self, other):
+        return self.name == other.name and self.words == other.words
+
+    def __hash__(self):
+        return hash((self.name, self.words))
+
+    def __repr__(self):
+        return "<%s: %s>" % (self.name, self.words)
+
+    def is_applicable(self, indent):
+        """Determine whether this block applies at the given indent"""
+        if self.name == self.test:
+            return True
+        elif self.indent > indent:
+            return False
+        elif self.indent == indent:
+            return self.name not in [self.describe, self.context, self.it]
+        return True
