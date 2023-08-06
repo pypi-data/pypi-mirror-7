fcd2e1579983cbde8c95b1ec5c1a5fff09600489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:04 +0000

    Add package opencivicdata-django/0.6.1/opencivicdata_django-0.6.1-py2.py3-none-any.whl

---
+++ packages/opencivicdata-django/opencivicdata_django-0.6.1-py2.py3-none-any.whl/opencivicdata/tests/test_settings.py
@@ -0,0 +1,13 @@
+# not tests, just Django settings
+SECRET_KEY = 'test'
+INSTALLED_APPS = ('opencivicdata.apps.BaseConfig',)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+    }
+}
+MIDDLEWARE_CLASSES = ()
