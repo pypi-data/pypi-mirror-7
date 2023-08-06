a14063ca3718723869069de5b107ea52ba300ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:38 +0000

    Add package faster-closeio/0.1.4/faster_closeio-0.1.4.tar.gz

---
+++ packages/faster-closeio/faster_closeio-0.1.4.tar.gz/faster_closeio-0.1.4/closeio/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Denis Cornehl'
+__email__ = 'denis.cornehl@gmail.com'
+__version__ = '0.1.4'
+
+
+from closeio import CloseIO
+from closeio import CloseIOError
---
+++ packages/faster-closeio/faster_closeio-0.1.4.tar.gz/faster_closeio-0.1.4/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='faster_closeio',
+    version='0.1.4',
+    description='Slim API wrapper to access close.io CRM',
+    long_description=readme + '\n\n' + history,
+    author='Denis Cornehl',
+    author_email='denis.cornehl@gmail.com',
+    url='https://github.com/fasterweb/closeio',
+    packages=[
+        'closeio',
+    ],
+    package_dir={'closeio': 'closeio'},
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'slumber',
+        'python-dateutil',
+        'six',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='closeio',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+)
