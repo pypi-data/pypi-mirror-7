6cc235e91c7bca58701212d6f2246486fde29b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:53 +0000

    Add package pytictactoe/1.0.3/pytictactoe-1.0.3.tar.gz

---
+++ packages/pytictactoe/pytictactoe-1.0.3.tar.gz/pytictactoe-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='pytictactoe',
+	version='1.0.3',
+	packages=['pytictactoe'],
+	scripts=['pyttt.py'],
+	license='Creative Commons Attribution-Noncommercial-Share Alike License',
+	long_description='This is the Best Graphical Tic-Tac-Toe Game Ever',
+)
