09441e1b8f2e08174980f049e1dbd56dd5968921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:12 +0000

    Add package cunidecode/0.0.1/cunidecode-0.0.1.tar.gz

---
+++ packages/cunidecode/cunidecode-0.0.1.tar.gz/cunidecode-0.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from setuptools import setup, Extension
+
+
+setup(
+    name='cunidecode',
+    version='0.0.1',
+    description='ASCII transliterations of Unicode text',
+    license='GPL',
+    long_description=open("README.rst").read(),
+    author='Paul Logston',
+    author_email='code@logston.me',
+    url = 'https://github.com/logston/cunidecode',
+    download_url = 'https://github.com/logston/cunidecode/tarball/0.0.1',
+    ext_modules=[Extension('cunidecode', ['cunidecode.c'])],
+    provides = [ 'cunidecode' ],
+    test_suite='tests',
+    keywords = ['unicode', 'c', 'unidecode'],
+    classifiers = [
+    "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+	"Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+	"Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+	"Topic :: Text Processing",
+	"Topic :: Text Processing :: Filters",
+	],
+)
