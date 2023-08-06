56efd9706884e04e24fbad72b7027e9a8793797b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:35 +0000

    Add package AutoHDL/2.7.909/AutoHDL-2.7.909.zip

---
+++ packages/AutoHDL/AutoHDL-2.7.909.zip/AutoHDL-2.7.909/autohdl/publisher.py
@@ -0,0 +1,60 @@
+import os
+import re
+import logging
+
+from autohdl import webdav
+
+alog = logging.getLogger(__name__)
+
+
+def form_message(config):
+    if config['hdlManager']['cl']['message']:
+        mes = config['hdlManager']['cl']['message']
+    else:
+        mes = input("Enter some information about firmware: ")
+    config['publisher']['message'] = '{comment}; \n' \
+        'technology: {technology}; part: {part}; package: {package}; ' \
+        'PROM size: {size} kilobytes; '.format(comment=mes,
+                                               technology=config['hdlManager'].get('technology'),
+                                               part=config['hdlManager'].get('part'),
+                                               package=config['hdlManager'].get('package'),
+                                               size=config['hdlManager'].get('size'),
+                                               )
+
+
+def scan_for_firmwares(config, patterns):
+    files = []
+    path = os.path.join(config['hdlManager']['dsn_root'], 'resource')
+    for afile in os.listdir(path):
+        for pattern in patterns:
+            res = re.search(afile, pattern)
+            print("search", pattern, afile)
+            print(res)
+            input('next')
+            if res:
+                files.append(os.path.join(path, afile))
+    return files
+
+
+def publish(config):
+    config['publisher'] = dict()
+    form_message(config)
+    # git.push_firmware(config)
+    config['publisher']['webdave_files'] = []
+    root = os.path.join(config['hdlManager']['dsn_root'], 'resource')
+    if config['hdlManager'].get('webdav_files'):
+        names = config['hdlManager'].get('webdav_files')
+        config['publisher']['webdave_files'] = scan_for_firmwares(config, names)
+    else:
+        names = ["{}_{}".format(config['hdlManager']['dsn_name'], config['hdlManager']['top'])]
+        for i in os.listdir(root):
+            for k in ['bit', 'mcs']:
+                for name in names:
+                    print(name)
+                    res = re.search("{name}_build_(\d)+_(\d)+\.{ext}".format(name=name, ext=k), i)
+                    if res:
+                        config['publisher']['webdave_files'].append(os.path.join(root, i))
+    if config['publisher']['webdave_files']:
+        webdav.upload_fw(config)
+    else:
+        alog.error('No file to publish')
