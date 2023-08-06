add00afcbc9ddf358d052d7d9de1a76abe318bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:37 +0000

    Add package plan/0.3/plan-0.3.tar.gz

---
+++ packages/plan/plan-0.3.tar.gz/plan-0.3/plan/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    plan
+    ~~~~
+
+    Cron jobs in Python.
+
+    :copyright: (c) 2014 by Shipeng Feng.
+    :license: BSD, see LICENSE for more details.
+"""
+
+__version__ = '0.3'
+
+from .core import Plan
+from .job import Job, CommandJob, ScriptJob, ModuleJob, RawJob
