811e2496c56ef1bfd8da1413b3bf9d0be886abaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:10 +0000

    Add package bash_kernel/0.2/bash_kernel-0.2.tar.gz

---
+++ packages/bash_kernel/bash_kernel-0.2.tar.gz/bash_kernel-0.2/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from distutils.command.install import install
+import sys
+
+class install_with_kernelspec(install):
+    def run(self):
+        install.run(self)
+        from IPython.kernel.kernelspec import install_kernel_spec
+        install_kernel_spec('kernelspec', 'bash', replace=True)
+
+with open('README.rst') as f:
+    readme = f.read()
+
+svem_flag = '--single-version-externally-managed'
+if svem_flag in sys.argv:
+    # Die, setuptools, die.
+    sys.argv.remove(svem_flag)
+
+setup(name='bash_kernel',
+      version='0.2',
+      description='A bash kernel for IPython',
+      long_description=readme,
+      author='Thomas Kluyver',
+      author_email='thomas@kluyver.me.uk',
+      url='https://github.com/takluyver/bash_kernel',
+      py_modules=['bash_kernel'],
+      cmdclass={'install': install_with_kernelspec},
+      install_requires=['pexpect>=3.3'],
+      classifiers = [
+          'Framework :: IPython',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3',
+          'Topic :: System :: Shells',
+      ]
+)
