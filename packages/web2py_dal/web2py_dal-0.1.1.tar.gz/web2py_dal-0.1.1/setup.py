b429c9668aa5f98b5746215259669a7cb050093d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:08 +0000

    Add package web2py_dal/0.1.1/web2py_dal-0.1.1.tar.gz

---
+++ packages/web2py_dal/web2py_dal-0.1.1.tar.gz/web2py_dal-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='web2py_dal',
+    version='0.1.1',
+    packages=['web2py_dal',],
+    license='LGPLv3 (http://www.gnu.org/licenses/lgpl.html)',
+    description='Independent packaging of web2py\'s Database Abstraction Layer',
+    long_description='Independent packaging of web2py\'s Database Abstraction Layer',
+
+)
