ba394f752d312a8bf8926c31ea6070032c674c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package shaape/1.1.1/shaape-1.1.1.tar.gz

---
+++ packages/shaape/shaape-1.1.1.tar.gz/shaape-1.1.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+try:
+    import cairo
+except ImportError:
+    print ("Some dependencies could not be found. Make sure to install "
+           "the Python bindings for Cairo(import cairo).")
+    sys.exit(1)
+
+try:
+    import pango
+except ImportError:
+    print ("Some dependencies could not be found. Make sure to install "
+           "the Python bindings for Pango(import pango).")
+    sys.exit(1)
+
+try:
+    import pangocairo
+except ImportError:
+    print ("Some dependencies could not be found. Make sure to install "
+           "the Python bindings for PangoCairo(import pangocairo).")
+    sys.exit(1)
+
+setup(name='shaape',
+      version='1.1.1',
+      description='Shaape - ascii art to image converter',
+      author='Christian Goltz',
+      author_email='goltzchristian@googlemail.com',
+      package_data={'shaape': ['data']},
+      packages=['shaape'],
+      scripts=['bin/shaape'],
+      license='LICENSE',
+      url='http://github.com/christiangoltz/shaape',
+      tests_require=['nose'],
+      install_requires=[
+          'networkx',
+          'PyYAML']
+     )
