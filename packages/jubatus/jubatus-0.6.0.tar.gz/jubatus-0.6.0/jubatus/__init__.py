7fd0106fd1154ea9f1bd7d28df367abd1f0e7fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:09 +0000

    Add package jubatus/0.6.0/jubatus-0.6.0.tar.gz

---
+++ packages/jubatus/jubatus-0.6.0.tar.gz/jubatus-0.6.0/jubatus/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ["anomaly", "classifier", "clustering", "graph", "nearest_neighbor", "recommender", "regression", "stat", ]
+
+from jubatus.anomaly.client import Anomaly
+from jubatus.classifier.client import Classifier
+from jubatus.clustering.client import Clustering
+from jubatus.graph.client import Graph
+from jubatus.nearest_neighbor.client import NearestNeighbor
+from jubatus.recommender.client import Recommender
+from jubatus.regression.client import Regression
+from jubatus.stat.client import Stat
---
+++ packages/jubatus/jubatus-0.6.0.tar.gz/jubatus-0.6.0/jubatus/classifier/__init__.py
@@ -0,0 +1,3 @@
+# This file is auto-generated from classifier.idl(0.5.4-148-gfea5e25) with jenerator version 0.5.4-224-g49229fa/develop
+# *** DO NOT EDIT ***
+
