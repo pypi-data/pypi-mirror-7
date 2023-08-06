b15d191063192bb536d321e1c200192779fee594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:07 +0000

    Add package abc1/1.0.1/abc1-1.0.1.zip

---
+++ packages/abc1/abc1-1.0.1.zip/abc1-1.0.1/abc1.py
@@ -0,0 +1,12 @@
+'''
+this is a 
+
+'''
+# hehe
+
+def print_lol(the_list):
+     for each_item in the_list:
+          if isinstance(each_item,list):
+               print_lol(each_item)
+          else:
+               print(each_item)
---
+++ packages/abc1/abc1-1.0.1.zip/abc1-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		='abc1',
+	version		='1.0.1',
+	py_modules	=['abc1'],
+	author		='yangfeng',
+	author_email='feng.yang@chinacache.com',
+	url			='http://www.sheooo.com',
+	description	='a list about my think',
+)
