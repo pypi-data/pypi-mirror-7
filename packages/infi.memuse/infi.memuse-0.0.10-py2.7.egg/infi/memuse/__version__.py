f78677ee25c9c426689d20d7c811c8f31d9da7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package infi.memuse/0.0.10/infi.memuse-0.0.10-py2.7.egg

---
+++ packages/infi.memuse/infi.memuse-0.0.10-py2.7.egg/infi/memuse/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b2211d9488c41691cdd6f5632449704178b4a611'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
