da0642dc3261a0d31eeb3ac2bdf2d50d295ca451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package pyNFFT/1.3.2/pyNFFT-1.3.2.tar.gz

---
+++ packages/pyNFFT/pyNFFT-1.3.2.tar.gz/pyNFFT-1.3.2/pynfft/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '1.3.2'
+version = '1.3.2'
+full_version = '1.3.2'
+git_revision = 'c45b7027a79c9633e8ffa84532f4bfa092e510a9'
+release = True
+
+if not release:
+    version = full_version
