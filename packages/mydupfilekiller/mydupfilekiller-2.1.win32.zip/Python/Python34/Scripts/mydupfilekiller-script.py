1108d3113aaf3c3e0f6b174f6c399e4868513e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:42 +0000

    Add package mydupfilekiller/2.1/mydupfilekiller-2.1.win32.zip

---
+++ packages/mydupfilekiller/mydupfilekiller-2.1.win32.zip/Python/Python34/Scripts/mydupfilekiller-script.py
@@ -0,0 +1,10 @@
+#!h:\Python\Python34\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'mydupfilekiller==2.1','console_scripts','mydupfilekiller'
+__requires__ = 'mydupfilekiller==2.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('mydupfilekiller==2.1', 'console_scripts', 'mydupfilekiller')()
+    )
