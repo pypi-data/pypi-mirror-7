29e8e8c5bd1736fb396baaaf068325b770b440aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:29 +0000

    Add package quantcluster/0.0.2/quantcluster-0.0.2.tar.gz

---
+++ packages/quantcluster/quantcluster-0.0.2.tar.gz/quantcluster-0.0.2/setup.py
@@ -0,0 +1,15 @@
+
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name='quantcluster',
+    url='http://www.goodeanalytics.com',
+    version='0.0.2',
+    description='A command line interface for QF cluster-computing on AWS EC2.',
+    author='Jimmie Goode',
+    author_email='jimmie@goodeanalytics.com',
+    packages=find_packages(),
+    install_requires=['StarCluster'],
+    )
