e67ea5305cbeaebb7d863131fb4a6662a7b002ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:36 +0000

    Add package firebasin/0.1.0-release/firebasin-0.1.0-release.tar.gz

---
+++ packages/firebasin/firebasin-0.1.0-release.tar.gz/firebasin-0.1.0-release/firebasin/general.py
@@ -0,0 +1,24 @@
+# General Methods
+
+# regular expression module
+import re
+
+# Valid Firebase Url
+# - Protocol must be HTTPS
+# - Domain Must be firebaseio.com
+# - firebasename can only contain:
+#       - hyphen , small and capitall letters and hyphen
+# - firebasename can not have leading and trailing hyphens
+# - childnames can not contain:
+#       - period, dollar sign, square brackets, pound, ASCII Control Chars from \x00 - \x1F and \x7F
+def valid_url(name):
+    try:
+        result = re.search(r'^https://([a-zA-Z0-9][a-zA-Z0-9\-]+[a-zA-Z0-9]).firebaseio.com(/[^\#\$\[\]\.\x00-\x1F\x7F]*)', name + '/').group()
+        result = result[:len(result) -1]
+        if result == name:
+            return name
+        else:
+            raise ValueError("InvalidURL")
+    except:
+        raise ValueError("InvalidURL")
+        
