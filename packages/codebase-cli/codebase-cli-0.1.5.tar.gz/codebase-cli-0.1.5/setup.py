824401bcab986a52fe8326dba10f09b2bed66e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:10 +0000

    Add package codebase-cli/0.1.5/codebase-cli-0.1.5.tar.gz

---
+++ packages/codebase-cli/codebase-cli-0.1.5.tar.gz/codebase-cli-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from os.path import join as pjoin
+from setuptools import setup
+
+setup(
+    name='codebase-cli',
+    description='A CLI for codebasehq.com',
+    author='Kevin Carrogan',
+    author_email='kevin.carrogan@gmail.com',
+    url='http://github.com/kevindmorgan/codebase-cli',
+    version='0.1.5',
+    scripts=[pjoin('bin', 'cb')],
+    requires=[],
+)
