2a746be6897a7983651d297ccc0990cc15f326b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:43 +0000

    Add package boxmongodb/0.0.3/boxmongodb-0.0.3.tar.gz

---
+++ packages/boxmongodb/boxmongodb-0.0.3.tar.gz/boxmongodb-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'boxmongodb',
+    version = '0.0.3',
+    keywords = ('mongodb', 'db'),
+    description = 'just a simple mongodb orm',
+    license = 'MIT License',
+    author = 'CcdjhMarx',
+    author_email = 'ccdjh.marx@all.com',
+    packages = ['boxmongodb'],
+)
