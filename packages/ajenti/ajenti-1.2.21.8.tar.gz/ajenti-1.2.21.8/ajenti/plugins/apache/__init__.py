5c4a25e96992ca5dedc324a5262dc049f28b88a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:20 +0000

    Add package ajenti/1.2.21.8/ajenti-1.2.21.8.tar.gz

---
+++ packages/ajenti/ajenti-1.2.21.8.tar.gz/ajenti-1.2.21.8/ajenti/plugins/apache/__init__.py
@@ -0,0 +1,21 @@
+from ajenti.api import *
+from ajenti.plugins import *
+from ajenti.util import platform_select
+
+
+info = PluginInfo(
+    title='Apache',
+    icon='globe',
+    dependencies=[
+        PluginDependency('webserver_common'),
+        BinaryDependency(platform_select(
+            centos='httpd',
+            freebsd='apachectl',
+            default='apache2',
+        )),
+    ],
+)
+
+
+def init():
+    import main
