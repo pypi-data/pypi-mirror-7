18fd3ed8b071a753ac326dbdd60822a222fb1183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package agilegeo/0.1.3/agilegeo-0.1.3.tar.gz

---
+++ packages/agilegeo/agilegeo-0.1.3.tar.gz/agilegeo-0.1.3/agilegeo/avo/__init__.py
@@ -0,0 +1,15 @@
+"""
+===============
+agilegeo.avo
+===============
+"""
+
+from reflection import zoeppritz, akirichards, akirichards_alt, \
+                       fatti, shuey2, shuey3, bortfeld2, bortfeld3
+from timedepthconv import time_to_depth, depth_to_time
+
+__all__=[ "zoeppritz", "akirichards", "akirichards_alt",
+          "fatti", "shuey2", "shuey3", "bortfeld2", "bortfeld3",
+          "time_to_depth", "depth_to_time" ]
+
+                
