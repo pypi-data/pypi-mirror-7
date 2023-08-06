96c9191f2bebd8d00501b489ec20ace944f6fc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:06 +0000

    Add package rinse/0.0.2/rinse-0.0.2.tar.gz

---
+++ packages/rinse/rinse-0.0.2.tar.gz/rinse-0.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='rinse',
+    version='0.0.2',
+    description='Python3 SOAP client built with lxml and requests.',
+    author='Tyson Clugg',
+    author_email='tyson@clugg.net',
+    url='http://github.com/tysonclugg/rinse',
+    license='MIT',
+    packages=['rinse'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
