a17e1ef762f6b9fb81e3885c0a4adecbbadc03b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:42 +0000

    Add package myutils/0.0.21/myutils-0.0.21.zip

---
+++ packages/myutils/myutils-0.0.21.zip/myutils-0.0.21/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'myutils',
+	version  = '0.0.21',
+	py_modules = ['myutils'],
+	author = 'magicfingr',
+	author_email = 'zxtlive@gmail.com',
+	url = '',
+	description = 'My custom util module'
+	)
