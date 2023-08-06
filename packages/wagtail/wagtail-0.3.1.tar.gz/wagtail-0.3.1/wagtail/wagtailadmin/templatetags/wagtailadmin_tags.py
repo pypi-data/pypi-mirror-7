290803667c1ca6a18ad593e8af5c98052b70fc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:24 +0000

    Add package wagtail/0.3.1/wagtail-0.3.1.tar.gz

---
+++ packages/wagtail/wagtail-0.3.1.tar.gz/wagtail-0.3.1/wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
@@ -0,0 +1,109 @@
+from django import template
+from django.core import urlresolvers
+from django.utils.translation import ugettext_lazy as _
+
+from wagtail.wagtailadmin import hooks
+from wagtail.wagtailadmin.menu import MenuItem
+
+from wagtail.wagtailcore.models import get_navigation_menu_items, UserPagePermissionsProxy
+from wagtail.wagtailcore.util import camelcase_to_underscore
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('wagtailadmin/shared/explorer_nav.html')
+def explorer_nav():
+    return {
+        'nodes': get_navigation_menu_items()
+    }
+
+
+@register.inclusion_tag('wagtailadmin/shared/explorer_nav.html')
+def explorer_subnav(nodes):
+    return {
+        'nodes': nodes
+    }
+
+
+@register.assignment_tag
+def get_wagtailadmin_tab_urls():
+    resolver = urlresolvers.get_resolver(None)
+    return [
+        (key, value[2].get("title", key))
+        for key, value
+        in resolver.reverse_dict.items()
+        if isinstance(key, basestring) and key.startswith('wagtailadmin_tab_')
+    ]
+
+
+@register.inclusion_tag('wagtailadmin/shared/main_nav.html', takes_context=True)
+def main_nav(context):
+    menu_items = [
+        MenuItem(_('Explorer'), '#', classnames='icon icon-folder-open-inverse dl-trigger', order=100),
+        MenuItem(_('Search'), urlresolvers.reverse('wagtailadmin_pages_search'), classnames='icon icon-search', order=200),
+    ]
+
+    request = context['request']
+
+    for fn in hooks.get_hooks('construct_main_menu'):
+        fn(request, menu_items)
+
+    return {
+        'menu_items': sorted(menu_items, key=lambda i: i.order),
+        'request': request,
+    }
+
+
+@register.filter("ellipsistrim")
+def ellipsistrim(value, max_length):
+    if len(value) > max_length:
+        truncd_val = value[:max_length]
+        if not len(value) == max_length+1 and value[max_length+1] != " ":
+            truncd_val = truncd_val[:truncd_val.rfind(" ")]
+        return truncd_val + "..."
+    return value
+
+
+@register.filter
+def fieldtype(bound_field):
+    try:
+        return camelcase_to_underscore(bound_field.field.__class__.__name__)
+    except AttributeError:
+        return ""
+
+
+@register.filter
+def meta_description(model):
+    try:
+        return model.model_class()._meta.description
+    except:
+        return ""
+
+
+@register.assignment_tag(takes_context=True)
+def page_permissions(context, page):
+    """
+    Usage: {% page_permissions page as page_perms %}
+    Sets the variable 'page_perms' to a PagePermissionTester object that can be queried to find out
+    what actions the current logged-in user can perform on the given page.
+    """
+    # Create a UserPagePermissionsProxy object to represent the user's global permissions, and
+    # cache it in the context for the duration of the page request, if one does not exist already
+    if 'user_page_permissions' not in context:
+        context['user_page_permissions'] = UserPagePermissionsProxy(context['request'].user)
+
+    # Now retrieve a PagePermissionTester from it, specific to the given page
+    return context['user_page_permissions'].for_page(page)
+
+
+@register.simple_tag
+def hook_output(hook_name):
+    """
+    Example: {% hook_output 'insert_editor_css' %}
+    Whenever we have a hook whose functions take no parameters and return a string, this tag can be used
+    to output the concatenation of all of those return values onto the page.
+    Note that the output is not escaped - it is the hook function's responsibility to escape unsafe content.
+    """
+    snippets = [fn() for fn in hooks.get_hooks(hook_name)]
+    return u''.join(snippets)
---
+++ packages/wagtail/wagtail-0.3.1.tar.gz/wagtail-0.3.1/wagtail/wagtailsearch/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.utils import timezone
+
+from indexed import Indexed
+import datetime
+import string
+
+
+class Query(models.Model):
+    query_string = models.CharField(max_length=255, unique=True)
+
+    def save(self, *args, **kwargs):
+        # Normalise query string
+        self.query_string = self.normalise_query_string(self.query_string)
+
+        super(Query, self).save(*args, **kwargs)
+
+    def add_hit(self, date=None):
+        if date is None:
+            date = timezone.now().date()
+        daily_hits, created = QueryDailyHits.objects.get_or_create(query=self, date=date)
+        daily_hits.hits = models.F('hits') + 1
+        daily_hits.save()
+
+    def __unicode__(self):
+        return self.query_string
+
+    @property
+    def hits(self):
+        hits = self.daily_hits.aggregate(models.Sum('hits'))['hits__sum']
+        return hits if hits else 0
+
+    @classmethod
+    def garbage_collect(cls):
+        """
+        Deletes all Query records that have no daily hits or editors picks
+        """
+        cls.objects.filter(daily_hits__isnull=True, editors_picks__isnull=True).delete()
+
+    @classmethod
+    def get(cls, query_string):
+        return cls.objects.get_or_create(query_string=cls.normalise_query_string(query_string))[0]
+
+    @classmethod
+    def get_most_popular(cls, date_since=None):
+        # TODO: Implement date_since
+        return cls.objects.filter(daily_hits__isnull=False).annotate(_hits=models.Sum('daily_hits__hits')).distinct().order_by('-_hits')
+
+    @staticmethod
+    def normalise_query_string(query_string):
+        # Convert query_string to lowercase
+        query_string = query_string.lower()
+
+        # Strip punctuation characters
+        query_string = ''.join([c for c in query_string if c not in string.punctuation])
+
+        # Remove double spaces
+        query_string = ' '.join(query_string.split())
+
+        return query_string
+
+
+class QueryDailyHits(models.Model):
+    query = models.ForeignKey(Query, db_index=True, related_name='daily_hits')
+    date = models.DateField()
+    hits = models.IntegerField(default=0)
+
+    @classmethod
+    def garbage_collect(cls):
+        """
+        Deletes all QueryDailyHits records that are older than 7 days
+        """
+        min_date = timezone.now().date() - datetime.timedelta(days=7)
+
+        cls.objects.filter(date__lt=min_date).delete()
+
+    class Meta:
+        unique_together = (
+            ('query', 'date'),
+        )
+
+
+class EditorsPick(models.Model):
+    query = models.ForeignKey(Query, db_index=True, related_name='editors_picks')
+    page = models.ForeignKey('wagtailcore.Page')
+    sort_order = models.IntegerField(null=True, blank=True, editable=False)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ('sort_order', )
+
+
+# Used for tests
+
+class SearchTest(models.Model, Indexed):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    live = models.BooleanField(default=False)
+
+    indexed_fields = ("title", "content", "callable_indexed_field", "live")
+
+    def callable_indexed_field(self):
+        return "Callable"
+
+
+class SearchTestChild(SearchTest):
+    extra_content = models.TextField()
+
+    indexed_fields = "extra_content"
