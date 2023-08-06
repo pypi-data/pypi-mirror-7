d16b24d118895c94fdefce4ba08fd4357488890f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:03 +0000

    Add package holmesalf/0.1.3/holmesalf-0.1.3.tar.gz

---
+++ packages/holmesalf/holmesalf-0.1.3.tar.gz/holmesalf-0.1.3/tests/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of holmesalf.
+# https://github.com/holmes-app/holmes-alf
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Pablo Aguiar scorphus@gmail.com
+
+from unittest import TestCase as PythonTestCase
+
+class TestCase(PythonTestCase):
+    pass
