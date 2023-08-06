79be8b90d4d61fd9fca360cdf646b2c78f89469d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:46 +0000

    Add package haas/0.4.0/haas-0.4.0-py2.py3-none-any.whl

---
+++ packages/haas/haas-0.4.0-py2.py3-none-any.whl/haas/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.5.0'
+full_version = '0.5.0.dev1-a64585f'
+git_revision = 'a64585f3176b0b7bca2223efd5b967910cfe2b9c'
+is_released = False
+
+if not is_released:
+    version = full_version
