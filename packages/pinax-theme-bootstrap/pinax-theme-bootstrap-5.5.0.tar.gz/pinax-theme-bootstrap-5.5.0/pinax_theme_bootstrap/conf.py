fb53666d0cfc9657caec0b1f37be14b67091be7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:03 +0000

    Add package pinax-theme-bootstrap/5.5.0/pinax-theme-bootstrap-5.5.0.tar.gz

---
+++ packages/pinax-theme-bootstrap/pinax-theme-bootstrap-5.5.0.tar.gz/pinax-theme-bootstrap-5.5.0/pinax_theme_bootstrap/conf.py
@@ -0,0 +1,10 @@
+from appconf import AppConf
+
+
+class ThemeAppConf(AppConf):
+
+    ADMIN_URL = "admin:index"
+    CONTACT_EMAIL = "support@example.com"
+
+    class Meta:
+        prefix = "theme"
