dafe9a954a4919167f2afd56ee07af4e8531632c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:40 +0000

    Add package Fabhacks/0.1.6/Fabhacks-0.1.6.tar.gz

---
+++ packages/Fabhacks/Fabhacks-0.1.6.tar.gz/Fabhacks-0.1.6/setup.py
@@ -0,0 +1,24 @@
+# Fabhacks
+# File: setup.py
+# Desc: needed
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    version='0.1.6',
+    name='Fabhacks',
+    description='Some Fabric-based hacks',
+    author='Nick @ EDITD',
+    author_email='dev@editd.com',
+    url='http://github.com/EDITD/fabhacks',
+    package_dir={ 'fabhacks': 'fabhacks' },
+    install_requires=[
+        'fabric>=1.8.0'
+    ],
+    packages=[
+        'fabhacks'
+    ]
+)
