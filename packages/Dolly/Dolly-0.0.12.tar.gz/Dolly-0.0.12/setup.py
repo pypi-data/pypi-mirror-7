54701533941151af1ec341a9e9442c00d44b41b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:36 +0000

    Add package Dolly/0.0.12/Dolly-0.0.12.tar.gz

---
+++ packages/Dolly/Dolly-0.0.12.tar.gz/Dolly-0.0.12/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 
+from setuptools import setup
+setup(
+	name = 'Dolly',
+	description='Dolly manages multiple git and svn repos',
+	long_description=open('README.md').read(),
+	packages = ['dolly'],
+	version = '0.0.12',
+	install_requires=['pyyaml', 'argparse'],
+	data_files=[('readme', ['README.md'])],
+	entry_points={
+		'console_scripts': [
+			'dolly = dolly.dolly:main',
+			'dly = dolly.dolly:main'
+		]
+	}
+)
