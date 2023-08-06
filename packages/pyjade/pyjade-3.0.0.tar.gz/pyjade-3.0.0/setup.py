af7bcf6628b0e322729e5b2b5cd7ea4d285abc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:05 +0000

    Add package pyjade/3.0.0/pyjade-3.0.0.tar.gz

---
+++ packages/pyjade/pyjade-3.0.0.tar.gz/pyjade-3.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+setup(name='pyjade',
+      version='3.0.0',
+      download_url='git@github.com:syrusakbary/pyjade.git',
+      packages=find_packages(),
+      author='Syrus Akbary',
+      author_email='me@syrusakbary.com',
+      description='Jade syntax template adapter for Django, Jinja2, Mako and '
+                  'Tornado templates',
+      long_description=open('README.rst').read(),
+      keywords='jade template converter',
+      url='http://github.com/syrusakbary/pyjade',
+      license='MIT',
+      entry_points={
+          'console_scripts' : ['pyjade = pyjade.convert:convert_file',]
+      },
+      install_requires=['six'],
+      tests_require=[
+            'nose',
+            'django',
+            'jinja2',
+            'tornado',
+            'pyramid >= 1.4, <= 1.4.99',
+            'mako',
+      ])
