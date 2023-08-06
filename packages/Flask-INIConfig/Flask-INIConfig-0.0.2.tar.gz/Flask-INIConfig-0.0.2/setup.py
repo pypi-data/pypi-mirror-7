31755530f0b811e49188198b96ecaae40a0cc3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:04 +0000

    Add package Flask-INIConfig/0.0.2/Flask-INIConfig-0.0.2.tar.gz

---
+++ packages/Flask-INIConfig/Flask-INIConfig-0.0.2.tar.gz/Flask-INIConfig-0.0.2/setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='Flask-INIConfig',
+      version='0.0.2',
+      author='Wampeter Foma',
+      author_email='foma@wampeter.org',
+      url='https://bitbucket.org/wampeter/flask-iniconfig/',
+      license='BSD',
+      description='A flask extension to load ini files via config',
+      long_description=open('README.rst').read(),
+      zip_safe=False,
+      py_modules=['flask_iniconfig'],
+      platforms='any',
+      install_requires=['Flask'],
+      setup_requires=['nose'],
+      tests_require=['nose', 'coverage'],
+      classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ packages/Flask-INIConfig/Flask-INIConfig-0.0.2.tar.gz/Flask-INIConfig-0.0.2/test_flask_iniconfig.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+# implicit test ??
+from flask.ext.iniconfig import INIConfig
+
+import nose.tools as nt
+
+class TestINIConfig(object):
+
+    @classmethod
+    def setupClass(cls):
+        cls.app = Flask('foo')
+        INIConfig(cls.app)
+
+    def setup(self):
+        with self.app.app_context():
+            self.app.config.from_inifile('test.ini')
+
+    @staticmethod
+    def check_type(value, type_):
+        nt.assert_is_instance(value, type_)
+
+    @staticmethod
+    def check_membership(config, name):
+        nt.assert_in(name, config)
+
+    @staticmethod
+    def check_no_membership(config, name):
+        nt.assert_not_in(name, config)
+
+    def test_init_app(self):
+        app = Flask('bar')
+        with app.app_context():
+            INIConfig().init_app(app)
+
+    def test_configparser_options(self):
+        app = Flask('bar')
+        with app.app_context():
+            INIConfig(defaults={'a': 1}, dict_type=dict,
+                    allow_no_value=True).init_app(app)
+
+    def test_config_method(self):
+        nt.ok_(hasattr(self.app.config, 'from_inifile'))
+
+    def test_load_nofile_noerror(self):
+        with self.app.app_context():
+            self.app.config.from_inifile('nonexistant.ini')
+
+    def test_load_flask_default(self):
+        nt.ok_('DEBUG' in self.app.config)
+        nt.ok_(self.app.config['DEBUG'])
+
+    def test_load_section_case_sensitivity(self):
+        settings = self.app.config.get('INTS')
+        nt.assert_is_none(settings)
+
+    def test_load_property_case_sensitivity(self):
+        settings = self.app.config.get('ints')
+        property = settings.get('G')
+        nt.assert_is_none(property)
+
+    def test_load_basic_types(self):
+        for type_ in [int, float, bool, str]:
+            settings = self.app.config['%ss' % type_.__name__]
+            for key, value in settings.items():
+                yield self.check_type, value, type_
+
+    def test_load_json_list(self):
+        settings = self.app.config['literals']
+        nt.assert_is_instance(settings['o'], list)
+
+    def test_load_json_dict(self):
+        settings = self.app.config['literals']
+        for name in ['p', 'q']:
+            yield self.check_type, settings[name], dict
+
+    def test_load_json_embedded_list(self):
+        settings = self.app.config['literals']
+        nt.assert_is_instance(settings['q']['b'], list)
+
+    def test_load_json_embedded_dict(self):
+        settings = self.app.config['literals']
+        nt.assert_is_instance(settings['q']['c'], dict)
+
+    def test_load_json_tuple(self):
+        settings = self.app.config['literals']
+        nt.assert_is_instance(settings['r'], tuple)
+
+    def test_load_colon_separated(self):
+        settings = self.app.config['colonsep']
+        for name in ['v', 'w']:
+            yield self.check_membership, settings, name
+
+    def test_load_empty(self):
+        settings = self.app.config['empty']
+        nt.eq_(settings['x'], '')
+
+    def test_load_settings_only(self):
+        app = Flask('bar')
+        with app.app_context():
+            INIConfig().init_app(app)
+            app.config.from_inifile_sections('test.ini', ['strs'])
+
+            for name in ['DEBUG', 'S', 'T', 'U']:
+                yield self.check_membership, app.config, name
+
+            for name in ['debug', 's', 't', 'u']:
+                yield self.check_no_membership, app.config, name
