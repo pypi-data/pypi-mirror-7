470b2f40b96d059ea8df08787bdf8bd12180d892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:22 +0000

    Add package pypops/201408/pypops-201408.tar.gz

---
+++ packages/pypops/pypops-201408.tar.gz/pypops-201408/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from pops import __version__ as version
+
+setup(
+    name='pypops',
+    version=version,
+    packages=[''],
+    url='https://github.com/shuge/pops',
+    license='MIT License',
+    author='Shuge Lee',
+    author_email='shuge.lee@gmail.com',
+    description='POPS is a simple HTTP proxy server and HTTP proxy slot server.',
+
+    platforms = ["Mac OS X", "Linux"],
+
+    scripts = [
+        "pops.py",
+    ],
+
+    install_requires = [
+        "argparse",
+        "python-daemon",
+        ],
+)
