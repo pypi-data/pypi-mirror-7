fdea019a4560f2fec2021138ed76805195265bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:05 +0000

    Add package pypc/0.1.2/pypc-0.1.2.tar.gz

---
+++ packages/pypc/pypc-0.1.2.tar.gz/pypc-0.1.2/pypc/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    pypc
+    ~~~~~
+
+    pypc python package creator
+
+    :copyright: (c) 2014 by Hackerlist.
+    :license: BSD, see LICENSE for more details.
+"""
+
+__version__ = "0.1.2"
---
+++ packages/pypc/pypc-0.1.2.tar.gz/pypc-0.1.2/setup.py
@@ -0,0 +1,74 @@
+#-*- coding: utf-8 -*-
+
+"""
+pypc
+----
+
+Pypc is a tool for creating Python Packages. 
+
+Generating a Project
+````````````````````
+
+.. code:: bash
+
+    $ pypc create helloworld
+
+Setup
+`````
+
+.. code:: bash
+
+    $ pip install pypc
+    $ ls
+    LICENSE pypc/ README requirements.txt setup.py
+
+Links
+`````
+
+* `website <http://github.com/mekarpeles/pypc/>`_
+
+"""
+
+from setuptools import setup
+import os
+
+setup(
+    name='pypc',
+    version='0.1.2',
+    url='http://github.com/mekarpeles/pypc',
+    author='hackerlist',
+    author_email='m@hackerlist.net',
+    packages=[
+        'pypc',
+        'pypc/tests'
+        ],
+    platforms='any',
+    license='LICENSE',
+    install_requires=[
+        'argparse == 1.2.1',
+        'pep8 == 1.5.7',
+        'pyflakes == 0.8.1',
+        'flake8 == 2.2.2',
+        'virtualenv == 1.11.6',
+        'wsgiref == 0.1.2'
+    ],
+    scripts=[
+        "scripts/pypc",
+        "scripts/pipcreate"
+        ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3"
+        ],
+    description="Python3 Package Creator",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+)
