60dd241ddff0426ad6a935e206ccdbce85f01c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:20 +0000

    Add package fastavro/0.7.9/fastavro-0.7.9.tar.gz

---
+++ packages/fastavro/fastavro-0.7.9.tar.gz/fastavro-0.7.9/setup.py
@@ -0,0 +1,94 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from distutils.core import Extension
+import distutils.log as log
+from os.path import join
+from distutils.command.build_ext import build_ext
+from distutils.errors import (
+    CCompilerError, DistutilsExecError, DistutilsPlatformError
+)
+import sys
+
+import fastavro
+
+
+def extension(base):
+    cmodule = '_{0}'.format(base)
+    cfile = join('fastavro', '{0}.c'.format(cmodule))
+
+    return Extension('fastavro.{0}'.format(cmodule), [cfile])
+
+
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
+              IOError)
+
+
+class maybe_build_ext(build_ext):
+    '''This class allows C extension building to fail.'''
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            log.info('cannot bulid C extension, will continue without.')
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            log.info('cannot bulid C extension, will continue without.')
+
+
+if sys.version_info[:2] > (2, 6):
+    install_requires = []
+else:
+    install_requires = ['argparse']
+
+# Don't compile extension under pypy > 1.8
+# See https://bitbucket.org/pypy/pypy/issue/1770
+ext_modules = [
+    extension('reader'),
+    extension('six'),
+    extension('writer')
+]
+if hasattr(sys, 'pypy_version_info') and sys.pypy_version_info[:2] > 1.8:
+    ext_modules = []
+
+
+setup(
+    name='fastavro',
+    version=fastavro.__version__,
+    description='Fast iteration of AVRO files',
+    long_description=open('README.rst').read(),
+    author='Miki Tebeka',
+    author_email='miki.tebeka@gmail.com',
+    license='MIT',
+    url='https://bitbucket.org/tebeka/fastavro',
+    packages=['fastavro'],
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': maybe_build_ext},
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'fastavro = fastavro.__main__:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=install_requires,
+    extras_require={
+        'snappy': ['python-snappy'],
+    },
+    tests_require=['nose', 'flake8'],
+)
