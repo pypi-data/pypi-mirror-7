3edfdb926a3f16bf4cdc732ff90700e5fc5bd486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:18 +0000

    Add package activity-monitor/0.8.0/activity-monitor-0.8.0.tar.gz

---
+++ packages/activity-monitor/activity-monitor-0.8.0.tar.gz/activity-monitor-0.8.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+#with open('docs/requirements.txt') as f:
+#    required = f.read().splitlines()
+
+setup(
+    name='activity-monitor',
+    version='0.8.0',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    url='http://github.com/tBaxter/activity-monitor',
+    license='LICENSE',
+    description='A sort of tumblelog-y thing heavily based on code I got somewhere',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
