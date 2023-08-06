5c270572b352a4fe00f5dfc8de227f5b66f546f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package pyjsonrpc/0.0.2/pyjsonrpc-0.0.2.tar.gz

---
+++ packages/pyjsonrpc/pyjsonrpc-0.0.2.tar.gz/pyjsonrpc-0.0.2/README.txt
@@ -0,0 +1,5 @@
+=========
+pyjsonrpc
+=========
+
+JSON-RPC compatibility library for Python 2.7+.
---
+++ packages/pyjsonrpc/pyjsonrpc-0.0.2.tar.gz/pyjsonrpc-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  author="Jeremy Shute",
+  author_email="j@altschool.com",
+  description="AltSchool JSON-RPC compatibility library",
+  install_requires=open("requirements.txt").readlines(),
+  long_description=open("README.txt").read(),
+  name="pyjsonrpc",
+  packages=["pyjsonrpc"],
+  url="http://github.com/AltSchool/pyjsonrpc",
+  # The following version is automatically computed by the Makefile.
+  version="0.0.2",
+)
