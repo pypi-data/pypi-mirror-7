ba50ad7782b6e5c4d047c7452f27a5ae4ce7da4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package utfhate/1.0.1/utfhate-1.0.1.tar.gz

---
+++ packages/utfhate/utfhate-1.0.1.tar.gz/utfhate-1.0.1/utfhate/__init__.py
@@ -0,0 +1,23 @@
+import lib
+
+def htmlstring(chars, lazy=False, literal=False):
+    if not hasattr(chars, '__next__'):
+        chars = iter(chars)
+        pass
+
+    def generator():
+        char = None
+        while True:
+            char = char or chars.next()
+            decimal = ord(char)
+
+            if decimal in xrange(192, 256):
+                entity, char = lib.multipart(char, chars)
+                yield entity
+                continue
+            elif literal and decimal in [60, 62]:
+                yield lib.utfentity(decimal)
+            else:
+                yield char
+            char = None
+    return generator() if lazy else ''.join(list(generator()))
