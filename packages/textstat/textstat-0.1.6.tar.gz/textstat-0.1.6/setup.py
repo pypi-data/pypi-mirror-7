e938ced198a642c77e477a4ca45e8575331c3dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:12 +0000

    Add package textstat/0.1.6/textstat-0.1.6.tar.gz

---
+++ packages/textstat/textstat-0.1.6.tar.gz/textstat-0.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='textstat',
+    packages=['textstat'],
+    version='0.1.6',
+    description='Calculate statistics from text',
+    author='Shivam Bansal, Chaitanya Aggarwal',
+    author_email='shivam5992@gmail.com, chaitanya.citupes@gmail.com',
+    url='https://github.com/shivam5992/textstat',
+    long_description=open('README.md').read(),
+    package_data={'': ['easy_word_list']},
+    include_package_data=False,
+    license='MIT',
+    classifiers=(
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        ),
+)
