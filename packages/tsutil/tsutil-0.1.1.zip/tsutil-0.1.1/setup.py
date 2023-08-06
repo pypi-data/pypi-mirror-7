3748ab94b98b6b92069c810bdd3cb0af99cdbfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package tsutil/0.1.1/tsutil-0.1.1.zip

---
+++ packages/tsutil/tsutil-0.1.1.zip/tsutil-0.1.1/setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+
+
+setup(name='tsutil', version='0.1.1', scripts=['enum.py', 'encoder.py', 'util.py'],
+      author='Turbidsoul Chen', author_email='sccn.sq+py@gmail.com',
+      url='http://blog.turbidsoul.me', description='my python tool module')
