3873f2e92feeabd11a598a20ebc348313bddd0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:23 +0000

    Add package dwdweather/0.3/dwdweather-0.3.tar.gz

---
+++ packages/dwdweather/dwdweather-0.3.tar.gz/dwdweather-0.3/setup.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+from setuptools import setup
+
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = ''
+
+setup(name='dwdweather',
+      version='0.3',
+      description='Inofficial DWD weather data client (Deutscher Wetterdienst)',
+      long_description=description,
+      author='Marian Steinbach',
+      author_email='marian@sendung.de',
+      url='http://github.com/marians/dwd-weather',
+      py_modules=['dwdweather'],
+      install_requires=[],
+      entry_points={
+          'console_scripts': [
+              'dwdweather = dwdweather:main'
+          ]
+      }
+)
