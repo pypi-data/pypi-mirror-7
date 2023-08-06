0beb6414e17e835b9bde3f933b8526ff1b0d4fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:35 +0000

    Add package graphcanvas/4.0.2/graphcanvas-4.0.2.tar.gz

---
+++ packages/graphcanvas/graphcanvas-4.0.2.tar.gz/graphcanvas-4.0.2/graphcanvas/layout.py
@@ -0,0 +1,56 @@
+import networkx
+import numpy
+
+def tree_layout(graph, dim=2, scale=1):
+
+    if dim != 2:
+        raise ValueError('currently only 2D graphs are supported')
+
+    if not graph.is_directed():
+        raise ValueError('graph must be directed')
+
+    if not networkx.is_directed_acyclic_graph(graph):
+        raise ValueError('graph must not contain cycles')
+
+    roots = numpy.array(graph.nodes())[numpy.where(numpy.array(graph.in_degree()) < 1)]
+
+    # Find the tree width at every depth in order to layout
+    # the nodes in a justified manner
+
+    depths = []
+    for node in graph.nodes():
+        depth = 0
+        parents = graph.predecessors(node)
+        while len(parents) > 0:
+            node = parents[0]
+            parents = graph.predecessors(node)
+            depth += 1
+        depths.append(depth)
+
+    max_depth = max(depths)
+    widths = [depths.count(i) for i in range(max_depth+1)]
+    max_width = max(widths)
+    nodes_positioned_at_depth = [0] * len(widths)
+
+    # breadth first tree transversal
+    positions = {}
+    for root in roots:
+        node_stack = [(0, root)]
+        while len(node_stack) > 0:
+            curr_depth, curr_node = node_stack.pop()
+            for child in graph.successors(curr_node):
+                node_stack.append((curr_depth + 1, child))
+
+            # top-down
+            draw_depth = 1.0 - float(curr_depth)/max_depth
+            depth_width = float(widths[curr_depth])
+
+            # center align
+            nodes_positioned_at_depth[curr_depth] += 1
+            width_positions = numpy.linspace(0, 1, depth_width+2)
+            draw_width = width_positions[nodes_positioned_at_depth[curr_depth]]
+
+            positions[curr_node] = (draw_width, draw_depth)
+
+
+    return positions
---
+++ packages/graphcanvas/graphcanvas-4.0.2.tar.gz/graphcanvas-4.0.2/setup.py
@@ -0,0 +1,46 @@
+from os.path import join
+from setuptools import setup, find_packages
+
+
+
+info = {}
+execfile(join('graphcanvas', '__init__.py'), info)
+
+
+setup(
+    name = 'graphcanvas',
+    version = info['__version__'],
+    author = 'Bryce Hendrix',
+    author_email = 'info@enthought.com',
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
+    url = 'https://github.com/enthought/graphcanvas',
+    download_url = (
+        'http://www.enthought.com/repo/ets/graphcanvas-%s.tar.gz' %
+        info['__version__']),
+    classifiers = [c.strip() for c in """\
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: C
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines() if len(c.strip()) > 0],
+    description = 'interactive graph visualization',
+    long_description = open('README.rst').read(),
+    ext_modules = [],
+    include_package_data = True,
+    install_requires = info['__requires__'],
+    license = 'BSD',
+    packages = find_packages(),
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    zip_safe = False,
+)
