19d6a81f215713d8391adcba11821a9192d00fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:01 +0000

    Add package salt-verifier/0.1.1/salt-verifier-0.1.1.tar.gz

---
+++ packages/salt-verifier/salt-verifier-0.1.1.tar.gz/salt-verifier-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(
+    name='salt-verifier',
+    version=VERSION,
+    description="salt-verifier - The salt verifier",
+    long_description=LONG_DESCRIPTION,
+    keywords='',
+    author='Reuven V. Gonzales',
+    author_email='reuven@virtru.com',
+    url="https://github.com/virtru-ops/salt-verifier",
+    license='MIT',
+    platforms='*nix',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'm2crypto',
+        'pyzmq'
+    ],
+    entry_points={
+        'console_scripts': [
+            'salt-verifier-server=saltverifier.server:run',
+            'salt-verifier-server-upstart-script=saltverifier.upstart:generate_upstart_script',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Build Tools',
+    ]
+)
