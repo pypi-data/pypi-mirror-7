3da120acd74b493cc0b5b35f2a1eba998c382d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:04 +0000

    Add package flask_rdf/0.1.5/flask_rdf-0.1.5.tar.gz

---
+++ packages/flask_rdf/flask_rdf-0.1.5.tar.gz/flask_rdf-0.1.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+try:
+	from setuptools import setup
+except:
+	pass
+
+requirements = open('requirements.txt').read().split('\n')
+test_requirements = open('requirements.test.txt').read().split('\n')
+long_description = open('README.md').read()
+
+setup(name='flask_rdf',
+      version='0.1.5',
+      description='Flask decorator to output RDF using content negotiation',
+      author='Walter Huf',
+      author_email='hufman@gmail.com',
+      url='https://github.com/hufman/flask_rdf',
+      packages=['flask_rdf'],
+      install_requires=requirements,
+      test_requires=requirements + test_requirements,
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Framework :: Flask',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+     ],
+     license='BSD',
+     platforms=['Any']
+)
