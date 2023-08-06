a9ff18a5c387fc9f0f1a4d32d0a3551ea58bea19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:01 +0000

    Add package chembl_compatibility/0.5.2/chembl_compatibility-0.5.2.tar.gz

---
+++ packages/chembl_compatibility/chembl_compatibility-0.5.2.tar.gz/chembl_compatibility-0.5.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'mnowotka'
+
+import sys, os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 7):
+    raise Exception('ChEMBL software stack requires python 2.7.3 - 2.7.7')
+
+setup(
+    name='chembl_compatibility',
+    version='0.5.2',
+    author='Michal Nowotka',
+    platforms=['Linux'],
+    author_email='mnowotka@ebi.ac.uk',
+    description='Compatibility layer for running webservices from outside ChEMBL',
+    url='https://www.ebi.ac.uk/chembl/',
+    license='CC BY-SA 3.0',
+    packages=['chembl_compatibility'],
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'chembl_core_model>=0.5.4',
+        'chembl_migration_model>=0.5.2'
+    ],
+    include_package_data=False,
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Scientific/Engineering :: Chemistry'],
+    zip_safe=False,
+)
