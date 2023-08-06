118595294d99a4873eeaa0e74faf69be22d114ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package syndicate/0.99.4/syndicate-0.99.4.tar.gz

---
+++ packages/syndicate/syndicate-0.99.4.tar.gz/syndicate-0.99.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='syndicate',
+    version='0.99.4',
+    description='A wrapper for REST APIs',
+    author='Justin Mayfield',
+    author_email='tooker@gmail.com',
+    url='https://github.com/mayfield/syndicate/',
+    license='MIT',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dateutil',
+        'tornado',
+    ],
+    test_suite='test',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
