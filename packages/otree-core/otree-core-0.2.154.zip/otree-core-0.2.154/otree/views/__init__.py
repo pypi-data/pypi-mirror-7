6836257b09ac470179892d068386f20a543158f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:06 +0000

    Add package otree-core/0.2.154/otree-core-0.2.154.zip

---
+++ packages/otree-core/otree-core-0.2.154.zip/otree-core-0.2.154/otree/views/__init__.py
@@ -0,0 +1,63 @@
+"""public api"""
+
+from otree.views.abstract import (
+    InitializePlayer,
+    InitializeExperimenter,
+)
+
+from importlib import import_module
+abstract = import_module('otree.views.abstract')
+
+class MatchWaitPage(abstract.MatchCheckpoint):
+
+    def body_text(self):
+        return super(MatchWaitPage, self).body_text()
+
+    def title_text(self):
+        return super(MatchWaitPage, self).title_text()
+
+    def action(self):
+        return super(MatchWaitPage, self).action()
+
+class SubsessionWaitPage(abstract.SubsessionCheckpoint):
+
+    def body_text(self):
+        return super(SubsessionWaitPage, self).body_text()
+
+    def title_text(self):
+        return super(SubsessionWaitPage, self).title_text()
+
+    def action(self):
+        return super(SubsessionWaitPage, self).action()
+
+class Page(abstract.PlayerUpdateView):
+
+    def variables_for_template(self):
+        return super(Page, self).variables_for_template()
+
+    def after_valid_form_submission(self):
+        return super(Page, self).after_valid_form_submission()
+
+    def get_form_class(self):
+        return super(Page, self).get_form_class()
+
+    def participate_condition(self):
+        return super(Page, self).participate_condition()
+
+    template_name = None
+
+class ExperimenterPage(abstract.ExperimenterUpdateView):
+
+    def variables_for_template(self):
+        return super(ExperimenterPage, self).variables_for_template()
+
+    def after_valid_form_submission(self):
+        return super(ExperimenterPage, self).after_valid_form_submission()
+
+    def get_form_class(self):
+        return super(ExperimenterPage, self).get_form_class()
+
+    def participate_condition(self):
+        return super(ExperimenterPage, self).participate_condition()
+
+    template_name = None
