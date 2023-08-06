69ff374f85e406186c611f3a2da00f1841e49cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:58 +0000

    Add package flask-autorest/0.1.5/flask_autorest-0.1.5.tar.gz

---
+++ packages/flask-autorest/flask_autorest-0.1.5.tar.gz/flask_autorest-0.1.5/flask_autorest/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+AUTOREST_SOURCES:
+    {
+        'test': {
+            'uri': 'mysql://root:@localhost/test_stat',
+            'engine_kwargs': {
+                'pool_size': 1,
+                'pool_recycle': -1,
+                'max_overflow': 0,
+            },
+            'auth': ('admin', 'admin'),
+            'tables': {
+                    'user': {
+                        'per_page': 10,
+                        'max_per_page': 100,
+                        'auto_fix': False,
+                    }
+                },
+            }
+        }
+    }
+AUTOREST_BLUEPRINT_NAME
+AUTOREST_URL_PREFIX
+
+"""
+
+__version__ = '0.1.5'
+
+from .autorest import AutoRest
---
+++ packages/flask-autorest/flask_autorest-0.1.5.tar.gz/flask_autorest-0.1.5/flask_autorest/constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+DEFAULT_BLUEPRINT_NAME = 'autorest'
+DEFAULT_URL_PREFIX = '/autorest'
+
+DEFAULT_PER_PAGE = 1000
+# -1 代表不限制
+DEFAULT_MAX_PER_PAGE = -1
+
+# 是否自动创建不存在的表，添加不存在的列
+DEFAULT_AUTO_FIX_TABLE = True
