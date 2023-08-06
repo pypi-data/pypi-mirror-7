a7d996c83653cb78784b9c4de9d120ff13aaa4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:06 +0000

    Add package svn/0.3.15/svn-0.3.15.tar.gz

---
+++ packages/svn/svn-0.3.15.tar.gz/svn-0.3.15/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+long_description = ""
+
+setuptools.setup(
+    name='svn',
+    version='0.3.15',
+    description="Straight-forward Subversion wrapper.",
+    long_description=long_description,
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Topic :: Software Development :: Version Control'],
+    keywords='svn subversion',
+    author='Dustin Oprea',
+    author_email='myselfasunder@gmail.com',
+    url='https://github.com/dsoprea/PySvn',
+    license='GPL 2',
+    packages=setuptools.find_packages(exclude=['test']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
---
+++ packages/svn/svn-0.3.15.tar.gz/svn-0.3.15/svn/common.py
@@ -0,0 +1,142 @@
+import subprocess
+import logging
+import dateutil.parser
+import collections
+import xml.etree.ElementTree
+
+import svn
+
+_logger = logging.getLogger('svn')
+
+
+class CommonClient(object):
+    def __init__(self, url_or_path, type_):
+        self.__url_or_path = url_or_path
+
+        if type_ not in (svn.T_URL, svn.T_PATH):
+            raise ValueError("Type is invalid: %s" % (type_))
+
+        self.__type = type_
+
+    def run_command(self, subcommand, args, success_code=0, 
+                    return_stderr=False, combine=False, return_binary=False):
+        cmd = ['svn', subcommand] + args
+
+        _logger.debug("RUN: %s" % (cmd,))
+
+        p = subprocess.Popen(cmd, 
+                             stdout=subprocess.PIPE, 
+                             stderr=subprocess.PIPE)
+
+        (stdout, stderr) = p.communicate()
+        if p.returncode != success_code:
+            raise ValueError("Command failed with (%d): %s\n%s\n%s" % 
+                             (p.returncode, cmd, stdout, stderr))
+
+        s = stderr if return_stderr is True else stdout
+        if return_binary is True:
+            return s
+
+        s = s.decode('ASCII')
+
+        return s if combine is True else s.split("\n")
+
+    def rows_to_dict(self, rows, lc=True):
+        d = {}
+        for row in rows:
+            row = row.strip()
+            if not row:
+                continue
+
+            pivot = row.index(': ')
+            
+            k = row[:pivot]
+            v = row[pivot + 2:]
+
+            if lc is True:
+                k = k.lower()
+
+            d[k] = v
+
+        return d
+
+    def info(self):
+        result = self.run_command(
+                    'info', 
+                    ['--xml', self.__url_or_path], 
+                    combine=True)
+
+        root = xml.etree.ElementTree.fromstring(result)
+        return root.find('entry')
+
+    def export(self, path):
+        self.run_command('export', [self.__url_or_path, path])
+
+    def cat(self, rel_filepath):
+# TODO(dustin): Verify that this handles binaries well.
+        return self.run_command(
+                'cat', 
+                [self.__url_or_path + '/' + rel_filepath], 
+                return_binary=True)
+
+    def log_default(self, timestamp_from_dt=None, timestamp_to_dt=None, 
+                    limit=None):
+        """Allow for the most-likely kind of log listing: the complete list, a 
+        FROM and TO timestamp, a FROM timestamp only, or a quantity limit.
+        """
+
+        timestamp_from_phrase = ('{' + timestamp_from_dt.isoformat() + '}') \
+                                    if timestamp_from_dt \
+                                    else ''
+
+        timestamp_to_phrase = ('{' + timestamp_to_dt.isoformat() + '}') \
+                                if timestamp_to_dt \
+                                else ''
+
+        args = []
+
+        if timestamp_from_phrase or timestamp_to_phrase:
+            if not timestamp_from_phrase:
+                raise ValueError("The default log retriever can not take a TO "
+                                 "timestamp without a FROM timestamp.")
+
+            if not timestamp_to_phrase:
+                timestamp_to_phrase = 'HEAD'
+
+            args += ['-r', timestamp_from_phrase + ':' + timestamp_to_phrase]
+
+        if limit is not None:
+            args += ['-l', str(limit)]
+
+        result = self.run_command(
+                    'log', 
+                    args + ['--xml', self.__url_or_path], 
+                    combine=True)
+
+        root = xml.etree.ElementTree.fromstring(result)
+        c = collections.namedtuple(
+                'LogEntry', 
+                ['date', 'msg', 'revision', 'author'])
+        
+        for e in root.findall('logentry'):
+            entry_info = dict([(x.tag, x.text) for x in e.getchildren()])
+
+            yield c(
+                msg=entry_info['msg'],
+                author=entry_info['author'],
+                revision=int(e.get('revision')),
+                date=dateutil.parser.parse(entry_info['date']))
+
+    @property
+    def url(self):
+        if self.__type != svn.T_URL:
+            raise EnvironmentError("Only the remote-client has access to the URL.")
+
+        return self.__url_or_path
+
+    @property
+    def path(self):
+        if self.__type != svn.T_PATH:
+            raise EnvironmentError("Only the local-client has access to the path.")
+
+        return self.__url_or_path
