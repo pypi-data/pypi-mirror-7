aa5ccdaa9304c93adfcb69c401657d60576ceb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package pas.plugins.ldap/1.3.1/pas.plugins.ldap-1.3.1.tar.gz

---
+++ packages/pas.plugins.ldap/pas.plugins.ldap-1.3.1.tar.gz/pas.plugins.ldap-1.3.1/src/pas/plugins/ldap/plonecontrolpanel/inspector.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.Five import BrowserView
+from node.ext.ldap import LDAPNode
+from node.ext.ldap.interfaces import ILDAPGroupsConfig
+from node.ext.ldap.interfaces import ILDAPProps
+from node.ext.ldap.interfaces import ILDAPUsersConfig
+from node.utils import encode
+from zope.component import getUtility
+
+import json
+
+
+class LDAPInspector(BrowserView):
+
+    @property
+    def plugin(self):
+        portal = getUtility(ISiteRoot)
+        aclu = portal.acl_users
+        plugin = aclu.pasldap
+        return plugin
+
+    @property
+    def props(self):
+        return ILDAPProps(self.plugin)
+
+    def users_children(self):
+        users = ILDAPUsersConfig(self.plugin)
+        return self.children(users.baseDN)
+
+    def groups_children(self):
+        groups = ILDAPGroupsConfig(self.plugin)
+        return self.children(groups.baseDN)
+
+    def node_attributes(self):
+        rdn = self.request['rdn']
+        base = self.request['base']
+        if base == 'users':
+            users = ILDAPUsersConfig(self.plugin)
+            baseDN = users.baseDN
+        else:
+            groups = ILDAPGroupsConfig(self.plugin)
+            baseDN = groups.baseDN
+        root = LDAPNode(baseDN, self.props)
+        node = root[rdn]
+        ret = dict()
+        for key, val in node.attrs.items():
+            try:
+                if not node.attrs.is_binary(key):
+                    ret[encode(key)] = encode(val)
+                else:
+                    ret[encode(key)] = "(Binary Data with %d Bytes)" % len(val)
+            except UnicodeDecodeError:
+                ret[key.encode('utf-8')] = '! (UnicodeDecodeError)'
+            except Exception:
+                ret[key.encode('utf-8')] = '! (Unknown Exception)'
+        return json.dumps(ret)
+
+    def children(self, baseDN):
+        node = LDAPNode(baseDN, self.props)
+        ret = list()
+        for key in node:
+            ret.append({'rdn': key})
+        return json.dumps(ret)
