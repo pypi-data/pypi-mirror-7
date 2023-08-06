3c21f76dd959cc2ce654e183c681b59caebb7173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:42 +0000

    Add package xtea/0.1.1/xtea-0.1.1.zip

---
+++ packages/xtea/xtea-0.1.1.zip/xtea-0.1.1/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README") as rm:
+    long_text = rm.read()
+
+setup(name='xtea',
+      version='0.1.1',
+      description="A python version of XTEA",
+      long_description = long_text,
+      author="Simon Biewald",
+      author_email="simon.biewald@hotmail.de",
+      py_modules=['xtea'],
+      classifiers=[
+	"Development Status :: 4 - Beta",
+	"Operating System :: OS Independent",
+	"Programming Language :: Python :: 2 :: Only",
+	"Topic :: Security :: Cryptography"]
+      )
