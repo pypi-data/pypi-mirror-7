99affcc0c0f01013b401c77b0121c24993b33cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:28 +0000

    Add package Biggus/0.6.0/Biggus-0.6.0.tar.gz

---
+++ packages/Biggus/Biggus-0.6.0.tar.gz/Biggus-0.6.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Biggus',
+    version='0.6.0',
+    url='https://github.com/SciTools/biggus',
+    author='Richard Hattersley',
+    author_email='rhattersley@gmail.com',
+    packages=['biggus', 'biggus.tests'],
+    classifiers=['License :: OSI Approved :: '
+                 'GNU Lesser General Public License v3 (LGPLv3)'],
+    description='Virtual large arrays and lazy evaluation',
+    long_description=open('README.rst').read(),
+    use_2to3=True,
+    test_suite='biggus.tests'
+)
