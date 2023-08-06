300bef9718ed1aad6341b742fb17c984aad43d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package WTForms-Appengine/0.1/WTForms-Appengine-0.1.zip

---
+++ packages/WTForms-Appengine/WTForms-Appengine-0.1.zip/WTForms-Appengine-0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='WTForms-Appengine',
+    version='0.1',
+    url='http://github.com/wtforms/wtforms-appengine/',
+    license='BSD',
+    author='Thomas Johansson, James Crasta',
+    author_email='wtforms@simplecodes.com',
+    description='Appengine tools for WTForms',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=[
+        'wtforms_appengine',
+    ],
+    package_data={
+    },
+    install_requires=['WTForms>=1.0.5'],
+    extras_require={
+    }
+)
---
+++ packages/WTForms-Appengine/WTForms-Appengine-0.1.zip/WTForms-Appengine-0.1/tests/gaetest_common.py
@@ -0,0 +1,41 @@
+"""
+This contains common tools for gae tests, and also sets up the environment.
+
+It should be the first import in the unit tests.
+"""
+# -- First setup paths
+import sys
+import os
+my_dir = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.abspath(os.path.join(my_dir, '..'))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+SAMPLE_AUTHORS = (
+    ('Bob', 'Boston'),
+    ('Harry', 'Houston'),
+    ('Linda', 'London'),
+)
+
+
+class DummyPostData(dict):
+    def getlist(self, key):
+        v = self[key]
+        if not isinstance(v, (list, tuple)):
+            v = [v]
+        return v
+
+
+def fill_authors(Author):
+    """
+    Fill authors from SAMPLE_AUTHORS.
+    Model is passed so it can be either an NDB or DB model.
+    """
+    AGE_BASE = 30
+    authors = []
+    for name, city in SAMPLE_AUTHORS:
+        author = Author(name=name, city=city, age=AGE_BASE)
+        author.put()
+        authors.append(author)
+        AGE_BASE += 1
+    return authors
