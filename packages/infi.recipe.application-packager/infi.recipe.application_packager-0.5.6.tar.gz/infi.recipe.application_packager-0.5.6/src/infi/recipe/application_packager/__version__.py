2b8c3eb83c418a535651f23ec60f5e66ba419eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:14 +0000

    Add package infi.recipe.application-packager/0.5.6/infi.recipe.application_packager-0.5.6.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.6.tar.gz/infi.recipe.application_packager-0.5.6/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.5.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8b1965407461a6d9fc5e49d225d2d8a400067b57'
+__git_head_subject__ = 'HOSTDEV-1699 python location on ftp'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
