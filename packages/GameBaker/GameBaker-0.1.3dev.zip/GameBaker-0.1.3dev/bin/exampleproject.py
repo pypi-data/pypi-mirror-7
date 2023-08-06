cf7b79ab1c4b82550c82ef6e159697e80e18119c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package GameBaker/0.1.3dev/GameBaker-0.1.3dev.zip

---
+++ packages/GameBaker/GameBaker-0.1.3dev.zip/GameBaker-0.1.3dev/bin/exampleproject.py
@@ -0,0 +1,56 @@
+"""
+Creates a simple example GameBaker project.
+
+Currently creates a game featuring squares that move up and down in response to keypresses.
+"""
+
+import os
+from shutil import copyfile
+
+with open("levels.py", "w") as f:
+    f.write("""from gamebaker.classes import Level, View
+from blueprints import *
+
+levels = [Level([Square() for _ in range(700)],
+[View(0, 0, 720, 480, 160, 0, 0)])]""")
+
+with open("blueprints.py", "w") as f:
+    f.write("""from gamebaker.classes import Blueprint, Sprite
+from random import randint
+
+class Square(Blueprint):
+    sprite = Sprite("red_square.png")
+    def __init__(self):
+        x = randint(20, 700)
+        y = randint(20, 460)
+        super().__init__(x, y)
+        
+    def tick(self):
+        self.x += randint(-1, 1)
+        self.y += randint(-1, 1)
+        super().tick()
+        
+    def key_up_press(self):
+        self.yspeed -= 1
+        
+    def key_down_press(self):
+        self.yspeed += 1""")
+    
+with open("settings.py", "w") as f:
+    f.write("""from gamebaker.classes import Settings, Version
+settings = Settings(window_width=720, window_height=480)""")
+    
+with open("game.py", "w") as f:
+    f.write("""from gamebaker import game
+import blueprints
+from settings import settings
+from levels import levels
+
+views, objects = game.load_level(levels, 0)
+events = game.get_events(blueprints)
+game.main(events, views, objects, settings)
+""")
+    
+os.makedirs("images")
+red_square_image = os.path.join(os.path.dirname(os.path.realpath(__file__)), "red_square.png")
+copyfile(red_square_image, os.path.join(os.getcwd(), "red_square.png"))
---
+++ packages/GameBaker/GameBaker-0.1.3dev.zip/GameBaker-0.1.3dev/bin/newproject.py
@@ -0,0 +1,34 @@
+"""
+Creates a new GameBaker project.
+
+Creates files named `levels.py`, `blueprints.py`, `settings.py`, `game.py`; and a directory called `images`.
+Source files are filled with the necessary boilerplate to create a minimum running game.
+"""
+
+from os import makedirs
+
+with open("levels.py", "w") as f:
+    f.write("""from gamebaker.classes import Level, View
+from blueprints import *
+
+levels = [Level([], [View(0, 0, 640, 480, 160, 0, 0)])]""")
+
+with open("blueprints.py", "w") as f:
+    f.write("""from gamebaker.classes import Blueprint, Sprite""")
+    
+with open("settings.py", "w") as f:
+    f.write("""from gamebaker.classes import Settings, Version
+settings = Settings()""")
+    
+with open("game.py", "w") as f:
+    f.write("""from gamebaker import game
+import blueprints
+from settings import settings
+from levels import levels
+
+views, objects = game.load_level(levels, 0)
+events = game.get_events(blueprints)
+game.main(events, views, objects, settings)
+""")
+    
+makedirs("images")
