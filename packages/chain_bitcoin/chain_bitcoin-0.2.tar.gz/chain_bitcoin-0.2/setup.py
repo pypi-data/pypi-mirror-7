d657b6e44005feb1f002d5157d6cda6292706b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:13 +0000

    Add package chain_bitcoin/0.2/chain_bitcoin-0.2.tar.gz

---
+++ packages/chain_bitcoin/chain_bitcoin-0.2.tar.gz/chain_bitcoin-0.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import os.path
+import re
+
+dirname = os.path.dirname(__file__)
+
+
+def resolve_path(path):
+    return os.path.join(dirname, path)
+
+
+def read_lines(path):
+    with open(resolve_path(path)) as f:
+        return f.readlines()
+
+
+def read(path):
+    with open(resolve_path(path)) as f:
+        return f.read()
+
+
+def long_description():
+    return re.split('\n\.\. pypi [^\n]*\n', read('README.rst'), 1)[1]
+
+
+setup(
+    name='chain_bitcoin',
+    version='0.2',
+    author='Chris Martin',
+    author_email='ch.martin@gmail.com',
+    packages=['chain_bitcoin'],
+    url='https://github.com/cardforcoin/chain-bitcoin-python',
+    license='MIT',
+    description='Integration library for the Chain.com API',
+    long_description=long_description(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=read_lines('requirements-install.txt'),
+    tests_require=read_lines('requirements-test.txt'),
+)
