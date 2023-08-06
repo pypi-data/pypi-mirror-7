02bec68ef32917797f5e3f20e4ac7c858e2eee2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:58 +0000

    Add package django-candv-choices/1.0.0/django-candv-choices-1.0.0.tar.gz

---
+++ packages/django-candv-choices/django-candv-choices-1.0.0.tar.gz/django-candv-choices-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-candv-choices',
+    version='1.0.0',
+    description="Use complex constants built with 'candv' library instead of "
+                "standard 'choices' fields for 'Django' models.",
+    keywords="choices constants Django candv values",
+    license='LGPLv3',
+    url='https://github.com/oblalex/django-candv-choices',
+    author='Alexander Oblovatniy',
+    author_email='oblovatniy@gmail.com',
+    packages=find_packages(),
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
