6809bcc68aceb58032256f51ecc70e8cb72e12aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package infi.mountoolinux/0.1.14/infi.mountoolinux-0.1.14.tar.gz

---
+++ packages/infi.mountoolinux/infi.mountoolinux-0.1.14.tar.gz/infi.mountoolinux-0.1.14/setup.py
@@ -0,0 +1,48 @@
+
+SETUP_INFO = dict(
+    name = 'infi.mountoolinux',
+    version = '0.1.14',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'http://www.infinidat.com',
+    license = 'PSF',
+    description = """Python library for handling POSIX mounts""",
+    long_description = """Python library for handling POSIX mounts""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['Brownie',
+'infi.execute',
+'infi.pyutils',
+'munch',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = []),
+    )
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
---
+++ packages/infi.mountoolinux/infi.mountoolinux-0.1.14.tar.gz/infi.mountoolinux-0.1.14/src/infi/mountoolinux/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.14"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'dd890eabc3d0a725722819a8b91a6c5e1c6c7f22'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
