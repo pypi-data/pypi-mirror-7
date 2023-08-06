438f5be21fd89f78f6406bcd5809b3b87777dd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package PsiVisual/0.1.06/PsiVisual-0.1.06.zip

---
+++ packages/PsiVisual/PsiVisual-0.1.06.zip/PsiVisual-0.1.06/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='PsiVisual',
+    version='0.1.06',
+    author='John Coady',
+    author_email='johncoady@shaw.ca',
+    packages=['psivisual', 'psivisual.test'],
+    package_data={'psivisual': ['data/*.js']},
+    url='http://pypi.python.org/pypi/PsiVisual/',
+    license='LICENSE.txt',
+    description='Test Stuff',
+    long_description=open('README.txt').read(),
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Multimedia :: Graphics :: 3D Modeling',
+          'Topic :: Multimedia :: Graphics :: 3D Rendering',
+          'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
