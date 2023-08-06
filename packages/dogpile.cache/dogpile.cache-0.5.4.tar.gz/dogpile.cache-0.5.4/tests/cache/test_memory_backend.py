a5b56a3fec5b7608caf5c226ddc2318f0edecbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package dogpile.cache/0.5.4/dogpile.cache-0.5.4.tar.gz

---
+++ packages/dogpile.cache/dogpile.cache-0.5.4.tar.gz/dogpile.cache-0.5.4/tests/cache/test_memory_backend.py
@@ -0,0 +1,8 @@
+from ._fixtures import _GenericBackendTest
+
+class MemoryBackendTest(_GenericBackendTest):
+    backend = "dogpile.cache.memory"
+
+
+class MemoryPickleBackendTest(_GenericBackendTest):
+    backend = "dogpile.cache.memory_pickle"
