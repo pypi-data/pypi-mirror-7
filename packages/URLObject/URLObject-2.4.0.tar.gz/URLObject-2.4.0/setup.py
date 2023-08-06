cfa60decb62c8b77362e89c2298679f1e8e4cab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:09 +0000

    Add package URLObject/2.4.0/URLObject-2.4.0.tar.gz

---
+++ packages/URLObject/URLObject-2.4.0.tar.gz/URLObject-2.4.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='URLObject',
+    version='2.4.0',
+    description='A utility class for manipulating URLs.',
+    author='Zachary Voase',
+    author_email='z@zacharyvoase.com',
+    url='http://github.com/zacharyvoase/urlobject',
+    packages=find_packages(exclude=('test',)),
+)
