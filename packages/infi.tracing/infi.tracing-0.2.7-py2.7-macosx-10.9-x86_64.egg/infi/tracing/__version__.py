2e13a97e11c98461cb36b86ef396d5c5864a8b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:04 +0000

    Add package infi.tracing/0.2.7/infi.tracing-0.2.7-py2.7-macosx-10.9-x86_64.egg

---
+++ packages/infi.tracing/infi.tracing-0.2.7-py2.7-macosx-10.9-x86_64.egg/infi/tracing/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.7"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '53f43157ee649517e66b9474d87b67457c559e8d'
+__git_head_subject__ = 'Finished release v0.2.7.'
+__git_head_message__ = '* release/v0.2.7:\n  HOSTDEV-1732 adjusting the tests\n  fixing FileTraceDump::process on windows, for now without the timestamp\n  TRIVIAL fixing tests'
+__git_dirty_diff__ = ''
