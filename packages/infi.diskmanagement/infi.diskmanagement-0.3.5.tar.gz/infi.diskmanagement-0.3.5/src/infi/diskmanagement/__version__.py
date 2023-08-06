c07274a7201732b9231bca2f2f9dcb67ffaf6367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package infi.diskmanagement/0.3.5/infi.diskmanagement-0.3.5.tar.gz

---
+++ packages/infi.diskmanagement/infi.diskmanagement-0.3.5.tar.gz/infi.diskmanagement-0.3.5/src/infi/diskmanagement/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.3.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '1cdd97c717b24885643db1ca9177ef432659e742'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
