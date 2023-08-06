9881c93d4fbc5061c87988538d29a7dba59b3ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:29 +0000

    Add package geoffrey-filecontent/0.0.2/geoffrey-filecontent-0.0.2.tar.gz

---
+++ packages/geoffrey-filecontent/geoffrey-filecontent-0.0.2.tar.gz/geoffrey-filecontent-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from io import open
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+readme = open('README.rst', encoding='utf-8').read()
+
+setup(name='geoffrey-filecontent',
+      version='0.0.2',
+      description='Centralize multiple data source plugins.',
+      long_description=readme,
+      author='Roberto Abdelkader Martínez Pérez',
+      author_email='robertomartinezp@gmail.com',
+      url='https://github.com/GeoffreyCI/geoffrey-filecontent',
+      include_package_data=True,
+      packages=find_packages(),
+      namespace_package=['geoffreyplugins'],
+      install_requires=[
+          'filemagic==1.6'
+      ]
+)
