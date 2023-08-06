b45bbfdcbc15294be42217ec11d8b24f7a495472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package gsmtpd/0.1.3/gsmtpd-0.1.3.tar.gz

---
+++ packages/gsmtpd/gsmtpd-0.1.3.tar.gz/gsmtpd-0.1.3/gsmtpd/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1'
+__author__ = 'Meng Zhuo <mengzhuo1203@gmail.com>'
+
+from channel import SMTPChannel
+from server import PureProxy, SMTPServer, DebuggingServer
---
+++ packages/gsmtpd/gsmtpd-0.1.3.tar.gz/gsmtpd-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as require:
+
+    setup(name='gsmtpd',
+            version='0.1.3',
+            license='MIT',
+            description='A smtpd server impletement base on Gevent',
+            author='Meng Zhuo',
+            author_email='mengzhuo1203@gmail.com',
+            url='https://github.com/34nm/gsmtpd',
+            packages=['gsmtpd'],
+            install_requires=[line for line in require.readlines()],
+            classifiers=[
+                        'License :: OSI Approved :: MIT License',
+                        'Programming Language :: Python :: 2',
+                        'Programming Language :: Python :: 2.6',
+                        'Programming Language :: Python :: 2.7',
+                        'Topic :: Communications :: Email :: Mail Transport Agents',
+                        'Topic :: Communications :: Email'
+            ],
+            long_description=long_description)
