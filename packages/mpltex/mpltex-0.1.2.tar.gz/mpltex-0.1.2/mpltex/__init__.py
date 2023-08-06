1d3dec7d596b9d52e5e700a8444daf76f3dfea73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:38 +0000

    Add package mpltex/0.1.2/mpltex-0.1.2.tar.gz

---
+++ packages/mpltex/mpltex-0.1.2.tar.gz/mpltex-0.1.2/mpltex/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+mpltex
+======
+
+**mpltex** is a python package for producing publication quality images using matplotlib.
+
+"""
+
+__author__ = "Yi-Xin Liu <liuyxpp@gmail.com>"
+__license__ = "BSD License"
+__version__ = "0.1.2"
+
+from .general import *
+from .colors import *
+from .styles import *
+from .layout import *
+from .acs import *
+from .presentation import *
+from .web import *
+
