93e00c073d19ec040214dfdefb96a37777dddb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package mos_first/1.0.0/mos_first-1.0.0.zip

---
+++ packages/mos_first/mos_first-1.0.0.zip/mos_first-1.0.0/mos_first.py
@@ -0,0 +1,7 @@
+def print_lol(the_list) :
+    """ This function is recursive function """
+    for each_item in the_list :
+        if isinstance(each_item, list) :
+            print_lol(each_item)
+        else :
+            print(each_item)
