903bf63730c47916d8390358393bea3ca7ca19c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:05 +0000

    Add package munerator/0.9.11/munerator-0.9.11-py2-none-any.whl

---
+++ packages/munerator/munerator-0.9.11-py2-none-any.whl/munerator/context.py
@@ -0,0 +1,144 @@
+"""Add game and player context to events
+
+Usage:
+  munerator [options] context
+
+Options:
+  -v --verbose          Verbose logging
+  --events-socket url   ZMQ socket for raw events [default: tcp://127.0.0.1:9001]
+  --context-socket url  ZMQ socket for context events [default: tcp://0.0.0.0:9002]
+  --rcon-socket url     ZMQ socket for rcon commands [default: tcp://127.0.0.1:9005]
+
+"""
+from docopt import docopt
+import zmq
+import logging
+import collections
+log = logging.getLogger(__name__)
+
+import json
+
+team_id_map = ['', 'blue', 'red', 'spectator']
+deduplicate = collections.deque(maxlen=5)
+
+
+def get_dict_value_from_key_if_key_value(data, value_key, query_key, query_value):
+    item = [v for k, v in data.items() if v.get(query_key) == query_value]
+    if item:
+        return item[0].get(value_key)
+
+
+class GameContext(object):
+
+    def __init__(self, in_socket, out_socket, rcon_socket):
+        self.start_ts = 0
+        self.stop_ts = 0
+
+        self.gameinfo = {}
+        self.clients = {}
+
+        self.in_socket = in_socket
+        self.out_socket = out_socket
+        self.rcon_socket = rcon_socket
+
+    def eventstream(self):
+        # prime commands for current game info
+        self.rcon_socket.send_string('status')
+        self.rcon_socket.send_string('getstatus')
+
+        while True:
+            data = self.in_socket.recv_json()
+            log.debug('   in: %s' % data)
+
+            if str(data) in deduplicate:
+                log.debug('skip')
+                continue
+            deduplicate.append(str(data))
+
+            contexted_data = self.handle_event(data)
+
+            self.out_socket.send_string("%s %s" % (contexted_data.get('kind'), json.dumps(contexted_data)))
+
+    def handle_event(self, data):
+        kind = data.get('kind')
+        ts = data.get('timestamp')
+        client_id = data.get('client_id')
+
+        # for some events we need to translate player_name into client_id
+        if not client_id and kind in ['say', 'killer', 'killed']:
+            client_id = get_dict_value_from_key_if_key_value(
+                self.clients, 'client_id', 'name', data.get('player_name'))
+            data['client_id'] = client_id
+
+        if kind == 'initgame':
+            self.start_ts = ts
+            self.gameinfo = {
+                'mapname': data.get('mapname'),
+                'gametype': data.get('gametype'),
+                'timestamp': data.get('timestamp'),
+                'num_players': 0,
+                'start': ts,
+                'stop': None,
+                'current': True
+            }
+            self.clients = {}
+        elif kind == 'clientuserinfochanged':
+            log.debug('setting client info: %s' % client_id)
+            self.clients[client_id] = {
+                'name': data.get('player_name'),
+                'id': data.get('guid'),
+                'guid': data.get('guid'),
+                'client_id': client_id,
+                'team_id': data.get('team_id'),
+                'team': team_id_map[int(data.get('team_id'))],
+                'score': 0,
+                'online': True
+            }
+            self.gameinfo['num_players'] = len(self.clients)
+        elif kind == 'playerscore':
+            log.debug('setting client score: %s' % client_id)
+            if client_id in self.clients:
+                self.clients[client_id]['score'] = data.get('score')
+        elif kind == 'clientdisconnect':
+            if client_id in self.clients:
+                self.clients[client_id]['online'] = False
+
+        data['game_info'] = self.gameinfo
+        data['client_info'] = self.clients.get(client_id, {})
+
+        if kind == 'clientdisconnect':
+            try:
+                del self.clients[client_id]
+            except KeyError:
+                pass
+        elif kind == 'shutdowngame':
+            # add stop time
+            self.gameinfo['stop'] = ts
+            self.gameinfo['current'] = False
+
+            # reset current context
+            self.gameinfo = {}
+            self.clients = {}
+
+        log.debug(' out: %s' % data)
+
+        return data
+
+
+def main(argv):
+    args = docopt(__doc__, argv=argv)
+
+    context = zmq.Context()
+    in_socket = context.socket(zmq.PULL)
+    in_socket.bind(args['--events-socket'])
+
+    out_socket = context.socket(zmq.PUB)
+    out_socket.bind(args['--context-socket'])
+
+    # setup rcon socket
+    rcon_socket = context.socket(zmq.PUSH)
+    rcon_socket.connect(args['--rcon-socket'])
+
+    gc = GameContext(in_socket, out_socket, rcon_socket)
+
+    gc.eventstream()
---
+++ packages/munerator/munerator-0.9.11-py2-none-any.whl/munerator/store.py
@@ -0,0 +1,160 @@
+"""Store game events in database
+
+Usage:
+  munerator [options] store
+
+Options:
+  -v --verbose            Verbose logging
+  --context-socket url    ZMQ socket for conext event [default: tcp://127.0.0.1:9002]
+  --database ip:port      Host and port for mongo database [default: 127.0.0.1:27017]
+  --rcon-socket url       ZMQ socket for rcon commands [default: tcp://127.0.0.1:9005]
+
+"""
+import json
+import logging
+from functools import partial
+
+import zmq
+from docopt import docopt
+from munerator.common.models import Games, Players, Votes
+from eve import Eve
+from eve_mongoengine import EveMongoengine
+
+log = logging.getLogger(__name__)
+
+
+def handle_events(in_socket, rcon_socket):
+    """
+    Loop over incoming messages and handle them individually.
+    """
+    # prime commands for current game info
+    rcon_socket.send_string('status')
+    rcon_socket.send_string('getstatus')
+
+    log.info('listening for game events')
+    while True:
+        msg = in_socket.recv_string()
+
+        log.debug('got: %s' % msg)
+        data = json.loads(msg.split(' ', 1)[-1])
+        kind = data.get('kind')
+
+        try:
+            handle_event(kind, data, rcon_socket)
+        except:
+            log.exception('error in event handling')
+
+
+def handle_event(kind, data, rcon_socket):
+    """
+    Parse event message and update database with new information.
+    """
+
+    # get player and/or game id from data
+    player_id = str(data.get('client_info', {}).get('guid', ''))
+    game_id = str(data.get('game_info', {}).get('timestamp', ''))
+
+    player, new = Players.objects.get_or_create(guid=player_id)
+    if new:
+        player.save()
+    game, new = Games.objects.get_or_create(game_id=game_id)
+    if new:
+        game.save()
+
+    # handle player updates
+    if player and kind in ['clientbegin', 'clientdisconnect', 'clientuserinfochanged', 'playerscore', 'clientstatus']:
+        if new:
+            log.debug('creating new player')
+
+        # on name change, store previous name
+        if data['client_info'] and player.name != data['client_info']['name']:
+            player.update(add_to_set__names=data['client_info']['name'])
+
+        # update variable data
+        player.update(**{'set__%s' % k: v for k, v in data['client_info'].items() if not k.endswith('id')})
+
+        # add player to game
+        if game_id:
+            game, new = Games.objects.get_or_create(game_id=game_id)
+            game.update(add_to_set__players=player)
+
+        log.info('updated player')
+
+    # handle game updates
+    if game and kind in ['initgame', 'shutdowngame', 'getstatus']:
+        if new:
+            log.debug('creating new game')
+
+        # update variable data
+        game.update(**{'set__%s' % k: v for k, v in data['game_info'].items()if not k.endswith('id')})
+
+        if data.get('extras'):
+            game.options = data.get('extras')
+            game.save()
+
+        log.info('updated game')
+
+        # request extra game info at start of game
+        if kind == 'initgame':
+            rcon_socket.send_string('getstatus')
+
+    # handle votes
+    if kind == 'say' and player and game:
+        vote = None
+        if data.get('text') in ['+1', 'gg', 'GG', 'GGG', 'like', 'fuckthismaprocks', '++', '+1337']:
+            vote = Votes(player=player, game=game, vote=1)
+        elif data.get('text') in ['-1', '--1', '-11', '-1000', '-2', 'dislike', 'hate', 'RAGE!!!', 'fuckthismap', '--']:
+            vote = Votes(player=player, game=game, vote=-1)
+        if vote:
+            vote.save()
+            rcon_socket.send_string('say %s^7 your vote has been counted' % player.name)
+            log.info('saved vote')
+
+
+def setup_eve_mongoengine(host, port):
+    # app settings
+    my_settings = {
+        'MONGO_HOST': host,
+        'MONGO_PORT': int(port),
+        'MONGO_PASSWORD': None,
+        'MONGO_USERNAME': None,
+        'MONGO_DBNAME': 'munerator',
+        'DOMAIN': {'eve-mongoengine': {}},
+    }
+    app = Eve(settings=my_settings)
+
+    # setup eve mongodb database
+    ext = EveMongoengine(app)
+
+    # add models
+    ext.add_model([Players, Games, Votes])
+
+
+def main(argv):
+    args = docopt(__doc__, argv=argv)
+
+    # setup zmq input socket
+    context = zmq.Context()
+    in_socket = context.socket(zmq.SUB)
+    in_socket.connect(args['--context-socket'])
+    in_socket.setsockopt(zmq.SUBSCRIBE, '')
+
+    # apply message filters
+    filters = [
+        'initgame', 'shutdowngame', 'clientdisconnect', 'say',
+        'clientbegin', 'clientuserinfochanged', 'playerscore'
+    ]
+    add_filter = partial(in_socket.setsockopt, zmq.SUBSCRIBE)
+    map(add_filter, filters)
+
+    # setup rcon socket
+    rcon_socket = context.socket(zmq.PUSH)
+    rcon_socket.connect(args['--rcon-socket'])
+
+    # setup database
+    host, port = args['--database'].split(':')
+
+    setup_eve_mongoengine(host, port)
+
+    # start event loop
+    handle_events(in_socket, rcon_socket)
