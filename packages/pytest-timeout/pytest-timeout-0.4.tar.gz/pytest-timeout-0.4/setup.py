bee81d8c2240b08a38c15ea752f9ab41607798a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package pytest-timeout/0.4/pytest-timeout-0.4.tar.gz

---
+++ packages/pytest-timeout/pytest-timeout-0.4.tar.gz/pytest-timeout-0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='pytest-timeout',
+    description='py.test plugin to abort hanging tests',
+    long_description=open("README").read(),
+    version='0.4',
+    author='Floris Bruynooghe',
+    author_email='flub@devork.be',
+    url='http://bitbucket.org/flub/pytest-timeout/',
+    license='MIT',
+    py_modules=['pytest_timeout'],
+    entry_points={'pytest11': ['timeout = pytest_timeout']},
+    install_requires=['pytest>=2.4'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: DFSG approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing'
+    ],
+)
---
+++ packages/pytest-timeout/pytest-timeout-0.4.tar.gz/pytest-timeout-0.4/test_pytest_timeout.py
@@ -0,0 +1,229 @@
+import os
+import os.path
+import signal
+
+import pytest
+
+
+pytest_plugins = 'pytester'
+
+
+have_sigalrm = pytest.mark.skipif(not hasattr(signal, "SIGALRM"),
+                                  reason='OS does not have SIGALRM')
+
+
+@have_sigalrm
+def test_sigalrm(testdir):
+    testdir.makepyfile("""
+        import time
+
+        def test_foo():
+            time.sleep(2)
+     """)
+    result = testdir.runpytest('--timeout=1')
+    result.stdout.fnmatch_lines([
+        '*Failed: Timeout >1s*'
+    ])
+
+
+def test_thread(testdir):
+    testdir.makepyfile("""
+        import time
+
+        def test_foo():
+            time.sleep(2)
+    """)
+    result = testdir.runpytest('--timeout=1', '--timeout_method=thread')
+    result.stderr.fnmatch_lines([
+        '*++ Timeout ++*',
+        '*~~ Stack of MainThread* ~~*',
+        '*File *, line *, in *',
+        '*++ Timeout ++*',
+    ])
+    assert '++ Timeout ++' in result.stderr.lines[-1]
+
+
+def test_timeout_env(testdir, monkeypatch):
+    testdir.makepyfile("""
+        import time
+
+        def test_foo():
+            time.sleep(2)
+    """)
+    monkeypatch.setitem(os.environ, 'PYTEST_TIMEOUT', '1')
+    result = testdir.runpytest()
+    assert result.ret > 0
+
+
+# @pytest.mark.parametrize('meth', [have_sigalrm('signal'), 'thread'])
+# def test_func_fix(meth, testdir):
+#     testdir.makepyfile("""
+#         import time, pytest
+
+#         @pytest.fixture(scope='function')
+#         def fix():
+#             time.sleep(2)
+
+#         def test_foo(fix):
+#             pass
+#     """)
+#     result = testdir.runpytest('--timeout=1',
+#                                '--timeout_method={0}'.format(meth))
+#     assert result.ret > 0
+#     assert 'Timeout' in result.stdout.str() + result.stderr.str()
+
+
+@pytest.mark.parametrize('meth', [have_sigalrm('signal'), 'thread'])
+@pytest.mark.parametrize('scope', ['function', 'class', 'module', 'session'])
+def test_fix_setup(meth, scope, testdir):
+    testdir.makepyfile("""
+        import time, pytest
+
+        class TestFoo:
+
+            @pytest.fixture(scope='{scope}')
+            def fix(self):
+                time.sleep(2)
+
+            def test_foo(self, fix):
+                pass
+    """.format(scope=scope))
+    result = testdir.runpytest('--timeout=1',
+                               '--timeout_method={0}'.format(meth))
+    assert result.ret > 0
+    assert 'Timeout' in result.stdout.str() + result.stderr.str()
+
+
+@pytest.mark.parametrize('meth', [have_sigalrm('signal'), 'thread'])
+@pytest.mark.parametrize('scope', ['function', 'class', 'module', 'session'])
+def test_fix_finalizer(meth, scope, testdir):
+    testdir.makepyfile("""
+        import time, pytest
+
+        class TestFoo:
+
+            @pytest.fixture(scope='{scope}')
+            def fix(self, request):
+                print('fix setup')
+                def fin():
+                    print('fix finaliser')
+                    time.sleep(2)
+                request.addfinalizer(fin)
+
+            def test_foo(self, fix):
+                pass
+    """.format(scope=scope))
+    result = testdir.runpytest('--timeout=1', '-s',
+                               '--timeout_method={0}'.format(meth))
+    assert result.ret > 0
+    assert 'Timeout' in result.stdout.str() + result.stderr.str()
+
+
+@have_sigalrm
+def test_timeout_mark_sigalrm(testdir):
+    testdir.makepyfile("""
+        import time, pytest
+
+        @pytest.mark.timeout(1)
+        def test_foo():
+            time.sleep(2)
+            assert False
+    """)
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines(['*Failed: Timeout >1s*'])
+
+
+def test_timeout_mark_timer(testdir):
+    testdir.makepyfile("""
+        import time, pytest
+
+        @pytest.mark.timeout(1)
+        def test_foo():
+            time.sleep(2)
+    """)
+    result = testdir.runpytest('--timeout_method=thread')
+    result.stderr.fnmatch_lines(['*++ Timeout ++*'])
+
+
+def test_timeout_mark_nonint(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.mark.timeout('foo')
+        def test_foo():
+            pass
+   """)
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines(['*ValueError*'])
+
+
+def test_timeout_mark_args(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.mark.timeout(1, 2)
+        def test_foo():
+            pass
+    """)
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines(['*ValueError*'])
+
+
+def test_timeout_mark_method_nokw(testdir):
+    testdir.makepyfile("""
+        import time, pytest
+
+        @pytest.mark.timeout(1, 'thread')
+        def test_foo():
+            time.sleep(2)
+    """)
+    result = testdir.runpytest()
+    result.stderr.fnmatch_lines(['*+ Timeout +*'])
+
+
+def test_timeout_mark_noargs(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.mark.timeout
+        def test_foo():
+            pass
+    """)
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines(['*TypeError*'])
+
+
+def test_ini_timeout(testdir):
+    testdir.makepyfile("""
+        import time
+
+        def test_foo():
+            time.sleep(2)
+    """)
+    testdir.makeini("""
+        [pytest]
+        timeout = 1
+    """)
+    result = testdir.runpytest()
+    assert result.ret
+
+
+def test_ini_method(testdir):
+    testdir.makepyfile("""
+        import time
+
+        def test_foo():
+            time.sleep(2)
+    """)
+    testdir.makeini("""
+        [pytest]
+        timeout = 1
+        timeout_method = thread
+    """)
+    result = testdir.runpytest()
+    assert '=== 1 failed in ' not in result.outlines[-1]
+
+
+def test_marker_help(testdir):
+    result = testdir.runpytest('--markers')
+    result.stdout.fnmatch_lines(['@pytest.mark.timeout(*'])
