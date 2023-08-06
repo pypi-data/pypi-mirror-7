78f188b01dcc661112095a41737b9f76ee6e0420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package infi.execute/0.0.18/infi.execute-0.0.18-py2.7.egg

---
+++ packages/infi.execute/infi.execute-0.0.18-py2.7.egg/infi/execute/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.18"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.18)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a3c229d6a772c5426b1a3a720433421a05cfa90a'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
