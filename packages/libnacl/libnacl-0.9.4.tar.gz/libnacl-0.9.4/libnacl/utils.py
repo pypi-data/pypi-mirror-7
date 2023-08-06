54e67551f74089ed572cedef2aabca19f316fbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package libnacl/0.9.4/libnacl-0.9.4.tar.gz

---
+++ packages/libnacl/libnacl-0.9.4.tar.gz/libnacl-0.9.4/libnacl/utils.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Import nacl libs
+import libnacl
+import libnacl.encode
+
+# Import python libs
+import time
+import binascii
+
+
+class BaseKey(object):
+    '''
+    Include methods for key management convenience
+    '''
+    def hex_sk(self):
+        if hasattr(self, 'sk'):
+            return libnacl.encode.hex_encode(self.sk)
+        else:
+            return ''
+
+    def hex_pk(self):
+        if hasattr(self, 'pk'):
+            return libnacl.encode.hex_encode(self.pk)
+
+    def hex_vk(self):
+        if hasattr(self, 'vk'):
+            return libnacl.encode.hex_encode(self.vk)
+
+    def hex_seed(self):
+        if hasattr(self, 'seed'):
+            return libnacl.encode.hex_encode(self.seed)
+
+
+def salsa_key():
+    '''
+    Generates a salsa2020 key
+    '''
+    return libnacl.randombytes(libnacl.crypto_secretbox_KEYBYTES)
+
+
+def time_nonce():
+    '''
+    Generates a safe nonce
+
+    The nonce generated here is done by grabbing the 20 digit microsecond
+    timestamp and appending 4 random chars
+    '''
+    nonce = '{0}{1}'.format(
+            str(int(time.time() * 1000000)),
+            binascii.hexlify(libnacl.randombytes(24)).decode(encoding='UTF-8'))
+    return nonce.encode(encoding='UTF-8')[:libnacl.crypto_box_NONCEBYTES]
