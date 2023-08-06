30c86e66c161d263f650739682e36f218fbae4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:43 +0000

    Add package mudicom/0.0.1/mudicom-0.0.1.tar.gz

---
+++ packages/mudicom/mudicom-0.0.1.tar.gz/mudicom-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import re
+from setuptools import setup, find_packages
+
+setup(
+	name='mudicom',
+	version='0.0.1',
+	author='Eric Bower',
+	author_email='neurosnap@gmail.com',
+	packages=find_packages(),
+	url='http://pypi.python.org/pypi/mudicom/',
+	license='LICENSE',
+)
