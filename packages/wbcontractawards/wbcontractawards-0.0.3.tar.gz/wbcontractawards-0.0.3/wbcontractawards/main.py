bc4af7eeb65f8b51b78d4195bc208b37275dc55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:58 +0000

    Add package wbcontractawards/0.0.3/wbcontractawards-0.0.3.tar.gz

---
+++ packages/wbcontractawards/wbcontractawards-0.0.3.tar.gz/wbcontractawards-0.0.3/wbcontractawards/main.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+import itertools
+
+import wbcontractawards.download as d
+import wbcontractawards.parse as p
+
+def contracts():
+    for os in itertools.count(0, 10):
+        response = d.search(os)
+        contract_urls = p.search(response)
+        if [] == contract_urls:
+            break
+        for url in contract_urls:
+            response = d.get(url)
+            try:
+                yield p.contract(response)
+            except:
+                sys.stderr.write('Error at %s\n' % url)
+                raise
+
+def cli():
+    writer = csv.writer(sys.stdout)
+    writer.writerow(['contract','bidder','status','amount','currency'])
+    for contract in contracts():
+        if contract != None:
+            for bid in contract['bids']:
+                row = [
+                    contract['url'],
+                    bid.get('bidder.name'),
+                    bid.get('status'),
+                    bid.get('opening.price.amount'),
+                    bid.get('opening.price.currency'),
+                ]
+                writer.writerow(row)
