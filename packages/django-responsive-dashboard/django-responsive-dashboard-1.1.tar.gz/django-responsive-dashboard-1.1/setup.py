6b02447934d77cfe6f87d8e9a479acceda9fc8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package django-responsive-dashboard/1.1/django-responsive-dashboard-1.1.tar.gz

---
+++ packages/django-responsive-dashboard/django-responsive-dashboard-1.1.tar.gz/django-responsive-dashboard-1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-responsive-dashboard",
+    version = "1.1",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("A generic and easy dashboard for Django applications."),
+    license = "BSD",
+    keywords = "django dashboard responsive",
+    url = "https://github.com/burke-software/django-responsive-dashboard",
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+        'south',
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=['django', 'django-positions',]
+)
