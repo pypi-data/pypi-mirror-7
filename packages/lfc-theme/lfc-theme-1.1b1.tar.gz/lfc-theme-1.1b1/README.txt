4dab6c43dbcbe80c74cb6aaef2bb689b2171ee47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:45 +0000

    Add package lfc-theme/1.1b1/lfc-theme-1.1b1.tar.gz

---
+++ packages/lfc-theme/lfc-theme-1.1b1.tar.gz/lfc-theme-1.1b1/README.txt
@@ -0,0 +1,24 @@
+Introduction
+============
+
+LFC THeme is the default theme for `LFC`_, a Content Management System (CMS)
+based on Django.
+
+See `LFC`_ for more information.
+
+.. _`LFC`: http://pypi.python.org/pypi/django-lfc
+
+Changes
+=======
+
+1.1 beta 1 (2014-06-17)
+-----------------------
+
+* Adds Django 1.6 support
+* Updates colophon
+* Updates german translations
+
+HISTORY
+=======
+
+For the complete history please look into HISTORY.txt.
---
+++ packages/lfc-theme/lfc-theme-1.1b1.tar.gz/lfc-theme-1.1b1/lfc_theme/static/js/lfctheme.js
@@ -0,0 +1,107 @@
+$.fn.delay = function(options) {
+    var timer;
+    var delayImpl = function(eventObj) {
+        if (timer != null) {
+            clearTimeout(timer);
+        }
+        var newFn = function() {
+            options.fn(eventObj);
+        };
+        timer = setTimeout(newFn, options.delay);
+    }
+
+    return this.each(function() {
+        var obj = $(this);
+        obj.bind(options.event, function(eventObj) {
+             delayImpl(eventObj);
+        });
+    });
+};
+
+$(function() {
+
+    $("a.lightbox").lightBox();
+
+    $("#search-form").keypress(function(e) {
+        if (e.keyCode == 13) {
+            var url = $("#live-search-results .selected a").attr("href");
+            if (url) {
+                window.location = url;
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+          return true;
+        }
+    });
+
+    $(".search").attr("autocomplete", "off")
+    $(".search").focus();
+
+    $(".search").keyup(function(e) {
+        var current = $("#live-search-results .selected")
+
+        // ESC
+        if (e.keyCode == 27) {
+            $("#live-search-results").hide();
+        }
+
+        // UP
+        else if (e.keyCode == 38) {
+            var prev = $("#live-search-results .selected").prev("#live-search-results div.item");
+
+            if (prev.html()) {
+                prev.addClass("selected");
+            }
+            else {
+                $("#live-search-results div.item:last").addClass("selected");
+            }
+            current.removeClass("selected");
+        }
+
+        // RIGHT
+        else if (e.keyCode == 39) {
+            var url = $("#live-search-results .selected a").attr("href");
+            if (url)
+                window.location = url;
+        }
+
+        // DOWN
+        else if (e.keyCode == 40) {
+            var next = $("#live-search-results .selected").next("#live-search-results div.item");
+            if (next.html()) {
+                next.addClass("selected");
+            }
+            else {
+                $("#live-search-results div.item:first").addClass("selected");
+            }
+            current.removeClass("selected");
+        }
+
+    })
+
+    $(".search").delay({
+        delay: 400,
+        event: "keyup",
+        fn: function(e) {
+            if (e.keyCode in {13:1, 27:1, 38:1, 39:1, 40:1} == false) {
+                var q = $(".search").attr("value");
+                if (q.length == 0) {
+                    $("#live-search-results").hide();
+                }
+
+                if (q.length > 0) {
+                    var url = $("#search-form").attr("data");
+                    $.get(url, {"q" : q}, function(data) {
+                        $("#live-search-results").html(data);
+                        $("#live-search-results").show();
+                    });
+                }
+            }
+        }
+    });
+
+});
