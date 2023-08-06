d4106f86167dc3aea8df4780dc46f09b318a6228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package suit_guardian/028.120.0/suit_guardian-028.120.0.tar.gz

---
+++ packages/suit_guardian/suit_guardian-028.120.0.tar.gz/suit_guardian-028.120.0/suit_guardian/__init__.py
@@ -0,0 +1,7 @@
+# suit_guardian versioning:
+# <suit version updated>.<guardian version updated>.<hotfix>
+__SUIT_VERSION__ = '0.2.8'
+__GUARDIAN_VERSION__ = '1.2.0'
+__HOTFIX__ = '0'
+
+__VERSION__ = '028.120.0'
