6db0c291efffd98861617c5f12f9d0dbc4655859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:48 +0000

    Add package grunt-django/0.1/grunt-django-0.1.tar.gz

---
+++ packages/grunt-django/grunt-django-0.1.tar.gz/grunt-django-0.1/README.md
@@ -0,0 +1,47 @@
+django-grunt
+============
+
+Managing Grunt with runserver and Grunt initialization for your project.
+Inspired to [Brandon Konkle's post](http://lincolnloop.com/blog/simplifying-your-django-frontend-tasks-grunt/)
+
+Installation
+----------
+From command line
+
+```
+pip install grunt-django
+```
+
+In your settings.py:
+
+```python
+INSTALLED_APPS = (
+    ...,
+    'grunt'
+)
+```
+optional:
+```python
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+GRUNTFILE_ROOT = os.path.join(BASE_DIR, 'MY_GRUNTFILE_DIR')
+```
+
+Usage 
+----
+django-grunt provide 2 management commands:
+
+1. 
+```
+manage.py gruntinit
+``` 
+which created a package.json and Gruntfile.js
+
+2.
+```
+manage.py gruntserver
+``` 
+Run this command instead of ```manage.py runserver``` for running django server along with grunt. This is helpful when you use livereload and watch.
+
+
+
---
+++ packages/grunt-django/grunt-django-0.1.tar.gz/grunt-django-0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='grunt-django',
+    version='0.1',
+    packages=['grunt'],
+    include_package_data=True,
+    license='Apache License',  # example license
+    description='Grunt initialization for your project',
+    long_description=README,
+    author='Luca Adalberto Vandro',
+    author_email='lucavandro@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
