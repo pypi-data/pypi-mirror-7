78cfef9517b41cd6f9d5700aa59b01d6ea5c46dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:39 +0000

    Add package xbob.db.verification.filelist/1.3.5/xbob.db.verification.filelist-1.3.5.zip

---
+++ packages/xbob.db.verification.filelist/xbob.db.verification.filelist-1.3.5.zip/xbob.db.verification.filelist-1.3.5/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <laurent.el-shafey@idiap.ch>
+# modified by Elie Khoury <elie.khoury@idiap.ch>
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+from setuptools import setup, find_packages
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='xbob.db.verification.filelist',
+    version='1.3.5',
+    description='Verification File List Database Access API for Bob',
+    url='https://pypi.python.org/pypi/xbob.db.verification.filelist',
+    license='GPLv3',
+    author='Laurent El Shafey',
+    author_email='laurent.el-shafey@idiap.ch',
+    keywords='bob, xbob, xbob.db, recognition, file lists',
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+      'setuptools',
+      'six',  # python2/3 compatibility package
+      'bob',  # base signal proc./machine learning library
+      'xbob.db.verification.utils' # defines a set of utilities for face verification databases like this one.
+    ],
+
+    namespace_packages = [
+      'xbob',
+      'xbob.db',
+      'xbob.db.verification'
+      ],
+
+    entry_points={
+
+      # declare database to bob
+      'bob.db': [
+        'verification.filelist = xbob.db.verification.filelist.driver:Interface',
+        ],
+
+      # declare tests to bob
+      'bob.test': [
+        'verification.filelist = xbob.db.verification.filelist.test:VerificationFilelistTest',
+        ],
+
+      },
+
+    classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Education',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+      ],
+)
