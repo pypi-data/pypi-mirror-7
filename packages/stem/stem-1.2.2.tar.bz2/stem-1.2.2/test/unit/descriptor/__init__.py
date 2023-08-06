7b2a28cd2e7c0c1d93f4a7ad5ec8285517208ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:17 +0000

    Add package stem/1.2.2/stem-1.2.2.tar.bz2

---
+++ packages/stem/stem-1.2.2.tar.bz2/stem-1.2.2/test/unit/descriptor/__init__.py
@@ -0,0 +1,13 @@
+"""
+Unit tests for stem.descriptor.
+"""
+
+__all__ = [
+  'export',
+  'extrainfo_descriptor',
+  'microdescriptor',
+  'networkstatus',
+  'reader',
+  'router_status_entry',
+  'server_descriptor',
+]
