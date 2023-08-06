ebe1f8271bd43a8f4159b939c6f9e0d0e088944a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:26 +0000

    Add package infi.tracing/0.2.6/infi.tracing-0.2.6-py2.7-macosx-10.9-x86_64.egg

---
+++ packages/infi.tracing/infi.tracing-0.2.6-py2.7-macosx-10.9-x86_64.egg/infi/tracing/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '17fc7beffdab03afe24407056d9f6dfac6e586b1'
+__git_head_subject__ = 'Finished release v0.2.6.'
+__git_head_message__ = '* release/v0.2.6:\n  HOSTDEV-1727 fixing timestamp calcualtion on POSIX systems\n  HOSTDEV-1725 adding timestamp to file tracing;'
+__git_dirty_diff__ = ''
