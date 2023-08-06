2588ad597ec9d0e3314adf7002b91ec370f02339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:20 +0000

    Add package pysed/0.2.0/pysed-0.2.0.tar.gz

---
+++ packages/pysed/pysed-0.2.0.tar.gz/pysed-0.2.0/pysed/__init__.py
@@ -0,0 +1,10 @@
+from .pysed import main
+from .pysed import replace
+from .pysed import append
+from .pysed import __version__
+from .pysed import __license__
+from .pysed import __email__
+
+from .args import *
+
+from .paint import colors
