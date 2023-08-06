f0ef7d80724af1271269bbc3e19f13b71e213703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:43 +0000

    Add package cheerio/1.2/cheerio-1.2.tar.gz

---
+++ packages/cheerio/cheerio-1.2.tar.gz/cheerio-1.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import setup
+
+with open('Readme.rst', 'r') as f:
+        long_description = f.read()
+
+setup(
+    zip_safe=False,
+    name='cheerio',
+    version=1.2,
+    author='Lucy Wyman',
+    author_email='wyman.lucy@gmail.com',
+    description='Pseudo-randomly generated goodbyes',
+    long_description=long_description,
+    packages=['cheerio'],
+    package_data={'cheerio': ['*.rst']},
+    include_package_data=True,
+    keywords=['goodbye', 'cheerio'],
+    url='https://github.com/lucywyman/cheerio',
+)
