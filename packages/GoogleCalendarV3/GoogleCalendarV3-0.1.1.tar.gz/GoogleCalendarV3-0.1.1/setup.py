f2e65265cd435bb94878db15944b63423926f380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package GoogleCalendarV3/0.1.1/GoogleCalendarV3-0.1.1.tar.gz

---
+++ packages/GoogleCalendarV3/GoogleCalendarV3-0.1.1.tar.gz/GoogleCalendarV3-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='GoogleCalendarV3',
+    version='0.1.1',
+    author='Ashutosh Priyadarshy',
+    author_email='static@siftcal.com',
+    packages=['google_calendar_v3', 'google_calendar_v3.test'],
+    scripts=['bin/example.py'],
+    url='http://www.github.com/priyadarshy/google-calendar-v3/',
+    license='LICENSE.txt',
+    description='Python Client for Google Calendar API V3.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests-oauthlib >= 0.4.0",
+    ],
+)
