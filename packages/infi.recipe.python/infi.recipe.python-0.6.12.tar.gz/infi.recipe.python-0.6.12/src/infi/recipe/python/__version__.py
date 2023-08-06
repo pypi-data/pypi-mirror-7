ceacd89e471af2baea8446228ad7caae9132bf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:26 +0000

    Add package infi.recipe.python/0.6.12/infi.recipe.python-0.6.12.tar.gz

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.12.tar.gz/infi.recipe.python-0.6.12/src/infi/recipe/python/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.12"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.6.12)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2159c4ed845c2419e57f350ee8c40d0d2c922d82'
+__git_head_subject__ = 'Revert "HOSTDEV-1717 Replace platform.dist with linux_distribution"'
+__git_head_message__ = 'This reverts commit f713d5faedd28702759f71675f22f90ecd090905, because it breaks CentOS:\nDownloading ftp://python.infinidat.com/python/python-v2.7.6.13-linux-centoslinux-7-x64.tar.gz\n\nstderr=While:\n  Installing isolated-python.\nError: Error downloading extends for URL ftp://python.infinidat.com/python/python-v2.7.6.13-linux-centoslinux-7-x64.tar.gz: [Errno ftp error] [Errno ftp error] 550 Failed to change directory.'
+__git_dirty_diff__ = ''
