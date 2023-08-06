b4a3dc40d25bec2dde0a10ff37361d16a1430857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package rainbowstream/0.3.6/rainbowstream-0.3.6.tar.gz

---
+++ packages/rainbowstream/rainbowstream-0.3.6.tar.gz/rainbowstream-0.3.6/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import os, sys
+
+# Bumped version
+version = '0.3.6'
+
+# Require
+install_requires = [
+    "python-dateutil",
+    "requests",
+    "pyfiglet",
+    "SQLAlchemy",
+    "twitter",
+    "Pillow",
+]
+# Python 3 doesn't hava pysqlite
+if sys.version[0] == "2":
+    install_requires += ["pysqlite"]
+
+# Copy default config if not exists
+default = os.environ.get(
+    'HOME',
+    os.environ.get(
+        'USERPROFILE',
+        '')) + os.sep + '.rainbow_config.json'
+if not os.path.isfile(default):
+    cmd = 'cp rainbowstream/colorset/config ' + default
+    os.system(cmd)
+
+# Setup
+setup(name='rainbowstream',
+      version=version,
+      description="A smart and nice Twitter client on terminal.",
+      long_description=open("./README.rst", "r").read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords='twitter, command-line tools, stream API',
+      author='Vu Nhat Minh',
+      author_email='nhatminh179@gmail.com',
+      url='http://www.rainbowstream.org/',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=install_requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      rainbowstream=rainbowstream.rainbow:fly
+      """,
+      )
