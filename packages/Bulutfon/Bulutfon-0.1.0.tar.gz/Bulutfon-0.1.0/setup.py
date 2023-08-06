45111a691cfe7ba20aaefcf12e93b614dab68496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:47 +0000

    Add package Bulutfon/0.1.0/Bulutfon-0.1.0.tar.gz

---
+++ packages/Bulutfon/Bulutfon-0.1.0.tar.gz/Bulutfon-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Bulutfon',
+    version='0.1.0',
+    description='Bulutfon Offical Python SDK',
+    url='https://github.com/bulutfon/python-sdk',
+    author='Bulutfon, Onur Ozgur OZKAN',
+    author_email='dev@bulutfon.com, onur.ozgur.ozkan@bulutfon.com',
+    license='MIT',
+    classifiers=[
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
