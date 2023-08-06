b21b38660e92e0ac8538b5abafea5301c1116c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:29 +0000

    Add package bw2calc/0.15/bw2calc-0.15.tar.gz

---
+++ packages/bw2calc/bw2calc-0.15.tar.gz/bw2calc-0.15/bw2calc/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*
+from .lca import LCA
+from .monte_carlo import MonteCarloLCA, ParallelMonteCarlo, MultiMonteCarlo
+from .mc_vector import ParameterVectorLCA
+from .graph_traversal import GraphTraversal
+from .matrices import MatrixBuilder, TechnosphereBiosphereMatrixBuilder
+
+__version__ = (0, 15)
