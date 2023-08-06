dc60d52e46034e0b03964ee86a342808cb5e6756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:51 +0000

    Add package pilite/1.0.0/pilite-1.0.0.tar.gz

---
+++ packages/pilite/pilite-1.0.0.tar.gz/pilite-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(  name='pilite',
+        description='Interface to Ciseco PiLite',
+        author='Stephen Blythe',
+        author_email='stephen@urwick.co.uk',
+        url='https://github.com/shblythe/python2-pilite',
+        version='1.0.0',
+        py_modules=['pilite'],
+        license='GPL v3',
+        )
