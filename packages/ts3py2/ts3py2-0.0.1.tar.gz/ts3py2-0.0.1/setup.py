df612ae7b1b11d3f4b214b0912fbef8013a5f61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:31 +0000

    Add package ts3py2/0.0.1/ts3py2-0.0.1.tar.gz

---
+++ packages/ts3py2/ts3py2-0.0.1.tar.gz/ts3py2-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ts3py2',
+    version='0.0.1',
+    author='zod',
+    author_email='urmom@gtfo.com',
+    packages=['ts3py2'],
+    url='http://pypi.python.org/pypi/ts3py2/',
+    license='LICENSE.txt',
+    description='idk.',
+    long_description=open('README.txt').read(),
+)
