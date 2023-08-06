fc3c6800f0af1f7f572d3ca13b4e61c39476c578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:14 +0000

    Add package pythonpy/0.1.6/pythonpy-0.1.6.tar.gz

---
+++ packages/pythonpy/pythonpy-0.1.6.tar.gz/pythonpy-0.1.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import os
+
+setup(
+    name='pythonpy',
+    version='0.1.6',
+    description='Command line utility for Python',
+    scripts=[os.path.join('bin', 'pythonpy')],
+    license='MIT',
+    long_description=open('README.rst').read(),
+)
