49af89c1c61d556727653cfb6c84e7f99d8e61e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package python-matrix/0.0/python-matrix-0.0.tar.gz

---
+++ packages/python-matrix/python-matrix-0.0.tar.gz/python-matrix-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-matrix',
+    version="0.0",
+    url='https://pypi.python.org/pypi/matrix/',
+    description='Generic matrix generator.',
+    long_description='''Use `matrix <https://pypi.python.org/pypi/matrix/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['matrix'],
+)
