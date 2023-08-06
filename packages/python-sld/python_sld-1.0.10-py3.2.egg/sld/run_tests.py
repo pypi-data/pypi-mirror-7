1c7f84456eb90eaed730c892bd45e4b0ef1ea998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:55 +0000

    Add package python-sld/1.0.9/python_sld-1.0.10-py3.2.egg

---
+++ packages/python-sld/python_sld-1.0.10-py3.2.egg/sld/run_tests.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Testing fixture for StyledLayerDescriptor library.
+
+License
+=======
+Copyright 2011-2014 David Zwarg <U{david.a@zwarg.com}>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+U{http://www.apache.org/licenses/LICENSE-2.0}
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: David Zwarg
+@contact: david.a@zwarg.com
+@copyright: 2011-2014, Azavea
+@license: Apache 2.0
+@version: 1.0.10
+"""
+import unittest
+import sys
+import logging
+from optparse import OptionParser
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option('-v', '--verbose', dest='verbosity',
+                      help='Logging verbosity.', action='store_true', default=False)
+
+    (options, args) = parser.parse_args()
+
+    loglevel = logging.WARNING
+    if options.verbosity:
+        loglevel = logging.DEBUG
+
+    logging.basicConfig(format='%(message)s', level=loglevel)
+
+    sys.path.insert(0, '..')
+
+    import sld.test
+    unittest.main(sld.test)
