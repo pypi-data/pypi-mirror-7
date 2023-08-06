621c0570a3b3e1fd194cd42c587816f87e52a5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:21 +0000

    Add package hog/0.1.4/hog-0.1.4.tar.gz

---
+++ packages/hog/hog-0.1.4.tar.gz/hog-0.1.4/hog/__init__.py
@@ -0,0 +1,10 @@
+# hog - Sending multiple HTTP requests ON GREEN thread.
+
+version_info = (0, 1, 4)
+
+__version__ = VERSION = '.'.join(map(str, version_info))
+__project__ = PROJECT = 'hog'
+__author__ = AUTHOR = "Park Hyunwoo <ez.amiryo@gmail.com>"
+
+
+from .hog import run
