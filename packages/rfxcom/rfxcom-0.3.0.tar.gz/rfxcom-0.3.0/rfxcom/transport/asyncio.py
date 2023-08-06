eee59234d3f4f9a1e653a7b578d7c7cdabbe4f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:31 +0000

    Add package rfxcom/0.3.0/rfxcom-0.3.0.tar.gz

---
+++ packages/rfxcom/rfxcom-0.3.0.tar.gz/rfxcom-0.3.0/rfxcom/transport/asyncio.py
@@ -0,0 +1,129 @@
+"""
+rfxcom.transport.asyncio
+========================
+
+"""
+
+import asyncio
+
+from rfxcom.transport.base import BaseTransport
+from rfxcom.protocol import RESET_PACKET, STATUS_PACKET, MODE_PACKET
+
+
+class AsyncioTransport(BaseTransport):
+    def __init__(self, device, loop, callback=None, callbacks=None,
+                 SerialClass=None):
+
+        super().__init__(device, callback=callback, callbacks=callbacks,
+                         SerialClass=SerialClass)
+
+        self.loop = loop
+        self.write_queue = []
+
+        self.log.info("Attaching writer for setup.")
+        loop.add_writer(self.dev.fd, self.setup)
+
+    def setup(self):
+        """Performs the RFXtrx initialisation protocol in a Future.
+
+        Currently this is the rough workflow of the interactions with the
+        RFXtrx. We also do a few extra things - flush the buffer, and attach
+        readers/writers to the asyncio loop.
+
+        1. Write a RESET packet (write all zeros)
+        2. Wait at least 50ms and less than 9000ms
+        3. Write the STATUS packet to verify the device is up.
+        4. Receive status response
+        5. Write the MODE packet to enable or disabled the required protocols.
+        """
+        self.log.info("Removing setup writer.")
+        self.loop.remove_writer(self.dev.fd)
+
+        asyncio.async(self._setup())
+
+    def _setup(self):
+        self.log.info("Adding reader to prepare to receive.")
+        self.loop.add_reader(self.dev.fd, self.read)
+
+        self.log.info("Flushing the RFXtrx buffer.")
+        self.flushSerialInput()
+
+        self.log.info("Writing the reset packet to the RFXtrx. (blocking)")
+        yield from self.sendRESET()
+
+        self.log.info("Wating 0.4s")
+        yield from asyncio.sleep(0.4)
+
+        self.log.info("Write the status packet (blocking)")
+        yield from self.sendSTATUS()
+
+        # TODO receive status response, compare it with the needed MODE and
+        # request a new MODE if required. Currently MODE is always sent.
+
+        self.log.info("Adding mode packet to the write queue (blocking)")
+        yield from self.sendMODE()
+
+        self.log.info("Adding the queued writer next loop iteration.")
+        self.loop.call_soon(self.loop.add_writer, self.dev.fd, self._writer)
+
+    @asyncio.coroutine
+    def flushSerialInput(self):
+        self.dev.flushInput()
+
+    @asyncio.coroutine
+    def sendRESET(self):
+        super().write(RESET_PACKET)
+
+    @asyncio.coroutine
+    def sendMODE(self):
+        super().write(MODE_PACKET)
+
+    @asyncio.coroutine
+    def sendSTATUS(self):
+        super().write(STATUS_PACKET)
+
+    def _writer(self):
+        """We have been called to write! Take the oldest item off the queue
+        and use the write method on BaseTransport.
+        """
+        l = len(self.write_queue)
+
+        if l > 0:
+            self.log.debug("Serving from write queue (length %s)" % l)
+            super().write(self.write_queue.pop(0))
+
+    def write(self, data):
+        """Add a data packet to the write queue. In this case, its a simple
+        list. which is then consumed. This method is as light as possible.
+        """
+        self.write_queue.append(data)
+
+    def do_callback(self, pkt):
+        """Add the callback to the event loop, we use call soon because we just
+        want it to be called at some point, but don't care when particularly.
+        """
+        callback, parser = self.get_callback_parser(pkt)
+        self.loop.call_soon(callback, parser)
+
+    def read(self):
+        """We have been called to read! As a consumer, continue to read for
+        the length of the packet and then pass to the callback.
+        """
+
+        data = self.dev.read()
+
+        if len(data) == 0:
+            self.log.warning("READ : Nothing received")
+            return
+
+        if data == b'\x00':
+            self.log.warning("READ : Empty packet (Got \\x00)")
+            return
+
+        pkt = bytearray(data)
+        data = self.dev.read(pkt[0])
+        pkt.extend(bytearray(data))
+
+        self.log.info("READ : %s" % self.format_packet(pkt))
+        self.do_callback(pkt)
+        return pkt
---
+++ packages/rfxcom/rfxcom-0.3.0.tar.gz/rfxcom-0.3.0/tests/transport/test_asyncio.py
@@ -0,0 +1,161 @@
+"""Unit tests for rfxcom.asyncio.AsyncioTransport."""
+from unittest import TestCase, mock
+
+from rfxcom.transport import AsyncioTransport
+
+# It's a unittest, let's be flexible
+# pylint: disable=C0111,W0212,R0201
+
+
+def execute_coroutine(coroutine):
+    """Execute a coroutine.
+
+    The coroutine is a generator which 'yields from' other iterables in the
+    form of asyncio.Future instances. For each generated value, wait for it to
+    be done and move on to the next one, until the generator has completed.
+
+    Since there is no loop instance to enter, it might be needed to mock each
+    yielded value before executing the coroutine, to avoid blocking situations.
+
+    Exemple:
+        with mock.patch('coro.yielded.metod.Function'):
+            result = execute_coroutine(coro)
+
+    Althogh this helper is thought to work with coroutined generators, it
+    should work with any iterator.
+
+    @param coroutine: an iterable, e.g. an asyncio.coroutine decorated method
+      yielding something.
+
+    @returns the return value of the coroutine, or None if nothing is returned
+    """
+    try:
+        future = next(coroutine)
+        while not future.done():
+            pass
+    except StopIteration as exc:
+        if hasattr(exc, 'value'):
+            return exc.value
+
+
+class AsyncioTransportTestCase(TestCase):
+
+    """AsyncioTransport test case."""
+
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_constructor(self, device, loop):
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+        loop.add_writer.assert_called_once_with(device.fd, unit.setup)
+
+    @mock.patch('rfxcom.transport.asyncio.AsyncioTransport._setup')
+    @mock.patch('asyncio.async')
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_setup(self, device, loop, async, _setup):
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+        # reset mocks which have been 'called' by the constructor
+        device.reset_mock()
+        loop.reset_mock()
+        unit.setup()
+
+        loop.remove_writer.assert_called_once_with(device.fd)
+        async.assert_called_once_with(_setup())
+
+    @mock.patch('rfxcom.transport.asyncio.AsyncioTransport.sendRESET')
+    @mock.patch('rfxcom.transport.asyncio.AsyncioTransport.sendSTATUS')
+    @mock.patch('rfxcom.transport.asyncio.AsyncioTransport.sendMODE')
+    @mock.patch('asyncio.sleep')
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport__setup(self, device, loop, sleep, mode, status, reset):
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+        # reset mocks which have been 'called' by the constructor
+        device.reset_mock()
+        loop.reset_mock()
+
+        # run: exaust the coroutine generator
+        execute_coroutine(unit._setup())
+
+        loop.add_reader.assert_called_with(device.fd, unit.read)
+        reset.assert_called_once_with()
+        sleep.assert_has_calls((mock.ANY, mock.ANY))
+        slept_time = sleep.call_args[0][0]
+        # by spec it needs to be between 0.5ms and 9000ms
+        self.assertGreater(slept_time, 0.05)
+        self.assertLess(slept_time, 9)
+        mode.assert_called_once_with()
+        status.assert_called_once_with()
+        loop.call_soon.assert_called_once_with(loop.add_writer, device.fd,
+                                               unit._writer)
+
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_read_nothing(self, device, loop):
+
+        device.read.return_value = b''
+
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+
+        with mock.patch.object(unit, 'log') as unit_log:
+            unit.read()
+            unit_log.warning.assert_called_once_with(
+                "READ : Nothing received")
+
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_read_empty_patcket(self, device, loop):
+
+        device.read.return_value = b'\x00'
+
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+
+        with mock.patch.object(unit, 'log') as unit_log:
+            unit.read()
+            unit_log.warning.assert_called_once_with(
+                "READ : Empty packet (Got \\x00)")
+
+    @mock.patch('rfxcom.transport.asyncio.AsyncioTransport.do_callback')
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_read(self, device, loop, callback):
+
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+
+        def fake_read(*x):
+            data = {
+                (): b'\x02',
+                (2, ): b'\x01\x01'
+            }
+            return data[x]
+        device.read.side_effect = fake_read
+
+        expected_result = b'\x02\x01\x01'
+        self.assertEquals(unit.read(), expected_result)
+        callback.assert_called_once_with(expected_result)
+
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_write_from_queue(self, device, loop):
+
+        payload = b'\x01\x01'
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+
+        # Call write and verify it was added to the queue
+        unit.write(payload)
+        self.assertIn(payload, unit.write_queue)
+
+        unit._writer()
+
+        device.write.assert_called_once_with(payload)
+
+    @mock.patch('asyncio.AbstractEventLoop')
+    @mock.patch('serial.Serial')
+    def test_transport_doesnt_write_with_emtpy_queue(self, device, loop):
+        unit = AsyncioTransport(device, loop, callback=mock.Mock())
+
+        # Call write and verify it was added to the queue
+        unit.write_queue = []
+        unit._writer()
+
+        self.assertFalse(device.write.called)
