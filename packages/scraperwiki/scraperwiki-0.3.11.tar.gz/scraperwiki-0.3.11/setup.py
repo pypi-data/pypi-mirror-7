9dd6351f497f502d2cda89a3fed983a5d49dfda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package scraperwiki/0.3.11/scraperwiki-0.3.11.tar.gz

---
+++ packages/scraperwiki/scraperwiki-0.3.11.tar.gz/scraperwiki-0.3.11/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+
+# This file is part of Local Scraperlibs.
+
+import warnings
+import os
+from distutils.core import setup
+
+def has_external_dependency(name):
+    'Check that a non-Python dependency is installed.'
+    for directory in os.environ['PATH'].split(':'):
+        if os.path.exists(os.path.join(directory, name)):
+            return True
+    return False
+
+if not has_external_dependency('pdftohtml'):
+    warnings.warn(
+        'scraperwiki.pdftoxml requires pdftohtml, but pdftohtml was not found\n'
+        'in the PATH. If you wish to use this function, you probably need to\n'
+        'install pdftohtml.'
+    )
+
+config = dict(name='scraperwiki',
+    author='Francis Irving',
+    author_email='francis@scraperwiki.com',
+    description='Local version of ScraperWiki libraries',
+    url='https://github.com/scraperwiki/scraperwiki-python',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: SQL',
+        'Topic :: Database :: Front-Ends',
+    ],
+    packages=['scraperwiki', 'scraperwiki.runlog'],
+
+    version = '0.3.11',
+    license='GPL',
+   )
+
+try:
+    from setuptools import setup
+    config['install_requires'] = ['dumptruck>=0.1.6', 'requests'],
+except ImportError:
+    pass
+
+setup(**config)
