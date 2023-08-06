da099720a72237c46db60bbdd3ed959aa3d8b4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package tsutil/0.1.3/tsutil-0.1.3.zip

---
+++ packages/tsutil/tsutil-0.1.3.zip/tsutil-0.1.3/setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+
+
+setup(name='tsutil', version='0.1.3', py_modules=['tsutil.enum', 'tsutil.encoder', 'tsutil.util'],
+      author='Turbidsoul Chen', author_email='sccn.sq+py@gmail.com',
+      url='http://blog.turbidsoul.me', description='my python tool module', listense='MIT')
