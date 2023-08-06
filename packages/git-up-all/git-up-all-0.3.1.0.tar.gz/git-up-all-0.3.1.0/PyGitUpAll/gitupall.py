69574484dbfc81788991a5fcb8aed9f9ab54c83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package git-up-all/0.3.1.0/git-up-all-0.3.1.0.tar.gz

---
+++ packages/git-up-all/git-up-all-0.3.1.0.tar.gz/git-up-all-0.3.1.0/PyGitUpAll/gitupall.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import os
+import sys
+import docopt
+
+from git import Repo, exc
+from git.repo.fun import is_git_dir
+from PyGitUp.git_wrapper import GitError
+from PyGitUp.gitup import GitUp
+from PyGitUpAll.utils import read_projects_from_json, read_projects_from_sourcetree
+from PyGitUpAll.project import Project
+from termcolor import colored
+
+
+PROJECTS_FILE = "projects.json"
+
+
+class GitUpAll(object):
+
+    def __init__(self):
+        self.start_dir = os.getcwd()
+
+    def git_up_all(self, sourcetree=False):
+
+        if sourcetree:
+            print(colored("Loading projects from ", attrs=["bold"]) + colored("SourceTree" + "\n", color="green"))
+            projects = read_projects_from_sourcetree()
+        else:
+            print(colored("Loading projects from ", attrs=["bold"]) + colored(PROJECTS_FILE + "\n", color="green"))
+            projects = read_projects_from_json(PROJECTS_FILE)
+
+        if not projects:
+            print(colored("Could not read projects\n", color="red"))
+            return False
+
+        results = {}
+        for project_name, project_settings in projects.iteritems():
+
+            """
+            This needs to be rewritten and tested!
+            """
+            # Check if we are able to initialize settings to an object
+            project = Project(project_settings)
+            if not project:
+                print (colored("Could not read project " + project_name, color="red"))
+                continue
+            print(colored("- Working on: " + project.name + " @" + project.absolute_path, attrs=["underline"]))
+
+            # Check if a dir exists or clone
+            if (not os.path.isdir(project.absolute_path) or os.listdir(
+                    project.absolute_path) == []) and project.git_url:
+                print ("Project is not directory or not initialized repo. Cloning from url")
+                project.repo = Repo.clone_from(url=project.git_url, to_path=project.absolute_path)
+                print (colored("Repository cloned", color="green"))
+
+            # Check if the is not git and if not skip
+            if not is_git_dir(os.path.join(project.absolute_path, ".git")) and not project.git_url:
+                print (colored("Project path is not a git dir and has no info. Skipping", color="red"))
+                continue
+
+            # @todo probably should be moved
+            if self.sync_repository(project):
+                results.update({project.name: True})
+            else:
+                results.update({project.name: False})
+            print(colored("- Finished on: " + project.name + " @" + project.absolute_path, attrs=["underline"]) + "\n")
+
+        self.print_results(results)
+
+
+    def sync_repository(self, project):
+
+        result = False
+        # ugly chdir. Will try to patch pygitup
+        os.chdir(project.absolute_path)
+        try:
+            # Creates a new object all the time
+            GitUp().run()
+            result = True
+        except (GitError, exc.GitCommandError, BaseException) as e:
+            print (colored("Could not update repository: " + project.name, color="red"))
+        finally:
+            os.chdir(self.start_dir)
+            return result
+
+
+    def print_results(self, results):
+        total = len(results)
+        successes = 0
+        fails = 0
+        for name, success in results.iteritems():
+            if success:
+                print (name + colored(" Updated", color="green"))
+                successes += 1
+            else:
+                print (name + colored(" Failed", color="red"))
+                fails += 1
+
+        print(colored("Total: " + str(total) + " projects", attrs=['bold'], color="green"))
+        print(colored("Successful: " + str(successes) + " projects", attrs=['bold'], color="green"))
+        print(colored("Fails: " + str(fails) + " projects", attrs=['bold'], color="red"))
+        print(colored("Ratio: " + str(float(successes) / float(total) * 100.0)[:4] + "% success", attrs=['underline'],
+                      color="green"))
+
+
+def run():
+    if '--sourcetree' in sys.argv:
+        GitUpAll().git_up_all(sourcetree=True)
+    else:
+        GitUpAll().git_up_all(sourcetree=False)
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/git-up-all/git-up-all-0.3.1.0.tar.gz/git-up-all-0.3.1.0/README.rst
@@ -0,0 +1,87 @@
+PyGitUpAll |Build Status|
+=========================
+
+Git-Up all your projects!
+
+Why use ``git-up-all`` ?
+------------------------
+
+ - Tired of going into each repo and ``pulling/rebasing/stashing`` in order to update
+ - Tired of updating all branches over all the repos you have following the above tactics?
+ - Need one custom action for sourcetree for eg? Just add ``git-up-all --sourcetree`` and you are good to go
+
+How it works
+------------
+
+1. Reads a json file (or SourceTree list OSX) containing the list of repos you want to keep updated (could also be sourced controlled).
+
+2. Iterates over each folder containing the repos provided and executes git-up as a module
+
+3. Prints results for each repo
+
+- Note that it will skip any fails and leaves it up to the user to manually resolve the conflicts.
+
+Setup
+-----
+
+ - Install via ``pip install git-up-all``
+ - Create a ``projects.json`` containing a list of projects (see example below) or use ``--sourcetree``
+ - Run ``git-up-all`` on that folder containing the projects.json or use ``--sourcetree``
+
+
+projects.json
+-------------
+
+This file should contain a list with projects/repos as below:
+
+.. code-block:: javascript
+
+    {
+        "gitup": {
+            "name": "PyGitUp",
+            "git_url": "https://github.com/msiemens/PyGitUp.git",
+            "absolute_path": "/Users/jimmykane/projects/pygitup"
+        },
+        "gitupall": {
+            "name": "PyGitUpAll",
+            "git_url": "https://github.com/jimmykane/PyGitUpAll.git",
+            "absolute_path": "/Users/jimmykane/projects/pygitupall"
+        }
+    }
+
+
+
+
+Changelog
+---------
+
+v0.3.0 (*2014-08-01*)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+- (BETA) Support for reading from SourceTree on OSX systems for now. Run with ``--sourcetree``
+- Logic fixes
+
+
+v0.2.0.1 (*2014-07-29*)
+~~~~~~~~~~~~~~~~~~~~~~~
+- This is a pre-release. I will start most of the work in this section after tests are done
+
+
+@TODO
+-----
+
+- Check repo status (rebase etc)
+- Check for branch ``origin`` validity
+- Patch ``PyGitUp`` to use dir paths as arguments let it do the chdir kind or the work is chdir is not needed
+- Add the tests finally
+- Rethink about structure and integration with ``PyGitUp``
+
+Acknowledgements
+----------------
+
+Thanks to the original port of GitUp in python (PyGitUp) by msiemens
+
+ - git: https://github.com/msiemens/PyGitUp.git
+
+.. |Build Status| image:: https://travis-ci.org/jimmykane/PyGitUpAll.svg?branch=master
+   :target: https://travis-ci.org/jimmykane/PyGitUpAll
