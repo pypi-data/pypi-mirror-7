44b9e5d2a4805de1f52bbe5b260e6c9b044b08ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:13 +0000

    Add package app_version/0.2.0/app_version-0.2.0.tar.gz

---
+++ packages/app_version/app_version-0.2.0.tar.gz/app_version-0.2.0/setup.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+import sys
+from setuptools import setup, find_packages
+
+NAME = 'app_version'
+VERSION = '0.2.0'
+
+
+def read(filename):
+    import os
+    BASE_DIR = os.path.dirname(__file__)
+    filename = os.path.join(BASE_DIR, filename)
+    fi = open(filename, 'r')
+    return fi.read()
+
+
+def readlist(filename):
+    rows = read(filename).split("\n")
+    rows = [x.strip() for x in rows if x.strip()]
+    return list(rows)
+
+setup_extras = {}
+if sys.version_info > (3,):
+    setup_extras['use_2to3'] = True
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='A tiny utility to get application version from pkg_resouces',
+    long_description=read('README.rst'),
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ),
+    keywords = 'application, version',
+    author = 'Alisue',
+    author_email = 'lambdalisue@hashnote.net',
+    url = 'https://github.com/lambdalisue/%s' % NAME,
+    download_url = 'https://github.com/lambdalisue/%s/tarball/master' % NAME,
+    license = 'MIT',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    package_data = {
+        '': ['LICENSE',
+             'README.rst',
+             'TEST.rst',
+             'requirements.txt',
+             'requirements-test.txt',
+             'requirements-docs.txt'],
+    },
+    zip_safe=True,
+    install_requires=readlist('requirements.txt'),
+    **setup_extras
+)
---
+++ packages/app_version/app_version-0.2.0.tar.gz/app_version-0.2.0/src/app_version/__init__.py
@@ -0,0 +1,5 @@
+from app_version.core import get_versions
+from app_version.core import get_tuple_version
+from app_version.core import get_string_version
+__version__, VERSION = get_versions('app_version')
+
