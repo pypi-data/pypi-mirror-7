8b7868813a2f15539a238b13b9e551d39a491979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:25 +0000

    Add package python-erlang/0.1.1/python-erlang-0.1.1.tar.gz

---
+++ packages/python-erlang/python-erlang-0.1.1.tar.gz/python-erlang-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-erlang',
+    version='0.1.1',
+    description='Telecom calculation library for python',
+    url='http://github.com/natemara/python-erlang',
+    author='Nate Mara',
+    author_email='natemara@gmail.com',
+    license='MIT',
+    packages=['erlang'],
+    zip_safe=False
+)
