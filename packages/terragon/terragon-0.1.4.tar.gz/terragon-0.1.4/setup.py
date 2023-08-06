0f5f6bd5579a231fc55eec3d207686f0becd8af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:46 +0000

    Add package terragon/0.1.4/terragon-0.1.4.tar.gz

---
+++ packages/terragon/terragon-0.1.4.tar.gz/terragon-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+required = []
+
+setup(
+    name="terragon",
+    version="0.1.4",
+    author="Greg Lamp",
+    author_email="greg@yhathq.com",
+    url="https://github.com/yhat/terragon/",
+    license="BSD",
+    packages=find_packages(),
+    description="a better pickle",
+    long_description="why is it so hard to reference a file in your setup.py?",
+    install_requires=required,
+)
+
