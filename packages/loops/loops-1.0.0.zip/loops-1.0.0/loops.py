4863ed1824b7725d2b8e4845bcf175f951a0bb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:49 +0000

    Add package loops/1.0.0/loops-1.0.0.zip

---
+++ packages/loops/loops-1.0.0.zip/loops-1.0.0/loops.py
@@ -0,0 +1,5 @@
+def print_lol(the_list):
+	for each_item in the_list:
+	 if isinstance(each_item, list): 
+	 else:
+	print(each_item)
---
+++ packages/loops/loops-1.0.0.zip/loops-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+name         ='loops',
+version      ='1.0.0',
+py_modules   =['loops'],
+author       ='akshay',
+author_email ='akshaysoni36@yahoo.com',
+url          ='http://www.google.com',
+description  ='a simple printed nested loop',
+
+)
