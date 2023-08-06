d34caba4220684459ea00874e60678caa90d21f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:02 +0000

    Add package Trolly/0.1.2/Trolly-0.1.2.zip

---
+++ packages/Trolly/Trolly-0.1.2.zip/Trolly-0.1.2/setup.py
@@ -0,0 +1,20 @@
+'''
+Created on 15 Feb 2013
+
+@author: plish
+'''
+
+from distutils.core import setup
+
+setup( 
+        name = 'Trolly',
+        version = '0.1.2',
+        author = 'plish',
+        author_email = 'plish.development@gmail.com',
+        url = 'https://github.com/plish/Trolly',
+        packages = ['trolly'],
+        license = 'LICENCE.txt',
+        install_requires = ['httplib2'],
+        description = 'Trello API Wrapper',
+        long_description = 'For more detail please see the github page'
+    )
