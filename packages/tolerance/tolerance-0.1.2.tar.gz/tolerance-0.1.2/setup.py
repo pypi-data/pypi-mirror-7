2cd9a265dc3b33333e0ce14b20766b4f1e729df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:13 +0000

    Add package tolerance/0.1.2/tolerance-0.1.2.tar.gz

---
+++ packages/tolerance/tolerance-0.1.2.tar.gz/tolerance-0.1.2/setup.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+import sys
+from setuptools import setup, find_packages
+
+NAME = 'tolerance'
+VERSION = '0.1.2'
+
+
+def read(filename):
+    import os
+    BASE_DIR = os.path.dirname(__file__)
+    filename = os.path.join(BASE_DIR, filename)
+    fi = open(filename, 'r')
+    return fi.read()
+
+
+def readlist(filename):
+    rows = read(filename).split("\n")
+    rows = [x.strip() for x in rows if x.strip()]
+    return list(rows)
+
+setup_extras = {}
+if sys.version_info > (3,):
+    setup_extras['use_2to3'] = True
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=('A function decorator which makes a function tolerant ('
+                 'the function fail silently).'),
+    long_description = read('README.rst'),
+    classifiers = (
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ),
+    keywords = 'function decorator, decorator, fail silently',
+    author = 'Alisue',
+    author_email = 'lambdalisue@hashnote.net',
+    url = 'https://github.com/lambdalisue/%s' % NAME,
+    download_url = 'https://github.com/lambdalisue/%s/tarball/master' % NAME,
+    license = 'MIT',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    package_data = {
+        '': ['LICENSE',
+             'README.rst',
+             'TEST.rst',
+             'requirements.txt',
+             'requirements-test.txt',
+             'requirements-docs.txt'],
+    },
+    zip_safe=True,
+    install_requires=readlist('requirements.txt'),
+    **setup_extras
+)
