38a696ea6fa5618ade68543b34d3fb58e328d91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:54 +0000

    Add package vzlog/0.1.1/vzlog-0.1.1.tar.gz

---
+++ packages/vzlog/vzlog-0.1.1.tar.gz/vzlog-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+CLASSIFIERS = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+"""
+
+setup(name='vzlog',
+    version='0.1.1',
+    url="https://github.com/gustavla/vzlog",
+    description="Python tool for logging rich content, "
+                "particularly plots and images",
+    author='Gustav Larsson',
+    author_email='gustav.m.larsson@gmail.com',
+    packages=[
+        'vzlog',
+    ],
+    license = 'BSD',
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+)
