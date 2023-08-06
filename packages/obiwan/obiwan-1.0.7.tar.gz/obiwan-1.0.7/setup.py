1c5f72c689f07ce5f71589c2ed5d3ce9a3b392e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:40 +0000

    Add package obiwan/1.0.7/obiwan-1.0.7.tar.gz

---
+++ packages/obiwan/obiwan-1.0.7.tar.gz/obiwan-1.0.7/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(name="obiwan",
+    version="1.0.7",
+    description="A runtime type checker (contract system) and JSON validator",
+    author="William Edwards",
+    author_email="willvarfar@gmail.com",
+    url="https://github.com/williame/obiwan",
+    long_description=os.popen("pandoc -t rst README.md").read(),
+    packages=["obiwan"])
+
