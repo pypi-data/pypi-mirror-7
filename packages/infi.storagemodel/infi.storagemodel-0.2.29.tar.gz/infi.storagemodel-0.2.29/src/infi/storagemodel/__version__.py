a9e5215d577cd8b0dcce97013761857c58805602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:04 +0000

    Add package infi.storagemodel/0.2.29/infi.storagemodel-0.2.29.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.29.tar.gz/infi.storagemodel-0.2.29/src/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.29"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.29)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f63627875fbbc8ba6bbd276782f20e687b631e6f'
+__git_head_subject__ = 'Finished release v0.2.29.'
+__git_head_message__ = '* release/v0.2.29:\n  Freezing dependencies\n  STORAGEMODEL-290 rescan_scsi_host: calling is_there_a_bug_in_sysfs_async_scanning regardless of how many channels already exist'
+__git_dirty_diff__ = ''
