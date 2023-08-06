f883c46f9b2799659ab4d127bf06380b110c12a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:50 +0000

    Add package django-rest-framework-mongoengine/1.3.1/django-rest-framework-mongoengine-1.3.1.tar.gz

---
+++ packages/django-rest-framework-mongoengine/django-rest-framework-mongoengine-1.3.1.tar.gz/django-rest-framework-mongoengine-1.3.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-rest-framework-mongoengine',
+    version='1.3.1',
+    packages=['rest_framework_mongoengine',],
+    license='see https://github.com/umutbozkurt/django-rest-framework-mongoengine/blob/master/LICENSE',
+    long_description='see https://github.com/umutbozkurt/django-rest-framework-mongoengine/blob/master/README.md',
+    url='https://github.com/umutbozkurt/django-rest-framework-mongoengine',
+    author='Umut Bozkurt',
+    author_email='umutbozkurt92@gmail.com'
+)
