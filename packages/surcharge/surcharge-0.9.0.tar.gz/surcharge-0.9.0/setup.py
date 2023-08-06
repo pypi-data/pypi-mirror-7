d8f7275a8f0575ef2cd5814eeac156af47f65c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:59 +0000

    Add package surcharge/0.9.0/surcharge-0.9.0.tar.gz

---
+++ packages/surcharge/surcharge-0.9.0.tar.gz/surcharge-0.9.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+import surcharge
+
+setup(
+    name=surcharge.__name__,
+    version=surcharge.__version__,
+    packages=find_packages(),
+    author=surcharge.__author__,
+    author_email="julien@thebault.co",
+    description="Surcharge is a tool for benchmarking your web server",
+    long_description=open('README.rst').read(),
+    install_requires=['requests', 'gevent'],
+    include_package_data=True,
+    url='https://github.com/Lujeni/Surcharge',
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        'console_scripts': [
+            'surcharge = surcharge.cli:main',
+        ],
+    },
+)
