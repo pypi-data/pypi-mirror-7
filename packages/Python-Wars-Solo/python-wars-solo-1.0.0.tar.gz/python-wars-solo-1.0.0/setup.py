f2b9c53c855ba4af7c828bcea744198219b34693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:04 +0000

    Add package Python-Wars-Solo/1.0.0/python-wars-solo-1.0.0.tar.gz

---
+++ packages/Python-Wars-Solo/python-wars-solo-1.0.0.tar.gz/python-wars-solo-1.0.0/setup.py
@@ -0,0 +1,28 @@
+README = open("README.rst")
+
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'python-wars-solo',
+    version = '1.0.0',
+    description = "A retro-style Apple ][ Basic game cooked up in 45 minutes",
+    license = 'GPL',
+    long_description = README,
+    url = 'https://github.com/pydanny/Python-Wars-Solo',
+    author = 'Daniel Greenfeld',
+    author_email = 'pydanny@gmail.com',
+    py_modules =  ['go','stuff', ],
+    classifiers = (
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+    ),
+)
