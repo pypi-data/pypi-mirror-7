b7c473fd88e17928053a67c392f906539706eac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:38 +0000

    Add package oneto/1.0.0/oneto-1.0.0.tar.gz

---
+++ packages/oneto/oneto-1.0.0.tar.gz/oneto-1.0.0/oneto.py
@@ -0,0 +1,7 @@
+def oneto(n):
+	a=0
+	b=0
+	for cc in range(n):
+		a=a+1
+		b=b+a
+		print(b)
---
+++ packages/oneto/oneto-1.0.0.tar.gz/oneto-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+       name          = 'oneto',
+       version       = '1.0.0',
+       py_modules    = ['oneto'],
+       author        = 'holin',
+       author_email  = 'holin@holin.net',
+       url           = 'http://www.holin.net',
+       description   = '1+2+3+4+.....+n,use faq.For example: oneto(100)   ',
+
+        )
+
