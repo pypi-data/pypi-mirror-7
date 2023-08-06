d0d868f5602540666894aab6e6a347180c073794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package sux/0.1.1/sux-0.1.1.tar.gz

---
+++ packages/sux/sux-0.1.1.tar.gz/sux-0.1.1/tests/test_urlparse.py
@@ -0,0 +1,7 @@
+import sux
+
+
+def test_urlparse():
+    up = sux.to_use('urlparse')
+    assert {} == up.parse_qs("")
+    assert 'http://www.cwi.nl/%7Eguido/FAQ.html' == up.urljoin('http://www.cwi.nl/%7Eguido/Python.html', 'FAQ.html')
