7a3d534aa08457241baa523395604c34225e33c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.traceback/0.3.10/infi.traceback-0.3.10-py2.7.egg

---
+++ packages/infi.traceback/infi.traceback-0.3.10-py2.7.egg/infi/traceback/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.3.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '5ddc668e9eb927e7f67326900a763b5a7bb26d08'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
