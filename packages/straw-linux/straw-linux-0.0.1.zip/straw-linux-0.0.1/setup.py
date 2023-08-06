b9beb5566e041ce01976b0c24254c1b1248a918f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:10 +0000

    Add package straw-linux/0.0.1/straw-linux-0.0.1.zip

---
+++ packages/straw-linux/straw-linux-0.0.1.zip/straw-linux-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+    Created on 2014. 6. 9.
+
+    @author: geeksaga
+    :copyright: (c) 2014 by geeksaga.
+    :license: MIT LICENSE 2.0, see license for more details.
+'''
+
+from distutils.core import setup
+
+setup(
+        name       = 'straw-linux',
+        version    = '0.0.1',
+        py_modules = ['straw-linux'],
+        author     = 'geeksaga',
+        author_email = 'geeksaga@geeksaga.com',
+        url          = 'http://geeksaga.com',
+        description  = 'A simple OS monitoring for linux',
+    )
+    
