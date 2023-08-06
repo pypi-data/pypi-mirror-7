04c5784f8958dfa9df2d78dbe086bf901849da48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:52 +0000

    Add package flask_raven/0.0.3/flask_raven-0.0.3.tar.gz

---
+++ packages/flask_raven/flask_raven-0.0.3.tar.gz/flask_raven-0.0.3/flask_raven/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_raven.config
+    ~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2014 Daniel Chatfield
+    :license: Artistic 2.0
+"""
+
+
+RAVEN_AUTH_ENDPOINT = 'https://raven.cam.ac.uk/auth/authenticate.html'
+RAVEN_RESPONSE_TIMESTAMP_DIFF = 60
