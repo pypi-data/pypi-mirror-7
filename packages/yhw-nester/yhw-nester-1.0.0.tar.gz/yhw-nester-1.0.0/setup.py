4f297631df0055259637383ec69aea254a028cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:30 +0000

    Add package yhw-nester/1.0.0/yhw-nester-1.0.0.tar.gz

---
+++ packages/yhw-nester/yhw-nester-1.0.0.tar.gz/yhw-nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name        ='yhw-nester',
+        version     ='1.0.0',
+        py_modules  =['yhw-nester'],        #將模組的中介資料關聯到setup函式的引數
+        author      ='yhw',
+        author_email='yuhung.tw@icloud.com',
+        url         ='http://www.yuhung.tw',
+        description ='A sample module',
+     )
