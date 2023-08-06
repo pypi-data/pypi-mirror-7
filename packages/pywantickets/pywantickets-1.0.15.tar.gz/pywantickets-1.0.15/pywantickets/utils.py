1aad9bba4e38155cf69bdfa66784b29b2a3cbba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:54 +0000

    Add package pywantickets/1.0.15/pywantickets-1.0.15.tar.gz

---
+++ packages/pywantickets/pywantickets-1.0.15.tar.gz/pywantickets-1.0.15/pywantickets/utils.py
@@ -0,0 +1,29 @@
+import json
+import decimal
+from datetime import datetime
+from pywantickets import WanticketsModel
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        super(DecimalEncoder, self).default(o)
+
+def to_json(tree):
+    return json.dumps(tree,
+    	cls=DecimalEncoder, default=make_serializable)
+
+def make_serializable(o):
+    if isinstance(o, WanticketsModel):
+    	return o.get_api_model()
+    else:
+    	return o
+
+def parse_wt_dtime(string):
+    return datetime.strptime(string, '"%Y-%m-%dT%H:%M:%SZ"')
+
+def parse_wt_date(string):
+    return datetime.utcfromtimestamp(
+        int(string.replace("/Date(", '').replace(')/', '')) / 1000
+    )
