9ab28b98eb65f17d43c23cb1d10f7969aed6bb31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:44 +0000

    Add package medialog.mobilethemeOne/0.1/medialog.mobilethemeOne-0.1.tar.gz

---
+++ packages/medialog.mobilethemeOne/medialog.mobilethemeOne-0.1.tar.gz/medialog.mobilethemeOne-0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='medialog.mobilethemeOne',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://github.com/espenmn/medialog.mobilethemeOne',
+      license='gpl',
+      packages=find_packages(),
+      namespace_packages=['medialog'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'ftw.mobilenavigation',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+  	  [z3c.autoinclude.plugin]
+  	  target = plone
+      """,
+      )
