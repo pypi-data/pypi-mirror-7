236e1d556bcf36a1587a3d59cde79d5b56608cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:52 +0000

    Add package dj-twiml/0.1.3/dj-twiml-0.1.3.tar.gz

---
+++ packages/dj-twiml/dj-twiml-0.1.3.tar.gz/dj-twiml-0.1.3/HISTORY.rst
@@ -0,0 +1,9 @@
+.. :changelog:
+
+History
+-------
+
+0.1.3 (2014-07-23)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/dj-twiml/dj-twiml-0.1.3.tar.gz/dj-twiml-0.1.3/dj_twiml/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from twilio.twiml import TwimlException
+from xml.etree import ElementTree as ET
+
+
+class Twiml(models.Model):
+
+    def __unicode__(self):
+        return ''.join(['/twiml/', unicode(self.id), '/'])
+
+    name = models.CharField(max_length=100)
+
+    description = models.CharField(max_length=100, blank=True)
+
+    content = models.TextField(max_length=300)
+
+    def xml_view(self):
+            ''' Return the xml-ified version of the content.
+
+                An XML validation is also performed and will return an
+                XML error if the content is not formatted correctly.
+            '''
+            try:
+                ET.fromstring(self.content)
+            except:
+                raise TwimlException
+            return '<?xml version="1.0" encoding="UTF-8"?>' + self.content
