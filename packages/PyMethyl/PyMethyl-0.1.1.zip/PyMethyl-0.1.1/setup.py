1b2279a57e1219ddd7a070180fc2c0c087c609c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package PyMethyl/0.1.1/PyMethyl-0.1.1.zip

---
+++ packages/PyMethyl/PyMethyl-0.1.1.zip/PyMethyl-0.1.1/setup.py
@@ -0,0 +1,27 @@
+##File to install pymethyl package
+
+##Author: Cody Watson
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PyMethyl',
+    version='0.1.1',
+    author='Cody Watson',
+    author_email='watsonca@email.wofford.edu',
+    packages= find_packages(),
+    package_data = {
+        '':['*.txt', '*.rst']
+        },
+    keywords = "Methylation patterns",
+    url= 'https://pypi.python.org/pypi/PyMethyl',
+    license='LICENSE.txt',
+    description='Tool to determine methylation patterns.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "cruzdb == 0.5.6",
+        "sqlalchemy >= 0.7",
+        "pysam >= 0.8.0",
+	"MySQL-python"
+    ]
+)
