cce7c8e7f13faa5c84e9a6e39d632f53b3abcb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:13 +0000

    Add package naturalsort/1.3/naturalsort-1.3.tar.gz

---
+++ packages/naturalsort/naturalsort-1.3.tar.gz/naturalsort-1.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+
+# Fill in the long description (for the benefit of PyPi)
+# with the contents of README.rst (rendered by GitHub).
+readme_file = join(dirname(abspath(__file__)), 'README.rst')
+readme_text = open(readme_file, 'r').read()
+
+setup(name='naturalsort',
+      version='1.3',
+      description="Simple natural order sorting API for Python",
+      long_description=readme_text,
+      url='https://github.com/xolox/python-naturalsort',
+      author='Peter Odding',
+      author_email='peter@peterodding.com',
+      packages=find_packages(),
+      test_suite='natsort.tests')
