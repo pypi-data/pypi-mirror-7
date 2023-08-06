218b2869d00c2a14c9a09d28894886810f1b06e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:47 +0000

    Add package django-gate2shop/1.0.1/django-gate2shop-1.0.1.tar.gz

---
+++ packages/django-gate2shop/django-gate2shop-1.0.1.tar.gz/django-gate2shop-1.0.1/g2s/__init__.py
@@ -0,0 +1,5 @@
+VERSION = (1, 0, 1)
+
+
+def get_version():
+    return '.'.join(map(str, VERSION))
---
+++ packages/django-gate2shop/django-gate2shop-1.0.1.tar.gz/django-gate2shop-1.0.1/g2s/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse, HttpResponseForbidden
+from g2s.forms import G2STransactionForm
+
+
+def save_g2s_transaction(request):
+    try:
+        form = G2STransactionForm(request.GET)
+        if form.is_valid() and form.save():
+            return HttpResponse("OK")
+        print'[g2s]', dict(form.errors)
+    except Exception, msg:
+        print '[g2s]', msg.__str__()
+    return HttpResponseForbidden()
