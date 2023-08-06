9e51513ff7a9a439dcb11c7c1b0646fbdcb4d956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:26 +0000

    Add package networkxgmml/0.1.2/networkxgmml-0.1.2.tar.gz

---
+++ packages/networkxgmml/networkxgmml-0.1.2.tar.gz/networkxgmml-0.1.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+setup(
+    name='networkxgmml',
+    version='0.1.2',
+    description='XGMML parser for networkx',
+    author='Yasunobu OKAMURA',
+    author_email='okamura@informationsea.info',
+    url='https://github.com/informationsea/networkxxgmml',
+    py_modules=['networkxgmml'],
+    install_requires=['setuptools', 'networkx']
+)
