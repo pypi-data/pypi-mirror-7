3955ac23073d169416aa99c4b89ca4d5f5201bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package normal/1.2.0/normal-1.2.0.zip

---
+++ packages/normal/normal-1.2.0.zip/normal-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name           = 'normal',
+    version        = '1.2.0',
+    py_modules     =['normal'],
+    author         = 'Rithish',
+    author_email   = 'rithishvisakoti@gmail.com',
+    url            = 'http://www.starthabit.com',
+    description    = 'Indentation and optional arguement'
+    )
