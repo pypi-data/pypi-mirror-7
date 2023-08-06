bfc06f5553dcbb27246f2bb7ca58eade16178e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:41 +0000

    Add package selectel-api/0.1.2/selectel-api-0.1.2.tar.gz

---
+++ packages/selectel-api/selectel-api-0.1.2.tar.gz/selectel-api-0.1.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='selectel-api',
+      version='0.1.2',
+      description='Simple selectel API',
+      author='Kirill Goldshtein',
+      author_email='goldshtein.kirill@gmail.com',
+      url='https://github.com/go1dshtein/selectel-api',
+      packages=['selectel'],
+      install_requires=['requests >= 1.2.1'],
+      license='GPLv2',
+      classifiers=['Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries',
+                   'Development Status :: 3 - Alpha',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3'])
