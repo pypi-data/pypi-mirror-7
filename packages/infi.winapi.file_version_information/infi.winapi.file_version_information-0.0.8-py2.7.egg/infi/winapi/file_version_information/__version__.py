18c46f0b900b2b38d3f579163e835ed812da2b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.winapi.file_version_information/0.0.8/infi.winapi.file_version_information-0.0.8-py2.7.egg

---
+++ packages/infi.winapi.file_version_information/infi.winapi.file_version_information-0.0.8-py2.7.egg/infi/winapi/file_version_information/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.8"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '149cc20e0c71a9ba056684d606723ca4e39034bc'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
