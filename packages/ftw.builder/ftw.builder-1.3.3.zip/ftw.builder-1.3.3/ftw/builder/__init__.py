1fe648b6a29640f63e7b8298043859537f4b0c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:22 +0000

    Add package ftw.builder/1.3.3/ftw.builder-1.3.3.zip

---
+++ packages/ftw.builder/ftw.builder-1.3.3.zip/ftw.builder-1.3.3/ftw/builder/__init__.py
@@ -0,0 +1,8 @@
+from ftw.builder.registry import builder_registry
+
+from ftw.builder.builder import Builder
+from ftw.builder.builder import create
+
+import ftw.builder.archetypes
+import ftw.builder.group
+import ftw.builder.user
