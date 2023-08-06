e0b43a5baa0b9ecaa99992001ec12eb52b8001dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:59 +0000

    Add package pyfaidx/0.1.9/pyfaidx-0.1.9.tar.gz

---
+++ packages/pyfaidx/pyfaidx-0.1.9.tar.gz/pyfaidx-0.1.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='pyfaidx',
+    provides='pyfaidx',
+    version='0.1.9',
+    author='Matthew Shirley',
+    author_email='mdshw5@gmail.com',
+    url='http://mattshirley.com',
+    description='pyfaidx: efficient pythonic random '
+                'access to fasta subsequences',
+    license='MIT',
+    packages=['pyfaidx'],
+    install_requires=['six'],
+    entry_points={'console_scripts': ['faidx = pyfaidx.cli:main']},
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "License :: OSI Approved :: MIT License",
+            "Environment :: Console",
+            "Intended Audience :: Science/Research",
+            "Natural Language :: English",
+            "Operating System :: Unix",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ]
+)
