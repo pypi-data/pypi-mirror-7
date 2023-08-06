76c0c55eae7205898d21f73ef2a318bfa2863d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:30 +0000

    Add package liucs/1.0.0/liucs-1.0.0.zip

---
+++ packages/liucs/liucs-1.0.0.zip/liucs-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		= 'liucs',
+		version 	= '1.0.0',
+		py_modules 	= ['liucs'],
+		author		= 'liucs',
+		author_email= 'liu.c.song@gmail.com',
+		url			= 'www.sdfsdf.com',
+		description = 'afsdf',
+	)
