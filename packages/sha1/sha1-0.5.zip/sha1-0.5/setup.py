5c2e63b1e3acac559b2530ef6dacc5977ec0aa25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:47 +0000

    Add package sha1/0.5/sha1-0.5.zip

---
+++ packages/sha1/sha1-0.5.zip/sha1-0.5/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'sha1',
+    version = '0.5',
+    author = 'anatoly techtonik',
+    author_email = 'techtonik@gmail.com',
+    description = 'usage: python -m sha1 <filename>',
+    license = 'Public Domain',
+
+    py_modules=['sha1']
+)
