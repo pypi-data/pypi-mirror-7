1b725cef5a2fd7c5ea40f71212bb1b96d8199027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:33 +0000

    Add package LightWriter/1.0.5/LightWriter-1.0.5.tar.gz

---
+++ packages/LightWriter/LightWriter-1.0.5.tar.gz/LightWriter-1.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LightWriter',
+    version='1.0.5',
+    author='Mike Kadin',
+    author_email='michaelkadin@gmail.com',
+    packages=find_packages(),
+    scripts=['bin/example.py'],
+    url='http://pypi.python.org/pypi/LightWriter/',
+    license='LICENSE.txt',
+    description='Control a RadioShack Light Strip with an Arduino and Python',
+    install_requires=[req.strip() for req in open('requirements.txt').readlines()],
+)
