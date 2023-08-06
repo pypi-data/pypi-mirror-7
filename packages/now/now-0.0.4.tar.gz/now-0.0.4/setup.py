3cc2203ec3cac0f8c49b96b9288b0333ade8c901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:59 +0000

    Add package now/0.0.4/now-0.0.4.tar.gz

---
+++ packages/now/now-0.0.4.tar.gz/now-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+with open("README.rst", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="now",
+    version="0.0.4",
+    packages=["now"],
+    scripts=["scripts/now"],
+    description="Record what you are doing SIMPLY from the command line",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",
+    url="https://github.com/sashahart/now",
+    author="Sasha Hart",
+    author_email="s@sashahart.net",
+)
