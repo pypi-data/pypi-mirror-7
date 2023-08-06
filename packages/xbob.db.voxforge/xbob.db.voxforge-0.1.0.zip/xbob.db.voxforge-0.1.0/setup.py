f4e0d10771b8d2b645fe6cee166a6e6d1e12df83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:17 +0000

    Add package xbob.db.voxforge/0.1.0/xbob.db.voxforge-0.1.0.zip

---
+++ packages/xbob.db.voxforge/xbob.db.voxforge-0.1.0.zip/xbob.db.voxforge-0.1.0/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
+# Fri Aug 23 12:32:01 CEST 2013
+#
+# Copyright (C) 2011-2014 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name='xbob.db.voxforge',
+    version='0.1.0',
+    description='Speaker verification protocol on a subset of the VoxForge database',
+    url='http://pypi.python.org/pypi/xbob.db.voxforge',
+    license='GPLv3',
+    keywords = "Speaker Recognition, Speaker verification, Gaussian Mixture Model, ISV, UBM-GMM, I-Vector, Audio processing, Database, Voxforge",
+    author='Elie Khoury',
+    author_email='Elie.Khoury@idiap.ch',
+    long_description=open('README.rst').read(),
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe = False,
+
+    install_requires=[
+      'setuptools',
+      'bob',
+      'xbob.db.verification.filelist',
+    ],
+
+    namespace_packages = [
+      'xbob',
+      'xbob.db',
+    ],
+
+    entry_points = {
+      # declare database to bob
+      'bob.db': [
+        'voxforge = xbob.db.voxforge.driver:Interface',
+        ],
+
+      # declare tests to bob
+      'bob.test': [
+        'voxforge = xbob.db.voxforge.test:VoxforgeDatabaseTest',
+        ],
+      },
+
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Education',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+      ],
+)
