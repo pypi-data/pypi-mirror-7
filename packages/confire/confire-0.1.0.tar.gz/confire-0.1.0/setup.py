ad7f163985ade3a966186f152c4f70087ae5c306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:09 +0000

    Add package confire/0.1.0/confire-0.1.0.tar.gz

---
+++ packages/confire/confire-0.1.0.tar.gz/confire-0.1.0/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# setup
+# Setup script for confire
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Sun Jul 20 11:06:56 2014 -0400
+#
+# Copyright (C) 2014 District Data Labs
+# For license information, see LICENSE.txt and NOTICE.md
+#
+# ID: setup.py [] benjamin@bengfort.com $
+
+"""
+Setup script for science-bookclub
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import os
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    raise ImportError("Could not import \"setuptools\"."
+                      "Please install the setuptools package.")
+
+##########################################################################
+## Package Information
+##########################################################################
+
+packages = find_packages(where=".", exclude=("tests", "bin", "docs", "fixtures", "register",))
+requires = []
+
+print packages
+
+with open('requirements.txt', 'r') as reqfile:
+    for line in reqfile:
+        requires.append(line.strip())
+
+classifiers = (
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+)
+
+keywords = ('configuration', 'yaml', 'config', 'confire')
+
+long_description = "Confire is a simple but powerful configuration scheme that builds on the configuration parsers of Scapy, elasticsearch, Django and others. The basic scheme is to have a configuration search path that looks for YAML files in standard locations. The search path is hierarchical (meaning that system configurations are overloaded by user configurations, etc). These YAML files are then added to a default, class-based configuration management scheme that allows for easy development.\n\nDocumentation is available here: http://confire.readthedocs.org/en/latest/"
+
+config = {
+    "name": "confire",
+    "version": "0.1.0",
+    "description": "A simple app configuration scheme using YAML and class based defaults.",
+    "long_description": long_description,
+    "license": "MIT",
+    "author": "Benjamin Bengfort",
+    "author_email": "benjamin@bengfort.com",
+    "url": "https://github.com/bbengfort/confire",
+    "download_url": 'https://github.com/bbengfort/confire/tarball/v0.1.0',
+    "packages": packages,
+    "install_requires": requires,
+    "classifiers": classifiers,
+    "keywords": keywords,
+    "zip_safe": True,
+    "scripts": [],
+}
+
+##########################################################################
+## Run setup script
+##########################################################################
+
+if __name__ == '__main__':
+    setup(**config)
