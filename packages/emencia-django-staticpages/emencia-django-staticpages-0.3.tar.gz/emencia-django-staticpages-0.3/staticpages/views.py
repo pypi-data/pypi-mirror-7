1f8535468681f7789ca74373b9b4104f4f1d5019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:31 +0000

    Add package emencia-django-staticpages/0.3/emencia-django-staticpages-0.3.tar.gz

---
+++ packages/emencia-django-staticpages/emencia-django-staticpages-0.3.tar.gz/emencia-django-staticpages-0.3/staticpages/views.py
@@ -0,0 +1,18 @@
+"""
+Views
+"""
+from django.views.generic import TemplateView
+
+class StaticPageView(TemplateView):
+    page_map = None
+    browse_map = True
+    
+    def get_context_data(self, **kwargs):
+        context = super(StaticPageView, self).get_context_data(**kwargs)
+        # Search templates from the base dir of the template
+        if self.browse_map and self.page_map:
+            context.update({
+                'page_map': self.page_map,
+            })
+        return context
+    
