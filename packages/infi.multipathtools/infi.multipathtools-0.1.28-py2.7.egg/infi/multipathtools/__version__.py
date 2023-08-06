9e19c64602a1b3def952c4441f68792f83c1cf00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:03 +0000

    Add package infi.multipathtools/0.1.28/infi.multipathtools-0.1.28-py2.7.egg

---
+++ packages/infi.multipathtools/infi.multipathtools-0.1.28-py2.7.egg/infi/multipathtools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.28"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.28)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '6f200b06eb91c84655fcd2ac21c9f94f253000e4'
+__git_head_subject__ = 'Finished release v0.1.28.'
+__git_head_message__ = '* release/v0.1.28:\n  HIP-1237 using gevent.socket if exists\n  HIP-1237 logging multipath socket errors'
+__git_dirty_diff__ = ''
