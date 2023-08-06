abf57c3a9f18d26562db4324f9983c87f98e4ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:01 +0000

    Add package apropos/0.1.0/apropos-0.1.0.tar.gz

---
+++ packages/apropos/apropos-0.1.0.tar.gz/apropos-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apropos',
+    py_modules=['apropos'],
+    version='0.1.0',
+    author='David Keijser',
+    author_email='keijser@gmail.com',
+    description='track the state of things',
+    license='MIT',
+    keywords='tracker'
+)
