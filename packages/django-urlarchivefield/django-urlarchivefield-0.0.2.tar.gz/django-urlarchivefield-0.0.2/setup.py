0636ab537d4d77e448259dca8793c2c068e2b4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:32 +0000

    Add package django-urlarchivefield/0.0.2/django-urlarchivefield-0.0.2.tar.gz

---
+++ packages/django-urlarchivefield/django-urlarchivefield-0.0.2.tar.gz/django-urlarchivefield-0.0.2/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                }
+            },
+            INSTALLED_APPS=('urlarchivefield',)
+        )
+        from django.core.management import call_command
+        call_command('test', 'urlarchivefield')
+
+
+setup(
+    name='django-urlarchivefield',
+    version='0.0.2',
+    description='A custom Django model field that automatically archives a URL',
+    author='Ben Welsh',
+    author_email='ben.welsh@gmail.com',
+    url='https://github.com/pastpages/django-urlarchivefield',
+    license='MIT',
+    packages=('urlarchivefield',),
+    install_requires=(
+        'storytracker==0.0.1',
+        'six==1.7.2',
+    ),
+    cmdclass={'test': TestCommand}
+)
