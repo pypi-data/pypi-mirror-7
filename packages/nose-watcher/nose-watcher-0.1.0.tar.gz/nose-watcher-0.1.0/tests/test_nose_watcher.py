d4e7a2e6d440aeb12fc126f62af114106256575e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package nose-watcher/0.1.0/nose-watcher-0.1.0.tar.gz

---
+++ packages/nose-watcher/nose-watcher-0.1.0.tar.gz/nose-watcher-0.1.0/tests/test_nose_watcher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_nose-watcher
+----------------------------------
+
+Tests for `nose-watcher` module.
+"""
+
+import unittest
+
+from nose_watcher import nose_watcher
+
+
+class TestNoseWatcher(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
