acc37e4dd5b4407dea47eeeaf53b5679c80e4eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:01 +0000

    Add package passletters/0.2.1/passletters-0.2.1.tar.gz

---
+++ packages/passletters/passletters-0.2.1.tar.gz/passletters-0.2.1/README.md
@@ -0,0 +1,12 @@
+passletters
+===========
+
+Read password from stdin and echo it with letters enumerated (for entry into web prompts). Clear terminal afterwards.
+
+Installation:
+```
+sudo pip install passletters
+```
+**NB**: sudo may be needed to install the `passletters` script somewhere in your PATH.
+
+PyPI: https://pypi.python.org/pypi/passletters
---
+++ packages/passletters/passletters-0.2.1.tar.gz/passletters-0.2.1/passletters/passletters.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import getpass
+import os
+import sys
+from colorama import init, Fore
+
+def main():
+    init()
+
+    password = getpass.getpass()
+
+    SPACING = ' ' * 3
+
+    lines = [[] for i in xrange(3)]
+    for number, letter in enumerate(password):
+        number += 1
+        numberStr = str(number)
+        padding = ' ' * (len(numberStr) - 1)
+        lines[0].append(Fore.GREEN + letter + Fore.RESET + padding)
+        lines[1].append('^' + padding)
+        lines[2].append(Fore.BLUE + numberStr + Fore.RESET)
+
+    for line in lines:
+        print(SPACING.join(line))
+
+    CLEAR_MSG = 'Press any key to clear the screen'
+    if sys.version_info >= (3, 0):
+        input(CLEAR_MSG)
+    else:
+        raw_input(CLEAR_MSG)
+
+    # http://stackoverflow.com/questions/2084508/clear-terminal-in-python
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+if __name__ == '__main__':
+    main()
