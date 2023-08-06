b98a597827aa64006eea784acf50871dbfa5215f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:03 +0000

    Add package HfCh6Levi/1.0.3/HfCh6Levi-1.0.3.tar.gz

---
+++ packages/HfCh6Levi/HfCh6Levi-1.0.3.tar.gz/HfCh6Levi-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	
+    	name='HfCh6Levi',
+    	version='1.0.3',
+    	py_modules=['HfCh6Levi'],
+    	author='levibinlee',
+    	author_email='levibinlee@gmail.com',
+    	url='http://www.headfirstlabs',
+	description='refined code for using Class to achieve the desired functionalities' ,    
+)
+    
