dee5b96dd231bb555b402ceb7af8af654d152de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:56 +0000

    Add package rq-dashboard/0.3.4/rq-dashboard-0.3.4.tar.gz

---
+++ packages/rq-dashboard/rq-dashboard-0.3.4.tar.gz/rq-dashboard-0.3.4/setup.py
@@ -0,0 +1,60 @@
+import os
+from setuptools import setup, find_packages
+
+
+def get_version():
+    basedir = os.path.dirname(__file__)
+    with open(os.path.join(basedir, 'rq_dashboard/version.py')) as f:
+        locals = {}
+        exec(f.read(), locals)
+        return locals['VERSION']
+    raise RuntimeError('No version info found.')
+
+setup(
+    name='rq-dashboard',
+    version=get_version(),
+    url='https://github.com/nvie/rq-dashboard',
+    license='BSD',
+    author='Vincent Driessen',
+    author_email='vincent@3rdcloud.com',
+    description='rq-dashboard is a general purpose, lightweight, web interface'
+                ' to monitor your RQ queues, jobs, and workers in realtime.',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['rq>=0.3.8', 'Flask', 'redis', 'simplejson', 'times'],
+    entry_points='''\
+    [console_scripts]
+    rq-dashboard = rq_dashboard.scripts.rq_dashboard:main
+    ''',
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Monitoring',
+    ]
+)
