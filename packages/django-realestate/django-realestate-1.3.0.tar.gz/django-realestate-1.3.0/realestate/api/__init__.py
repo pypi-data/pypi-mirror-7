57d14405d6180bbb09ee8f011247406483d64022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:48 +0000

    Add package django-realestate/1.3.0/django-realestate-1.3.0.tar.gz

---
+++ packages/django-realestate/django-realestate-1.3.0.tar.gz/django-realestate-1.3.0/realestate/api/__init__.py
@@ -0,0 +1,2 @@
+from realestate.api.serializers import PropertySerializer
+from realestate.api.views import PropiedadViewSet
---
+++ packages/django-realestate/django-realestate-1.3.0.tar.gz/django-realestate-1.3.0/testproject/settings_local.py
@@ -0,0 +1,6 @@
+# DEBUG = False
+# TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['localhost', ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
