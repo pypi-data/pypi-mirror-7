9e3c088a8f1413eb1a6579d9098532b5c7e23d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:01 +0000

    Add package subunitdetails/0.0.2/subunitdetails-0.0.2.tar.gz

---
+++ packages/subunitdetails/subunitdetails-0.0.2.tar.gz/subunitdetails-0.0.2/subunitdetails/subunitdetails.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+import re
+
+from subunit import ByteStreamToStreamResult
+from testtools import StreamToExtendedDecorator, TestResult
+
+
+logging.basicConfig(level='INFO', format='%(message)s')
+logger = logging.getLogger(__name__)
+
+
+class FileSaveResult(TestResult):
+    """A TestResult that saves arbitrary test details to files."""
+
+    def addError(self, test, err=None, details=None):
+        super().addError(test, err=err, details=details)
+        self.save_file_attachments(test.id(), details)
+
+    def addExpectedFailure(self, test, err=None, details=None):
+        super().addExpectedFailure(test, err=err, details=details)
+        self.save_file_attachments(test.id(), details)
+
+    def addFailure(self, test, err=None, details=None):
+        super().addFailure(test, err=err, details=details)
+        self.save_file_attachments(test.id(), details)
+
+    def addSkip(self, test, reason=None, details=None):
+        super().addSkip(test, reason=reason, details=details)
+        self.save_file_attachments(test.id(), details)
+
+    def addSuccess(self, test, details=None):
+        super().addSuccess(test, details=details)
+        self.save_file_attachments(test.id(), details)
+
+    def addUnexpectedSuccess(self, test, details=None):
+        super().addUnexpectedSuccess(test, details=details)
+        self.save_file_attachments(test.id(), details)
+
+    def save_file_attachments(self, test_id, details):
+        for key, detail in details.items():
+            if detail.iter_bytes() == [b'']:
+                continue
+            slug = re.sub(r'(?u)[^-\w.]', '', key)
+            file_name = '%s-%s' % (test_id, slug)
+            with open(file_name, 'wb') as f:
+                for chunk in detail.iter_bytes():
+                    f.write(chunk)
+            file_size = os.path.getsize(file_name)
+            logger.info(' test id: %r' % test_id)
+            logger.info(' attachment: %r (%s) [%s bytes]' % (
+                key,
+                detail.content_type,
+                file_size,)
+            )
+            logger.info(' saved as: %r\n' % file_name)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Subunit Detail Extractor')
+    parser.add_argument('subunit_file', action="store")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    logger.info('\nParsing Subunit input: %r [%s bytes]' % (
+        args.subunit_file,
+        os.path.getsize(args.subunit_file))
+    )
+    stream = open(args.subunit_file, 'rb')
+    suite = ByteStreamToStreamResult(stream)
+    logger.info('\nExtracting details...\n')
+    result = StreamToExtendedDecorator(FileSaveResult())
+    result.startTestRun()
+    try:
+        suite.run(result)
+    finally:
+        result.stopTestRun()
+    logger.info('\nDone.')
+
+
+if __name__ == '__main__':
+    main()
