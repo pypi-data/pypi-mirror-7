a4c151ac41246325be761340942d455250fe8098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:33 +0000

    Add package jasmin_cis/0.7/jasmin_cis-0.7.tar.gz

---
+++ packages/jasmin_cis/jasmin_cis-0.7.tar.gz/jasmin_cis-0.7/jasmin_cis/data_io/products/AProduct.py
@@ -0,0 +1,116 @@
+from abc import ABCMeta, abstractmethod
+import logging
+
+class AProduct(object):
+    '''
+    Abstract class for the various possible data products. This just defines the interface which
+    the subclasses must implement.
+    '''
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def create_data_object(self, filenames, variable):
+        '''
+        Create a an ungridded data object for a given variable from many files
+
+        :param filenames:    List of filenames of files to read
+        :param usr_variable:    Variable to read from the files
+        :return: An UngriddedData object for the specified variable
+
+        :raise FileIOError: Unable to read a file
+        :raise InvalidVariableError: Variable not present in file
+        '''
+
+    @abstractmethod
+    def create_coords(self, filenames):
+        '''
+        Reads the coordinates from a bunch of files
+        :param filenames: List of filenames to read coordinates from
+        :return: L{CoordList} object
+        '''
+
+    @abstractmethod
+    def get_file_signature(self):
+        '''
+        :return: a list of regex to match the product's file naming convention.
+
+        Example
+        -------
+
+        return [r'.*CODE*.nc']
+        This will match all files with a name containing the string 'CODE' and with the 'nc' extension.
+        '''
+
+
+def __get_class(filename, product=None):
+    '''
+    Identify the subclass of L{AProduct} to a given product name if specified.
+    If the product name is not specified, the routine uses the signature (regex)
+    given by get_file_signature() to infer the product class from the filename.
+
+    Note, only the first filename of the list is use here.
+
+    :param filename: A single filename
+    :param product: name of the product
+    :return: a subclass of L{AProduct}
+    '''
+    import re
+    import os
+    import jasmin_cis.plugin as plugin
+    from jasmin_cis.exceptions import ClassNotFoundError
+
+    # Ensure the filename doesn't include the path
+    filename = os.path.basename(filename)
+
+    product_classes = plugin.find_plugin_classes(AProduct, 'jasmin_cis.data_io.products.products')
+
+    for cls in product_classes:
+
+        if product is None:
+            # search for a pattern that matches file signature
+            patterns = cls().get_file_signature()
+            for pattern in patterns:
+                # Match the pattern - re.I allows for case insensitive matches
+                if re.match(pattern,filename,re.I) is not None:
+                    logging.debug("Found product class " + cls.__name__ + " matching regex pattern " + pattern)
+                    return cls
+        else:
+            # product specified directly
+            if product == cls.__name__:
+                logging.debug("Selected product class " +  cls.__name__)
+                return cls
+    error_message = "Product cannot be found for given file.\nSupported products and signatures are:\n"
+    for cls in product_classes:
+        error_message += cls().__class__.__name__ + ": " + str(cls().get_file_signature()) + "\n"
+    raise ClassNotFoundError(error_message)
+
+
+def get_data(filenames, variable, product=None):
+    '''
+    Top level routine for calling the correct product's create_ungridded_data routine.
+
+    :param product: The product to read data from - this should be a string which matches the name of one of the subclasses of AProduct
+    :param filenames: A list of filenames to read data from
+    :param variable: The variable to create the UngriddedData object from
+    :return: An Ungridded data variable
+    '''
+    product_cls = __get_class(filenames[0], product)
+
+    logging.info("Retrieving data using product " +  product_cls.__name__ + "...")
+    data = product_cls().create_data_object(filenames, variable)
+    return data
+
+
+def get_coordinates(filenames, product=None):
+    '''
+    Top level routine for calling the correct product's create_coords routine.
+
+    :param product: The product to read data from - this should be a string which matches the name of one of the subclasses of AProduct
+    :param filenames: A list of filenames to read data from
+    :return: A CoordList object
+    '''
+    product_cls = __get_class(filenames[0], product)
+
+    logging.info("Retrieving coordinates using product " +  product_cls.__name__)
+    coord = product_cls().create_coords(filenames)
+    return coord
---
+++ packages/jasmin_cis/jasmin_cis-0.7.tar.gz/jasmin_cis-0.7/jasmin_cis/subsetting/subset_constraint.py
@@ -0,0 +1,148 @@
+from abc import ABCMeta
+import logging
+from collections import namedtuple
+
+import numpy as np
+import iris
+
+from jasmin_cis.subsetting.subset_framework import SubsetConstraintInterface
+from jasmin_cis.data_io.Coord import Coord, CoordList
+import jasmin_cis.data_io.gridded_data as gridded_data
+from jasmin_cis.data_io.ungridded_data import UngriddedData
+
+
+class CoordLimits(namedtuple('CoordLimits', ['coord', 'start', 'end', 'constraint_function'])):
+    """Holds the start and end values for subsetting limits.
+    :ivar coord: the coordinate the limit applies to
+    :ivar start: subsetting limit start
+    :ivar end: subsetting limit end
+    :ivar constraint_function: function determining whether the constraint is satisfied
+    """
+    pass
+
+
+class SubsetConstraint(SubsetConstraintInterface):
+    """Abstract Constraint for subsetting.
+
+    Holds the limits for subsetting in each dimension.
+    """
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self._limits = {}
+        logging.debug("Created SubsetConstraint of type %s", self.__class__.__name__)
+
+    def set_limit(self, coord, dim_min, dim_max, wrapped):
+        """Sets boundary values for a dimension to be used in subsetting.
+
+        :param coord: coordinate to which limit applies
+        :param dim_min: lower bound on dimension or None to indicate no lower bound
+        :param dim_max: upper bound on dimension or None to indicate no upper bound
+        """
+        if dim_min is not None or dim_max is not None:
+            logging.info("Setting limit for dimension '%s' [%s, %s]", coord.name(), str(dim_min), str(dim_max))
+            self._limits[coord.name()] = CoordLimits(coord, dim_min, dim_max,
+                                                     self._make_constraint_function(dim_min, dim_max, wrapped))
+
+    @staticmethod
+    def _make_constraint_function(dim_min, dim_max, wrapped):
+        """Constructs a function enforcing the specified bounds on the values of a dimension.
+
+        The boundary values are included in the constrained interval.
+        :param dim_min: lower bound on dimension or None to indicate no lower bound
+        :param dim_max: upper bound on dimension or None to indicate no upper bound
+        :param wrapped: True if the coordinate is circular and the included range is wrapped, otherwise False
+        :return: lambda function with one argument returning bool
+        """
+        if wrapped:
+            return lambda x: x >= dim_min or x <= dim_max
+        else:
+            return lambda x: dim_min <= x <= dim_max
+
+    def __str__(self):
+        limit_strs = []
+        for name, limit in self._limits.iteritems():
+            limit_strs.append("{}: [{}, {}]".format(name, str(limit.start), str(limit.end)))
+        return ', '.join(limit_strs)
+
+
+class GriddedSubsetConstraint(SubsetConstraint):
+    """Implementation of SubsetConstraint for subsetting gridded data.
+    """
+    def make_iris_constraint(self):
+        """Constructs an Iris constraint corresponding to the limits set for each dimension.
+
+        :return: iris.Constraint object
+        """
+        constraint = None
+        for coord, limits in self._limits.iteritems():
+            constraint_function = limits.constraint_function
+            if constraint is None:
+                constraint = iris.Constraint(**{coord: constraint_function})
+            else:
+                constraint = constraint & iris.Constraint(**{coord: constraint_function})
+        return constraint
+
+    def constrain(self, data):
+        """Subsets the supplied data.
+
+        :param data: data to be subsetted
+        :type data: iris.cube.Cube
+        :return: subsetted data
+        @rtype: jasmin_cis.data_io.gridded_data.GriddedData
+        """
+        iris_constraint = self.make_iris_constraint()
+        return gridded_data.make_from_cube(data.extract(iris_constraint))
+
+
+class UngriddedSubsetConstraint(SubsetConstraint):
+    """Implementation of SubsetConstraint for subsetting ungridded data.
+    """
+    def constrain(self, data):
+        """Subsets the supplied data.
+
+        :param data: data to be subsetted
+        :return: subsetted data
+        """
+        CoordPair = namedtuple('CoordPair', ['input', 'output'])
+
+        new_values = []
+        coord_pairs = []
+        for coord in data.coords():
+            coord_pairs.append(CoordPair(coord, []))
+
+        # Filter points to include in subset.
+        is_masked = isinstance(data.data, np.ma.masked_array)
+        missing_value = data.metadata.missing_value
+        for idx, value in enumerate(data.data.flat):
+            if is_masked and value is np.ma.masked:
+                value = missing_value
+
+            include = True
+            for limit in self._limits.itervalues():
+                if not limit.constraint_function(limit.coord.data_flattened[idx]):
+                    include = False
+                    break
+            if include:
+                # Append coordinates and value.
+                new_values.append(value)
+                for coord in coord_pairs:
+                    coord.output.append(coord.input.data_flattened[idx])
+
+        if len(new_values) > 0:
+            # Collect output into object.
+            new_data = np.array(new_values, dtype=data.data.dtype, copy=False)
+            subset_metadata = data.metadata
+            subset_metadata.shape = (len(new_values),)
+
+            subset_coords = CoordList()
+            for coord in coord_pairs:
+                new_coord = Coord(np.array(coord.output, dtype=coord.input.data.dtype, copy=False),
+                                  coord.input.metadata)
+                new_coord.metadata.shape = (len(coord.output),)
+                subset_coords.append(new_coord)
+            subset = UngriddedData(new_data, subset_metadata, subset_coords)
+        else:
+            subset = None
+
+        return subset
