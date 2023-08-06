2b444c4462e562e9209b824c7f7075b6444f7f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:11 +0000

    Add package django-inspect/0.2/django-inspect-0.2.tar.gz

---
+++ packages/django-inspect/django-inspect-0.2.tar.gz/django-inspect-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-inspect",
+    description="Provides information about django models",
+    version="0.2",
+    packages=find_packages(),
+    author="Thiago Pisani",
+    author_email="pisani.thiago@gmail.com",
+    url="https://github.com/tpisani/django-inspect",
+    license="BSD",
+    install_requires=["Django"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
