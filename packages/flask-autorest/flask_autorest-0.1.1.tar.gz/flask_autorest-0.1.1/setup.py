e99e249d677fca82f7dc9f61b82f27d5204ca9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:38 +0000

    Add package flask-autorest/0.1.1/flask_autorest-0.1.1.tar.gz

---
+++ packages/flask-autorest/flask_autorest-0.1.1.tar.gz/flask_autorest-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flask_autorest",
+    version='0.1.1',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['flask', 'dataset'],
+    url="https://github.com/dantezhu/flask_autorest",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="auto create restful apis for database, with the help of dataset.",
+)
