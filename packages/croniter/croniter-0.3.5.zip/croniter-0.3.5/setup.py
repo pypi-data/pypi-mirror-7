693a31b42e4f1a664ab6780d4f8fa7c867a26b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:14 +0000

    Add package croniter/0.3.5/croniter-0.3.5.zip

---
+++ packages/croniter/croniter-0.3.5.zip/croniter-0.3.5/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(
+        os.path.join('.', *rnames)
+    ).read()
+
+long_description = "\n\n".join(
+    [
+        read('README.rst'),
+        read('docs', 'CHANGES.rst'),
+    ]
+)
+
+setup(
+    name='croniter',
+    version='0.3.5',
+    py_modules=['croniter', ],
+    description=(
+        'croniter provides iteration for datetime '
+        'object with cron like format'
+    ),
+    long_description=long_description,
+    author="Matsumoto Taichi, kiorky",
+    author_email='taichino@gmail.com, kiorky@cryptelium.net',
+    url='http://github.com/kiorky/croniter',
+    keywords='datetime, iterator, cron',
+    install_requires=[
+        "python-dateutil",
+        "setuptools",
+    ],
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    extras_require={
+        'test': [
+            "pytz",
+        ],
+    },
+)
