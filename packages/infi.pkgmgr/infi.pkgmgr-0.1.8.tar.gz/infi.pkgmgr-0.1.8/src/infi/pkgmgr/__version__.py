4c9312b5a1b82c42a547176ad1664753505fffd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:40 +0000

    Add package infi.pkgmgr/0.1.8/infi.pkgmgr-0.1.8.tar.gz

---
+++ packages/infi.pkgmgr/infi.pkgmgr-0.1.8.tar.gz/infi.pkgmgr-0.1.8/src/infi/pkgmgr/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.8"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '1f42cc650565b92a7705fb057dac204f0b0cbf83'
+__git_head_subject__ = 'Finished release v0.1.8.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
