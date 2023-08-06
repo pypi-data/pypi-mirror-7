d2b545d12015ae4720c220eaf1fc2dc995599a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:04 +0000

    Add package schedule/0.3.0/schedule-0.3.0.tar.gz

---
+++ packages/schedule/schedule-0.3.0.tar.gz/schedule-0.3.0/setup.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r pypi')
+    sys.exit()
+
+SCHEDULE_VERSION = '0.3.0'
+SCHEDULE_DOWNLOAD_URL = (
+    'https://github.com/dbader/schedule/tarball/' + SCHEDULE_VERSION
+)
+
+setup(
+    name='schedule',
+    packages=['schedule'],
+    version=SCHEDULE_VERSION,
+    description='Job scheduling for humans.',
+    long_description=(
+        open('README.rst').read() + '\n\n' +
+        open('HISTORY.rst').read()
+    ),
+    license=open('LICENSE.txt').read(),
+    author='Daniel Bader',
+    author_email='mail@dbader.org',
+    url='https://github.com/dbader/schedule',
+    download_url=SCHEDULE_DOWNLOAD_URL,
+    keywords=[
+        'schedule', 'periodic', 'jobs', 'scheduling', 'clockwork',
+        'cron'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Natural Language :: English',
+    ],
+)
