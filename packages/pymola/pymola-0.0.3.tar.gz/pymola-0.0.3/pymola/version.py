d0c7bb6f65b3dec3bcc2635e9208d3625c0aecb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:00 +0000

    Add package pymola/0.0.3/pymola-0.0.3.tar.gz

---
+++ packages/pymola/pymola-0.0.3.tar.gz/pymola-0.0.3/pymola/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.0.3'
+version = '0.0.3'
+full_version = '0.0.3'
+git_revision = '1a8de7b3dfa7476abecb972122ff83abc4ba9974'
+release = True
+
+if not release:
+    version = full_version
