aa96f187fd1f638532740b627be8a7b38b11a882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package will/0.4.10/will-0.4.10.tar.gz

---
+++ packages/will/will-0.4.10.tar.gz/will-0.4.10/will/settings.py
@@ -0,0 +1,23 @@
+import os
+
+for k, v in os.environ.items():
+    globals()[k] = v
+
+if "WILL_ROOMS" in os.environ:
+    WILL_ROOMS = WILL_ROOMS.split(";")
+
+if not "WILL_DEFAULT_ROOM" in os.environ:
+    WILL_DEFAULT_ROOM = WILL_ROOMS[0]
+
+if not "WILL_HTTPSERVER_PORT" in os.environ:
+    # For heroku
+    if "PORT" in os.environ:
+        WILL_HTTPSERVER_PORT = PORT
+    else:
+        WILL_HTTPSERVER_PORT = "80"
+
+if not "WILL_URL" in os.environ:
+    WILL_URL = "http://localhost:%s" % WILL_HTTPSERVER_PORT
+
+WILL_ADMINS = "*" if os.environ.get('WILL_ADMINS', '*') == "*" else \
+    [a.strip().lower() for a in os.environ.get('WILL_ADMINS', '').split(';') if a.strip()]
