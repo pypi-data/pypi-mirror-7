c22bf5498577da2fb711940754ebe15d88755593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:30 +0000

    Add package infi.monotonic_time/0.1.4/infi.monotonic_time-0.1.4.tar.gz

---
+++ packages/infi.monotonic_time/infi.monotonic_time-0.1.4.tar.gz/infi.monotonic_time-0.1.4/src/infi/monotonic_time/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f9327d211c75ad37f88ab77382e323a5b9aa47ec'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
