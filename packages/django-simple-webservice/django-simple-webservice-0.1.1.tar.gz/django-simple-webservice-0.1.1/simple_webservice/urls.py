709211f3cd995859c19607a2719c8515d5daee65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:11 +0000

    Add package django-simple-webservice/0.1.1/django-simple-webservice-0.1.1.tar.gz

---
+++ packages/django-simple-webservice/django-simple-webservice-0.1.1.tar.gz/django-simple-webservice-0.1.1/simple_webservice/urls.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# django-simple-webservice
+# Copyright (c) 2014, Liricus, All rights reserved.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
+
+
+#==============================================================================
+# IMPS
+#==============================================================================
+
+from django.conf.urls import patterns
+
+urlpatterns = patterns(
+    'simple_webservice.views',
+    (r'^call/$', 'call'),
+)
