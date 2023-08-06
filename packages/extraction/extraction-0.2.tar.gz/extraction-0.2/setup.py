0f6f7155c1a02d1dc20aa38f08987f7748e610c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package extraction/0.2/extraction-0.2.tar.gz

---
+++ packages/extraction/extraction-0.2.tar.gz/extraction-0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='extraction',
+    version='0.2',
+    author='Will Larson',
+    author_email='lethain@gmail.com',
+    packages=['extraction', 'extraction.tests', 'extraction.examples'],
+    url='http://pypi.python.org/pypi/extraction/',
+    license='LICENSE.txt',
+    description='Extract basic info from HTML webpages.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "beautifulsoup4 >= 4.1.3",
+        "html5lib",
+        ],
+)
