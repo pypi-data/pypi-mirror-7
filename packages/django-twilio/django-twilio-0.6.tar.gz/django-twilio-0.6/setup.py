f859bacf17f0a4247f714bac8d877b36a5a8b7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:04 +0000

    Add package django-twilio/0.6/django-twilio-0.6.tar.gz

---
+++ packages/django-twilio/django-twilio-0.6.tar.gz/django-twilio-0.6/setup.py
@@ -0,0 +1,30 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import find_packages, setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'django-twilio',
+    version = '0.6',
+    packages = find_packages(),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['twilio>=3.3.6', 'Django>=1.3.1', 'django-phonenumber-field>=0.2a3'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'rdegges@gmail.com',
+    license = 'UNLICENSE',
+    url = 'http://twilio.com/',
+    keywords = 'twilio telephony call phone voip sms',
+    description = 'Build Twilio functionality into your Django apps.',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.rst'))).read()
+
+)
