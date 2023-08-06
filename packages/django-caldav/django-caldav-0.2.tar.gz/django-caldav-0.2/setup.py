6ccfd337aa56f0a80114e47fca37fda2c85688e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:08 +0000

    Add package django-caldav/0.2/django-caldav-0.2.tar.gz

---
+++ packages/django-caldav/django-caldav-0.2.tar.gz/django-caldav-0.2/setup.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+from setuptools import setup, find_packages
+import os 
+
+setup(
+    name='django-caldav',
+    version='0.2',
+    description='CalDAV implementation for Django offering Feed interface',
+    author='Petr Knap',
+    author_email='knap@wpj.cz',
+    url = 'http://www.wpj.cz',
+    download_url='',
+    packages=["django_caldav",],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business',
+    ],
+    zip_safe = False,
+    install_requires = [
+        'django',
+        'django-ical',
+        'djangodav',
+        'lxml',
+    ],
+)
