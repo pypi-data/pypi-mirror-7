d4ce0b18fb9eb0260cfbc9f7117bb422fe7a996b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package cz7129183/1.0.0/cz7129183-1.0.0.zip

---
+++ packages/cz7129183/cz7129183-1.0.0.zip/cz7129183-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    
+name='cz7129183',
+    
+version='1.0.0',
+    
+py_modules=['nester'],
+    
+author='hfpython',
+    
+author_email='123@126.com'
+    
+    )
