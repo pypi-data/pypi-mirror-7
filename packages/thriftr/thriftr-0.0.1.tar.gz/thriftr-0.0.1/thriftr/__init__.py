197147d5f0a57d00a947958482f24e9f97954ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:13 +0000

    Add package thriftr/0.0.1/thriftr-0.0.1.tar.gz

---
+++ packages/thriftr/thriftr-0.0.1.tar.gz/thriftr-0.0.1/thriftr/__init__.py
@@ -0,0 +1,23 @@
+# coding=utf8
+
+"""
+thriftr
+~~~~~~~
+
+The pure python parser for apache thrift.
+
+Author: hit9 <wangchao@ele.me>
+License: MIT.
+
+    >>> from thriftr import parse
+    >>> thrift = parse(open('sample.thrift').read())
+"""
+
+__version__ = '0.0.1'
+
+
+class ThriftSyntaxError(Exception):
+    pass
+
+
+from .parser import parse
