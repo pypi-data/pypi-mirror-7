3c3310de2ec6efe788b7d82fe288935f03073cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:24 +0000

    Add package XSLClearer/0.1.3/XSLClearer-0.1.3.tar.gz

---
+++ packages/XSLClearer/XSLClearer-0.1.3.tar.gz/XSLClearer-0.1.3/xsls/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""xsls package"""
+
+from .compiler_exception import CompilerException
+from .compiler import Compiler
+from .tokenizer_exception import TokenizerException
+from .tokenizer import Tokenizer
+from .xslclearer import xsls_compile
---
+++ packages/XSLClearer/XSLClearer-0.1.3.tar.gz/XSLClearer-0.1.3/xsls/keywords/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""List of XSL tags and attributes"""
+
+__all__ = [
+    "xsl_fo_tags",
+    "xsl_fo_attributes",
+    "xslt_tags",
+    "xslt_attributes",
+    "xml_attributes",
+    "xsls_tags",
+    "xsls_to_xslt_tags",
+]
