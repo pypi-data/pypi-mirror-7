1fa532989fc7aeb36ca6075587785de661557b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.recipe.python/0.6.9/infi.recipe.python-0.6.9.tar.gz

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.9.tar.gz/infi.recipe.python-0.6.9/src/infi/recipe/python/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.6.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a87c188351ce14c3a9e5cfc459525ed167968b9f'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
