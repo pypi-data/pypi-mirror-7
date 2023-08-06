48f332b3e15b51ba9ff6715df1ca9161b6750120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:26 +0000

    Add package django-respite/1.4.0/django-respite-1.4.0.tar.gz

---
+++ packages/django-respite/django-respite-1.4.0.tar.gz/django-respite-1.4.0/README.rst
@@ -0,0 +1,14 @@
+Respite
+=======
+
+.. image:: https://secure.travis-ci.org/jgorset/django-respite.png?branch=master
+
+About
+-----
+
+Respite conforms Django to Representational State Transfer (and, incidentally, HTTP).
+
+Documentation
+-------------
+
+Respite's documentation is available on `Read the Docs <http://readthedocs.org/docs/django-respite>`_.
---
+++ packages/django-respite/django-respite-1.4.0.tar.gz/django-respite-1.4.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+execfile('respite/version.py')
+
+setup(
+    name = 'django-respite',
+    version = __version__,
+    description = "Respite conforms Django to Representational State Transfer (REST)",
+    long_description = open('README.rst').read(),
+    author = "Johannes Gorset",
+    author_email = "jgorset@gmail.com",
+    url = "http://github.com/jgorset/django-respite",
+    packages = ['respite', 'respite.lib', 'respite.serializers', 'respite.urls', 'respite.views', 'respite.utils']
+)
