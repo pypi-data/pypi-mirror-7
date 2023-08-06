f975162c07627569d1ddf014efd4d09bc25aeeee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:20 +0000

    Add package nose-picker/0.5.1/nose-picker-0.5.1.tar.gz

---
+++ packages/nose-picker/nose-picker-0.5.1.tar.gz/nose-picker-0.5.1/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+VERSION = '0.5.1'
+
+
+setup(
+    name='nose-picker',
+    version=VERSION,
+    classifiers=['License :: OSI Approved :: BSD License'],
+    long_description=README,
+    url='https://github.com/eventbrite/nose-picker',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'nose.plugins.0.10': ['picker = picker.nose_plugin:NosePicker'],
+    },
+)
