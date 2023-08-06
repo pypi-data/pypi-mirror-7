bad2512455440cccd3ed78a921c1de7b87bb170c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package infi.win32service/0.1.6/infi.win32service-0.1.6.tar.gz

---
+++ packages/infi.win32service/infi.win32service-0.1.6.tar.gz/infi.win32service-0.1.6/src/infi/win32service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2c2402b799549a91260d51aad363618745cdcc68'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
