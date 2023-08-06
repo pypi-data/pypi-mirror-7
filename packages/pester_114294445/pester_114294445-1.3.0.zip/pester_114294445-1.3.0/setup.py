bed7026c9e70d340b1750800120650c4b415d2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:35 +0000

    Add package pester_114294445/1.3.0/pester_114294445-1.3.0.zip

---
+++ packages/pester_114294445/pester_114294445-1.3.0.zip/pester_114294445-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'pester_114294445',
+    version='1.3.0',
+    py_modules=['pester_114294445'],
+    author='ZhiquanQin',
+    author_email='pinballwizard1898@yahoo.com',
+    description='A simple for me to learn how to upload',
+    )
