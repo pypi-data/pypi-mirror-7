db9db1512be52e96100d04df5f6e204eb33058c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:01 +0000

    Add package osuosl_hieroglyph_theme/0.8/osuosl_hieroglyph_theme-0.8.tar.gz

---
+++ packages/osuosl_hieroglyph_theme/osuosl_hieroglyph_theme-0.8.tar.gz/osuosl_hieroglyph_theme-0.8/setup.py
@@ -0,0 +1,24 @@
+#from distutils.core import setup
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+        long_description = f.read()
+
+setup(
+    zip_safe=False,
+    install_requires=[
+            "sphinx>=1.1",
+            "hieroglyph>=0.6",
+            ],
+    name="osuosl_hieroglyph_theme",
+    version=0.8,
+    author="Lucy Wyman",
+    author_email="lucyw@osuosl.org",
+    description="Slides theme for  the OSU Open Source Lab",
+    long_description=long_description,
+    packages=['osuosl_hieroglyph_theme'],
+    include_package_data=True,
+    keywords="hieroglyph extension theme",
+    url="https://github.com/osuosl/osuosl-hieroglyph-theme",
+)
+
