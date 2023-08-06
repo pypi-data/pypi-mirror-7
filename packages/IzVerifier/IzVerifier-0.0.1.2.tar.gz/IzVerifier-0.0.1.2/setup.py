7171db388e97c71e69d1bf7f1d23cb7162666dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:33 +0000

    Add package IzVerifier/0.0.1.2/IzVerifier-0.0.1.2.tar.gz

---
+++ packages/IzVerifier/IzVerifier-0.0.1.2.tar.gz/IzVerifier-0.0.1.2/setup.py
@@ -0,0 +1,23 @@
+__author__ = 'fcanas'
+
+from distutils.core import setup
+
+setup(
+        name='IzVerifier',
+        version='0.0.1.2',
+        author='Francisco Canas',
+        author_email='mailfrancisco@gmail.com',
+        packages=[
+            'IzVerifier',
+            'IzVerifier.exceptions',
+            'IzVerifier.izspecs',
+            'IzVerifier.test'
+        ],
+        url='https://github.com/FranciscoCanas/IzVerifier',
+        license='LICENSE.txt',
+        description='Static spec file  verification for IzPack installers.',
+        long_description=open('README.txt').read(),
+        install_requires=[
+                "beautifulsoup4 >= 4.3.2"
+            ]
+        )
