1e680ffb1889ce70b6567bea125a79b3a30472dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package django-debug-toolbar-template-timings/0.6.4/django-debug-toolbar-template-timings-0.6.4.zip

---
+++ packages/django-debug-toolbar-template-timings/django-debug-toolbar-template-timings-0.6.4.zip/django-debug-toolbar-template-timings-0.6.4/setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+import os
+
+license = ""
+
+if os.path.isfile("LICENSE"):
+    with open('LICENSE') as f:
+        license = f.read()
+
+readme = ""
+
+if os.path.isfile("README.rst"):
+    with open("README.rst") as f:
+        readme = f.read()
+
+
+setup(
+    zip_safe=False,
+    name='django-debug-toolbar-template-timings',
+    version='0.6.4',
+    packages=['template_timings_panel', 'template_timings_panel.panels'],
+    package_data={'': ['templates/*']},
+    url='https://github.com/orf/django-debug-toolbar-template-timings',
+    license=license,
+    author='Tom Forbes',
+    author_email='tom@tomforb.es',
+    description='A django-debug-toolbar panel that shows you template rendering times for Django',
+    install_requires=['Django', 'django-debug-toolbar>=1.0'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Debuggers'],
+
+    long_description=readme,
+)
