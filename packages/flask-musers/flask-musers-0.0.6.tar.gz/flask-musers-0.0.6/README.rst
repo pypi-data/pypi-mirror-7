9a02dd862aac4b464fd5ec92dd5966ed114b4e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:30 +0000

    Add package flask-musers/0.0.6/flask-musers-0.0.6.tar.gz

---
+++ packages/flask-musers/flask-musers-0.0.6.tar.gz/flask-musers-0.0.6/README.rst
@@ -0,0 +1,47 @@
+===============================
+Flask mongo users
+===============================
+
+.. image:: https://badge.fury.io/py/flask-musers.png
+    :target: http://badge.fury.io/py/flask-musers
+
+.. image:: https://travis-ci.org/s-m-i-t-a/flask-musers.png?branch=master
+        :target: https://travis-ci.org/s-m-i-t-a/flask-musers
+
+.. image:: https://coveralls.io/repos/s-m-i-t-a/flask-musers/badge.png
+        :target: https://coveralls.io/r/s-m-i-t-a/flask-musers
+
+
+Flask app for store user in MongoDB and simple views for login, logout and registration.
+
+* Free software: BSD license
+
+Quickstart
+----------
+Install flask-musers::
+
+    pip install flask-musers
+
+In the app you must init the ``MUsers``::
+
+    from flask import Flask
+    from flask.ext.musers import MUsers
+
+    app = Flask(__name__)
+    MUsers(app)
+
+and register ``flask-musers`` blueprint::
+
+    from flask_musers.views import musers
+
+    app.register_blueprint(musers)
+
+then must be prepared templates for registration - ``musers/register.html`` and login - ``musers/login.html``.
+
+Views has names ``musers.register``, ``musers.login`` and ``musers.logout``. They are used in ``url_for``::
+
+    from flask import url_for
+
+    url_for('musers.register')
+    url_for('musers.login')
+    url_for('musers.logout')
---
+++ packages/flask-musers/flask-musers-0.0.6.tar.gz/flask-musers-0.0.6/flask_musers/models.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from mongoengine import Document, EmailField, StringField, BooleanField, queryset_manager
+
+from passlib.hash import pbkdf2_sha256
+from passlib.utils import consteq
+
+
+class UserError(Exception):
+    pass
+
+
+class User(Document):
+    email = EmailField(required=True, unique=True)
+    _password = StringField()
+    name = StringField()
+    activated = BooleanField(default=False)
+
+    @queryset_manager
+    def active(doc_cls, queryset):
+        return queryset.filter(activated=True)
+
+    def __str__(self):
+        return self.email
+
+    def __repr__(self):
+        return str(self)
+
+    # def get_absolute_url(self):
+    #     pass
+
+    def set_password(self, password):
+        self._password = pbkdf2_sha256.encrypt(password)
+
+    def check_password(self, password):
+        return pbkdf2_sha256.verify(password, self._password)
+
+    def is_active(self):
+        return self.activated
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        #if not hasattr(self, '_authenticated'):
+            #self._authenticated = False
+
+        #return self._authenticated
+        return True
+
+    def get_id(self):
+        return str(self.pk) if self.pk else None
+
+    @classmethod
+    def register(cls, email, password, activated=False, name=''):
+        user = cls()
+        user.email = email
+        user.name = name
+        user.activated = activated
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    @classmethod
+    def get_user(cls, email, password):
+        try:
+            user = cls.active.get(email=email)
+            if not user.check_password(password):
+                raise UserError()
+        except cls.DoesNotExist:
+            raise UserError()
+        return user
+
+    @classmethod
+    def get_active_user_by_pk_or_none(cls, userid):
+        try:
+            user = cls.active.get(pk=userid)
+        except cls.DoesNotExist:
+            user = None
+        return user
