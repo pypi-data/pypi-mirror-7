8dd8e64a75d4295344f3809b8e0ca04aea1f7cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package cielo24/1.0.5/cielo24-1.0.5.zip

---
+++ packages/cielo24/cielo24-1.0.5.zip/cielo24-1.0.5/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+# Script for PyPI
+
+setup(name='cielo24',
+      version='1.0.5',
+      description='Package for interacting with the cielo24 public REST API',
+      author='cielo24',
+      author_email='support@cielo24.com',
+      url='http://www.cielo24.com',
+      packages=['cielo24']
+     )
