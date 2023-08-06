72b59356b3bf2e5044cf73fea6704682a70f54b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:46 +0000

    Add package uwsgi-sloth/2.0.1/uwsgi-sloth-2.0.1.tar.gz

---
+++ packages/uwsgi-sloth/uwsgi-sloth-2.0.1.tar.gz/uwsgi-sloth-2.0.1/uwsgi_sloth/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__VERSION__ = [2, 0, 0]
+
+# Init logger
+import logging
+import logging.handlers
+logger = logging.getLogger('uwsgi_sloth')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+handler.setLevel(logging.DEBUG)
+
+import uwsgi_sloth.compat
