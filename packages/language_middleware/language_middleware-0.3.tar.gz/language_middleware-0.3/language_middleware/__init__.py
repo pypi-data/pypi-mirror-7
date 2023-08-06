ba7656cf749def8ee31ad6fe1f1e1368c95dbe46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:56 +0000

    Add package language_middleware/0.3/language_middleware-0.3.tar.gz

---
+++ packages/language_middleware/language_middleware-0.3.tar.gz/language_middleware-0.3/language_middleware/__init__.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import gettext
+import os
+import re
+
+
+class LanguageMiddleware(object):
+    def __init__(
+        self,
+        app,
+        default_language='en',
+        valid_languages=('en', 'es'),
+        force_lang=False,
+        clean_url=True,
+        locale_path=None,
+        locale_name=None
+    ):
+        if default_language not in valid_languages:
+            raise Exception("Default language must be contained in valid languages")
+
+        self.app = app
+        self.default_language = default_language
+        self.valid_languages = valid_languages
+        self.force_lang = force_lang
+        self.clean_url = clean_url
+        self.locale_path = locale_path
+        self.locale_name = locale_name
+
+    def _validate_language(self, language):
+        if language and language in self.valid_languages:
+            return language
+
+    def _get_url_language(self, environ):
+        could_have_language = environ.get('PATH_INFO', '')[0:4]
+        if len(could_have_language) == 3:
+            could_have_language += "/"
+        language = re.findall('\/([a-z]{2,2})\/', could_have_language)
+        if len(language) > 0:
+            if self.clean_url:
+                environ['PATH_INFO'] = environ['PATH_INFO'][3:]  # Remove language from url
+            return self._validate_language(language[0])
+
+    def _get_headers_language(self, environ):
+        accept_language = environ.get("HTTP_ACCEPT_LANGUAGE", "")
+        if accept_language:
+            accept_language = accept_language[0:2]
+            if self._validate_language(accept_language):
+                return accept_language
+
+    def __call__(self, environ, start_response):
+        url_language = self._get_url_language(environ)
+        if url_language and self._validate_language(url_language):
+            environ['HTTP_ACTIVE_LANGUAGE'] = url_language
+        else:
+            headers_language = self._get_headers_language(environ)
+            if headers_language and self._validate_language(headers_language) and not self.force_lang:
+                environ['HTTP_ACTIVE_LANGUAGE'] = headers_language
+            else:
+                environ['HTTP_ACTIVE_LANGUAGE'] = self.default_language
+        if self.locale_path and self.locale_name:
+            if os.path.exists(os.path.join(self.locale_path, environ['HTTP_ACTIVE_LANGUAGE'], "LC_MESSAGES")):
+                translation = gettext.translation(
+                    self.locale_name,
+                    self.locale_path,
+                    languages=[environ['HTTP_ACTIVE_LANGUAGE']],
+                    codeset="utf-8"
+                )
+                translation.install(unicode=True)
+        return self.app(environ, start_response)
---
+++ packages/language_middleware/language_middleware-0.3.tar.gz/language_middleware-0.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = '0.3'
+
+setup(
+      name='language_middleware',
+      version=version,
+      description="This Wsgi middleware, takes the language from urls similiar to '/es/documents' or from the Accept language header.",
+      long_description=read('README'),
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+      ],
+      keywords='wsgi middleware language i18n translation',
+      author='Daniel Perez Rada',
+      author_email='daniel at etiqs com',
+      url='https://github.com/dperezrada/wsgi-language-middleware',
+      license='GNU Public License v3.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      tests_require=['nose >= 0.11', 'mock', 'mako'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      test_suite="tests",
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
