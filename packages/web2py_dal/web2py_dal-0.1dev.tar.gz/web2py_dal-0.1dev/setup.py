2273962bc7058f9b2bfe877ffd29e5956175bf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:08 +0000

    Add package web2py_dal/0.1dev/web2py_dal-0.1dev.tar.gz

---
+++ packages/web2py_dal/web2py_dal-0.1dev.tar.gz/web2py_dal-0.1dev/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='web2py_dal',
+    version='0.1dev',
+    packages=['w2pdal',],
+    license='LGPLv3 (http://www.gnu.org/licenses/lgpl.html)',
+)
