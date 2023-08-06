217a56ad8260a5dcd3b18cc34e1d393ecb5568b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:11 +0000

    Add package crawl-me/0.1.4dev-20140611/crawl-me-0.1.4dev-20140611.tar.gz

---
+++ packages/crawl-me/crawl-me-0.1.4dev-20140611.tar.gz/crawl-me-0.1.4dev-20140611/crawl_me/common/crawlercore.py
@@ -0,0 +1,102 @@
+import time
+
+from rangedownloader import RangeDownloader
+from utils import *
+from ..sysconf import *
+
+
+class PictureThread(threading.Thread):
+    def __init__(self,
+                 startIndex,
+                 splitNum,
+                 opener,
+                 urlList,
+                 savePath,
+                 useRangeHeaders,
+                 downloadLock,
+                 handlerName):
+        threading.Thread.__init__(self)
+        self.startIndex = startIndex
+        self.urlList = urlList
+        self.opener = opener
+        self.splitNum = splitNum
+        self.name = handlerName
+        self.savePath = savePath
+        self.useRangeHeaders = useRangeHeaders
+        self.downloadLock = downloadLock
+
+    def run(self):
+        syslog(self.getName() + " picture thread start!", LOG_INFO)
+        pictureSize = len(self.urlList)
+        maxIndex = self.startIndex + self.splitNum - 1
+        if maxIndex > pictureSize:
+            maxIndex = pictureSize
+
+        for index in range(self.startIndex - 1, maxIndex):
+            self.downloadLock.acquire()
+            self.__crawl(self.urlList[index], index)
+            self.downloadLock.release()
+
+    def __crawl(self, url, picIndex):
+        syslog("downloading " + url, LOG_INFO)
+        savePath = self.savePath + str(picIndex) + '.jpg'
+        if self.useRangeHeaders:
+            rd = RangeDownloader(self.opener)
+            rd.rangeDownload(url, savePath)
+        else:
+            content = urlReadWithRetry(self.opener, url)
+            if content == None:
+                syslog("error! retry too many times, url=" % (url), LOG_ERROR)
+                return
+            file = open(savePath, "wb")
+            file.write(content)
+            file.close()
+
+
+class CrawlerManager(object):
+    # conf.url and conf.savePath are required
+    def __init__(self, opener, urlList, savePath, useRangeHeaders, maxDownloadCount=MAX_DOWNLOAD_COUNT):
+        self.savePath = getPathWithSep(savePath)
+        self.useRangeHeaders = useRangeHeaders
+        self.opener = opener
+        self.urlList = urlList
+        self.downloadLock = threading.Semaphore(maxDownloadCount)
+        createDir(self.savePath)
+
+    def startCrawl(self):
+        beginTime = time.time()
+        self.PictureThreadList = list()
+        index = 1
+        count = 1
+        picNum = len(self.urlList)
+        splitNum = getShardingConf(picNum)[0]
+
+        while index <= picNum:
+            picThread = PictureThread(
+                index,
+                splitNum,
+                self.opener,
+                self.urlList,
+                self.savePath,
+                self.useRangeHeaders,
+                self.downloadLock,
+                "pictureThread:" + str(count))
+            self.PictureThreadList.append(picThread)
+            picThread.start()
+            index += splitNum
+            count += 1
+
+        for th in self.PictureThreadList:
+            th.join()
+
+        endTime = time.time()
+        cost = round(endTime - beginTime, 1)
+        average = cost / picNum
+        syslog("download complete!", LOG_INFO)
+        syslog('crawl ' + str(picNum) + " imgs", LOG_INFO)
+        syslog('total cost: ' + str(cost) + 's', LOG_INFO)
+        syslog('average ' + str(average) + 's per image', LOG_INFO)
+
+    def stopCrawl(self):
+        #TODO not surported yet
+        pass
---
+++ packages/crawl-me/crawl-me-0.1.4dev-20140611.tar.gz/crawl-me-0.1.4dev-20140611/crawl_me/plugin/gamersky.py
@@ -0,0 +1,58 @@
+from pyquery import PyQuery as pq
+
+from pageBasedHandler import *
+from ..common.utils import *
+
+
+class GamerskyHandler(PageBasedHandler):
+    # def getPageUrl(self, baseUrl, opener, beginPage, endPage):
+    def getPageUrl(self, opener, paraConf):
+        urlList = list()
+        for page in range(paraConf["beginPage"], paraConf["endPage"] + 1):
+            if page == 1:
+                crawlUrl = paraConf["url"]
+            else:
+                crawlUrl = paraConf["url"][0: paraConf["url"].index('.shtml')] + "_" + str(page) + ".shtml"
+            urlList.append(crawlUrl)
+        return urlList
+
+    def getPictureUrl(self, pageUrl, opener):
+        urlList = list()
+        htmlContent = urlReadWithRetry(opener, pageUrl)
+        if htmlContent == None:
+            syslog("GameskyPicIterator init fail at url=" + pageUrl, LOG_ERROR)
+        q = pq(htmlContent)
+        imgP = q('p')
+        for i in range(0, imgP.size()):
+            try:
+                #check if the sub element has a href attribute
+                href = imgP.eq(i).find('a').attr('href')
+                if (href == None):
+                    continue
+                #check postfix 
+                imgUrl = href.split('?')[1]
+                urlList.append(imgUrl)
+                syslog("imgurl find, url=%s" % (imgUrl))
+            except Exception, e:
+                syslog("Exception:" + Exception + ", " + e, LOG_ERROR)
+                continue
+
+        return urlList
+
+    def initOpener(self, conf):
+        self.opener = urllib2.build_opener()
+        return self.opener
+
+    def initPara(self, parser):
+        parser.add_argument('url', help='your url to crawl')
+        parser.add_argument('savePath', help='the path where the imgs ars saved')
+        parser.add_argument('beginPage', help='the page where we start crawling', type=int)
+        parser.add_argument('endPage', help='the page where we end crawling', type=int)
+        args = parser.parse_args()
+        return {
+            "url": args.url,
+            "savePath": args.savePath,
+            "beginPage": args.beginPage,
+            "endPage": args.endPage,
+            "useRangeHeaders": True
+        }
