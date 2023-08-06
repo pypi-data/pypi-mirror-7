2bb41bbadd64c645b48915e8363984a2ebc5f2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package F5NoMore/0.0.2/F5NoMore-0.0.2.zip

---
+++ packages/F5NoMore/F5NoMore-0.0.2.zip/F5NoMore-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='F5NoMore',
+    version='0.0.2',
+    author='Alex Rodrigues',
+    author_email='alex@rodrigues.com',
+    packages=['f5nomore'],
+    license='LICENSE.txt',
+    description='Autoreload webpage on file system changes',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "watchdog>= 0.7.0< 0.8.0"
+    ]
+)
