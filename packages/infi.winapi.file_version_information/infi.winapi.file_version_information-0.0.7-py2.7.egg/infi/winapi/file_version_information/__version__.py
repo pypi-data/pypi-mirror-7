9859dddcc4620deedcf4d84628f31e88358b601d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package infi.winapi.file_version_information/0.0.7/infi.winapi.file_version_information-0.0.7-py2.7.egg

---
+++ packages/infi.winapi.file_version_information/infi.winapi.file_version_information-0.0.7-py2.7.egg/infi/winapi/file_version_information/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '149096bb05dfa8e2af29c3aeda0ba95e9651433d'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
