f07f6994adfe1f63303ed94d5a3ac25e492aa023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package djangorecipebook/1.1/djangorecipebook-1.1-py2.7.egg

---
+++ packages/djangorecipebook/djangorecipebook-1.1-py2.7.egg/djangorecipebook/scripts/manage.py
@@ -0,0 +1,30 @@
+"""
+Recipe generating a management script
+"""
+
+import sys
+
+
+def manage_main(settings_module, command, *args):
+    from django.core import management
+    settings = []
+    for arg in sys.argv:
+        if arg.startswith('--settings='):
+            break
+    else:
+        settings = ['--settings=%s' % settings_module]
+
+    if command:
+        command = [command]
+    else:
+        command = []
+
+    # the arguments need to be inserted in sys.argv as subsequent packages
+    # (e.g. nose) fetch them directly from it and would ignore
+    sys.argv[1:1] = args
+    management.execute_from_command_line(['manage.py'] + command +
+                                         settings + sys.argv[1:])
+
+
+def main(settings_module, *args):
+    manage_main(settings_module, None, *args)
