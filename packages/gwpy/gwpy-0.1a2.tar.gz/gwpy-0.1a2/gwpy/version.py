d94a5c51be61096d46ac4f68761c679341bfb4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:15 +0000

    Add package gwpy/0.1a2/gwpy-0.1a2.tar.gz

---
+++ packages/gwpy/gwpy-0.1a2.tar.gz/gwpy-0.1a2/gwpy/version.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# Copyright (C) Duncan Macleod (2013)
+
+"""Versioning record for gwpy
+"""
+
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+__version__ = '0.1a2'
+__date__ = '2014-06-04 01:45:27 +0000'
+
+version = '0.1a2'
+git_id = '701776d858a016acbbc5173bdd49ae823e976d7e'
+git_branch = 'master'
+git_tag = 'v0.1a2'
+git_author = 'Duncan Macleod <duncan.macleod@ligo.org>'
+git_committer = 'Duncan Macleod <duncan.macleod@ligo.org>'
+git_status = 'UNCLEAN: Modified working tree'
