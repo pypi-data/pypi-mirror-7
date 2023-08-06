a006d6f0f65b80a725ee3f72b8a636bfeb5c11e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:39 +0000

    Add package PyOrgMode/0.03c/PyOrgMode-0.03c.tar.gz

---
+++ packages/PyOrgMode/PyOrgMode-0.03c.tar.gz/PyOrgMode-0.03c/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyOrgMode',
+    packages = ['PyOrgMode'], # this must be the same as the name above
+    version = '0.03c',
+    description = 'Library to interact with org-mode from python',
+    author = 'Jonathan BISSON',
+    author_email = 'gonvaled@gonvaled.com', # I do not know the email address of the author
+    url = 'https://github.com/gonvaled/PyOrgMode', # use the URL to the github repo
+    download_url = 'https://github.com/gonvaled/PyOrgMode/tarball/0.03b', # I'll explain this in a second
+    keywords = ['org-mode'],
+    classifiers = [],
+)
