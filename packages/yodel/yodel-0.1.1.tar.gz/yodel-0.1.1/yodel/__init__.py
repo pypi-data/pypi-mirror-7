951bbd5e6e29b3f8a96b78467ad8ccf2d2ef2638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package yodel/0.1.1/yodel-0.1.1.tar.gz

---
+++ packages/yodel/yodel-0.1.1.tar.gz/yodel-0.1.1/yodel/__init__.py
@@ -0,0 +1,13 @@
+"""
+**yodel** (*the Swiss Army knife of your sound*) is an easy-to-use python
+package for digital audio signal processing, analysis and synthesis.
+"""
+
+__author__ = 'Romain Clement'
+__copyright__ = 'Copyright 2014, Romain Clement'
+__credits__ = []
+__license__ = 'MIT'
+__version__ = '0.1.1'
+__maintainer__ = 'Romain Clement'
+__email__ = 'contact@romainclement.com'
+__status__ = 'Development'
