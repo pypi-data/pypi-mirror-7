96c24c4bf0e0764bee6539d16882fecb3df0b2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:00 +0000

    Add package schoolbus/0.3.6/schoolbus-0.3.6.tar.gz

---
+++ packages/schoolbus/schoolbus-0.3.6.tar.gz/schoolbus-0.3.6/schoolbus/research_orgs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+RESEARCH_ORG_DOMAINS = {
+    'niper.ac.in': [u'National Institute of Pharmaceutical Education and Research'],
+    'nyumc.org': [u'NYU Langone Medical Center'],
+    'embl.de': [u'European Molecular Biology Laboratory'],
+    'erasmusmc.nl': [u'Erasmus University Medical Center'],
+    'ncbs.res.in': [u'National Centre for Biological Sciences'],
+    'bccrc.ca': [u'BC Cancer Research Centre'],
+    'mpimf-heidelberg.mpg.de': [u'Max Planck Institute for Medical Research'],
+    'mpg.de': [u'Max Planck Institute'],
+    'dkfz-heidelberg.de': [u'German Cancer Research Center'],
+}
