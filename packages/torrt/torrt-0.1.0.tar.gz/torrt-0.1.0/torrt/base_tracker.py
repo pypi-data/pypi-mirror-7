28c5f8ebf501d04177732f868d9bcf0def4fd285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:12 +0000

    Add package torrt/0.1.0/torrt-0.1.0.tar.gz

---
+++ packages/torrt/torrt-0.1.0.tar.gz/torrt-0.1.0/torrt/base_tracker.py
@@ -0,0 +1,319 @@
+import re
+import logging
+from urlparse import urlparse, urljoin, parse_qs
+
+import requests
+from bs4 import BeautifulSoup
+
+from torrt.utils import parse_torrent, WithSettings, TrackerObjectsRegistry, TorrtException
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class BaseTracker(WithSettings):
+    """Base torrent tracker handler class offering helper methods for its ancestors."""
+
+    alias = None
+    config_entry_name = 'trackers'
+
+    def register(self):
+        """Adds this object into TrackerObjectsRegistry.
+
+        :return:
+        """
+        TrackerObjectsRegistry.add(self)
+
+    @classmethod
+    def get_response(cls, url, form_data=None, allow_redirects=True, referer=None, cookies=None, query_string=None, as_soup=False):
+        """Returns an HTTP resource object from given URL.
+
+        If a dictionary is passed in `form_data` POST HTTP method
+        would be used to pass data to resource (even if that dictionary is empty).
+
+        :param url: str - URL to get data from
+        :param form_data: dict - data for POST
+        :param allow_redirects: bool - whether to follow server redirects
+        :param referer: str or None - data to put into Referer header
+        :param cookies: dict or None - cookies to use
+        :param query_string: str or None - query string (GET parameters) to add to URL
+        :param as_soup: bool - whether to return BeautifulSoup object instead of Requests response
+        :return: object
+        :rtype: Response or BeautifulSoup
+        """
+        if query_string is not None:
+            delim = '?'
+            if '?' in url:
+                delim = '&'
+            url = '%s%s%s' % (url, delim, query_string)
+
+        LOGGER.debug('Fetching %s ...' % url)
+
+        headers = {'User-agent': 'Mozilla/5.0 (Ubuntu; X11; Linux i686; rv:8.0) Gecko/20100'}
+
+        if referer is not None:
+            headers['Referer'] = referer
+
+        r_kwargs = {
+            'allow_redirects': allow_redirects,
+            'headers': headers
+        }
+
+        if cookies is not None:
+            r_kwargs['cookies'] = cookies
+
+        if form_data is not None:
+            method = lambda: requests.post(url, data=form_data, **r_kwargs)
+        else:
+            method = lambda: requests.get(url, **r_kwargs)
+
+        try:
+            result = method()
+            if as_soup:
+                result = cls.make_page_soup(result.text)
+            return result
+        except requests.exceptions.RequestException as e:
+            LOGGER.error('Failed to get resource from `%s`: %s' % (url, e.message))
+            return None
+
+    @classmethod
+    def make_page_soup(cls, html):
+        """Returns BeautifulSoup object from a html.
+
+        :param html: str
+        :return: object
+        :rtype: BeautifulSoup
+        """
+        return BeautifulSoup(html)
+
+    @classmethod
+    def find_links(cls, url, page_soup, definite=None):
+        """Returns a list with hyperlinks found in supplied page_soup
+        or a definite link.
+
+        :param url: str - page URL
+        :param page_soup: BeautifulSoup - page soup
+        :param definite: str - regular expression to match link
+        :return: list or str
+        :rtype: list or str
+        """
+        if definite is not None:
+            link = page_soup.find(href=re.compile(definite))
+            if link:
+                return link.get('href')
+            return link
+        else:
+            links = []
+            for link in page_soup.find_all('a'):
+                l = link.get('href')
+                if l:
+                    links.append(cls.expand_link(url, l))
+            return links
+
+    @classmethod
+    def expand_link(cls, base_url, link):
+        """Expands a given relative link using base URL if required.
+
+        :param base_url: str
+        :param link: str - absolute or relative link
+        :return: str
+        :rtype: str
+        """
+        if not link.startswith('http'):
+            link = urljoin(base_url, link)
+        return link
+
+    def test_configuration(self):
+        """This should implement a configuration test, e.g. make test login and report success.
+
+        :return: bool
+        """
+        return True
+
+    def get_torrent(self, url):
+        """This method should be implemented in torrent tracker handler class
+        and must return .torrent file contents.
+
+        :param url: str - URL to download torrent file from
+        :return: str - torrent file contents
+        :rtype: str
+        """
+        raise NotImplementedError('`%s` class should implement `get_torrent_file()` method' % self.__class__.__name__)
+
+
+class GenericTracker(BaseTracker):
+    """Generic torrent tracker handler class implementing most common tracker handling methods."""
+
+    def get_id_from_link(self, url):
+        """Returns forum thread identifier from full thread URL.
+
+        :param url: str
+        :return: str
+        :rtype: str
+        """
+        return url.split('=')[1]
+
+    def get_torrent(self, url):
+        """This is the main method which returns torrent file contents
+        of file located at URL.
+
+        :param url: str - URL to find and get torrent from
+        :return: str or Nonr - torrent file contents
+        :rtype: str or None
+        """
+        torrent_data = None
+        download_link = self.get_download_link(url)
+        if download_link is None:
+            LOGGER.error('Cannot find torrent file download link at %s' % url)
+        else:
+            LOGGER.debug('Torrent download link found: %s' % download_link)
+            torrent_data = self.download_torrent(download_link, referer=url)
+            if torrent_data is None:
+                LOGGER.debug('Torrent download from `%s` has failed' % download_link)
+            else:
+                torrent_data = parse_torrent(torrent_data)
+        return torrent_data
+
+    def get_download_link(self, url):
+        """Tries to find .torrent file download link on page and return it.
+
+        :param url: str - URL to find a download link at.
+        :return: str or None
+        :rtype: str or None
+        """
+        raise NotImplementedError('`%s` class should implement `get_download_link()` method' % self.__class__.__name__)
+
+    def download_torrent(self, url, referer=None):
+        """Returns .torrent file contents from the given URL.
+
+        :param url: str - torrent file URL
+        :param referer: str or None - Referer header value
+        :return: str or None
+        :rtype: str or None
+        """
+        raise NotImplementedError('`%s` class should implement `download_torrent()` method' % self.__class__.__name__)
+
+
+class GenericPublicTracker(GenericTracker):
+    """Generic torrent tracker handler class implementing most common handling methods for public trackers."""
+
+    login_required = False
+
+    def get_id_from_link(self, url):
+        return url.split('/')[-1]
+
+    def download_torrent(self, url, referer=None):
+        LOGGER.debug('Downloading torrent file from %s ...' % url)
+        # That was a check that user himself visited torrent's page ;)
+        response = self.get_response(url, referer=referer)
+        if response is None:
+            return None
+        return response.content
+
+
+class GenericPrivateTracker(GenericPublicTracker):
+    """Generic torrent tracker handler class implementing most common handling methods
+    for private trackers (that require user registration).
+
+    """
+
+    login_required = True
+    login_url = None
+
+    # Cookie name to verify that a log in was successful.
+    auth_cookie_name = None
+    # HTTP GET (query string) parameter name to verify that a log in was successful. Probably session ID.
+    auth_qs_param_name = None
+
+    def __init__(self, username=None, password=None, cookies=None, query_string=None):
+        self.logged_in = False
+        # Stores a number of login attempts to prevent recursion.
+        self.login_counter = 0
+        self.username = username
+        self.password = password
+        if cookies is None:
+            cookies = {}
+        self.cookies = cookies
+        self.query_string = query_string
+
+    def get_login_form_data(self, login, password):
+        """Should return a dictionary with data to be pushed to authorization form.
+
+        :param login:
+        :param password:
+        :return: dict
+        :rtype: dict
+        """
+        return {'username': login, 'password': password}
+
+    def test_configuration(self):
+        return self.login()
+
+    def login(self):
+        """Implements tracker login procedure.
+        Returns success bool.
+
+        :return: bool
+        :rtype: bool
+        """
+        LOGGER.info('Trying to login at %s ...' % self.login_url)
+
+        if self.logged_in:
+            raise TorrtTrackerException('Consecutive login attempt detected at `%s`' % self.__class__.__name__)
+
+        if not self.username or self.password is None:
+            return False
+
+        self.login_counter += 1
+
+        # No recursion wanted.
+        if self.login_counter > 1:
+            return False
+
+        allow_redirects = False  # Not to loose cookies on the redirect.
+        if self.auth_qs_param_name:
+            allow_redirects = True  # To be able to get Session ID from query string.
+
+        response = self.get_response(self.login_url, self.get_login_form_data(self.username, self.password), allow_redirects=allow_redirects, cookies=self.cookies)
+
+        # Login success checks.
+        parsed_qs = parse_qs(urlparse(response.url).query)
+        if self.auth_cookie_name in response.cookies or self.auth_qs_param_name in parsed_qs:
+            self.logged_in = True
+
+            if parsed_qs:
+                self.query_string = parsed_qs[self.auth_qs_param_name][0]
+            self.cookies = response.cookies
+
+            # Save auth info to config.
+            self.save_settings()
+            LOGGER.info('Login is successful')
+        else:
+            LOGGER.warning('Login with given credentials failed')
+
+        return self.logged_in
+
+    def before_download(self, url):
+        """Used to perform some required actions right before .torrent download.
+        E.g.: to set a sentinel cookie that allows the download.
+
+        :param url: str - torrent file URL
+        :return:
+        """
+        return None
+
+    def download_torrent(self, url, referer=None):
+        LOGGER.debug('Downloading torrent file from %s ...' % url)
+        self.before_download(url)
+        query_string = None
+        if self.auth_qs_param_name:
+            query_string = '%s=%s' % (self.auth_qs_param_name, self.query_string)
+        response = self.get_response(url, cookies=self.cookies, query_string=query_string, referer=referer)
+        if response is None:
+            return None
+        return response.content
+
+
+class TorrtTrackerException(TorrtException):
+    """Base torrt tracker exception. All other tracker related exception should inherit from that."""
+
---
+++ packages/torrt/torrt-0.1.0.tar.gz/torrt-0.1.0/torrt/utils.py
@@ -0,0 +1,322 @@
+import re
+import os
+import json
+import logging
+from datetime import datetime
+from collections import Mapping
+from pkgutil import iter_modules
+from inspect import getargspec
+
+import libtorrent as lt
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+# This regex is used to get hyperlink from torrent comment.
+RE_LINK = re.compile(r'(?P<url>https?://[^\s]+)')
+
+
+def import_classes():
+    """Dynamically imports RPC classes and tracker handlers from their directories.
+
+    :return:
+    """
+    LOGGER.debug('Importing RPC classes ...')
+    import_from_path('rpc')
+
+    LOGGER.debug('Importing Tracker classes ...')
+    import_from_path('trackers')
+
+
+def import_from_path(path):
+    """Dynamically imports modules from package.
+    It is an .egg-friendly alternative to os.listdir() walking.
+
+    :param path: str - path under torrt
+    :return:
+    """
+    for mloader, pname, ispkg in iter_modules([os.path.join(os.path.dirname(__file__), path)]):
+        __import__('torrt.%s.%s' % (path, pname))
+
+
+def parse_torrent(torrent):
+    """Returns a dictionary with basic information from torrent contents.
+
+    :param torrent:
+    :return: torrent info dict - keys: hash; name; files; torrent (torrent file contents just from input).
+    :rtype: dict
+    """
+    torrent_info = lt.torrent_info(lt.bdecode(torrent))
+    files_from_torrent = [a_file.path.decode('utf-8') for a_file in torrent_info.files()]
+    info = {'hash': str(torrent_info.info_hash()), 'name': str(torrent_info.name()), 'files': files_from_torrent, 'torrent': torrent}
+    return info
+
+
+def parse_torrent_file(filepath):
+    """Reads a torrent file from filesystem and returns information about it.
+
+    :param filepath: str
+    :return: file contents
+    :rtype: str
+    """
+    with open(filepath, 'rb') as f:
+        contents = f.read()
+    return parse_torrent(contents)
+
+
+def get_url_from_string(string):
+    """Returns URL from a string, e.g. torrent comment.
+
+    :param string:
+    :return: url
+    :rtype: str
+    """
+    return RE_LINK.search(string).group('url')
+
+
+def get_iso_from_timestamp(ts):
+    """Get ISO formatted string from timestamp.
+
+    :param ts: int - timestamp
+    :return: string
+    :rtype: str
+    """
+    return datetime.fromtimestamp(ts).isoformat(' ')
+
+
+def update_dict(old_dict, new_dict):
+    """Updates old dictionary with data from a new one with respect to existing values.
+
+    :param old_dict:
+    :param new_dict:
+    :return: updated dict
+    :rtype: dict
+    """
+    for key, val in new_dict.iteritems():
+        if isinstance(val, Mapping):
+            old_dict[key] = update_dict(old_dict.get(key, {}), val)
+        else:
+            old_dict[key] = new_dict[key]
+    return old_dict
+
+
+def structure_torrent_data(target_dict, hash, data):
+    """Updated target dict with torrent data structured suitably
+    for config storage.
+
+    :param target_dict: dict - dictionary to update
+    :param hash: str - torrent identifying hash
+    :param data: dict - torrent data recieved from RPC (see parse_torrent())
+    :return:
+    """
+    data = dict(data)
+
+    if 'hash' not in data:
+        data['hash'] = hash
+
+    if 'name' not in data:
+        data['name'] = None
+
+    target_dict[hash] = {
+        'hash': data['hash'],
+        'name': data['name']
+    }
+
+
+def get_torrent_from_url(url):
+    """Downloads torrent from a given URL and returns it as string.
+
+    :param url: str or None
+    :return: torrent contents
+    :rtype: str
+    """
+    LOGGER.info('Downloading torrent file from `%s` ...' % url)
+
+    tracker = TrackerObjectsRegistry.get_for_string(url)
+    if tracker:
+        result = tracker.get_torrent(url)
+        if result is None:
+            LOGGER.info('Unable to get torrent from `%s`' % url)
+        else:
+            LOGGER.info('Torrent was downloaded from `%s`' % url)
+            return result
+    else:
+        LOGGER.warning('Tracker handler for `%s` is not registered' % url)
+    return None
+
+
+def iter_rpc():
+    """Generator to iterate through available and enable RPC objects.
+
+    :return: tuple - rpc_alias, rpc_object
+    :rtype: tuple
+    """
+    rpc_objects = RPCObjectsRegistry.get()
+    if not rpc_objects:
+        LOGGER.error('No RPC objects registered, unable to proceed')
+        raise StopIteration()
+
+    for rpc_alias, rpc_object in rpc_objects.items():
+        if not rpc_object.enabled:
+            LOGGER.info('RPC `%s` is disabled, skipped.' % rpc_object.alias)
+            continue
+
+        yield rpc_alias, rpc_object
+
+
+class WithSettings(object):
+    """Introduces settings support for class objects.
+
+    NB: * Settings names are taken from inheriting classes __init__() methods.
+        * __init__() method MUST use keyword arguments only.
+        * Inheriting classes MUST save settings under object properties with the same name as in __init__().
+
+    """
+
+    config_entry_name = None
+    settings = {}
+    alias = None
+
+    @classmethod
+    def spawn_with_settings(cls, settings):
+        """Spawns and returns object initialized with given settings.
+
+        :param settings:
+        :return: object
+        """
+        LOGGER.debug('Spawning `%s` object with the given settings ...' % cls.__name__)
+        return cls(**settings)
+
+    def save_settings(self):
+        """Saves object settings into torrt configuration file.
+
+        :return:
+        """
+        settings = {}
+
+        try:
+            settings_names = getargspec(self.__init__)[0]
+            del settings_names[0]  # do not need `self`
+            for name in settings_names:
+                settings[name] = getattr(self, name)
+        except TypeError:
+            pass  # Probably __init__ method is not user-defined.
+
+        TorrtConfig.update({self.config_entry_name: {self.alias: settings}})
+
+
+class TorrtConfig(object):
+    """Gives methods to work with torrt configuration file."""
+
+    USER_DATA_PATH = os.path.join(os.path.expanduser('~'), '.torrt')
+    USER_SETTINGS_FILE = os.path.join(USER_DATA_PATH, 'config.json')
+
+    @classmethod
+    def bootstrap(cls):
+        """Initializes configuration file if needed,
+
+        :return:
+        """
+        if not os.path.exists(cls.USER_DATA_PATH):
+            os.makedirs(cls.USER_DATA_PATH)
+
+        if not os.path.exists(cls.USER_SETTINGS_FILE):
+            basic_settings = {
+                'time_last_check': 0,
+                'walk_interval_hours': 1,
+                'rpc': {},
+                'trackers': {},
+                'torrents': {}
+            }
+            cls.save(basic_settings)
+
+        # My precious.
+        os.chmod(cls.USER_SETTINGS_FILE, 0600)
+
+    @classmethod
+    def update(cls, settings_dict):
+        """Updates configuration file with given settings.
+
+        :param settings_dict: dict
+        :return:
+        """
+        cls.save(update_dict(cls.load(), settings_dict))
+
+    @classmethod
+    def load(cls):
+        """Returns current settings dictionary.
+
+        :return: settings dict
+        :rtype: dict
+        """
+        LOGGER.debug('Loading configuration file %s ...' % cls.USER_SETTINGS_FILE)
+        cls.bootstrap()
+        with open(cls.USER_SETTINGS_FILE) as f:
+            return json.load(f)
+
+    @classmethod
+    def save(cls, settings_dict):
+        """Saves a given dict as torrt configuration.
+
+        :param settings_dict: dict
+        :return:
+        """
+        LOGGER.debug('Saving configuration file %s ...' % cls.USER_SETTINGS_FILE)
+        with open(cls.USER_SETTINGS_FILE, 'w') as f:
+            json.dump(settings_dict, f, indent=4)
+
+
+class ObjectsRegistry(object):
+
+    __slots__ = ['_items']
+
+    def __init__(self):
+        self._items = {}
+
+    def add(self, obj):
+        """Add an object to registry.
+
+        NB: object MUST have `alias` attribute.
+
+        :param obj: object
+        :return:
+        """
+        name = getattr(obj, 'alias')
+        LOGGER.debug('Registering `%s` from %s ...' % (name, obj))
+        self._items[name] = obj
+
+    def get(self, obj_alias=None):
+        """Returns registered objects or a definite object by its alias.
+
+        :param obj_alias: str or None
+        :return: dict or object
+        :rtype: dict or object
+        """
+        if obj_alias is None:
+            return self._items
+        return self._items.get(obj_alias)
+
+    def get_for_string(self, string):
+        """Returns registered object whose alias is found in a given string.
+
+        :param string: str
+        :return: object or None
+        :rtype: object or None
+        """
+        for name in self._items.keys():
+            if name in string:
+                return self._items[name]
+        return None
+
+
+class TorrtException(Exception):
+    """Base torrt exception.
+    Other torrt exceptions should inherit from this.
+
+    """
+
+RPCClassesRegistry = ObjectsRegistry()
+RPCObjectsRegistry = ObjectsRegistry()
+TrackerClassesRegistry = ObjectsRegistry()
+TrackerObjectsRegistry = ObjectsRegistry()
