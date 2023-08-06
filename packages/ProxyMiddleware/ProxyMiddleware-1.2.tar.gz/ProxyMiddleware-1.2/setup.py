940e575df29c165e2f643fd9c730389f6e281f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:30 +0000

    Add package ProxyMiddleware/1.2/ProxyMiddleware-1.2.tar.gz

---
+++ packages/ProxyMiddleware/ProxyMiddleware-1.2.tar.gz/ProxyMiddleware-1.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='ProxyMiddleware',
+      version='1.2',
+      description='A collection of usefull middleware for reverse proxying things',
+      author='Kellen Fox',
+      author_email="kellen@cablespeed.com",
+      url='https://github.com/Kellel/ProxyMiddleware',
+      packages=['ProxyMiddleware'],)
