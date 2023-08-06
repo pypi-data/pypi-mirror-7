2aade44d6110f72abdd1d69ba2001a8b18db731f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package pybreaker/0.2.3/pybreaker-0.2.3.tar.gz

---
+++ packages/pybreaker/pybreaker-0.2.3.tar.gz/pybreaker-0.2.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'pybreaker',
+    version = '0.2.3',
+    description = 'Python implementation of the Circuit Breaker pattern',
+    long_description = open('README.rst', 'r').read(),
+    keywords = ['design', 'pattern', 'circuit', 'breaker', 'integration'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    platforms = [
+        'Any',
+    ],
+    license = 'BSD',
+    author = 'Daniel Fernandes Martins',
+    author_email = 'daniel.tritone@gmail.com',
+    url = 'http://github.com/danielfm/pybreaker',
+    package_dir = {'':'src'},
+    py_modules = ['pybreaker'],
+    include_package_data = True,
+    zip_safe = False,
+    test_suite = 'tests'
+)
