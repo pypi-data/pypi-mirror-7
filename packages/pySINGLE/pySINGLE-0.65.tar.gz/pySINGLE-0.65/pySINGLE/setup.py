f18396dca5571f0d2e29bcc1c22b692e0fd9dc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package pySINGLE/0.65/pySINGLE-0.65.tar.gz

---
+++ packages/pySINGLE/pySINGLE-0.65.tar.gz/pySINGLE-0.65/pySINGLE/setup.py
@@ -0,0 +1,13 @@
+#from distutils.core import setup
+#from distutils.extension import Extension
+#from Cython.Distutils import build_ext
+#from Cython.Build import cythonize
+#import numpy
+
+#extension = [Extension("Z_shooting", ["Z_shooting.c"],),]
+
+#setup(
+    #cmdclass = {'build_ext': build_ext},
+    #ext_modules = [],
+    #include_dirs=[numpy.get_include(),'.', ],
+#)
