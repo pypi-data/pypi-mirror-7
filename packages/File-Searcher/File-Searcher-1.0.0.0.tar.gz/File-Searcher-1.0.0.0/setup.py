bc258d50223c25f28ffdf646d612ca65b8ddcf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:50 +0000

    Add package File-Searcher/1.0.0.0/File-Searcher-1.0.0.0.tar.gz

---
+++ packages/File-Searcher/File-Searcher-1.0.0.0.tar.gz/File-Searcher-1.0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'File-Searcher',
+    version = '1.0.0.0',
+    py_modules = ['file_modify','__init__','Instruct','mean','search','show'],
+    author = "Aadit Kapoor",
+    author_email = "aadit.kapoor@outlook.com",
+    url = "www.facebook.com/aadit.kapoor71",
+    description = "A module to search a word in a given file.",
+    )
