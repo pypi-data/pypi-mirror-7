30faf7bb91d4c6c40ef6639198582539785c512f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package pythonpy/0.2.3/pythonpy-0.2.3.tar.gz

---
+++ packages/pythonpy/pythonpy-0.2.3.tar.gz/pythonpy-0.2.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import os
+
+setup(
+    name='pythonpy',
+    version='0.2.3',
+    description='Take advantage of your python skills from the command line',
+    scripts=[os.path.join('bin', 'pythonpy')],
+    license='MIT',
+    url='https://github.com/Russell91/pythonpy',
+    long_description='',
+)
