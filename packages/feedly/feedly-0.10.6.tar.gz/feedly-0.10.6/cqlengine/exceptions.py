68ee4c0e2d4155c1ec728fa3a4ed2a0ad34be46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package feedly/0.10.6/feedly-0.10.6.tar.gz

---
+++ packages/feedly/feedly-0.10.6.tar.gz/feedly-0.10.6/cqlengine/exceptions.py
@@ -0,0 +1,11 @@
+# cqlengine exceptions
+class CQLEngineException(Exception):
+    pass
+
+
+class ModelException(CQLEngineException):
+    pass
+
+
+class ValidationError(CQLEngineException):
+    pass
