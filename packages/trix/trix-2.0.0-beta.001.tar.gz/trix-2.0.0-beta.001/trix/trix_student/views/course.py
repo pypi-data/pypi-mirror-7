5d8e5f12b82bcdda182e9aa79000aaa102037edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:08 +0000

    Add package trix/2.0.0-beta.001/trix-2.0.0-beta.001.tar.gz

---
+++ packages/trix/trix-2.0.0-beta.001.tar.gz/trix-2.0.0-beta.001/trix/trix_student/views/course.py
@@ -0,0 +1,34 @@
+from django.shortcuts import get_object_or_404
+
+from trix.trix_core import models
+from trix.trix_student.views.common import AssignmentListViewBase
+
+
+class CourseDetailView(AssignmentListViewBase):
+    template_name = "trix_student/course.django.html"
+    paginate_by = 20
+
+    def get(self, request, **kwargs):
+        self.course_id = kwargs['course_id']
+        self.course = get_object_or_404(models.Course, id=self.course_id)
+        return super(CourseDetailView, self).get(request, **kwargs)
+
+    def get_all_available_assignments(self):
+        return models.Assignment.objects\
+            .filter_by_tag(self.course.course_tag)\
+            .filter_by_tag(self.course.active_period)
+
+    def get_already_selected_tags(self):
+        already_selected_tags = [
+            self.course.course_tag.tag,
+            self.course.active_period.tag
+        ]
+        return already_selected_tags
+
+    def get_nonremoveable_tags(self):
+        return [self.course.course_tag, self.course.active_period]
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['course'] = self.course
+        return context
