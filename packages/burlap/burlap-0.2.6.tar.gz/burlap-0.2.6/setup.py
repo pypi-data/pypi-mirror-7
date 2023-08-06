3de5efad026d8ce35401f11a9eecdd6b69e980b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package burlap/0.2.6/burlap-0.2.6.tar.gz

---
+++ packages/burlap/burlap-0.2.6.tar.gz/burlap-0.2.6/setup.py
@@ -0,0 +1,37 @@
+ 
+from setuptools import setup, find_packages, Command
+
+import os
+
+import burlap
+
+setup(
+    name = "burlap",
+    version = burlap.__version__,
+    packages = find_packages(),
+    scripts = ['bin/burlap'],
+    package_data = {
+        'burlap': [
+            'templates/*.*',
+        ],
+    },
+    author = "Chris Spencer",
+    author_email = "chrisspen@gmail.com",
+    description = "Fabric commands for simplifying server deployments",
+    license = "LGPL",
+    url = "https://github.com/chrisspen/burlap",
+    #https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [
+        'Fabric>=1.9.0',
+        'PyYAML>=3.11',
+        'feedparser>=5.1.3',
+    ],
+)
