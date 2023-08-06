7b36ec1e7f36432982251a60fc00066aff5a3ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package mygrate/0.6/mygrate-0.6.tar.gz

---
+++ packages/mygrate/mygrate-0.6.tar.gz/mygrate-0.6/setup.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2013 Ian C. Good
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+from setuptools import setup, find_packages
+
+
+setup(name='mygrate',
+      version='0.6',
+      author='Ian Good',
+      author_email='ian.good@rackspace.com',
+      description='Facilitates the migration of data from MySQL or MariaDB with no downtime.',
+      license='MIT',
+      url='https://github.com/icgood/mygrate',
+      packages=find_packages(),
+      entry_points={'console_scripts': [
+              'mygrate-query = mygrate.query:main',
+              'mygrate-binlog = mygrate.binlog:main',
+              'mygrate-skip = mygrate.binlog:skip_existing'
+          ]},
+      install_requires=['MySQL-python',
+                        'bitstring'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Information Technology',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python'])
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
