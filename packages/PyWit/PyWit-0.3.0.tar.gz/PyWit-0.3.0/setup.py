ee48f534fd7c194373cc4ba1f9a3550a6e80a932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:05 +0000

    Add package PyWit/0.3.0/PyWit-0.3.0.tar.gz

---
+++ packages/PyWit/PyWit-0.3.0.tar.gz/PyWit-0.3.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='PyWit',
+      version='0.3.0',
+      author='Lex Toumbourou',
+      author_email='lextoumbourou@gmail.com',
+      description='Python bindings for the Wit HTTP API',
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Utilities',
+          'Programming Language :: Python',
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment'
+      ],
+      packages=['wit'],
+      package_dir={'wit': 'wit'},
+      url='https://github.com/lextoumbourou/PyWit',
+      install_requires=['requests'])
