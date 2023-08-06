9cb5075c5bae8a065312911b8d4e71ef4cb3d3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:27 +0000

    Add package django-firestone/0.3.6/django-firestone-0.3.6.tar.gz

---
+++ packages/django-firestone/django-firestone-0.3.6.tar.gz/django-firestone-0.3.6/setup.py
@@ -0,0 +1,37 @@
+import os
+del os.link
+from setuptools import setup
+
+setup(
+    name='django-firestone',
+    packages=('firestone', ),
+    version='0.3.6',
+    description='REST API Framework',
+    author='C. Paschalides',
+    author_email='already.late@gmail.com',
+    license='WTFPL',
+    url='http://github.com/stargazer/django-firestone',
+    keywords=('firestone', 'django-firestone', 'rest', 'restful', 'api', 'crud'),
+    install_requires=(
+        'Django',
+        'django-preserialize',
+        'django-extensions', # Required to interact easily with the testproject
+        'model-mommy',       # Required for the tests
+        'coverage',          # Required for test coverage on Travis C.I.
+    ),
+    tests_require=(),       # Kept it empty and instead moved all dependencies on
+                            # ``install_requires``, which makes for way better dependency
+                            # resolution.    
+    test_suite='runtests.run',
+    zip_safe=False,
+    classifiers=(
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: Freely Distributable',
+        'Development Status :: 3 - Alpha',
+    ),
+)                                                    
+
