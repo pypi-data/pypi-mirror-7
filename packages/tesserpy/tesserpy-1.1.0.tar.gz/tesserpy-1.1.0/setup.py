c1a45444e6e563a0124a5648bce635ea137c25af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package tesserpy/1.1.0/tesserpy-1.1.0.tar.gz

---
+++ packages/tesserpy/tesserpy-1.1.0.tar.gz/tesserpy-1.1.0/setup.py
@@ -0,0 +1,20 @@
+# Copyright 2013 The Blindsight Corporation
+
+from distutils.core import setup, Extension
+import numpy
+
+setup(
+		name = 'tesserpy',
+		version = '1.1.0',
+		description = 'Python interface to the Tesseract library',
+		maintainer = 'Kevin Rauwolf',
+		maintainer_email = 'kevin@blindsight.com',
+		url = 'https://github.com/blindsightcorp/tesserpy',
+		requires = ['numpy', ],
+		ext_modules = [
+			Extension('tesserpy', ['tesserpy.cpp', ],
+				libraries = ['tesseract', ],
+				include_dirs = [numpy.get_include(), ],
+			),
+		]
+)
