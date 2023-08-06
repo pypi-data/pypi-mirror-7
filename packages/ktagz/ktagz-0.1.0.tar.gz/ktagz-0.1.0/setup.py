0f0672239e211bd20bcd8233bc6ac6c92a5a4053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package ktagz/0.1.0/ktagz-0.1.0.tar.gz

---
+++ packages/ktagz/ktagz-0.1.0.tar.gz/ktagz-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+	name = "ktagz",
+	packages = find_packages(),
+	version = "0.1.0",
+	description = "A command line file-tagger/file-search tool for linux",
+	long_description = open('README.md').read(),
+	author = "Khirod Kant Naik",
+	author_email = "khirod234@gmail.com",
+	url='https://github.com/shinigamiryuk/TagZ',
+	scripts = ['bin/ktagz', 'bin/createIndex.sh'],
+	install_requires=[
+	"elasticsearch >= 1.0.0",
+	]
+)
