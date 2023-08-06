3ff776c311053552f92032ae53c0cd5fa4969d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package okapi/0.5.1/okapi-0.5.1.tar.gz

---
+++ packages/okapi/okapi-0.5.1.tar.gz/okapi-0.5.1/setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='okapi',
+    version='0.5.1',
+    author='RedBeacon',
+    author_email='support@redbeacon.com',
+    description='Python Library to send API info to Storage Server',
+    packages=find_packages(),
+    data_files=[('config',['setup.cfg'])],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+    install_requires = [
+        'pymongo',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'okapi=okapi:mongo',
+        ],
+    },
+)
