dcdfa5799e567f92f5ee3da5ae16812f0449aebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package infi.devicemanager/0.2.10/infi.devicemanager-0.2.10-py2.7.egg

---
+++ packages/infi.devicemanager/infi.devicemanager-0.2.10-py2.7.egg/infi/devicemanager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd29c0f6cc195280aeef055fcf404b39dabaace1c'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
