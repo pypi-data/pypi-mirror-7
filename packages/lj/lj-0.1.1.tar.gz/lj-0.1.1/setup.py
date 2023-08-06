65bf130969b406785a52780e18d2ebd4796a8376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:17 +0000

    Add package lj/0.1.1/lj-0.1.1.tar.gz

---
+++ packages/lj/lj-0.1.1.tar.gz/lj-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='lj',
+    packages=['lj'],  # this must be the same as the name above
+    version='0.1.1',
+    description='A python realization of LiveJournal (LJ) API. Checkout https://github.com/daniilr/python-lj for more info',
+    author='Original developer: David Lynch; Curent maintainer: Daniil Ryzhkov ',
+    author_email='i@daniil-r.ru',
+    url='https://github.com/daniilr/python-lj',
+    download_url='https://github.com/daniilr/python-lj/tarball/0.1',
+    keywords=['livejournal', 'blog', 'writing'],
+    classifiers=[],
+)
