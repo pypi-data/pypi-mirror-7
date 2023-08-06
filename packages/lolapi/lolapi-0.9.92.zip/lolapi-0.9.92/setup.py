4c43c2c4778d52f88bdd658b237b083b33b59d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package lolapi/0.9.92/lolapi-0.9.92.zip

---
+++ packages/lolapi/lolapi-0.9.92.zip/lolapi-0.9.92/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'docs',
+    'lolapi',
+    'lolapi.resources',
+]
+
+setup (
+    name='lolapi',
+    version='0.9.92',
+    description='Python wrapper for the League of Legends RESTful API by Riot Games',
+    author='Steven Motes',
+    author_email='steven.motes@gmail.com',
+    license='MIT',
+    url='https://github.com/smotes/lolapi',
+    packages=packages,
+    py_modules=['lolapi'],
+    platforms=['any'],
+    requires='requests',
+)
