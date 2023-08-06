776226c7d008ccb3faeb1c7596c2915c05483b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package Flask-Logging/0.1/Flask-Logging-0.1.tar.gz

---
+++ packages/Flask-Logging/Flask-Logging-0.1.tar.gz/Flask-Logging-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-Logging',
+    version='0.1',
+    packages=['flask_logging',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
