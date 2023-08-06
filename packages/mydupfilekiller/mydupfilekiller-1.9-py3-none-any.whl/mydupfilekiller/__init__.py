57116ab7c99c400b5c1b5fd2c7f3335107057126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package mydupfilekiller/1.9/mydupfilekiller-1.9-py3-none-any.whl

---
+++ packages/mydupfilekiller/mydupfilekiller-1.9-py3-none-any.whl/mydupfilekiller/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Wiadufa Chen <wiadufachen@gmail.com>"
+from mydupfilekiller.core import *
+from mydupfilekiller.gui import *
+from mydupfilekiller.console import *
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mydupfilekiller/mydupfilekiller-1.9-py3-none-any.whl/mydupfilekiller/exceptions.py
@@ -0,0 +1,2 @@
+class SkipAllException(BaseException):
+    pass
