1bffffcc3481421d269de1ad1b22b91fd5e67284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:08 +0000

    Add package darksky_catalog/0.1.1/darksky_catalog-0.1.1.tar.gz

---
+++ packages/darksky_catalog/darksky_catalog-0.1.1.tar.gz/darksky_catalog-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+version = '0.1.1'
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(README).read() + '\n'
+setup(name='darksky_catalog',
+      version=version,
+      description=("Catalog of Dark Sky Simulations"),
+      long_description=long_description,
+      classifiers=[
+          "Programming Language :: Python",
+          ("Topic :: Software Development :: Libraries :: Python Modules"),
+      ],
+      keywords='data',
+      author='Matthew Turk <matthewturk@gmail.com>, Samuel Skillman <samskillman@gmail.com>, Michael S. Warren <mswarren@gmail.com>',
+      license='BSD',
+      packages=find_packages(),
+      install_requires = ['yt','thingking'],
+      )
