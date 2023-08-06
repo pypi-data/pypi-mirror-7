ae3bebc57d7be06b715b0e3d71920e93b9265942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:18 +0000

    Add package django-alerts/0.3.2/django-alerts-0.3.2.tar.gz

---
+++ packages/django-alerts/django-alerts-0.3.2.tar.gz/django-alerts-0.3.2/alerts/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from models import Alert
+from utils import get_message_handler
+
+
+class AlertAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'level', 'tag_display', 'recipient', 'subject', 'is_read', 'is_email_sent', 'is_pushed', 'created')
+    list_filter = ('level', 'tag')
+
+    def formfield_for_choice_field(self, db_field, request, **kwargs):
+        if db_field.name == "tag":
+            kwargs['choices'] = self.tag_choices()
+        return super(AlertAdmin, self).formfield_for_choice_field(db_field, request, **kwargs)
+
+    def tag_choices(self):
+        message_handler = get_message_handler()
+        return getattr(message_handler, 'TAGS', None)
+
+    def tag_display(self, alert):
+        choices = self.tag_choices()
+        choice = dict(choices).get(alert.tag, None)
+        if choice is not None:
+            return choice
+        return alert.tag
+
+admin.site.register(Alert, AlertAdmin)
