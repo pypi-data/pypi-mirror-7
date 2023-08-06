c8bb411cb3873ccbe454dccefff5dce74a857278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:32 +0000

    Add package Yoav/0.2.0/Yoav-0.2.0.tar.gz

---
+++ packages/Yoav/Yoav-0.2.0.tar.gz/Yoav-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+
+setup(
+    name='Yoav',
+    description='Yoavs Test Library',
+    version='0.2.0', # NB: must be kept in sync with sift/version.py
+    url='',
+
+    author='Yoav Schatzberg',
+    author_email='',
+    long_description='',
+
+    packages=['yoav'],
+    install_requires=[
+        "requests >= 0.14.1",
+        "mock >= 1.0.1",
+    ],
+
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
