199962baf3edac461c0423ed72fd8f54d944ea54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:59 +0000

    Add package yarg/0.1.7/yarg-0.1.7.tar.gz

---
+++ packages/yarg/yarg-0.1.7.tar.gz/yarg-0.1.7/setup.py
@@ -0,0 +1,46 @@
+import re
+from setuptools import setup
+from setuptools import find_packages
+import sys
+
+
+desc = "A semi hard Cornish cheese, also queries PyPI (PyPI client)"
+long_desc = open('README.rst').read() + "\n\n" + open('CHANGES.rst').read()
+long_desc = re.sub(r":[a-z]*:`", "`", long_desc)
+
+exec(open('yarg/__about__.py').read())
+
+setup(name='yarg',
+      version=__version__,
+      url="https://yarg.readthedocs.org/",
+      author="Kura",
+      author_email="kura@kura.io",
+      maintainer="Kura",
+      maintainer_email="kura@kura.io",
+      description=desc,
+      long_description=long_desc,
+      license='MIT',
+      platforms=['linux'],
+      packages=find_packages(exclude=["*.tests"]),
+      install_requires=['requests', ],
+      requires=['requests', ],
+      provides=['yarg', ],
+      keywords=['pypi', 'client', 'packages'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving :: Packaging',
+      ],
+      zip_safe=True,
+      )
