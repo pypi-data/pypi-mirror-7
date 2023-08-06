086e28127be32d3d1796d43b05064dc9a31eecbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:32 +0000

    Add package slpkg/1.6.3/slpkg-1.6.3.tar.gz

---
+++ packages/slpkg/slpkg-1.6.3.tar.gz/slpkg-1.6.3/slpkg/messages.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from colors import colors
+
+def pkg_not_found(bol, pkg, message, eol):
+    '''
+    Print message when package not found
+    '''
+    print ("{0}No such package {1}: {2}{3}".format(bol, pkg, message, eol))
+
+def pkg_found(pkg):
+    '''
+    Print message when package found
+    '''
+    print ("| Package {0} is already installed".format(pkg))
+
+def pkg_installed(pkg):
+    '''
+    Print message when package installed
+    '''
+    print ("| Package {0} installed".format(pkg))
+
+def err_args(bol, eol):
+    '''
+    Print error message arguments
+    '''
+    print ("{0}Error: must enter at least two arguments{1}".format(bol, eol))
+
+def s_user(user):
+    '''
+    Check for root user
+    '''
+    if user != "root":
+        print ("\nError: must have root privileges\n")
+        sys.exit()
+
+def template(max):
+    '''
+    Print view template
+    '''
+    print ("+" + "=" * max)
+
+def view_sbo(pkg, sbo_url, sbo_dwn, source_dwn, extra_dwn, sbo_req):
+    print # new line at start
+    template(78)
+    print ("| {0}The {1}`{2}`{3} found in --> {4}".format(colors.GREEN,
+            colors.CYAN, pkg, colors.GREEN, colors.ENDC + sbo_url))
+    template(78)
+    print ("| {0}Download SlackBuild : {1}{2}".format(colors.GREEN, colors.ENDC, sbo_dwn))
+    print ("| {0}Source Downloads : {1}{2}".format(colors.GREEN, colors.ENDC, source_dwn))
+    print ("| {0}Extra Downloads : {1}{2}".format(colors.GREEN, colors.ENDC, extra_dwn))
+    print ("| {0}Package requirements : {1}{2}".format(colors.YELLOW, colors.ENDC, sbo_req))
+    template(78)
+    print (" {0}R{1}EADME               View the README file".format(colors.RED, colors.ENDC))
+    print (" {0}S{1}lackBuild           View the SlackBuild file".format(colors.RED, colors.ENDC))
+    print (" In{0}f{1}o                 View the Info file".format(colors.RED, colors.ENDC))
+    print (" {0}D{1}ownload             Download this package".format(colors.RED, colors.ENDC))
+    print (" {0}B{1}uild                Download and build".format(colors.RED, colors.ENDC))
+    print (" {0}I{1}nstall              Download/Build/Install\n".format(colors.RED, colors.ENDC))
---
+++ packages/slpkg/slpkg-1.6.3.tar.gz/slpkg-1.6.3/slpkg/sbo/search.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+from slpkg.colors import colors
+from slpkg.url_read import url_read
+from slpkg.functions import get_file
+from slpkg.slack.slack_version import slack_ver
+
+def sbo_search_pkg(name):
+    '''
+    Find SlackBuilds packages links from repository slackbuilds.org
+    '''
+    sbo_location = []
+    print ('Searching {0}[ {1} ]{2} from slackbuilds.org ...'.format(
+            colors.CYAN, name, colors.ENDC))
+    sbo_url = ("http://slackbuilds.org/repository/{0}/".format(slack_ver()))
+    SLACKBUILDS_TXT = url_read(("http://slackbuilds.org/slackbuilds/{0}/SLACKBUILDS.TXT".format(
+                                 slack_ver())))
+    for line in SLACKBUILDS_TXT.splitlines():
+        if line.startswith('SLACKBUILD LOCATION'):
+            sbo_location.append(line.replace('SLACKBUILD LOCATION: ./', ''))
+    for loc in sbo_location:
+        if get_file(loc, '/') == name:
+            return sbo_url + loc.replace(name, '') + name + "/"
+    
