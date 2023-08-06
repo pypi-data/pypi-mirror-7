d3d2b312e0fe4fbbdb03dc0017e9a9e9ebd0c283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:16 +0000

    Add package infi.storagemodel/0.2.16/infi.storagemodel-0.2.16.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.16.tar.gz/infi.storagemodel-0.2.16/src/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.16"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b27469f152a5f24168fc83fb0beacecf3e4ef227'
+__git_head_subject__ = 'get_scsi_vendor_id_or_unknown_on_error: ignore storagemodel errors as well'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
