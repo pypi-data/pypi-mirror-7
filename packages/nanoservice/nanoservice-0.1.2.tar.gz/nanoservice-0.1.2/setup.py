b118296a7cae2d712c5604854ae8a5e965b2ca4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:33 +0000

    Add package nanoservice/0.1.2/nanoservice-0.1.2.tar.gz

---
+++ packages/nanoservice/nanoservice-0.1.2.tar.gz/nanoservice-0.1.2/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import platform
+import sys
+import warnings
+
+try:
+    import setuptools
+    from setuptools import setup
+except ImportError:
+    setuptools = None
+    from distutils.core import setup
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+with open('./nanoservice/version.py') as fh:
+    for line in fh:
+        if line.startswith('VERSION'):
+            version = line.split('=')[1].strip().strip("'")
+
+setup(
+    name='nanoservice',
+    version=version,
+    packages = ['nanoservice'],
+    author='Tony Walker',
+    author_email='walkr.walkr@gmail.com',
+    url='https://github.com/walkr/nanoservice',
+    license='MIT',
+    description='nanoservice is a small Python library for writing lightweight networked services using nanomsg',
+    long_description=long_description,
+    install_requires=[
+        'msgpack-python',
+        'nanomsg',
+    ],
+    dependency_links=[
+        'git+ssh://git@github.com/tonysimpson/nanomsg-python.git@master#egg=nanomsg',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        ],
+)
