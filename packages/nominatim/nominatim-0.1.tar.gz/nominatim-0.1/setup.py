99d550c0763186e62a4acf8691c74a8c7e4a81d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package nominatim/0.1/nominatim-0.1.tar.gz

---
+++ packages/nominatim/nominatim-0.1.tar.gz/nominatim-0.1/setup.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name='nominatim',
+    version='0.1',
+    author='Damian Braun',
+    author_email='brunek5252@gmail.com',
+    url='https://github.com/damianbraun/nominatim',
+    description='OSM Nominatim API module',
+    packages=['nominatim'],
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
