8115705fcc444dbf190b88232c6370f5cf7ec939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package infi.devicemanager/0.2.11/infi.devicemanager-0.2.11-py2.7.egg

---
+++ packages/infi.devicemanager/infi.devicemanager-0.2.11-py2.7.egg/infi/devicemanager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.11"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.11)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '12977113302dbeecf0087a86ca48072de5883840'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
