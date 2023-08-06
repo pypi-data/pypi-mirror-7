89b7772c8d473a93a0adf6136e0f9931d9fb2db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:39 +0000

    Add package bw2data/1.2/bw2data-1.2.tar.gz

---
+++ packages/bw2data/bw2data-1.2.tar.gz/bw2data-1.2/bw2data/backends/utils.py
@@ -0,0 +1,28 @@
+from ..meta import databases
+import copy
+
+
+def convert_backend(database_name, backend):
+    """Convert a Database to another backend.
+
+    bw2data currently supports the `default` and `json` backends.
+
+    Args:
+        * `database_name` (unicode): Name of database.
+        * `backend` (unicode): Type of database. `backend` should be recoginized by `DatabaseChooser`.
+
+    Returns `False` if the old and new backend are the same. Otherwise returns an instance of the new Database object."""
+    from ..database import Database
+    db = Database(database_name)
+    if db.backend == backend:
+        return False
+    data = db.load()
+    metadata = copy.deepcopy(databases[database_name])
+    del databases[database_name]
+    new_db = Database(database_name, backend)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        new_db.register(**metadata)
+    new_db.write(data)
+    new_db.process()
+    return new_db
