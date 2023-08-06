c2e66630823e5f11b00aefd48d7d09693e24cb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:26 +0000

    Add package pythonioc/0.1/pythonioc-0.1.tar.gz

---
+++ packages/pythonioc/pythonioc-0.1.tar.gz/pythonioc-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pythonioc',
+      packages=['pythonioc'],
+      description='simplistic dependency injection container for python',
+      author='Franz Eichhorn',
+      author_email='frairon@googlemail.com',
+      url='https://bitbucket.org/eh14/python-ioc',
+      version='0.1'
+      )
