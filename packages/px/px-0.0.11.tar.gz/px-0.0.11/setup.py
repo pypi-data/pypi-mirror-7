7768db473a6f55c9c0541df651d2045e75e9bf46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:42 +0000

    Add package px/0.0.11/px-0.0.11.tar.gz

---
+++ packages/px/px-0.0.11.tar.gz/px-0.0.11/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='px',
+	version='0.0.11',
+	author='6px',
+	author_email='ops@6px.io',
+	packages=['_6px'],
+	url='http://pypi.python.org/pypi/6px/',
+	license='LICENSE',
+	description='6px python sdk',
+	long_description=open('README.txt').read(),
+)
