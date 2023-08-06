4b362236e972dadf99744388264c118968ee9935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:52 +0000

    Add package fibonacci-heap-mod/0.9/fibonacci-heap-mod-0.9.tar.gz

---
+++ packages/fibonacci-heap-mod/fibonacci-heap-mod-0.9.tar.gz/fibonacci-heap-mod-0.9/setup.py
@@ -0,0 +1,33 @@
+
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "fibonacci-heap-mod",
+    version = "0.9",
+    packages = find_packages(),
+    scripts = ['fibonacci_heap_mod.py'],
+
+    # metadata for upload to PyPI
+    author = "Daniel Richard Stromberg",
+    author_email = "strombrg@gmail.com",
+    description='Pure Python Fibonacci Heap (Priority Queue)',
+    long_description='''
+A Pure Python Fibonacci Heap implementation of a Priority Queue is provided.
+
+It passes pylint, passes pep8, is pretty thoroughly unit tested, and runs on CPython 2.[67], CPython 3.[01234],
+Pypy 2.3.1 and Jython 2.7b3.
+''',
+    license = "Apache v2",
+    keywords = "Fibonacci Heap, Priority Queue",
+    url='http://stromberg.dnsalias.org/~strombrg/fibonacci-heap-mod/',
+    platforms='Cross platform',
+    classifiers=[
+         "Development Status :: 4 - Beta",
+         "Intended Audience :: Developers",
+         "Programming Language :: Python :: 2",
+         "Programming Language :: Python :: 3",
+         ],
+)
+
