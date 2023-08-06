4804ae816014af6ba626a5d240f7f0ac6b9bdfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:29 +0000

    Add package complyadvantage/0.3/complyadvantage-0.3.tar.gz

---
+++ packages/complyadvantage/complyadvantage-0.3.tar.gz/complyadvantage-0.3/complyadvantage/__init__.py
@@ -0,0 +1,20 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+class ComplyAdvantage:
+
+	base_uri = "https://api.complyadvantage.com/v1/"
+
+	def __init__(self, api_key):
+		self.auth = HTTPBasicAuth(api_key, 'x')
+
+	def search(self, names):
+		response = requests.post(self.base_uri + 'search', data=json.dumps({'names': names}), auth=self.auth, verify=False)
+		response.raise_for_status()
+		return response.json()
+
+	def get(self, id):
+		response = requests.get(self.base_uri + 'get/%s' % id, auth=self.auth, verify=False)
+		response.raise_for_status()
+		return response.json()
