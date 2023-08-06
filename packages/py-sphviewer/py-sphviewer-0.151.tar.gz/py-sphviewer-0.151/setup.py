22bb5e54e1bafdef4a7d13d85da52d1ab97c9eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:50 +0000

    Add package py-sphviewer/0.151/py-sphviewer-0.151.tar.gz

---
+++ packages/py-sphviewer/py-sphviewer-0.151.tar.gz/py-sphviewer-0.151/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name='py-sphviewer',
+      version='0.151',
+      description='Py-SPHViewer is a framework for rendering particles using the smoothed particle hydrodynamics scheme.',
+      author='Alejandro Benitez Llambay',
+      author_email='alejandrobll@oac.uncor.edu',
+      url='https://code.google.com/p/py-sphviewer/',
+      packages=['sphviewer'],
+      package_data={'sphviewer': ['*.c','*.txt']},
+      license='GNU GPL v3',
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'Topic :: Multimedia :: Graphics :: Viewers',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Utilities'
+            ],
+      keywords="smoothed particle hydrodynamics render particles nbody galaxy formation dark matter sph cosmology movies",      
+     )
