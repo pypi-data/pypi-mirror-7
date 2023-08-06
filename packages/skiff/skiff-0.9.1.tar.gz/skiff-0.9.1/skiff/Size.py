5a34dd50d7f421a96c1bf0c0d512b2c06ab0adc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:58 +0000

    Add package skiff/0.9.1/skiff-0.9.1.tar.gz

---
+++ packages/skiff/skiff-0.9.1.tar.gz/skiff-0.9.1/skiff/Size.py
@@ -0,0 +1,36 @@
+skiff = None
+
+
+def setSkiff(s):
+    global skiff
+    skiff = s
+
+
+class SkiffSize(object):
+    """SkiffSize"""
+    def __init__(self, options=None, **kwargs):
+        super(SkiffSize, self).__init__()
+        if not options:
+            options = kwargs
+
+        self.__dict__.update(options)
+
+    def __repr__(self):
+        return '<%s>' % (self.slug)
+
+
+def all(params=None, **kwargs):
+    if not params:
+        params = kwargs
+
+    r = skiff.get('/sizes', params)
+    return [SkiffSize(a) for a in r['sizes']]
+
+
+def get(s):
+    s = str(s)
+    sizes = all()
+    for size in sizes:
+        if s in size.slug:
+            return size
+    raise ValueError('Unknown Size')
