a2e2070a76b63638031e7cad0f173038aa8aaa9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:45 +0000

    Add package octocat/0.1.0/octocat-0.1.0.tar.gz

---
+++ packages/octocat/octocat-0.1.0.tar.gz/octocat-0.1.0/octocat/__init__.py
@@ -0,0 +1,12 @@
+"""
+    octocat description.
+
+"""
+
+# Package information
+# ===================
+
+__version__ = "0.1.0"
+__project__ = "octocat"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
