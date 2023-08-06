a6ee20a264e59f3940394af5f778e6bcbe02ddbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:33 +0000

    Add package spring/5.8.4/spring-5.8.4.tar.gz

---
+++ packages/spring/spring-5.8.4.tar.gz/spring-5.8.4/spring/cbgen.py
@@ -0,0 +1,112 @@
+from random import choice
+from time import time
+
+from couchbase import experimental
+experimental.enable()
+from couchbase.exceptions import (ConnectError,
+                                  CouchbaseError,
+                                  HTTPError,
+                                  KeyExistsError,
+                                  NotFoundError,
+                                  TemporaryFailError,
+                                  TimeoutError,
+                                  )
+from couchbase.connection import Connection
+from txcouchbase.connection import Connection as TxConnection
+
+import requests
+from decorator import decorator
+from logger import logger
+
+
+@decorator
+def quiet(method, *args, **kwargs):
+    try:
+        return method(*args, **kwargs)
+    except (ConnectError, CouchbaseError, HTTPError, KeyExistsError,
+            NotFoundError, TemporaryFailError, TimeoutError) as e:
+        logger.warn('{}: {}'.format(method, e))
+
+
+class CBAsyncGen(object):
+
+    def __init__(self, **kwargs):
+        self.client = TxConnection(quiet=True, timeout=60, **kwargs)
+
+    def create(self, key, doc, ttl=None):
+        extra_params = {}
+        if ttl is None:
+            extra_params['ttl'] = ttl
+        return self.client.set(key, doc, **extra_params)
+
+    def read(self, key):
+        return self.client.get(key)
+
+    def update(self, key, doc):
+        return self.client.set(key, doc)
+
+    def cas(self, key, doc):
+        cas = self.client.get(key).cas
+        return self.client.set(key, doc, cas=cas)
+
+    def delete(self, key):
+        return self.client.delete(key)
+
+
+class CBGen(CBAsyncGen):
+
+    def __init__(self, **kwargs):
+        self.client = Connection(timeout=60, quiet=True, **kwargs)
+        self.session = requests.Session()
+        self.session.auth = (kwargs['username'], kwargs['password'])
+
+    @quiet
+    def create(self, *args, **kwargs):
+        super(CBGen, self).create(*args, **kwargs)
+
+    @quiet
+    def read(self, *args, **kwargs):
+        super(CBGen, self).read(*args, **kwargs)
+
+    @quiet
+    def update(self, *args, **kwargs):
+        super(CBGen, self).update(*args, **kwargs)
+
+    @quiet
+    def cas(self, *args, **kwargs):
+        super(CBGen, self).cas(*args, **kwargs)
+
+    @quiet
+    def delete(self, *args, **kwargs):
+        super(CBGen, self).delete(*args, **kwargs)
+
+    def query(self, ddoc, view, query):
+        node = choice(self.client.server_nodes).replace('8091', '8092')
+        url = 'http://{}/{}/_design/{}/_view/{}?{}'.format(
+            node, self.client.bucket, ddoc, view, query.encoded
+        )
+        t0 = time()
+        resp = self.session.get(url=url)
+        latency = time() - t0
+        return resp.text, latency
+
+    @quiet
+    def lcb_query(self, ddoc, view, query):
+        return tuple(self.client.query(ddoc, view, query=query))
+
+
+class N1QLGen(CBGen):
+
+    def __init__(self, **kwargs):
+        self.client = Connection(**kwargs)
+        self.session = requests.Session()
+        self.session.headers.update({'Content-Type': 'text/plain'})
+
+    def query(self, ddoc_name, view_name, query):
+        query = query.format(bucket=self.client.bucket)
+        node = choice(self.client.server_nodes).replace('8091', '8093')
+        url = 'http://{}/query'.format(node)
+        t0 = time()
+        resp = self.session.post(url=url, data=query)
+        latency = time() - t0
+        return resp.text, latency
