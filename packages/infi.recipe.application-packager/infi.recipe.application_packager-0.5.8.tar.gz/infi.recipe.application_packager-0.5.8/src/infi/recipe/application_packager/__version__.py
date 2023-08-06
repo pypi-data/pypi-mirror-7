40a2caa152590354fe2b29ba091729a7b283e4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package infi.recipe.application-packager/0.5.8/infi.recipe.application_packager-0.5.8.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.8.tar.gz/infi.recipe.application_packager-0.5.8/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.8"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "yalon@infinidat.com"
+__git_branch__ = '(detached from v0.5.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '814b37d19bba385ec9fff2ff16c0e9279b1fc240'
+__git_head_subject__ = 'fixed missing quote that caused ssl not to work on win32'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
