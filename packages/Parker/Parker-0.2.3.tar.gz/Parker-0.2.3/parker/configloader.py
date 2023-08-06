e149e5ca18339c236b83f58c310779c4035f5a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:59 +0000

    Add package Parker/0.2.3/Parker-0.2.3.tar.gz

---
+++ packages/Parker/Parker-0.2.3.tar.gz/Parker-0.2.3/parker/configloader.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Module for loading JSON configuration."""
+
+import json
+import os
+
+CONFIG_EXT = ".json"
+CONFIG_PATH = os.path.join(
+    os.environ['PROJECT'],
+    'etc',
+    'parker'
+)
+CONFIG_SITES_PATH = 'sites'
+
+
+def _load_config_json(file_path):
+    """Load the passed file as JSON."""
+    return json.load(open(file_path))
+
+
+def load_config(name):
+    """Load and return configuration as a dict."""
+    return _load_config_json(
+        os.path.join(
+            CONFIG_PATH,
+            name + CONFIG_EXT
+        )
+    )
+
+
+def load_site_config(name):
+    """Load and return site configuration as a dict."""
+    return _load_config_json(
+        os.path.join(
+            CONFIG_PATH,
+            CONFIG_SITES_PATH,
+            name + CONFIG_EXT
+        )
+    )
