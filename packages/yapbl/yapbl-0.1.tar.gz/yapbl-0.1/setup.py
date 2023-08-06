3ab2248a5d41a7b0eb74f139451229d685cfcc9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:55 +0000

    Add package yapbl/0.1/yapbl-0.1.tar.gz

---
+++ packages/yapbl/yapbl-0.1.tar.gz/yapbl-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='yapbl',
+    version='0.1',
+    packages=['yapbl'],
+    url='https://github.com/Spittie/yapbl.py',
+    downloadurl='https://github.com/Spittie/yapbl.py/archive/0.1.tar.gz',
+    license='MIT',
+    author='Spittie',
+    author_email='spittiepie@gmail.com',
+    description='Yet Another PushBullet Library',
+    requires=['requests']
+)
