c9c67fe1494b530a9f9bf72525b35e679793a82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:36 +0000

    Add package ldap_paged_search/0.1/ldap_paged_search-0.1.tar.gz

---
+++ packages/ldap_paged_search/ldap_paged_search-0.1.tar.gz/ldap_paged_search-0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='ldap_paged_search',
+    version='0.1',
+    packages=['ldap_paged_search'],
+    license='LGPLv2.1',
+    description='Easily perform LDAP queries with more than 1000 results',
+    author='Michael Henry a.k.a. neoCrimeLabs',
+    author_email='mhenry@neocri.me',
+    url='https://github.com/neoCrimeLabs/python-ldap_paged_search',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP']
+    )
