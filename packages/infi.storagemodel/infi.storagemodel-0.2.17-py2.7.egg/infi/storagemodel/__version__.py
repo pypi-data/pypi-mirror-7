6a19c8ceb6259f6f89878c807385e0020f66fc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:30 +0000

    Add package infi.storagemodel/0.2.17/infi.storagemodel-0.2.17-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.17-py2.7.egg/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.17"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.17)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'dd696210f70268741bba011b991ba34cc8b7eaa7'
+__git_head_subject__ = "vmware patches: don't return paths instead of crashing when none found"
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
