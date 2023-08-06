f66bb409ec23075f3db349b37dd6ef1f7ab13b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:37 +0000

    Add package ffindex/0.0.3/ffindex-0.0.3.tar.gz

---
+++ packages/ffindex/ffindex-0.0.3.tar.gz/ffindex-0.0.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='ffindex',
+    version='0.0.3',
+    description='ffindex support for Python',
+    author='Stefan Seemayer',
+    author_email='mail@semicolonsoftware.de',
+    license='MIT',
+    url='https://github.com/sseemayer/ffindex-python',
+    packages=['ffindex'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent"
+    ]
+)
