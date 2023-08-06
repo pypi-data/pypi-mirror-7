1055772872c450d5324fe746807c61cb710f46a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:50 +0000

    Add package yahmm/1.0.0/yahmm-1.0.0.zip

---
+++ packages/yahmm/yahmm-1.0.0.zip/yahmm-1.0.0/yahmm/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py: Yet Another Hidden Markov Model library
+# Contact: Jacob Schreiber ( jmschreiber91@gmail.com )
+#          Adam Novak ( anovak1@ucsc.edu )
+
+"""
+For detailed documentation and examples, see the README.
+"""
+
+import numpy as np
+import pyximport
+pyximport.install( setup_args={ 'include_dirs':np.get_include(),
+						'options': {'build_ext': {'compiler': 'mingw32'}}})
+from yahmm import *
+
+__version__ = '1.0.0'
