335acb458bca360c5e39c822053ff64c5a21d961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:50 +0000

    Add package pytwitch/0.0.1/pytwitch-0.0.1.tar.gz

---
+++ packages/pytwitch/pytwitch-0.0.1.tar.gz/pytwitch-0.0.1/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+PyTwitch 0.0.1
+==============
+
+See GitHub for more information:
+
+https://github.com/dhh-hss/pytwitch
---
+++ packages/pytwitch/pytwitch-0.0.1.tar.gz/pytwitch-0.0.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pytwitch',
+    version='0.0.1',
+    description='PyTwitch - Twitch Integration for Python',
+    long_description=open('DESCRIPTION.rst').read(),
+    author='Daniel Hyldebrandt Hemmingsen',
+    author_email='daniel.h.hemmingsen@gmail.com',
+    url='https://github.com/dhh-hss/pytwitch',
+    keywords=[
+        'twitch',
+        'api',
+        'development',
+        'authentication'
+    ],
+    install_requires=[
+        'requests==2.4.0'
+    ],
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    zip_safe=False,
+)
