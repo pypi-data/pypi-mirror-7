7610f947a87dfe9c17be8023f6efbe2b67f58fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:53 +0000

    Add package PyTic-Tac-Toe/1.0.1/PyTic-Tac-Toe-1.0.1.tar.gz

---
+++ packages/PyTic-Tac-Toe/PyTic-Tac-Toe-1.0.1.tar.gz/PyTic-Tac-Toe-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='PyTic-Tac-Toe',
+	version='1.0.1',
+	packages=['pytictactoe'],
+	scripts=['pyttt.py'],
+	license='Creative Commons Attribution-Noncommercial-Share Alike License',
+	long_description=open('README.txt').read(),
+)
