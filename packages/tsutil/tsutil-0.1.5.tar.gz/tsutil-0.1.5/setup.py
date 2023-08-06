0d0b5fb4b176c9c988b90841896bebedb1488c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:10 +0000

    Add package tsutil/0.1.5/tsutil-0.1.5.tar.gz

---
+++ packages/tsutil/tsutil-0.1.5.tar.gz/tsutil-0.1.5/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+reqs = ['requests==2.3.0', 'chardet==2.1.1']
+
+setup(
+      name='tsutil',
+      version='0.1.5',
+      py_modules=['tsutil.enum', 'tsutil.encoder', 'tsutil.util'],
+      author='Turbidsoul Chen',
+      author_email='sccn.sq+py@gmail.com',
+      url='http://github.com/turbidsoul/tsutil',
+      description='my python tool module',
+      license='MIT',
+      install_requires=reqs,
+      package_dir={'':'.'},
+      include_package_data=True,
+      package_data = {'require': ['*.txt']}
+)
