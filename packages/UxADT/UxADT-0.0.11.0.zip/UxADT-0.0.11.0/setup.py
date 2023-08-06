d8b43eabc445b6b88ed4596414fe5f33628e5948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:13 +0000

    Add package UxADT/0.0.11.0/UxADT-0.0.11.0.zip

---
+++ packages/UxADT/UxADT-0.0.11.0.zip/UxADT-0.0.11.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name             = 'UxADT',
+    version          = '0.0.11.0',
+    packages         = ['uxadt',],
+    license          = 'MIT License',
+	url              = 'http://uxadt.org',
+	author           = 'A. Lapets',
+	author_email     = 'lapets@bu.edu',
+    description = 'Universal (cross-language) extensible representation for algebraic data type instances.',
+    long_description = open('README.txt').read(),
+)
