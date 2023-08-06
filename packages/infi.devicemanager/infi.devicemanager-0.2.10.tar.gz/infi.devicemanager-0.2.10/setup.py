5d9570c49b4d4b1223432a207bd1c20eb7bb1f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:29 +0000

    Add package infi.devicemanager/0.2.10/infi.devicemanager-0.2.10.tar.gz

---
+++ packages/infi.devicemanager/infi.devicemanager-0.2.10.tar.gz/infi.devicemanager-0.2.10/setup.py
@@ -0,0 +1,49 @@
+
+SETUP_INFO = dict(
+    name = 'infi.devicemanager',
+    version = '0.2.10',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'http://www.infinidat.com',
+    license = 'PSF',
+    description = """Python bindings to Windows Device Manager's APIs""",
+    long_description = """Python bindings to Windows Device Managers' API""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.cwrap',
+'infi.exceptools',
+'infi.instruct',
+'infi.pyutils',
+'infi.wioctl',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = ['rescan = infi.devicemanager.scripts:rescan'],
+        gui_scripts = []),
+    )
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
