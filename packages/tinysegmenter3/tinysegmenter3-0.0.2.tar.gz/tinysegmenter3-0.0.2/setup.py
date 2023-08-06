038aee79e1a1946b7f34b18edec4c7349e1bd7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:22 +0000

    Add package tinysegmenter3/0.0.2/tinysegmenter3-0.0.2.tar.gz

---
+++ packages/tinysegmenter3/tinysegmenter3-0.0.2.tar.gz/tinysegmenter3-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'tinysegmenter3',
+    packages = ['tinysegmenter'],
+    version = '0.0.2',
+    description = 'Super compact Japanese tokenizer',
+    maintainer = 'Tatsuro Yasukawa',
+    maintainer_email = 't.yasukawa01@gmail.com',
+    url = 'https://github.com/SamuraiT/tinysegmenter',
+    license='New BSD',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Environment :: MacOS X",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
+
