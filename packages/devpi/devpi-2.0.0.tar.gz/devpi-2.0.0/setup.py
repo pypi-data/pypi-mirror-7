6da1323229f545abe312c8e0b143d273411c42d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:02 +0000

    Add package devpi/2.0.0/devpi-2.0.0.tar.gz

---
+++ packages/devpi/devpi-2.0.0.tar.gz/devpi-2.0.0/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import os
+from setuptools import setup
+
+if __name__ == "__main__":
+    here = os.path.abspath(".")
+    README = open(os.path.join(here, 'README.rst')).read()
+
+    setup(
+      name="devpi",
+      description="devpi: github-style pypi index server and packaging meta tool.",
+      install_requires = ["devpi-server>=2.0.0,<2.1",
+                          "devpi-client>=2.0.0,<2.1",
+                          "devpi-web>=2.0.0,<2.1",
+      ],
+      keywords="pypi cache server installer wsgi",
+      long_description=README,
+      url="http://doc.devpi.net",
+      version='2.0.0',
+      maintainer="Holger Krekel",
+      maintainer_email="holger@merlinux.eu",
+      zip_safe=False,
+      license="MIT",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      )
