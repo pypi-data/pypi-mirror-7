5e17a24f5434a15ee7ebc6711dd1e62bb498687d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:36 +0000

    Add package SendSMSApp/1.0/SendSMSApp-1.0.tar.gz

---
+++ packages/SendSMSApp/SendSMSApp-1.0.tar.gz/SendSMSApp-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='SendSMSApp',
+  version='1.0',
+  description='Application to send SMS to multiple people',
+  author='Bhavya Juneja',
+  author_email='bhavyaj_42@python.net',
+ )
