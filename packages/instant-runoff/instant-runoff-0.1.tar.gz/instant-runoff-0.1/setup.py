318359b7312484e67148b603e18401b5b8f22f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:38 +0000

    Add package instant-runoff/0.1/instant-runoff-0.1.tar.gz

---
+++ packages/instant-runoff/instant-runoff-0.1.tar.gz/instant-runoff-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='instant-runoff',
+      author='Robert Ochshorn',
+      author_email='mail@rmozone.com',
+      description='Write emails in groff.',
+      url='http://readmes.numm.org/instant-runoff/',
+      scripts = ['instant-runoff'],
+      version='0.1',
+      license='AGPL',
+)
