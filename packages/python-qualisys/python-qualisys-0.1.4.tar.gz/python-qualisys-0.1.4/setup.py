1f6b44ecd21f330766d581a470724f6229dd9f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:15 +0000

    Add package python-qualisys/0.1.4/python-qualisys-0.1.4.tar.gz

---
+++ packages/python-qualisys/python-qualisys-0.1.4.tar.gz/python-qualisys-0.1.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from os.path import join, dirname
+
+
+def read(name):
+    return open(join(dirname(__file__), name)).read()
+
+
+setup(
+    name='python-qualisys',
+    version='0.1.4',
+    license='MIT',
+
+    description='Import data from Qualisys Track Manager into pandas.',
+    long_description=read('README.rst'),
+
+    author='Henry S. Harrison',
+    author_email='henry.schafer.harrison@gmail.com',
+
+    url='https://bitbucket.org/hharrison/python-qualisys',
+    download_url='https://bitbucket.org/hharrison/python-qualisys/get/default.tar.gz',
+
+    py_modules=['qualisys'],
+
+    keywords='qualisys qtm motion-capture import biomechanics',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering',
+    ],
+
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
