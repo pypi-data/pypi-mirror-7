771ff5c60d8ad5e473e225d6351fae16f9d38856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:27 +0000

    Add package plone.app.versioningbehavior/1.1.4/plone.app.versioningbehavior-1.1.4.zip

---
+++ packages/plone.app.versioningbehavior/plone.app.versioningbehavior-1.1.4.zip/plone.app.versioningbehavior-1.1.4/plone/app/versioningbehavior/behaviors.py
@@ -0,0 +1,62 @@
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.autoform import directives as form
+from plone.supermodel import model
+from plone.app.versioningbehavior import MessageFactory as _
+from z3c.form.interfaces import IAddForm
+from z3c.form.interfaces import IEditForm
+from rwproperty import getproperty, setproperty
+from zope import schema
+from zope.annotation.interfaces import IAnnotations
+from zope.component import adapts
+from zope.interface import Interface, implements, alsoProvides
+
+
+class IVersionable(model.Schema):
+    """ Behavior for enabling CMFEditions's versioning for dexterity
+    content types. Be shure to enable versioning in the plone types
+    control-panel for your content type.
+    """
+
+    changeNote = schema.TextLine(
+        title=_(u'label_change_note', default=u'Change Note'),
+        description=_(u'help_change_note',
+                      default=u'Enter a comment that describes the changes '
+                      'you made.'),
+        required=False)
+
+    form.omitted('changeNote')
+    form.no_omit(IEditForm, 'changeNote')
+    form.no_omit(IAddForm, 'changeNote')
+
+
+alsoProvides(IVersionable, IFormFieldProvider)
+
+
+class IVersioningSupport(Interface):
+    """
+    Marker Interface for the IVersionable behavior.
+    """
+
+
+class Versionable(object):
+    """ The Versionable adapter prohibits dexterity from saving the changeNote
+    on the context. It stores it in a request-annotation for later use in
+    event-handlers
+    """
+
+    implements(IVersionable)
+    adapts(IDexterityContent)
+
+    def __init__(self, context):
+        self.context = context
+
+    @getproperty
+    def changeNote(self):
+        return ''
+
+    @setproperty
+    def changeNote(self, value):
+        # store the value for later use (see events.py)
+        annotation = IAnnotations(self.context.REQUEST)
+        annotation['plone.app.versioningbehavior-changeNote'] = value
---
+++ packages/plone.app.versioningbehavior/plone.app.versioningbehavior-1.1.4.zip/plone.app.versioningbehavior-1.1.4/plone/app/versioningbehavior/tests/tests.py
@@ -0,0 +1,13 @@
+from plone.app.versioningbehavior.testing import VERSIONING_FUNCTIONAL_TESTING
+from plone.testing import layered
+import doctest
+import unittest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+            layered(doctest.DocFileSuite('doctest_behavior.txt'),
+                    layer=VERSIONING_FUNCTIONAL_TESTING),
+            ])
+    return suite
