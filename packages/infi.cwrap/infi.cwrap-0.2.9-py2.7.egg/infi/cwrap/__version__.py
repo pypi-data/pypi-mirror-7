6ba3566d0f37cc5633f3aab23f952bd503fdb4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package infi.cwrap/0.2.9/infi.cwrap-0.2.9-py2.7.egg

---
+++ packages/infi.cwrap/infi.cwrap-0.2.9-py2.7.egg/infi/cwrap/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '7d24cacd87508734f4984804db9148faf7a3aa0d'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
