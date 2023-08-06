028a1a716d0cffee6ea16898e67e6eb008f88715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:32 +0000

    Add package j1ster/1.4.0/j1ster-1.4.0.zip

---
+++ packages/j1ster/j1ster-1.4.0.zip/j1ster-1.4.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (
+
+name = 'j1ster',
+version = '1.4.0',
+py_modules = ['j1ster'],
+author = 'jinny',
+author_email = 'jinneous@gmail.com',
+url = 'http://www.headfirstlabs.com',
+decription = 'A simple function prints nested lists',
+
+)
