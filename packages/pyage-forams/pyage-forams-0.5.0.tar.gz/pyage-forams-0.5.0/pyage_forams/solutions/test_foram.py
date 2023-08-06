99819fed10989563d321653b98c4eefc7ce99b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package pyage-forams/0.5.0/pyage-forams-0.5.0.tar.gz

---
+++ packages/pyage-forams/pyage-forams-0.5.0.tar.gz/pyage-forams-0.5.0/pyage_forams/solutions/test_foram.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from pyage.core import inject
+from pyage_forams.solutions.foram import Foram
+
+__author__ = 'makz'
+
+
+class TestForam(TestCase):
+    def test_step(self):
+        inject.config = "pyage_forams.conf.dummy_conf"
+        foram = Foram(10)
+        # foram.step()
