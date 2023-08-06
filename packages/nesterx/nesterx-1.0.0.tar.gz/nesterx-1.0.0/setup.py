09bc85ef1df07bbf0a11ad7137b68221e785fa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:36 +0000

    Add package nesterx/1.0.0/nesterx-1.0.0.tar.gz

---
+++ packages/nesterx/nesterx-1.0.0.tar.gz/nesterx-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'nesterx',
+    version      = '1.0.0',
+    py_modules   = ['nesterx'],
+    author       = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url          = 'http://www.headfirstpython.com',
+    description  = 'A simple printer of nested lines',
+)
