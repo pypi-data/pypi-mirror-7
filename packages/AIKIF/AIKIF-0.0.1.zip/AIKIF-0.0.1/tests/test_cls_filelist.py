5eece1738aca2c3ca73a69466803950764fcb452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:45 +0000

    Add package AIKIF/0.0.1/AIKIF-0.0.1.zip

---
+++ packages/AIKIF/AIKIF-0.0.1.zip/AIKIF-0.0.1/tests/test_cls_filelist.py
@@ -0,0 +1,68 @@
+# test_cls_file.py     written by Duncan Murray 22/6/2014
+# unit testing for collection class
+# NOTE - this is different to the agent_filelist.py program because 
+#        the agent_fileList USES this, and so should have a single 
+#        higher level test.
+
+
+import unittest
+import os
+import sys
+
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + "..") 
+sys.path.append(root_folder)
+
+
+import AI.lib.cls_filelist as fl 
+                    
+class TestClassFile(unittest.TestCase):
+ 
+    def setUp(self):
+        self.fname = 'test_results/cls_filelist_results1.csv'
+        
+    def test_01_file_result(self):
+        """print("test1 - filelist with one file")"""
+        lst1 = fl.FileList([root_folder ], ['README.md'], [],  self.fname)
+        self.assertEqual(len(lst1.get_list()), 1) 
+        
+    def test_02_multiple_file_result(self):
+        """print("test2 - Collecting multiple file metadata")"""
+        lst2 = fl.FileList([root_folder + os.sep + 'tests'], ['*.*'], [],  self.fname)
+        self.assertEqual(len(lst2.get_list()) > 50, True) 
+        
+    def test_03_exclude_files(self):
+        """print("test2 - Collecting multiple file metadata")"""
+        lst3 = fl.FileList([root_folder + os.sep + 'tests' + os.sep + 'test_results'], ['*.*'], ['*.sql'],  self.fname)
+        self.assertEqual(len(lst3.get_list()), 12) 
+        
+    def test_04_save_filelist(self):
+        """ test saving filelist  """
+        
+        if os.path.isfile(self.fname):
+            os.remove(self.fname)
+        aikif_fl = fl.FileList([root_folder + os.sep + 'tests'], ['*.py'], [],  self.fname)
+        aikif_fl.save_filelist(self.fname, ["name", "path", "size", "date"])
+        if os.path.isfile(self.fname):
+            self.assertEqual("File Exists", "File Exists") 
+        else:
+            self.assertEqual("File Exists", "Whoops - nope") 
+
+    def test_05_check_metadata(self):
+        """ make sure metadata is correct for this file """
+        lst5 = fl.FileList([os.path.dirname(os.path.abspath(__file__))], ['test_cls_filelist.py'], [],  self.fname)
+        files = lst5.get_metadata()
+        for file_dict in files:
+            self.assertEqual(file_dict["name"], 'test_cls_filelist.py') 
+            self.assertTrue(file_dict["size"] > 2500) 
+            self.assertTrue(file_dict["date"] > '2014-08-12 21:32:57') 
+            self.assertEqual(file_dict["path"], 'T:\\user\\dev\\src\\python\\AI\\tests') 
+    
+    def test_06_check_duplicate_folders(self):
+        """ not really a test but more checking the version of python works as expected """
+        lst = ['C:\\AAA', 'C:\\BBB', 'C:\\BBB', 'C:\\CCC', 'C:\\BBB', 'C:\\CCC']
+        self.assertEqual(len(lst), 6)
+        self.assertEqual(len(list(set(lst))), 3)
+
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/AIKIF/AIKIF-0.0.1.zip/AIKIF-0.0.1/tests/test_dataTools.py
@@ -0,0 +1,32 @@
+# test_dataTools.py     written by Duncan Murray 20/6/2014
+# unit testing for dataTools module
+
+import unittest
+import os
+import sys
+import csv
+sys.path.append("..\\AI")
+from dataTools import dataTools as cl
+					
+class TestClassDataTools(unittest.TestCase):
+ 
+	def setUp(self):
+		pass
+
+	def test_clean_col_upper(self):
+		self.assertEqual(cl.clean_column_heading('date'), 'DATE') 
+		self.assertEqual(cl.clean_column_heading('Date'), 'DATE') 
+		
+	def test_clean_col_bad_chars(self):
+		self.assertEqual(cl.clean_column_heading('__Country_@@@_'), 'COUNTRY') 
+		self.assertEqual(cl.clean_column_heading('phone$%#$'), 'PHONE') 
+		self.assertEqual(cl.clean_column_heading('_First_name?*__'), 'FIRST_NAME') 
+		
+	def test_clean_col_spaces(self):
+		self.assertEqual(cl.clean_column_heading('country of birth'), 'COUNTRY_OF_BIRTH') 
+		self.assertEqual(cl.clean_column_heading('country of birth '), 'COUNTRY_OF_BIRTH')  
+		self.assertEqual(cl.clean_column_heading(' country of birth'), 'COUNTRY_OF_BIRTH')  
+		
+		
+if __name__ == '__main__':
+	unittest.main()
