a3a8aec84194478db0a7b2b3c6498870e2595350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:10 +0000

    Add package django-glinks/0.1.1/django-glinks-0.1.1.tar.gz

---
+++ packages/django-glinks/django-glinks-0.1.1.tar.gz/django-glinks-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-glinks',
+    version='0.1.1',
+    author='Shawn Simon',
+    author_email='shawn.simon.developer@gmail.com',
+    packages=['glinks'],
+    url='http://pypi.python.org/pypi/django-glinks/',
+    license='LICENSE.txt',
+    description='Interal add manager for django sites.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.6.0",
+    ],
+)
