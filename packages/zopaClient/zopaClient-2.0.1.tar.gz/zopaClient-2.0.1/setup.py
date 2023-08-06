2d1c0d0f078f2868f64bd04c63bae2bbc0984a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:08 +0000

    Add package zopaClient/2.0.1/zopaClient-2.0.1.tar.gz

---
+++ packages/zopaClient/zopaClient-2.0.1.tar.gz/zopaClient-2.0.1/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright 2014 Jean-Francois Paris
+#
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import zopaclient
+
+setup(
+    name='zopaClient',
+    version="2.0.1",
+    description='python library for Zopa.com',
+    author='Jean-Francois Paris',
+    author_email='jfparis@rouge.eu.org',
+    package_dir={'':'.'},
+    url="https://github.com/jfparis/ZopaClient",
+    packages=['zopaclient'],
+    license='LGPL 2.0',
+    requires=[
+        'lxml',
+        'requests',
+        'cssselect',
+    ],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+
+    ),
+)
