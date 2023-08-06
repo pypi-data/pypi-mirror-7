9acc6e2da5c18c6ee508620447f91dbeef31a85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:59 +0000

    Add package rbPrintList/1.5.0/rbPrintList-1.5.0.zip

---
+++ packages/rbPrintList/rbPrintList-1.5.0.zip/rbPrintList-1.5.0/rbPrintList.py
@@ -0,0 +1,20 @@
+"""This module will help pint items in list of list.
+   Additionaly allowing nested items to be printed 
+   with tab stops. 
+"""
+import sys
+
+def print_lol(the_list,indent=False,level=0,filename=sys.stdout):
+	"""This is a recursive function
+	which takes three arguments. 
+	First one is a list
+	Second is optional switch to turn on/off the indenting of sublists 
+	Third is also an optional providing the starting tab value"""	  
+	for nested_item in the_list:
+		if isinstance(nested_item,list):
+			print_lol(nested_item,indent,level+1,filename)
+		else:	
+			if(indent):
+				for tab_stop in range(level):
+					print("\t",end="",file=filename)
+			print(nested_item,file=filename)
---
+++ packages/rbPrintList/rbPrintList-1.5.0.zip/rbPrintList-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'rbPrintList',
+		version = '1.5.0',
+		py_modules = ['rbPrintList'],
+		author = 'rb',
+		author_email = 'rb@rb.com',
+		url = 'http://rb.test.com',
+		description = 'A simple list printing module',
+	)
