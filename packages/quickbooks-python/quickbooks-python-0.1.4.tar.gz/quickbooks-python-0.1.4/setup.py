52afab15edb542fe5940137e9038611e64723ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:11 +0000

    Add package quickbooks-python/0.1.4/quickbooks-python-0.1.4.tar.gz

---
+++ packages/quickbooks-python/quickbooks-python-0.1.4.tar.gz/quickbooks-python-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quickbooks-python',
+    url='https://github.com/naudo/quickbooks-python',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'rauth'
+    ]
+)
