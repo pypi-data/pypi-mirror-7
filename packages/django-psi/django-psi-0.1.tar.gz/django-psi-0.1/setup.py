a70464fd5f01e56ab3e0d1273d1ff55a751976c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:28 +0000

    Add package django-psi/0.1/django-psi-0.1.tar.gz

---
+++ packages/django-psi/django-psi-0.1.tar.gz/django-psi-0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = __import__('psi').__version__
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-psi',
+    version=VERSION,
+    description='Google Pagespeed Insights for your Django project.',
+    author='Kevin Fricovsky',
+    author_email='kevin@montylounge.com',
+    url='https://github.com/montylounge/django-psi/',
+    packages=find_packages(),
+    install_requires=['Django>=1.6.5','google-api-python-client==1.2'],
+    download_url='https://github.com/montylounge/django-psi/archive/v0.1.zip',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
