61eb50684a5392e275daa030266a8058c262aec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:23 +0000

    Add package infi.recipe.application-packager/0.5.1/infi.recipe.application_packager-0.5.1.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.1.tar.gz/infi.recipe.application_packager-0.5.1/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8239e6d501cdfd7c0d7743710588810e5bd2033c'
+__git_head_subject__ = 'HOSTDEV-1586 passing -U to buildout so it will not read the user configuration from his home direcotry'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
