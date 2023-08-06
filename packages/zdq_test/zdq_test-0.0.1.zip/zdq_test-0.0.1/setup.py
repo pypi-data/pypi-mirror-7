a7a8f4a1cd824f45619cfefa2bab042703c87aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:05 +0000

    Add package zdq_test/0.0.1/zdq_test-0.0.1.zip

---
+++ packages/zdq_test/zdq_test-0.0.1.zip/zdq_test-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+setup(
+    name='zdq_test',
+    version='0.0.1',
+    keywords=('zdq_test'),
+    description='a python json serialize/deserialize library which is easy to use',
+    license='MIT License',
+    install_requires=[],
+    author='zhoudeqiang',
+    author_email='zhoudeqiang@outlook.com',
+    packages=find_packages(),
+    platforms='any',
+)
---
+++ packages/zdq_test/zdq_test-0.0.1.zip/zdq_test-0.0.1/zdq_test/__init__.py
@@ -0,0 +1,1 @@
+print("zdq test upload pypi")
