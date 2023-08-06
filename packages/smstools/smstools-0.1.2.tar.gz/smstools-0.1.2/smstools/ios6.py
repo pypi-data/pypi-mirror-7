e8f456799b728de1f6abdb58a7fd3f33c28db5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:10 +0000

    Add package smstools/0.1.2/smstools-0.1.2.tar.gz

---
+++ packages/smstools/smstools-0.1.2.tar.gz/smstools-0.1.2/smstools/ios6.py
@@ -0,0 +1,27 @@
+import sqlite3
+import core, sms_exceptions
+
+
+class IOS6:
+    """ iOS 6 sqlite reader and writer """
+
+
+    def parse(self, file):
+        """ Parse iOS 6 sqlite file to Text[] """
+
+        conn = sqlite3.connect(file)
+        c = conn.cursor()
+        texts = []
+        query = c.execute(
+            'SELECT handle.id, message.date, message.is_from_me, message.text, message.handle_id \
+             FROM message \
+             INNER JOIN handle ON message.handle_id = handle.ROWID \
+             ORDER BY message.ROWID ASC;')
+        for row in query:
+            txt = core.Text(row[0],long((row[1] + 978307200)*1000),(row[2]==1),row[3])
+            texts.append(txt)
+        return texts
+
+    def write(self, texts, outfile):
+        raise sms_exceptions.UnfinishedError("iOS output not yet implemented :/ (email me to help test!)!")
+        #TODO !!!
