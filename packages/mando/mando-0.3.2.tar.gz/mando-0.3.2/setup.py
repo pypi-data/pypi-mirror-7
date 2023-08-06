210dc9e69b9b56fd770db6a07c63ad25d0caf4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:12 +0000

    Add package mando/0.3.2/mando-0.3.2.tar.gz

---
+++ packages/mando/mando-0.3.2.tar.gz/mando-0.3.2/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup
+try:
+    import mando
+except ImportError as e:
+    version = e.version
+    deps = ['argparse']
+else:
+    version = mando.__version__
+    deps = []
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fobj:
+    readme = fobj.read()
+
+setup(name='mando',
+      version=version,
+      author='Michele Lacchia',
+      author_email='michelelacchia@gmail.com',
+      url='https://mando.readthedocs.org/',
+      download_url='https://pypi.python.org/mando/',
+      license='MIT',
+      description='Create Python CLI apps with little to no effort at all!',
+      platforms='any',
+      long_description=readme,
+      packages=['mando', 'mando.tests'],
+      install_requires=deps,
+      test_suite='mando.tests',
+      keywords='argparse,argument parser,arguments,cli,command line,'
+               'commands,decorator,dispatch,flags,getopt,options,optparse,'
+               'parser,subcommands',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+      ]
+)
