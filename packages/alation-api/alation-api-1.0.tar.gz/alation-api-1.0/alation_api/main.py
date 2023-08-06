716a9fde8fbb2279c32744c81ecd8a90b08730cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:49 +0000

    Add package alation-api/1.0/alation-api-1.0.tar.gz

---
+++ packages/alation-api/alation-api-1.0.tar.gz/alation-api-1.0/alation_api/main.py
@@ -0,0 +1,24 @@
+import api_setup_client as api_setup
+import api_request_client as api
+import argparse
+import sys
+
+
+def get_sql(*args):
+    try:
+        qid = int(sys.argv[2])
+        print api.get_query_sql(qid)
+    except ValueError:
+        print "Not a valid integer for query id"
+
+
+def get_result(*args):
+    try:
+        rid = int(sys.argv[2])
+        print api.get_result_csv(qid)
+    except ValueError:
+        print "Not a valid integer for result id"
+
+
+def setup():
+    api_setup.run_setup()
