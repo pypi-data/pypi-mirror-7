b3fe43ea3f0173602ac03534b781ea1694d51433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:30 +0000

    Add package Flask-Edits/0.4/Flask-Edits-0.4.tar.gz

---
+++ packages/Flask-Edits/Flask-Edits-0.4.tar.gz/Flask-Edits-0.4/flask_edits/editable.py
@@ -0,0 +1,44 @@
+"""Jinja extensions to mark sections as editable
+"""
+
+from collections import OrderedDict
+import hashlib
+
+from jinja2.nodes import Output, Template, TemplateData
+from jinja2.ext import Extension
+
+
+class EditableExtension(Extension):
+    tags = set(['editable'])
+
+    def parse(self, parser):
+        _db = self.environment.edits
+
+        # Skip begining node
+        parser.stream.next()
+
+        # Get section key
+        key = parser.parse_expression().value
+
+        # Read editable section
+        section = parser.parse_statements(['name:endeditable'], drop_needle=True)
+
+        # Render original section contents
+        compiled = self.environment.compile(Template(section), '', '')
+        original = self.environment.template_class.from_code(self.environment, compiled, {}, True).render()
+
+        _db.setdefault(parser.name, OrderedDict())
+        _db[parser.name].setdefault(key, OrderedDict())
+        _db[parser.name][key].setdefault('original', original.strip())
+        _db[parser.name][key].setdefault('edited', None)
+
+        if _db[parser.name][key].get('edited', None):
+            if self.environment.edits_preview:
+                if self.environment.globals['request'].args.get('preview'):
+                    return Output([TemplateData(_db[parser.name][key]['edited'])])
+                else:
+                    return section
+            else:
+                return Output([TemplateData(_db[parser.name][key]['edited'])])
+
+        return section
