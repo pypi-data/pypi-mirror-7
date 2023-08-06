7979c6e755ee23b2548e07128f0558aa201d3607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package urllib2_prior_auth/0.1.2/urllib2_prior_auth-0.1.2.tar.gz

---
+++ packages/urllib2_prior_auth/urllib2_prior_auth-0.1.2.tar.gz/urllib2_prior_auth-0.1.2/setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='urllib2_prior_auth',
+    version="0.1.2",
+    description='List all issues assigned to the user on GitHub',
+    author=u'Matěj Cepl',
+    author_email='mcepl@cepl.eu',
+    url='https://github.com/mcepl/github_list_issues',
+    scripts=['github_list_issues'],
+    keywords=['github', 'issues', 'developer'],
+    install_requires=['urllib2_prior_auth'],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
