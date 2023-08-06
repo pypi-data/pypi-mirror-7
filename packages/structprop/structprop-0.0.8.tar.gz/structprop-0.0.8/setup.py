3bd69fd56a180d027eddda809202860d4777eb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:07 +0000

    Add package structprop/0.0.8/structprop-0.0.8.tar.gz

---
+++ packages/structprop/structprop-0.0.8.tar.gz/structprop-0.0.8/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='structprop',
+      version='0.0.8',
+      description='Parser for structured property config file format',
+      author='Edgeware AB',
+      author_email='info@edgeware.tv',
+      license='MIT',
+      scripts=['structprop.py'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Software Development'
+      ])
