621f0c790a7d1b4fbe7e73d3c021d15614f6bf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:00 +0000

    Add package bencodepy/0.9.4/bencodepy-0.9.4.zip

---
+++ packages/bencodepy/bencodepy-0.9.4.zip/bencodepy-0.9.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import os
+
+description = ''
+if os.path.isfile('README.md'):
+    with open('README.md', 'r') as f:
+        description = f.read()
+
+setup(
+    name='bencodepy',
+    version='0.9.4',
+    packages=['bencodepy'],
+    url='https://github.com/eweast/bencodepy_opti',
+    license='GPLv2',
+    author='Eric Weast',
+    author_email='eweast@hotmail.com',
+    description='Bencode encoder/decoder written in Python 3 under the GPLv2.',
+    long_description=description
+)
