351ac395a37086295f64d44ce7691b2bc2b86bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package gsshapy/2.0.0/gsshapy-2.0.0.tar.gz

---
+++ packages/gsshapy/gsshapy-2.0.0.tar.gz/gsshapy-2.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'sqlalchemy>=0.7',
+    'mapkit>=1.1.0'
+    ]
+
+setup(name='gsshapy',
+      version='2.0.0',
+      description='An SQLAlchemy ORM for GSSHA model files.',
+      long_description='',
+      author='Nathan Swain',
+      author_email='nathan.swain@byu.net',
+      url='https://bitbucket.org/swainn/gsshapy',
+      license='BSD 2-Clause License',
+      keywords='GSSHA, database, object relational model',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite='gsshapy.tests.all_tests'
+      )
