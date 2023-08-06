8cf1b02766169ac4bfd97a519c16c7e8a516fa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:24 +0000

    Add package pyicloud/0.5.0/pyicloud-0.5.0.tar.gz

---
+++ packages/pyicloud/pyicloud-0.5.0.tar.gz/pyicloud-0.5.0/pyicloud/services/__init__.py
@@ -0,0 +1,4 @@
+from pyicloud.services.calendar import CalendarService
+from pyicloud.services.findmyiphone import FindMyiPhoneServiceManager
+from pyicloud.services.ubiquity import UbiquityService
+from contacts import ContactsService
