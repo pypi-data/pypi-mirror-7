32aa4e6f5ed0b136d82a1d6d3093b50dbb801ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:49 +0000

    Add package lifelogger/0.1.0/lifelogger-0.1.0.tar.gz

---
+++ packages/lifelogger/lifelogger-0.1.0.tar.gz/lifelogger-0.1.0/lifelogger/commands/google.py
@@ -0,0 +1,255 @@
+# coding=utf-8
+"""
+All commands that call the Google API service.
+"""
+import re
+import sys
+from datetime import datetime, timedelta
+
+import dateutil.parser
+
+from ..connection import connect
+from ..config import config
+
+from .parser import subparsers
+
+
+def quickadd(summary):
+    summary = ' '.join(summary)
+
+    service = connect()
+
+    # Double up single-time events to be 0-length
+    match = re.match(r'^\d\d:\d\d ', summary)
+    if match:
+        summary = match.group(0)[:-1] + '-' + summary
+
+    # Make request
+    print "Quick add >>", summary
+
+    result = service.events().quickAdd(
+        calendarId=config['calendar_id'],
+        text=summary
+    ).execute()
+
+    if result['status'] == 'confirmed':
+        print "Added! Link: ", result['htmlLink']
+        return True
+    else:
+        sys.stdout.write("Failed :( - status %s\n" % result['status'])
+        return False
+
+quickadd.parser = subparsers.add_parser(
+    'quickadd',
+    description="Use 'quick-add' to add an event to your calendar - works the "
+                "same as the Google Calendar web interface function, with "
+                "some extensions. To add a 0-minute event at a particular "
+                "time today, run with the time in 24-hour format at the "
+                "start, e.g. '10:00 Coffee'."
+)
+quickadd.parser.add_argument(
+    'summary',
+    help="The summary for the event.",
+    nargs='*'
+)
+quickadd.parser.set_defaults(func=quickadd)
+
+
+def now(summary, duration):
+    try:
+        offset = int(summary[0])
+        summary = summary[1:]
+    except ValueError:
+        offset = 0
+
+    summary = ' '.join(summary)
+
+    service = connect()
+
+    start = datetime.now() + timedelta(minutes=offset)
+    end = start + timedelta(minutes=duration)
+
+    print "Adding %i-minute event >> %s" % (duration, summary)
+
+    result = service.events().insert(
+        calendarId=config['calendar_id'],
+        body={
+            'summary': summary,
+            'start': {
+                'dateTime': start.isoformat(),
+                'timeZone': config['timezone']
+            },
+            'end': {
+                'dateTime': end.isoformat(),
+                'timeZone': config['timezone']
+            }
+        }
+    ).execute()
+
+    if result['status'] == 'confirmed':
+        print "Added! Link: ", result['htmlLink']
+        return True
+    else:
+        sys.stdout.write("Failed :( - status %s\n" % result['status'])
+        return False
+
+now.parser = subparsers.add_parser(
+    'now',
+    description="Adds an event 'right now'.")
+now.parser.add_argument(
+    '-d',
+    '--duration',
+    type=int,
+    default=0,
+    help="The duration of the event (default 0)"
+)
+now.parser.add_argument(
+    'summary',
+    nargs="+",
+    type=unicode,
+    help="The summary for the event."
+)
+now.parser.set_defaults(func=now)
+
+
+def for_command(duration, summary):
+    summary = ' '.join(summary)
+
+    service = connect()
+
+    times = [
+        datetime.now(),
+        datetime.now() + timedelta(minutes=duration)
+    ]
+    times.sort()
+    start, end = times
+
+    print "Adding %i-minute event >> %s" % (abs(duration), summary)
+
+    result = service.events().insert(
+        calendarId=config['calendar_id'],
+        body={
+            'summary': summary,
+            'start': {
+                'dateTime': start.isoformat(),
+                'timeZone': config['timezone']
+            },
+            'end': {
+                'dateTime': end.isoformat(),
+                'timeZone': config['timezone']
+            }
+        }
+    ).execute()
+
+    if result['status'] == 'confirmed':
+        print "Added! Link: ", result['htmlLink']
+        return True
+    else:
+        sys.stdout.write("Failed :( - status %s\n" % result['status'])
+        return False
+
+for_command.parser = subparsers.add_parser(
+    'for',
+    description="Adds an event that lasts *for* the specified number of "
+                "minutes, relative to now."
+)
+for_command.parser.add_argument(
+    'duration',
+    type=int,
+    help="The duration of the event. Give a negative number, and the event "
+         "will be set to have started 'duration' minutes ago, and end now; "
+         "otherwise it starts now and ends in 'duration' minutes time."
+)
+for_command.parser.add_argument(
+    'summary',
+    help="The summary for the event.",
+    nargs='*'
+)
+for_command.parser.set_defaults(func=for_command)
+
+
+def add(summary, start=None, end=None, duration=None):
+    summary = ' '.join(summary)
+
+    if start is None:
+        start = datetime.now()
+    else:
+        start = dateutil.parser.parse(start)
+
+    if end is not None:
+        end = dateutil.parser.parse(end)
+
+    if duration is None:
+        duration = 0
+
+    if end is None:
+        end = start + timedelta(minutes=duration)
+
+    service = connect()
+
+    times = [start, end]
+    times.sort()
+    start, end = times
+
+    duration = (end - start).total_seconds() / 60
+
+    print "Adding {length}-minute event at {start} >> {summary}".format(
+        length=abs(duration),
+        start=start,
+        summary=summary
+    )
+
+    result = service.events().insert(
+        calendarId=config['calendar_id'],
+        body={
+            'summary': summary,
+            'start': {
+                'dateTime': start.isoformat(),
+                'timeZone': config['timezone']
+            },
+            'end': {
+                'dateTime': end.isoformat(),
+                'timeZone': config['timezone']
+            }
+        }
+    ).execute()
+
+    if result['status'] == 'confirmed':
+        print "Added! Link: ", result['htmlLink']
+        return True
+    else:
+        sys.stdout.write("Failed :( - status %s\n" % result['status'])
+        return False
+
+
+add.parser = subparsers.add_parser(
+    'add',
+    description="Generic event adding command, with all the bells and "
+                "whistles."
+)
+add.parser.add_argument(
+    '-s',
+    '--start',
+    default=None,
+    help="The start time for the event - default is now."
+)
+add.parser.add_argument(
+    '-e',
+    '--end',
+    default=None,
+    help="The end time for the event - default is to make a 0-minute event."
+)
+add.parser.add_argument(
+    '-d',
+    '--duration',
+    default=None,
+    type=int,
+    help="The duration, in minutes, for the event. If both end and duration "
+         "are set, duration overrides. Can be negative."
+)
+add.parser.add_argument(
+    'summary',
+    help="The summary for the event.",
+    nargs='*'
+)
+add.parser.set_defaults(func=add)
---
+++ packages/lifelogger/lifelogger-0.1.0.tar.gz/lifelogger-0.1.0/lifelogger/commands/local.py
@@ -0,0 +1,244 @@
+# coding=utf-8
+"""
+All commands that create & use the local copies of the Google Calendar data.
+"""
+import requests
+
+from icalendar import Calendar
+
+from ..config import config, ICAL_PATH
+from ..utils import nice_format
+
+from .parser import subparsers
+
+
+def download(reset=None):
+    if reset:
+        config.pop('ical_url', None)
+
+    try:
+        ical_url = config['ical_url']
+    except KeyError:
+        print "To download the iCal file for analysis, you must give me the " \
+              "public URL for it."
+        print "Please go to the Google Calendar web interface " \
+              ", 'Calendar Settings', and then copy the link address from " \
+              "the ICAL button under 'Calendar Address'"
+        ical_url = raw_input("Paste --> ")
+        config['ical_url'] = ical_url
+
+    print "Downloading private iCal file..."
+    req = requests.get(ical_url, stream=True)
+
+    if req.status_code != 200:
+        print "Could not fetch iCal url - has it expired? "
+        print "To change, run download --reset"
+        print ical_url
+        return False
+
+    with open(ICAL_PATH, 'wb') as f:
+        for chunk in req.iter_content():
+            f.write(chunk)
+
+    print "Download successful!"
+
+    make_db()
+
+    return True
+
+download.parser = subparsers.add_parser(
+    'download',
+    description="Downloads the iCal that contains the whole of your Google "
+                "Calendar, and then parses it into the local database"
+)
+download.parser.add_argument(
+    '-r',
+    '--reset',
+    const=True,
+    default=False,
+    nargs='?',
+    help="Pass this in to force re-pasting in the iCal url, if e.g. the url "
+         " stored in lifelogger is no longer valid."
+)
+download.parser.set_defaults(func=download)
+
+
+def make_db():
+    from ..database import Event
+
+    print "Converting iCal file into sqlite database..."
+
+    with open(ICAL_PATH, 'rb') as f:
+        ical_data = f.read()
+
+    cal = Calendar.from_ical(ical_data)
+
+    try:
+        Event.drop_table()
+    except:
+        pass
+    try:
+        Event.create_table()
+    except:
+        pass
+
+    for event in cal.walk("VEVENT"):
+        Event.create_from_ical_event(event)
+
+    print "Imported {} events.".format(
+        Event.select().count()
+    )
+
+    return True
+
+make_db.parser = subparsers.add_parser(
+    'make_db',
+    description="Parses the downloaded iCal file into the local sqlite "
+                "database. Normally done when the download command is run, "
+                "but may need re-running on changes to lifelogger."
+)
+make_db.parser.set_defaults(func=make_db)
+
+
+def shell():
+    from datetime import datetime, date
+    from ..database import Event, regexp
+
+    # This is just to avoid 'unused import' linter errors
+    [datetime, date, Event, regexp]
+
+    from IPython import embed
+    embed()
+
+shell.parser = subparsers.add_parser(
+    'shell',
+    description="Loads the local database and an IPython shell so you can "
+                "manually search around the events using the 'peewee' ORM."
+)
+shell.parser.set_defaults(func=shell)
+
+
+def sql(statement, separator):
+    from ..database import conn
+    statement = ' '.join(statement)
+
+    cursor = conn.cursor()
+    cursor.execute(statement)
+
+    separator = {
+        'comma': ',',
+        'semicolon': ';',
+        'tab': '\t',
+    }[separator]
+
+    # Header
+    print separator.join([d[0] for d in cursor.description])
+
+    # Data
+    for row in cursor.fetchall():
+        print separator.join([str(v) for v in row])
+
+sql.parser = subparsers.add_parser(
+    'sql',
+    description="Execute a SQL statement direct on the db and output results "
+                "as csv."
+)
+sql.parser.add_argument(
+    'statement',
+    nargs="+",
+    type=unicode,
+    help="The SQL statement."
+)
+sql.parser.add_argument(
+    '-s',
+    '--separator',
+    nargs="?",
+    type=unicode,
+    default="comma",
+    choices=['comma', 'semicolon', 'tab'],
+    help="Separator for the output - default comma."
+)
+sql.parser.set_defaults(func=sql)
+
+
+def list_command(filter_re):
+    filter_re = ' '.join(filter_re)
+    from ..database import Event, regexp
+
+    events = Event.select().where(regexp(Event.summary, filter_re))
+
+    for event in events:
+        print event.display()
+
+    return True
+
+list_command.parser = subparsers.add_parser(
+    'list',
+    description="Lists the events that match a given regex."
+)
+list_command.parser.add_argument(
+    'filter_re',
+    nargs="+",
+    type=unicode,
+    help="The regex to filter events by."
+)
+list_command.parser.set_defaults(func=list_command)
+
+
+def csv(filter_re, separator, varnames):
+    filter_re = ' '.join(filter_re)
+
+    varnames = varnames.split(',')
+
+    separator = {
+        'comma': ',',
+        'semicolon': ';',
+        'tab': '\t',
+    }[separator]
+
+    from ..database import Event, regexp
+
+    events = Event.select().where(regexp(Event.summary, filter_re))
+
+    # Header
+    print separator.join(varnames)
+
+    # Data
+    for event in events:
+        print separator.join([
+            nice_format(event.get_var(varname)) for varname in varnames
+        ])
+
+
+csv.parser = subparsers.add_parser(
+    'csv',
+    description="Used to output properties of events that a given filter as "
+                "CSV data."
+)
+csv.parser.add_argument(
+    '-s',
+    '--separator',
+    nargs="?",
+    type=unicode,
+    default="comma",
+    choices=['comma', 'semicolon', 'tab'],
+    help="Separator for the output - default comma."
+)
+csv.parser.add_argument(
+    '-v',
+    '--varnames',
+    nargs="?",
+    type=unicode,
+    default="start,end,summary",
+    help="A comma-separated list of the Event variables to output (options: "
+         "start, end, summary, duration_seconds, duration_minutes, "
+         "duration_hours, units, percentage, kg, mg). "
+         "Defaults to 'start,end,summary'."
+)
+csv.parser.add_argument(
+    'filter_re',
+    nargs="+",
+    type=unicode,
+    help="The regex to filter events by."
+)
+csv.parser.set_defaults(func=csv)
