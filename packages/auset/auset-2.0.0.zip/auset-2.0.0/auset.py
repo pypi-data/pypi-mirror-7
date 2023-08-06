7b88994e27feef725f933c1c10e55f0a6d87b37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:01 +0000

    Add package auset/2.0.0/auset-2.0.0.zip

---
+++ packages/auset/auset-2.0.0.zip/auset-2.0.0/auset.py
@@ -0,0 +1,35 @@
+def built(name_setup, version, description, name_py, place=0):
+    
+    
+    """The "name_setup" is what does your moduel's name,
+
+       next "version" just your coding's version,and then
+
+       "description" is its mean,finally "name_py" of index
+
+       is that you want to bulit file.py and "place" can
+
+       change where do you save setup.py place ."""
+    
+    """<PS> . All index must add <""> or <''> in their left
+
+       and right side."""
+    
+    """Note that if you have the turn storedirectory, it will
+
+       change the loadable module's default archive path
+
+       (To store, "another directory",please enter a new location)"""
+    
+    import os   
+    
+    if place != 0:
+        os.chdir(place)
+    else:
+        pass   #如果前面的指令已有設定過新位置 因為os是整個移動所在目錄 所以後來再用即使沒設定位置也會存在之前移去的目錄(尋找能否有暫時性移位置或能自動移回來的方法)
+    
+    try:
+        with open('setup.py', 'w')as f:
+            print('import sys\nfrom cx_Freeze import setup, Executable\n# Dependencies are automatically detected, but it might need fine tuning.\nbuild_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}\n# GUI applications require a different base on Windows (the default is for a\n# console application).\nbase = None\nif sys.platform == "win32":\n\tbase = "Win32GUI"\nsetup(  name = '+'"'+name_setup+'"'+',\n\tversion = "'+version+'",\n\tdescription = '+'"'+description+'"'+',\n\toptions = {"build_exe": build_exe_options},\n\texecutables = [Executable('+'"'+name_py+'"'+', base=base)])', file=f)
+    except IOError as ioerr:
+                  print("File error :"+ str(ioerr))
---
+++ packages/auset/auset-2.0.0.zip/auset-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         = 'auset',
+    version      = '2.0.0',
+    py_modules   = ['auset'],
+    author       = 'V.p',
+    author_email = 'tpesluckyt@gmail.com',
+    url 	 = 'http://www.tpeslucky.com',
+    description  = 'This is a automatic moduel for making cx_Freeze setup! v2.0.0 add new change save place!',
+    )
