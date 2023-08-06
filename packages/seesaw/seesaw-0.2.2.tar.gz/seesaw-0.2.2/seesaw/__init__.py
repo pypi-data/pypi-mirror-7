7c2b7fd372f41e9deb83fbb839ee1e18bb84e65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:01 +0000

    Add package seesaw/0.2.2/seesaw-0.2.2.tar.gz

---
+++ packages/seesaw/seesaw-0.2.2.tar.gz/seesaw-0.2.2/seesaw/__init__.py
@@ -0,0 +1,8 @@
+# TODO: i'm not sure why we need all
+__all__ = ['config', 'event', 'externalprocess', 'item', 'pipeline', 'project', 'runner', 'util', 'task', 'tracker', 'warrior', 'web']
+
+__title__ = 'seesaw'
+__version__ = '0.2.2'
+__build__ = 0x00202
+runner_type = ""
+warrior_build = ""
