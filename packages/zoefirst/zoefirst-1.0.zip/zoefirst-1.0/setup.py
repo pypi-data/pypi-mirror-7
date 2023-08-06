b029e10382099a4387496c4e1dd678d7359eb759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:04 +0000

    Add package zoefirst/1.0/zoefirst-1.0.zip

---
+++ packages/zoefirst/zoefirst-1.0.zip/zoefirst-1.0/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+setup(
+		name = 'zoefirst',
+		version = '1.0',
+		py_modules = ['zoefirst'],
+		author = 'zhuo',
+		author_email = 'idorable@163.com',
+		url = 'null',
+		description = 'my first',
+	)
---
+++ packages/zoefirst/zoefirst-1.0.zip/zoefirst-1.0/zoefirst.py
@@ -0,0 +1,13 @@
+#my first pyhton file!
+"""这是一个多行注释！"""
+def print_lol(a_list):
+
+    for each_item in a_list:
+
+        if isinstance(each_item, list):
+
+            print_lol(each_item)
+
+        else:
+
+            print(each_item)
