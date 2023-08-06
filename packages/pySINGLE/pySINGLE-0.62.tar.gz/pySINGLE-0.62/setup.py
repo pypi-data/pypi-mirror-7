6a56072f5410fae41033de0a1a0c787ada236f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:59 +0000

    Add package pySINGLE/0.62/pySINGLE-0.62.tar.gz

---
+++ packages/pySINGLE/pySINGLE-0.62.tar.gz/pySINGLE-0.62/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy
+
+setup(name='pySINGLE', 
+    packages = ['pySINGLE'],
+    version='0.62', 
+    description = 'Python implementation of SINGLE algorithm',
+    author = 'Ricardo Pio Monti',
+    author_email = 'ricardo.monti08@gmail.com',
+    url = 'https://github.com/piomonti/pySINGLE',
+    download_url = 'https://github.com/piomonti/pySINGLE/tarball/0.1',
+    ext_modules = [Extension("Z_shooting", ["Z_shooting.pyx"],)],
+    include_dirs=[numpy.get_include(),'.', ],
+    #py_modules=['pySINGLE']
+    )
