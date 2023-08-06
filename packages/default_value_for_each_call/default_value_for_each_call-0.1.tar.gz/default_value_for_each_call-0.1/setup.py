ac388faa6f8f3cc1ef272c2d641d9c45c7a2c81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:34 +0000

    Add package default_value_for_each_call/0.1/default_value_for_each_call-0.1.tar.gz

---
+++ packages/default_value_for_each_call/default_value_for_each_call-0.1.tar.gz/default_value_for_each_call-0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+__author__ = 'chenfengyuan'
+
+from setuptools import setup
+
+setup(
+    name="default_value_for_each_call",
+    version="0.1",
+    author="Chen Fengyuan",
+    author_email="jeova.sanctus.unus@gmail.com",
+    description="compute function default value for each call",
+    license="BSD",
+    keywords="example documentation tutorial",
+    py_modules=['default_value_for_each_call'],
+)
