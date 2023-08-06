cbaf1545340ddd4f0dbf273037117d9387e735f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package hellopypi/1.0/hellopypi-1.0.tar.gz

---
+++ packages/hellopypi/hellopypi-1.0.tar.gz/hellopypi-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name = "hellopypi",
+        version = "1.0",
+        author = "Simon Desaulniers",
+        author_email = "rostydela@gmail.com",
+        description = "Trivial python package uploaded as test pypi test",
+        classifiers = ['Topic :: Education :: Testing'],
+)
