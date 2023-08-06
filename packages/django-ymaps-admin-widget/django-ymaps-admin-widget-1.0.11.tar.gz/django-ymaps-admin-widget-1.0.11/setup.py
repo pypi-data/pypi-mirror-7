d74541481f458614407f93f7e18b66ce843684ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:06 +0000

    Add package django-ymaps-admin-widget/1.0.11/django-ymaps-admin-widget-1.0.11.tar.gz

---
+++ packages/django-ymaps-admin-widget/django-ymaps-admin-widget-1.0.11.tar.gz/django-ymaps-admin-widget-1.0.11/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+from distutils.core import setup
+from yandexmaps_widget import __version__
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ymaps-admin-widget',
+    version=__version__,
+    packages=['yandexmaps_widget'],
+    url='https://github.com/DrMartiner/django-ymaps-admin-widget',
+    license='MIT',
+    author='Alexey Kuzmin',
+    author_email='DrMartiner@GMail.Com',
+    description='Widget admin for Yandex Maps',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
