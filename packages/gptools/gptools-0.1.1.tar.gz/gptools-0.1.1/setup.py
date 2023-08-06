dec06a5ab2696ee8dde60c0d5ec75154f5bdfc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:33 +0000

    Add package gptools/0.1.1/gptools-0.1.1.tar.gz

---
+++ packages/gptools/gptools-0.1.1.tar.gz/gptools-0.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='gptools',
+    version='0.1.1',
+    packages=['gptools', 'gptools.kernel'],
+    requires=['scipy', 'numpy', 'matplotlib', 'mpmath', 'emcee', 'triangle_plot'],
+    author='Mark Chilenski',
+    author_email='mark.chilenski@gmail.com',
+    url='https://github.com/markchil/gptools',
+    description='Gaussian process regression with derivative constraints and predictions.',
+    long_description=open('README.rst', 'r').read(),
+    license='GPL'
+)
