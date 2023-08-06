058e5c1bd3627d2a32d2550188de07b8963bc046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:17 +0000

    Add package timestamp/0.0.1/timestamp-0.0.1.tar.gz

---
+++ packages/timestamp/timestamp-0.0.1.tar.gz/timestamp-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup	
+
+setup(
+	name='timestamp',
+	version='0.0.1',
+	description='shortcut for converting datetime to timestamp',
+	author='yangchen',
+	author_email='yuhan534@126.com',
+	url='http://github.com/jarvys/timestamp',
+	install_requires=[],
+	py_modules=['timestamp'],
+	scripts=[],
+	keywords=[]
+)
+
