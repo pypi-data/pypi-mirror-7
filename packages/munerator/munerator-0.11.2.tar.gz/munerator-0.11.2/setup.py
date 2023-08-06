8dbfc49ca63e73d67fcc21b88f5e2c3c810088cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:41 +0000

    Add package munerator/0.11.2/munerator-0.11.2.tar.gz

---
+++ packages/munerator/munerator-0.11.2.tar.gz/munerator-0.11.2/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+setup(
+    name='munerator',
+    version="0.11.2",
+    description='Manager of OpenArena battles',
+    long_description=read("README.rst"),
+    author='Johan Bloemberg',
+    author_email='mail@ijohan.nl',
+    url='https://github.com/aequitas/munerator',
+    install_requires=[
+        'six',
+        'docopt',
+        'pyzmq',
+        'webcolors',
+        'python-Levenshtein',
+        'python-dateutil',
+        'rcfile'
+    ],
+    extras_require={
+        'db': [
+            'mongoengine',
+            'eve-mongoengine',
+            'eve==0.3',
+            'flask',
+        ]
+    },
+    packages=find_packages(),
+    license=read("LICENSE"),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points={
+        'console_scripts': [
+            "munerator = munerator:main",
+        ]
+    },
+)
