05373a554985febeef112b9c925f5fdd1688652d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:42 +0000

    Add package jsonpickle/0.8.0/jsonpickle-0.8.0.tar.gz

---
+++ packages/jsonpickle/jsonpickle-0.8.0.tar.gz/jsonpickle-0.8.0/tests/runtests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 John Paulett (john -at- paulett.org)
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+import os
+import sys
+
+testdir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(1, os.path.dirname(testdir))
+
+import unittest
+
+import backends_tests
+import datetime_tests
+import document_test
+import handler_tests
+import jsonpickle_test
+import object_test
+import thirdparty_tests
+import util_tests
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(util_tests.suite())
+    suite.addTest(handler_tests.suite())
+    suite.addTest(backends_tests.suite())
+    suite.addTest(jsonpickle_test.suite())
+    suite.addTest(datetime_tests.suite())
+    suite.addTest(document_test.suite())
+    suite.addTest(object_test.suite())
+    suite.addTest(thirdparty_tests.suite())
+    return suite
+
+
+def main():
+    return unittest.TextTestRunner(verbosity=2).run(suite())
+
+
+if __name__ == '__main__':
+    sys.exit(not main().wasSuccessful())
