686042e22f70c1e7869bbfa57849b9ee687bbec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:19 +0000

    Add package celery_geolocator/0.0.10/celery_geolocator-0.0.10.tar.gz

---
+++ packages/celery_geolocator/celery_geolocator-0.0.10.tar.gz/celery_geolocator-0.0.10/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+requirements = [
+    'celery>=3.1.11',
+    'geopy>=0.99'
+]
+
+
+setup(
+    name='celery_geolocator',
+    version='0.0.10',
+    description='Celery Geolocator',
+    long_description="A celery wrapper around geopy",
+    author='Brent Payne',
+    author_email='brent.payne@gmail.com',
+    url='http://www.github.com/brentpayne/celery-geolocator',
+    packages=[
+        'celery_geolocator',
+    ],
+    install_requires=requirements,
+    keywords=['celery', 'geopy', 'geolocate', 'celery-geolocator', 'geolocator', 'celery_geolocator'],
+    classifiers=[
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        ("License :: OSI Approved :: GNU Lesser General Public License v3" +
+         " (LGPLv3)")
+    ], requires=['celery', 'geopy']
+
+)
