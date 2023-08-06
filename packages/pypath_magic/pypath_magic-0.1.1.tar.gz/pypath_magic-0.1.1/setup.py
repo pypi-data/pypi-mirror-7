568b351a9da7590541d6f3c550b873efdbc18506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package pypath_magic/0.1.1/pypath_magic-0.1.1.tar.gz

---
+++ packages/pypath_magic/pypath_magic-0.1.1.tar.gz/pypath_magic-0.1.1/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import setuptools
+
+
+VERSION = '0.1.1'
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup_kwargs = {
+    'name': 'pypath_magic',
+    'description': "Python magic to manipulate the Python path",
+    'long_description': long_description,
+    'author': 'Tony S. Yu',
+    'author_email': 'tsyu80@gmail.com',
+    'py_modules': ['pypath_magic'],
+    'version': VERSION,
+    'license': 'Modified BSD',
+    'url': 'http://tonysyu.github.com/pypath_magic',
+    'download_url': 'http://tonysyu.github.com/pypath_magic',
+    'classifiers': [
+        'Topic :: Utilities',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Topic :: Desktop Environment :: File Managers',
+    ],
+    'keywords': ['PYTHONPATH', 'utility', 'IPython'],
+    'data_files': [('', ['LICENSE', 'README.rst']),],
+    'install_requires': ['IPython >= 1.0'],
+}
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_kwargs)
