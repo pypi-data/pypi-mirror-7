ec1850a17e3b88371c530340cbd96a47c3558de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:13 +0000

    Add package pygcvs/0.1.4/pygcvs-0.1.4.tar.gz

---
+++ packages/pygcvs/pygcvs-0.1.4.tar.gz/pygcvs-0.1.4/pygcvs/__init__.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals, print_function
+
+from .parser import GcvsParser
+
+try:
+    import ephem
+except ImportError:
+    ephem = None
+
+
+__version_info__ = (0, 1, 4, 'final', 0)
+
+
+def get_version():
+    version = '%s.%s' % (__version_info__[0], __version_info__[1])
+    if __version_info__[2]:
+        version = '%s.%s' % (version, __version_info__[2])
+    if __version_info__[3] != 'final':
+        version = '%s%s' % (version, __version_info__[3])
+        if __version_info__[4]:
+            version = '%s%s' % (version, __version_info__[4])
+    return version
+
+
+__version__ = get_version()
+
+
+def read_gcvs(filename):
+    """
+    Reads variable star data in `GCVS format`_.
+
+    :param filename: path to GCVS data file (usually ``iii.dat``)
+
+    .. _`GCVS format`: http://www.sai.msu.su/gcvs/gcvs/iii/html/
+    """
+    with open(filename, 'rb') as fp:
+        parser = GcvsParser(fp)
+        for star in parser:
+            yield star
+
+
+def dict_to_body(star_dict):
+    """
+    Converts a dictionary of variable star data to a `Body` instance.
+
+    Requires `PyEphem <http://rhodesmill.org/pyephem/>`_ to be installed.
+    """
+    if ephem is None:
+        raise NotImplementedError("Please install PyEphem in order to use dict_to_body.")
+    body = ephem.FixedBody()
+    body.name = star_dict['name']
+    body._ra = ephem.hours(str(star_dict['ra']))
+    body._dec = ephem.degrees(str(star_dict['dec']))
+    body._epoch = ephem.J2000
+    return body
---
+++ packages/pygcvs/pygcvs-0.1.4.tar.gz/pygcvs-0.1.4/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pygcvs',
+    version=__import__('pygcvs').__version__,
+    description='A Python library for reading variable star data from GCVS.',
+    long_description=read('README.rst'),
+    author='Zbigniew Siciarz',
+    author_email='zbigniew@siciarz.net',
+    url='http://github.com/zsiciarz/pygcvs',
+    download_url='http://pypi.python.org/pypi/pygcvs',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    tests_require=['nose', 'six'],
+    test_suite='nose.collector',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Utilities'
+    ],
+)
