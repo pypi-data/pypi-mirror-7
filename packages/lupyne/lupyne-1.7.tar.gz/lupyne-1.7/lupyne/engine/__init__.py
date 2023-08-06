a15e924a32686d0eff8643123e7638ab227bc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:21 +0000

    Add package lupyne/1.7/lupyne-1.7.tar.gz

---
+++ packages/lupyne/lupyne-1.7.tar.gz/lupyne-1.7/lupyne/engine/__init__.py
@@ -0,0 +1,19 @@
+"""
+Pythonic wrapper around `PyLucene <http://lucene.apache.org/pylucene/>`_ search engine.
+
+Provides high-level interfaces to indexes and documents,
+abstracting away java lucene primitives.
+"""
+
+import warnings
+import lucene
+warnings.simplefilter('default', DeprecationWarning)
+
+from .queries import Query, SortField, TermsFilter
+from .documents import Document, Field, MapField, NestedField, DocValuesField, NumericField, DateTimeField
+from .indexers import TokenFilter, Analyzer, IndexSearcher, MultiSearcher, IndexWriter, Indexer, ParallelIndexer
+from .spatial import PointField, PolygonField
+
+assert lucene.VERSION >= '4.6'
+if lucene.VERSION < '4.8':
+    warnings.warn('Support for lucene 4.6 and 4.7 will be removed in the next release.', DeprecationWarning)
