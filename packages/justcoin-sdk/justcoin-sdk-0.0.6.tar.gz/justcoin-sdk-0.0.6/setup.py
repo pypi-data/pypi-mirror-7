85af0f3f3b9fd9a3d66e4f885c99a6f0a1359bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:55 +0000

    Add package justcoin-sdk/0.0.6/justcoin-sdk-0.0.6.tar.gz

---
+++ packages/justcoin-sdk/justcoin-sdk-0.0.6.tar.gz/justcoin-sdk-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='justcoin-sdk',
+    version='0.0.6',
+    packages=['justcoin',],
+    license='MIT',
+    description = "Python SDK for the Justcoin API",
+    author = "Vasco Pinho",
+    author_email = "vascogpinho@gmail.com",
+    url = "https://github.com/vascop/justcoin-sdk",
+    download_url = "https://github.com/vascop/justcoin-sdk/zipball/master",
+    keywords = ["sdk", "justcoin", "api"],
+    install_requires=[
+        "requests",
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet",
+        ],
+    long_description=open('README.rst').read(),
+)
