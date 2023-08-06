88b2882618ec858c49e45114abe7c77f3bd3ab40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:38 +0000

    Add package fortest/1.6.0/fortest-1.6.0.tar.gz

---
+++ packages/fortest/fortest-1.6.0.tar.gz/fortest-1.6.0/fortest.py
@@ -0,0 +1,11 @@
+import sys
+""" this is an area for list """
+def print_lol(the_list,indent=False,level=0,fn=sys.stdout):
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,indent,level+1,fn)
+                else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t",end='',file=fn)
+                        print(each_item,file=fn)
