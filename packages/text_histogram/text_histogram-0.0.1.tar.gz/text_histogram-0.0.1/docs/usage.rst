30ad88f589306dac6643706d144e867813255cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:03 +0000

    Add package text_histogram/0.0.1/text_histogram-0.0.1.tar.gz

---
+++ packages/text_histogram/text_histogram-0.0.1.tar.gz/text_histogram-0.0.1/docs/usage.rst
@@ -0,0 +1,7 @@
+========
+Usage
+========
+
+To use cli_histogram in a project::
+
+	import cli_histogram
---
+++ packages/text_histogram/text_histogram-0.0.1.tar.gz/text_histogram-0.0.1/tests/test_cli_histogram.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_cli_histogram
+----------------------------------
+
+Tests for `cli_histogram` module.
+"""
+
+import unittest
+
+from cli_histogram import cli_histogram
+
+
+class TestCli_histogram(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
