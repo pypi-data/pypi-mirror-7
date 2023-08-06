539a455d0f1c2bc9c0f650d8ea4d5accefbfe9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:53 +0000

    Add package boxclient/1.0.0/boxclient-1.0.0.tar.gz

---
+++ packages/boxclient/boxclient-1.0.0.tar.gz/boxclient-1.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+import os
+
+from distutils.core import setup
+
+
+setup(name='boxclient',
+      version='1.0.0',
+      description='Box API v2.0 client',
+      author='Roberto Aguilar',
+      author_email='r@rreboto.com',
+      packages=['box'],
+      long_description=open('README.md').read(),
+      url='http://github.com/rca/box',
+      license='LICENSE',
+)
