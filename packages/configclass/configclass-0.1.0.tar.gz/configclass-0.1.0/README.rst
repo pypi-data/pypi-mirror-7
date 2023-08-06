7c772b7fb2fdc50a3dc3f330f6f70e7f54e078df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:56 +0000

    Add package configclass/0.1.0/configclass-0.1.0.tar.gz

---
+++ packages/configclass/configclass-0.1.0.tar.gz/configclass-0.1.0/README.rst
@@ -0,0 +1,96 @@
+configclass - A Python to class to hold configuration values
+==============================================================
+
+.. display some badges
+
+.. image:: https://travis-ci.org/schettino72/configclass.png?branch=master
+  :target: https://travis-ci.org/schettino72/configclass
+
+.. image:: https://coveralls.io/repos/schettino72/configclass/badge.png
+        :target: https://coveralls.io/r/schettino72/configclass
+
+
+
+
+A `Config` is a `dict` with a where:
+
+ * existing items can be modified but no items can not be added
+ * has `make()` method so you can easily created derived configs
+ * `make()` has the same API as `dict.update()`
+ * `make()` will merge values according to `mergedict.ConfigDict.merge()`
+ * for convenience, make can take a `None` to perform a simple copy
+
+
+::
+
+    >>> from configclass import Config
+
+    >>> c1 = Config({'a': 1, 'b': ['foo']})
+
+    # can't add new items to config
+    >>> c1.make({'a':2, 'c': [2]})
+    Traceback (most recent call last):
+    KeyError: 'New items can not be added to Config, invalid key:c'
+
+    # new config object created
+    >>> c2 = c1.make({'a':2})
+    >>> c2
+    Config({'a': 2, 'b': ['foo']})
+
+    # original object is not modified
+    >>> c1
+    Config({'a': 1, 'b': ['foo']})
+
+    # make() can take keyword arguments, note how lists are merged
+    >>> c2.make(b=['bar'])
+    Config({'a': 2, 'b': ['foo', 'bar']})
+
+
+
+`configclass.ConfigMixin` can be used to create a `Config` class
+that is not based on `mergedict.ConfigDict`. Check unittests for usage.
+
+
+
+Project Details
+===============
+
+- Project management on github - https://github.com/schettino72/configclass/
+
+
+license
+=======
+
+The MIT License
+Copyright (c) 2014 Eduardo Naufel Schettino
+
+see LICENSE file
+
+
+developers / contributors
+==========================
+
+- Eduardo Naufel Schettino
+
+
+install
+=======
+
+::
+
+ $ pip install configclass
+
+or download and::
+
+ $ python setup.py install
+
+
+tests
+=======
+
+Install dependencies in `dev_requirements.txt`.
+
+To run the tests::
+
+  $ py.test
+
---
+++ packages/configclass/configclass-0.1.0.tar.gz/configclass-0.1.0/configclass.py
@@ -0,0 +1,98 @@
+
+# configclass
+
+# The MIT License
+# Copyright (c) 2014 Eduardo Naufel Schettino
+# See LICENSE for details
+
+__version__ = (0, 1, 0)
+
+
+import copy
+
+from mergedict import ConfigDict
+
+
+class ConfigMixin(object):
+    """A dict with a `make()` to easy the creation with derived values.
+
+    New items can not be added to dict after its creation.
+    """
+    def __setitem__(self, key, value):
+        """make sure new items are not added after initialization"""
+        if key not in self:
+            msg = 'New items can not be added to Config, invalid key:{}'
+            raise KeyError(msg.format(key))
+        super(ConfigMixin, self).__setitem__(key, value)
+
+    def __repr__(self):
+        items = ['{!r}: {!r}'.format(k, v) for k,v in sorted(self.items())]
+        return '{}({{{}}})'.format(self.__class__.__name__, ', '.join(items))
+
+    # http://stackoverflow.com/questions/2060972
+    # subclassing-python-dictionary-to-override-setitem
+    def update(self, *args, **kwargs):
+        """overwrite `update` method so custom `__setitem__` is called"""
+        if args:
+            if len(args) > 1:
+                raise TypeError("update expected at most 1 arguments, "
+                                "got %d" % len(args))
+            other = dict(args[0])
+            for key in other:
+                self[key] = other[key]
+        for key in kwargs:
+            self[key] = kwargs[key]
+
+    def setdefault(self, key, value=None):
+        """overwrite `setdefault` method so custom `__setitem__` is called"""
+        if key not in self:
+            self[key] = value
+        return self[key]
+    # end - redefinition of methods to make sure __setitem__ is always called
+
+    def copy(self):
+        """copy that returns a Config object instead of plain dict"""
+        return self.__class__(self)
+
+    def as_dict(self):
+        """return config as plain dict"""
+        return dict(self)
+
+    # implement magic methods used on `copy` module
+    __copy__ = copy
+
+    def __deepcopy__(self, memo):
+        return self.__class__(copy.deepcopy(self.as_dict(), memo))
+
+
+    # non-dict methods
+    def make(self, *args, **kwargs):
+        """Returns a new Config object, updating with given values.
+
+        Arguments are same as dict.update().
+
+        Also accepts None as single argument, in this case just return a copy
+        of self.
+        """
+        result = copy.deepcopy(self)
+        if not(args and args[0] is None):
+            result.update(*args, **kwargs)
+        return result
+
+
+
+class Config(ConfigMixin, ConfigDict):
+    # overwrite to use ConfigDict.merge() instead of update
+    def make(self, *args, **kwargs):
+        """Returns a new Config object, updating with given values.
+
+        Arguments are same as dict.update().
+
+        Also accepts None as single argument, in this case just return a copy
+        of self.
+        """
+        result = copy.deepcopy(self)
+        if not(args and args[0] is None):
+            result.merge(*args, **kwargs)
+        return result
+
