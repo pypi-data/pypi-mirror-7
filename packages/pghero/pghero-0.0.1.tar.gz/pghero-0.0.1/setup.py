6ad8b46f9717b63605b0aef4f5bf8859f29b2cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:43 +0000

    Add package pghero/0.0.1/pghero-0.0.1.tar.gz

---
+++ packages/pghero/pghero-0.0.1.tar.gz/pghero-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='pghero',
+        version='0.0.1',
+        packages=['.'],
+        license='MIT',
+        long_description=open('README.md').read(),
+)
