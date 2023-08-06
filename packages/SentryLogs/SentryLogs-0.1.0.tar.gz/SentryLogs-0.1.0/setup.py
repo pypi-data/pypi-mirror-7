68e745dd9e1b3794c21af772ca6baed40acea964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:09 +0000

    Add package SentryLogs/0.1.0/SentryLogs-0.1.0.tar.gz

---
+++ packages/SentryLogs/SentryLogs-0.1.0.tar.gz/SentryLogs-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='SentryLogs',
+    version='0.1.0',
+    author='Mauro De Giorgi',
+    author_email='mauro.degiorgi@gmail.com',
+    scripts=['bin/sentrylogs',],
+    packages=['sentrylogs', 'sentrylogs.conf', 'sentrylogs.parsers'],
+    url='http://pypi.python.org/pypi/SentryLogs/',
+    license='LICENSE.txt',
+    description='Send logs to Django Sentry.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "raven >= 2.0.10",
+        "tailer >= 0.3",
+        ],
+)
