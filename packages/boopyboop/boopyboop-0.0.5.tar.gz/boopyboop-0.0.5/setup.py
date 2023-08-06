bccb95e2744a066ccb8688a95309c164ce2c4a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:04 +0000

    Add package boopyboop/0.0.5/boopyboop-0.0.5.tar.gz

---
+++ packages/boopyboop/boopyboop-0.0.5.tar.gz/boopyboop-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+
+setup(
+	name='boopyboop',
+	version="0.0.5",
+	description="Turn long numbers in whatever format to human-readable long series of words",
+	url="https://github.com/howonlee/boopyboop",
+	author="Howon Lee",
+	author_email="howon@howonlee.com",
+	license="MIT",
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+		],
+	install_requires=['python-baseconv'],
+	packages=['boopyboop']
+	)
+
