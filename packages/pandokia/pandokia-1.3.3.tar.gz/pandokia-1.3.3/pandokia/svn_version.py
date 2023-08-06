65c9f14dc51ac3d393f9e7dae3c38a6f03692728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package pandokia/1.3.3/pandokia-1.3.3.tar.gz

---
+++ packages/pandokia/pandokia-1.3.3.tar.gz/pandokia-1.3.3/pandokia/svn_version.py
@@ -0,0 +1,15 @@
+__svn_version__ = '1264'
+
+__full_svn_info__ = '''
+Path: .
+URL: https://svn.stsci.edu/svn/ssb/etal/pandokia/branches/release_1.3.3
+Repository Root: https://svn.stsci.edu/svn/ssb/etal
+Repository UUID: d34015c8-bcbb-4646-8ac8-8ba5febf221d
+Revision: 1264
+Node Kind: directory
+Schedule: normal
+Last Changed Author: sienkiew
+Last Changed Rev: 1263
+Last Changed Date: 2014-06-06 12:05:59 -0400 (Fri, 06 Jun 2014)
+'''
+
