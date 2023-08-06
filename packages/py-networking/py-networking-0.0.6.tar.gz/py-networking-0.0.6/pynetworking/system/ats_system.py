a4c91767c03cf810aab7059c0c1ed8e5f7d16c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:43 +0000

    Add package py-networking/0.0.6/py-networking-0.0.6.tar.gz

---
+++ packages/py-networking/py-networking-0.0.6.tar.gz/py-networking-0.0.6/pynetworking/system/ats_system.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import re
+from time import sleep
+import socket
+import logging
+import zmq
+import json
+
+log = logging.getLogger(__name__)
+
+class ats_system(object):
+    """
+    System for ATS
+    """
+    def __init__(self, device):
+        self._d =  device
+
+    def get_config(self):
+        self._d.log_info('getting device configuration')
+        config = ''
+        for line in self._d.cmd('show running-config').replace('\r','').split('\n'):
+            config += line+'\n'
+        self._d.log_debug('got device configuration \n{0}'.format(config))
+        return config
+
+    def get_startup_config(self):
+        self._d.log_info('getting device configuration')
+        config = ''
+        for line in self._d.cmd('show startup-config').replace('\r','').split('\n'):
+            config += line+'\n'
+        self._d.log_debug('got device configuration \n{0}'.format(config))
+        return config
+
+    def save_config(self):
+        self._d.log_info('save running configuration')
+        cmds = {'cmds':[{'cmd': 'copy r s', 'prompt':''},
+                        {'cmd': 'y'       , 'prompt':'\#'}]}
+        self._d.cmd(cmds, cache=False, flush_cache=True)
+        self._d.load_system()
+
+    def shell_init(self):
+        self._d.log_info('shell_init')
+        return [{'cmd': 'terminal datadump', 'prompt':'\#'},]
+
+    def shell_prompt(self):
+        self._d.log_info('shell_prompt')
+        return r'[\>\#]'
+
+    def ping(self):
+        self._d.log_info('ping')
+        self._d.cmd('show version', use_cache=False)
+
+        
