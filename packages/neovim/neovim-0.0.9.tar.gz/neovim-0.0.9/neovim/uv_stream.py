8966b91dcb082a06a0e9204593f31ae183087f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:43 +0000

    Add package neovim/0.0.9/neovim-0.0.9.tar.gz

---
+++ packages/neovim/neovim-0.0.9.tar.gz/neovim-0.0.9/neovim/uv_stream.py
@@ -0,0 +1,160 @@
+from collections import deque
+from util import VimExit
+from signal import SIGTERM
+import sys, pyuv, logging
+
+logger = logging.getLogger(__name__)
+debug, warn = (logger.debug, logger.warn,)
+
+class UvStream(object):
+    """
+    Stream abstraction implemented on top of libuv
+    """
+    def __init__(self, address=None, port=None):
+        pass # replaces next logging statement
+        #debug('initializing UvStream instance')
+        self._loop = pyuv.Loop()
+        self._connected = False
+        self._data_cb = None
+        self._error_cb = None
+        self._connection_error = None
+        self._pending_writes = 0
+        # Select the type of handle
+        if port:
+            pass # replaces next logging statement
+            #debug('TCP address was provided, connecting...')
+            # tcp
+            self._stream = pyuv.TCP(self._loop)
+            self._stream.connect((address, port), self._on_connect)
+        elif address:
+            pass # replaces next logging statement
+            #debug('Pipe address was provided, connecting...')
+            # named pipe or unix socket
+            self._stream = pyuv.Pipe(self._loop)
+            self._stream.connect(address, self._on_connect)
+        else:
+            pass # replaces next logging statement
+            #debug('No addresses were provided, will use stdin/stdout')
+            # stdin/stdout
+            self._read_stream = pyuv.Pipe(self._loop) 
+            self._read_stream.open(sys.stdin.fileno())
+            self._write_stream = pyuv.Pipe(self._loop) 
+            self._write_stream.open(sys.stdout.fileno())
+            self._connected = True
+        self._async = pyuv.Async(self._loop, self._on_async)
+        self._term = pyuv.Signal(self._loop)
+        self._term.start(self._on_term, SIGTERM)
+
+
+    """
+    Called when the libuv stream is connected
+    """
+    def _on_connect(self, stream, error):
+        self.loop_stop()
+        if error:
+            msg = pyuv.errno.strerror(error)
+            pass # replaces next logging statement
+            #warn('error connecting to neovim: %s', msg)
+            self._connection_error = IOError(msg)
+            return
+        self._connected = True
+        self._read_stream = self._write_stream = stream
+
+
+    def _on_term(self, handle, signum):
+        self.loop_stop()
+        self._error_cb(IOError('Received SIGTERM'))
+
+
+    def _on_async(self, handle):
+        """
+        Called when the async handle is fired
+        """
+        self.loop_stop()
+
+
+    def _connect(self):
+        while not self._connected and not self._connection_error:
+            self._loop.run(pyuv.UV_RUN_ONCE)
+
+
+    def _on_read(self, handle, data, error):
+        """
+        Called when data is read from the libuv stream
+        """
+        if error:
+            msg = pyuv.errno.strerror(error)
+            pass # replaces next logging statement
+            #warn('error reading data: %s', msg)
+            self._error_cb(IOError(msg))
+        elif not data:
+            pass # replaces next logging statement
+            #warn('connection was closed by neovim')
+            self._error_cb(IOError('EOF'))
+        else:
+            pass # replaces next logging statement
+            #debug('successfully read %d bytes of data', len(data))
+            self._data_cb(data)
+
+
+    def interrupt(self):
+        """
+        Stops the event loop from another thread.
+        """
+        self._async.send()
+
+
+    def send(self, data):
+        if not self._connected:
+            self._connect()
+            if self._connection_error:
+                err = self._connection_error
+                self._connection_error = None
+                raise err
+        self._pending_writes += 1
+        data_len = len(data)
+
+        def write_cb(handle, error):
+            self._pending_writes -= 1
+            if error:
+                msg = pyuv.errno.strerror(error)
+                pass # replaces next logging statement
+                #warn('error writing data: %s', msg)
+                self._error_cb(IOError(msg))
+            pass # replaces next logging statement
+            #debug('successfully wrote %d bytes of data', data_len)
+
+        pass # replaces next logging statement
+        #debug('writing %d bytes of data', data_len)
+        # queue the data for writing
+        self._write_stream.write(data, write_cb)
+
+
+    def loop_start(self, data_cb, error_cb):
+        if not self._connected:
+            self._connect()
+            if self._connection_error:
+                err = self._connection_error
+                self._connection_error = None
+                return error_cb(err)
+
+        self._data_cb = data_cb
+        self._error_cb = error_cb
+        self._read_stream.start_read(self._on_read)
+        pass # replaces next logging statement
+        #debug('entering libuv event loop')
+        self._loop.run(pyuv.UV_RUN_DEFAULT)
+        pass # replaces next logging statement
+        #debug('exited libuv event loop')
+        self._read_stream.stop_read()
+        self._data_cb = None
+        self._error_cb = None
+
+
+    def loop_stop(self):
+        """
+        Stops the event loop
+        """
+        self._loop.stop()
+        pass # replaces next logging statement
+        #debug('stopped event loop')
---
+++ packages/neovim/neovim-0.0.9.tar.gz/neovim-0.0.9/neovim/vim.py
@@ -0,0 +1,56 @@
+import os
+from util import RemoteSequence, RemoteMap, Current, VimError
+
+os_chdir = os.chdir
+os_fchdir = os.fchdir
+
+
+class Vim(object):
+    @classmethod
+    def initialize(self, vim, classes, channel_id):
+        vim.buffers = RemoteSequence(vim,
+                                     classes['buffer'],
+                                     lambda: vim.get_buffers())
+        vim.windows = RemoteSequence(vim,
+                                     classes['window'],
+                                     lambda: vim.get_windows())
+        vim.tabpages = RemoteSequence(vim,
+                                      classes['tabpage'],
+                                      lambda: vim.get_tabpages())
+        vim.current = Current(vim)
+        vim.vars = RemoteMap(lambda k: vim.get_var(k),
+                             lambda k, v: vim.set_var(k, v))
+        vim.vvars = RemoteMap(lambda k: vim.get_vvar(k),
+                              None)
+        vim.options = RemoteMap(lambda k: vim.get_option(k),
+                                lambda k, v: vim.set_option(k, v))
+        vim.channel_id = channel_id
+        vim.error = VimError
+
+    def foreach_rtp(self, cb):
+        """
+        Call the given callable for each path in 'runtimepath' until either 
+        callable returns something but None, the exception is raised or there 
+        are no longer paths. If stopped in case callable returned non-None, 
+        vim.foreach_rtp function returns the value returned by callable.
+        """
+        for path in self.list_runtime_paths():
+            try:
+                if cb(path) != None:
+                    break;
+            except:
+                break
+
+    def chdir(self, dir_path):
+        """
+        Run os.chdir, then all appropriate vim stuff.
+        """
+        os_chdir(dir_path)
+        self.change_directory(dir_path)
+
+    def fchdir(self, dir_fd):
+        """
+        Run os.chdir, then all appropriate vim stuff.
+        """
+        os_fchdir(dir_fd)
+        self.change_directory(os.getcwd())
