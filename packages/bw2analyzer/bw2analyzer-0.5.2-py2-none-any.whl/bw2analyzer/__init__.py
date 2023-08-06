473d2e16688b27f81bcabc19c4c1026cfe9deb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:38 +0000

    Add package bw2analyzer/0.5.2/bw2analyzer-0.5.2-py2-none-any.whl

---
+++ packages/bw2analyzer/bw2analyzer-0.5.2-py2-none-any.whl/bw2analyzer/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+from .contribution import ContributionAnalysis
+from .page_rank import PageRank
+from .explorer import DatabaseExplorer
+from .sc_graph import GTManipulator
+from .report import SerializedLCAReport
+
+__version__ = (0, 5, 2)
