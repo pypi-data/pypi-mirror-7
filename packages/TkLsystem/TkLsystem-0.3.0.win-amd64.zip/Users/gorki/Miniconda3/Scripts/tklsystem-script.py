a618463739f42e12370dce83645426f1a5ad742c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:08 +0000

    Add package TkLsystem/0.3.0/TkLsystem-0.3.0.win-amd64.zip

---
+++ packages/TkLsystem/TkLsystem-0.3.0.win-amd64.zip/Users/gorki/Miniconda3/Scripts/tklsystem-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\gorki\Miniconda3\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'TkLsystem==0.3.0','console_scripts','tklsystem'
+__requires__ = 'TkLsystem==0.3.0'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('TkLsystem==0.3.0', 'console_scripts', 'tklsystem')()
+    )
