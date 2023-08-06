e45b21a0c3f248ade2b2e088fb735689730b33ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:08 +0000

    Add package smsframework/0.0.3-2/smsframework-0.0.3-2.tar.gz

---
+++ packages/smsframework/smsframework-0.0.3-2.tar.gz/smsframework-0.0.3-2/setup.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    # http://pythonhosted.org/setuptools/setuptools.html
+    name='smsframework',
+    version='0.0.3-2',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-smsframework',
+    license='MIT',
+    description="Bi-directional SMS gateway with pluggable providers",
+    long_description=open('README.rst').read(),
+    keywords=['sms', 'message', 'notification', 'receive', 'send'],
+
+    packages=[
+        'smsframework',
+        'smsframework.data',
+        'smsframework.lib',
+        'smsframework.providers',
+    ],
+    scripts=[],
+
+    install_requires=[
+    ],
+    extras_require={
+        'clickatell': ['smsframework-clickatell >= 0.0.1'],
+        'vianett': ['smsframework-vianett >= 0.0.1'],
+        'receiver': ['flask >= 0.10']
+    },
+    include_package_data=True,
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
---
+++ packages/smsframework/smsframework-0.0.3-2.tar.gz/smsframework-0.0.3-2/smsframework/providers/__init__.py
@@ -0,0 +1,5 @@
+""" Bundled providers """
+
+from .null import NullProvider
+from .log import LogProvider
+from .loopback import LoopbackProvider
