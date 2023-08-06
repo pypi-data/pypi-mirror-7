da23b23bd49098a11e6f4f12b4f6fa9df6e2e478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:44 +0000

    Add package autodebug/1.0/autodebug-1.0.tar.gz

---
+++ packages/autodebug/autodebug-1.0.tar.gz/autodebug-1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="autodebug",
+    version="1.0",
+    author="Michał Podeszwa",
+    author_email="michal.podeszwa@gmail.com",
+    description=("Importing this module enables automatic post mortem debugging"
+                 " upon any exception. It uses module specified in envvar"),
+    license="BSD",
+    keywords="debugger debug automatic auto auto-debug",
+    packages=["autodebug"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Debuggers'
+    ]
+)
