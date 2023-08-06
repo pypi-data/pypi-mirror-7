727a20078d9e300e9589a34524752324aacce8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:11 +0000

    Add package sentry-hal/0.1.0/sentry-hal-0.1.0.tar.gz

---
+++ packages/sentry-hal/sentry-hal-0.1.0.tar.gz/sentry-hal-0.1.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+sentry-hal
+==============
+
+An extension for Sentry which integrates with a JSON based XMPP chat bot.
+
+:license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+
+tests_require = [
+    'nose>=1.1.2',
+]
+
+install_requires = [
+    'sentry>=4.6.0',
+]
+
+setup(
+    name='sentry-hal',
+    version='0.1.0',
+    author='Alexandre Dias',
+    author_email='alexnvdias@gmail.com',
+    url='http://www.github.com/alexnvdias',
+    description='A Sentry extension which integrates with a JSON based XMPP chat bot',
+    long_description=__doc__,
+    license='BSD',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+    entry_points={
+       'sentry.plugins': [
+            'sentry_hal = sentry_hal.plugin:HALMessage'
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
