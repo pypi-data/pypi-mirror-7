80cc199388a16db14a7fe1aad064a9795cf6c24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:06 +0000

    Add package UwoCalendar/2.0.0/UwoCalendar-2.0.0.zip

---
+++ packages/UwoCalendar/UwoCalendar-2.0.0.zip/UwoCalendar-2.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='UwoCalendar',
+    author='Peter Argall',
+    author_email='peter@argall.ca',
+    url='https://github.com/pargall/UWO-Cal',
+    version='2.0.0',
+    packages=['uwoCalendar',],
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "icalendar >= 3.0",
+        "lxml >= 3.3.5",
+    ],
+)
