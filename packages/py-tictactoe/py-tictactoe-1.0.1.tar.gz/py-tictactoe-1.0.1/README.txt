0a5ef014bd074646163c1a9e8baf133681162694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:54 +0000

    Add package py-tictactoe/1.0.1/py-tictactoe-1.0.1.tar.gz

---
+++ packages/py-tictactoe/py-tictactoe-1.0.1.tar.gz/py-tictactoe-1.0.1/README.txt
@@ -0,0 +1,5 @@
+This is a simple yet enjoyable Tic-Tac-Toe game - The Best TicTacToe game Ever.
+
+http://www.pythonpalace.blogspot.com
+
+- Gurpreet Singh
---
+++ packages/py-tictactoe/py-tictactoe-1.0.1.tar.gz/py-tictactoe-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='py-tictactoe',
+	version='1.0.1',
+	packages=['py-tictactoe'],
+	scripts=['pyttt.py'],
+	license='Creative Commons Attribution-Noncommercial-Share Alike License',
+	long_description='This is the Best Graphical Tic-Tac-Toe Game Ever',
+)
