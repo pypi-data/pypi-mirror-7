18fdf5075fced255c5f213fb41640341ba9582d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:16 +0000

    Add package eusful/0.7/eusful-0.7.tar.gz

---
+++ packages/eusful/eusful-0.7.tar.gz/eusful-0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(name='eusful',
+      version='0.7',
+      author='Eugene Efremov',
+      author_email='eaefremov@gmail.com',
+      license='MIT',
+      description='These are things I use in places.',
+      packages=find_packages())
