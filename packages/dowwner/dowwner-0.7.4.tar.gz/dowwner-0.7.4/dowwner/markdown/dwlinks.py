bf0fc58e44b8d3d54f30ee5c3f5353c0ff8cf984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package dowwner/0.7.4/dowwner-0.7.4.tar.gz

---
+++ packages/dowwner/dowwner-0.7.4.tar.gz/dowwner-0.7.4/dowwner/markdown/dwlinks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import markdown
+
+
+class DWLinkExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        pat = DWLinks(r"\[\[([\w0-9_ -/.]+)\]\]")
+        md.inlinePatterns.add("dwlink", pat, "<not_strong")
+        return
+
+
+class DWLinks(markdown.inlinepatterns.Pattern):
+    def handleMatch(self, m):
+        if m.group(2).strip():
+            url = m.group(2).strip()
+            a = markdown.util.etree.Element('a')
+            a.text = url
+            a.set('href', url)
+        else:
+            a = ''
+        return a
+
+
+def makeExtension(configs=None):
+    return DWLinkExtension(configs=configs)
---
+++ packages/dowwner/dowwner-0.7.4.tar.gz/dowwner-0.7.4/dowwner/op/xheaders.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import dowwner.op.edit
+
+
+class ContentGET(dowwner.op.edit.ContentGET):
+    pass
