fb31dbf6cf27be2941b1e1a2023d2ddef8eb7893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:47 +0000

    Add package usefulutils/0.1.2/usefulutils-0.1.2.tar.gz

---
+++ packages/usefulutils/usefulutils-0.1.2.tar.gz/usefulutils-0.1.2/usefulutils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    Useful Utils
+    ~~~~~
+
+    :copyright: (c) 2014 by Halfmoon Labs
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.2'
+
+from .dicts import recursive_dict, scrub_dict, to_dict, recursive_dict_to_dict
+from .charset import int_to_charset, charset_to_int, change_charset
