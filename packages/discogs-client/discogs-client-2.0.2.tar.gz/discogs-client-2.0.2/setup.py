8f13ad150856fff56561a43af2fe951c0a244221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:38 +0000

    Add package discogs-client/2.0.2/discogs-client-2.0.2.tar.gz

---
+++ packages/discogs-client/discogs-client-2.0.2.tar.gz/discogs-client-2.0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+        name='discogs-client',
+        version='2.0.2',
+        description='Official Python API client for Discogs',
+        url='https://github.com/discogs/discogs-python-client',
+        author='Discogs',
+        author_email='info@discogs.com',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Communications',
+            'Topic :: Utilities',
+            ],
+        install_requires=[
+            'requests',
+            'oauth2',
+            ],
+        packages=[
+            'discogs_client',
+            ],
+        )
+
