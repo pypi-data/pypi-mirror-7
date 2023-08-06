f7804efa81e5d1f2c2df1f51e97a15bd8a0b18c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:20 +0000

    Add package etianen-cms/2.6.5/etianen-cms-2.6.5.tar.gz

---
+++ packages/etianen-cms/etianen-cms-2.6.5.tar.gz/etianen-cms-2.6.5/src/test_project/test_project/apps/__init__.py
@@ -0,0 +1,1 @@
+"""Custom applications installed by test_project project."""
---
+++ packages/etianen-cms/etianen-cms-2.6.5.tar.gz/etianen-cms-2.6.5/src/test_project/test_project/apps/site/models.py
@@ -0,0 +1,12 @@
+"""Models used by test_project project."""
+
+from cms.models import HtmlField
+from cms.apps.pages.models import ContentBase
+
+
+class Content(ContentBase):
+    
+    content_primary = HtmlField(
+        "primary content",
+        blank = True
+    )
