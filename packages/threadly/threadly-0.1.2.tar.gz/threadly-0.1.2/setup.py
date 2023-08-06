104678a1da47130fc7fe1ef5d4e6798a43d7395b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:29 +0000

    Add package threadly/0.1.2/threadly-0.1.2.tar.gz

---
+++ packages/threadly/threadly-0.1.2.tar.gz/threadly-0.1.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup (
+       name = "threadly",
+       version = "0.1.2",
+       author = "Luke Wahlmeier",
+       author_email = "lwahlmeier@gmail.com",
+       url = "https://github.com/lwahlmeier/python-threadly",
+       download_url = "https://github.com/lwahlmeier/python-threadly/tarball/0.1.2",
+       license = "lgpl",
+       description = "Threading pool and scheduler for python",
+       py_modules =  ['threadly'],
+      )
