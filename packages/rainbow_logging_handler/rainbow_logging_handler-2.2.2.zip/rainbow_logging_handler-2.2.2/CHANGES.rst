e819ba7e1994ba34ef3f83eb8dc7dde2abae32eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:02 +0000

    Add package rainbow_logging_handler/2.2.2/rainbow_logging_handler-2.2.2.zip

---
+++ packages/rainbow_logging_handler/rainbow_logging_handler-2.2.2.zip/rainbow_logging_handler-2.2.2/CHANGES.rst
@@ -0,0 +1,82 @@
+Changelog for rainbow_logging_handler
+=====================================
+
+2.2.2 (2014-08-07)
+------------------
+
+- bugfix. 2.1.1 could not be installed...
+
+
+2.2.1 (2014-07-29)
+------------------
+
+- Bugfix on encoding specification
+- Fix spelling mistake. Thanks to `dueyfinster <https://github.com/dueyfinster>`_ .
+
+
+2.2.0 (2014-01-13)
+------------------
+
+- Adds custom time format support
+
+
+2.1.2 (2013-12-27)
+------------------
+
+- Adds version badge on README
+
+
+2.1.1 (2013-12-20)
+------------------
+
+- Updats Django example to match the RainbowLoggingHandler new initializer
+
+
+2.1.0 (2013-12-06)
+------------------
+
+- Adds usage w/ Django
+
+2.0.2 (2013-12-05)
+------------------
+
+- Updates docstring of `RainbowLoggingHandler.__init__()`
+
+2.0.1 (2013-12-05)
+------------------
+
+- Bugfix for Python 3.2
+
+2.0.0 (2013-12-05)
+------------------
+
+- Adds custom color & format feature
+
+1.0.4 (2013-12-03)
+------------------
+
+- Updates `classifiers` on `setup.py`
+
+
+1.0.3 (2013-12-03)
+------------------
+
+- Fix on README
+
+
+1.0.2 (2013-12-03)
+------------------
+
+- Fix on license
+
+
+1.0.1 (2013-12-03)
+------------------
+
+- Fix on packaging
+
+
+1.0.0 (2013-12-03)
+------------------
+
+- First release
---
+++ packages/rainbow_logging_handler/rainbow_logging_handler-2.2.2.zip/rainbow_logging_handler-2.2.2/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name             = 'rainbow_logging_handler',
+    description      = 'Ultimate Python colorized logger with user-custom color',
+    long_description = open('README.rst').read(),
+    url              = 'https://github.com/laysakura/rainbow_logging_handler',
+    license          = 'LICENSE.txt',
+    version          = '2.2.2',
+    author           = 'Mikko Ohtamaa, Sho Nakatani',
+    author_email     = 'mikko@opensourcehacker.com, lay.sakura@gmail.com',
+    install_requires = [
+        'logutils',
+        'colorama',
+    ],
+    tests_require    = [
+        'nose',
+        'coverage',
+        'nose-cov',
+    ],
+    packages         = [
+        'rainbow_logging_handler',
+        'rainbow_logging_handler.test',
+    ],
+    scripts          = [
+    ],
+    classifiers      = '''
+Programming Language :: Python
+Development Status :: 5 - Production/Stable
+License :: Public Domain
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: Implementation :: PyPy
+Operating System :: POSIX :: Linux
+Operating System :: POSIX :: BSD
+Operating System :: Unix
+Operating System :: MacOS
+Operating System :: Microsoft :: Windows
+Intended Audience :: Developers
+Topic :: Software Development :: Debuggers
+Topic :: System :: Logging
+Topic :: Terminals
+Topic :: Utilities
+'''.strip().splitlines()
+)
