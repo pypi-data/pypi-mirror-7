23c0575d5c0031d181246e9b1dd5114eebd61beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package chembl_beaker/0.5.20/chembl_beaker-0.5.20.tar.gz

---
+++ packages/chembl_beaker/chembl_beaker-0.5.20.tar.gz/chembl_beaker-0.5.20/chembl_beaker/run_beaker.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+__author__ = 'mnowotka'
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+from bottle import run
+from optparse import OptionParser
+from chembl_beaker.beaker import app, config
+from chembl_beaker.beaker.plugins.enableCors import EnableCors
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-c", "--config", dest="config_path",
+                  help="path to config file", default="beaker.conf")
+    (options, args) = parser.parse_args()
+    conf_path = options.config_path
+else:
+    conf_path = "beaker.conf"
+
+config.load_config(conf_path)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+if config.get('enable_cors', 'true') != 'false':
+    app.install(EnableCors())
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def main():
+    run(app=app, host=config.get('bottle_host', 'localhost'), port=config.get('bottle_port', '8080'),
+                                debug=config.get('debug', True), server=config.get('server_middleware', 'tornado'))
+
+if __name__ == "__main__":
+    main()
+
+else:
+    application = app
+
+#-----------------------------------------------------------------------------------------------------------------------
