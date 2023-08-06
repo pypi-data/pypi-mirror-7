9f792ab67252aa6f5d44fdae600813bfb1d606a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:16 +0000

    Add package mozmill-automation/2.0.6.2/mozmill-automation-2.0.6.2.tar.gz

---
+++ packages/mozmill-automation/mozmill-automation-2.0.6.2.tar.gz/mozmill-automation-2.0.6.2/mozmill_automation/application.py
@@ -0,0 +1,152 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import ConfigParser
+import os
+import re
+import sys
+
+import errors
+
+
+def get_bin_folder(app_folder):
+    """ Returns the folder which contains the binaries of the application. """
+    if sys.platform in ("darwin"):
+        app_folder = os.path.join(app_folder, 'Contents', 'MacOS')
+    return app_folder
+
+
+def get_mozmill_tests_branch(gecko_branch):
+    """ Identify the mozmill-tests branch from the application branch. """
+
+    # Retrieve the name of the repository
+    branch = re.search('.*/([\S\.]+$)', gecko_branch).group(1)
+
+    # Supported branches: mozilla-aurora, mozilla-beta, mozilla-release, mozilla-esr*
+    # All other branches (mozilla-central, mozilla-inbound, birch, elm, oak etc.) should fallback to the 'default' branch
+    # This will work with Firefox and Thunderbird
+    if not re.match(r'.*/releases/', gecko_branch):
+        branch = "default"
+
+    return branch
+
+
+def is_app_folder(path):
+    """ Checks if the folder is an application folder. """
+    if sys.platform != "darwin":
+        path = os.path.dirname(path)
+
+    file = os.path.join(get_bin_folder(path),
+                        "application.ini")
+
+    return os.path.exists(file)
+
+
+def is_installer(path, application):
+    """ Checks if a binary is an installer. """
+    try:
+        if (os.path.splitext(path)[1] in (".bz2", ".dmg", ".exe")):
+            return os.path.basename(path) not in (application + ".exe")
+        else:
+            return False
+    except Exception:
+        return False
+
+
+class ApplicationIni(object):
+    """ Class to retrieve entries from the application.ini file. """
+
+    def __init__(self, binary):
+        self.ini_file = os.path.join(os.path.dirname(binary),
+                                     'application.ini')
+
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(self.ini_file)
+
+    def get(self, section, option):
+        """ Retrieve the value of an entry. """
+        return self.config.get(section, option)
+
+
+class UpdateChannel(object):
+    """ Class to handle the update channel. """
+
+    pref_regex = "(?<=pref\(\"app\.update\.channel\", \")([^\"]*)(?=\"\))"
+
+    def __init__(self, binary, *args, **kwargs):
+        self.folder = os.path.dirname(binary)
+
+    @property
+    def channel_prefs_path(self):
+        """ Returns the channel prefs path. """
+        for pref_folder in ('preferences', 'pref'):
+            pref_path = os.path.join(self.folder,
+                                     'defaults',
+                                     pref_folder,
+                                     'channel-prefs.js')
+            if os.path.exists(pref_path):
+                return pref_path
+        raise errors.NotFoundException('Channel prefs not found.', pref_path)
+
+    def _get_channel(self):
+        """ Returns the current update channel. """
+        try:
+            file = open(self.channel_prefs_path, "r")
+        except IOError:
+            raise
+        else:
+            content = file.read()
+            file.close()
+
+            result = re.search(self.pref_regex, content)
+            return result.group(0)
+
+    def _set_channel(self, value):
+        """ Sets the update channel. """
+
+        try:
+            file = open(self.channel_prefs_path, "r")
+        except IOError:
+            raise
+        else:
+            # Replace the current update channel with the specified one
+            content = file.read()
+            file.close()
+
+            # Replace the current channel with the specified one
+            result = re.sub(self.pref_regex, value, content)
+
+            try:
+                file = open(self.channel_prefs_path, "w")
+            except IOError:
+                raise
+            else:
+                file.write(result)
+                file.close()
+
+                # Check that the correct channel has been set
+                if value != self.channel:
+                    raise Exception("Update channel wasn't set correctly.")
+
+    channel = property(_get_channel, _set_channel, None)
+
+class UpdateSettingsIni(object):
+    """ Class to retrieve and set entries in the update-settings.ini file. """
+
+    def __init__(self, binary):
+        self.ini_file = os.path.join(os.path.dirname(binary),
+                                     'update-settings.ini')
+        self.config = ConfigParser.RawConfigParser()
+        self.config.optionxform = str
+        self.config.read(self.ini_file)
+
+    def get(self, section, option):
+        """ Retrieve the value of an entry. """
+        return self.config.get(section, option)
+
+    def set(self, section, option, value):
+        """ Set the value of an entry. """
+        self.config.set(section, option, value)
+        with open(self.ini_file, 'w+') as file:
+            self.config.write(file)
