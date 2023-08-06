f3cdba7d26a3354d312e34fa4c4e9f3b7d24099c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package pythonpy/0.2.4dev2/pythonpy-0.2.4dev2.tar.gz

---
+++ packages/pythonpy/pythonpy-0.2.4dev2.tar.gz/pythonpy-0.2.4dev2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import os
+
+setup(
+    name='pythonpy',
+    version='0.2.4dev2',
+    description='Take advantage of your python skills from the command line',
+    scripts=['pythonpy'],
+    license='MIT',
+    url='https://github.com/Russell91/pythonpy',
+    long_description='',
+)
