1b73af0ea14776c185324686fb916d89217df370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:51 +0000

    Add package pyee-topics/0.0.8/pyee-topics-0.0.8.tar.gz

---
+++ packages/pyee-topics/pyee-topics-0.0.8.tar.gz/pyee-topics-0.0.8/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "pyee-topics",
+    version = "0.0.8",
+    packages = ["pyee"],
+    description = "A port of node.js's EventEmitter to python.",
+    author = "Joshua Holbrook",
+    author_email = "josh.holbrook@gmail.com",
+    url = "https://github.com/jesusabdullah/pyee",
+    keywords = ["events", "emitter", "node.js", "node", "eventemitter", "event_emitter"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7", #only one tested
+        "Topic :: Other/Nonlisted Topic"
+    ],
+    long_description = """\
+pyee
+======
+
+pyee supplies an event_emitter object that acts similar to the `EventEmitter`
+that comes with node.js.
+
+Example
+-------
+
+::
+
+    In [1]: from pyee import EventEmitter
+
+    In [2]: ee = EventEmitter()
+
+    In [3]: @ee.on('event')
+       ...: def event_handler():
+       ...:     print 'BANG BANG'
+       ...:
+
+    In [4]: ee.emit('event')
+    BANG BANG
+
+    In [5]:
+
+Easy-peasy.
+
+For more, visit <https://github.com/jesusabdullah/pyee> .
+
+"""
+)
