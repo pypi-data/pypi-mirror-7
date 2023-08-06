31423bfb445147918675568daab78bb3547f0009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:55 +0000

    Add package PyMoJo/0.8.3/PyMoJo-0.8.3.tar.gz

---
+++ packages/PyMoJo/PyMoJo-0.8.3.tar.gz/PyMoJo-0.8.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name='PyMoJo',
+  version='0.8.3',
+  author='Ryan Jung',
+  author_email='gradysghost@gmail.com',
+  packages=['pymojo'],
+  url='https://github.com/GradysGhost/pymojo',
+  license='LICENSE',
+  description='PyJoJo client library',
+  long_description=open('README.md').read(),
+  install_requires=[
+    'requests == 1.1.0',
+    'PyYAML == 3.10'
+  ],
+  entry_points={
+    'console_scripts' : [
+      'mojo = pymojo.cli_bin:main'
+    ]
+  }
+)
