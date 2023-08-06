52ce1274fdf3ad3a50c7e4a9d281da81a5aa62c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:27 +0000

    Add package Flask-Statics-Helper/0.1.0/Flask-Statics-Helper-0.1.0.tar.gz

---
+++ packages/Flask-Statics-Helper/Flask-Statics-Helper-0.1.0.tar.gz/Flask-Statics-Helper-0.1.0/flask_statics/__init__.py
@@ -0,0 +1,69 @@
+"""Provides Bootstrap3 and other static resources in a modular fashion.
+
+https://github.com/Robpol86/Flask-Statics-Helper
+https://pypi.python.org/pypi/Flask-Statics-Helper
+"""
+
+from collections import OrderedDict
+
+from flask import Blueprint
+
+import resource_base
+import resource_definitions
+import resource_definitions_angular
+
+__author__ = '@Robpol86'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+
+
+class _StaticsState(object):
+    """Remembers the configuration for the (statics, app) tuple. Modeled from SQLAlchemy."""
+
+    def __init__(self, statics, app):
+        self.statics = statics
+        self.app = app
+
+
+class Statics(object):
+    """Static css/js resources for Flask applications.
+
+    Relevant configuration settings from the Flask app config:
+    STATICS_MINIFY -- set to True to have minified resources selected instead of uncompressed resources.
+
+    Optional settings to enable specific static resources on all templates by default instead of on-demand:
+    STATICS_ENABLE_RESOURCE_<resource name> -- refer to resource_definitions.py for list of options. Set to True to
+        enable everywhere.
+    """
+
+    def __init__(self, app=None):
+        self.all_variables = list()
+        self.all_resources = OrderedDict()
+        self.blueprint = None
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Initialize the extension."""
+        # Set default Flask config option.
+        app.config.setdefault('STATICS_MINIFY', False)
+
+        # Populate resources
+        subclasses = resource_base.ResourceBase.__subclasses__() + resource_definitions.ResourceAngular.__subclasses__()
+        for resource in subclasses:
+            self.all_variables.append(resource.RESOURCE_NAME)
+            obj = resource(app)
+            self.all_resources[resource.RESOURCE_NAME] = dict(css=tuple(obj.resources_css), js=tuple(obj.resources_js))
+
+        # Add this instance to app.extensions.
+        if not hasattr(app, 'extensions'):
+            app.extensions = dict()
+        app.extensions['statics'] = _StaticsState(self, app)
+
+        # Initialize blueprint.
+        name = 'flask_statics_helper'
+        static_url_path = '{}/{}'.format(app.static_url_path, name)
+        self.blueprint = Blueprint(name, __name__, template_folder='templates', static_folder='static',
+                static_url_path=static_url_path)
+        self.blueprint.add_app_template_global(self.all_resources, '_flask_statics_helper_all_resources')
+        app.register_blueprint(self.blueprint)
---
+++ packages/Flask-Statics-Helper/Flask-Statics-Helper-0.1.0.tar.gz/Flask-Statics-Helper-0.1.0/flask_statics/resource_definitions.py
@@ -0,0 +1,105 @@
+"""Holds defined resources, subclassing resource_base.ResourceBase."""
+
+from resource_base import ResourceBase
+
+
+class ResourceJQuery(ResourceBase):
+    DIR = 'jquery-2.1.1'
+    RESOURCE_NAME = 'JQUERY'
+
+    def __init__(self, app):
+        super(ResourceJQuery, self).__init__(app)
+        self.add_js('', 'jquery-2.1.1')
+
+
+class ResourceAngular(ResourceBase):
+    DIR = 'angular-1.3.0-beta.14'
+    RESOURCE_NAME = 'ANGULARJS'
+
+    def __init__(self, app):
+        super(ResourceAngular, self).__init__(app)
+        self.add_js('', 'angular')
+
+
+class ResourceBootstrap(ResourceBase):
+    DIR = 'bootstrap-3.2.0'
+    RESOURCE_NAME = 'BOOTSTRAP'
+
+    def __init__(self, app):
+        super(ResourceBootstrap, self).__init__(app)
+        self.add_css('css', 'bootstrap')
+        self.add_js('js', 'bootstrap')
+
+
+class ResourceBootstrapEditable(ResourceBase):
+    DIR = 'bootstrap3-editable-1.5.1'
+    RESOURCE_NAME = 'BOOTSTRAP_EDITABLE'
+
+    def __init__(self, app):
+        super(ResourceBootstrapEditable, self).__init__(app)
+        self.add_css('css', 'bootstrap-editable')
+        self.add_js('js', 'bootstrap-editable')
+
+
+class ResourceBootstrapGrowl(ResourceBase):
+    DIR = 'bootstrap-growl-2.0.0'
+    RESOURCE_NAME = 'BOOTSTRAP_GROWL'
+
+    def __init__(self, app):
+        super(ResourceBootstrapGrowl, self).__init__(app)
+        self.add_js('', 'bootstrap-growl')
+
+
+class ResourceBootstrapTypeahead(ResourceBase):
+    DIR = 'Bootstrap-3-Typeahead-3.0.3'
+    RESOURCE_NAME = 'BOOTSTRAP_TYPEAHEAD'
+
+    def __init__(self, app):
+        super(ResourceBootstrapTypeahead, self).__init__(app)
+        self.add_js('', 'bootstrap3-typeahead')
+
+
+class ResourceCSShake(ResourceBase):
+    DIR = 'csshake-20140709'
+    RESOURCE_NAME = 'CSSHAKE'
+
+    def __init__(self, app):
+        super(ResourceCSShake, self).__init__(app)
+        self.add_css('', 'csshake')
+
+
+class ResourceD3(ResourceBase):
+    DIR = 'd3-3.4.9'
+    RESOURCE_NAME = 'D3'
+
+    def __init__(self, app):
+        super(ResourceD3, self).__init__(app)
+        self.add_js('', 'd3')
+
+
+class ResourceDataTables(ResourceBase):
+    DIR = 'DataTables-1.10.0'
+    RESOURCE_NAME = 'DATATABLES'
+
+    def __init__(self, app):
+        super(ResourceDataTables, self).__init__(app)
+        self.add_css('css', 'jquery.dataTables')
+        self.add_js('js', 'jquery.dataTables')
+
+
+class ResourceFontAwesome(ResourceBase):
+    DIR = 'font-awesome-4.1.0'
+    RESOURCE_NAME = 'FONT_AWESOME'
+
+    def __init__(self, app):
+        super(ResourceFontAwesome, self).__init__(app)
+        self.add_css('css', 'font-awesome')
+
+
+class ResourceWHHGFont(ResourceBase):
+    DIR = 'whhg-font-20140709'
+    RESOURCE_NAME = 'WHHG_FONT'
+
+    def __init__(self, app):
+        super(ResourceWHHGFont, self).__init__(app)
+        self.add_css('css', 'whhg')
