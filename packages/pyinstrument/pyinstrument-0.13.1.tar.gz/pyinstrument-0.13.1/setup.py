52afe5c33f4505119b17df5613f84e07c4e222b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:03 +0000

    Add package pyinstrument/0.13.1/pyinstrument-0.13.1.tar.gz

---
+++ packages/pyinstrument/pyinstrument-0.13.1.tar.gz/pyinstrument-0.13.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyinstrument",
+    packages=['pyinstrument'],
+    version="0.13.1",
+    description="A call stack profiler for Python. Inspired by Apple's Instruments.app",
+    author='Joe Rickerby',
+    author_email='joerick@mac.com',
+    url='https://github.com/joerick/pyinstrument',
+    keywords=['profiling', 'profile', 'profiler', 'cpu', 'time'],
+    include_package_data=True,
+    entry_points={'console_scripts': ['pyinstrument = pyinstrument.__main__:main']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Testing',
+    ]
+)
