8c10321e1d67c0ad4ff16410eb2d51f8a24e523f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:33 +0000

    Add package Yoav/0.2.1/Yoav-0.2.1.tar.gz

---
+++ packages/Yoav/Yoav-0.2.1.tar.gz/Yoav-0.2.1/yoav/__init__.py
@@ -0,0 +1,1 @@
+from yslient import Yoav 
---
+++ packages/Yoav/Yoav-0.2.1.tar.gz/Yoav-0.2.1/yoav/yslient.py
@@ -0,0 +1,15 @@
+"""Python client for Sift Science's API.
+See: https://siftscience.com/docs/references/events-api
+"""
+
+from yoav import version
+
+class Yoav(object):
+    def __init__(self):
+        """Initialize the client.
+        """
+        self.pstring = "Hello Yoav!"
+
+    def myprint(self)
+        print self.pstring
+
