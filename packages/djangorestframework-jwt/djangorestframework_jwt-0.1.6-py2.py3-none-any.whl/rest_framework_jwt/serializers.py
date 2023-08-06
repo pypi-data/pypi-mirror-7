f4018b0a647de890c3f0a8d161c3342aef820f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:02 +0000

    Add package djangorestframework-jwt/0.1.6/djangorestframework_jwt-0.1.6-py2.py3-none-any.whl

---
+++ packages/djangorestframework-jwt/djangorestframework_jwt-0.1.6-py2.py3-none-any.whl/rest_framework_jwt/serializers.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework import serializers
+
+from rest_framework_jwt.settings import api_settings
+
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+
+class JSONWebTokenSerializer(serializers.Serializer):
+    """
+    Serializer class used to validate a username and password.
+
+    'username' is identified by the custom UserModel.USERNAME_FIELD.
+
+    Returns a JSON Web Token that can be used to authenticate later calls.
+    """
+
+    password = serializers.CharField(write_only=True)
+
+    def __init__(self, *args, **kwargs):
+        """Dynamically add the USERNAME_FIELD to self.fields."""
+        super(JSONWebTokenSerializer, self).__init__(*args, **kwargs)
+        self.fields[self.username_field] = serializers.CharField()
+
+    @property
+    def username_field(self):
+        return get_user_model().USERNAME_FIELD
+
+    def validate(self, attrs):
+        credentials = {self.username_field: attrs.get(self.username_field),
+                       'password': attrs.get('password')}
+        if all(credentials.values()):
+            user = authenticate(**credentials)
+
+            if user:
+                if not user.is_active:
+                    msg = 'User account is disabled.'
+                    raise serializers.ValidationError(msg)
+
+                payload = jwt_payload_handler(user)
+
+                return {
+                    'token': jwt_encode_handler(payload)
+                }
+            else:
+                msg = 'Unable to login with provided credentials.'
+                raise serializers.ValidationError(msg)
+        else:
+            msg = 'Must include "username" and "password"'
+            raise serializers.ValidationError(msg)
---
+++ packages/djangorestframework-jwt/djangorestframework_jwt-0.1.6-py2.py3-none-any.whl/rest_framework_jwt/settings.py
@@ -0,0 +1,34 @@
+import datetime
+
+from django.conf import settings
+from rest_framework.settings import APISettings
+
+
+USER_SETTINGS = getattr(settings, 'JWT_AUTH', None)
+
+DEFAULTS = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_SECRET_KEY': settings.SECRET_KEY,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300)
+}
+
+# List of settings that may be in string import notation.
+IMPORT_STRINGS = (
+    'JWT_ENCODE_HANDLER',
+    'JWT_DECODE_HANDLER',
+    'JWT_PAYLOAD_HANDLER',
+)
+
+api_settings = APISettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS)
