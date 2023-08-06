5c021d72cfe660cf1cc29b752533c01320d7d967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:59 +0000

    Add package workbench_cli/0.1.7/workbench_cli-0.1.7.tar.gz

---
+++ packages/workbench_cli/workbench_cli-0.1.7.tar.gz/workbench_cli-0.1.7/workbench/clients/customer_report.py
@@ -0,0 +1,28 @@
+"""This client generates customer reports on all the samples in workbench."""
+
+import zerorpc
+import pprint
+import os
+import client_helper
+
+def run():
+    """This client generates customer reports on all the samples in workbench."""
+    
+    # Grab server args
+    args = client_helper.grab_server_args()
+
+    # Start up workbench connection
+    workbench = zerorpc.Client(timeout=300, heartbeat=60)
+    workbench.connect('tcp://'+args['server']+':'+args['port'])
+
+    results = workbench.batch_work_request('view_customer')
+    for customer in results:
+        pprint.pprint(customer)
+
+def test():
+    """Executes test for customer_report."""
+    run()
+
+if __name__ == '__main__':
+    run()
+
