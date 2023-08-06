e69748f3f4dc5407bb25aa39b1030b30b596c0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:30 +0000

    Add package pysdl2-cffi/0.5.0/pysdl2-cffi-0.5.0.tar.gz

---
+++ packages/pysdl2-cffi/pysdl2-cffi-0.5.0.tar.gz/pysdl2-cffi-0.5.0/builder/build_sdl.py
@@ -0,0 +1,51 @@
+# Generate libSDL2 wrappers.
+
+import json
+import os.path
+import pprint
+
+from .builder import Builder
+
+header = """# Automatically generated wrappers.
+# Override by adding wrappers to helpers.py.
+from .dso import ffi, _LIB
+from .structs import Struct, unbox, SDLError, u8
+
+"""
+
+api = ["""# API
+import apipkg
+
+exports = """, """
+
+ns_dict = dict((s, '_sdl.renamed:%s' % s)
+               for s in exports if not s.startswith('_'))
+ns_dict['image'] = '_sdl_image.renamed'
+ns_dict['mixer'] = '_sdl_mixer.renamed'
+ns_dict['ttf'] = '_sdl_ttf.renamed'
+
+apipkg.initpkg(__name__, ns_dict, {'__all__':exports})
+"""]
+
+
+def go():
+    from _sdl import cdefs, helpers
+
+    try:
+        with open(os.path.join(os.path.dirname(__file__), 'dox.json'), 'r') as funcdocs:
+            all_funcdocs = json.load(funcdocs)
+    except IOError:
+        all_funcdocs = {}
+
+    builder = Builder(all_funcdocs)
+
+    output_filename = os.path.join(os.path.dirname(__file__),
+                                   "..",
+                                   "_sdl",
+                                   "autohelpers.py")
+    with open(output_filename, "w+") as output:
+        output.write(header)
+        builder.generate(output, cdefs=cdefs, helpers=helpers)
+
+if __name__ == "__main__":
+    go()
