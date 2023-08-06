d8ac9137c387163967331cfa44802896ca0ab66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:57 +0000

    Add package compoundfiles/0.3/compoundfiles-0.3.tar.gz

---
+++ packages/compoundfiles/compoundfiles-0.3.tar.gz/compoundfiles-0.3/setup.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# vim: set et sw=4 sts=4 fileencoding=utf-8:
+#
+# A library for reading Microsoft's OLE Compound Document format
+# Copyright (c) 2014 Dave Hughes <dave@waveform.org.uk>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"Library for parsing and reading OLE Compound Documents"
+
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division,
+    )
+str = type('')
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info[0] == 2:
+    if not sys.version_info >= (2, 7):
+        raise ValueError('This package requires Python 2.7 or above')
+elif sys.version_info[0] == 3:
+    if not sys.version_info >= (3, 2):
+        raise ValueError('This package requires Python 3.2 or above')
+else:
+    raise ValueError('Unrecognized major version of Python')
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Workaround <http://bugs.python.org/issue10945>
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func)
+
+# Workaround <http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html>
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+__project__      = 'compoundfiles'
+__version__      = '0.3'
+__author__       = 'Dave Hughes'
+__author_email__ = 'dave@waveform.org.uk'
+__url__          = 'https://compound-files.readthedocs.org/'
+__platforms__    = 'ALL'
+
+__classifiers__ = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Unix',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    ]
+
+__keywords__ = [
+    'microsoft',
+    'ole',
+    'compound',
+    'document',
+    ]
+
+__requires__ = [
+    ]
+
+__extra_requires__ = {
+    'doc': ['sphinx'],
+    'test': ['pytest', 'coverage', 'mock'],
+    }
+
+if sys.version_info[:2] == (3, 2):
+    __extra_requires__['doc'].extend([
+        # Particular versions are required for Python 3.2 compatibility. The
+        # ordering is reversed because that's what easy_install needs...
+        'Jinja2<2.7',
+        'MarkupSafe<0.16',
+        ])
+
+__entry_points__ = {
+    }
+
+
+def main():
+    import io
+    with io.open(os.path.join(HERE, 'README.rst'), 'r') as readme:
+        setup(
+            name                 = __project__,
+            version              = __version__,
+            description          = __doc__,
+            long_description     = readme.read(),
+            classifiers          = __classifiers__,
+            author               = __author__,
+            author_email         = __author_email__,
+            url                  = __url__,
+            license              = [
+                c.rsplit('::', 1)[1].strip()
+                for c in __classifiers__
+                if c.startswith('License ::')
+                ][0],
+            keywords             = __keywords__,
+            packages             = find_packages(),
+            package_data         = {},
+            include_package_data = True,
+            platforms            = __platforms__,
+            install_requires     = __requires__,
+            extras_require       = __extra_requires__,
+            entry_points         = __entry_points__,
+            )
+
+if __name__ == '__main__':
+    main()
+
+
