4fe58f160e514591980d3d545b1a5c1c3f515153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:03 +0000

    Add package python-vim/0.3/python-vim-0.3.tar.gz

---
+++ packages/python-vim/python-vim-0.3.tar.gz/python-vim-0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='python-vim',
+      version='0.3',
+      description='Python utility that integrates the python interactive console with vim',
+      author='Héctor Acosta',
+      author_email='hector.acosta@gmail.com',
+      url='https://github.com/hacosta/python-vim',
+      py_modules=['vim'],
+      license='BSD'
+      )
