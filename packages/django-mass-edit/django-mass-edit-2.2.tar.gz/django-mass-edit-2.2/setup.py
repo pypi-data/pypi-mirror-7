383b611f17e6054f7de4b673c134e7499d1579b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:16 +0000

    Add package django-mass-edit/2.2/django-mass-edit-2.2.tar.gz

---
+++ packages/django-mass-edit/django-mass-edit-2.2.tar.gz/django-mass-edit-2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-mass-edit",
+    version = "2.2",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("Make bulk changes in the Django admin interface"),
+    license = "BSD",
+    keywords = "django admin",
+    url = "https://github.com/burke-software/django-mass-edit",
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=['django']
+)
