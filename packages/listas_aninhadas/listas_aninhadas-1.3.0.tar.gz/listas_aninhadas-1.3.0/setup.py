15a7ee246798949c5e68b0b6f7a8af9369b14b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:41 +0000

    Add package listas_aninhadas/1.3.0/listas_aninhadas-1.3.0.tar.gz

---
+++ packages/listas_aninhadas/listas_aninhadas-1.3.0.tar.gz/listas_aninhadas-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'listas_aninhadas',
+        version = '1.3.0',
+        py_modules = ['listas_aninhadas'],
+        author = 'douglasarts',
+        author_email = 'douglasarts@gmail.com',
+        url = '',
+        description = 'Uma simples impressão de listas aninhadas',
+    )
