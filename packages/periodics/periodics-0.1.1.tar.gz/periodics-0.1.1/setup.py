1c5ec746625cc69cac3d67720c731a267d17aa7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package periodics/0.1.1/periodics-0.1.1.tar.gz

---
+++ packages/periodics/periodics-0.1.1.tar.gz/periodics-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='periodics',
+    version='0.1.1',
+    packages=['periodics'],
+    url='',
+    license='GPL',
+    author='yarnaid',
+    author_email='yarnaid@gmail.com',
+    description='Some statistical functions for searchin periodics in time rows'
+)
