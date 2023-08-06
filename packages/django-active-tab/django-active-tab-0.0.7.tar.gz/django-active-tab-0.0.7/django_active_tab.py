6dc9706c89c4bed4ad8d1afb33c66e061f11e0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:55 +0000

    Add package django-active-tab/0.0.7/django-active-tab-0.0.7.tar.gz

---
+++ packages/django-active-tab/django-active-tab-0.0.7.tar.gz/django-active-tab-0.0.7/django_active_tab.py
@@ -0,0 +1,18 @@
+import sys
+
+def active_tab(tab, sub_tab=None):
+	def outer_wrapper(func):
+		import functools.wraps
+		
+		@functools.wraps
+		def wrapper(request):
+			request.nav = request.nav if hasattr(request, "nav") else {}
+			request.nav["tab"] = tab
+			if sub_tab is not None:
+				request.nav["sub_tab"] = sub_tab
+			return func(request)
+		return wrapper
+
+	return outer_wrapper
+
+sys.modules[__name__] = active_tab
