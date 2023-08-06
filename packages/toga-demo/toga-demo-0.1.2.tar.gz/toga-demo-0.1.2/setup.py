18a59946179fa1caa37ff4cde4f0cc0400cc9d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:32 +0000

    Add package toga-demo/0.1.2/toga-demo-0.1.2.tar.gz

---
+++ packages/toga-demo/toga-demo-0.1.2.tar.gz/toga-demo-0.1.2/setup.py
@@ -0,0 +1,50 @@
+#/usr/bin/env python
+import io
+import re
+from setuptools import setup, find_packages
+
+
+with io.open('./toga_demo/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with io.open('README.rst', encoding='utf8') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='toga-demo',
+    version=version,
+    description='A demonstration of the capabilities of the Toga widget toolkit.',
+    long_description=long_description,
+    author='Russell Keith-Magee',
+    author_email='russell@keith-magee.com',
+    url='http://pybee.org/toga-demo',
+    include_package_data=True,
+    packages=find_packages(),
+    package_data={
+        'toga_demo': ['icons/*.icns', 'icons/*.png'],
+    },
+    install_requires=[
+        'toga',
+    ],
+    entry_points={
+        'console_scripts': [
+            'toga-demo = toga_demo.__main__:main',
+        ]
+    },
+    license='New BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
