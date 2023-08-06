c38ac0052fe4059e686d6c8d61714c00c6c82566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:46 +0000

    Add package django-zebra/0.4.5/django-zebra-0.4.5.tar.gz

---
+++ packages/django-zebra/django-zebra-0.4.5.tar.gz/django-zebra-0.4.5/setup.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
+setup(
+    name = "django-zebra",
+    description = "Library for Django and Stripe",
+    author = "Steven Skoczen",
+    author_email = "steven@agoodcloud.com",
+    url = "https://github.com/GoodCloud/django-zebra",
+    version = "0.4.5",
+    packages = find_packages(),
+    zip_safe = False,
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
