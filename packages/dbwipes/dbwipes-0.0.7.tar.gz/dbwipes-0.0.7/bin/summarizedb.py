af3e1acd0f58190d23da28fdb521e14eeb354f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:01 +0000

    Add package dbwipes/0.0.7/dbwipes-0.0.7.tar.gz

---
+++ packages/dbwipes/dbwipes-0.0.7.tar.gz/dbwipes-0.0.7/bin/summarizedb.py
@@ -0,0 +1,35 @@
+import click
+import sys
+from sqlalchemy import *
+from dbwipes.summary import Summary
+
+
+@click.command()
+@click.option('--reset', is_flag=True)
+@click.argument('dbname')
+@click.argument('tables', nargs=-1)
+def script(reset, dbname, tables):
+  def get_tables(db):
+    q = "select tablename from pg_tables where schemaname = 'public'"
+    return [str(table) for (table,) in db.execute(q).fetchall()]
+
+  if not tables:
+    engine = create_engine("postgresql://localhost/%s" % dbname)
+    db = engine.connect()
+    tables = get_tables(db)
+    db.close()
+    engine.dispose()
+
+  for table in tables:
+    summary = Summary(dbname, table, nbuckets=500)
+    if reset:
+      summary.reset_cache()
+    summary()
+    summary.close()
+
+
+if __name__ == '__main__':
+  script()
+
+
+
