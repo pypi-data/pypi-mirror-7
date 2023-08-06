798099ac676f5159ee79a63e611f397f87d64c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:38 +0000

    Add package ETipsService/1.0.1/ETipsService-1.0.1.zip

---
+++ packages/ETipsService/ETipsService-1.0.1.zip/ETipsService-1.0.1/setup.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+# coding=utf-8
+__author__ = 'Jayin Ton'
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ETipsService',
+    packages=find_packages(),
+    version="1.0.1",
+    description="service lib for ETips",
+    author="Jayin Ton",
+    author_email="tonjayin@gmail.com",
+    url="https://github.com/Jayin/ETipsService",
+    keywords=['ETips'],
+    license='Apache License, Version 2.0',
+    install_requires=['beautifulsoup4', 'requests'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent"
+    ]
+
+)
