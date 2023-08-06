4256834d6fcb4e809a46492c8504bad84f9ac92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:11 +0000

    Add package helga-haskell/0.1.2/helga-haskell-0.1.2.tar.gz

---
+++ packages/helga-haskell/helga-haskell-0.1.2.tar.gz/helga-haskell-0.1.2/helga_haskell.py
@@ -0,0 +1,44 @@
+from functools import wraps
+from tryhaskell import TryHaskell
+from helga.plugins import match
+
+
+def clean_output(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            result = f(*args, **kwargs)
+        except TryHaskell.Error as e:
+            result = 'Uh oh: ' + e.message
+        # Truncate output and replace newlines to send only one message.
+        return result.replace('\n', ' ')[:300] if result else result
+    return wrapper
+
+
+def show_type(result):
+    return ' :: '.join([result.expr.strip(), result.type])
+
+
+def show_value(result):
+    if not result.ok:
+        # Reduce multiple white spaces with a single space.
+        msg = ' '.join(result.value.split())
+        return 'ERROR: ' + msg
+    if result.stdout:
+        return ' '.join(result.stdout)
+    if result.value:
+        return result.value
+    return show_type(result)
+
+
+@match(r'^(\:t |>\s*\:t |>).*$')
+@clean_output
+def haskell(client, channel, nick, message, matches):
+    # Simplify checking matches.
+    matches = ''.join(matches)
+    if ':t' in matches:
+        _, exp = message.split(':t', 1)
+        r = TryHaskell.get(exp)
+        return show_type(r) if r.ok else show_value(r)
+    elif '>' in matches:
+        return show_value(TryHaskell.get(message[1:]))
