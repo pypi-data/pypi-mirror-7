94ad7b80448bb455fcba70eb62dfc0bc6f4a1b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:44 +0000

    Add package citibike/0.0.3/citibike-0.0.3.tar.gz

---
+++ packages/citibike/citibike-0.0.3.tar.gz/citibike-0.0.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+py-citibike
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='citibike',
+    version='0.0.3',
+    author='Adam DePrince',
+    author_email='deprince@googlealumni.com',
+    description='',
+    long_description=__doc__,
+    py_modules=[
+        'citibike/__init__',
+        'citibike/reader',
+        'citibike/download',
+        'citibike/station',
+    ],
+    packages=['citibike'],
+    zip_safe=True,
+    include_package_data=True,
+    classifiers=[
+    ],
+    scripts=[
+        # 'scripts/citibike_to_postgresql'
+        'scripts/citibike_download_originals',
+        # Downloads and unpacks original citibike data
+        'scripts/citibike_dump',
+        # Downloads and unpacks original citibike data
+        'scripts/citibike_stations',
+        # Dumps station data and manages station data cache
+    ],
+    url='https://github.com/adamdeprince/python-citibike-data',
+    install_requires=[
+        'bz2file',
+        'functional',
+        'python-gflags',
+        'requests',
+        'progressbar',
+    ]
+)
