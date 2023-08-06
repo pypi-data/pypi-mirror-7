f2765ad6c234d192616b35d5d98ca06c9ab13956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:58 +0000

    Add package dc-campaign-finance-scraper/0.5.0/dc-campaign-finance-scraper-0.5.0.tar.gz

---
+++ packages/dc-campaign-finance-scraper/dc-campaign-finance-scraper-0.5.0.tar.gz/dc-campaign-finance-scraper-0.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dc-campaign-finance-scraper",
+    version="0.5.0",
+    packages=find_packages(exclude=["tests.*"]),
+    entry_points={
+        'console_scripts': [
+            'dc-campaign-finance-scraper=dc_campaign_finance_scraper.cli:cli'
+        ]
+    },
+    install_requires=[
+        'requests',
+        'click',
+        'beautifulsoup4',
+        'requests-cache',
+        'retrying',
+        'tablib'
+    ],
+
+    # metadata for upload to PyPI
+    long_description=open('README.md').read(),
+    author="Saul Shanabrook",
+    author_email="s.shanabrook@gmail.com",
+    description="Provides data from http://www.ocf.dc.gov/serv/download.asp in a nicer way",
+    url="http://github.com/codefordc/dc-campaign-finance-scraper",
+)
