17508d2dd2fb4c48f09b9bdd3c048c459553e642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package opentuner/0.1.0/opentuner-0.1.0.tar.gz

---
+++ packages/opentuner/opentuner-0.1.0.tar.gz/opentuner-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rest')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+
+setup(
+  name='opentuner',
+  version='0.1.0',
+  url='http://opentuner.org/',
+  license='MIT',
+  author='Jason Ansel',
+  author_email='jansel@jansel.net',
+  description='An extensible framework for program autotuning',
+  long_description=read_md('README.md'),
+  packages=['opentuner'],
+)
