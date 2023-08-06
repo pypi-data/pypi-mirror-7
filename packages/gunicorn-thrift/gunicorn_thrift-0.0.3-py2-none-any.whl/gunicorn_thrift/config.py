4fb3536c6703b794911617e2809af016c63765c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:09 +0000

    Add package gunicorn-thrift/0.0.3/gunicorn_thrift-0.0.3-py2-none-any.whl

---
+++ packages/gunicorn-thrift/gunicorn_thrift-0.0.3-py2-none-any.whl/gunicorn_thrift/config.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+from gunicorn.config import Setting, validate_string, validate_pos_int, WorkerClass
+
+WorkerClass.default = "thrift_sync"
+
+class ThriftTransportFactoryClass(Setting):
+    name = "thrift_transport_factory"
+    section = "Thrift"
+    cli = ["--thrift-transport-factory"]
+    validator = validate_string
+    default = "thrift.transport.TTransport:TBufferedTransportFactory"
+    desc = """\
+        The factory class for thrift transport.
+    """
+
+
+class ThriftProtocolFactoryClass(Setting):
+    name = "thrift_protocol_factory"
+    section = "Thrift"
+    cli = ["--thrift-protocol-factory"]
+    validator = validate_string
+    default = \
+        "thrift.protocol.TBinaryProtocol:TBinaryProtocolAcceleratedFactory"
+    desc = """\
+        The factory class for thrift transport.
+    """
+
+
+class ThriftClientTimeout(Setting):
+    name = "thrift_client_timeout"
+    section = "Thrift"
+    cli = ["--thrift-client-timeout"]
+    validator = validate_pos_int
+    default = None
+    desc = """\
+        Seconds to timeout a client if client is silent after this duration
+    """
