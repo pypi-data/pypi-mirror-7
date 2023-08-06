3f1b1e7df86b2ec64683ef46e72ea8432a1fb5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:55 +0000

    Add package quickbooks-python/0.1.2/quickbooks-python-0.1.2.tar.gz

---
+++ packages/quickbooks-python/quickbooks-python-0.1.2.tar.gz/quickbooks-python-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quickbooks-python',
+    url='',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'rauth'
+    ]
+)
