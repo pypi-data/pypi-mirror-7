43183c07997a5ffb57768118e4699dbfb4c6488c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:18 +0000

    Add package django-analytical/0.19.0/django-analytical-0.19.0.tar.gz

---
+++ packages/django-analytical/django-analytical-0.19.0.tar.gz/django-analytical-0.19.0/analytical/tests/settings.py
@@ -0,0 +1,16 @@
+"""
+django-analytical testing settings.
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'analytical',
+]
+
+SECRET_KEY = 'testing'
