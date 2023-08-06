784b11a8cba2427981d2cac3e4690ba637ec0221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:29 +0000

    Add package django-rest-tzolkin/0.3/django-rest-tzolkin-0.3.tar.gz

---
+++ packages/django-rest-tzolkin/django-rest-tzolkin-0.3.tar.gz/django-rest-tzolkin-0.3/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+def get_version(package):
+    '''
+    Return package version as listed in `__version__` in `init.py`.
+    '''
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search(
+        '^__version__ = [\'"]([^\'"]+)[\'"]', init_py, re.MULTILINE
+    ).group(1)
+
+
+version = get_version('tzolkin')
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    args = {'version': version}
+    print('You probably want to also tag the version now:')
+    print(' git tag -a release/{version} -m \'version {version}\''.format(
+        **args))
+    print(' git push --tags')
+    sys.exit()
+
+
+setup(
+    name='django-rest-tzolkin',
+    version=version,
+    url='http://github.com/dimddev/django-rest-tzolkin',
+    license='GPL',
+    description='Django-Rest API for convert: Gregorian, Long Count, Haab and Tzolkin dates',
+    author='Dim Mitar Hakini',
+    author_email='dmhakini@gmail.com',
+    packages=find_packages(),
+    install_requires=['Django==1.6', 'djangorestframework==2.3.14'],
+    test_suite='runtests.runtests',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
---
+++ packages/django-rest-tzolkin/django-rest-tzolkin-0.3.tar.gz/django-rest-tzolkin-0.3/tzolkin/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from distutils import version
+
+__version__ = '0.3'
+version_info = version.StrictVersion(__version__).version
