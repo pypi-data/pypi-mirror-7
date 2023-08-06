1ad56e2d42e8dbc55d84b8a19d9c2255771ec00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:00 +0000

    Add package SimpleMenu/1.0.3/SimpleMenu-1.0.3.tar.gz

---
+++ packages/SimpleMenu/SimpleMenu-1.0.3.tar.gz/SimpleMenu-1.0.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(name='SimpleMenu',
+	  version='1.0.3',
+	  py_modules=['SimpleMenu'],
+	  description="Console menus made simple.",
+	  author="Bede Kelly",
+	  author_email="bedekelly97@gmail.com",
+	  maintainer="Bede Kelly",
+	  maintainer_email="bedekelly97@gmail.com",
+	  url="https://github.com/bedekelly/simple-menu",
+	  provides=['SimpleMenu'],
+	  classifiers=[
+	    "Environment :: Console :: Curses",
+	    "Intended Audience :: Developers",
+	    "Natural Language :: English",
+	    "Operating System :: OS Independent",
+	    "Programming Language :: Python :: 3",
+	    "Programming Language :: Python :: Implementation :: CPython",
+	    "Topic :: Utilities"]
+	  )
