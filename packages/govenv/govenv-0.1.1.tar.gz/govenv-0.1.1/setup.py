ee627e1b12d820967fc60f06439d1513bdc083ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:07 +0000

    Add package govenv/0.1.1/govenv-0.1.1.tar.gz

---
+++ packages/govenv/govenv-0.1.1.tar.gz/govenv-0.1.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name='govenv',
+  version='0.1.1',
+  description='Golang Enhancements to virtualenv',
+  long_description=read('README.rst'),
+  url='https://github.com/termie/govenv',
+  packages=['govenv'],
+  author='Andy Smith',
+  author_email='github@anarkystic.com',
+  entry_points={
+    'virtualenvwrapper.post_mkvirtualenv': [
+      'gopath = govenv.gopath:post_mkvirtualenv',
+      ],
+    'virtualenvwrapper.post_activate_source': [
+      'gopath = govenv.gopath:post_activate_source',
+      ],
+    'virtualenvwrapper.post_deactivate_source': [
+      'gopath = govenv.gopath:post_deactivate_source',
+      ],
+    },
+  )
