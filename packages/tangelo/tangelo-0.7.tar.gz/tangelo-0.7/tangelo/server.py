6825d206bc11dddddef0706b51940da3b99172a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:52 +0000

    Add package tangelo/0.7/tangelo-0.7.tar.gz

---
+++ packages/tangelo/tangelo-0.7.tar.gz/tangelo-0.7/tangelo/server.py
@@ -0,0 +1,292 @@
+import cgi
+import datetime
+import sys
+import HTMLParser
+import os
+import cherrypy
+import json
+import imp
+import traceback
+import types
+
+import tangelo
+from tangelo.minify_json import json_minify
+import tangelo.util
+
+
+class Tangelo(object):
+    # An HTML parser for use in the error_page handler.
+    html = HTMLParser.HTMLParser()
+
+    # An in-band signal to treat HTML error messages as literal strings.
+    literal = "literal:::"
+
+    def __init__(self, stream=None):
+        self.stream = stream
+
+        # A dict containing information about imported modules.
+        self.modules = {}
+
+        # Mount a streaming API if requested.
+        #
+        # TODO(choudhury): make the mounting directory configurable by the
+        # user.
+        if self.stream:
+            cherrypy.tree.mount(stream, "/stream")
+
+    @staticmethod
+    def error_page(status, message, traceback, version):
+        if message.startswith(Tangelo.literal):
+            message = Tangelo.html.unescape(message[len(Tangelo.literal):])
+        return """<!doctype html>
+<h2>%s</h2>
+<p>%s
+<hr>
+<p><em>Powered by Tangelo</em> <img src=/favicon.ico>""" % (status, message)
+
+    def invoke_service(self, module, *pargs, **kwargs):
+        # TODO(choudhury): This method should attempt to load the named module,
+        # then invoke it with the given arguments.  However, if the named
+        # module is "config" or something similar, the method should instead
+        # launch a special "config" app, which lists the available app modules,
+        # along with docstrings or similar.  It should also allow the user to
+        # add/delete search paths for other modules.
+        tangelo.content_type("text/plain")
+
+        # Save the system path (be sure to *make a copy* using the list()
+        # function) - it will be modified before invoking the service, and must
+        # be restored afterwards.
+        origpath = list(sys.path)
+
+        # By default, the result should be an object with error message in if
+        # something goes wrong; if nothing goes wrong this will be replaced
+        # with some other object.
+        result = {}
+
+        # Store the modpath in the thread-local storage (tangelo.paths() makes
+        # use of this per-thread data, so this is the way to get the data
+        # across the "module boundary" properly).
+        modpath = os.path.dirname(module)
+        cherrypy.thread_data.modulepath = modpath
+        cherrypy.thread_data.modulename = module
+
+        # Extend the system path with the module's home path.
+        sys.path.insert(0, modpath)
+
+        # Import the module if not already imported previously (or if the
+        # module to import, or its configuration file, has been updated since
+        # the last import).
+        try:
+            stamp = self.modules.get(module)
+            mtime = os.path.getmtime(module)
+
+            config_file = module[:-2] + "json"
+            config_mtime = None
+            if os.path.exists(config_file):
+                config_mtime = os.path.getmtime(config_file)
+
+            if (stamp is None or
+                    mtime > stamp["mtime"] or
+                    (config_mtime is not None and
+                     config_mtime > stamp["mtime"])):
+                if stamp is None:
+                    tangelo.log("loading new module: " + module)
+                else:
+                    tangelo.log("reloading module: " + module)
+
+                # Load any configuration the module might carry with it.
+                if config_mtime is not None:
+                    try:
+                        with open(config_file) as f:
+                            config = json.loads(json_minify(f.read()))
+                            if type(config) != dict:
+                                msg = ("Service module configuration file " +
+                                       "does not contain a key-value store " +
+                                       "(i.e., a JSON Object)")
+                                tangelo.log(msg)
+                                raise TypeError(msg)
+                    except IOError:
+                        tangelo.log("Could not open config file %s" %
+                                    (config_file))
+                        raise
+                    except ValueError as e:
+                        tangelo.log("Error reading config file %s: %s" %
+                                    (config_file, e))
+                        raise
+                else:
+                    config = {}
+
+                cherrypy.config["module-config"][module] = config
+
+                # Remove .py to get the module name
+                name = module[:-3]
+
+                # Load the module.
+                service = imp.load_source(name, module)
+                self.modules[module] = {"module": service,
+                                        "mtime": max(mtime, config_mtime)}
+            else:
+                service = stamp["module"]
+        except:
+            bt = traceback.format_exc()
+
+            tangelo.log("Error importing module %s" % (tangelo.request_path()),
+                        "SERVICE")
+            tangelo.log(bt, "SERVICE")
+
+            result = tangelo.HTTPStatusCode("501 Error in Python Service",
+                                            Tangelo.literal + "There was an error while " +
+                                            "trying to import module " +
+                                            "%s:<br><pre>%s</pre>" %
+                                            (tangelo.request_path(), bt))
+        else:
+            # Try to run the service - either it's in a function called
+            # "run()", or else it's in a REST API consisting of at least one of
+            # "get()", "put()", "post()", or "delete()".
+            #
+            # Collect the result in a variable - depending on its type, it will
+            # be transformed in some way below (by default, to JSON, but may
+            # also raise a cherrypy exception, log itself in a streaming table,
+            # etc.).
+            try:
+                if 'run' in dir(service):
+                    # Call the module's run() method, passing it the positional
+                    # and keyword args that came into this method.
+                    result = service.run(*pargs, **kwargs)
+                else:
+                    # Reaching here means it's a REST API.  Check for the
+                    # requested method, ensure that it was marked as being part
+                    # of the API, and call it; or give a 405 error.
+                    method = cherrypy.request.method
+                    restfunc = service.__dict__[method.lower()]
+                    if (restfunc is not None and
+                            hasattr(restfunc, "restful") and
+                            restfunc.restful):
+                        result = restfunc(*pargs, **kwargs)
+                    else:
+                        result = tangelo.HTTPStatusCode(405,
+                                                        "Method not allowed")
+            except Exception as e:
+                bt = traceback.format_exc()
+
+                tangelo.log("Caught exception while executing service %s" %
+                            (tangelo.request_path()), "SERVICE")
+                tangelo.log(bt, "SERVICE")
+
+                result = tangelo.HTTPStatusCode("501 Error in Python Service",
+                                                Tangelo.literal + "There was an error " +
+                                                "executing service " +
+                                                "%s:<br><pre>%s</pre>" %
+                                                (tangelo.request_path(), bt))
+
+        # Restore the path to what it was originally.
+        sys.path = origpath
+
+        # Check the type of the result to decide what result to finally return:
+        #
+        # 1. If it is an HTTPStatusCode object, raise a cherrypy HTTPError
+        # exception, which will cause the browser to do the right thing.
+        #
+        # 2. TODO: If it's a Python generator object, log it with the Tangelo
+        # streaming API.
+        #
+        # 3. If it's a Python dictionary, convert it to JSON.
+        #
+        # 4. If it's a string, don't do anything to it.
+        #
+        # This allows the services to return a Python object if they wish, or
+        # to perform custom serialization (such as for MongoDB results, etc.).
+        if isinstance(result, tangelo.HTTPStatusCode):
+            if result.msg:
+                raise cherrypy.HTTPError(result.code, result.msg)
+            else:
+                raise cherrypy.HTTPError(result.code)
+        elif "next" in dir(result):
+            if self.stream:
+                return self.stream.add(result)
+            else:
+                return json.dumps({"error": "Streaming is not supported " +
+                                            "in this instance of Tangelo"})
+        elif not isinstance(result, types.StringTypes):
+            try:
+                result = json.dumps(result)
+            except TypeError as e:
+                msg = Tangelo.literal + "<p>A JSON type error occurred in service " + tangelo.request_path() + ":</p>"
+                msg += "<p><pre>" + cgi.escape(e.message) + "</pre></p>"
+
+                raise cherrypy.HTTPError("501 Error in Python Service", msg)
+
+        return result
+
+    @staticmethod
+    def dirlisting(dirpath, reqpath):
+        if reqpath[-1] == "/":
+            reqpath = reqpath[:-1]
+        files = filter(lambda x: len(x) > 0 and x[0] != ".",
+                       os.listdir(dirpath))
+        # filespec = ["Type", "Name", "Last modified", "Size"]
+        filespec = []
+        for f in files:
+            p = dirpath + os.path.sep + f
+            try:
+                s = os.stat(p)
+            except OSError:
+                pass
+            else:
+                mtime = (datetime.datetime
+                         .fromtimestamp(s.st_mtime)
+                         .strftime("%Y-%m-%d %H:%M:%S"))
+
+                if os.path.isdir(p):
+                    f += "/"
+                    t = "dir"
+                    s = "-"
+                else:
+                    t = "file"
+                    s = s.st_size
+
+                filespec.append([t, "<a href=\"%s/%s\">%s</a>" %
+                                    (reqpath, f, f),
+                                 mtime, s])
+
+        filespec = "\n".join(
+            map(lambda row: "<tr>" +
+                            "".join(map(lambda x: "<td>%s</td>" % x,
+                                    row)) +
+                            "</tr>",
+                filespec))
+
+        result = """<!doctype html>
+<title>Index of %s</title>
+<h1>Index of %s</h1>
+<table>
+<tr>
+    <th>Type</th><th>Name</th><th>Last Modified</th><th>Size</th>
+</tr>
+%s
+</table>
+""" % (reqpath, reqpath, filespec)
+
+        return result
+
+    @cherrypy.expose
+    def default(self, *path, **args):
+        target = cherrypy.thread_data.target
+        if target is not None:
+            if target["type"] == "file":
+                return cherrypy.lib.static.serve_file(target["path"])
+            elif target["type"] == "dir":
+                return Tangelo.dirlisting(target["path"],
+                                          cherrypy.request.path_info)
+            elif target["type"] == "service":
+                return self.invoke_service(target["path"],
+                                           *target["pargs"],
+                                           **args)
+            elif target["type"] == "404":
+                raise cherrypy.lib.static.serve_file(target["path"])
+            elif target["type"] == "restricted":
+                raise cherrypy.HTTPError("403 Forbidden",
+                                         "The path '%s' is forbidden" % (cherrypy.serving.request.path_info))
+            else:
+                raise RuntimeError("Illegal target type '%s'" %
+                                   (target["type"]))
---
+++ packages/tangelo/tangelo-0.7.tar.gz/tangelo-0.7/web/examples/lyra.save/index.js
@@ -0,0 +1,278 @@
+/*jslint browser: true */
+/*globals $, tangelo, d3 */
+
+var app = {};
+app.vis = {
+    name: null,
+    vega: null,
+    timeline: null
+};
+app.lyra = null;
+app.data = null;
+app.editor = null;
+app.unsaved = {};
+app.unsavedIdx = 1;
+app.new = false;
+
+function launchLyra(qargs) {
+    "use strict";
+
+    var query = $.param(qargs || {});
+    if (query.length > 0) {
+        query = "?" + query;
+    }
+
+    app.lyra = window.open("/lyra/editor.html" + query, "_blank");
+}
+
+function createNew() {
+    "use strict";
+
+    app.new = true;
+    launchLyra({
+        editor: true,
+        data: encodeURIComponent(JSON.stringify(app.data))
+    });
+}
+
+function edit() {
+    "use strict";
+
+    launchLyra({
+        editor: true,
+        timeline: encodeURIComponent(JSON.stringify(app.vis.timeline)),
+        data: encodeURIComponent(JSON.stringify(app.data))
+    });
+}
+
+function refresh(vega) {
+    "use strict";
+
+    vg.parse.spec(vega, function (chart) {
+        chart({
+            el: "#vega",
+            renderer: "svg"
+        }).update();
+
+        d3.select("#edit")
+            .attr("disabled", null);
+    });
+}
+
+function loadEditorData(editor, data) {
+    "use strict";
+
+    if (editor) {
+        editor.setValue(JSON.stringify(data, null, "    "));
+    }
+}
+
+function getUrls(girderApi, collection, folder, callback) {
+    "use strict";
+
+    var data = {
+        text: collection
+    };
+
+    $.getJSON(girderApi + "/collection", data, function (lyra) {
+        data = {
+            parentType: "collection",
+            parentId: lyra._id,
+            text: folder
+        };
+
+        $.getJSON(girderApi + "/folder", data, function (folder) {
+            if (folder.length === 0) {
+                console.warn("error: no girder path /lyra/" + folder + " found!");
+                return;
+            }
+
+            if (folder.length > 1) {
+                console.warn("error: more than one path /lyra/" + folder + " found!");
+                return;
+            }
+
+            data = {
+                folderId: folder[0]._id
+            };
+
+            $.getJSON(girderApi + "/item", data, function (files) {
+                callback(files);
+            });
+
+        });
+    });
+}
+
+function receiveMessage(e) {
+    "use strict";
+
+    app.vis.timeline = e.data.timeline;
+    app.vis.vega = e.data.vega;
+    app.data = $.extend(true, [], e.data.vega.data[0].values);
+
+    if (app.new) {
+        app.vis.name = app.vis.visName = "Unsaved " + app.unsavedIdx;
+
+        app.unsaved[app.vis.name] = $.extend(true, {}, app.vis);
+
+        app.unsavedIdx += 1;
+
+        d3.select("#vis-files")
+            .data([app.vis], function (d) {
+                return app.vis.visName;
+            })
+            .append("li")
+            .append("a")
+            .classed("vis-file", true)
+            .attr("href", "#")
+            .text(app.vis.visName)
+            .on("click", app.clickVis);
+
+        d3.select("#vis-file")
+            .html(app.vis.visName + " <span class=\"caret\"></span>");
+
+        app.new = false;
+    }
+
+    refresh(app.vis.vega);
+    app.lyra = null;
+}
+
+$(function () {
+    "use strict";
+
+    tangelo.config("config.json", function (config) {
+        window.addEventListener("message", receiveMessage, false);
+
+        d3.select("#create")
+            .on("click", createNew);
+
+        d3.select("#edit")
+            .on("click", edit);
+
+        d3.select("#edit-data")
+            .on("click", function () {
+                var el;
+
+                d3.select("#edit-area")
+                    .selectAll("*")
+                    .remove();
+
+                el = d3.select("#edit-area")
+                    .append("div")
+                    .attr("id", "ace-editor")
+                    .node();
+
+                app.editor = ace.edit(el);
+                app.editor.setTheme("ace/theme/twilight");
+                app.editor.getSession().setMode("ace/mode/javascript");
+
+                //app.editor.setValue(JSON.stringify(app.data, null, "    "));
+                loadEditorData(app.editor, app.data);
+
+                d3.select("#edit-area")
+                    .append("button")
+                    .classed("btn", true)
+                    .classed("btn-default", true)
+                    .text("Save")
+                    .on("click", function () {
+                        var text = app.editor.getValue();
+
+                        try {
+                            app.data = JSON.parse(text);
+
+                            if (app.vega) {
+                                app.vega.data[0].values = app.data;
+                                refresh(app.vega);
+                            }
+
+                            d3.select("#edit-area")
+                                .selectAll("*")
+                                .remove();
+                        } catch (e) {
+                            alert("Error!  Couldn't not parse data as JSON: " + e);
+                        }
+                    });
+
+                d3.select("#edit-area")
+                    .append("button")
+                    .classed("btn", true)
+                    .classed("btn-default", true)
+                    .text("Close")
+                    .on("click", function () {
+                        d3.select("#edit-area")
+                            .selectAll("*")
+                            .remove();
+                    });
+            });
+
+        getUrls(config.girderApi, config.collection, config.dataFolder, function (files) {
+            d3.select("#data-sources")
+                .selectAll("li")
+                .data(files)
+                .enter()
+                .append("li")
+                .append("a")
+                .classed("data-source", true)
+                .attr("href", "#")
+                .text(function (d) {
+                    d.dataName = d.name.split(".")[0];
+                    return d.dataName;
+                })
+                .on("click", function (d) {
+                    d3.select("#data-source")
+                        .html(d.dataName + " <span class=\"caret\"></span>");
+
+                    $.getJSON(config.girderApi + "/item/" + d._id + "/download", function (data) {
+                        app.data = data;
+                        loadEditorData(app.editor, app.data);
+                    });
+                });
+            $("a.data-source").get(0) && $("a.data-source").get(0).click();
+        });
+
+        app.clickVis = function (d) {
+            d3.select("#vis-file")
+                .html(d.visName + " <span class=\"caret\"></span>");
+
+            if (d.visName.lastIndexOf("Unsaved") === 0) {
+                app.vis = app.unsaved[d.visName];
+                console.log(app.vis);
+            } else {
+                $.getJSON(config.girderApi + "/item/" + d._id + "/download", function (data) {
+                    console.log(data);
+                });
+            }
+
+            refresh(app.vis.vega);
+        };
+
+        getUrls(config.girderApi, config.collection, config.visFolder, function (files) {
+            var unsaved = Object.keys(app.unsaved).map(function (x) {
+                return app.unsaved(x);
+            }),
+                data = files.concat(unsaved);
+
+            $.each(data, function (i, v) {
+                v.visName = v.name.split(".")[0];
+            });
+
+            d3.select("#vis-files")
+                .selectAll("li")
+                .data(data, function (d) {
+                    return d.visName;
+                })
+                .enter()
+                .append("li")
+                .append("a")
+                .classed("vis-file", true)
+                .attr("href", "#")
+                .text(function (d) {
+                    return d.visName;
+                })
+                .on("click", app.clickVis);
+            $("a.vis-file").get(0) && ("a.vis-file").get(0).click();
+        });
+    });
+});
