a2b62717204bf05166a005655e506f13bdfe4372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:53 +0000

    Add package value_objects/2.0.0/value_objects-2.0.0.tar.gz

---
+++ packages/value_objects/value_objects-2.0.0.tar.gz/value_objects-2.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+# http://guide.python-distribute.org/creation.html
+setup( 
+    name = 'value_objects',
+    version = '2.0.0',
+    author = 'Steve Wedig',
+    packages = ['value_objects'],
+    url = 'https://github.com/stevewedig/value-objects-in-python/',
+    license = 'LICENSE.txt',
+    description = 'Helpers for implementing value objects.',
+    long_description = 'The online documentation can be found here: https://github.com/stevewedig/value-objects-in-python/blob/master/README.md',
+ )
+
