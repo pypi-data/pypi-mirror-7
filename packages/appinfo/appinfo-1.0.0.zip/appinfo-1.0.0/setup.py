938d6d6c1ee1d6370de1179150de7858d793845c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:51 +0000

    Add package appinfo/1.0.0/appinfo-1.0.0.zip

---
+++ packages/appinfo/appinfo-1.0.0.zip/appinfo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+﻿from distutils.core import setup
+
+setup(
+        name = 'appinfo',
+        version = '1.0.0',
+        py_modules = ['appinfo'],
+        author = 'guiyun',
+        author_email = 'guiyun.gy@gmail.com'
+        # description = 'test for appinfo setup',
+        )
