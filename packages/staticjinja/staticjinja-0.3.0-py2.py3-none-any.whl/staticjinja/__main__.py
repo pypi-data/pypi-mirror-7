ba08ef2f8ce649e6f94c530a7e607623ed50a8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:59 +0000

    Add package staticjinja/0.3.0/staticjinja-0.3.0-py2.py3-none-any.whl

---
+++ packages/staticjinja/staticjinja-0.3.0-py2.py3-none-any.whl/staticjinja/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from __future__ import absolute_import
+
+import os
+
+import staticjinja
+
+if __name__ == "__main__":
+    searchpath = os.path.join(os.getcwd(), 'templates')
+    renderer = staticjinja.make_renderer(searchpath=searchpath)
+    renderer.run(use_reloader=True)
