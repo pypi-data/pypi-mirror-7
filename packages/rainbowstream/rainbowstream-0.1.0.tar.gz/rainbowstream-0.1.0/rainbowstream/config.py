85c1e7fa1601c4a46331c657d8c66365e27f2d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:48 +0000

    Add package rainbowstream/0.1.0/rainbowstream-0.1.0.tar.gz

---
+++ packages/rainbowstream/rainbowstream-0.1.0.tar.gz/rainbowstream-0.1.0/rainbowstream/config.py
@@ -0,0 +1,27 @@
+# Max Search record
+SEARCH_MAX_RECORD = 5
+# Default home tweet
+HOME_TWEET_NUM = 5
+# Default retweets show
+RETWEETS_SHOW_NUM = 5
+# Default direct message's number
+MESSAGES_DISPLAY = 5
+# Max trending topics display
+TREND_MAX = 10
+# Autocomplete history
+HISTORY_FILENAME = 'completer.hist'
+
+# Stream Domain
+USER_DOMAIN = 'userstream.twitter.com'
+PUBLIC_DOMAIN = 'stream.twitter.com'
+SITE_DOMAIN = 'sitestream.twitter.com'
+# Actually called
+DOMAIN = USER_DOMAIN
+
+# Filter and Ignore list ex: ['@fat','@mdo']
+ONLY_LIST = []
+IGNORE_LIST = []
+
+# Image shift and size
+IMAGE_SHIFT = 10
+IMAGE_MAX_HEIGHT = 40
