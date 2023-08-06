7f1918c69f140b04837f2011444f4ec771edf475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:06 +0000

    Add package pluginplot/0.2.0/pluginplot-0.2.0.tar.gz

---
+++ packages/pluginplot/pluginplot-0.2.0.tar.gz/pluginplot-0.2.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__version__ = '0.2.0'
+__author__  = 'Zeuxis Lo'
+__email__   = 'seekstudio@gmail.com'
+
+setup(
+    name='pluginplot',
+    author=__author__,
+    author_email=__email__,
+    version=__version__,
+    license='BSD',
+    url='https://github.com/zeuxisoo/python-pluginplot',
+    description='Yet another simple plugin system for Python like WordPress plugin system',
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
