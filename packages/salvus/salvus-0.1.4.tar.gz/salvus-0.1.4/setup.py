5ac726ffc5a58dd660b9ebd72166a0affb698735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package salvus/0.1.4/salvus-0.1.4.tar.gz

---
+++ packages/salvus/salvus-0.1.4.tar.gz/salvus-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "salvus",
+    version = "0.1.4",
+    packages = ['salvus'],
+    author = "philipbergen",
+    author_email = "philipbergen at gmail com",
+    description = "In-memory credential store with yubikey auth",
+    long_description=open('README.rst').read(),
+    license = 'LICENSE.txt',
+    keywords = "yubikey auth",
+    scripts = ['bin/salvus'],
+    install_requires=[
+        "yubikey>=0.2",
+        "docopt>=0.6",
+        "daemonize>=2.2",
+    ],
+)
+
