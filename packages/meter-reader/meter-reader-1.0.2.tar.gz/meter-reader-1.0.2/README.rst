4248e215b782c72892449ada4c3845e3bba61445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:54 +0000

    Add package meter-reader/1.0.2/meter-reader-1.0.2.tar.gz

---
+++ packages/meter-reader/meter-reader-1.0.2.tar.gz/meter-reader-1.0.2/README.rst
@@ -0,0 +1,122 @@
+Meter Reader is a client library and command line client for retrieving
+nearly realtime energy usage data from a smart meter via the Eagle™ Home
+Energy Gateway. See
+`Rainforest™ Automation <http://www.rainforestautomation.com>`_ for more
+about the Eagle™ Home Energy Gateway.
+
+Meter Reader is not affiliated with the Eagle™ Home Energy Gateway or
+Rainforest™ Automation.
+
+Installation
+-------------------------------------------------------------------------------
+
+.. code-block:: bash
+
+    $ pip install meter-reader
+
+Usage
+-------------------------------------------------------------------------------
+Meter Reader is intended to be used as a library for other applications
+but it does contain a command line application called mr.
+
+.. code-block:: bash
+
+    $ mr < ip address >
+
+This will run the ``LIST_DEVICES`` devices command on the gateway and display
+a formatted response. Other commands, such as ``GET_DEVICE_DATA``, will first
+run the ``LIST_DEVICES`` command to determine the MAC address of the gateway.
+
+Commands can be specified with the `'-c'` option. For example
+
+.. code-block:: bash
+
+    $ mr -c GET_DEVICE_DATA < ip address >
+
+    MessageCluster
+        DeviceMacId          xx:xx:xx:xx:xx:xx:xx:xx
+        MeterMacId           xx:xx:xx:xx:xx:xx:xx
+        TimeStamp            0
+        Id                   0
+        Priority             None
+        Text                 None
+        ConfirmationRequired N
+        Confirmed            N
+        Read                 Y
+        Queue                active
+    CurrentSummation
+        DeviceMacId          xx:xx:xx:xx:xx:xx:xx:xx
+        MeterMacId           xx:xx:xx:xx:xx:xx:xx
+        TimeStamp            2014-04-19 16:01:22+00:00
+        SummationDelivered   12949746
+        SummationReceived    0
+        Multiplier           1
+        Divisor              1000
+        DigitsRight          3
+        DigitsLeft           15
+        SuppressLeadingZero  Y
+    NetworkInfo
+    ...
+
+    $ mr -c GET_SUMMATION_VALUES < ip address >
+
+    2014-04-18 16:30:00+00:00, Summation, 0.350
+    2014-04-18 17:30:00+00:00, Summation, 0.322
+    2014-04-18 18:30:00+00:00, Summation, 0.193
+    2014-04-18 19:30:00+00:00, Summation, 0.285
+    2014-04-18 20:30:00+00:00, Summation, 0.286
+    2014-04-18 21:30:00+00:00, Summation, 0.351
+    ...
+
+There are two ways to retrieve instantaneous demand:
+
+1. Send the ``GET_INSTANTANEOUS_DEMAND`` command directly to the gateway. This
+   will return a nearly raw response from the gateway (formatting is applied).
+
+.. code-block:: bash
+
+    $ mr -c GET_INSTANTANEOUS_DEMAND < ip address >
+
+        InstantaneousDemand
+        DeviceMacId         xx:xx:xx:xx:xx:xx:xx:xx
+        MeterMacId          xx:xx:xx:xx:xx:xx:xx
+        TimeStamp           2014-04-19 15:35:27+00:00
+        Demand              297
+        Multiplier          1
+        Divisor             1000
+        DigitsRight         3
+        DigitsLeft          15
+        SuppressLeadingZero Y
+
+2. Supply the ``--get-instant-demand`` argument. This will post-process the
+response before displaying it.
+
+.. code-block:: bash
+
+    $ mr --get-instant-demand < ip address >
+
+    2014-04-19 15:58:39+00:00, 0.292kW
+
+Raw and unformatted data returned by the gatway, can be viewed by using the
+`'-r'` option.
+
+.. code-block:: bash
+
+    $ mr -r -c GET_DEVICE_DATA < ip address >
+
+Including Meter Reader in an application
+-------------------------------------------------------------------------------
+
+.. code-block:: python
+
+    from meter_reader import Gateway
+
+    GATEWAY_ADDRESS = '192.168.1.10'
+
+    gw = Gateway(GATEWAY_ADDRESS)
+    response = gw.run_command('GET_DEVICE_DATA')
+    print('Network Info')
+    print(response['NetworkInfo'])
+
+    timestamp, demand = gw.get_instantaneous_demand()
+    print('Demand {0!s} at {1!s}'.format(demand, timestamp))
---
+++ packages/meter-reader/meter-reader-1.0.2.tar.gz/meter-reader-1.0.2/meter_reader/gateway.py
@@ -0,0 +1,152 @@
+"""
+Meter Reader
+
+Copyright (c) 2014, Emmanuel Levijarvi
+License: BSD
+"""
+
+import socket
+from datetime import timedelta, datetime
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+from contextlib import closing
+import collections
+# python 2/3 compatibilty fix-up
+try:
+    import StringIO
+except ImportError:
+    import io as StringIO
+
+from meter_reader import utc
+
+utctz = utc.UTC()
+BEGINNING_OF_TIME = datetime(2000, 1, 1, tzinfo=utctz)
+
+DEFAULT_PORT = 5002
+COMMANDS = ('LIST_DEVICES', 'GET_DEVICE_DATA', 'GET_INSTANTANEOUS_DEMAND',
+            'GET_DEMAND_VALUES', 'GET_SUMMATION_VALUES',
+            'GET_FAST_POLL_STATUS')
+SUPPORTED_ARGS = ('INTERVAL', 'FREQUENCY', 'STARTTIME', 'ENDTIME', 'DURATION',
+                  'NAME')
+
+
+class GatewayError(Exception):
+    def __init__(self, address, command, error='', code=None):
+        self.address = address
+        self.code = code
+        self.error = error
+        self.command = command
+
+    def __str__(self):
+        return 'Unable to connect to {0}:{1}. {2}'.format(self.address[0],
+                                                          self.address[1],
+                                                          self.error)
+
+
+class Gateway(object):
+    def __init__(self, address, port=DEFAULT_PORT):
+        self.address = (address, port)
+        self.timeout = socket.getdefaulttimeout()
+        self.mac_id = None
+        devices = self.run_command(Name='LIST_DEVICES', convert=False)
+        self.mac_id = devices['DeviceInfo']['DeviceMacId']
+
+    def generate_command_xml(self, **kwargs):
+        c = ET.Element('LocalCommand')
+        for tag, value in kwargs.items():
+            if tag.upper() not in SUPPORTED_ARGS or value is None:
+                continue
+            if tag.upper() in ('STARTTIME', 'ENDTIME'):
+                value = hex((value - BEGINNING_OF_TIME).seconds)
+            elif tag.upper() in ('FREQUENCY', 'DURATION'):
+                value = hex(value)
+            ET.SubElement(c, tag).text = value
+        if self.mac_id is not None:
+            ET.SubElement(c, 'MacID').text = self.mac_id
+        md = minidom.parseString(ET.tostring(c, encoding='utf-8'))
+        return md.toprettyxml(indent="  ")
+
+    def run_command_raw(self, **kwargs):
+        with closing(socket.create_connection(self.address,
+                                              self.timeout)) as s:
+            s.sendall(self.generate_command_xml(**kwargs).encode('utf-8'))
+            cmd_output = s.makefile().read()
+        return cmd_output
+
+    def run_command(self, convert=True, **kwargs):
+        try:
+            response = self.run_command_raw(**kwargs)
+        except socket.error as e:
+            raise GatewayError(self.address, kwargs.get('Name', ''),
+                               e.strerror, e.errno)
+        # responses come as multiple XML fragments. Enclose them in
+        # <response> to ensure valid XML.
+        if 'Interval data start' in response or 'HistoryData' in response:
+            return self.xml2list('<response>{0}</response>'.format(response),
+                                 convert)
+        else:
+            return self.xml2dict('<response>{0}</response>'.format(response),
+                                 convert)
+
+    @staticmethod
+    def xml2dict(xml, convert=True):
+        with closing(StringIO.StringIO(xml)) as f:
+            path = [{}]
+            for event, element in ET.iterparse(f, events=('start', 'end')):
+                if element.tag == 'response':
+                    continue
+                if event == 'start':
+                    if element.text is None or element.text.strip() != '':
+                        if convert:
+                            value = convert_data(element.tag, element.text)
+                        else:
+                            value = element.text
+                        key = next(reversed(path[-1]))
+                        path[-1][key][element.tag] = value
+                    else:
+                        new_level = collections.OrderedDict()
+                        new_level[element.tag] = collections.OrderedDict()
+                        path.append(new_level)
+                elif element.text is not None and element.text.strip() == '':
+                    later = path.pop()
+                    path[-1].update(later)
+        return path[0]
+
+    @staticmethod
+    def xml2list(xml, convert=True):
+        with closing(StringIO.StringIO(xml)) as f:
+            response = []
+            for event, element in ET.iterparse(f, events=('start', 'end')):
+                if element.tag in ('Info', 'Text', 'response'):
+                    continue
+                if event == 'start' and (element.text is not None and
+                                         element.text.strip() == ''):
+                    response.append({})
+                if event == 'end' and (element.text is None or
+                                       element.text.strip() != ''):
+                    if convert:
+                        value = convert_data(element.tag, element.text)
+                    else:
+                        value = element.text
+                    response[-1][element.tag] = value
+        return response
+
+    def get_instantaneous_demand(self):
+        resp = self.run_command(name='GET_DEVICE_DATA')['InstantaneousDemand']
+        demand = float(resp['Demand']) * resp['Multiplier'] / resp['Divisor']
+        return (resp['TimeStamp'], demand)
+
+
+def convert_data(key, value):
+    if value is None:
+        return None
+    if 'MacId' in key or 'Code' in key or 'Key' in key:
+        len_ = 15
+        if key == 'MeterMacId' or key == 'CoordMacId':
+            len_ = 13
+        return ':'.join(value.lstrip('0x')[i:i+2] for i in range(0, len_, 2))
+    if key.upper() in ('TIMESTAMP', 'ENDTIME') and int(value, 0):
+        return BEGINNING_OF_TIME + timedelta(0, int(value, 0))
+    if isinstance(value, str) and value.startswith('0x'):
+        return int(value, 0)
+    return value
