e286f95d92d1b0a2c99dbc226032e45257bd3966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package describe_it/1.1.0/describe_it-1.1.0.tar.gz

---
+++ packages/describe_it/describe_it-1.1.0.tar.gz/describe_it-1.1.0/describe_it/__init__.py
@@ -0,0 +1,143 @@
+import sys
+
+registered_contexts = []
+active_contexts = []
+registered_it_fns = []
+
+
+class Fixture(object):
+    pass
+
+
+def get_it_fns_for(module):
+    return (i for i in registered_it_fns if i.context.module == module)
+
+
+class Context(object):
+
+    def __init__(self, describe_fn, parent, skip=False):
+        self.describe_fn = describe_fn
+        self.parent = parent
+        self.it_fns = []
+        self.before_each_fns = []
+        self.after_each_fns = []
+        self.skip = skip
+
+    @property
+    def module(self):
+        return sys.modules[self.describe_fn.__module__]
+
+    def add_before_each(self, before_each_fn):
+        self.before_each_fns.append(before_each_fn)
+
+    def add_after_each(self, after_each_fn):
+        self.after_each_fns.append(after_each_fn)
+
+    def run_before_eaches(self):
+        if self.parent:
+            self.parent.run_before_eaches()
+
+        for be in self.before_each_fns:
+            be()
+
+    def run_after_eaches(self):
+        for ae in self.after_each_fns:
+            ae()
+
+        if self.parent:
+            self.parent.run_after_eaches()
+
+    def should_skip(self):
+        return self.skip or self.parent and self.parent.should_skip()
+
+    def __str__(self):
+        if self.parent:
+            return '{0} {1}'.format(
+                str(self.parent), self.describe_fn.__name__)
+        return self.describe_fn.__name__
+
+
+def describe(describe_fn,
+             registered_contexts=registered_contexts,
+             active_contexts=active_contexts,
+             skip=False):
+
+    parent = _current_active_context(active_contexts=active_contexts)
+
+    context = Context(describe_fn=describe_fn, parent=parent, skip=skip)
+
+    registered_contexts.append(context)
+
+    active_contexts.append(context)
+    describe_fn()
+    active_contexts.pop()
+
+
+def xdescribe(describe_fn,
+              registered_contexts=registered_contexts,
+              active_contexts=active_contexts):
+    describe(describe_fn, registered_contexts, active_contexts, skip=True)
+
+
+def describe_skip(*args, **kwargs):
+    xdescribe(*args, **kwargs)
+
+
+def it(it_fn,
+       registered_it_fns=registered_it_fns,
+       active_contexts=active_contexts):
+
+    context = _current_active_context(active_contexts=active_contexts)
+    if not context:
+        raise Exception("Adding an 'it' without telling me what you're "
+                        "describing seems a bit silly.")
+
+    it_fn.context = context
+    it_fn.skip = False
+    registered_it_fns.append(it_fn)
+
+
+def xit(it_fn,
+        registered_it_fns=registered_it_fns,
+        active_contexts=active_contexts):
+
+    context = _current_active_context(active_contexts=active_contexts)
+    if not context:
+        raise Exception("Adding an 'xit' without telling me what you're "
+                        "describing seems a bit silly.")
+
+    it_fn.context = context
+    it_fn.skip = True
+    registered_it_fns.append(it_fn)
+
+
+def it_skip(*args, **kwargs):
+    xit(*args, **kwargs)
+
+
+def before_each(before_each_fn,
+                registered_contexts=registered_contexts,
+                active_contexts=active_contexts):
+
+    context = _current_active_context(active_contexts=active_contexts)
+    if not context:
+        raise Exception("Adding a 'before_each' without describing something "
+                        "seems a bit silly.")
+
+    context.add_before_each(before_each_fn)
+
+
+def after_each(after_each_fn,
+               registered_contexts=registered_contexts,
+               active_contexts=active_contexts):
+
+    context = _current_active_context(active_contexts=active_contexts)
+    if not context:
+        raise Exception("Adding a 'after_each' without describing something "
+                        "seems a bit silly.")
+
+    context.add_after_each(after_each_fn)
+
+
+def _current_active_context(active_contexts=active_contexts):
+    return active_contexts[-1] if active_contexts else None
---
+++ packages/describe_it/describe_it-1.1.0.tar.gz/describe_it-1.1.0/describe_it/noseplugin.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import logging
+import nose
+import re
+import describe_it as di
+import unittest
+import itertools
+
+log = logging.getLogger('nose.plugins.describe_it')
+
+
+class DescribeItPlugin(nose.plugins.Plugin):
+    name = 'describe-it'
+
+    def wantModule(self, module):
+        return re.search('spec$', module.__name__)
+
+    def wantFile(self, file):
+        return re.search('spec\.py$', file)
+
+    def loadTestsFromModule(self, module):
+        is_spec_module = re.search('spec$', module.__name__)
+        if is_spec_module:
+            test_cases = (ContextTestCase(i)
+                          for i in di.get_it_fns_for(module))
+            return test_cases
+
+
+class ContextTestCase(unittest.TestCase):
+
+    __test__ = False
+
+    def __init__(self, it_fn):
+        self.it_fn = it_fn
+        unittest.TestCase.__init__(self, methodName='run_test')
+
+    def setUp(self):
+        self.it_fn.context.run_before_eaches()
+
+    def tearDown(self):
+        self.it_fn.context.run_after_eaches()
+
+    def run_test(self):
+        if self.it_fn.skip or self.it_fn.context.should_skip():
+            raise unittest.SkipTest('Marked as skipped')
+
+        self.it_fn()
+
+    def __str__(self):
+        return '{0}: {1}'.format(str(self.it_fn.context), self.it_fn.__name__)
+
+
+
+
+if __name__ == "__main__":
+    log.setLevel(logging.DEBUG)
+    nose.main(addplugins=[DescribeItPlugin()])
