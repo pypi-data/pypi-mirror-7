0c4a63178c3788c8ee17cbf15d9ed8a24ab7d580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:28 +0000

    Add package python-simple-hipchat/0.3.2/python-simple-hipchat-0.3.2.tar.gz

---
+++ packages/python-simple-hipchat/python-simple-hipchat-0.3.2.tar.gz/python-simple-hipchat-0.3.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	description="Easy peasy wrapper for HipChat's v1 API",
+    name='python-simple-hipchat',
+    url='https://github.com/kurttheviking/python-simple-hipchat',
+    version='0.3.2',
+    packages=['hipchat'],
+    author='Kurt Ericson',
+    author_email='kurttheviking@outlook.com',
+    license='MIT',
+    long_description=open('README.rst').read()
+)
