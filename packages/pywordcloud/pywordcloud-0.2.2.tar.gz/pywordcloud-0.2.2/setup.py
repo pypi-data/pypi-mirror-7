e99fd513aaa4560b8b366b8b92f885e6298c61fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:41 +0000

    Add package pywordcloud/0.2.2/pywordcloud-0.2.2.tar.gz

---
+++ packages/pywordcloud/pywordcloud-0.2.2.tar.gz/pywordcloud-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="pywordcloud",
+    version="0.2.2",
+    author="Shivam Bansal",
+    author_email="shivam5992@gmail.com",
+    packages=["pywordcloud"],
+    include_package_data=True,
+    url="https://github.com/shivam5992/pywordcloud",
+    license='MIT',
+    description="Generate Word Clouds in Python.",
+    keywords = ['wordcloud','wordtagcloud','word','tagcloud']
+)
