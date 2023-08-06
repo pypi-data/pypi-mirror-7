0a93f20beae797854e4e188550592e0d49709e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:46 +0000

    Add package cold-start-recommender/0.3.2/cold-start-recommender-0.3.2.tar.gz

---
+++ packages/cold-start-recommender/cold-start-recommender-0.3.2.tar.gz/cold-start-recommender-0.3.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='cold-start-recommender',
+      description='Fast, Ready-to-be-used recommender',
+      author='Mario Alemi',
+      author_email='mario.alemi@gmail.com',
+      version='0.3.2',
+      py_modules=['csrec.Recommender'],
+      url='https://github.com/malemi/cold-start-recommender',
+      )
