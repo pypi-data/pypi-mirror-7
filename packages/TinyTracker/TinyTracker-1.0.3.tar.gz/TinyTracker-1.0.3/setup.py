f3fe88ebb98dcc5063251ba677dc0f8d29892309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:42 +0000

    Add package TinyTracker/1.0.3/TinyTracker-1.0.3.tar.gz

---
+++ packages/TinyTracker/TinyTracker-1.0.3.tar.gz/TinyTracker-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="TinyTracker",
+    version="1.0.3",
+    description="A small pixel tracker built in python, and designed to send data to carbon.",
+    author="Chris Sinchok",
+    author_email="csinchok@theonion.com",
+    packages=["tinytracker"],
+    install_requires=[
+        "gevent >= 1.0"
+    ],
+    url="https://github.com/theonion/tinytracker"
+)
