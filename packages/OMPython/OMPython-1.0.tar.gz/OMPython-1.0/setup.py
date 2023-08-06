6c5d60685187c17cec6e2a15141ad419375ed46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:18 +0000

    Add package OMPython/1.0/OMPython-1.0.tar.gz

---
+++ packages/OMPython/OMPython-1.0.tar.gz/OMPython-1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='OMPython',
+      version='1.0',
+      description='OpenModelica-Python API Interface',
+      author='Anand Kalaiarasi Ganeson',
+      author_email='ganan642@student.liu.se',
+      maintainer='Adeel Asghar',
+      maintainer_email='adeel.asghar@liu.se',
+      url='http://openmodelica.org/',
+      packages=['OMPython', 'OMPython.OMParser','OMPythonIDL', 'OMPythonIDL._OMCIDL', 'OMPythonIDL._OMCIDL__POA'],
+      install_requires=[
+        # 'omniORB', # Required, but not part of pypi
+        'pyparsing'
+      ]
+)
+
+
