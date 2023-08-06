c050da5962f4a83b61f35d7d0cb72aa0618b9313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:03 +0000

    Add package gnscli/0.7.0-alpha-8c942f8/gnscli-0.7.0-alpha-8c942f8.tar.gz

---
+++ packages/gnscli/gnscli-0.7.0-alpha-8c942f8.tar.gz/gnscli-0.7.0-alpha-8c942f8/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(name='gnscli',
+          version='0.7.0',
+          description='GNS Command line tool',
+          author='Alexander Kushnarev',
+          author_email='avkushnarev@gmail.com',
+          url='https://github.com/yandex-sysmon/gns-cli',
+          packages=['gnscli'],
+          package_data={'gnscli': ['config.yaml']},
+          entry_points={'console_scripts': ['gns-cli = gnscli.client:main']},
+          install_requires=['gns', 'raava', 'pyyaml', 'click', 'envoy-beta', 'requests', 'colorlog'])
