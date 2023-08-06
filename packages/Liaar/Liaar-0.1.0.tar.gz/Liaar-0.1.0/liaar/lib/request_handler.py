649e86d087184c6a6034f495f92ec5dd8511a600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package Liaar/0.1.0/Liaar-0.1.0.tar.gz

---
+++ packages/Liaar/Liaar-0.1.0.tar.gz/Liaar-0.1.0/liaar/lib/request_handler.py
@@ -0,0 +1,20 @@
+'''
+Twisted request handlers
+'''
+from liaar.lib import app, parser
+import json
+
+
+def method(request, route_params):
+    # set response content-type to json
+    request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+    method_setting = app.get_method(route_params['app_name'],
+                                    route_params['version'],
+                                    route_params['resource_name'],
+                                    route_params['method_name'])
+
+    output = {}
+    for param in method_setting:
+        output[param] = parser.parse_param(method_setting[param])
+
+    return json.dumps(output)
