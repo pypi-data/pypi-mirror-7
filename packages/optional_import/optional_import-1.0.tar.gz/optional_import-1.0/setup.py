7a176f7cbe8ab3898dc9d7afe1d0ef92aca8ba24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:56 +0000

    Add package optional_import/1.0/optional_import-1.0.tar.gz

---
+++ packages/optional_import/optional_import-1.0.tar.gz/optional_import-1.0/setup.py
@@ -0,0 +1,32 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+
+def long_description():
+    from os.path import join, dirname
+    import re
+    text = open(join(dirname(__file__), 'README.rst')).read()
+    return re.split('\n\.\. pypi [^\n]*\n', text, 1)[1]
+
+
+setup(
+    name='optional_import',
+    version='1.0',
+    py_modules=['optional_import'],
+    description='Import something if it exists.',
+    long_description=long_description(),
+    author='Chris Martin',
+    author_email='ch.martin@gmail.com',
+    url='https://github.com/cardforcoin/optional_import',
+    license='MIT',
+    test_suite='nose.collector',
+    tests_require=[
+        'nose==1.3.3',
+        'sure==1.2.7',
+        # workaround for https://github.com/gabrielfalcao/sure/pull/60
+        # undeclared requirements of `sure`
+        'mock',
+        'six',
+    ],
+)
