c4f71d536bf9c6084ee53b91858fe5effe26ace0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:20 +0000

    Add package Ferris/3.0.0-alpha1/Ferris-3.0.0-alpha1.tar.gz

---
+++ packages/Ferris/Ferris-3.0.0-alpha1.tar.gz/Ferris-3.0.0-alpha1/ferris3/messages.py
@@ -0,0 +1,27 @@
+import protopigeon
+
+
+def serialize(MessageType, entity):
+    return protopigeon.to_message(entity, MessageType)
+
+
+def deserialize(Model, message):
+    return protopigeon.to_entity(message, Model)
+
+
+def serialize_list(ListMessageType, entities):
+    from .ndb import PaginationResults
+
+    if isinstance(entities, PaginationResults):
+        next_page_token = entities.next_page_token
+        entities = entities.items
+    else:
+        next_page_token = None
+
+    MessageType = ListMessageType.items.message_type
+
+    message = ListMessageType()
+    message.items = [serialize(MessageType, x) for x in entities]
+    message.next_page_token = next_page_token
+
+    return message
