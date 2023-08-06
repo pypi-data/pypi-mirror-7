c4f88db7a19bca3d234250aef5003a56960a2e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:42 +0000

    Add package pygccxml/v1.6.0/pygccxml-v1.6.0.tar.gz

---
+++ packages/pygccxml/pygccxml-v1.6.0.tar.gz/pygccxml-v1.6.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Copyright 2014 Insight Software Consortium.
+# Copyright 2004-2008 Roman Yakovenko.
+# Distributed under the Boost Software License, Version 1.0.
+# See http://www.boost.org/LICENSE_1_0.txt
+
+from distutils.core import setup
+
+setup(name="pygccxml",
+      version="v1.6.0",
+      description="GCC-XML generated file reader",
+      author="Insight Software Consortium.",
+      author_email="gccxml@gccxml.org",
+      packages=['pygccxml',
+                'pygccxml.declarations',
+                'pygccxml.parser',
+                'pygccxml.binary_parsers',
+                'pygccxml.utils']
+      )
