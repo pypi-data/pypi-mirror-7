e10bf1405c56ac5bcee72adaba23ca562d07739b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:57 +0000

    Add package ontic/0.0.2/ontic-0.0.2.tar.gz

---
+++ packages/ontic/ontic-0.0.2.tar.gz/ontic-0.0.2/setup.py
@@ -0,0 +1,19 @@
+"""The setuptools setup file."""
+from setuptools import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    name='ontic',
+    version='0.0.2',
+    author='Raul Gonzalez',
+    author_email='mindbender@gmail.com',
+    url='https://github.com/neoinsanity/ontic',
+    license='Apache License 2.0',
+    description='Qualities and Quantities are the stuff of Objects.',
+    long_description=long_description,
+    packages=['ontic',],
+    install_requires=[],
+    include_package_data = True,
+)
