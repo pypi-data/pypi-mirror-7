73180d292ff2dd0a885c51b636c67c35ada0d40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:19 +0000

    Add package Desks/0.1/Desks-0.1.tar.gz

---
+++ packages/Desks/Desks-0.1.tar.gz/Desks-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'Desks'
+    , version='0.1'
+    , description='Desks to manage and drive Shelf, and provide API and data to the world'
+    , author='Okhin'
+    , author_email='okhin@okhin.fr'
+    , url='https://git.leloop.org/broadcast-system/desks'
+    , packages = ['rest']
+    , requires = ['flask_restful', 'filemagic', 'shelves', 'itsdangrous', 'flask']
+    )
