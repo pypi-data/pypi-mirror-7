1ab659829d5dfb21912ef156073b0dc6e787fa79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package shadowsocks/2.0.1/shadowsocks-2.0.1.tar.gz

---
+++ packages/shadowsocks/shadowsocks-2.0.1.tar.gz/shadowsocks-2.0.1/shadowsocks/local.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 clowwindy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import sys
+import os
+import logging
+import utils
+import encrypt
+import eventloop
+import tcprelay
+import udprelay
+
+
+def main():
+    utils.check_python()
+
+    # fix py2exe
+    if hasattr(sys, "frozen") and sys.frozen in \
+            ("windows_exe", "console_exe"):
+        p = os.path.dirname(os.path.abspath(sys.executable))
+        os.chdir(p)
+
+    config = utils.get_config(True)
+
+    utils.print_shadowsocks()
+
+    encrypt.init_table(config['password'], config['method'])
+
+    try:
+        logging.info("starting local at %s:%d" %
+                     (config['local_address'], config['local_port']))
+
+        tcp_server = tcprelay.TCPRelay(config, True)
+        udp_server = udprelay.UDPRelay(config, True)
+        loop = eventloop.EventLoop()
+        tcp_server.add_to_loop(loop)
+        udp_server.add_to_loop(loop)
+        loop.run()
+    except (KeyboardInterrupt, IOError, OSError) as e:
+        logging.error(e)
+        os._exit(0)
+
+if __name__ == '__main__':
+    main()
