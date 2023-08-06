f3f2678f71df5a3eca26fa76df7d406bf908ad24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package func1/1.0.0/func1-1.0.0.zip

---
+++ packages/func1/func1-1.0.0.zip/func1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='func1',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='JohnnyLiu',
+    author_email='www771519748@126.com',
+    description='the first python block',
+    )
