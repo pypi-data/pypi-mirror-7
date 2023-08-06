17730e518d17296fbc9787fe81276cc2d90a541e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:06 +0000

    Add package expecter/0.3.0/expecter-0.3.0.tar.gz

---
+++ packages/expecter/expecter-0.3.0.tar.gz/expecter-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='expecter',
+      version='0.3.0',
+      description='Expecter Gadget, a better expectation (assertion) library',
+      long_description=open('README.txt').read(),
+      author='Gary Bernhardt',
+      author_email='gary.bernhardt@gmail.com',
+      py_modules=['expecter'],
+      url='https://github.com/garybernhardt/expecter',
+      license='BSD',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Topic :: Software Development :: Testing',
+                   'Intended Audience :: Developers']
+     )
