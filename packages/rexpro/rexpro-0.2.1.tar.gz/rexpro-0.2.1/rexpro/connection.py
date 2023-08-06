205dc3b8397aa0c1dfb27e7ed73957e171f8a220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package rexpro/0.2.1/rexpro-0.2.1.tar.gz

---
+++ packages/rexpro/rexpro-0.2.1.tar.gz/rexpro-0.2.1/rexpro/connection.py
@@ -0,0 +1,3 @@
+from connectors.sync import RexProSyncSocket as RexProSocket
+from connectors.sync import RexProSyncConnection as RexProConnection
+from connectors.sync import RexProSyncConnectionPool as RexProConnectionPool
