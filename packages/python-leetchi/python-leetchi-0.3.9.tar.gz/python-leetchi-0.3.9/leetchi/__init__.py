243e8387093b670665429981025c071abbca78d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:48 +0000

    Add package python-leetchi/0.3.9/python-leetchi-0.3.9.tar.gz

---
+++ packages/python-leetchi/python-leetchi-0.3.9.tar.gz/python-leetchi-0.3.9/leetchi/__init__.py
@@ -0,0 +1,3 @@
+version = (0, 3, 9)
+
+__version__ = '.'.join(map(str, version))
---
+++ packages/python-leetchi/python-leetchi-0.3.9.tar.gz/python-leetchi-0.3.9/tests/credentials/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+API_PARTNER_ID = 'ulule'
+API_PRIVATE_KEY = os.path.join(os.path.dirname(__file__), 'ulule.pem')
+API_PRIVATE_KEY_PASSWORD = ''
+API_HOST = 'http://api-preprod.leetchi.com'
