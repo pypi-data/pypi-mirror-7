d3a59190bcf02c634b0728d9afbff04e1d5300f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:17 +0000

    Add package pytest-httpbin/0.0.2/pytest_httpbin-0.0.2-py2.py3-none-any.whl

---
+++ packages/pytest-httpbin/pytest_httpbin-0.0.2-py2.py3-none-any.whl/pytest_httpbin/plugin.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+import pytest
+from httpbin import app as httpbin_app
+from . import serve
+
+
+@pytest.fixture(scope='session')
+def httpbin(request):
+    server = serve.Server(application=httpbin_app)
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
+
+
+@pytest.fixture(scope='session')
+def httpbin_secure(request):
+    server = serve.SecureServer(application=httpbin_app)
+    server.start()
+    request.addfinalizer(server.stop)
+    return server
+
+
+@pytest.fixture(scope='session', params=['http', 'https'])
+def httpbin_both(request, httpbin, httpbin_secure):
+    if request.param == 'http':
+        return httpbin
+    elif request.param == 'https':
+        return httpbin_secure
