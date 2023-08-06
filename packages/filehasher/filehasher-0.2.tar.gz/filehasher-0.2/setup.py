f1df076a3b965577ecb0b37e97a62306c8757e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:56 +0000

    Add package filehasher/0.2/filehasher-0.2.tar.gz

---
+++ packages/filehasher/filehasher-0.2.tar.gz/filehasher-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='filehasher',
+      version='0.2',
+      description='File Hasher',
+      url='http://github.com/cyberplant/filehasher',
+      author='Luar Roji',
+      author_email='cyberplant@roji.net',
+      license='MIT',
+      entry_points={
+          'console_scripts': [
+              'filehasher = filehasher.cmdline:main',
+          ]},
+      packages=['filehasher'],
+      keywords=[
+          'filehasher', 'file', 'hasher', 'dupe'
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      zip_safe=True)
