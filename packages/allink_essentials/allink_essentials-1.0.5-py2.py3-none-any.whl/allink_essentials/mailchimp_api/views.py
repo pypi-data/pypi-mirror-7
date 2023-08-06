9c3ce6f103e4d88b73560437c0dd0350e2acbb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package allink_essentials/1.0.5/allink_essentials-1.0.5-py2.py3-none-any.whl

---
+++ packages/allink_essentials/allink_essentials-1.0.5-py2.py3-none-any.whl/allink_essentials/mailchimp_api/views.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.utils.module_loading import import_by_path
+from django.views.generic import FormView
+
+from allink_essentials.mailchimp_api.forms import SignupForm
+
+
+class SignupView(FormView):
+    form_class = SignupForm
+    template_name = 'mailchimp_api/signup_form.html'
+
+    def __init__(self, **kwargs):
+        super(SignupView, self).__init__(**kwargs)
+        if getattr(settings, 'MAILCHIMP_SIGNUP_FORM', False):
+            self.form_class = import_by_path(settings.MAILCHIMP_SIGNUP_FORM)
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponse('ok')
