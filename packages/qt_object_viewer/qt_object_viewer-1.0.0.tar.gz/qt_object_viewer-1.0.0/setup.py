0b3e8104d6103d8173732e3ac458eede5040e1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package qt_object_viewer/1.0.0/qt_object_viewer-1.0.0.tar.gz

---
+++ packages/qt_object_viewer/qt_object_viewer-1.0.0.tar.gz/qt_object_viewer-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+description = "qt tree model + tree viewer for nested objects"
+
+long_description = """
+This Python package provides a Qt based widget and a dialog class for exploring a
+nested object structure.
+
+For examples see: http://github.com/uweschmitt/qt_object_viewer.git
+"""
+
+setup(name="qt_object_viewer",
+      description=description,
+      long_description=long_description,
+      maintainer="Uwe Schmitt",
+      maintainer_email="uwe.schmitt@id.ethz.ch",
+      platforms=["any"],
+      version="1.0.0",
+      packages=find_packages(exclude=["tests"]),
+      include_package_data=True,
+      zip_safe=False,
+      )
