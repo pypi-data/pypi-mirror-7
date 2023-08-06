37d914fe8a72fa2341db136330bf63e8fb9c5a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:24 +0000

    Add package aiohttp/0.8.0/aiohttp-0.8.0.tar.gz

---
+++ packages/aiohttp/aiohttp-0.8.0.tar.gz/aiohttp-0.8.0/examples/curl.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import aiohttp
+import sys
+import asyncio
+
+
+def curl(url):
+    response = yield from aiohttp.request('GET', url)
+    print(repr(response))
+
+    while True:
+        try:
+            chunk = yield from response.content.read()
+            print('Downloaded: %s', len(chunk))
+        except aiohttp.EofStream:
+            break
+
+    response.close()
+
+
+if __name__ == '__main__':
+    if '--iocp' in sys.argv:
+        from asyncio import events, windows_events
+        sys.argv.remove('--iocp')
+        el = windows_events.ProactorEventLoop()
+        events.set_event_loop(el)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(curl(sys.argv[1]))
