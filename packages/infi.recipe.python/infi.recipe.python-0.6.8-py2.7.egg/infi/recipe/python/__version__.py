c53370516a98b1a3a72de783fdb9d98e38c38c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package infi.recipe.python/0.6.8/infi.recipe.python-0.6.8-py2.7.egg

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.8-py2.7.egg/infi/recipe/python/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.8"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.6.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '46c32e56bdca15955f2e11d0617f8afc994e3cf6'
+__git_head_subject__ = 'buildout.cfg: changed isolated python version to v2.7.6.6'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
