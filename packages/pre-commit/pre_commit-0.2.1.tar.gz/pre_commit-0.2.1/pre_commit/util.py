fcb5f5bfa597f3822be22f524b5efed3bcebbc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:41 +0000

    Add package pre-commit/0.2.1/pre_commit-0.2.1.tar.gz

---
+++ packages/pre-commit/pre_commit-0.2.1.tar.gz/pre_commit-0.2.1/pre_commit/util.py
@@ -0,0 +1,67 @@
+from __future__ import unicode_literals
+
+import contextlib
+import functools
+import hashlib
+import os
+import os.path
+import shutil
+import sys
+
+
+def memoize_by_cwd(func):
+    """Memoize a function call based on os.getcwd()."""
+    @functools.wraps(func)
+    def wrapper(*args):
+        cwd = os.getcwd()
+        key = (cwd,) + args
+        try:
+            return wrapper._cache[key]
+        except KeyError:
+            ret = wrapper._cache[key] = func(*args)
+            return ret
+
+    wrapper._cache = {}
+
+    return wrapper
+
+
+def entry(func):
+    """Allows a function that has `argv` as an argument to be used as a
+    commandline entry.  This will make the function callable using either
+    explicitly passed argv or defaulting to sys.argv[1:]
+    """
+    @functools.wraps(func)
+    def wrapper(argv=None):
+        if argv is None:
+            argv = sys.argv[1:]
+        return func(argv)
+    return wrapper
+
+
+@contextlib.contextmanager
+def clean_path_on_failure(path):
+    """Cleans up the directory on an exceptional failure."""
+    try:
+        yield
+    except BaseException:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        raise
+
+
+@contextlib.contextmanager
+def noop_context():
+    yield
+
+
+def shell_escape(arg):
+    return "'" + arg.replace("'", "'\"'\"'".strip()) + "'"
+
+
+def hex_md5(s):
+    """Hexdigest an md5 of the string.
+
+    :param text s:
+    """
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
