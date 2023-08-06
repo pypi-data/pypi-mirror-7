6bc0e7f89a0b9d8f430b0b9f8b48f194f178b055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:54 +0000

    Add package zhou_nester/1.0.0/zhou_nester-1.0.0.tar.gz

---
+++ packages/zhou_nester/zhou_nester-1.0.0.tar.gz/zhou_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+	
+setup(
+	name	= 'zhou_nester',
+	version = '1.0.0',
+	py_modules = ['zhou_nester'],
+	author     = 'zhou',
+	author_email     = 'zhoulovely.zy@gmail.com',
+	url   = 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nester listd',
+	)
