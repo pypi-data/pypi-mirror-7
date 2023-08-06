09f0b9073c7359c430e71465e59df8f6f55f9ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:48 +0000

    Add package NeedForCryptography/1.04/NeedForCryptography-1.04.tar.gz

---
+++ packages/NeedForCryptography/NeedForCryptography-1.04.tar.gz/NeedForCryptography-1.04/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+
+setup_info = {'name': 'NeedForCryptography',
+              'version': '1.04',
+              'description': 'Python cryptographic tools and ciphers',
+              'author': 'Veselin Genadiev',
+              'author_email': 'genadiev.veselin@gmail.com',
+              'url': 'https://github.com/Veselin-Genadiev/NeedForCryptography',
+              'packages': ['crypto', 'crypto.tools', 'crypto.symmetric',
+                           'crypto.asymmetric'],
+              'classifiers': [
+                  'Development Status :: 4 - Beta',
+                  'Environment :: Console',
+                  'Environment :: Web Environment',
+                  'Intended Audience :: End Users/Desktop',
+                  'Intended Audience :: Developers',
+                  'Intended Audience :: System Administrators',
+                  'License :: OSI Approved ::'
+                  ' Python Software Foundation License',
+                  'Operating System :: MacOS :: MacOS X',
+                  'Operating System :: POSIX',
+                  'Programming Language :: Python',
+                  'Topic :: Communications :: Email',
+                  'Topic :: Office/Business',
+                  'Topic :: Software Development :: Bug Tracking',
+              ],
+              }
+
+setup(**setup_info)
