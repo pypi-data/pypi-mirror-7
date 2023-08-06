f80ce22570da9abb490a3126c7de07707bfb584a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:52 +0000

    Add package sauce/1.1/sauce-1.1.tar.gz

---
+++ packages/sauce/sauce-1.1.tar.gz/sauce-1.1/setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sauce',
+    version='1.1',
+    description='SAUCE record parser',
+    author='Wijnand Modderman-Lenstra',
+    author_email='maze@pyth0n.org',
+    url='https://github.com/tehmaze/sauce',
+    packages=['sauce'],
+    )
