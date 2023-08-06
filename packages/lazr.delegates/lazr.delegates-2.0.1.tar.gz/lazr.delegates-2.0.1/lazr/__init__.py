53a95051b33883feecbba1f9ed131663e9c4d175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package lazr.delegates/2.0.1/lazr.delegates-2.0.1.tar.gz

---
+++ packages/lazr.delegates/lazr.delegates-2.0.1.tar.gz/lazr.delegates-2.0.1/lazr/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2008-2014 Canonical Ltd.  All rights reserved.
+#
+# This file is part of lazr.delegates.
+#
+# lazr.delegates is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# lazr.delegates is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lazr.delegates.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is a namespace package, however under >= Python 3.3, let it be a true
+# namespace package (i.e. this cruft isn't necessary).
+import sys
+
+if sys.hexversion < 0x30300f0:
+    try:
+        import pkg_resources
+        pkg_resources.declare_namespace(__name__)
+    except ImportError:
+        import pkgutil
+        __path__ = pkgutil.extend_path(__path__, __name__)
