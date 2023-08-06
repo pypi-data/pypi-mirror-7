95786d6f777c85bd04743076dfa96375e68478cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:43 +0000

    Add package filearchive/1.1.1/filearchive-1.1.1.tar.gz

---
+++ packages/filearchive/filearchive-1.1.1.tar.gz/filearchive-1.1.1/README.md
@@ -0,0 +1,17 @@
+filearchive
+===========
+
+Python module to work with file archives
+
+Example
+-------
+
+```
+>>> import filearchive
+>>> filearchive.unpack_archive("/Users/srid/Downloads/data.tgz")
+...
+>>> srcdir = "/home/me/project"
+>>> filearchive.pack_archive(
+       "/tmp/data.zip", [srcdir], os.path.dirname(srcdir), "zip")
+...
+```
---
+++ packages/filearchive/filearchive-1.1.1.tar.gz/filearchive-1.1.1/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    inf = open(os.path.join(os.path.dirname(__file__), fname))
+    out = "\n" + inf.read().replace("\r\n", "\n")
+    inf.close()
+    return out
+
+
+setup(
+    name='filearchive',
+    version="1.1.1",
+    description='Python module to work with file archives',
+    long_description=read('README.md'),
+    author='Sridhar Ratnakumar',
+    author_email='github@srid.name',
+    url='http://github.com/ActiveState/filearchive',
+    license='MIT',
+    py_modules=["filearchive"],
+)
