9442f9d4cb55675084d50f5ff7bec343a2f938c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:14 +0000

    Add package confire/0.2.0/confire-0.2.0.tar.gz

---
+++ packages/confire/confire-0.2.0.tar.gz/confire-0.2.0/confire/__init__.py
@@ -0,0 +1,37 @@
+# confire
+# A simple app configuration scheme using YAML and class based defaults.
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Sun Jul 20 09:44:32 2014 -0400
+#
+# Copyright (C) 2014 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+A simple app configuration scheme using YAML and class based defaults.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+from .config import Configuration, environ_setting
+from .exceptions import ImproperlyConfigured
+
+##########################################################################
+## Package constants
+##########################################################################
+
+__version__ = (0, 2, 0)
+
+##########################################################################
+## Package helpers
+##########################################################################
+
+def get_version():
+    """
+    Returns a string containing the version number
+    """
+    return ".".join("%i" % idx for idx in __version__)
