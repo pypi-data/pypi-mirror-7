8eeb855f6fe72c06c62de1a1b82e3bae5388fa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package django-app-flag/0.9.1/django-app-flag-0.9.1.tar.gz

---
+++ packages/django-app-flag/django-app-flag-0.9.1.tar.gz/django-app-flag-0.9.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name = 'django-app-flag',
+      version = '0.9.1',
+      author = 'Joe Groseclose',
+      author_email = 'joe.groseclose@example.com',
+      packages = ['django_app_flag'],
+      url='http://pypi.python.org/pypi/django_app_flag/',
+      license='LICENSE.txt',
+      description='Simple flag for your Django webapps',
+      long_description=open('README.txt').read(),
+      install_requires=["Django >= 1.6.5"],
+      )
