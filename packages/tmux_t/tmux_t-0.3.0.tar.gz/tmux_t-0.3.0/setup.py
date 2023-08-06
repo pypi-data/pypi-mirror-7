87c7ef3a5daf7eaa35d962a465bc9cc973e45343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:01 +0000

    Add package tmux_t/0.3.0/tmux_t-0.3.0.tar.gz

---
+++ packages/tmux_t/tmux_t-0.3.0.tar.gz/tmux_t-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="tmux_t",
+      version="0.3.0",
+      description="t, The simple tmux helper.",
+      url="https://github.com/giodamelio/t",
+      author="Gio d'Amelio",
+      author_email="giodamelio@gmail.com",
+      scripts=["t"],
+      install_requires=[
+          "tmuxp",
+          "docopt"
+      ],
+      license="MIT",
+      zip_safe=False)
