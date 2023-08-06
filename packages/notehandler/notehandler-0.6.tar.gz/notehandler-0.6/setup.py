5b8ea10a63b29c9e59f826a75afbd607ed801f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:47 +0000

    Add package notehandler/0.6/notehandler-0.6.tar.gz

---
+++ packages/notehandler/notehandler-0.6.tar.gz/notehandler-0.6/setup.py
@@ -0,0 +1,36 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+
+classifiers = [ 'Development Status :: 4 - Beta'
+              , 'Environment :: Console'
+              , 'Intended Audience :: Developers'
+              , 'Intended Audience :: End Users/Desktop'
+              , 'Intended Audience :: System Administrators'
+              , 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)'
+              , 'Natural Language :: English'
+              , 'Operating System :: MacOS :: MacOS X'
+              , 'Operating System :: Microsoft :: Windows'
+              , 'Operating System :: POSIX'
+              , 'Programming Language :: Python :: 2.6'
+              , 'Programming Language :: Python :: 2.7'
+              , 'Programming Language :: Python :: Implementation :: CPython'
+              , 'Topic :: Communications'
+              , 'Topic :: Text Processing'
+               ]
+
+setup( author = 'Paul Jimenez'
+     , author_email = 'pj@place.org'
+     , classifiers = classifiers
+     , description = 'NoteHandler is a Commandline Interface for Evernote'
+     , entry_points = {'console_scripts': [ 'nh = notehandler:main' ]}
+     , name = 'notehandler'
+     , py_modules = [ 'distribute_setup', 'notehandler' ]
+     , url = 'http://github.com/pjz/notehandler'
+     # there must be nothing on the following line after the = other than a string constant
+     , version = '0.6'
+     , zip_safe = False
+     , install_requires = [ 'evernote', 'cmdpy' ]
+      )
