b7d7e653f096e6168b5b5dd2555cfd0032d7ea3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:37 +0000

    Add package pythonpy/0.3.1dev3/pythonpy-0.3.1dev3.tar.gz

---
+++ packages/pythonpy/pythonpy-0.3.1dev3.tar.gz/pythonpy-0.3.1dev3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import os
+
+if os.geteuid() == 0:
+     data_files = [('/etc/bash_completion.d', ['extras/pycompletion.sh']),]
+else:
+     print('User does not have write access to /etc completion will not work')
+     data_files = [('bash_completion.d', ['extras/pycompletion.sh']),]
+
+setup(
+    name='pythonpy',
+    version='0.3.1dev3',
+    description='Take advantage of your python skills from the command line',
+    py_modules=['completion'],
+    scripts=['py', 'extras/py3', 'extras/pycompleter'],
+    data_files=data_files,
+    license='MIT',
+    url='https://github.com/Russell91/pythonpy',
+    long_description='',
+)
