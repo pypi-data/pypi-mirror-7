6617286cae0883599ede47554c57fdad7db6bdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:55 +0000

    Add package px/0.0.17/px-0.0.17.tar.gz

---
+++ packages/px/px-0.0.17.tar.gz/px-0.0.17/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='px',
+	version='0.0.17',
+	author='6px',
+	author_email='ops@6px.io',
+	packages=['_6px'],
+    url='https://github.com/6px-io/6px-python',
+	license='LICENSE',
+	description='6px python sdk',
+	long_description=open('README.rst').read(),
+)
