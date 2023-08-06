c81d474183bf6fdf64f6a07e5a2fe451b2917f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:36 +0000

    Add package tempman/0.1.3/tempman-0.1.3.tar.gz

---
+++ packages/tempman/tempman-0.1.3.tar.gz/tempman-0.1.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='tempman',
+    version='0.1.3',
+    description='Create and clean up temporary directories',
+    long_description=read("README"),
+    author='Michael Williamson',
+    author_email='mike@zwobble.org',
+    url='http://github.com/mwilliamson/python-tempman',
+    packages=['tempman'],
+    keywords="temporary temp directory directories cleanup clean",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+    ],
+)
+
