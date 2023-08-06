e51b12e5f6945f25d0c65677177876f2b84e94b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:13 +0000

    Add package PsyLab/0.4.2/PsyLab-0.4.2.tar.gz

---
+++ packages/PsyLab/PsyLab-0.4.2.tar.gz/PsyLab-0.4.2/psylab/io/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 Christopher Brown
+#
+# This file is part of Psylab.
+#
+# Psylab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Psylab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Psylab.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Bug reports, bug fixes, suggestions, enhancements, or other 
+# contributions are welcome. Go to http://code.google.com/p/psylab/ 
+# for more information and to contribute. Or send an e-mail to: 
+# cbrown1@pitt.edu.
+#
+
+'''
+io - File i/o specific functions that depend on medussa
+
+Modules include:
+
+fileio - Helper functions for reading audio data in less typical ways
+listPlayer - Standalone script to play blocks of soundfiles in folders
+
+Notes
+-----
+Tested on windows and linux.
+
+Depends on medussa (http://medussa.googlecode.com)
+'''
+
+from .file import read_multi_file, read_audio_file
+import listPlayer
+
