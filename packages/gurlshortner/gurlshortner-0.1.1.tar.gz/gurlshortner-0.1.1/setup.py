a4d0ac5ed085c1de187ce4c05daf13776bc6b209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:08 +0000

    Add package gurlshortner/0.1.1/gurlshortner-0.1.1.tar.gz

---
+++ packages/gurlshortner/gurlshortner-0.1.1.tar.gz/gurlshortner-0.1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gurlshortner',
+      version='0.1.1',
+      description='URL shortner using Google APIs',
+      author='Aditya Patawari',
+      author_email='aditya@adityapatawari.com',
+      url='https://github.com/adimania/gurlshortner',
+      py_modules=['gurlshortner'],
+     )
