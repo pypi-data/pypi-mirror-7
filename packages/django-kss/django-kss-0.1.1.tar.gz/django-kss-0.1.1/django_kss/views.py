d87271d8f64444cbdaebdfacc1cbe599409aee01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:50 +0000

    Add package django-kss/0.1.1/django-kss-0.1.1.tar.gz

---
+++ packages/django-kss/django-kss-0.1.1.tar.gz/django-kss-0.1.1/django_kss/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.conf import settings
+from pykss.contrib.django.views import StyleguideView
+import itertools
+
+
+class AutoStyleguideView(StyleguideView):
+
+    def get_context_data(self, **kwargs):
+
+        context = super(AutoStyleguideView, self).get_context_data(**kwargs)
+        styleguide = context["styleguide"]
+
+        def section_main_key(tp):
+            key = tp[0]
+            return key.split(".")[0]
+
+        sections = list(
+            sorted(
+                set(
+                    map(
+                        lambda key: key.split(".")[0],
+                        styleguide.sections
+                    )
+                )
+            )
+        )
+
+        if not self.kwargs['section']:
+            current_section = sections[0]
+        else:
+            current_section = self.kwargs['section']
+
+        section_descriptions = {}
+        for section in sections:
+            if section in styleguide.sections:
+                section_descriptions[section] = \
+                    styleguide.sections[section].description
+            else:
+                section_descriptions[section] = section
+
+        context.update({'current_section': current_section})
+        context.update({'section_descriptions': section_descriptions})
+        context.update({'PYKSS_STATIC_FILES':
+                        getattr(settings, "PYKSS_STATIC_FILES", [])
+                        })
+        return context
