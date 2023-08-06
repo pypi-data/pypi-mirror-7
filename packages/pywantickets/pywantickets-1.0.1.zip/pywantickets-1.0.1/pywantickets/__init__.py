dd5e60f3f979d97ab3c9bc8ddef973c65a217946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:53 +0000

    Add package pywantickets/1.0.1/pywantickets-1.0.1.zip

---
+++ packages/pywantickets/pywantickets-1.0.1.zip/pywantickets-1.0.1/pywantickets/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Wantickets.com API integration
+"""
+
+__title__ = 'pywantickets'
+__version__ = '1.0.1'
+__build__ = 0x02
+__author__ = 'Gamaliel Espinoza'
+#__license__ = 'Apache 2.0'
+#__copyright__ = 'Copyright 2014 Kenneth Reitz'
+
+class WanticketsModel(object):
+	def get_api_model(self):
+		return {}
