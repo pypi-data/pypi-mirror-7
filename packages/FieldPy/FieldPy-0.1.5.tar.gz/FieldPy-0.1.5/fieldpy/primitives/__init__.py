4ff4fd4ca20a47a7ae24fe5224b125dc555de88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:45 +0000

    Add package FieldPy/0.1.5/FieldPy-0.1.5.tar.gz

---
+++ packages/FieldPy/FieldPy-0.1.5.tar.gz/FieldPy-0.1.5/fieldpy/primitives/__init__.py
@@ -0,0 +1,5 @@
+import fieldpy.primitives.point
+import fieldpy.primitives.axis
+import fieldpy.primitives.idx
+import fieldpy.primitives.grid
+import fieldpy.primitives.ioMHD
---
+++ packages/FieldPy/FieldPy-0.1.5.tar.gz/FieldPy-0.1.5/fieldpy/test/test_all.py
@@ -0,0 +1,18 @@
+import nose
+import os
+
+def runTests():
+    r"""Run all tests"""
+    # Get directory of current file
+    cfd = os.path.dirname(os.path.realpath(__file__))
+    # Get current working directory
+    cwd = os.getcwd()
+    # Change current working directory to 'cfd'
+    os.chdir(cfd)
+    # Run all tests
+    nose.run(argv = [cfd,"--verbosity=2"])
+    # Change current working directory to its previous state
+    os.chdir(cwd)
+
+if __name__ == "__main__":
+    runTests()
