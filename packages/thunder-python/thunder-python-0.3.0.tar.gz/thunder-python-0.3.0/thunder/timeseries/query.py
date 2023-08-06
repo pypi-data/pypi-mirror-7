3e1154eb05294f9982b333053260f78c5bc33108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:53 +0000

    Add package thunder-python/0.3.0/thunder-python-0.3.0.tar.gz

---
+++ packages/thunder-python/thunder-python-0.3.0.tar.gz/thunder-python-0.3.0/thunder/timeseries/query.py
@@ -0,0 +1,96 @@
+"""
+Class and standalone app for querying
+"""
+
+import argparse
+from numpy import zeros
+from scipy.io import loadmat
+from thunder.utils import ThunderContext, getdims, subtoind
+from thunder.utils import save
+
+
+class Query(object):
+    """Class for querying time series data by averaging together
+    data points with the specified indices
+
+    Parameters
+    ----------
+    indsfile : str, or array-like (2D)
+        Array of indices, each an array-like of integer indices, or
+        filename of a MAT file containing a set of indices as a cell array
+        stored in the variable inds
+    """
+
+    def __init__(self, indsfile):
+        if type(indsfile) is str:
+            inds = loadmat(indsfile)['inds'][0]
+        else:
+            inds = indsfile
+        self.inds = inds
+        self.n = len(inds)
+
+    def select(self, data, i):
+        """Subselect rows of data with a given set of integer indices
+
+        Parameters
+        ----------
+        data : RDD of (int, array) pairs, each array of shape (ncols,)
+            Data to subselect rows from
+
+        i : int
+            Which set of indices to use
+
+        Returns
+        -------
+        subset : RDD of (int, array) pairs
+            Data with subset of rows
+        """
+
+        inds_set = set(self.inds[i].flat)
+        inds_bc = data.context.broadcast(inds_set)
+        subset = data.filter(lambda (k, _): k in inds_bc.value)
+        return subset
+
+    def calc(self, data):
+        """Calculate averages. Keys (tuples) are converted
+        into linear indices based on their dimensions
+
+        Parameters
+        ----------
+        data : RDD of (tuple, array) pairs, each array of shape (ncols,)
+            Data to compute averages from
+
+        Returns
+        -------
+        ts : array, shape (n, ncols)
+        """
+
+        dims = getdims(data)
+        data = subtoind(data, dims.max)
+
+        # loop over indices, averaging time series
+        ts = zeros((self.n, len(data.first()[1])))
+        for i in range(0, self.n):
+            if len(self.inds[i]) > 0:
+                ts[i, :] = self.select(data, i).map(lambda (k, x): x).sum() / len(self.inds[i])
+
+        return ts
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="query time series data by averaging values for given indices")
+    parser.add_argument("datafile", type=str)
+    parser.add_argument("indsfile", type=str)
+    parser.add_argument("outputdir", type=str)
+    parser.add_argument("--preprocess", choices=("raw", "dff", "sub", "dff-highpass", "dff-percentile"
+                        "dff-detrendnonlin", "dff-detrend-percentile"), default="raw", required=False)
+
+    args = parser.parse_args()
+
+    tsc = ThunderContext.start(appName="query")
+
+    data = tsc.loadText(args.datafile, args.preprocess).cache()
+    ts = Query(args.indsfile).calc(data)
+
+    outputdir = args.outputdir + "-query"
+    save(ts, outputdir, "ts", "matlab")
---
+++ packages/thunder-python/thunder-python-0.3.0.tar.gz/thunder-python-0.3.0/thunder/utils/load.py
@@ -0,0 +1,205 @@
+"""
+Utilities for loading and preprocessing data
+"""
+
+import pyspark
+from numpy import array, mean, cumprod, append, mod, ceil, size, \
+    polyfit, polyval, arange, percentile, inf, subtract
+from scipy.signal import butter, lfilter
+
+
+class Dimensions(object):
+    """Helper class for estimating and storing dimensions of data
+    based on the keys"""
+
+    def __init__(self, values=[], n=3):
+        self.min = tuple(map(lambda i: inf, range(0, n)))
+        self.max = tuple(map(lambda i: -inf, range(0, n)))
+
+        for v in values:
+            self.merge(v)
+
+    def merge(self, value):
+        self.min = tuple(map(min, self.min, value))
+        self.max = tuple(map(max, self.max, value))
+        return self
+
+    def count(self):
+        return tuple(map(lambda x: x + 1, map(subtract, self.max, self.min)))
+
+    def mergedims(self, other):
+        self.min = tuple(map(min, self.min, other.min))
+        self.max = tuple(map(max, self.max, other.max))
+        return self
+
+
+class Parser(object):
+    """Class for parsing lines of a data file"""
+
+    def __init__(self, nkeys):
+        def func(line):
+            vec = [float(x) for x in line.split(' ')]
+            ts = array(vec[nkeys:])
+            keys = tuple(int(x) for x in vec[:nkeys])
+            return keys, ts
+
+        self.func = func
+
+    def get(self, y):
+        return self.func(y)
+
+
+class PreProcessor(object):
+    """Class for preprocessing data"""
+     # TODO Refactor to make it easier to combine options
+
+    def __init__(self, preprocessmethod):
+        if preprocessmethod == "sub":
+            func = lambda y: y - mean(y)
+
+        if preprocessmethod == "dff":
+            def func(y):
+                mnval = mean(y)
+                return (y - mnval) / (mnval + 0.1)
+
+        if preprocessmethod == "raw":
+            func = lambda x: x
+
+        if preprocessmethod == "dff-percentile":
+
+            def func(y):
+                mnval = percentile(y, 20)
+                y = (y - mnval) / (mnval + 0.1)   
+                return y
+
+        if preprocessmethod == "dff-detrend":
+
+            def func(y):
+                mnval = mean(y)
+                y = (y - mnval) / (mnval + 0.1)   
+                x = arange(1, len(y)+1) 
+                p = polyfit(x, y, 1)
+                yy = polyval(p, x)
+                return y - yy
+
+        if preprocessmethod == "dff-detrendnonlin":
+
+            def func(y):
+                mnval = mean(y)
+                y = (y - mnval) / (mnval + 0.1)   
+                x = arange(1, len(y)+1) 
+                p = polyfit(x, y, 5)
+                yy = polyval(p, x)
+                return y - yy
+
+        if preprocessmethod == "dff-detrendnonlin-percentile":
+
+            def func(y):
+                # first do nonlinear detrending (but don't subtract the intercept)
+                x = arange(1, len(y)+1)
+                p = polyfit(x, y, 5)
+                p[-1] = 0  # do not subtract intercept
+                yy = polyval(p, x)
+                y = y - yy
+
+                # then take 20th percentile as baseline
+                mnval = percentile(y, 20)
+                y = (y - mnval) / (mnval + 0.1)
+                return y
+
+        if preprocessmethod == "dff-detrend-percentile":
+
+            def func(y):
+                # first do linear detrending (but don't subtract the intercept)
+                x = arange(1, len(y)+1)
+                p = polyfit(x, y, 1)
+                yy = p[0] * x  # subtract off just the slope term
+                y = y - yy
+
+                # then take 20th percentile as baseline
+                mnval = percentile(y, 20)
+                y = (y - mnval) / (mnval + 0.1)
+                return y
+
+        if preprocessmethod == "dff-highpass":
+            fs = 1
+            nyq = 0.5 * fs
+            cutoff = (1.0/360) / nyq
+            b, a = butter(6, cutoff, "highpass")
+
+            def func(y):
+                mnval = mean(y)
+                y = (y - mnval) / (mnval + 0.1)
+                return lfilter(b, a, y)
+
+        self.func = func
+
+    def get(self, y):
+        return self.func(y)
+
+
+def isrdd(data):
+    """ Check whether data is an RDD or not"""
+
+    dtype = type(data)
+    if (dtype == pyspark.rdd.RDD) | (dtype == pyspark.rdd.PipelinedRDD):
+        return True
+    else:
+        return False
+
+
+def getdims(data):
+    """Get dimensions of data via the keys. Ranges can have arbtirary minima
+    and maximum, but they must be contiguous (e.g. the indices of a dense matrix)."""
+
+    def redfunc(left, right):
+        return left.mergedims(right)
+
+    if isrdd(data):
+        entry = data.first()[0]
+        n = size(entry)
+        d = data.map(lambda (k, _): k).mapPartitions(lambda i: [Dimensions(i, n)]).reduce(redfunc)
+    else:
+        entry = data[0][0]
+        rng = range(0, size(entry))
+        d = Dimensions()
+        if size(entry) == 1:
+            distinctvals = list(set(map(lambda x: x[0][0], data)))
+        else:
+            distinctvals = map(lambda i: list(set(map(lambda x: x[0][i], data))), rng)
+        d.max = tuple(map(max, distinctvals))
+        d.min = tuple(map(min, distinctvals))
+
+    return d
+
+
+def subtoind(data, dims):
+    """Convert subscript indexing to linear indexing"""
+
+    def subtoind_inline(k, dimprod):
+        return sum(map(lambda (x, y): (x - 1) * y, zip(k[1:], dimprod))) + k[0]
+    if size(dims) > 1:
+        dimprod = cumprod(dims)[0:-1]
+        if isrdd(data):
+            return data.map(lambda (k, v): (subtoind_inline(k, dimprod), v))
+        else:
+            return map(lambda (k, v): (subtoind_inline(k, dimprod), v), data)
+    else:
+        return data.map(lambda (k, v): (k[0], v))
+
+
+def indtosub(data, dims):
+    """Convert linear indexing to subscript indexing"""
+
+    def indtosub_inline(k, dimprod):
+        return tuple(map(lambda (x, y): int(mod(ceil(float(k)/y) - 1, x) + 1), dimprod))
+
+    if size(dims) > 1:
+        dimprod = zip(dims, append(1, cumprod(dims)[0:-1]))
+        if isrdd(data):
+            return data.map(lambda (k, v): (indtosub_inline(k, dimprod), v))
+        else:
+            return map(lambda (k, v): (indtosub_inline(k, dimprod), v), data)
+
+    else:
+        return data
