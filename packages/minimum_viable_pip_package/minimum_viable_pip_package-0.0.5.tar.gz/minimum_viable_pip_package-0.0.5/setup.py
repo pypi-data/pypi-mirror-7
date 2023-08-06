e7963f2b534300709f19fafe6b49553cbdbbe019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:54 +0000

    Add package minimum_viable_pip_package/0.0.5/minimum_viable_pip_package-0.0.5.tar.gz

---
+++ packages/minimum_viable_pip_package/minimum_viable_pip_package-0.0.5.tar.gz/minimum_viable_pip_package-0.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+package = 'minimum_viable_pip_package'
+
+setup(
+    name=package,
+    author='Brian M. Carlson',
+    author_email='brian.m.carlson@gmail.com',
+    version='0.0.5',
+    url='https://github.com/brianc/mvp.py',
+    packages=[package]
+)
