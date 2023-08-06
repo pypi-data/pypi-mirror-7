6f3befac744d698d6a18b424eea73c75805bdb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package pysed/0.3.0/pysed-0.3.0.tar.gz

---
+++ packages/pysed/pysed-0.3.0.tar.gz/pysed-0.3.0/pysed/paint.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def colors(color):
+    '''Print colors'''
+
+    paint = {
+        'black'      : '\x1b[30m',
+        'red'        : '\x1b[31m',
+        'green'      : '\x1b[32m',
+        'yellow'     : '\x1b[33m',
+        'blue'       : '\x1b[34m',
+        'magenta'    : '\x1b[35m',
+        'cyan'       : '\x1b[36m',
+        'default'    : '\x1b[0m'
+        }
+
+    return paint[color]
