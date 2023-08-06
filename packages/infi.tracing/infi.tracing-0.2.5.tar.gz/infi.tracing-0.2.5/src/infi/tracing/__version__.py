58b14cbfe5781c36b4be3d270a9a586844a7640e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:40 +0000

    Add package infi.tracing/0.2.5/infi.tracing-0.2.5.tar.gz

---
+++ packages/infi.tracing/infi.tracing-0.2.5.tar.gz/infi.tracing-0.2.5/src/infi/tracing/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.5"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "yalon@infinidat.com"
+__git_branch__ = '(detached from v0.2.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b8dcdc8cef115f50d72f514446f877aca51ab8d6'
+__git_head_subject__ = 'fixed timestamp issue and rsyslog not handling comma as the second fraction separator well'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
