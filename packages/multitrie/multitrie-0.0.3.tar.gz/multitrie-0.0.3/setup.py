51c7cd291e9fd82ce35b1c802127a8c73d7b5829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:21 +0000

    Add package multitrie/0.0.3/multitrie-0.0.3.tar.gz

---
+++ packages/multitrie/multitrie-0.0.3.tar.gz/multitrie-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+try:
+  from setuptools import setup, find_packages
+except ImportError:
+  from distutils.core import setup
+
+version = '0.0.3'
+
+setup(
+  name='multitrie',
+  version=version,
+  download_url='https://github.com/Pixelapse/multitrie/tarball/v%s' % version,
+  url='http://github.com/Pixelapse/multitrie',
+  author='Michael Wu',
+  author_email='michael@pixelapse.com',
+  maintainer='Pixelapse',
+  maintainer_email='hello@pixelapse.com',
+  packages=find_packages(),
+  description='Implementation of the trie data structure',
+  long_description=open('README.md').read(),
+  license=open('LICENSE').read()
+)
