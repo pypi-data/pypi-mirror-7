ef64a5dc1293a6a292d16991f2442ebdbecd16c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:40 +0000

    Add package dnsq/1.1.3/dnsq-1.1.3.tar.gz

---
+++ packages/dnsq/dnsq-1.1.3.tar.gz/dnsq-1.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='dnsq',
+      version='1.1.3',
+      description='DNS Query Tool',
+      long_description=open("README.rst").read(),
+      author='Rackspace',
+      author_email='admin@mailgunhq.com',
+      license='Apache 2',
+      url='http://www.mailgun.com',
+      py_modules=['dnsq'],
+      zip_safe=True,
+      install_requires=[
+        'dnspython==1.11.1',
+        'expiringdict>=1.1',
+      ],
+      )
