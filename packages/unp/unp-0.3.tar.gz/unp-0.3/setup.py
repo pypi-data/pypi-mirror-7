739514c2355c06a00bb1b84b2c1f29d4a17f1d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:05 +0000

    Add package unp/0.3/unp-0.3.tar.gz

---
+++ packages/unp/unp-0.3.tar.gz/unp-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='unp',
+    license='BSD',
+    version='0.3',
+    url='http://github.com/mitsuhiko/unp',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    description='Command line tool that can unpack archives easily',
+    py_modules=['unp'],
+    install_requires=[
+        'click>=3.0',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    entry_points='''
+        [console_scripts]
+        unp=unp:cli
+    ''',
+)
