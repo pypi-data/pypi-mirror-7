a47e132e7ff49d0e98cb9305782c9b0ab80c41ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package infi.projector/0.6.24/infi.projector-0.6.24-py2.7.egg

---
+++ packages/infi.projector/infi.projector-0.6.24-py2.7.egg/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.24"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.6.24)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '27ec26f84e59d1e54e1c1ca6ed87a9470740be72'
+__git_head_subject__ = 'TRIVIAL fixing whitespaces and newlines when saving multi-line attributes in buildout.cfg'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
