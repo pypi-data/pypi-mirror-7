4c21ddce603f2bc1f1f18e6ddcb19683cffb8fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:04 +0000

    Add package 02exercicio/1.0.0/02exercicio-1.0.0.tar.gz

---
+++ packages/02exercicio/02exercicio-1.0.0.tar.gz/02exercicio-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= '02exercicio',
+	version		= '1.0.0',
+	py_modules	= ['02'],
+	author		= 'fernando',
+	author_email	= 'fernando.bzx@gamail.com',
+	url		= '***********************',
+	description	= 'Um simples programa dee teste da função def e loop for',
+)
