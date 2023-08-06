e792cf66121eda3e2811b380b7b59336e74537af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:59 +0000

    Add package worldcup/1.0/worldcup-1.0.tar.gz

---
+++ packages/worldcup/worldcup-1.0.tar.gz/worldcup-1.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+setup(
+    name='worldcup',
+    version='1.0',
+    py_modules=['worldcup'],
+    entry_points={
+        'console_scripts': [
+            'worldcup = worldcup:main',
+        ],
+    },
+    install_requires=[
+        'python-dateutil==1.5',
+        'colorama==0.3.1',
+        'pytz==2014.4',
+        'humanize==0.5'
+    ],
+    classifiers=[
+      'Environment :: Console'
+    ],
+)
