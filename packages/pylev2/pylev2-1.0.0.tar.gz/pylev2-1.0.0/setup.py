36a734e8367bd67a88132485922fde6701fc7f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:49 +0000

    Add package pylev2/1.0.0/pylev2-1.0.0.tar.gz

---
+++ packages/pylev2/pylev2-1.0.0.tar.gz/pylev2-1.0.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+module1 = Extension('_pylev2', sources = ['_pylev2.c'])
+
+setup(name='pylev2',
+      version='1.0.0',
+      description='A Levenshtein implementation with a premissive license.',
+      author='Anders Jensen',
+      license='MIT',
+      author_email='johndoee+pylev2@tidalstream.org',
+      url='https://github.com/JohnDoee/pylev2/',
+      packages=['pylev2'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    ext_modules = [module1]
+)
+
