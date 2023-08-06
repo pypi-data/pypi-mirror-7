771d56946551792d16cbe58e5a7ed4c07d3112aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:05 +0000

    Add package PYTRip/1.0/PYTRip-1.0.tar.gz

---
+++ packages/PYTRip/PYTRip-1.0.tar.gz/PYTRip-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='PYTRip',
+      version='1.0',
+      description='A collection of python functions to aid in ripping songs from YouTube',
+      url='https://github.com/Rhysjc/PYTRip',
+      author='Rhys Camm',
+      author_email='rhysjc@gmail.com',
+      py_modules=['PYTRip'],
+      download_url='https://github.com/Rhysjc/PYTRip/tarball/0.1'
+      )
