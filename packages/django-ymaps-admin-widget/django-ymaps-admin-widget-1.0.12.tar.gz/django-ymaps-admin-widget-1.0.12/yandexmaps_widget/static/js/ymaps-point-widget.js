a16cc165b41395cdbb42210b5edc7f47cb94a682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:06 +0000

    Add package django-ymaps-admin-widget/1.0.12/django-ymaps-admin-widget-1.0.12.tar.gz

---
+++ packages/django-ymaps-admin-widget/django-ymaps-admin-widget-1.0.12.tar.gz/django-ymaps-admin-widget-1.0.12/yandexmaps_widget/static/js/ymaps-point-widget.js
@@ -0,0 +1,67 @@
+(function() {
+  $(document).ready(function() {
+    var placemark;
+    placemark = null;
+    return ymaps.ready(function() {
+      var $input, NewPlacemark, center, convertPostGisToYandex, inputId, latitude, longitude, map, toPostgisPoint, zoom, _ref, _ref1, _ref2, _ref3;
+      inputId = (_ref = window.inputId) != null ? _ref : 'id_coords';
+      zoom = (_ref1 = window.zoom) != null ? _ref1 : 10;
+      latitude = (_ref2 = window.latitude) != null ? _ref2 : '37.64';
+      latitude = parseFloat(window.latitude.replace(',', '.'));
+      longitude = (_ref3 = window.longitude) != null ? _ref3 : '37.64';
+      longitude = parseFloat(window.longitude.replace(',', '.'));
+      console.debug('Init params', latitude, longitude, zoom, inputId);
+      toPostgisPoint = function(geo_point) {
+        return ['POINT(', geo_point[1].toString(), geo_point[0].toString(), ')'].join(' ');
+      };
+      convertPostGisToYandex = function(point) {
+        var coords;
+        coords = /POINT\s*\(\s*([0-9\.]+)\s*([0-9.]+)\s*\)/.exec(point);
+        if (coords.length !== 3) {
+          console.error('Error at parse GIS point', point);
+          return [];
+        }
+        return [parseFloat(coords[2]), parseFloat(coords[1])];
+      };
+      $input = $('#' + inputId);
+      NewPlacemark = function(point) {
+        var newPoint;
+        newPoint = new ymaps.Placemark(point, {}, {
+          draggable: false,
+          hasBalloon: false
+        });
+        newPoint.events.add('dragend', function(e) {
+          return $input.val(toPostgisPoint(point));
+        });
+        return newPoint;
+      };
+      center = [latitude, longitude];
+      if ($input.val()) {
+        center = convertPostGisToYandex($input.val());
+      }
+      placemark = NewPlacemark(center);
+      console.debug('Init Yandex -> ', center);
+      console.debug('Init PostGIS -> ', $input.val());
+      map = new ymaps.Map("map", {
+        zoom: zoom,
+        center: center
+      });
+      map.geoObjects.add(placemark);
+      map.controls.add('mapTools').add('zoomControl');
+      return map.events.add('click', function(e) {
+        if (placemark) {
+          map.geoObjects.remove(placemark);
+          placemark.geometry.setCoordinates(e.get('coordPosition'));
+        } else {
+          placemark = NewPlacemark(e.get('coordPosition'));
+        }
+        map.geoObjects.add(placemark);
+        $input.val(toPostgisPoint(placemark.geometry.getCoordinates()));
+        console.debug('');
+        console.debug('Set Yandex -> ', e.get('coordPosition'));
+        return console.debug('Set PostGIS -> ', $input.val());
+      });
+    });
+  });
+
+}).call(this);
---
+++ packages/django-ymaps-admin-widget/django-ymaps-admin-widget-1.0.12.tar.gz/django-ymaps-admin-widget-1.0.12/yandexmaps_widget/widgets.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.forms import widgets
+from django.template import loader
+
+JS = (
+    "http://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=ru-RU",
+)
+YMAP_JQUERY_URL = getattr(settings, 'YMAP_JQUERY_URL', 'https://code.jquery.com/jquery-2.1.1.min.js')
+if YMAP_JQUERY_URL:
+    JS = JS + (YMAP_JQUERY_URL,)
+
+
+class YMapsPointWidget(widgets.Input):
+    input_type = 'hidden'
+    template = 'yandexmaps_widget/ymaps_point_widget.html'
+
+    def __init__(self, *args, **kwargs):
+        super(YMapsPointWidget, self).__init__(*args, **kwargs)
+
+        # Define map options
+        self.map_options = self.default_map_options
+        extra_map_options = kwargs.get('map_options')
+        if extra_map_options:
+            self.map_options.update(extra_map_options)
+
+    @property
+    def default_map_options(self):
+        return {
+            'width': getattr(settings, 'YMAP_WIDTH', 600),
+            'height': getattr(settings, 'YMAP_HEIGHT', 400),
+            'zoom': getattr(settings, 'YMAP_ZOOM', 10),
+            'latitude': getattr(settings, 'YMAP_CENTER_LATITUDE', 55.76),
+            'longitude': getattr(settings, 'YMAP_CENTER_LONGITUDE', 37.64),
+        }
+
+    def render(self, name, value, attrs=None):
+        r = super(YMapsPointWidget, self).render(name, value, attrs)
+        params = self.map_options
+        params['input_id'] = attrs['id']
+        params['STATIC_URL'] = settings.STATIC_URL
+        return r + loader.render_to_string(self.template, params)
+
+    class Media(object):
+        js = JS
