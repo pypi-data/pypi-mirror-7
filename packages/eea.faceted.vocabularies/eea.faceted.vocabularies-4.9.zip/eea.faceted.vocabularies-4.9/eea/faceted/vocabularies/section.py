1e6a041f57d1213077c7428ba31a1439e4cf3d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:35 +0000

    Add package eea.faceted.vocabularies/4.9/eea.faceted.vocabularies-4.9.zip

---
+++ packages/eea.faceted.vocabularies/eea.faceted.vocabularies-4.9.zip/eea.faceted.vocabularies-4.9/eea/faceted/vocabularies/section.py
@@ -0,0 +1,27 @@
+""" Widget Sections Vocabularies
+"""
+from eea.faceted.vocabularies.utils import IVocabularyFactory
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+
+from eea.faceted.vocabularies import EEAMessageFactory as _
+
+class WidgetSections(object):
+    """ Widget position in page
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context=None):
+
+        items = (
+            SimpleTerm('default', 'default', _('Basic search')),
+            SimpleTerm('advanced', 'advanced', _('Extended search')),
+        )
+        return SimpleVocabulary(items)
+
+VocabularyFactory = WidgetSections()
+
+__all__ = [
+   IVocabularyFactory.__name__,
+]
