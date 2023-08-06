53780ac4aac5bd58e515d24aa52f6ae9b4fcb516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:51 +0000

    Add package brickpi_python/1.0/brickpi_python-1.0.tar.gz

---
+++ packages/brickpi_python/brickpi_python-1.0.tar.gz/brickpi_python-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'brickpi_python',
+  packages = ['brickpi_python'], 
+  version = '1.0',
+  description = 'This library for using the BrickPi in Python.',
+  author = 'Dexter Industries',
+  author_email = 'john@dexterindustries.com',
+  url = 'https://github.com/DexterInd/BrickPi_Python', 
+  download_url = 'https://github.com/DexterInd/BrickPi_Python/tarball/1.0', 
+  keywords = ['raspberry pi','BrickPi','LEGO','NXT','EV3'], 
+  classifiers = ['Development Status :: 5 - Production/Stable'],
+)
