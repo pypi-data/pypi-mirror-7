dbab7480c3622ae9aa81ba10ee7612a520e81570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package infi.recipe.close_application/0.0.11/infi.recipe.close_application-0.0.11-py2.7.egg

---
+++ packages/infi.recipe.close_application/infi.recipe.close_application-0.0.11-py2.7.egg/infi/recipe/close_application/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.11"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.11)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2226c40d202b282be463bd7dbe0485090d468df7'
+__git_head_subject__ = 'update homepage'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
