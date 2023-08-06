1cb4a1cdce094ef32f5bf4a970569426eb4e5654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:38 +0000

    Add package smstools/0.0.10/smstools-0.0.10.tar.gz

---
+++ packages/smstools/smstools-0.0.10.tar.gz/smstools-0.0.10/smstools/ios7.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+
+class IOS7:
+    """ iOS 7 sqlite reader and writer """
+
+
+    def parse(self, file):
+        raise Exception("not implemented!")
+        #TODO !!!
+    def write(self, texts, outfile):
+        raise Exception("not implemented!")
+        #TODO !!!
