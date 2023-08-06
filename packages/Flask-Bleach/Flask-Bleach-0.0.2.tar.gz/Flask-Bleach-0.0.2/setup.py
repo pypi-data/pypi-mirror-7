21e0acf9ca8b1f20faac28a9dae534a3af12aa75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:35 +0000

    Add package Flask-Bleach/0.0.2/Flask-Bleach-0.0.2.tar.gz

---
+++ packages/Flask-Bleach/Flask-Bleach-0.0.2.tar.gz/Flask-Bleach-0.0.2/setup.py
@@ -0,0 +1,35 @@
+"""
+Flask-Bleach
+------------
+
+This is the description for that library
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Bleach',
+    version='0.0.2',
+    license='BSD',
+    author='Dennis Fink',
+    author_email='dennis.fink@c3l.lu',
+    description='Easy integration of bleach',
+    long_description=__doc__,
+    py_modules=['flask_bleach'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'bleach',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
