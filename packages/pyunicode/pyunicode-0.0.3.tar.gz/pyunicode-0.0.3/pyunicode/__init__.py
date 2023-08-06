1d019d7099415d947f0865119156dc2429c77938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:44 +0000

    Add package pyunicode/0.0.3/pyunicode-0.0.3.tar.gz

---
+++ packages/pyunicode/pyunicode-0.0.3.tar.gz/pyunicode-0.0.3/pyunicode/__init__.py
@@ -0,0 +1,5 @@
+__title__ = 'pyunicode'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Pixelapse'
+
+from .api import safely_decode, safely_encode
---
+++ packages/pyunicode/pyunicode-0.0.3.tar.gz/pyunicode-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+try:
+  from setuptools import setup, find_packages
+except ImportError:
+  from distutils.core import setup
+
+version = '0.0.3'
+
+setup(
+  name='pyunicode',
+  version=version,
+  url='http://github.com/Pixelapse/pyunicode',
+  download_url='https://github.com/Pixelapse/pyunicode/tarball/v%s' % version,
+  author='Shravan Reddy',
+  author_email='shravan@pixelapse.com',
+  maintainer='Pixelapse',
+  maintainer_email='hello@pixelapse.com',
+  packages=find_packages(),
+  description='Library to deal with common python unicode handling issues',
+  long_description=open('README.md').read(),
+  license=open('LICENSE').read()
+)
