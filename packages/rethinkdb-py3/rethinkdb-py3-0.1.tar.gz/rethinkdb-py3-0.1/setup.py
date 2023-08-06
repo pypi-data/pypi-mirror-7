2934d19e6d3db0a69b7cb58fbd9064ab17266848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:36 +0000

    Add package rethinkdb-py3/0.1/rethinkdb-py3-0.1.tar.gz

---
+++ packages/rethinkdb-py3/rethinkdb-py3-0.1.tar.gz/rethinkdb-py3-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='rethinkdb-py3',
+    version='0.1',
+    description='Unofficial RethinkDB driver ported to Python 3',
+    url='http://rethinkdb.com/',
+    maintainer='Barosl Lee',
+    zip_safe=True,
+    packages=[
+        'rethinkdb',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rethinkdb-import=rethinkdb._import:main',
+            'rethinkdb-export=rethinkdb._export:main',
+            'rethinkdb-dump=rethinkdb._dump:main',
+            'rethinkdb-restore=rethinkdb._restore:main',
+        ],
+    },
+)
