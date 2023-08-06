30ced17ae68869aff419fbf9fa359b80442ef525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:21 +0000

    Add package sendgrid/0.5.3/sendgrid-0.5.3.tar.gz

---
+++ packages/sendgrid/sendgrid-0.5.3.tar.gz/sendgrid-0.5.3/setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup, find_packages
+
+
+def getRequires():
+    deps = ['smtpapi==0.1.2']
+    if sys.version_info < (3, 0):
+        deps.append('unittest2')
+    else:
+        deps.append('unittest2py3k')
+    return deps
+
+
+setup(
+    name='sendgrid',
+    version='0.5.3',
+    author='Yamil Asusta',
+    author_email='yamil@sendgrid.com',
+    url='https://github.com/sendgrid/sendgrid-python/',
+    packages=find_packages(),
+    license='MIT',
+    description='SendGrid library for Python',
+    long_description=open('./README.rst').read(),
+    install_requires=getRequires(),
+)
