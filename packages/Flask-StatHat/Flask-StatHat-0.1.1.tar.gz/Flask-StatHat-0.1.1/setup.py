4c6aa6569a36036e38ec28201d6fb15ced5520a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package Flask-StatHat/0.1.1/Flask-StatHat-0.1.1.tar.gz

---
+++ packages/Flask-StatHat/Flask-StatHat-0.1.1.tar.gz/Flask-StatHat-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name="Flask-StatHat",
+    version='0.1.1',
+    url='https://github.com/marksteve/flask-stathat',
+    license='MIT',
+    author="Mark Steve Samson",
+    author_email='hello@marksteve.com',
+    zip_safe=False,
+    description="StatHat extension for Flask",
+    py_modules=['flask_stathat'],
+    install_requires=[
+        'requests>=2.0,<3',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
