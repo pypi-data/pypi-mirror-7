7f7d6c3aa3527d63a65dce373502f850f9873c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:25 +0000

    Add package django-etc/0.3.0/django-etc-0.3.0.tar.gz

---
+++ packages/django-etc/django-etc-0.3.0.tar.gz/django-etc-0.3.0/etc/toolbox.py
@@ -0,0 +1,62 @@
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from django.apps import apps
+    apps_get_model = apps.get_model
+except ImportError:  # Django < 1.7
+    from django.db.models import get_model
+    apps_get_model = None
+
+
+def set_form_widgets_attrs(form, attrs):
+    """Applies a given HTML attributes to each field widget of a given form.
+
+    Example:
+
+        set_form_widgets_attrs(my_form, {'class': 'clickable'})
+
+    """
+    for _, field in form.fields.items():
+        attrs_ = dict(attrs)
+        for name, val in attrs.items():
+            if hasattr(val, '__call__'):
+                attrs_[name] = val(field)
+        field.widget.attrs = field.widget.build_attrs(attrs_)
+
+
+def get_model_class_from_settings(settings_module, settings_entry_name):
+    """Returns a certain model as defined in a given settings module.
+
+    Example:
+
+        myapp/settings.py
+
+            from django.conf import settings
+
+            MY_MODEL = getattr(settings, 'MYAPP_MY_MODEL', 'myapp.MyModel')
+
+
+        myapp/some.py
+
+            from myapp import settings
+
+            model = get_model_class_from_settings(settings, 'MY_MODEL')
+
+    """
+    try:
+        app_name, model_name = getattr(settings_module, settings_entry_name).split('.')
+    except ValueError:
+        raise ImproperlyConfigured('`%s` must have the following format: `app_name.model_name`.' % settings_entry_name)
+
+    if apps_get_model is None:
+        model = get_model(app_name, model_name)
+    else:
+        try:
+            model = apps_get_model(app_name, model_name)
+        except (LookupError, ValueError):
+            model = None
+
+    if model is None:
+        raise ImproperlyConfigured('`%s` refers to a model `%s` that has not been installed.' % (settings_entry_name, model_name))
+
+    return model
