e2cf822f99092054a6d10e8f7999ca7b09a208aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:45 +0000

    Add package crontab/0.19/crontab-0.19.tar.gz

---
+++ packages/crontab/crontab-0.19.tar.gz/crontab-0.19/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try:
+    with open('README') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+setup(
+    name='crontab',
+    version='0.19',
+    description='Parse and use crontab schedules in Python',
+    author='Josiah Carlson',
+    author_email='josiah.carlson@gmail.com',
+    url='https://github.com/josiahcarlson/parse-crontab',
+    packages=['crontab', 'tests'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    license='GNU LGPL v2.1',
+    long_description=long_description,
+)
