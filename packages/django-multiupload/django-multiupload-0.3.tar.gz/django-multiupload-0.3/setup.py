cc4978ba2ef9d2617ebeda5a398d40032e39ce88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:57 +0000

    Add package django-multiupload/0.3/django-multiupload-0.3.tar.gz

---
+++ packages/django-multiupload/django-multiupload-0.3.tar.gz/django-multiupload-0.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+from multiupload import __version__
+
+
+REQUIREMENTS = []
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    name='django-multiupload',
+    version=__version__,
+    description='Dead simple drop-in multi file upload field for django forms using HTML5\'s multiple attribute.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author='Chive',
+    author_email='kim@smuzey.ch',
+    url='https://github.com/Chive/django-multiupload',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False
+)
