7a4d28cf465f8fa3ba7219e3830f5dd1a98b8b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:08 +0000

    Add package dispatchonvalue/0.9.0/dispatchonvalue-0.9.0.tar.gz

---
+++ packages/dispatchonvalue/dispatchonvalue-0.9.0.tar.gz/dispatchonvalue-0.9.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='dispatchonvalue',
+    version='0.9.0',
+    author='Ian Macinnes',
+    author_email='ian.macinnes@gmail.com',
+    packages=['dispatchonvalue', 'dispatchonvalue.test'],
+    url='https://github.com/minimind/dispatch-on-value-for-python',
+    license='MIT',
+    description='Python package providing dispatch on values for ' +
+                'arbitrarily nested lists and dictionary data structures.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2 :: Only'
+    ],
+    keywords=['dispatch on value', 'multiple dispatch', 'dynamic dispatch'],
+    include_package_data=True
+)
