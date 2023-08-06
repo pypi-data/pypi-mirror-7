8c955a5eb7822b66a926b8614cb26856dbf2e161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:50 +0000

    Add package django-colorfield/0.1.3/django-colorfield-0.1.3.tar.gz

---
+++ packages/django-colorfield/django-colorfield-0.1.3.tar.gz/django-colorfield-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+version = '0.1.3'
+
+setup(
+    name="django-colorfield",
+    version=version,
+    keywords=["django", "color"],
+    author='Jared Forsyth',
+    author_email='jared@jaredforsyth.com',
+    license='MIT',
+    long_description="A small app providing a colorpicker field for django",
+    description="A small app providing a colorpicker field for django",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=['colorfield'],
+    include_package_data=True,
+    install_requires=['django>=1.2'],
+    requires=['django (>=1.2)'],
+)
+
