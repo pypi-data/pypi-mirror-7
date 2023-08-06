f4a526657c2ba350e7d027c2b16ff806b05a98f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:47 +0000

    Add package django-render-csv/0.2.2/django-render-csv-0.2.2.tar.gz

---
+++ packages/django-render-csv/django-render-csv-0.2.2.tar.gz/django-render-csv-0.2.2/django_render_csv.py
@@ -0,0 +1,26 @@
+import csv, codecs, cStringIO
+from django.http import HttpResponse
+from django.utils.encoding import smart_text
+
+
+def render_csv(data, filename="data.csv"):
+	res = HttpResponse(content_type='text/csv')
+	res['Content-Disposition'] = 'attachment; filename="%s"' % filename
+
+	writer = csv.writer(res)
+	for item in data:
+		writer.writerow(smart_text(item))
+
+	return res
+
+
+def as_csv(fn=None, filename=None):
+	def decorator(fn):
+		def _fn(request, *args, **kwargs):
+			data = fn(request, *args, **kwargs)
+			return render_csv(data, filename=filename)
+
+		return _fn
+
+	return decorator(fn) if fn else decorator
+
