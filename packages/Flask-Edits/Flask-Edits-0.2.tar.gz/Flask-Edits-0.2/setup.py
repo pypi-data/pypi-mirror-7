bc1749085020b0a0a6e5f20efd0db30f36428d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:36 +0000

    Add package Flask-Edits/0.2/Flask-Edits-0.2.tar.gz

---
+++ packages/Flask-Edits/Flask-Edits-0.2.tar.gz/Flask-Edits-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-Edits',
+    version='0.2',
+    packages=['flask_edits',],
+    license='BSD',
+    url='https://pypi.python.org/pypi/Flask-Edits',
+    author='Nathan Cahill',
+    author_email='nathan@nathancahill.com',
+    description='Editable Content in Flask',
+    long_description=open('README').read(),
+)
