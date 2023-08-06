c53e711f4c0fe1d0f9eca426288739d90629b32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:44 +0000

    Add package maproxy/0.0.8/maproxy-0.0.8.zip

---
+++ packages/maproxy/maproxy-0.0.8.zip/maproxy-0.0.8/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+
+setup(name='maproxy',
+
+    version = "0.0.8",
+    author = "Zvika Ferentz",
+    author_email = "zvika dot ferentz at gmail",
+    description = ("My first attempt to create a simple and awesome "
+                   "TCP proxy using Tornado"),
+    
+
+
+    # For now, let's just build a module, not a package
+    packages=['maproxy'],
+
+    keywords = "TCP proxy ssl http https certificates",
+    long_description=open('README').read(),
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Internet :: Proxy Servers"]
+      )
