371493986edfbed472c00d60aa96901aeffb8295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:51 +0000

    Add package hdfs/0.2.4/hdfs-0.2.4.tar.gz

---
+++ packages/hdfs/hdfs-0.2.4.tar.gz/hdfs-0.2.4/hdfs/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""HdfsCLI."""
+
+__version__ = '0.2.4'
+
+import logging as lg
+try:
+  from .client import Client, InsecureClient, TokenClient
+  from .ext import * # import all exported extensions, nothin' on you
+  from .util import Config
+except ImportError:
+  pass # in setup.py
+
+
+class _NullHandler(lg.Handler):
+
+  """For python <2.7."""
+
+  def emit(self, record):
+    pass
+
+
+lg.getLogger(__name__).addHandler(_NullHandler())
