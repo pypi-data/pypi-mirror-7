59523f6ad97ddc0b1111788bcae2c5f54d01ce47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:21 +0000

    Add package django-usertools/1.0.7/django-usertools-1.0.7.tar.gz

---
+++ packages/django-usertools/django-usertools-1.0.7.tar.gz/django-usertools-1.0.7/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+
+setup(
+    name = "django-usertools",
+    version = "1.0.7",
+    description = "A fire-and-forget enhancement to the Django user admin.",
+    author = "Dave Hall",
+    author_email = "dave@etianen.com",
+    url = "http://github.com/etianen/django-usertools",
+    zip_safe = False,
+    packages = [
+        "usertools",
+        "usertools.management",
+        "usertools.management.commands",
+        "usertools.templatetags",
+        "usertools.migrations_auth",
+    ],
+    package_dir = {
+        "": "src",
+    },
+    package_data = {
+        "usertools": [
+            "locale/*/LC_MESSAGES/django.*",
+            "templates/admin/auth/user/*.html",
+            "templates/admin/auth/user/*.txt",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+)
