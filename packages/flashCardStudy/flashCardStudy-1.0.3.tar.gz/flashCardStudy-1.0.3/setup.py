1e30fc11ec3767ddab19fc1dafce8bae79dba8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:55 +0000

    Add package flashCardStudy/1.0.3/flashCardStudy-1.0.3.tar.gz

---
+++ packages/flashCardStudy/flashCardStudy-1.0.3.tar.gz/flashCardStudy-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='flashCardStudy',
+    version='1.0.3',
+    author='comatory',
+    author_email='osekdomains@gmail.com',
+    packages=['flashcardstudy','tests','bin'],
+    scripts=['bin/flashstudy.py'],
+    url='https://github.com/comatory/flashCardStudy',
+    license='LICENSE.txt',
+    description='CLI-based utility for memorizing and studying',
+    long_description=open('README.txt').read(),
+	classifiers=[
+				  'Programming Language :: Python',
+				  'Programming Language :: Python :: 2.7',
+				  'License :: OSI Approved :: MIT License',
+				  'Topic :: Education',
+				  ], 
+    install_requires=[
+        "PrettyTable",
+    ],
+	entry_points = {
+		'console_scripts': ['flashstudy = bin.flashstudy:flashcard']
+			}
+)
---
+++ packages/flashCardStudy/flashCardStudy-1.0.3.tar.gz/flashCardStudy-1.0.3/tests/app_tests.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#from nose.tools import *
+#from flashcardstudy import main
+
+#def test_card_class():
+	#test_card = main.Card()
+	#assert_equal(test_card.content, {})
+	#assert_is_instance(test_card, main.Card, msg="Object is not instance of Card()")
+
+#def test_create_new_card():
+	#test_card = main.Card()
+	#test_card.content['Side One'] = 'Side Two'
+	#assert_equal(test_card.content, {'Side One': 'Side Two'})
+
+#def test_create_new_stack():
+	#another_card = main.Card()
+	#another_card.content['FOO'] = 'BAR'
+	#test_stack = main.Stack('Test Stack')
+	#assert_equal(test_stack.name, 'Test Stack')
+	#test_stack.stacks.append(another_card.content)
+	#assert_equal(test_stack.stacks, [{'FOO': 'BAR'}])
