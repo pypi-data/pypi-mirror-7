245ebd0e098434ed468030a18bbf41eebb66a384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:38 +0000

    Add package portability/0.1/portability-0.1.tar.gz

---
+++ packages/portability/portability-0.1.tar.gz/portability-0.1/portability/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'portability',
+  packages = ['portability'], 
+  version = '0.1',
+  description = "Seattle's portability package",
+  author = 'Seattle',
+  url = 'https://github.com/choksi81/portability', # use the URL to the github repo
+  classifiers = [],
+)
---
+++ packages/portability/portability-0.1.tar.gz/portability-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'portability',
+  packages = ['portability'], # this must be the same as the name above
+  version = '0.1',
+  description = "Seattle's seash package",
+  author = 'Seattle',
+  author_email = 'cc4256@nyu.edu',
+  url = 'https://github.com/choksi81/portability', # use the URL to the github repo
+  download_url = 'https://github.com/choksi81/portability/tarball/0.1', # I'll explain this in a second
+  classifiers = [],
+)
