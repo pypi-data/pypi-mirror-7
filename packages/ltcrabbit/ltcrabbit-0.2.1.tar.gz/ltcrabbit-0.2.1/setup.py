d05446af4cc7833ca497194ec4afe84a93a66556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:38 +0000

    Add package ltcrabbit/0.2.1/ltcrabbit-0.2.1.tar.gz

---
+++ packages/ltcrabbit/ltcrabbit-0.2.1.tar.gz/ltcrabbit-0.2.1/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='ltcrabbit',
+    version='0.2.1',
+    author='Jaromir Fojtu',
+    author_email='jaromir.fojtu@gmail.com',
+    url='https://github.com/syntaxsugar/ltcrabbit',
+    py_modules=['ltcrabbit'],
+    scripts=['bin/ltcrabbit-cli'],
+    license='BSD',
+    description='LTCRabbit.com API wrapper',
+    long_description=open('README.md').read(),
+    install_requires=['requests', 'prettytable', 'docopt'],
+)
