ecb8c86495d86f80785d2df873a788490621881a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:57 +0000

    Add package pymosh/0.1.1/pymosh-0.1.1.tar.gz

---
+++ packages/pymosh/pymosh-0.1.1.tar.gz/pymosh-0.1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pymosh',
+    version='0.1.1',
+    description='Arbitrary video file manipulation.',
+    author='Joe Friedl',
+    author_email='joe@joefriedl.net',
+    url='http://github.com/grampajoe/pymosh/',
+    packages=['pymosh'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
