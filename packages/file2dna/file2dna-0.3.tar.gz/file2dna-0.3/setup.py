df6d1e5a570441d03c7083d691cb345ce9b83f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:32 +0000

    Add package file2dna/0.3/file2dna-0.3.tar.gz

---
+++ packages/file2dna/file2dna-0.3.tar.gz/file2dna-0.3/setup.py
@@ -0,0 +1,22 @@
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='file2dna',
+    version='0.3',
+    author = 'Allan Inocencio de Souza Costa',
+    author_email = 'allaninocencio@yahoo.com.br',
+    description = 'A script to encode/decode arbitrary computer files into DNA sequences.',
+    url= 'https://github.com/allanino/DNA',
+    packages=['dna',],
+    license='MIT',
+    keywords= 'dna encoding decoding file',
+    long_description=read('README.rst'),
+    entry_points = {
+        'console_scripts': ['dna=dna.dna:main'],
+    }
+)
