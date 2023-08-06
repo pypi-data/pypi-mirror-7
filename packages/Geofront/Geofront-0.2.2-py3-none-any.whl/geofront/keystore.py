be2568139029e0a12757666925a6262e67c2c899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:15 +0000

    Add package Geofront/0.2.2/Geofront-0.2.2-py3-none-any.whl

---
+++ packages/Geofront/Geofront-0.2.2-py3-none-any.whl/geofront/keystore.py
@@ -0,0 +1,154 @@
+""":mod:`geofront.keystore` --- Public key store
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+import base64
+import collections.abc
+
+from paramiko.dsskey import DSSKey
+from paramiko.rsakey import RSAKey
+from paramiko.pkey import PKey
+
+from .identity import Identity
+from .util import typed
+
+__all__ = ('KEY_TYPES', 'AuthorizationError', 'DuplicatePublicKeyError',
+           'KeyStore', 'KeyStoreError', 'KeyTypeError',
+           'format_openssh_pubkey', 'get_key_fingerprint',
+           'parse_openssh_pubkey')
+
+
+#: (:class:`collections.Mapping`) The mapping of supported key types.
+KEY_TYPES = {
+    'ssh-rsa': RSAKey,
+    'ssh-dss': DSSKey
+}
+
+
+@typed
+def parse_openssh_pubkey(line: str) -> PKey:
+    """Parse an OpenSSH public key line, used by :file:`authorized_keys`,
+    :file:`id_rsa.pub`, etc.
+
+    :param line: a line of public key
+    :type line: :class:`str`
+    :return: the parsed public key
+    :rtype: :class:`paramiko.pkey.PKey`
+    :raise ValueError: when the given ``line`` is an invalid format
+    :raise KeyTypeError: when it's an unsupported key type
+
+    """
+    keytype, b64, *_ = line.split()
+    try:
+        cls = KEY_TYPES[keytype]
+    except KeyError:
+        raise KeyTypeError('unsupported key type: ' + repr(keytype))
+    return cls(data=base64.b64decode(b64))
+
+
+@typed
+def format_openssh_pubkey(key: PKey) -> str:
+    """Format the given ``key`` to an OpenSSH public key line, used by
+    :file:`authorized_keys`, :file:`id_rsa.pub`, etc.
+
+    :param key: the key object to format
+    :type key: :class:`paramiko.pkey.PKey`
+    :return: a formatted openssh public key line
+    :rtype: :class:`str`
+
+    """
+    return '{} {} '.format(key.get_name(), key.get_base64())
+
+
+@typed
+def get_key_fingerprint(key: PKey, glue: str=':') -> str:
+    """Get the hexadecimal fingerprint string of the ``key``.
+
+    :param key: the key to get fingerprint
+    :type key: :class:`paramiko.pkey.PKey`
+    :param glue: glue character to be placed between bytes.
+                 ``':'`` by default
+    :type glue: :class:`str`
+    :return: the fingerprint string
+    :rtype: :class:`str`
+
+    """
+    return glue.join(map('{:02x}'.format, key.get_fingerprint()))
+
+
+class KeyStore:
+    """The key store backend interface.  Every key store has to guarantee
+    that public keys are unique for all identities i.e. the same public key
+    can't be registered across more than an identity.
+
+    """
+
+    @typed
+    def register(self, identity: Identity, public_key: PKey):
+        """Register the given ``public_key`` to the ``identity``.
+
+        :param ientity: the owner identity
+        :type identity: :class:`~.identity.Identity`
+        :param public_key: the public key to register
+        :type public_key: :class:`paramiko.pkey.PKey`
+        :raise geofront.keystore.AuthorizationError:
+            when the given ``identity`` has no required permission
+            to the key store
+        :raise geofront.keystore.DuplicatePublicKeyError:
+            when the ``public_key`` is already in use
+
+
+        """
+        raise NotImplementedError('register() has to be implemented')
+
+    @typed
+    def list_keys(self, identity: Identity) -> collections.abc.Set:
+        """List registered public keys of the given ``identity``.
+
+        :param identity: the owner of keys to list
+        :type identity: :class:`~.identity.Identity`
+        :return: the set of :class:`paramiko.pkey.PKey`
+                 owned by the ``identity``
+        :rtype: :class:`collections.abc.Set`
+        :raise geofront.keystore.AuthorizationError:
+            when the given ``identity`` has no required permission
+            to the key store
+
+        """
+        raise NotImplementedError('list_keys() has to be implemented')
+
+    @typed
+    def deregister(self, identity: Identity, public_key: PKey):
+        """Remove the given ``public_key`` of the ``identity``.
+        It silently does nothing if there isn't the given ``public_key``
+        in the store.
+
+        :param ientity: the owner identity
+        :type identity: :class:`~.identity.Identity`
+        :param public_key: the public key to remove
+        :type public_key: :class:`paramiko.pkey.PKey`
+        :raise geofront.keystore.AuthorizationError:
+            when the given ``identity`` has no required permission
+            to the key store
+
+        """
+        raise NotImplementedError('deregister() has to be implemented')
+
+
+class KeyStoreError(Exception):
+    """Exceptions related to :class:`KeyStore` are an instance of this."""
+
+
+class AuthorizationError(KeyStoreError):
+    """Authorization exception that rise when the given identity has
+    no required permission to the key store.
+
+    """
+
+
+class DuplicatePublicKeyError(KeyStoreError):
+    """Exception that rise when the given public key is already registered."""
+
+
+class KeyTypeError(ValueError):
+    """Unsupported public key type raise this type of error."""
---
+++ packages/Geofront/Geofront-0.2.2-py3-none-any.whl/tests/backends/cloud_test.py
@@ -0,0 +1,142 @@
+import hashlib
+import io
+import os
+
+from libcloud.compute.base import Node, KeyPair
+from libcloud.compute.drivers.dummy import DummyNodeDriver
+from libcloud.compute.types import KeyPairDoesNotExistError
+from libcloud.storage.drivers import dummy
+from libcloud.storage.drivers.dummy import DummyStorageDriver
+from paramiko.rsakey import RSAKey
+from pytest import raises
+
+from geofront.backends.cloud import (CloudKeyStore, CloudMasterKeyStore,
+                                     CloudMasterPublicKeyStore, CloudRemoteSet,
+                                     get_metadata, supports_metadata)
+from geofront.identity import Identity
+from geofront.keystore import (format_openssh_pubkey, get_key_fingerprint,
+                               parse_openssh_pubkey)
+from geofront.masterkey import EmptyStoreError
+from geofront.remote import Remote
+from ..keystore_test import assert_keystore_compliance
+from ..server_test import DummyTeam, MemoryMasterKeyStore
+
+
+@supports_metadata.register(DummyNodeDriver)
+def dummy_supports_metadata(driver: DummyNodeDriver):
+    return True
+
+
+@get_metadata.register(DummyNodeDriver)
+def dummy_get_metadata(driver: DummyNodeDriver, node: Node):
+    return {'dummy': 'test'}
+
+
+def test_cloud_remote_set():
+    driver = DummyNodeDriver('')
+    set_ = CloudRemoteSet(driver)
+    assert len(set_) == 2
+    assert set_['dummy-1'] == Remote('ec2-user', '127.0.0.1')
+    assert set_['dummy-1'].metadata == {'dummy': 'test'}
+    assert set_['dummy-2'] == Remote('ec2-user', '127.0.0.1')
+    assert set_['dummy-2'].metadata == {'dummy': 'test'}
+
+
+def test_cloud_master_key_store():
+    driver = DummyStorageDriver('', '')
+    container = driver.create_container('geofront-test')
+    s = CloudMasterKeyStore(driver, container, 'test_id_rsa')
+    with raises(EmptyStoreError):
+        s.load()
+    key = RSAKey.generate(1024)
+    s.save(key)
+    driver.get_object(container.name, 'test_id_rsa')  # assert object exists
+    # Mocking implementation
+    with io.StringIO() as mock:
+        key.write_private_key(mock)
+        mock.seek(0)
+        dummy.DummyFileObject = lambda *a, **k: mock
+        stored_key = s.load()
+        assert isinstance(stored_key, RSAKey)
+        assert stored_key.get_base64() == stored_key.get_base64()
+
+
+class KeyPairSupportedDummyNodeDriver(DummyNodeDriver):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key_pairs = {}
+
+    def get_key_pair(self, name):
+        try:
+            key_material = self.key_pairs[name]
+        except KeyError:
+            raise KeyPairDoesNotExistError(name, self)
+        return KeyPair(name,
+                       key_material,
+                       get_key_fingerprint(parse_openssh_pubkey(key_material)),
+                       self)
+
+    def list_key_pairs(self):
+        return [self.get_key_pair(name) for name in self.key_pairs]
+
+    def import_key_pair_from_string(self, name, key_material):
+        self.key_pairs[name] = key_material
+
+    def delete_key_pair(self, key_pair):
+        del self.key_pairs[key_pair.name]
+
+    def import_key_pair_from_file(self, name, key_file_path):
+        with open(key_file_path) as f:
+            self.import_key_pair_from_string(name, f.read())
+
+    def create_key_pair(self, name):
+        self.import_key_pair_from_string(
+            name,
+            format_openssh_pubkey(RSAKey.generate(1024))
+        )
+
+
+def test_cloud_key_store():
+    driver = KeyPairSupportedDummyNodeDriver('')
+    keystore = CloudKeyStore(driver)
+    identity = Identity(DummyTeam, 'abcd')
+    assert_keystore_compliance(keystore, identity)
+    identity2 = Identity(DummyTeam, 'efg')
+    assert_keystore_compliance(keystore, identity2)
+
+
+def test_cloud_key_store_get_key_name_pattern():
+    driver = KeyPairSupportedDummyNodeDriver('')
+    keystore = CloudKeyStore(driver)
+    identity = Identity(DummyTeam, 'abcd')
+    pattern = keystore._get_key_name_pattern(identity)
+    random_fp = lambda: ':'.join(
+        map('{:02x}'.format, hashlib.md5(os.urandom(100)).digest())
+    )
+    actual = {
+        'tests.server_test.DummyTeam abcd ' + random_fp()
+        for _ in range(5)
+    }
+    result = filter(pattern.match, actual | {
+        'tests.server_test.DummyTeam defg ' + random_fp(),
+        'tests.server_test.OtherTeam abcd ' + random_fp(),
+        'tests.server_test.DummyTeam abcd ',
+        'junk'
+    })
+    assert frozenset(result) == actual
+
+
+def test_cloud_master_public_key_store():
+    driver = KeyPairSupportedDummyNodeDriver('')
+    actual_store = MemoryMasterKeyStore()
+    store = CloudMasterPublicKeyStore(driver,
+                                      'geofront-masterkey',
+                                      actual_store)
+    for _ in range(2):
+        master_key = RSAKey.generate(1024)
+        store.save(master_key)
+        assert actual_store.load() == store.load() == master_key
+        assert parse_openssh_pubkey(
+            driver.get_key_pair('geofront-masterkey').public_key
+        ) == master_key
