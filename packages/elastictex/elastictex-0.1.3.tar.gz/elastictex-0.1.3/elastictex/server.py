67b5e24af03c19b44ddb1c000b24edd669993e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package elastictex/0.1.3/elastictex-0.1.3.tar.gz

---
+++ packages/elastictex/elastictex-0.1.3.tar.gz/elastictex-0.1.3/elastictex/server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from elastictex import api
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+def main():
+    try:
+        with open("~/.elastictex", "r") as f:
+            host = f.read().strip()
+    except:
+        host = "localhost:9200"
+
+    api.init(host)
+    app.run(debug=True)
+
+
+@app.route('/')
+def get():
+    results = None
+    q = request.args.get('q')
+    if q:
+        results = api.search(q)
+    return render_template("index.html",
+                           results=results)
+
+
+@app.route('/upload', methods=['POST'])
+def post():
+    url = request.args.get('url')
+    if url is None:
+        return "Invalid URL"
+    else:
+        api.index(url, request.data)
+        return "Indexed %s\n" % url
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/elastictex/elastictex-0.1.3.tar.gz/elastictex-0.1.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(name="elastictex",
+      version="0.1.3",
+      description="Search with Latex",
+      author="Xander Masotto",
+      author_email="xmasotto@gmail.com",
+      url="http://github.com/xmasotto/elastictex",
+      packages=["elastictex",
+                "mongo_connector",
+                "mongo_connector.doc_managers"],
+      package_data={
+          'elastictex': ['templates/index.html']
+      },
+      install_requires=["elasticsearch", "flask", "beautifulsoup4"],
+      entry_points={
+          'console_scripts': [
+              'elastictex = elastictex.server:main',
+              'elastictex_crawl = elastictex.crawl:main'
+          ]
+      })
