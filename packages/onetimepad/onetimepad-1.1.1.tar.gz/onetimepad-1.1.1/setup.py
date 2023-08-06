e985c359cb792c0b31ba1d3b8b20d6a2cae772e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:18 +0000

    Add package onetimepad/1.1.1/onetimepad-1.1.1.tar.gz

---
+++ packages/onetimepad/onetimepad-1.1.1.tar.gz/onetimepad-1.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='onetimepad',
+    
+    version='1.1.1',
+    description='A hacky implementation of One-time pad',
+    long_description=open('README.rst').read(),
+
+    py_modules=['onetimepad'],
+
+    url='http://jailuthra.in/onetimepad',
+    author='Jai Luthra',
+    author_email='me@jailuthra.in',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'onetimepad = onetimepad:main',
+        ],
+    },
+)
