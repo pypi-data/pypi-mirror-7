4f742b1e2b4236ffcb772c7afeea32846835c47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:37 +0000

    Add package line/0.0.7/line-0.0.7.tar.gz

---
+++ packages/line/line-0.0.7.tar.gz/line-0.0.7/line/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    line 
+    ~~~~
+
+    May the LINE be with you...
+
+    :copyright: (c) 2014 by Taehoon Kim.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import, unicode_literals
+import sys
+
+from .client import LineClient
+
+__version__ = '0.0.7'
+__all__ = ['LineClient']
