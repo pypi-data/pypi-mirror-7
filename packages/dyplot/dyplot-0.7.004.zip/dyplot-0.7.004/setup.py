392ae422258b80acd0967dfdfdc056d8064f5b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:43 +0000

    Add package dyplot/0.7.004/dyplot-0.7.004.zip

---
+++ packages/dyplot/dyplot-0.7.004.zip/dyplot-0.7.004/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='dyplot',
+    version='0.7.004',
+    author='Tsung-Han Yang',
+    author_email='blacksburg98@yahoo.com',
+    packages=['dyplot'],
+    url='http://pypi.python.org/pypi/dyplot/',
+    license='LICENSE.txt',
+    description='matplotlib-like plot functions for dygraphs.js and c3.js.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pandas >= 0.7.3",
+    ],
+    classifiers = [
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Programming Language :: JavaScript",
+    ],
+)
+
