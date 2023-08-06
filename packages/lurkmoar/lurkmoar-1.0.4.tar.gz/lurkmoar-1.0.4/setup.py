d6f39df2e9c3fddc2fff605a6575da4ad0cb165a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:21 +0000

    Add package lurkmoar/1.0.4/lurkmoar-1.0.4.tar.gz

---
+++ packages/lurkmoar/lurkmoar-1.0.4.tar.gz/lurkmoar-1.0.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lurkmoar import __version__
+from setuptools import setup
+
+setup(
+    name='lurkmoar',
+    version=__version__,
+    author='Louis Thibault',
+    author_email='louist87@gmail.com',
+    packages=['lurkmoar'],
+    include_package_data=True,
+    install_requires=['requests', 'pillow'],
+    url='',
+    license='Unlicense',
+    description='4chan API wrapper geared towards text analysis',
+    keywords=['4chan', 'b', '/b/', 'imageboard', 'bbs', 'text'],
+    long_description=''
+)
