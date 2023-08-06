25047dab66baa16de5ec3127bc1a14eae7b53553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:18 +0000

    Add package bagit/1.5.0/bagit-1.5.0.tar.gz

---
+++ packages/bagit/bagit-1.5.0.tar.gz/bagit-1.5.0/setup.py
@@ -0,0 +1,50 @@
+from sys import version, exit
+from setuptools import setup
+
+if version < '2.4.0' or version > '3.0.0':
+    print "python 2.4 - 2.7 is required"
+    exit(1)
+
+description = \
+"""
+This package can be used to create BagIt style packages of
+digital content for safe transmission and digital preservation.
+See: http://en.wikipedia.org/wiki/BagIt for more details.
+"""
+
+# for older pythons ...
+requirements = []
+try:
+    import multiprocessing
+except:
+    requirements.append("multiprocessing")
+try:
+    import hashlib
+except:
+    requirements.append("hashlib")
+
+
+setup(
+    name = 'bagit',
+    version = '1.5.0',
+    url = 'https://libraryofcongress.github.io/bagit-python/',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['bagit',],
+    scripts = ['bagit.py'],
+    description = description,
+    platforms = ['POSIX'],
+    test_suite = 'test',
+    install_requires = requirements,
+    classifiers = [
+        'License :: Public Domain',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
