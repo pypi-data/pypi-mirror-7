993eb7589ae379ffdcdb6d04fea4efd6a0f37911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:43 +0000

    Add package tarnish/0.1/tarnish-0.1.tar.gz

---
+++ packages/tarnish/tarnish-0.1.tar.gz/tarnish-0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+VERSION = "0.1"
+REPO = "https://github.com/icio/tarnish"
+README = "README.rst"
+
+with open(README) as f:
+    long_description = f.read()
+
+setup(
+    name="tarnish",
+    version=VERSION,
+    description="Can't handle my tarn",
+    author="Paul Scott, Duedil Limited",
+    author_email="paul@duedil.com",
+    url=REPO,
+    download_url="%s/tarball/%s" % (REPO, VERSION),
+    py_modules=["tarnish"],
+    license="MIT",
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'tarnish = tarnish:main',
+        ],
+    },
+    keywords=["tarnfeld", "tarnfeid", "tarn", "can't handle my"],
+    classifiers=[],
+)
