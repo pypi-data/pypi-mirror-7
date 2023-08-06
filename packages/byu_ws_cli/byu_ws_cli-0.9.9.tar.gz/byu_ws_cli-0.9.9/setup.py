b1787caa083c3d3a443d4a712cd6a8ae2fbb22ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package byu_ws_cli/0.9.9/byu_ws_cli-0.9.9.tar.gz

---
+++ packages/byu_ws_cli/byu_ws_cli-0.9.9.tar.gz/byu_ws_cli-0.9.9/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+from setuptools import setup, find_packages
+
+
+if not ((sys.version_info >= (2, 7,) and sys.version_info < (3,)) or sys.version_info >= (3, 3,)):
+    sys.stderr.write("byu_ws_cli currently requires Python 2.7 or Python 3.3 or greater\n")
+    sys.exit(-1)
+
+# we only use the subset of markdown that is also valid reStructuredText so
+# that our README.md works on both github (markdown) and pypi (reStructuredText)
+with open("README.md") as rm_file:
+    long_description = rm_file.read()
+
+setup(name='byu_ws_cli',
+      version='0.9.9',
+      description='A command-line web service client for calling BYU REST web services.',
+      long_description=long_description,
+      author='BYU OIT Core Application Engineering',
+      author_email='paul_eden@byu.edu',
+      url='https://github.com/byu-oit-core-appeng/byu-ws-cli-python',
+      packages=find_packages(),
+      data_files=[('', ['README.md', 'LICENSE'])],
+      test_suite="byu_ws_cli.test",
+      license="MIT",
+      obsoletes=['oit_web_service_client'],
+      install_requires=['byu_ws_sdk < 1.1.0'],
+      scripts=['bin/call_byu_ws'],
+      zip_safe=True
+      )
