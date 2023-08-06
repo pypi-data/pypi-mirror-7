5e00ec4bc221d21fef61052bc11aaa75d2626950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:48 +0000

    Add package SRPAstro.FITS/1.0.1/SRPAstro.FITS-1.0.1.tar.gz

---
+++ packages/SRPAstro.FITS/SRPAstro.FITS-1.0.1.tar.gz/SRPAstro.FITS-1.0.1/SRP/SRPFITS/__init__.py
@@ -0,0 +1,32 @@
+""" 
+
+Context : SRP
+Module  : FITS
+Version : 1.0.3
+Author  : Stefano Covino
+Date    : 06/08/2014
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+
+Usage   : to be imported
+
+Remarks :
+
+History : (25/03/2014) First version.
+        : (16/05/2014) V. 1.0.1b1.
+        : (16/07/2014) V. 1.0.1b2.
+        : (21/07/2014) V. 1.0.1b3.
+        : (06/08/2014) V. 1.0.1.
+"""
+
+__version__ = '1.0.1'
+
+
+
+#__all__ =  ['FITS',]
+
+
+
+
+
