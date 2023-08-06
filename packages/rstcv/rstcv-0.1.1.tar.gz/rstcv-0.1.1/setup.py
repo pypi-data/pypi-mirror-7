7ef27ba885b58f7b508ac9b7f21cab7b9b665844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:38 +0000

    Add package rstcv/0.1.1/rstcv-0.1.1.tar.gz

---
+++ packages/rstcv/rstcv-0.1.1.tar.gz/rstcv-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+requirements = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            'requirements.txt')
+
+with open(requirements) as rf:
+    requires = [l.strip() for l in rf.readlines()]
+
+    setup(name='rstcv',
+          author='Hung Nguyen Viet',
+          author_email='hvn@familug.org',
+          url='https://pypi.python.org/pypi/rstcv',
+          version='0.1.1',
+          scripts=['scripts/rstcv-build'],
+          install_requires=requires,
+          )
