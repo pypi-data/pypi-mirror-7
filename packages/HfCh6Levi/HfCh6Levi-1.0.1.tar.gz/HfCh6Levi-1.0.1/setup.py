9e162b950b1f85b1c566de9537080a2db8fe5c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:13 +0000

    Add package HfCh6Levi/1.0.1/HfCh6Levi-1.0.1.tar.gz

---
+++ packages/HfCh6Levi/HfCh6Levi-1.0.1.tar.gz/HfCh6Levi-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+	
+    	name='HfCh6Levi',
+    	version='1.0.1',
+    	py_modules=['HfCh6Levi'],
+    	author='levibinlee',
+    	author_email='levibinlee@gmail.com',
+    	url='http://www.headfirstlabs',
+	description='refined code for displatying sarah best times along with her name and bod using Dictionary rather than List' ,    
+)
+    
