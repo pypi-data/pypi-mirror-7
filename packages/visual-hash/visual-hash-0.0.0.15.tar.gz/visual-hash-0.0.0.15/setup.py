75b95decbcdd6dadfc33b2b13cedc534699efd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:05 +0000

    Add package visual-hash/0.0.0.15/visual-hash-0.0.0.15.tar.gz

---
+++ packages/visual-hash/visual-hash-0.0.0.15.tar.gz/visual-hash-0.0.0.15/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+    ext = '.pyx'
+except:
+    USE_CYTHON = False
+    ext = '.c'
+
+extensions = [Extension("VisualHashPrivate.FractalTransform",
+                        ["VisualHashPrivate/FractalTransform"+ext])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+import os
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    packages = ['VisualHash', 'VisualHashPrivate'],
+    #ext_modules = extensions,
+    install_requires = [
+        'pycrypto'
+        ],
+    license = "BSD",
+    name = "visual-hash",
+    version = "0.0.0.15",
+    url = "https://github.com/droundy/visual-hash",
+    author = "David Roundy",
+    author_email = "daveroundy@gmail.com",
+    description = ("A package to generate visual hashes."),
+    long_description=read('README'),
+    package_data={'': ['*.md']},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
