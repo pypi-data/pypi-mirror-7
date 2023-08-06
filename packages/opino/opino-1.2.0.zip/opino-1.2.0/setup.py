7b6e3f9a8501dce00ddc9cc9a7b75c245150310f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package opino/1.2.0/opino-1.2.0.zip

---
+++ packages/opino/opino-1.2.0.zip/opino-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'opino',
+	version 	= '1.2.0',
+	py_modules 	= ['opino'],
+	author 		= 'opino',
+	author_email 	= 'opino@khu.ac.kr',
+	url 		= 'http://www.headfirstlabs.com',
+	description 	= 'A simple printer of lists',
+)
