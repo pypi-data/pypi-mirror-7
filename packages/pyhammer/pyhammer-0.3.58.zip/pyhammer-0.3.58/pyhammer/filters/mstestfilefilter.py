63736dcf4bda8244b42519a82c65aba2ecf8258a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:45 +0000

    Add package pyhammer/0.3.58/pyhammer-0.3.58.zip

---
+++ packages/pyhammer/pyhammer-0.3.58.zip/pyhammer-0.3.58/pyhammer/filters/mstestfilefilter.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from pyhammer.filters.filefilter import FileFilter
+
+class MsTestFileFilter(FileFilter):
+    def __init__( self, mode = 'Release'):
+        if mode == None:
+            mode = 'Release'
+        FileFilter.__init__( self, include=['*Tests.dll'], folderPattern='*\\bin\\'+mode+'\\*')
