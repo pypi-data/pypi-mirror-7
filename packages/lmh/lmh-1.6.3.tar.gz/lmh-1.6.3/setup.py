aac932cd64c51972f3944d54a540664090251a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:05 +0000

    Add package lmh/1.6.3/lmh-1.6.3.tar.gz

---
+++ packages/lmh/lmh-1.6.3.tar.gz/lmh-1.6.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='lmh',
+      version='1.6.3',
+      description='Local MathHub Utility (setup package)',
+      author='The KWARC Group',
+      author_email = "postmaster@kwarc.info",
+      scripts=['lmh'],
+      license='GPL',
+      packages=['lmh_core'],
+      install_requires=['beautifulsoup4', 'psutil', 'pyapi-gitlab']
+)
