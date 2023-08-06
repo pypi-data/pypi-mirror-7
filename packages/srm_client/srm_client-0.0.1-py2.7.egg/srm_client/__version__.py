6047e33d7a26fb1a1397df82a30bb00682255dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package srm_client/0.0.1/srm_client-0.0.1-py2.7.egg

---
+++ packages/srm_client/srm_client-0.0.1-py2.7.egg/srm_client/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b843f6a40806b73a39fde72b69daf8fe411aa895'
+__git_head_subject__ = 'Finished release v0.0.1.'
+__git_head_message__ = '* release/v0.0.1:\n  include xml templates in build\n  Remove debugging code\n  fix bug in get_recovery_plans when there are multiple plans\n  SRA-43 SRM client: write README file\n  comments\n  nicer cli and more commands\n  initial commit\n  added all project files'
+__git_dirty_diff__ = ''
