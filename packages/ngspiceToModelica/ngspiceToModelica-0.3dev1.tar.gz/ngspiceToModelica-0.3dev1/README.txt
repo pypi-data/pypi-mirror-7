a8155dde2a653663749cce14690413b49b41aae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:23 +0000

    Add package ngspiceToModelica/0.3dev1/ngspiceToModelica-0.3dev1.tar.gz

---
+++ packages/ngspiceToModelica/ngspiceToModelica-0.3dev1.tar.gz/ngspiceToModelica-0.3dev1/README.txt
@@ -0,0 +1,9 @@
+Ngspice to Modelica Converter
+------------------------------
+
+Author: Rakhi R, IIT Bombay, India, 2014
+
+This project converts an ngspice netlist to Modelica code. It can handle device models for diode, BJTs and MOSFETs, and subcircuits. Xspice is not currently supported.
+
+For MOSFET based Modelica models, BondLib3 library for Modelica 3 is required. 
+
---
+++ packages/ngspiceToModelica/ngspiceToModelica-0.3dev1.tar.gz/ngspiceToModelica-0.3dev1/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+desc = """\
+More details on the project
+===========================
+This project converts an ngspice netlist to Modelica code. It can handle device models for diode, BJTs and MOSFETs, and subcircuits. Xspice is not currently supported.
+"""
+
+setup(
+    name='ngspiceToModelica',
+    version='0.3dev1',
+    description='A python project to convert ngspice netlists to Modelica code',
+    long_description=desc,
+    url='https://pypi.python.org/pypi/ngspiceToModelica',
+    author='Rakhi R',
+    author_email='rakhiwarriar@iitb.ac.in',
+    license='GPL',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+	'Intended Audience :: End Users/Desktop',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='ngspice to modelica code conversion',
+    py_modules = ['NgspicetoModelica']
+)
