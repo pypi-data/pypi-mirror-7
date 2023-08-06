fce8178262d6ea8664fdf7a6031f817d29ef2b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:53 +0000

    Add package complyadvantage/0.2/complyadvantage-0.2.tar.gz

---
+++ packages/complyadvantage/complyadvantage-0.2.tar.gz/complyadvantage-0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'complyadvantage',
+  packages = ['complyadvantage'],
+  version = '0.2',
+  description = 'ComplyAdvantage sanctions API wrapper',
+  author = 'ComplyAdvantage',
+  author_email = 'jake@complyadvantage.com',
+  url = 'https://github.com/complyadvantage/python-sanctions-api', # use the URL to the github repo
+  download_url = 'https://github.com/complyadvantage/python-sanctions-api/tarball/0.2', # I'll explain this in a second
+  keywords = ['complyadvantage', 'sanctions', 'api'], # arbitrary keywords
+  classifiers = [],
+  requires=[
+        "requests",
+  ]
+)
