cf350150cd9218314181c7fca666889860b5653c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:46 +0000

    Add package pyaltmetric/0.1.1/pyaltmetric-0.1.1.tar.gz

---
+++ packages/pyaltmetric/pyaltmetric-0.1.1.tar.gz/pyaltmetric-0.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pyaltmetric',
+    version='0.1.1',
+    packages=['pyaltmetric'],
+    description='Python Altmetric API v1 wrapper',
+    long_description=open('README.rst').read(),
+    author='Will Earp',
+    author_email='will.earp@icloud.com',
+    url='https://github.com/wearp/pyaltmetric',
+    install_requires=['requests'],
+    license="BSD",
+    zip_safe=True,
+    keywords='Altmetric altmetrics api wrapper',
+    test_suite='tests',
+)
