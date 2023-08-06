f9f71cec552e871dd89bbacb5703f050495cd707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:07 +0000

    Add package aioawait/1/aioawait-1.tar.gz

---
+++ packages/aioawait/aioawait-1.tar.gz/aioawait-1/aioawait.py
@@ -0,0 +1,42 @@
+"""
+asyncio.await.py
+~~~~~~~~~~~~~~~~
+
+This module implements **await** and **spawn** on top of
+Python 3's asyncio infrastructure. To learn more see:
+
+    https://pypi.python.org/pypi/asyncio.await
+
+:copyright: (c) 2014 by Carlo Pires <carlopires@gmail.com>.
+:license: MIT, see COPYING for more details.
+"""
+from asyncio.tasks import iscoroutinefunction, Task, async
+from asyncio.events import get_event_loop
+
+__version__ = 1
+
+def await(coro, loop=None):
+    if loop is None:
+        loop = get_event_loop()
+
+    if iscoroutinefunction(coro):
+        coro = coro()
+        
+    if loop.is_running():
+        if isinstance(coro, Task):
+            future = coro
+        else:
+            future = Task(coro, loop=loop)
+        while not future.done():
+            loop._run_once()
+        Task._current_tasks[loop] = future
+        return future.result()
+    else:
+        return loop.run_until_complete(coro)
+
+def spawn(coro, loop=None):
+    if loop is None:
+        loop = get_event_loop()
+    if iscoroutinefunction(coro):
+        coro = coro()
+    return async(coro, loop=loop)
---
+++ packages/aioawait/aioawait-1.tar.gz/aioawait-1/setup.py
@@ -0,0 +1,39 @@
+"""
+Created on 19/07/2014
+@author: Carlo Pires <carlopires@gmail.com>
+"""
+from setuptools import setup
+
+def get_version():
+    from aioawait import __version__
+    return __version__
+
+def get_long_description():
+    with open('README.rst') as readme:
+        return readme.read()
+
+setup(
+    name='aioawait',
+    version=get_version(),
+    url='https://bitbucket.org/carlopires/aioawait',
+    author='Carlo Pires',
+    author_email='carlopires@gmail.com',
+    description="Call asynchronous functions of python 3 asyncio infrastructure from synchronous code",
+    long_description=get_long_description(),
+    zip_safe=True,
+    py_modules=['aioawait'],
+    platforms='any',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Testing',        
+        'Topic :: Software Development :: Libraries',        
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
