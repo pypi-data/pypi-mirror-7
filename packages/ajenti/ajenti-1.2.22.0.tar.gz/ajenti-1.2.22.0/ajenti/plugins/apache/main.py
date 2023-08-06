4acf4c5d19cac3a9d7108517889f02834021b0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:42 +0000

    Add package ajenti/1.2.22.0/ajenti-1.2.22.0.tar.gz

---
+++ packages/ajenti/ajenti-1.2.22.0.tar.gz/ajenti-1.2.22.0/ajenti/plugins/apache/main.py
@@ -0,0 +1,52 @@
+import ajenti
+from ajenti.api import *
+from ajenti.plugins.webserver_common.api import WebserverPlugin
+from ajenti.util import platform_select
+
+
+@plugin
+class Apache (WebserverPlugin):
+    service_name = 'apache2'
+    service_buttons = [
+        {
+            'command': 'force-reload',
+            'text': _('Reload'),
+            'icon': 'step-forward',
+        }
+    ]
+    hosts_available_dir = platform_select(
+        debian='/etc/apache2/sites-available',
+        centos='/etc/httpd/conf.d',
+        freebsd='/usr/local/etc/apache/sites-available',
+    )
+    hosts_enabled_dir = '/etc/apache2/sites-enabled'
+    supports_host_activation = platform_select(
+        debian=True,
+        default=False,
+    )
+
+    template = """<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+
+    DocumentRoot /var/www
+
+    <Directory />
+            Options FollowSymLinks
+            AllowOverride None
+    </Directory>
+
+    <Directory /var/www/>
+            Options Indexes FollowSymLinks MultiViews
+            AllowOverride None
+            Order allow,deny
+            allow from all
+    </Directory>
+</VirtualHost>
+"""
+
+    def init(self):
+        self.title = 'Apache'
+        self.category = _('Software')
+        self.icon = 'globe'
+        if ajenti.platform == 'centos':
+            self.service_name = 'httpd'
