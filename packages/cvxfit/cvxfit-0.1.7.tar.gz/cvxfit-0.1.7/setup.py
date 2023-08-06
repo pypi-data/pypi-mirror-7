7e4a643b56016a9984e11215aa274a4149c1161f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:05 +0000

    Add package cvxfit/0.1.7/cvxfit-0.1.7.tar.gz

---
+++ packages/cvxfit/cvxfit-0.1.7.tar.gz/cvxfit-0.1.7/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "cvxfit",
+    version = "0.1.7",
+    author = "Mainak Chowdhury, Alon Kipnis, Milind Rao",
+    author_email = "mainakch@gmail.com",
+    description = ("A package for fitting convex functions to data. "),
+    license = "BSD",
+    keywords = "convex fit",
+    url = "http://packages.python.org/cvxfit",
+    packages=['cvxfit'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+    ],
+    install_requires=['scipy>=0.13.0'],
+)
