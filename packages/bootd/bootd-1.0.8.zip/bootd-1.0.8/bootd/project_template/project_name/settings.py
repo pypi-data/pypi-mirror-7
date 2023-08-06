28bb2ff9d7a3e1cd709ccf3a135c875ab7f93793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package bootd/1.0.8/bootd-1.0.8.zip

---
+++ packages/bootd/bootd-1.0.8.zip/bootd-1.0.8/bootd/project_template/project_name/settings.py
@@ -0,0 +1,47 @@
+from .common_settings import *
+
+
+INSTALLED_APPS += []
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+]
+
+if DEBUG:
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        INSTALLED_APPS.append('debug_toolbar')
+        INTERNAL_IPS = ['127.0.0.1']
+        MIDDLEWARE_CLASSES.insert(
+            MIDDLEWARE_CLASSES.index('django.middleware.common.CommonMiddleware') + 1,
+            'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+try:
+    import picker
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS.append('picker')
+    PICKER_INSTALLED_APPS = (
+        'jquery',
+        'bootstrap',
+        'less',
+    )
