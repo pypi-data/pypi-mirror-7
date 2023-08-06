4d7011cc5fff47c0e56adb49ca9f5e9decb4c46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:03 +0000

    Add package djorm-ext-pool/0.8.2/djorm-ext-pool-0.8.2.tar.gz

---
+++ packages/djorm-ext-pool/djorm-ext-pool-0.8.2.tar.gz/djorm-ext-pool-0.8.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+description="""
+DB-API2 connection pool for Django (for postgresql, mysql and sqlite)
+"""
+
+setup(
+    name = "djorm-ext-pool",
+    version = '0.8.2',
+    url = 'https://github.com/djangonauts/djorm-ext-pool',
+    license = 'BSD',
+    platforms = ['OS Independent'],
+    description = description.strip(),
+    author = 'Andrey Antukh',
+    author_email = 'niwi@niwi.be',
+    maintainer = 'Andrey Antukh',
+    maintainer_email = 'niwi@niwi.be',
+    packages = find_packages(),
+    include_package_data = False,
+    install_requires = [
+        'sqlalchemy >= 0.7.5',
+    ],
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
