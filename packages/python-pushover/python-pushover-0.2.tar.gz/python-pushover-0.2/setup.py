51537027db7c45eb473d1de84ae799da19b6cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:48 +0000

    Add package python-pushover/0.2/python-pushover-0.2.tar.gz

---
+++ packages/python-pushover/python-pushover-0.2.tar.gz/python-pushover-0.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='python-pushover',
+      version='0.2',
+      description="Comprehensive bindings and command line utility for the "
+                  "Pushover notification service",
+      long_description=open("README.rst").read() + "\n"
+      + open("AUTHORS.rst").read() + "\n" + open("CHANGES.rst").read(),
+      url='https://github.com/Thibauth/python-pushover',
+      author='Thibaut Horel',
+      author_email='thibaut.horel+pushover@gmail.com',
+      py_modules=['pushover'],
+      entry_points={"console_scripts": ["pushover = pushover:main"]},
+      install_requires=['requests>=1.0'],
+      use_2to3=True,
+      license='GNU GPLv3'
+      )
