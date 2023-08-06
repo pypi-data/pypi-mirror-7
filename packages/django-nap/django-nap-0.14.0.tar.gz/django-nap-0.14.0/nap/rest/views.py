589394be54e7cca0f44eb4774db36ff42bacc41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:29 +0000

    Add package django-nap/0.14.0/django-nap-0.14.0.tar.gz

---
+++ packages/django-nap/django-nap-0.14.0.tar.gz/django-nap-0.14.0/nap/rest/views.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+
+from .publisher import Publisher, SimplePatternsMixin
+from .models import ModelPublisher
+
+from django.template.response import TemplateResponse
+
+
+class ViewPublisherMixin(object):
+    '''
+    A Publisher for non-API views.
+
+    Replaces create_response with a wrapper around TemplateResponse.
+    '''
+    api_name = None
+
+    def create_response(self, content, **response_kwargs):
+        '''Return a response serialising the content'''
+        template = response_kwargs.pop('template', None)
+        if not template:
+            template = self.get_template_names()
+        return TemplateResponse(self.request, template, content,
+            **response_kwargs
+        )
+
+
+class ViewPublisher(SimplePatternsMixin, Publisher):
+    def get_template_name(self):
+        return self.template_name % {
+            'action': self.action,
+            'method': self.method,
+            'mode': self.mode,
+        }
+
+
+class ModelViewPublisher(SimplePatternsMixin, ModelPublisher):
+    model = None
+
+    def get_template_name(self):
+        '''Return the template name to use for this view.'''
+        return '%s/%s_%s.html' % (
+            self.api_name or self.model._meta.app_label,
+            self.model._meta.model_name,
+            self.action,
+        )
