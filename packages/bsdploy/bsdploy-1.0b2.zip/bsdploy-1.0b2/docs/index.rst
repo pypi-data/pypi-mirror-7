6819eee81f29b2696c07a321d2d5dc4833a5d329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:53 +0000

    Add package bsdploy/1.0b2/bsdploy-1.0b2.zip

---
+++ packages/bsdploy/bsdploy-1.0b2.zip/bsdploy-1.0b2/docs/index.rst
@@ -0,0 +1,95 @@
+.. include:: ../README.rst
+
+.. warning:: BSDploy is currently still in beta. While it's not considered ready for production, it is now in feature freeze mode and you are encouraged to :doc:`give it a spin </quickstart>` and `report any issues <http://github.com/ployground/bsdploy/issues>`_ that you find, so it can move to a 1.0 release – thanks!
+
+
+Dive in
+-------
+
+.. toctree::
+   :maxdepth: 2
+
+   installation
+   quickstart
+
+
+Setup
+-----
+
+How to setup a host from scratch or make an existing one ready for BSDploy:
+
+.. toctree::
+   :maxdepth: 2
+
+   setup/overview
+   setup/provisioning-plain
+   setup/provisioning-virtualbox
+   setup/bootstrapping
+   setup/configuration
+
+
+General usage
+-------------
+
+How to create an manage jails once the host is set up:
+
+.. toctree::
+   :maxdepth: 2
+
+   usage/jails
+   usage/ansible
+
+
+Special use cases
+-----------------
+
+.. toctree::
+   :maxdepth: 2
+
+   advanced/staging
+   advanced/updating
+
+
+Contribute
+----------
+
+Code and issues are hosted at github:
+
+- Issue Tracker: http://github.com/ployground/bsdploy/issues
+- Source Code: http://github.com/tomster/bsdploy
+- IRC: the developers can be found on #bsdploy on freenode.net
+
+
+License
+-------
+
+The project is licensed under the Beerware license.
+
+
+TODO
+----
+
+The following features already exist but still need to be documented:
+
+- provisioning + bootstrapping
+   - EC2 (daemonology based)
+   - pre-configured SSH server keys
+- jail access
+   - port forwarding
+   - public IP
+- fabric support
+- combining ansible and fabric
+- ZFS management
+- Creating and restoring ZFS snapshots
+- poudriere support
+- Upgrading strategies
+- 'vagrant mode' (use - virtualized - jails as development environment)
+
+The following features don't exist yet but should eventually :)
+
+- OS installers
+   - homebrew
+   - freebsd ports + pkgng
+   - ?
+- support ``bootstrap-rc-XXX``
+- support vmware explicitly (like virtualbox)?
---
+++ packages/bsdploy/bsdploy-1.0b2.zip/bsdploy-1.0b2/setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+version = "1.0b2"
+
+install_requires = [
+    'PyYAML',
+    'jinja2',
+    'setuptools',
+    'ploy>=1.0rc11',
+    'ploy_ansible>=1.0b6',
+    'ploy_ezjail>=1.0b8',
+    'ploy_fabric>=1.0b4',
+    'ploy_virtualbox>=1.0b2',
+]
+
+# workaround for installing via buildout, as ansible
+# violates its sandbox limitations
+try:
+    import ansible  # noqa
+except ImportError:
+    install_requires.append('ansible')
+
+
+setup(
+    version=version,
+    description="A tool to provision, configure and maintain FreeBSD jails",
+    name="bsdploy",
+    author='Tom Lazar',
+    author_email='tom@tomster.org',
+    url='http://github.com/ployground/bsdploy',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+    ],
+    license='Beerware Licence',
+    zip_safe=False,
+    packages=['bsdploy'],
+    install_requires=install_requires,
+    extras_require={
+        'development': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'coverage',
+            'jarn.mkrelease',
+            'pytest >= 2.4.2',
+            'pytest-flakes',
+            'pytest-pep8',
+            'tox',
+            'mock',
+        ],
+    },
+    entry_points="""
+        [console_scripts]
+        ploy = ploy:ploy
+        ploy-ssh = ploy:ploy_ssh
+        ploy-download = bsdploy.download:run
+        [ansible_paths]
+        bsdploy = bsdploy:ansible_paths
+        [ploy.plugins]
+        bsdploy = bsdploy:plugin
+    """)
