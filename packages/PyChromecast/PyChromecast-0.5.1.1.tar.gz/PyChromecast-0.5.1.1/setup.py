61f5eca92ed8d81dbca1a7171a5355b761149c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:21 +0000

    Add package PyChromecast/0.5.1.1/PyChromecast-0.5.1.1.tar.gz

---
+++ packages/PyChromecast/PyChromecast-0.5.1.1.tar.gz/PyChromecast-0.5.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='PyChromecast',
+    version='0.5.1.1',
+    license='MIT',
+    url='https://github.com/balloob/pychromecast',
+    author='Paulus Schoutsen',
+    author_email='paulus@paulusschoutsen.nl',
+    description='Python module to talk to Google Chromecast.',
+    packages=['pychromecast'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['requests', 'ws4py'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
