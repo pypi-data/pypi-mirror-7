b566a5ecee34aab11f4138b39157493754eac952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:03 +0000

    Add package Touchandgo/0.4/Touchandgo-0.4.tar.gz

---
+++ packages/Touchandgo/Touchandgo-0.4.tar.gz/Touchandgo-0.4/touchandgo/helpers.py
@@ -0,0 +1,92 @@
+import os
+import signal
+import socket
+
+from daemon import DaemonContext
+from datetime import datetime
+from lock import Lock
+from os import mkdir
+from os.path import getmtime, exists
+
+from netifaces import interfaces, ifaddresses
+
+from ojota import set_data_source
+
+
+LOCKFILE = "/tmp/touchandgo"
+
+
+def get_free_port():
+    socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_.bind(('localhost', 0))
+    addr, port = socket_.getsockname()
+    socket_.close()
+    return port
+
+
+def get_interface():
+    for ifaceName in interfaces():
+        addresses = ifaddresses(ifaceName)
+        for address in addresses.values():
+            for item in address:
+                if item.get('netmask') is not None and \
+                        not item['addr'].startswith("127") and \
+                        not item['addr'].startswith(":") and \
+                        len(item['addr']) < 17:
+                    return item['addr']
+
+
+def is_process_running(process_id):
+    try:
+        os.kill(process_id, 0)
+        return True
+    except OSError:
+        return False
+
+
+def daemonize(args, callback):
+    with DaemonContext():
+        create_process = False
+        lock = Lock(LOCKFILE, os.getpid(), args.name, args.sea_ep[0],
+                    args.sea_ep[1], args.port)
+        if lock.is_locked():
+            lock_pid = lock.get_pid()
+            if not lock.is_same_file(args.name, args.sea_ep[0],
+                                     args.sea_ep[1]) \
+                    or not is_process_running(lock_pid):
+                try:
+                    os.kill(lock_pid, signal.SIGQUIT)
+                except OSError:
+                    pass
+                except TypeError:
+                    pass
+                lock.break_lock()
+                create_process = True
+        else:
+            create_process = True
+
+        if create_process:
+            lock.acquire()
+            callback(args.name, season=args.sea_ep[0], episode=args.sea_ep[1],
+                     serve=True, port=args.port)
+            lock.release()
+
+
+def get_lock_diff():
+    timediff = 0
+    try:
+        now = datetime.now()
+        timediff = now - datetime.fromtimestamp(getmtime(LOCKFILE + ".lock"))
+        timediff = timediff.total_seconds()
+    except OSError:
+        pass
+    return timediff
+
+
+def set_config_dir():
+    data_folder = "%s/.touchandgo" % os.getenv("HOME")
+    if not exists(data_folder):
+        mkdir(data_folder)
+
+    set_data_source(data_folder)
+
