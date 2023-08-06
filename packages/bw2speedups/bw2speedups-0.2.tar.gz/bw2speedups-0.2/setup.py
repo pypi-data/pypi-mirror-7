75bc6958c5fb2e6adb616c3e1e08d762bc0ffde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:07 +0000

    Add package bw2speedups/0.2/bw2speedups-0.2.tar.gz

---
+++ packages/bw2speedups/bw2speedups-0.2.tar.gz/bw2speedups-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+# from Cython.Distutils import build_ext
+import numpy as np
+
+SRC_DIR = "bw2speedups"
+
+extension_1 = Extension(SRC_DIR + "._indexer",
+                        # [SRC_DIR + "/_indexer.pyx"],
+                        [SRC_DIR + "/_indexer.c"],
+                        libraries=[],
+                        include_dirs=[np.get_include()])
+
+
+if __name__ == "__main__":
+    setup(
+        author="Chris Mutel",
+        author_email="cmutel@gmail.com",
+        # cmdclass = {'build_ext': build_ext},
+        description="""Cython functions to speed up Brightway calculations""",
+        ext_modules = [extension_1],
+        install_requires=['numpy', 'cython'],
+        name = 'bw2speedups',
+        packages=[SRC_DIR],
+        version="0.2",
+        zip_safe=False,
+    )
