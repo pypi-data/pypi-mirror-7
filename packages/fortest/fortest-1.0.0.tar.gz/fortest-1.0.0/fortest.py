2627a4750eb3268dd6d31857b3e4e8c7b1f2f8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package fortest/1.0.0/fortest-1.0.0.tar.gz

---
+++ packages/fortest/fortest-1.0.0.tar.gz/fortest-1.0.0/fortest.py
@@ -0,0 +1,7 @@
+""" this is an area for list """
+def print_lol(the_list):
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item)
+                else:
+                        print(each_item)
