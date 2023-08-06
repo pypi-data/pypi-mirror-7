feaf77a5edfd0ae8cf5bc299e964f9adef005339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:55 +0000

    Add package libnacl/1.0.0/libnacl-1.0.0.tar.gz

---
+++ packages/libnacl/libnacl-1.0.0.tar.gz/libnacl-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Import python libs
+import os
+import sys
+# Import libnacl libs
+from libnacl.version import __version__
+
+if 'USE_SETUPTOOLS' in os.environ or 'setuptools' in sys.modules:
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+NAME = 'libnacl'
+DESC = ('Python bindings for libsodium/tweetnacl based on ctypes')
+VERSION = __version__
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESC,
+      author='Thomas S Hatch',
+      author_email='thatch45@gmail.com',
+      url='https://github.com/thatch45/nacl',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Security :: Cryptography',
+          ],
+      packages=['libnacl'])
