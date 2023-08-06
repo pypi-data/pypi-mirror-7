c72f36c1e389eac8e303b5eb28e8cf89b4db5f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:00 +0000

    Add package pyage-forams/0.6.2/pyage_forams-0.6.2-py2.7.egg

---
+++ packages/pyage-forams/pyage_forams-0.6.2-py2.7.egg/pyage_forams/conf/foram_simple.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+from pyage.core import address
+
+from pyage.core.stop_condition import StepLimitStopCondition
+from pyage_forams.solutions.environment import environment_factory, Environment3d
+from pyage_forams.solutions.foram import create_forams, create_agent
+from pyage_forams.solutions.genom import GenomFactory
+from pyage_forams.solutions.insolation_meter import StaticInsolation, DynamicInsolation
+from pyage_forams.solutions.statistics import SimpleStatistics, PsiStatistics
+
+
+factory = GenomFactory(chambers_limit=5)
+genom_factory = lambda: factory.generate
+forams = create_forams(8, initial_energy=5)
+agents = create_agent
+insolation_meter = lambda: DynamicInsolation([(20, 10, 0.2), (10, 20, 0.4)])
+size = lambda: 10
+
+reproduction_minimum = lambda: 10
+movement_energy = lambda: 0.25
+growth_minimum = lambda: 10
+energy_need = lambda: 0.2
+
+environment = environment_factory(regeneration_factor=0.1, clazz=Environment3d)
+
+stop_condition = lambda: StepLimitStopCondition(90)
+
+address_provider = address.SequenceAddressProvider
+stats = PsiStatistics
