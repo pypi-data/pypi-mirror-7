0366c9ecf411641eb978586c9ca19fe59de6b2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:49 +0000

    Add package rainbowstream/0.0.3/rainbowstream-0.0.3.tar.gz

---
+++ packages/rainbowstream/rainbowstream-0.0.3.tar.gz/rainbowstream-0.0.3/rainbowstream/c_image.py
@@ -0,0 +1,48 @@
+from PIL import Image
+from os.path import join, dirname, getmtime, exists, expanduser
+from .config import *
+
+import ctypes
+import sys
+import os
+
+
+def call_c():
+    library = expanduser('~/.image.so')
+    sauce = join(dirname(__file__), 'image.c')
+    if not exists(library) or getmtime(sauce) > getmtime(library):
+        build = "gcc -fPIC -shared -o %s %s" % (library, sauce)
+        assert os.system(build + " >/dev/null 2>&1") == 0
+    image_c = ctypes.cdll.LoadLibrary(library)
+    image_c.init()
+    return image_c.rgb_to_ansi
+
+rgb2short = call_c()
+
+
+def pixel_print(ansicolor):
+    sys.stdout.write('\033[48;5;%sm \033[0m' % (ansicolor))
+
+
+def image_to_display(path):
+    i = Image.open(path)
+    i = i.convert('RGBA')
+    w, h = i.size
+    i.load()
+    rows, columns = os.popen('stty size', 'r').read().split()
+    width = min(w, int(columns) - 2 * IMAGE_SHIFT)
+    height = int(float(h) * (float(width) / float(w)))
+    height //= 2
+    i = i.resize((width, height), Image.ANTIALIAS)
+    height = min(height, IMAGE_MAX_HEIGHT)
+
+    for y in xrange(height):
+        print ' ' * IMAGE_SHIFT,
+        for x in xrange(width):
+            p = i.getpixel((x, y))
+            r, g, b = p[:3]
+            pixel_print(rgb2short(r, g, b))
+        print ''
+
+if __name__ == '__main__':
+    image_to_display(sys.argv[1])
---
+++ packages/rainbowstream/rainbowstream-0.0.3.tar.gz/rainbowstream-0.0.3/rainbowstream/config.py
@@ -0,0 +1,21 @@
+# Max Search record
+SEARCH_MAX_RECORD = 5
+# Default home tweet
+HOME_TWEET_NUM = 5
+# Autocomplete history
+HISTORY_FILENAME = 'completer.hist'
+
+# Stream Domain
+USER_DOMAIN = 'userstream.twitter.com'
+PUBLIC_DOMAIN = 'stream.twitter.com'
+SITE_DOMAIN = 'sitestream.twitter.com'
+# Actually called
+DOMAIN = USER_DOMAIN
+
+# Filter and Ignore list ex: ['@fat','@mdo']
+ONLY_LIST = []
+IGNORE_LIST = []
+
+# Image shift and size
+IMAGE_SHIFT = 10
+IMAGE_MAX_HEIGHT = 40
