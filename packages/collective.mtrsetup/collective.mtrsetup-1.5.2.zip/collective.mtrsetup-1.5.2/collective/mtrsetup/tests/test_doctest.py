8d9517f94f4003a0e867413d8a78b05ca18d2e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:38 +0000

    Add package collective.mtrsetup/1.5.2/collective.mtrsetup-1.5.2.zip

---
+++ packages/collective.mtrsetup/collective.mtrsetup-1.5.2.zip/collective.mtrsetup-1.5.2/collective/mtrsetup/tests/test_doctest.py
@@ -0,0 +1,28 @@
+import unittest
+import doctest
+
+#from zope.testing import doctestunit
+#from zope.component import testing, eventtesting
+
+from Testing import ZopeTestCase as ztc
+
+from collective.mtrsetup.tests import base
+
+functionl_doctest_files = [
+    '../../README.rst',
+    'tests/profile.txt',
+    ]
+
+def test_suite():
+    return unittest.TestSuite([
+            # Demonstrate the main content types
+            ztc.ZopeDocFileSuite(
+                file_, package='collective.mtrsetup',
+                test_class=base.FunctionalTestCase,
+                optionflags=doctest.REPORT_ONLY_FIRST_FAILURE |
+                doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
+            for file_ in functionl_doctest_files
+            ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ packages/collective.mtrsetup/collective.mtrsetup-1.5.2.zip/collective.mtrsetup-1.5.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.5.2'
+
+tests_require = [
+    'Plone',
+    'zope.testing',
+    'Products.PloneTestCase',
+    ]
+
+setup(name='collective.mtrsetup',
+      version=version,
+      description="Extension for GenericSetup, adding support for import / export of mimetypes_registry",
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='generic setup gs mimetypes registry import export',
+      author='Jonas Baumann, 4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/collective/collective.mtrsetup',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'Products.GenericSetup',
+        'Products.MimetypesRegistry',
+        'z3c.autoinclude',
+        # -*- Extra requirements: -*-
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      #      test_suite = 'collective.mtrsetup.tests.test'
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
