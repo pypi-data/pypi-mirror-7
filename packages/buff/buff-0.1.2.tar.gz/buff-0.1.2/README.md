69bd223bc916abfca5de917f360db0a089a1ed85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package buff/0.1.2/buff-0.1.2.tar.gz

---
+++ packages/buff/buff-0.1.2.tar.gz/buff-0.1.2/README.md
@@ -0,0 +1,4 @@
+buff
+====
+
+Buffer and protocol builder and designer
---
+++ packages/buff/buff-0.1.2.tar.gz/buff-0.1.2/buff/bin.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def buff():
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
