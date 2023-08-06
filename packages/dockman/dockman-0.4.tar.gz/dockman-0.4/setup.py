3f2619074cab6010031cf82ec213e61bf8a4442a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:40 +0000

    Add package dockman/0.4/dockman-0.4.tar.gz

---
+++ packages/dockman/dockman-0.4.tar.gz/dockman-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+def read(filename):
+    with open(filename) as fp:
+        return fp.read()
+
+setup(
+    name='dockman',
+    version='0.4',
+    author='Mark McGuire',
+    author_email='mark.b.mcg@gmail.com',
+    url='http://github.com/TronPaul/dockman',
+    description='A webservice that runs docker builds/deployments on docker webhooks',
+    long_description=read('README.rst'),
+    py_modules=['dockman'],
+    zip_safe=False,
+    install_requires=['flask', 'docker-py']
+)
