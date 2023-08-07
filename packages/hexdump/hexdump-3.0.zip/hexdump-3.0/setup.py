e09214363b888674fe54c08a05ff5646192ccb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:22 +0000

    Add package hexdump/3.0/hexdump-3.0.zip

---
+++ packages/hexdump/hexdump-3.0.zip/hexdump-3.0/setup.py
@@ -0,0 +1,54 @@
+from distutils.core import setup
+
+from os.path import dirname, join
+
+ROOT = dirname(__file__)
+
+def get_version(relpath):
+    """read version info from file without importing it"""
+    from os.path import dirname, join
+    # Below is a hack to read text file with cp1252 characters
+    # in Python2/3 compatible way. Reading this text file
+    # without specifying encoding will fail in Python 3 on some
+    # systems (see http://goo.gl/5XmOH). Specifying encoding as
+    # as open() parameter is incompatible with Python 2
+    for line in open(join(dirname(__file__), relpath), 'rb'):
+        line = line.decode('cp1252')
+        if '__version__' in line:
+            if '"' in line:
+                # __version__ = "0.9"
+                return line.split('"')[1]
+            elif "'" in line:
+                return line.split("'")[1]
+
+# Distutils 'API' to ship test data along with hexdump.py
+# http://stackoverflow.com/questions/1612733/including-non-python-files-with-setup-py
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+  scheme['data'] = scheme['purelib']
+
+
+setup(
+    name='hexdump',
+    version=get_version('hexdump.py'),
+    author='anatoly techtonik <techtonik@gmail.com>',
+    url='http://bitbucket.org/techtonik/hexdump/',
+
+    description="view/edit your binary with any text editor",
+    license="Public Domain",
+    classifiers=[
+        #'Environment :: Console',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        #'Topic :: Utilities',
+    ],
+
+    py_modules=['hexdump'],
+    data_files=[('', ['hexfile.bin'])],
+
+    long_description=open(join(ROOT, 'README.txt')).read(),
+)
