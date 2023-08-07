2b0dd25f686e1304e1d74e98a568e215cf60ce49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:06 +0000

    Add package numeronym/1.0.4/numeronym-1.0.4.tar.gz

---
+++ packages/numeronym/numeronym-1.0.4.tar.gz/numeronym-1.0.4/numeronym/main.py
@@ -0,0 +1,36 @@
+class Numeronym:
+    def __init__(self, short=None, lower=None):
+        # Default variables
+        allow_short             = False
+        return_lower            = True
+        
+        self.allow_short        = allow_short
+        if short:
+            self.allow_short    = short
+        
+        self.return_lower       = return_lower
+        if lower:
+            self.return_lower   = lower
+        
+        
+    def encode(self, input):
+        if len(input) < 4:
+            if self.allow_short == True:
+                f = input[0]
+                output = "%s%d" % (f, len(input[1:]))
+                if self.return_lower == True:
+                    output = output.lower()
+
+                return output
+            else:
+                raise Exception("Input string must be at least four characters in length")
+
+        f = input[0]
+        l = input[-1]
+
+        output = "%s%d%s" % (f, len(input[1:-1]), l)
+
+        if self.return_lower == True:
+            output = output.lower()
+
+        return output
