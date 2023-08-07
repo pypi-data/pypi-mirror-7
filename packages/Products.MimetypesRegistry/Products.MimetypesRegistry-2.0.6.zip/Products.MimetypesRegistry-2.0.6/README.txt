56d06ef65cebfa4dc14fe23cf7cfea66ba0b4f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:11 +0000

    Add package Products.MimetypesRegistry/2.0.6/Products.MimetypesRegistry-2.0.6.zip

---
+++ packages/Products.MimetypesRegistry/Products.MimetypesRegistry-2.0.6.zip/Products.MimetypesRegistry-2.0.6/README.txt
@@ -0,0 +1,21 @@
+Mimetypes Registry
+==================
+
+ * mimetypes_registry (the mimetypes tool) : handle mime types information
+
+
+Authors
+=======
+Benjamin Saller <bcsaller@yahoo.com>
+Sidnei da Silva  <sidnei@x3ng.com>
+Sylvain Thenault <sylvain.thenault@logilab.fr>
+Christian Heimes <tiran@cheimes.de>
+
+Credits
+=======
+Icons from:
+
+  * Plone: http://plone.org
+  * Tango: http://tango.freedesktop.org
+  * FamFamFam: http://www.famfamfam.com
+
---
+++ packages/Products.MimetypesRegistry/Products.MimetypesRegistry-2.0.6.zip/Products.MimetypesRegistry-2.0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '2.0.6'
+
+setup(name='Products.MimetypesRegistry',
+      version=version,
+      description="MIME type handling for Zope",
+      long_description=open("README.txt").read() + "\n" + \
+              open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Operating System :: OS Independent",
+        ],
+      keywords='Zope catalog index',
+      author='Benjamin Saller',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.MimetypesRegistry',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'Products.Archetypes',
+        ]
+      ),
+      install_requires=[
+          'setuptools',
+          'zope.contenttype',
+          'zope.interface',
+          'Products.CMFCore',
+          'Acquisition',
+          'ZODB3',
+          'Zope2',
+      ],
+      )
