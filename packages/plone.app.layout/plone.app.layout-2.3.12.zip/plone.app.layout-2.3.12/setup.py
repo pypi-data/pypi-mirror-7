bde9256ead9c727c35312c369d75146f730432a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:16 +0000

    Add package plone.app.layout/2.3.12/plone.app.layout-2.3.12.zip

---
+++ packages/plone.app.layout/plone.app.layout-2.3.12.zip/plone.app.layout-2.3.12/setup.py
@@ -0,0 +1,72 @@
+from setuptools import find_packages
+from setuptools import setup
+
+version = '2.3.12'
+long_description = (
+    open('README.rst').read() + '\n' +
+    open('CHANGES.rst').read()
+)
+
+setup(name='plone.app.layout',
+      version=version,
+      description="Layout mechanisms for Plone",
+      long_description=long_description,
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+      ],
+      keywords='plone layout viewlet',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.layout',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'Acquisition',
+          'DateTime',
+          'plone.app.content',
+          'plone.app.portlets',
+          'plone.app.viewletmanager >=1.2',
+          'plone.i18n',
+          'plone.memoize',
+          'plone.portlets',
+          'plone.registry',
+          'Products.CMFCore',
+          'Products.CMFDefault',
+          'Products.CMFDynamicViewFTI',
+          'Products.CMFEditions >=1.2.2',
+          'Products.CMFPlone >=4.3,<5.0',
+          'setuptools',
+          'zope.component',
+          'zope.deprecation',
+          'zope.dottedname',
+          'zope.i18n',
+          'zope.interface',
+          'zope.publisher',
+          'zope.schema',
+          'zope.viewlet',
+          'Zope2',
+      ],
+      extras_require=dict(
+          test=[
+              'plone.app.intid',
+              'plone.app.relationfield',
+              'plone.app.testing',
+              'plone.dexterity',
+              'plone.locking',
+              'plone.testing',
+              'Products.Archetypes',
+              'Products.PloneTestCase',
+              'unittest2',
+              'z3c.relationfield',
+              'zope.annotation',
+          ],
+      ),
+      )
