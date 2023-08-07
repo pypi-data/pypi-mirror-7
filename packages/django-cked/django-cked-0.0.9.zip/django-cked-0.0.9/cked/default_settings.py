fbee846e2ae2566de7f8fccd0e92c07bf62025da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:14 +0000

    Add package django-cked/0.0.9/django-cked-0.0.9.zip

---
+++ packages/django-cked/django-cked-0.0.9.zip/django-cked-0.0.9/cked/default_settings.py
@@ -0,0 +1,11 @@
+# coding: utf8
+
+ELFINDER_DEFAULT_OPTIONS = {
+}
+
+CKEDITOR_DEFAULT_OPTIONS = {
+    'skin': 'moono',
+    'toolbar': 'full',
+    'filebrowserWindowWidth': 940,
+    'filebrowserWindowHeight': 450,
+}
---
+++ packages/django-cked/django-cked-0.0.9.zip/django-cked-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-cked',
+    version='0.0.9',
+    author='Ivan Gorochov',
+    author_email='cwteam@bk.ru',
+    description=('CKEditor and elFinder integration for Django Framework.'),
+    license='BSD',
+    keywords='django, ckeditor, elfinder, wysiwyg, upload',
+    url='https://bitbucket.org/CWTeam/django-cked',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    include_package_data=True
+)
