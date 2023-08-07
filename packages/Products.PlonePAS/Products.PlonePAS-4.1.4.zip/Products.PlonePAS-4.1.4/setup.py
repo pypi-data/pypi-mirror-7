6b19b97a08682cf70cbffd8644abb2f5cdcff2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:11 +0000

    Add package Products.PlonePAS/4.1.4/Products.PlonePAS-4.1.4.zip

---
+++ packages/Products.PlonePAS/Products.PlonePAS-4.1.4.zip/Products.PlonePAS-4.1.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '4.1.4'
+
+setup(name='Products.PlonePAS',
+      version=version,
+      description="PlonePAS adapts the PluggableAuthService for use by Plone.",
+      long_description=open("README.txt").read() + "\n" + \
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+      ],
+      keywords='Zope CMF Plone PAS authentication',
+      author='Kapil Thangavelu, Wichert Akkerman',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.PlonePAS',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'Products.PloneTestCase',
+            'Products.Archetypes',
+        ],
+        atstorage=[
+            'Products.Archetypes',
+        ]
+      ),
+      install_requires=[
+        'setuptools',
+        'plone.memoize',
+        'plone.session',
+        'plone.i18n',
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Products.PluggableAuthService',
+        'Zope2 > 2.12.4',
+      ],
+)
