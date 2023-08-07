db33e6a491f68f6aaed05c75cf32e37258f1c2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:11 +0000

    Add package pyformat/0.5.7/pyformat-0.5.7.tar.gz

---
+++ packages/pyformat/pyformat-0.5.7.tar.gz/pyformat-0.5.7/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""Setup for pyformat."""
+
+import ast
+
+from setuptools import setup
+
+
+def version():
+    """Return version string."""
+    with open('pyformat.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+with open('README.rst') as readme:
+    setup(name='pyformat',
+          version=version(),
+          description='Formats Python code to follow a consistent style.',
+          long_description=readme.read(),
+          license='Expat License',
+          author='Steven Myint',
+          url='https://github.com/myint/pyformat',
+          classifiers=[
+              'Intended Audience :: Developers',
+              'Environment :: Console',
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.2',
+              'Programming Language :: Python :: 3.3',
+              'Programming Language :: Python :: 3.4',
+              'Topic :: Software Development :: Libraries :: Python Modules',
+              'Topic :: Software Development :: Quality Assurance'],
+          keywords='beautify, code, format, formatter, reformat, style',
+          py_modules=['pyformat'],
+          zip_safe=False,
+          install_requires=['autoflake>=0.6.6',
+                            'autopep8>=1.0.3',
+                            'docformatter>=0.7',
+                            'unify>=0.2'],
+          entry_points={
+              'console_scripts': ['pyformat = pyformat:main']},
+          test_suite='test_pyformat')
