7a59ae6fbb80314990e14550bf080515da6fe649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:12 +0000

    Add package django-ad-code/0.5.0/django-ad-code-0.5.0.tar.gz

---
+++ packages/django-ad-code/django-ad-code-0.5.0.tar.gz/django-ad-code-0.5.0/setup.py
@@ -0,0 +1,44 @@
+from __future__ import unicode_literals
+
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='django-ad-code',
+    version=__import__('adcode').__version__,
+    author='Mark Lavin',
+    author_email='markdlavin@gmail.com',
+    packages=find_packages(exclude=['example']),
+    include_package_data=True,
+    url='https://github.com/mlavin/django-ad-code',
+    license='BSD',
+    description=' '.join(__import__('adcode').__doc__.splitlines()).strip(),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.rst'),
+    test_suite="runtests.runtests",
+    zip_safe=False, # because we're including media that Django needs
+)
